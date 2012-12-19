3f4b30deab56f2dc78e2eada8a3bdff34571512d
Graeme Winter <graeme.winter@gmail.com>
2012-12-19 11:45:20 +0000

    More appropriate naming, full automation of calculus

--- scratch/gw/refine.py
+++ scratch/gw/refine.py
@@ -2,28 +2,7 @@ from __future__ import division
 import numpy
 from scipy.optimize import minimize
 
-def f(xy):
-    from math import sin, cos, pow, pi
-    x, y = xy
-    return pow(x * x + y * y + pi * sin(x) + pi * cos(y), 2)
-
-def J(xy):
-    from math import sin, cos, pow, pi
-    x, y = xy
-    return numpy.array(
-        [(4*x + 2*pi*cos(x))*(x**2 + y**2 + pi*sin(x) + pi*cos(y)),
-         (4*y - 2*pi*sin(y))*(x**2 + y**2 + pi*sin(x) + pi*cos(y))])
-
-def H(xy):
-    from math import sin, cos, pow, pi
-    x, y = xy
-    return numpy.matrix(
-        [[(2*x + pi*cos(x))*(4*x + 2*pi*cos(x)) +
-          (-2*pi*sin(x) + 4)*(x**2 + y**2 + pi*sin(x) + pi*cos(y)),
-            (4*x + 2*pi*cos(x))*(2*y - pi*sin(y))],
-            [(2*x + pi*cos(x))*(4*y - 2*pi*sin(y)),
-             (2*y - pi*sin(y))*(4*y - 2*pi*sin(y)) +
-                (-2*pi*cos(y) + 4)*(x**2 + y**2 + pi*sin(x) + pi*cos(y))]])
+from fJH import f, J, H
 
 def run_nelder_mead():
     xy0 = numpy.array([1.0, 1.0])
@@ -43,15 +22,6 @@ def run_newton_cg():
     x, y = res.x
     return x, y, f((x, y))
 
-import time
-t0 = time.time()
-for j in range(1000): run_nelder_mead()
-t1 = time.time()
-for j in range(1000): run_bfgs()
-t2 = time.time()
-for j in range(1000): run_newton_cg()
-t3 = time.time()
-
-print 'Nelder-Mead; %.2f' % (t1 - t0)
-print 'BFGS:        %.2f' % (t2 - t1)
-print 'Newton CG:   %.2f' % (t3 - t2)
+print run_nelder_mead()
+print run_bfgs()
+print run_newton_cg()
--- scratch/gw/sympy_spells.py
+++ scratch/gw/sympy_spells.py
@@ -5,13 +5,7 @@ symbols = 'x y'
 functions = 'sin cos pi'
 f = '(x*x + y*y + pi * sin(x) + pi * cos(y)) ** 2'
 
-x, y = sympy.symbols(symbols)
-dfdx = sympy.diff(f, x)
-dfdy = sympy.diff(f, y)
-dfdxx = sympy.diff(dfdx, x)
-dfdxy = sympy.diff(dfdx, y)
-dfdyx = sympy.diff(dfdy, x)
-dfdyy = sympy.diff(dfdy, y)
+_syms = sympy.symbols(symbols)
 
 fout = open('fJH.py', 'w')
 fout.write('def f(%s):\n' % ''.join(symbols.split()))
@@ -25,12 +19,16 @@ fout.write('    import numpy\n')
 fout.write('    from math import %s\n' % ', '.join(functions.split()))
 fout.write('    %s = %s\n' % (', '.join(symbols.split()),
                               ''.join(symbols.split())))
-fout.write('    return numpy.array([%s, %s])\n' % (dfdx, dfdy))
+fout.write('    return numpy.array([%s])\n' % \
+           ', '.join([str(sympy.diff(f, _s)) for _s in _syms]))
 fout.write('def H(%s):\n' % ''.join(symbols.split()))
 fout.write('    import numpy\n')
 fout.write('    from math import %s\n' % ', '.join(functions.split()))
 fout.write('    %s = %s\n' % (', '.join(symbols.split()),
                               ''.join(symbols.split())))
-fout.write('    return numpy.matrix([[%s, %s], [%s, %s]])\n' % \
-    (dfdxx, dfdxy, dfdyx, dfdyy))
-
+fout.write('    return numpy.matrix([%s])\n' % \
+           (', '.join(
+               ['[%s]' % ', '.join(
+                   [str(sympy.diff(sympy.diff(f, _t), _s)) for _s in _syms])
+                   for _t in _syms])))
+fout.close()
