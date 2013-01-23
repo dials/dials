88b0b02b101a38bbdb86b1438a0f5b07af226dd8
Graeme Winter <graeme.winter@gmail.com>
2013-01-23 09:02:00 +0000

    Starting work on toolbox to handle unmerged reflections gracefully - for xia2 in first instance, useful / needed for DIALS

---
+++ scratch/gw/x2tbx/command_line/make_list.py
@@ -0,0 +1,10 @@
+from toytbx import make_list
+
+def main(args):
+    assert(args)
+    n = int(args[0])
+    print make_list(n)
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv[1:])
