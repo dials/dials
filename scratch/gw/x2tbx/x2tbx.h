370d5bbed0eb9707645db73c4d44ecf53fc5eb5b
Graeme Winter <graeme.winter@gmail.com>
2013-01-25 13:30:48 +0000

    Added comments for doxygen; made code more conventional (removed usage of (void) to (); added trailing underscore to private member variables of classes) and removed some legacy struct definitions which are not used.

--- scratch/gw/x2tbx/x2tbx.h
+++ scratch/gw/x2tbx/x2tbx.h
@@ -4,7 +4,7 @@
  * instance) calculations in PyChef and resolution limits. N.B. will have
  * fundamental data structures:
  *
- * observation - float I, float sigI, float property, int flag
+ * observation - float I, float sigI (at the moment) as a tiny array
  *
  * unmerged_reflections - map(cctbx::miller::index, std::vector<observation>)
  *
@@ -14,6 +14,9 @@
  * first task try just implementing one calculation for e.g. resolution limits.
  *
  */
+
+#ifndef X2TBX_X2TBX_H
+#define X2TBX_X2TBX_H
 
 #include <boost/python.hpp>
 #include <scitbx/array_family/shared.h>
@@ -30,78 +33,83 @@ namespace cuc = cctbx::uctbx;
 
 namespace x2tbx {
 
-  void init_module(void);
-
-  struct observation {
-    float I;
-    float sigI;
-    float property;
-    int flag;
-  };
+  void init_module();
 
   struct merged_isig {
     float I;
     float sigI;
   };
 
-  typedef std::vector<observation> observation_list;
-
   typedef scitbx::af::tiny<float, 2> i_sig_type;
+
+  /**
+   * ObservationList class - to store multiple observations of a given reflection
+   * assuming all belong to one unique miller index.
+   */
 
   class ObservationList {
   public:
-    ObservationList(void);
-    ~ObservationList(void);
+    ObservationList();
 
     void add(i_sig_type);
-    void merge(void);
-    i_sig_type i_sigma(void);
-    float total_i_sigma(void);
-    size_t multiplicity(void);
-    float rmerge(void);
+    void merge();
+    i_sig_type i_sigma();
+    float total_i_sigma();
+    size_t multiplicity();
+    float rmerge();
 
   private:
-    scitbx::af::shared<i_sig_type> observations;
-    float imean, sigimean, total_i_sigi;
+    scitbx::af::shared<i_sig_type> observations_;
+    float imean_, sigimean_, total_i_sigi_;
   };
 
   typedef cmil::index<int> miller_index_type;
   typedef scitbx::af::shared<miller_index_type> miller_index_list_type;
   typedef scitbx::af::shared<float> float_value_list_type;
 
+  /**
+   * ReflectionList class - to store and manipulate an entire data set
+   * consisting of unmerged observations of many unique reflections.
+   */
+
   class ReflectionList {
   public:
     ReflectionList();
-    ~ReflectionList();
 
     void setup(miller_index_list_type,
                float_value_list_type,
                float_value_list_type);
     void set_unit_cell(scitbx::af::tiny<double, 6>);
 
-    miller_index_list_type get_indices(void);
+    miller_index_list_type get_indices();
     void setup_resolution_shells(size_t);
 
-    void merge(void);
-    float i_sigma(void);
-    float total_i_sigma(void);
-    float rmerge(void);
-    scitbx::af::shared<float> rmerge_shells(void);
-    scitbx::af::shared<float> i_sigma_shells(void);
-    scitbx::af::shared<float> total_i_sigma_shells(void);
-    scitbx::af::shared<float> shell_high_limits(void);
-    scitbx::af::shared<float> shell_low_limits(void);
+    void merge();
+    float i_sigma();
+    float total_i_sigma();
+    float rmerge();
+    scitbx::af::shared<float> rmerge_shells();
+    scitbx::af::shared<float> i_sigma_shells();
+    scitbx::af::shared<float> total_i_sigma_shells();
+    scitbx::af::shared<float> shell_high_limits();
+    scitbx::af::shared<float> shell_low_limits();
     miller_index_list_type get_shell(size_t);
 
   private:
-    cuc::unit_cell unit_cell;
-    std::map<miller_index_type, ObservationList> reflections;
-    std::vector<miller_index_list_type> shells;
-    miller_index_list_type unique_indices;
-    scitbx::af::shared<float> high_limits;
-    scitbx::af::shared<float> low_limits;
+    cuc::unit_cell unit_cell_;
+    std::map<miller_index_type, ObservationList> reflections_;
+    std::vector<miller_index_list_type> shells_;
+    miller_index_list_type unique_indices_;
+    scitbx::af::shared<float> high_limits_;
+    scitbx::af::shared<float> low_limits_;
 
   };
+
+  /**
+   * Magic tool used to sort reflection indices in terms of their resolution
+   * order, from the smallest dspacings to the largest. For use in std::sort.
+   * Requires cctbx::uctbx::unit_cell in constructor.
+   */
 
   struct sorter_by_resolution {
     cuc::unit_cell unit_cell;
@@ -114,3 +122,5 @@ namespace x2tbx {
     }
   };
 }
+
+#endif
--- scratch/gw/x2tbx/x2tbx_ol.cpp
+++ scratch/gw/x2tbx/x2tbx_ol.cpp
@@ -2,70 +2,96 @@
 
 namespace x2tbx {
 
-  ObservationList::ObservationList(void)
+  ObservationList::ObservationList()
   {
-    imean = 0.0;
-    sigimean = 0.0;
+    imean_ = 0.0;
+    sigimean_ = 0.0;
   }
 
-  ObservationList::~ObservationList(void) { }
+  /**
+   * Add one observation
+   * @param o I and sigI of observation
+   */
 
   void
   ObservationList::add(i_sig_type o)
   {
-    observations.push_back(o);
-    imean = 0.0;
-    sigimean = 0.0;
+    observations_.push_back(o);
   }
 
+  /**
+   * Merge observations - necessary after all reflections loaded.
+   */
+
   void
-  ObservationList::merge(void)
+  ObservationList::merge()
   {
-    CCTBX_ASSERT(observations.size() > 0);
+    CCTBX_ASSERT(observations_.size() > 0);
     float sum_wi = 0.0;
     float sum_w = 0.0;
-    total_i_sigi = 0.0;
+    total_i_sigi_ = 0.0;
 
-    for(size_t j = 0; j < observations.size(); j ++) {
-      float i = observations[j][0];
-      float w = 1.0 / (observations[j][1] * observations[j][1]);
+    for(size_t j = 0; j < observations_.size(); j ++) {
+      float i = observations_[j][0];
+      float w = 1.0 / (observations_[j][1] * observations_[j][1]);
       sum_w += w;
       sum_wi += w * i;
 
-      total_i_sigi += observations[j][0] / observations[j][1];
+      total_i_sigi_ += observations_[j][0] / observations_[j][1];
     }
 
-    imean = sum_wi / sum_w;
-    sigimean = 1.0 / sqrt(sum_w);
+    imean_ = sum_wi / sum_w;
+    sigimean_ = 1.0 / sqrt(sum_w);
   }
+
+  /**
+   * Get average I/sigma for observations
+   * @returns i_sig_type containing Imean, sigImean
+   */
 
   i_sig_type
-  ObservationList::i_sigma(void)
+  ObservationList::i_sigma()
   {
-    return i_sig_type(imean, sigimean);
+    return i_sig_type(imean_, sigimean_);
   }
 
+  /**
+   * Get total (unweighted) I/sigma for observations
+   * @returns total I/sigma - useful for computing average unmerged I/sigma
+   */
+
   float
-  ObservationList::total_i_sigma(void)
+  ObservationList::total_i_sigma()
   {
-    return total_i_sigi;
+    return total_i_sigi_;
   }
+
+  /**
+   * Get multiplicity of observations of this reflection.
+   * @returns multiplicity
+   */
 
   size_t
-  ObservationList::multiplicity(void)
+  ObservationList::multiplicity()
   {
-    return observations.size();
+    return observations_.size();
   }
 
+  /**
+   * Get rmerge contribution from observations of this reflection, equal to
+   * sum(|I - <I>|) for all observations I and weighted mean <I>.
+   * @returns rmerge contribution
+   */
+
   float
-  ObservationList::rmerge(void)
+  ObservationList::rmerge()
   {
-    CCTBX_ASSERT(observations.size() > 0);
-    CCTBX_ASSERT(sigimean > 0.0);
+    CCTBX_ASSERT(observations_.size() > 0);
+    CCTBX_ASSERT(sigimean_ > 0.0);
     float sum_di = 0.0;
 
-    for(size_t j = 0; j < observations.size(); j ++) {
-      sum_di += fabs(observations[j][0] - imean);
+    for(size_t j = 0; j < observations_.size(); j ++) {
+      sum_di += fabs(observations_[j][0] - imean_);
     }
 
     return sum_di;
