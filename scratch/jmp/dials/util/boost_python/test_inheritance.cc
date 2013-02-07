2b3c65ce2e64c6356a9cac02fd6e053ddb5b069a
James Parkhurst <james.parkhurst@diamond.ac.uk>
2013-02-07 17:23:30 +0000

    Made some changes to inheritance test.
    In the previous test, BaseWrapper was imported into python as "Base".
    
    This meant that when I just wanted as Base object and passed it to a function calling it's methods, I would have the added overhead of the boost python stuff.
    
    I don't this this is a good solution, using the c++ objects directly shouldn't incur extra overhead.
    
    Therefore, I've split the test objects into export Base and Derived directly and have exposed the BaseWrapper (and DerivedWrapper) into python as themselves (changed names to BaseInterface, DerivedInterface). 
    
    Python classes would have to inherit from these instead of directly from Base, Derived. It's not as neat but means you only pay for what you get in terms of extra boost python overhead.

--- scratch/jmp/dials/util/boost_python/test_inheritance.cc
+++ scratch/jmp/dials/util/boost_python/test_inheritance.cc
@@ -3,6 +3,7 @@
 #include <boost/python/call_method.hpp>
 #include <iostream>
 #include <memory>
+#include <boost/noncopyable.hpp>
 
 namespace dials { namespace util { namespace boost_python {
 
@@ -18,37 +19,54 @@ public:
      virtual void do_something() { std::cout << "hello Derived" << std::endl; }
 };
 
-class ExtraDerived : public Derived {
-public:
-    virtual void do_something() { std::cout << "hello ExtraDerived" << std::endl; }
-};
-
 void callback_do_something(Base& a) {
     a.do_something();
 }
 
-struct BaseCallback : public Base {
-public:
 
-    BaseCallback(PyObject *p) : Base(), self(p) {}
-    BaseCallback(PyObject *p, const Base& x) : Base(x), self(p) {}
-    void do_something() { 
-        call_method <void> (self, "do_something"); 
+template <typename BaseClass>
+struct BaseClassInterface : BaseClass, boost::python::wrapper <BaseClass> {
+    void do_something()
+    {
+        if (override f = this->get_override("do_something")) {
+            f();
+        } else {
+            BaseClass::do_something();    
+        }
     }
-    static void default_do_something(Base& self_) { 
-        return self_.Base::do_something(); 
+
+    void default_do_something() { 
+        return this->BaseClass::do_something(); 
     }
- private:
-    PyObject* self;
 };
+
+struct BaseInterface : BaseClassInterface <Base> {};
+struct DerivedInterface : BaseClassInterface <Derived> {};
+
+template <typename T, typename B>
+class_<T, boost::noncopyable> base_class_wrapper(const char *name) {
+    return class_ <T, boost::noncopyable> (name)
+        .def("do_something", 
+            &B::do_something, 
+            &T::default_do_something);
+}
+
+template <typename T, typename B>
+class_<T, boost::noncopyable> derived_class_wrapper(const char *name) {
+    return base_class_wrapper <T, B> (name);
+}
 
 void export_test_inheritance()
 {
-    class_<Base, BaseCallback> ("Base")
-        .def("do_something", &BaseCallback::default_do_something);
+    class_<Base>("Base")
+        .def("do_something", 
+            &Base::do_something);
        
-    class_<Derived, bases<Base> >("Derived")
-        .def("do_something", &Derived::do_something);
+    class_<Derived, bases<Base> >("Derived");
+
+    base_class_wrapper <BaseInterface, Base>("BaseInterface");
+    derived_class_wrapper <DerivedInterface, Derived> ("DerivedInterface");
+
 
     def("callback_do_something", &callback_do_something);
 };
--- scratch/jmp/dials/util/test_inheritance.py
+++ scratch/jmp/dials/util/test_inheritance.py
@@ -1,21 +1,21 @@
 
 from dials_util_ext import *
 
-class PythonDerived (Base):
+class PythonDerived (DerivedInterface):
 
     def __init__(self):
-        Base.__init__(self)
+        DerivedInterface.__init__(self)
 
     def do_something(self):
         print "hello PythonDerived"
 
 class PythonDerived2 (PythonDerived):
   
-  def __init__(self):
-    PythonDerived.__init__(self)
+    def __init__(self):
+        PythonDerived.__init__(self)
   
-  def do_something(self):
-    print "hello PythonDerived2"
+    def do_something(self):
+        print "hello PythonDerived2"
   
 
 a = Base()
@@ -23,13 +23,23 @@ b = Derived()
 d = PythonDerived()
 e = PythonDerived2()
 
-
+print "Callback Base"
 callback_do_something(a)
+print "Callback Derived"
 callback_do_something(b)
+print "Callback PythonDerived"
 callback_do_something(d)
 
+print "Do something Base"
 a.do_something()
+print "Do something Derived"
 b.do_something()
+
+print "Do something PythonDerived"
 d.do_something()
+
+print "Do something PythonDerived2"
 e.do_something()
+
+print "Callback Python derived2"
 callback_do_something(e)
