7615bfa2a58db373c3e102e7d0205f265d9c4d57
Graeme Winter <graeme.winter@gmail.com>
2013-02-14 01:07:09 +0000

    Clean up test case: not finished yet though

--- dxtbx/tst_dxtbx.py
+++ dxtbx/tst_dxtbx.py
@@ -1,6 +1,13 @@
 from boost.python import streambuf
 from dxtbx import read_uint16
-f = open('/Users/graeme/data/demo/insulin_1_001.img', 'rb')
+import sys
+from dxtbx.format.Registry import Registry
+    
+format = Registry.find(sys.argv[1])
+i = format(sys.argv[1])
+size = i.get_detector().get_image_size()
+
+f = open(sys.argv[1], 'rb')
 hdr = f.read(512)
-l = read_uint16(streambuf(f), 2304 * 2304)
+l = read_uint16(streambuf(f), int(size[0] * size[1]))
 print sum(l)
