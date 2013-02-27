b9596839e4adcfb767fbf5b6fdca6bf3a60299c3
James Parkhurst <james.parkhurst@diamond.ac.uk>
2013-02-27 18:15:37 +0000

    Initialised members to remove unitialised variable warning. Added some comments

--- model/data/reflection.h
+++ model/data/reflection.h
@@ -22,26 +22,38 @@ namespace dials { namespace model {
   using scitbx::vec2;
   using scitbx::vec3;
 
+  // Typedef the miller type
   typedef cctbx::miller::index <> miller_index_type;
 
+  /** A base class for reflections. */
   class ReflectionBase {
   public:
 
-    ReflectionBase() {}
+    /** Initialise the reflection */
+    ReflectionBase()
+      : miller_index_(0, 0, 0) {}
 
+    /**
+     * Initialise the reflection with the miller index
+     * @param miller_index The miller index
+     */
     ReflectionBase(miller_index_type miller_index)
       : miller_index_(miller_index) {}
 
+    /** Virtual destructor */
     virtual ~ReflectionBase() {}
 
+    /** Get the miller index */
     miller_index_type get_miller_index() const {
       return miller_index_;
     }
 
+    /** Set the miller index */
     void set_miller_index(miller_index_type miller_index) {
       miller_index_ = miller_index;
     }
 
+    /** True/False the miller index is (0, 0, 0) */
     bool is_zero() {
       return miller_index_.is_zero();
     }
@@ -50,67 +62,112 @@ namespace dials { namespace model {
     miller_index_type miller_index_;
   };
 
+  /**
+   * A class for reflections containing some additional data
+   */
   class Reflection : public ReflectionBase {
   public:
 
-    Reflection() {}
+    /** Default initialisation */
+    Reflection()
+      : ReflectionBase(),
+        rotation_angle_(0.0),
+        beam_vector_(0.0, 0.0, 0.0),
+        image_coord_px_(0.0, 0.0),
+        image_coord_mm_(0.0, 0.0),
+        frame_number_(0),
+        panel_number_(0) {}
 
+    /**
+     * Initialise the reflection with the miller index
+     * @param miller_index The miller index
+     */
     Reflection(miller_index_type miller_index)
-      : ReflectionBase(miller_index) {}
+      : ReflectionBase(miller_index),
+        rotation_angle_(0.0),
+        beam_vector_(0.0, 0.0, 0.0),
+        image_coord_px_(0.0, 0.0),
+        image_coord_mm_(0.0, 0.0),
+        frame_number_(0),
+        panel_number_(0) {}
 
+    /**
+     * Initialise the reflection with the miller index, rotation angle and
+     * beam parameters
+     * @param miller_index The miller index
+     * @param rotation_angle The rotation angle
+     * @param beam_vector The beam vector
+     */
     Reflection(miller_index_type miller_index,
                double rotation_angle,
                vec3 <double> beam_vector)
       : ReflectionBase(miller_index),
         rotation_angle_(rotation_angle),
-        beam_vector_(beam_vector){}
+        beam_vector_(beam_vector),
+        image_coord_px_(0.0, 0.0),
+        image_coord_mm_(0.0, 0.0),
+        frame_number_(0),
+        panel_number_(0) {}
 
+    /** Virtual destructor */
     virtual ~Reflection() {}
 
+    /** Get the rotation angle */
     double get_rotation_angle() const {
       return rotation_angle_;
     }
 
+    /** Get the beam vector */
     vec3 <double> get_beam_vector() const {
       return beam_vector_;
     }
 
+    /** Get the image coordinate in mm */
     vec2 <double> get_image_coord_mm() const {
       return image_coord_mm_;
     }
 
+    /** Get the image coordinate in pixels */
     vec2 <double> get_image_coord_px() const {
       return image_coord_px_;
     }
 
+    /** Get the frame number */
     double get_frame_number() const {
       return frame_number_;
     }
 
+    /** Get the panel number */
     int get_panel_number() const {
       return panel_number_;
     }
 
+    /** Set the rotation angle */
     void set_rotation_angle(double rotation_angle) {
       rotation_angle_ = rotation_angle;
     }
 
+    /** Set the beam vector */
     void set_beam_vector(vec3 <double> beam_vector) {
       beam_vector_ = beam_vector;
     }
 
+    /** Set the image coordinate in mm */
     void set_image_coord_mm(vec2 <double> image_coord_mm) {
       image_coord_mm_ = image_coord_mm;
     }
 
+    /** Set the image coordinate in pixels */
     void set_image_coord_px(vec2 <double> image_coord_px) {
       image_coord_px_ = image_coord_px;
     }
 
+    /** Set the frame number */
     void set_frame_number(double frame_number) {
       frame_number_ = frame_number;
     }
 
+    /** Get the frame number */
     void set_panel_number(int panel_number) {
       panel_number_ = panel_number;
     }
