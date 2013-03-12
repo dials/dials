36059441cd51964fab92d05d2ead6de3d3003516
Graeme Winter <graeme.winter@gmail.com>
2013-03-12 09:36:43 +0000

    Added code to read array copying some stuff James had aready done: for the moment want this self-contained...

---
+++ scratch/gw/biostruct-25.py
@@ -0,0 +1,27 @@
+# copied from James's code...
+
+def open_file_return_array(self, filename):
+    import pycbf
+    import numpy
+
+    # open file
+    cbf_handle = pycbf.cbf_handle_struct()
+    cbf_handle.read_file(filename, pycbf.MSG_DIGEST)
+    cbf_handle.rewind_datablock()
+
+    # find right datablock
+    cbf_handle.select_datablock(0)
+    cbf_handle.select_category(0)
+    cbf_handle.select_column(2)
+    cbf_handle.select_row(0)
+
+    type = cbf_handle.get_typeofvalue()
+    assert (type.find('bnry') > -1)
+
+    # read and reshape
+    image = numpy.fromstring(cbf_handle.get_integerarray_as_string(),
+                             numpy.int32)
+    parameters = cbf_handle.get_integerarrayparameters_wdims()
+    image.shape = (parameters[10], parameters[9])
+
+    return image
