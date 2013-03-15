14cd5e23b4506dc72bc4a19c459632235b89b831
David Waterman <david.waterman@stfc.ac.uk>
2013-03-15 12:13:07 +0000

    DIALS reflecting angle prediction behaves the same as angle_predictor, so I can go ahead and use it

--- scratch/dgw/prediction/tst.py
+++ scratch/dgw/prediction/tst.py
@@ -80,7 +80,7 @@ def align_reference_frame(primary_axis, primary_target,
 
 if __name__ == "__main__":
 
-    ### Perform a unit test by comparing the results of angle_predictor_py to 
+    ### Perform a unit test by comparing the results of angle_predictor_py to
     ### those from rstbx's rotation_angles
 
     ### python and cctbx imports
@@ -134,12 +134,13 @@ if __name__ == "__main__":
                     mybeam.get_wavelength(),
                     R_to_rossmann * matrix.col(mygonio.get_rotation_axis()))
 
-    obs_indices, obs_angles = ra.observed_indices_and_angles_from_angle_range(
-        phi_start_rad = 0.0, phi_end_rad = math.pi, indices = indices)
+    obs_indices_rstbx, obs_angles_rstbx = \
+        ra.observed_indices_and_angles_from_angle_range(
+          phi_start_rad = 0.0, phi_end_rad = math.pi, indices = indices)
 
-    # test rotation_angles wrapper
+    # test my rotation_angles wrapper, angle_predictor
     ap = angle_predictor(mycrystal, mybeam, mygonio, resolution)
-    obs_indices2, obs_angles2 = ap.observed_indices_and_angles_from_angle_range(
+    obs_indices_wrap, obs_angles_wrap = ap.observed_indices_and_angles_from_angle_range(
         phi_start_rad = 0.0, phi_end_rad = math.pi, indices = indices)
 
     # NB
@@ -148,38 +149,41 @@ if __name__ == "__main__":
     # * obs_indices2 is a cctbx_array_family_flex_ext.miller_index containing
     # integer indices
 
-    for h1, h2, p1, p2 in zip(obs_indices, obs_indices2, obs_angles, obs_angles2):
+    # indices and angles should be the same
+    for h1, h2, p1, p2 in zip(obs_indices_rstbx, obs_indices_wrap,
+                              obs_angles_rstbx, obs_angles_wrap):
+        # NB h1 are floats while h2 are ints. Nevertheless the assert
+        # works (a mild surprise of Python)
         assert(h1 == h2)
         assert(p1 == p2)
 
-    # Now we're ready to test angle_predictor_py
+    # Now we're ready to test angle_predictor_py. First check prediction
+    # for individual indices. The angles should be approx. the same
     ap = angle_predictor_py(mycrystal, mybeam, mygonio, resolution)
 
-    for hkl, phi in zip(obs_indices2, obs_angles2):
+    for hkl, phi in zip(obs_indices_rstbx, obs_angles_rstbx):
         ap_phi = ap.predict(hkl)
         if ap_phi is None: continue
-        assert (approx_equal(ap_phi[0], phi, out = None)) or \
-               (approx_equal(ap_phi[1], phi, out = None))
+        assert approx_equal(ap_phi[0], phi, out = None) or \
+               approx_equal(ap_phi[1], phi, out = None)
 
-    # Test prediction of multiple indices from angle_predictor_py
-    obs_indices3, obs_angles3 = ap.observed_indices_and_angles_from_angle_range(
-        phi_start_rad = 0.0, phi_end_rad = math.pi, indices = indices)
+    # Now test prediction of multiple indices from angle_predictor_py
+    obs_indices_dgw, obs_angles_dgw = \
+        ap.observed_indices_and_angles_from_angle_range(
+          phi_start_rad = 0.0, phi_end_rad = math.pi, indices = indices)
 
-    # Test method for multiple indices
-    # NB I'm going to do something a bit horrible here. The values in obs_indices
-    # are floats (it is an array of vec3), while obs_indices2 are ints (a Miller
-    # index array). To compare the sets of indices I convert obs_indices back to
-    # integer Miller indices
-    temp = flex.miller_index()
-    for hkl in obs_indices:
-        hkl = map(lambda x: int(round(x)), hkl)
-        temp.append(tuple(hkl))
-    obs_indices = temp
+    #temp = flex.miller_index()
+    #for hkl in obs_indices:
+    #    hkl = map(lambda x: int(round(x)), hkl)
+    #    temp.append(tuple(hkl))
+    #obs_indices = temp
 
     for i in xrange(20):
-        print (obs_indices[i], obs_angles[i], obs_indices2[i], obs_angles2[i],
-              obs_indices3[i], obs_angles3[i])
-    print len(obs_indices), len(obs_indices2), len(obs_indices3)
+        print (obs_indices_rstbx[i], obs_angles_rstbx[i],
+               obs_indices_wrap[i], obs_angles_wrap[i],
+               obs_indices_dgw[i], obs_angles_dgw[i])
+        assert(obs_indices_rstbx[i] == obs_indices_wrap[i])
+    print len(obs_indices_rstbx), len(obs_indices_wrap), len(obs_indices_dgw)
 
     # FIXME how do I compare these sets?
     # There are some differences between the limits of reflections that are
@@ -189,26 +193,19 @@ if __name__ == "__main__":
     # in the above loop. Sometimes one method predicts an extra 'shell' of
     # reflections compared to the other.
 
-#    obs_indices2, obs_angles2 = ap.observed_indices_and_angles_from_angle_range(
-#        phi_start_rad = 0.0, phi_end_rad = math.pi, indices = obs_indices1)
-#
-#    print len(obs_indices2)
-#    for i in range(10):
-#        print obs_indices[i], obs_angles[i], obs_indices2[i], obs_angles2[i]
-#    for ang, ang2 in zip(obs_angles, obs_angles2):
-#        assert approx_equal(ang, ang2)
-#
-
     # Now compare dials.algorithms.spot_prediction with the above methods. I
     # want to switch over to using DIALS spot prediction, but should verify that
     # it gives me the same results first.
-    from dials.algorithms.spot_prediction import IndexGenerator, RotationAngles
+    from dials.algorithms.spot_prediction import IndexGenerator, \
+                                                 RotationAngles
 
     # Construct an IndexGenerator(unit_cell, space_group_type, d_min)
-    index_generator = IndexGenerator(mycrystal.get_unit_cell(), space_group(space_group_symbols(1).hall()).type(), 1.0)
+    index_generator = IndexGenerator(mycrystal.get_unit_cell(),
+                space_group(space_group_symbols(1).hall()).type(), 1.0)
     dials_indices = index_generator.to_array()
-    
-    # Ensure generated indices are the same
+
+    # Ensure generated (not necessarily observed) full sphere indices
+    # are the same
     for h1, h2 in zip(indices, dials_indices):
         assert h1 == h2
 
@@ -219,7 +216,9 @@ if __name__ == "__main__":
 
     UB = mycrystal.get_U() * mycrystal.get_B()
 
-    # try a few indices
+    # generate angles within a range, i.e. the analogue of
+    # the observed_indices_and_angles_from_angle_range method
+    sweep_range = (0., math.pi)
     dials_angles = flex.double()
     dials_obs_indices = flex.miller_index()
     for h in indices:
@@ -227,17 +226,21 @@ if __name__ == "__main__":
             angles = rotation_angles(h, UB)
         except RuntimeError as e:
             continue
-        dials_obs_indices.append(h)
-        dials_angles.append(angles[0])
-        dials_obs_indices.append(h)
-        dials_angles.append(angles[1])
+        for ang in angles:
+            if sweep_range[0] < ang < sweep_range[1]:
+                dials_obs_indices.append(h)
+                dials_angles.append(ang)
 
-    for i in xrange(20):
-        print (obs_indices2[i], obs_angles2[i], dials_obs_indices[i], dials_angles[i])
-    print len(obs_indices2), len(dials_obs_indices)
-    
-    # Note dials_indices is about twice as long as obs_indices2, because it
-    # hasn't been filtered by a rotation range of pi radians!
+    # make sure they're the same
+    for h1, phi1, h2, phi2 in zip(obs_indices_wrap,
+                                  obs_angles_wrap,
+                                  dials_obs_indices,
+                                  dials_angles):
+        assert (h1 == h2)
+        assert approx_equal(phi1, phi2)
 
-    # if we got this far,
+    print len(obs_indices_wrap), len(dials_obs_indices)
+
+    # if we got this far, excellent, DIALS reflection prediction behaves
+    # the same as my angle_predictor, so I can go ahead and use it.
     print "OK"
