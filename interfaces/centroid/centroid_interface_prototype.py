e224d46b71f2acf0d9197c1faa0d3c4de8ed99f9
Graeme Winter <graeme.winter@gmail.com>
2013-03-15 11:59:32 +0000

    First cut of what the centroid interface should look like:
    Need to inherit from this and actually implement the calculation which will compute the centroid for data in a given bounding box. I will add a toy example of this directly.

---
+++ interfaces/centroid/centroid_interface_prototype.py
@@ -0,0 +1,39 @@
+class centroid_interface_prototype(object):
+    '''Centroid calculation interface: all of the stuff in here is in pixels.'''
+    
+    def __init__(self, bounding_boxes, dxtbx_sweep_object):
+        self._bounding_boxes = bounding_boxes
+        self._sweep = dxtbx_sweep_object
+        self._centroids = self._compute_centroids()
+
+        return
+
+    def get_centroids(self):
+        return self._centroids
+
+    def compute_centroid_from_bbox(self, bbox):
+        '''Overload me: bbox has form tuple:
+
+        (fmin, fmax, rmin, rmax, cmin, cmax)
+
+        where f is frame #, r is row #, c is column #. Should return f, r, c
+        in pixels of the centroid position, and df, dr, dc standard deviations
+        of the same.'''
+
+        raise RuntimeError, 'overload me'
+
+    def _compute_centroids(self):
+
+        self._centroids = { }
+
+        for hkl in self._bounding_boxes:
+            self._centroids[hkl] = []
+
+            for bbox in self._bounding_boxes[hkl]:
+                self._centroids[hkl].append(self.compute_centroid_from_bbox(
+                    bbox))
+
+        return
+
+    
+        
