3aa0e0908ef7717ea8f1897922ef149f0c114528
Graeme Winter <graeme.winter@gmail.com>
2013-03-19 08:54:31 +0000

    Started working on generating fake profiles to test the covariance stuff

--- algorithms/centroid/toy_centroid_helpers.py
+++ algorithms/centroid/toy_centroid_helpers.py
@@ -1,3 +1,4 @@
+from __future__ import division
 
 def covar_contrib(x1, x2, weights):
     '''Compute contribution to covariance matrix.'''
@@ -32,12 +33,43 @@ def form_covariance_matrix(pixel_list, _f, _r, _c):
     m = flex.double(flex.grid(3, 3))
     for j in range(9):
         m[j] = m_elems[j]
+
     s = eigensystem.real_symmetric(m)
     values = s.values()
-    vectors = s.vectors()
+    vector_elems = s.vectors()
+    vectors = []
 
     for j in range(3):
-        vec = tuple(vectors[j * 3:j * 3 + 3])
-        print '%.3f %.3f %.3f %.3f' % (values[j], vec[0], vec[1], vec[2])
+        vectors.append(matrix.col(vector_elems[j * 3:j * 3 + 3]))
 
-    return
+    return values, vectors
+
+def nint(a):
+    return int(round(a))
+
+def generate_fake_profile(axes, variances, counts_total):
+    '''Generate fake reflection profile with principle axes axes[0] ... [2],
+    variance along each axis of variances[0..2] and a total of counts_total
+    counts. Assume that the axes / variances are given in units of pixels.'''
+
+    import math
+    import random
+
+    grid = { }
+
+    sigmas = map(math.sqrt, variances)
+
+    for j in range(counts_total):
+        R = [random.gauss(0, s) for s in sigmas]
+        x = R[0] * axes[0] + R[1] * axes[1] + R[2] * axes[2]
+        c = tuple(map(nint, x))
+        if not c in grid:
+            grid[c] = 0
+        grid[c] += 1
+
+    pixels = []
+    for c in grid:
+        pixels.append((c[0], c[1], c[2], grid[c]))
+
+    return pixels
+
