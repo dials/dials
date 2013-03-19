0850bf932eebb2aa3a33091838ba252296fcddbc
Graeme Winter <graeme.winter@gmail.com>
2013-03-19 08:10:17 +0000

    Code moved to helpers: need to think about how this will be used..

--- algorithms/centroid/toy_centroid.py
+++ algorithms/centroid/toy_centroid.py
@@ -4,15 +4,6 @@ from __future__ import division
 from dials.interfaces.centroid.centroid_interface_prototype import \
     centroid_interface_prototype as centroid_interface
 
-def covar_contrib(x1, x2, weights):
-    '''Compute contribution to covariance matrix.'''
-
-    covar = sum([_x1 * _x2 * _w for _x1, _x2, _w in zip(x1, x2, weights)]) / \
-        sum(weights)
-
-    return covar
-        
-
 class toy_centroid(centroid_interface):
     def __init__(self, bounding_boxes, dxtbx_sweep_object):
 
@@ -62,37 +53,6 @@ class toy_centroid(centroid_interface):
 
         _f, _r, _c = f_tot / d_tot, r_tot / d_tot, c_tot / d_tot
 
-        # form the variance matrix
-
-        pixels = [(f - _f, r - _r, c - _c, d) for f, r, c, d in pixel_list]
-
-        data = ([pixel[0] for pixel in pixels],
-                [pixel[1] for pixel in pixels],
-                [pixel[2] for pixel in pixels],
-                [pixel[3] for pixel in pixels])
-
-        m_elems = []
-
-        for i in range(3):
-            for j in range(3):
-                m_elems.append(covar_contrib(data[i], data[j], data[3]))
-
-        from scitbx.array_family import flex
-        from scitbx.linalg import eigensystem
-        from scitbx import matrix
-
-        m = flex.double(flex.grid(3, 3))
-        for j in range(9):
-            m[j] = m_elems[j]
-        s = eigensystem.real_symmetric(m)
-        values = s.values()
-        vectors = s.vectors()
-
-        print 'Eigenvalues and vectors of covariance matrix from spot...'
-        for j in range(3):
-            vec = tuple(vectors[j * 3:j * 3 + 3])
-            print '%.3f %.3f %.3f %.3f' % (values[j], vec[0], vec[1], vec[2])
-            
         # now compute the variance
 
         f_tot = 0.0
---
+++ algorithms/centroid/toy_centroid_helpers.py
@@ -0,0 +1,43 @@
+
+def covar_contrib(x1, x2, weights):
+    '''Compute contribution to covariance matrix.'''
+
+    covar = sum([_x1 * _x2 * _w for _x1, _x2, _w in zip(x1, x2, weights)]) / \
+        sum(weights)
+
+    return covar
+        
+def form_covariance_matrix(pixel_list, _f, _r, _c):
+    '''Compute covariance matrix.'''
+
+    # form the variance matrix
+
+    pixels = [(f - _f, r - _r, c - _c, d) for f, r, c, d in pixel_list]
+    
+    data = ([pixel[0] for pixel in pixels],
+            [pixel[1] for pixel in pixels],
+            [pixel[2] for pixel in pixels],
+            [pixel[3] for pixel in pixels])
+    
+    m_elems = []
+
+    for i in range(3):
+        for j in range(3):
+            m_elems.append(covar_contrib(data[i], data[j], data[3]))
+
+    from scitbx.array_family import flex
+    from scitbx.linalg import eigensystem
+    from scitbx import matrix
+
+    m = flex.double(flex.grid(3, 3))
+    for j in range(9):
+        m[j] = m_elems[j]
+    s = eigensystem.real_symmetric(m)
+    values = s.values()
+    vectors = s.vectors()
+
+    for j in range(3):
+        vec = tuple(vectors[j * 3:j * 3 + 3])
+        print '%.3f %.3f %.3f %.3f' % (values[j], vec[0], vec[1], vec[2])
+            
+    return
