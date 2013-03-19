b7ea0789915e82471eaaa4fa589ae89b78c1b384
Graeme Winter <graeme.winter@gmail.com>
2013-03-19 08:17:17 +0000

    Cleaning clutter

--- algorithms/centroid/toy_centroid_helpers.py
+++ algorithms/centroid/toy_centroid_helpers.py
@@ -6,19 +6,19 @@ def covar_contrib(x1, x2, weights):
         sum(weights)
 
     return covar
-        
+
 def form_covariance_matrix(pixel_list, _f, _r, _c):
     '''Compute covariance matrix.'''
 
     # form the variance matrix
 
     pixels = [(f - _f, r - _r, c - _c, d) for f, r, c, d in pixel_list]
-    
+
     data = ([pixel[0] for pixel in pixels],
             [pixel[1] for pixel in pixels],
             [pixel[2] for pixel in pixels],
             [pixel[3] for pixel in pixels])
-    
+
     m_elems = []
 
     for i in range(3):
@@ -39,5 +39,5 @@ def form_covariance_matrix(pixel_list, _f, _r, _c):
     for j in range(3):
         vec = tuple(vectors[j * 3:j * 3 + 3])
         print '%.3f %.3f %.3f %.3f' % (values[j], vec[0], vec[1], vec[2])
-            
+
     return
