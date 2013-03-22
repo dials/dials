3839f77e9cf9537580fd698bd5827c2d4f094755
James Parkhurst <james.parkhurst@diamond.ac.uk>
2013-03-22 10:13:21 +0000

    Added a timer to the progress bar

--- util/command_line.py
+++ util/command_line.py
@@ -35,50 +35,101 @@ def interactive_console(namespace):
     except ImportError:
         print "IPython not available"
 
+
+class ProgressBarTimer:
+    """ A simple timer for the progress bar. """
+
+    def __init__(self):
+        """ Init the progress bar timer. """
+        from time import time
+        self._last_time = time()
+        self._last_perc = 0
+        self._update_period = 1
+        self._n_seconds_left = -1
+
+    def update(self, percent):
+        """ Update the timer. """
+        from time import time
+
+        # Get the current time diff between last time
+        curr_time = time()
+        diff_time = curr_time - self._last_time
+
+        # Only update after certain period or at 100%
+        if percent < 0: percent = 0
+        if percent > 100: percent = 100
+        if diff_time >= self._update_period or percent >= 100:
+
+            # Check the difference in percentage and calculate
+            # number of seconds remaining
+            diff_perc = percent - self._last_perc
+            if (diff_perc == 0):
+                self._n_seconds_left = 0
+            else:
+                self._n_seconds_left = diff_time * (100 - percent) / diff_perc
+
+        # Return number of seconds
+        return self._n_seconds_left
+
 class ProgressBar:
     """ A command line progress bar. """
 
-    def __init__(self, spinner=True, bar=True, length=50):
+    def __init__(self, spinner=True, bar=True, estimate_time=True, length=50):
         """ Init the progress bar parameters. """
 
         # Set the parameters
         self._spinner = spinner
+        self._estimate_time = estimate_time
         self._bar = bar
         self._length = length
-        
+
+        self._timer = ProgressBarTimer()
+
         # Print 0 percent
         self.update(0)
 
-    def update(self, percent):
+    def update(self, fpercent):
         """ Update the progress bar with a percentage. """
         import sys
-        
+        from math import ceil
+
         # Get integer percentage
-        percent = int(percent)
+        percent = int(fpercent)
         if percent < 0: percent = 0
         if percent > 100: percent = 100
-        
+
         # Add a percentage counter
         progress_str = '\r'
         progress_str += '{0: >3}%'.format(percent)
-        
+
         # Add a spinner
         if self._spinner:
             progress_str += ' '
             progress_str += '[ {0} ]'.format('-\|/'[percent % 4])
-        
+
         # Add a bar
         if self._bar:
             n_char = int(percent * self._length / 100)
             n_space = self._length - n_char
             progress_str += ' '
             progress_str += '[ {0}>{1} ]'.format('=' * n_char, ' ' * n_space)
-        
+
+        # Add a timer
+        if self._estimate_time:
+            n_seconds_left = self._timer.update(fpercent)
+            if n_seconds_left < 0:
+                n_seconds_left = '?'
+            else:
+                n_seconds_left = int(ceil(n_seconds_left))
+            time_str = '{0: >5}'.format(n_seconds_left)
+            progress_str += ' '
+            progress_str += 'remaining: {0}s'.format(time_str)
+
         # Print progress string to stdout
         sys.stdout.write(progress_str)
         sys.stdout.flush()
-        
+
     def finished(self):
         """ The progress bar is finished. """
-        self.update(100)        
+        self.update(100)
         print ""
