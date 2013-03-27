3f0fb5eb58a7a8689597e63bd8e4801e144a14b1
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2013-03-27 11:24:38 +0000

    changing names of variables to make the code more clear

--- algorithms/centroid/toy_centroid_Lui.py
+++ algorithms/centroid/toy_centroid_Lui.py
@@ -103,48 +103,48 @@ class toy_centroid_lui(centroid_interface):
 
 def single_spot_integrate_2d(data2d):
 
-    x_to = numpy.size(data2d[0:1, :])
-    y_to = numpy.size(data2d[:, 0:1])
-    #print 'x_to,y_to =', x_to, y_to
+    n_col = numpy.size(data2d[0:1, :])
+    n_row = numpy.size(data2d[:, 0:1])
+    #print 'n_col,n_row =', n_col, n_row
 
-    data2dsmoth = numpy.zeros(y_to * x_to, dtype = float).reshape(y_to, x_to)
+    data2dsmoth = numpy.zeros(n_row * n_col, dtype = float).reshape(n_row, n_col)
 
-    diffdata2d = numpy.zeros(y_to * x_to, dtype = int).reshape(y_to, x_to)
-    diffdata2d_ext = numpy.zeros(y_to * x_to, dtype = int).reshape(y_to, x_to)
+    diffdata2d = numpy.zeros(n_row * n_col, dtype = int).reshape(n_row, n_col)
+    diffdata2d_ext = numpy.zeros(n_row * n_col, dtype = int).reshape(n_row, n_col)
     data2dtmp = data2d
 
     for times in range(5):
-        for y in range(1, y_to - 1, 1):
-            for x in range(1, x_to - 1, 1):
-                pscan = float(numpy.sum(data2dtmp[y - 1:y + 2, x - 1:x + 2]) / 9.0)
-                data2dsmoth[y, x] = pscan
+        for row in range(1, n_row - 1, 1):
+            for col in range(1, n_col - 1, 1):
+                pscan = float(numpy.sum(data2dtmp[row - 1:row + 2, col - 1:col + 2]) / 9.0)
+                data2dsmoth[row, col] = pscan
     data2dtmp = data2dsmoth
     #threshold_shift = (numpy.max(data2dsmoth) - numpy.min(data2dsmoth)) / 2.0 # This used to be one of this "magical variables"
 
 #######################################################################################################
     cont = 0                                                           # This way to calculate
     dif_tot = 0                                                        # this magical variable
-    for y in range(0, y_to, 1):                                        # is more statistical
-        for x in range(0, x_to, 1):                                    # and seems to be giving
+    for row in range(0, n_row, 1):                                        # is more statistical
+        for col in range(0, n_col, 1):                                    # and seems to be giving
             cont += 1                                                  # better results
-            dif_tot += numpy.abs(data2d[y, x] - data2dsmoth[y, x])     #
+            dif_tot += numpy.abs(data2d[row, col] - data2dsmoth[row, col])     #
     dif_avg = dif_tot / cont                                           #
     #print 'dif_avg=', dif_avg                                         #
     threshold_shift = dif_avg * 2.0                                    #
 #######################################################################################################
     #print 'threshold_shift =', threshold_shift
 
-    data2dsmoth[0:y_to, 0:x_to] = data2dsmoth[0:y_to, 0:x_to] + threshold_shift
+    data2dsmoth[0:n_row, 0:n_col] = data2dsmoth[0:n_row, 0:n_col] + threshold_shift
     ext_area = 1                                                               # This used to be one of this "magical variables"
-    for y in range(0, y_to, 1):
-        for x in range(0, x_to, 1):
-            if data2d[y, x] > data2dsmoth[y, x]:
-                diffdata2d[y, x] = 1
+    for row in range(0, n_row, 1):
+        for col in range(0, n_col, 1):
+            if data2d[row, col] > data2dsmoth[row, col]:
+                diffdata2d[row, col] = 1
 
-    for y in range(ext_area, y_to - ext_area + 1, 1):
-        for x in range(ext_area, x_to - ext_area + 1, 1):
-            if diffdata2d[y, x] == 1:
-                diffdata2d_ext[y - ext_area:y + ext_area + 1, x - ext_area:x + ext_area + 1] = 1
+    for row in range(ext_area, n_row - ext_area + 1, 1):
+        for col in range(ext_area, n_col - ext_area + 1, 1):
+            if diffdata2d[row, col] == 1:
+                diffdata2d_ext[row - ext_area:row + ext_area + 1, col - ext_area:col + ext_area + 1] = 1
 
     #print '_____________diffdata2d'
     #print diffdata2d
@@ -155,54 +155,54 @@ def single_spot_integrate_2d(data2d):
 ############################################################################### flat background
     tot_bkgr = 0.0                                                            # version
     cont = 0.0                                                                #
-    for y in range(0, y_to, 1):                                               #
-        for x in range(0, x_to, 1):                                           #
-            if diffdata2d_ext[y, x] == 0:                                     #
+    for row in range(0, n_row, 1):                                               #
+        for col in range(0, n_col, 1):                                           #
+            if diffdata2d_ext[row, col] == 0:                                     #
                 cont += 1                                                     #
-                tot_bkgr += data2d[y, x]                                      #
+                tot_bkgr += data2d[row, col]                                      #
     bkgr = tot_bkgr / cont                                                    #
     #print 'bkgr=', bkgr                                                      #
-    for y in range(0, y_to, 1):                                               #
-        for x in range(0, x_to, 1):                                           #
-            if diffdata2d_ext[y, x] == 1 and data2d[y, x] > bkgr:             #
-                data2d[y, x] = data2d[y, x] - bkgr                            #
+    for row in range(0, n_row, 1):                                               #
+        for col in range(0, n_col, 1):                                           #
+            if diffdata2d_ext[row, col] == 1 and data2d[row, col] > bkgr:             #
+                data2d[row, col] = data2d[row, col] - bkgr                            #
             else:                                                             #
-                data2d[y, x] = 0                                              #
+                data2d[row, col] = 0                                              #
 ###############################################################################
 
 ############################################################################### curved background
-#   xbord = int(x_to / 5)                                                     # version
-#   ybord = int(y_to / 5)                                                     #
-#   for y in range(0, y_to, 1):                                               #
-#       for x in range(0, x_to, 1):                                           #
-#           if diffdata2d_ext[y, x] == 1:                                     #
-#               top_av = float(numpy.sum(data2d[:ybord, x - 1:x + 2]))        #
-#               bot_av = float(numpy.sum(data2d[y_to - ybord:, x - 1:x + 2])) #
-#               lft_av = float(numpy.sum(data2d[y - 1:y + 2, xbord]))         #
-#               rgt_av = float(numpy.sum(data2d[y - 1:y + 2, x_to - xbord:])) #
+#   colbord = int(n_col / 5)                                                     # version
+#   colbord = int(n_row / 5)                                                     #
+#   for row in range(0, n_row, 1):                                               #
+#       for col in range(0, n_col, 1):                                           #
+#           if diffdata2d_ext[row,col] == 1:                                     #
+#               top_av = float(numpy.sum(data2d[:colbord, col - 1:col + 2]))        #
+#               bot_av = float(numpy.sum(data2d[n_row - colbord:, col - 1:col + 2])) #
+#               lft_av = float(numpy.sum(data2d[row - 1:row + 2, colbord]))         #
+#               rgt_av = float(numpy.sum(data2d[row - 1:row + 2, n_col - colbord:])) #
 #               bkgr = (top_av + bot_av + lft_av + rgt_av) / 4.0              #
-#               if data2d[y, x] > bkgr:                                       #
-#                   data2d[y, x] = data2d[y, x] - bkgr                        #
+#               if data2d[row,col] > bkgr:                                       #
+#                   data2d[row,col] = data2d[row,col] - bkgr                        #
 #               else:                                                         #
-#                   data2d[y, x] = 0                                          #
-#   for y in range(0, y_to, 1):                                               #
-#       for x in range(0, x_to, 1):                                           #
-#           if diffdata2d_ext[y, x] == 0:                                     #
-#               data2d[y, x] = 0                                              #
+#                   data2d[row,col] = 0                                          #
+#   for row in range(0, n_row, 1):                                               #
+#       for col in range(0, n_col, 1):                                           #
+#           if diffdata2d_ext[row,col] == 0:                                     #
+#               data2d[row,col] = 0                                              #
 ###############################################################################
-    #x_num_sum = 0.0
-    #y_num_sum = 0.0
+    #col_num_sum = 0.0
+    #row_num_sum = 0.0
     itst_sum = 0.0
-    for y in range(0, y_to, 1):
-        for x in range(0, x_to, 1):
-            itst_sum += float(data2d[y, x])
+    for row in range(0, n_row, 1):
+        for col in range(0, n_col, 1):
+            itst_sum += float(data2d[row, col])
     if itst_sum > 0.0:
         tot = itst_sum
     else:
         print 'itst_sum =', itst_sum
         tot = -1
 
-#    display_image_with_predicted_spots_n_centoids(data2d, col_cm, row_cm, x_to / 2, y_to / 2)
+#    display_image_with_predicted_spots_n_centoids(data2d, col_cm, row_cm, n_col / 2, n_row / 2)
 
     return tot
 def display_image_with_predicted_spots_n_centoids(image, xcoords, ycoords, xc, yc):
