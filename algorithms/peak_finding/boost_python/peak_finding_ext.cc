2d1383743f00344d4708e22abfb8a116233df8de
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2013-04-18 10:05:43 +0000

    first cpp example thanks to James

--- algorithms/peak_finding/boost_python/peak_finding_ext.cc
+++ algorithms/peak_finding/boost_python/peak_finding_ext.cc
@@ -18,12 +18,14 @@ namespace dials { namespace algorithms { namespace boost_python {
   void export_label_pixels();
   void export_mean_sdev_filter();
   void export_thresholding();
+  void export_lui_find_peak_helper();
 
   BOOST_PYTHON_MODULE(dials_algorithms_peak_finding_ext)
   {
     export_label_pixels();
     export_mean_sdev_filter();
     export_thresholding();
+    export_lui_find_peak_helper();
   }
 
 }}} // namespace = dials::algorithms::boost_python
---
+++ algorithms/peak_finding/lui_find_peak_helper.h
@@ -0,0 +1,13 @@
+#ifndef DIALS_ALGORITHMS_PEAK_FINDING_LUI_HELPER_H
+#define DIALS_ALGORITHMS_PEAK_FINDING_LUI_HELPER_H
+#include <iostream>
+namespace dials { namespace algorithms {
+  void hello() {
+	std::cout << "hi there \n";
+  }
+
+
+
+}}
+
+#endif
