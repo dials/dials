6eae8fa3d9288bc257c89d1e43b963400c8c95a5
James Parkhurst <james.parkhurst@diamond.ac.uk>
2013-04-18 08:12:58 +0000

    Added a Command class to print out nicely formatted data about command being run with timing information. Modified Progress bar to accept a preceeding string

--- util/command_line.py
+++ util/command_line.py
@@ -44,7 +44,7 @@ class ProgressBarTimer:
         from time import time
         self._last_time = time()
         self._last_perc = 0
-        self._update_period = 1
+        self._update_period = 0.5
         self._n_seconds_left = -1
 
     def update(self, percent):
@@ -74,19 +74,20 @@ class ProgressBarTimer:
 class ProgressBar:
     """ A command line progress bar. """
 
-    def __init__(self, spinner=True, bar=True, estimate_time=True, length=50):
+    def __init__(self, title=None, spinner=True, bar=True, estimate_time=True, 
+                 indent=0, length=80):
         """ Init the progress bar parameters. """
 
-        if spinner:
-            length -= 5
-
         # Set the parameters
+        self._title = title
+        self._indent = indent
         self._spinner = spinner
         self._estimate_time = estimate_time
         self._bar = bar
         self._length = length
 
         self._timer = ProgressBarTimer()
+        self._start_time = self._timer._last_time
 
         # Print 0 percent
         self.update(0)
@@ -102,20 +103,20 @@ class ProgressBar:
         if percent > 100: percent = 100
 
         # Add a percentage counter
-        progress_str = '\r'
-        progress_str += '{0: >3}%'.format(percent)
+        right_str = ''
+        left_str = '\r'
+        left_str += ' ' * self._indent
+
+        # Add a title if given
+        if self._title:
+            left_str += self._title + ': '
+
+        left_str += '{0: >3}%'.format(percent)
 
         # Add a spinner
         if self._spinner:
-            progress_str += ' '
-            progress_str += '[ {0} ]'.format('-\|/'[percent % 4])
-
-        # Add a bar
-        if self._bar:
-            n_char = int(percent * self._length / 100)
-            n_space = self._length - n_char
-            progress_str += ' '
-            progress_str += '[ {0}>{1} ]'.format('=' * n_char, ' ' * n_space)
+            left_str += ' '
+            left_str += '[ {0} ]'.format('-\|/'[percent % 4])
 
         # Add a timer
         if self._estimate_time:
@@ -124,18 +125,125 @@ class ProgressBar:
                 n_seconds_left = '?'
             else:
                 n_seconds_left = int(ceil(n_seconds_left))
-            time_str = '{0: >5}'.format(n_seconds_left)
-            progress_str += ' '
-            progress_str += 'remaining: {0}s'.format(time_str)
+            right_str = ' ' + 'est: {0}s'.format(n_seconds_left) + right_str
+
+        # Add a bar
+        if self._bar:
+            bar_length = self._length - (len(left_str) + len(right_str)) - 5
+            n_char = int(percent * bar_length / 100)
+            n_space = bar_length - n_char
+            left_str += ' '
+            left_str += '[ {0}>{1} ]'.format('=' * n_char, ' ' * n_space)
+
+        # Append strings
+        progress_str = left_str + right_str
 
         # Print progress string to stdout
         sys.stdout.write(progress_str)
         sys.stdout.flush()
 
-    def finished(self):
+    def finished(self, string=None):
         """ The progress bar is finished. """
-        self.update(100)
-        print ""
+        if string:
+            self._title = string
+    
+        ''' Print the 'end of comand' string.'''
+        from sys import stdout
+        from time import time
+
+
+        if self._estimate_time:
+            # Get the time string
+            time_string = '{0:.2f}s'.format(time() - self._start_time)
+
+            # Truncate the string
+            max_length = self._length - self._indent - len(time_string) - 1
+            string = string[:max_length]
+
+            # Add an indent and a load of dots and then the time string
+            dot_length = 1 + max_length - len(string)
+            string = (' ' * self._indent) + string
+            string = string + '.' * (dot_length)
+            string = string + time_string
+            
+        else:
+        
+            # Truncaet the string
+            max_length = self._length - self._indent
+            string = string[:max_length]
+
+            # Add a load of dots
+            dot_length = max_length - len(string) 
+            string = (' ' * self._indent) + string           
+            string = string + '.' * (dot_length)
+
+        # Write the string to stdout
+        stdout.write('\r' + string + '\n')
+        stdout.flush()
+
+class Command(object):
+    '''Class to nicely print out a command with timing info.'''
+
+    # Variables available in class methods    
+    indent = 0    
+    max_length = 80
+    print_time = True
+
+    @classmethod
+    def start(self, string):
+        ''' Print the 'start command' string.'''
+        from sys import stdout
+        from time import time
+        
+        # Truncate the string to the maximum length
+        max_length = self.max_length - self.indent - 3
+        string = string[:max_length]
+        string = (' ' * self.indent) + string + '...'
+        
+        # Write the string to stdout
+        stdout.write(string)
+        stdout.flush()
+        
+        # Get the command start time
+        self._start_time = time() 
+
+    @classmethod
+    def end(self, string):
+        ''' Print the 'end of comand' string.'''
+        from sys import stdout
+        from time import time
+
+        # Check if we want to print the time or not
+        if self.print_time:
+            
+            # Get the time string
+            time_string = '{0:.2f}s'.format(time() - self._start_time)
+
+            # Truncate the string
+            max_length = self.max_length - self.indent - len(time_string) - 1
+            string = string[:max_length]
+
+            # Add an indent and a load of dots and then the time string
+            dot_length = 1 + max_length - len(string)
+            string = (' ' * self.indent) + string
+            string = string + '.' * (dot_length)
+            string = string + time_string
+            
+        else:
+        
+            # Truncaet the string
+            max_length = self.max_length - self.indent
+            string = string[:max_length]
+
+            # Add a load of dots
+            dot_length = max_length - len(string)
+            string = (' ' * self.indent) + string            
+            string = string + '.' * (dot_length)
+
+        # Write the string to stdout
+        stdout.write('\r' + string + '\n')
+        stdout.flush()
+
 
 if __name__ == '__main__':
     import time
@@ -144,6 +252,10 @@ if __name__ == '__main__':
 
     for j in range(100):
         p.update(j)
-        time.sleep(0.1)
+        time.sleep(0.05)
 
-    p.finish()
+    p.finished()
+    
+    Command.start("Starting to do a command")
+    time.sleep(1)
+    Command.end("Ending the command")
