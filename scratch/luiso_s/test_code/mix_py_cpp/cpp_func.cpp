db402b8bd280d531a577bb6815be0083f312d3d2
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2013-04-30 15:43:08 +0000

    starting to prepare some code for next step

---
+++ scratch/luiso_s/test_code/mix_py_cpp/cpp_func.cpp
@@ -0,0 +1,12 @@
+#ifndef DIALS_ALGORITHMS_PEAK_FINDING_LUI_SMOOTHING_H
+#define DIALS_ALGORITHMS_PEAK_FINDING_LUI_SMOOTHING_H
+#include <iostream>
+
+#include <scitbx/array_family/flex_types.h>
+
+namespace dials { namespace algorithms {
+  using scitbx::af::flex_int;
+
+  flex_int smooth_2d(flex_int & data2d, int tot_times) {
+
+  }
