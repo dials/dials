d23d9c4c535b26ccb0c5e60a2754ec03adcdf840
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2013-05-01 15:50:09 +0000

    just scratching

---
+++ scratch/luiso_s/test_code/mix_py_cpp/lui_find_peak_helper.h
@@ -0,0 +1,14 @@
+#ifndef DIALS_ALGORITHMS_PEAK_FINDING_LUI_HELPER_H
+#define DIALS_ALGORITHMS_PEAK_FINDING_LUI_HELPER_H
+#include <iostream>
+namespace dials { namespace algorithms {
+  void hello() {
+        std::cout << "hi there \n";
+        std::cout << "hi again \n";
+  }
+
+
+
+}}
+
+#endif
