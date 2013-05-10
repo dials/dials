bf6c8f0b31efe3f399b0336f1a0103b1563d6a2f
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2013-05-10 15:52:35 +0000

    Voigt 2D profile done

--- algorithms/peak_finding/integrate_helper.py
+++ algorithms/peak_finding/integrate_helper.py
@@ -3,12 +3,17 @@ from dials.algorithms.peak_finding import ref_2d
 
 
 
-flex_ref2d_g = ref_2d(100, 100, 10, 20, .33333, 1000, 0.9)
-flex_ref2d_l = ref_2d(100, 100, 10, 20, .33333, 1000, 0.1)
+flex_ref2d_g = ref_2d(25, 25, 5, 3, .33333, 25, 0.9)
+flex_ref2d_l = ref_2d(25, 25, 5, 3, .33333, 25, 0.1)
 
 
 dat2d_ref_g = flex_ref2d_g.as_numpy_array()
 dat2d_ref_l = flex_ref2d_l.as_numpy_array()
+
+print dat2d_ref_g
+print dat2d_ref_l
+
+
 from matplotlib import pyplot as plt
 
 plt.imshow(dat2d_ref_g , interpolation = "nearest")
--- algorithms/peak_finding/lui_integrate_helper.h
+++ algorithms/peak_finding/lui_integrate_helper.h
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <cmath>
 #include <scitbx/array_family/flex_types.h>
-const float pi=3.141595358;
+const float pi=3.14159265358;
 namespace dials { namespace algorithms {
   using scitbx::af::flex_int;
   using scitbx::af::flex_grid;
@@ -16,34 +16,32 @@ namespace dials { namespace algorithms {
     y = dist * cos(ang);
   }
 
-  flex_int ref_2d(int nrow,int ncol, float a, float b,
+  flex_int ref_2d(int nrow, int ncol, float a, float b,
                 float delta_ang, float imax, float asp) {
     //int ncol=data2d.accessor().all()[1];
     //int nrow=data2d.accessor().all()[0];
     float dx,dy,dd,xc,yc;
     float mw = 0.5;
     float cntnt, gss, lrz, i_tt;
-    xc=ncol/2;
-    yc=nrow/2;
+    xc=float(ncol)/2;
+    yc=float(nrow)/2;
     // flex_int curv3d(data2d.accessor(),0);
     flex_int curv3d(flex_grid<>(nrow, ncol),0);
-
-    cntnt=1/sqrt(2.0*pi);
-
+    cntnt=1.0 / sqrt(2.0 * pi);
     for (int row = 0; row<nrow; row++) {
       for (int col = 0; col<ncol; col++) {
-        dx = col - xc;
-        dy = row - yc;
+        dx = float(col) - xc;
+        dy = float(row) - yc;
         rotate(dx, dy, delta_ang);
         dx = dx / a;
         dy = dy / b;
         dd = sqrt(dx*dx + dy*dy);
-        if (dd < 0.0000000000000001){
-          dd=0.0000000000000001;
+        if (dd < 0.000000000000000000001){
+          dd=0.000000000000000000001;
         }
-        gss=(1.0/pi) * exp(-mw * (dd*dd));
-        lrz=(1.0/pi) * ( mw / ( dd*dd + mw*mw ) );
-        i_tt=gss*asp+lrz*(1-asp);
+        gss = cntnt * exp(-mw * (dd*dd));
+        lrz = 1.0 /(pi * (1.0 + dd*dd )) ;
+        i_tt=gss*asp+lrz*(1.0 - asp);
         // curv3d(row, col) = data2d(row, col) + int(i_tt*imax);
         curv3d(row, col) = int(i_tt*imax);
       }
