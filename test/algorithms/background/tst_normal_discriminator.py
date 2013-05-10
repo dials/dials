9a17b8fa69653bac62274cb539860cb5e0a9f7ad
James Parkhurst <james.parkhurst@diamond.ac.uk>
2013-05-10 08:30:23 +0000

    Added a test for normal discriminator.

---
+++ test/algorithms/background/tst_normal_discriminator.py
@@ -0,0 +1,34 @@
+
+class Test(object):
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        self.tst_no_mask()
+
+    def is_correct(self, shoebox, mask, n_sigma, min_pixels):
+        from scitbx.array_family import flex
+        flags = [m & (1 << 0) for m in mask]
+        pixels = flex.select(shoebox, flags=flags)
+        assert(len(pixels) >= min_pixels)
+        meanp = flex.mean_and_variance(flex.double(pixels))
+        maxp = flex.max(flex.double(pixels))
+        m = meanp.mean()
+        s = meanp.unweighted_sample_standard_deviation()
+        assert(maxp <= m + n_sigma * s)
+
+    def tst_no_mask(self):
+        from scitbx.array_family import flex
+        from dials.algorithms.background import NormalDiscriminator
+        discriminate = NormalDiscriminator(n_sigma=3.0, min_data=10)
+        shoebox_d = flex.random_double(5 * 5 * 5) * 100
+        shoebox = flex.int([int(s) for s in shoebox_d])
+        shoebox.reshape(flex.grid((5, 5, 5)))
+        mask = discriminate(shoebox)
+        self.is_correct(shoebox, mask, 3.0, 10)
+        print 'OK'
+
+if __name__ == '__main__':
+    test = Test()
+    test.run()
