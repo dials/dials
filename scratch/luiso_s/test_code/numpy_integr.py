3f0cf5b2614f33da6d35a0a6afee7d4a4857b4e0
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2013-05-13 16:11:09 +0000

    preparing the ground for a quicker integration algorithm

---
+++ scratch/luiso_s/test_code/numpy_integr.py
@@ -0,0 +1,17 @@
+import numpy
+data2d = numpy.zeros((5, 5), dtype = numpy.int32)
+data2d[0, 1] = data2d[1, 0] = 1
+data2d[0, 2] = data2d[2, 0] = 3
+data2d[0, 3] = data2d[3, 0] = 2
+data2d[0, 4] = data2d[4, 0] = 4
+data2d[4, 1] = data2d[1, 4] = 1
+data2d[4, 2] = data2d[2, 4] = 3
+data2d[4, 3] = data2d[3, 4] = 2
+data2d[0, 0] = data2d[4, 4] = 5
+data2d[1:4, 1:4] = 10
+data2d[2:3, 2:3] = 50
+print data2d
+
+mask2d = numpy.zeros((5, 5), dtype = numpy.int32)
+mask2d[1:4, 1:4] = 1
+print mask2d
