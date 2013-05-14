d1ce06ea01e09037989e1fa3457c7e9b0cd80cd3
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2013-05-14 12:32:43 +0000

    working on summation integration

--- scratch/luiso_s/test_code/numpy_integr.py
+++ scratch/luiso_s/test_code/numpy_integr.py
@@ -1,4 +1,6 @@
 import numpy
+from dials.algorithms.background.flat_subtraction import flat_background_subtraction_2d
+
 data2d = numpy.zeros((5, 5), dtype = numpy.int32)
 data2d[0, 1] = data2d[1, 0] = 1
 data2d[0, 2] = data2d[2, 0] = 3
@@ -15,3 +17,8 @@ print data2d
 mask2d = numpy.zeros((5, 5), dtype = numpy.int32)
 mask2d[1:4, 1:4] = 1
 print mask2d
+
+# def layer_treatment(self, data2d, diffdata2d_ext):
+bkgr = flat_background_subtraction_2d(data2d, mask2d)
+print 'bkgr =', bkgr
+print data2d
