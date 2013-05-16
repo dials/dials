9072d4d691397b67d163ec99eea798b5f6b450fc
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2013-05-16 10:34:21 +0000

    just saving this essay before tying another that must be better.

--- algorithms/peak_finding/integrate_helper.py
+++ algorithms/peak_finding/integrate_helper.py
@@ -1,15 +1,20 @@
 from scitbx.array_family import flex
 from dials.algorithms.peak_finding import model_2d, measure_2d_angl
-
-ref2d = model_2d(20, 20, 1, 2, .33333, 125, 0.5)
-
-ang = measure_2d_angl(ref2d, 10, 10)
-
-print "ang =", ang
-
-dat2d_ref = ref2d.as_numpy_array()
-print dat2d_ref
 from matplotlib import pyplot as plt
+for int_ref_ang in range(30):
+    print "_________________________________________"
+    ref_ang = float(int_ref_ang) / 30.0
 
-plt.imshow(dat2d_ref , interpolation = "nearest")
-plt.show()
+    ref2d = model_2d(30, 30, 2, 5, ref_ang, 25, 0.5)
+    dat2d_ref = ref2d.as_numpy_array()
+    for pr_line in dat2d_ref:
+        print pr_line
+
+    ang = measure_2d_angl(ref2d, 15, 15)
+    print "ref_ang =", ref_ang
+    print "ang =", ang
+
+    new_ref2d = model_2d(30, 30, 2, 5, ang, 25, 0.5)
+    dat2d_paint = new_ref2d.as_numpy_array()
+    plt.imshow(dat2d_paint , interpolation = "nearest")
+    plt.show()
--- algorithms/peak_finding/lui_integrate_helper.h
+++ algorithms/peak_finding/lui_integrate_helper.h
@@ -24,6 +24,7 @@ namespace dials { namespace algorithms {
     // float tot=0;
     xc=float(ncol)/2;
     yc=float(nrow)/2;
+    std::cout << "\n" << "xc =" << xc << ", yc =" << yc << "\n";
     // flex_int curv3d(data2d.accessor(),0);
     flex_int curv3d(flex_grid<>(nrow, ncol),0);
     cntnt=1.0 / sqrt(2.0 * pi);
@@ -54,29 +55,31 @@ namespace dials { namespace algorithms {
     int ncol=data2d.accessor().all()[1];
     int nrow=data2d.accessor().all()[0];
     float dx, dy, xfl, yfl;
-    float ang, ang_tot = 0, tot =0;
-    std::cout <<"\n" << "x,y =" << xpos << ", " << ypos << "\n";
+    float r_ang, r_avg_ang, r_ang_tot = 0, r_tot = 0;
+    float l_ang, l_avg_ang, l_ang_tot = 0, l_tot = 0;
+    float avg_ang;
+    // std::cout <<"\n" << "x,y =" << xpos << ", " << ypos << "\n";
     for (int row = 0; row < nrow; row++) {
       for (int col = 0; col < ncol; col++) {
         xfl = float(col);// + 0.5;
         yfl = float(row);// + 0.5;
         dx = xfl - xpos;
         dy = yfl - ypos;
-        ang = atan2(dx, dy);
-        if (ang>pi){
-          ang=ang-pi;
+        if( dx < 0 ){
+          r_ang = atan2(dx, dy);
+          r_ang_tot += r_ang * float(data2d(row, col));
+          r_tot += data2d(row, col);
+        } else {
+          l_ang = atan2(dx, dy);
+          l_ang_tot += l_ang * float(data2d(row, col));
+          l_tot += data2d(row, col);
         }
-        else if (ang<0){
-          ang=ang+pi;
-        }
-        ang_tot += ang*float(data2d(row, col));
-        tot += data2d(row, col);
-
       }
     }
-    ang = (ang_tot/tot)/pi;
-
-    return ang;
+    r_avg_ang = (r_ang_tot/r_tot)/pi;
+    l_avg_ang = (l_ang_tot/l_tot)/pi;
+    avg_ang=(r_avg_ang+(l_avg_ang-pi))/2.0;
+    return avg_ang;
   }
 
 }}
