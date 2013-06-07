247ec8dad91e04527fa360a1b7ed34d3ec9eb76d
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2013-06-07 08:45:45 +0000

    fixing variable types for the next step

--- algorithms/peak_finding/lui_integrate_helper.h
+++ algorithms/peak_finding/lui_integrate_helper.h
@@ -56,7 +56,7 @@ namespace dials { namespace algorithms {
     int nrow=data2d.accessor().all()[0];
     std::cout <<"\n" << "ncol =" << ncol <<"\n" << "nrow =" << nrow <<"\n";
     float dx, dy;
-    float pie_size;
+    int pie_size;
     // std::cout <<"\n" << "x,y =" << xpos << ", " << ypos << "\n";
     int contr = 0;
     float pi_sqrt = 1.77245385091;
@@ -87,9 +87,26 @@ namespace dials { namespace algorithms {
       }
     }
     int area=contr;
-    pie_size=2.0 * pi_sqrt * sqrt(contr);
-    float dist[int(pie_size)];
-    float angl[int(pie_size)];
+    pie_size=int(2.0 * pi_sqrt * sqrt(float(area)));
+    std::cout << "pie_size =" << pie_size << "\n";
+    /*float dist[int(pie_size)];
+    float angl[int(pie_size)];*/
+    float ang_min, ang_max;
+    for (int pos=0; pos < pie_size; pos++){
+      ang_min = 2.0 * pi * float(pos) / float(pie_size);
+      ang_max = 2.0 * pi * float(pos + 1) / float(pie_size);
+   /*   if (ang_max > 2.0*pi){
+        ang_max = 2.0*pi;
+      }
+      if (ang_max < 0.0){
+        ang_max = 0.0;
+      } */
+
+      std::cout << "pos =" << pos << "\n";
+
+      std::cout << "\n" << "ang min =" << ang_min << "\n" << "ang max =" << ang_max << "\n";
+    }
+    std::cout << "\n" << "2 * pi =" << 2.0 * pi << "\n";
 
     return pie_size;
   }
