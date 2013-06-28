a25f1fd0ddcb79daba5b2d9ab442fe44caab4058
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2013-06-28 12:57:16 +0000

    L P corrections done

--- algorithms/integration/lp_correction.py
+++ algorithms/integration/lp_correction.py
@@ -1,6 +1,6 @@
 def correct_intensity(sweep, crystal, reflections):
     for ref in reflections:
-        if ref.is_valid():
+        if ref.status == 0:
             LP_calculations(sweep, crystal, ref)
     return reflections
 
@@ -9,39 +9,22 @@ def LP_calculations(sweep, crystal, reflection):
     tpl_s0 = sweep.get_beam().get_s0()
     tpl_m2 = sweep.get_goniometer().get_rotation_axis()
     tpl_s1 = reflection.beam_vector
-
     p = sweep.get_beam().get_polarization_fraction()
+    import numpy as np
 
+    n = np.array(tpl_n)
+    s0 = np.array(tpl_s0)
+    u = np.array(tpl_m2)
+    s = np.array(tpl_s1)
 
-#    print "______________________________________________________________________________"
-#    print "p  =", p
-#    print "tpl_n  =", tpl_n
-#    print "tpl_s0 =", tpl_s0
-#    print "tpl_m2 =", tpl_m2
-#    print "tpl_s1 =", tpl_s1
-#    print
-#    print "I(R) =", reflection.intensity
-#    import numpy as np
+    L_f = np.abs(np.dot(s, np.cross(u, s0))) / (modulus_3D(s) * modulus_3D(s0))
 
-#    n = np.array(tpl_n)
-#    s0 = np.array(tpl_s0)
-#    u = np.array(tpl_m2)
-#    s = np.array(tpl_s1)
+    P_f = (1 - 2 * p) * (1 - (np.dot(n, s) / modulus_3D(s)) ** 2.0)          \
+      + p * (1 + (np.dot(s, s0) / (modulus_3D(s) * modulus_3D(s0))) ** 2.0)
 
-#    print "np vect(np_n ) =", n
-#    print "np vect(np_s0) =", s0
-#    print "np vect(np_m2) =", u
-#    print "np vect(np_s1) =", s
-#    print
-#    print "cross_prod(s0,s) =", np.cross(s0, s)
+    reflection.intensity = reflection.intensity * L_f * P_f
 
-#    L_f = abs(np.dot(s, np.cross(u, s0))) / np.dot(abs(s), abs(s0))
-#    print "Loretz factor =", L_f
-#    print
-
-
-
-#    print "______________________________________________________________________________"
-
-
-    reflection.intensity = reflection.intensity / 2.0
+def modulus_3D(vect):
+    import numpy as np
+    mold = np.sqrt(vect[0] * vect[0] + vect[1] * vect[1] + vect[2] * vect[2])
+    return mold
