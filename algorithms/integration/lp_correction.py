f1848311001e60df86225bfa771b2019966292ee
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2013-06-28 13:59:55 +0000

    removing numpy from L P corrections

--- algorithms/integration/lp_correction.py
+++ algorithms/integration/lp_correction.py
@@ -10,21 +10,49 @@ def LP_calculations(sweep, crystal, reflection):
     tpl_m2 = sweep.get_goniometer().get_rotation_axis()
     tpl_s1 = reflection.beam_vector
     p = sweep.get_beam().get_polarization_fraction()
-    import numpy as np
+    from scitbx import matrix
+    #import math
+    #import numpy as np
 
-    n = np.array(tpl_n)
-    s0 = np.array(tpl_s0)
-    u = np.array(tpl_m2)
-    s = np.array(tpl_s1)
+    n = matrix.col(tpl_n)
+    s0 = matrix.col(tpl_s0)
+    u = matrix.col(tpl_m2)
+    s = matrix.col(tpl_s1)
 
-    L_f = np.abs(np.dot(s, np.cross(u, s0))) / (modulus_3D(s) * modulus_3D(s0))
+    L_f = abs(s.dot(u.cross(s0))) / (s.length() * s0.length())
 
-    P_f = (1 - 2 * p) * (1 - (np.dot(n, s) / modulus_3D(s)) ** 2.0)          \
-      + p * (1 + (np.dot(s, s0) / (modulus_3D(s) * modulus_3D(s0))) ** 2.0)
+    P_f = (1 - 2 * p) * (1 - (n.dot(s) / s.length()) ** 2.0) + \
+     p * (1 + (s.dot(s0) / (s.length() * s0.length())) ** 2.0)
 
     reflection.intensity = reflection.intensity * L_f * P_f
 
-def modulus_3D(vect):
-    import numpy as np
-    mold = np.sqrt(vect[0] * vect[0] + vect[1] * vect[1] + vect[2] * vect[2])
-    return mold
+'''
+>>> from scitbx import matrix
+>>> c = matrix.col((1, 0, 0))
+>>> help(c)
+
+>>> c.length()
+1.0
+>>> b = matrix.col((0, 1, 0))
+>>> c.cross(b)
+matrix.rec(elems=(0, 0, 1), n=(3,1))
+>>> M = matrix.rec((1, 0, 0, 0, 0, 1, 0, 1, 0))
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: __init__() takes exactly 3 arguments (2 given)
+>>> M = matrix.rec((1, 0, 0, 0, 0, 1, 0, 1, 0))
+KeyboardInterrupt
+>>> M = matrix.sqr((1, 0, 0, 0, 0, 1, 0, 1, 0))
+>>> M * c
+matrix.rec(elems=(1, 0, 0), n=(3,1))
+>>> M * b
+matrix.rec(elems=(0, 0, 1), n=(3,1))
+>>> c.dot(c)
+1
+>>> c.dot(b)
+0
+>>> c.cross(b)
+matrix.rec(elems=(0, 0, 1), n=(3,1))
+>>> c.length()
+1.0
+'''
