222f2d4ac6314d15e5644e739a2fdc1481bdea15
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2013-07-05 12:15:02 +0000

    moving to production code (algorithms area) the completed summation algorithm, now translated to C++, also making it compatible with 3D blocks of several images. (part 2)

--- algorithms/integration/summation2d.py
+++ algorithms/integration/summation2d.py
@@ -1,3 +1,4 @@
+
 #
 # integrate2d.py
 #
@@ -22,10 +23,11 @@ class Summation2d(IntegrationInterface):
         return reflections
 
     def integrate(self, reflections):
-        tmp_numpy_layering_n_integrating(reflections)
+        #tmp_numpy_layering_n_integrating(reflections)
+        flex_2d_layering_n_integrating(reflections)
 
 def tmp_numpy_layering_n_integrating(reflections):
-    print "hi there translating and layering 01"
+    print "hi there translating and layering 01 "
 
     from dials.algorithms.integration.summation_2d import raw_2d_integration
     for ref in reflections:
@@ -33,27 +35,31 @@ def tmp_numpy_layering_n_integrating(reflections):
             shoebox = ref.shoebox.as_numpy_array()
             mask = ref.shoebox_mask.as_numpy_array()
             backgound = ref.shoebox_background.as_numpy_array()
-
+            ref.intensity = 0.0
+            ref.intensity_variance = 0.0
             for i in range(shoebox.shape[0]):
                 data2d = shoebox[i]
                 mask2d = mask[i]
                 backgound2d = backgound[i]
                 itns, sigma = raw_2d_integration(data2d, mask2d, backgound2d)
-            ref.intensity = float(itns)
-            ref.intensity_variance = float(sigma * sigma)
+                ref.intensity += float(itns)
+                ref.intensity_variance += float(sigma * sigma)
+                #ref.intensity_variance += float(sigma)
+            #ref.intensity_variance = ref.intensity_variance * ref.intensity_variance
 
-    print "hi there translating and layering 02"
+    print "hi there translating and layering 02 "
 def flex_2d_layering_n_integrating(reflections):
     from scitbx.array_family import flex
-    from dials.scratch.luiso_s import raw_2d_cut
-    print "flex array version of summation integration 01"
+    from dials.algorithms.integration import raw_2d_cut
+    print "starts flex array version of summation integration "
 
     for ref in reflections:
         if ref.is_valid():
             shoebox = ref.shoebox
             mask = ref.shoebox_mask
             background = ref.shoebox_background
-
+            ref.intensity = 0.0
+            ref.intensity_variance = 0.0
             for i in range(shoebox.all()[0]):
                 data2d = shoebox[i:i + 1, :, :]
                 mask2d = mask[i:i + 1, :, :]
@@ -65,47 +71,9 @@ def flex_2d_layering_n_integrating(reflections):
 
                 reslt = raw_2d_cut(data2d, mask2d, background2d)
 
-            ref.intensity = reslt[0]
-            ref.intensity_variance = reslt[1] * reslt[1]
+                ref.intensity += reslt[0]
+                ref.intensity_variance += reslt[1] * reslt[1]
+
+    print "flex array version of summation integration ends"
 
     return reflections
-
-##############################################################################################
-    from dials.algorithms.integration.summation_2d import raw_2d_integration
-    for ref in reflections:
-        if ref.is_valid():
-            shoebox = ref.shoebox.as_numpy_array()
-            mask = ref.shoebox_mask.as_numpy_array()
-            backgound = ref.shoebox_background.as_numpy_array()
-
-            for i in range(shoebox.shape[0]):
-                data2d = shoebox[i]
-                mask2d = mask[i]
-                backgound2d = backgound[i]
-                itns, sigma = raw_2d_integration(data2d, mask2d, backgound2d)
-            ref.intensity = float(itns)
-            ref.intensity_variance = float(sigma * sigma)
-#############################################################################################
-    a = tst_01(flex.int(data2d)).as_numpy_array()
-    print a
-    '''
-        from dials.algorithms.background import curved_background_flex_2d
-        from scitbx.array_family import flex
-        for ref in reflections:
-            if ref.is_valid():
-                shoebox = ref.shoebox
-                mask = ref.shoebox_mask
-                background = ref.shoebox_background
-                for i in range(shoebox.all()[0]):
-                    data2d = shoebox[i:i + 1, :, :]
-                    mask2d = mask[i:i + 1, :, :]
-                    data2d.reshape(flex.grid(shoebox.all()[1:]))
-                    mask2d.reshape(flex.grid(shoebox.all()[1:]))
-                    background2d = curved_background_flex_2d(data2d.as_double(), mask2d)
-                    background2d.reshape(flex.grid(1, background2d.all()[0], background2d.all()[1]))
-                    background[i:i + 1, :, :] = background2d.as_double()
-
-
-        return reflections
-    '''
-    print "flex array version of summation integration 02"
