bf2fbe830dbe383e24ece40456253102ff810a62
James Parkhurst <james.parkhurst@diamond.ac.uk>
2013-07-10 08:30:38 +0000

    Added a test for the new reciprocal space package (initial commit)

---
+++ test/algorithms/reflexion_basis/tst_coordinate_system.py
@@ -0,0 +1,59 @@
+
+class Test(object):
+    def __init__(self):
+        pass
+
+    def run(self):
+        self.tst_zeta_factor()
+        self.tst_coordinate_system()
+        self.tst_from_beam_vector()
+        self.tst_from_rotation_angle_fast()
+        self.tst_from_rotation_angle_accurate()
+        self.tst_from_beam_vector_and_rotation_angle_fast()
+        self.tst_from_beam_vector_and_rotation_angle_accurate()
+        self.tst_to_beam_vector()
+        self.tst_to_rotation_angle_fast()
+        self.tst_to_rotation_angle_accurate()
+        self.tst_to_beam_vector_and_rotation_angle_fast()
+        self.tst_to_beam_vector_and_rotation_angle_accurate()
+
+    def tst_zeta_factor(self):
+        pass
+
+    def tst_coordinate_system(self):
+        pass
+
+    def tst_from_beam_vector(self):
+        pass
+
+    def tst_from_rotation_angle_fast(self)
+        pass
+
+    def tst_from_rotation_angle_accurate(self)
+        pass
+
+    def tst_from_beam_vector_and_rotation_angle_fast(self)
+        pass
+
+    def tst_from_beam_vector_and_rotation_angle_accurate(self)
+        pass
+
+    def tst_to_beam_vector(self)
+        pass
+
+    def tst_to_rotation_angle_fast(self)
+        pass
+
+    def tst_to_rotation_angle_accurate(self)
+        pass
+
+    def tst_to_beam_vector_and_rotation_angle_fast(self)
+        pass
+
+    def tst_to_beam_vector_and_rotation_angle_accurate(self)
+        pass
+
+
+if __name__ == '__main__':
+    test = Test()
+    test.run()
