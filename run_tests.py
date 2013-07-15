4c66c96c7a2fa0f44a2b9eeb898f3f4a81bb536f
James Parkhurst <james.parkhurst@diamond.ac.uk>
2013-07-15 15:17:58 +0000

    Added test to run_test.py

--- run_tests.py
+++ run_tests.py
@@ -33,6 +33,7 @@ tst_list = (
     "$D/test/algorithms/reflexion_basis/tst_map_frames.py",
     "$D/test/algorithms/reflexion_basis/tst_rebin_pixels.py",
     "$D/test/algorithms/reflexion_basis/tst_beam_vector_map.py",
+    "$D/test/algorithms/reflexion_basis/tst_grid_index_generator.py",
     "$D/test/algorithms/peak_finding/tst_spotfinder.py",
     )
 
