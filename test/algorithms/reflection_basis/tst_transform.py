8e1c0c208b97167194afa523ba626944e9e78434
James Parkhurst <james.parkhurst@diamond.ac.uk>
2013-07-23 14:08:47 +0000

    Added test for convervation of counts

--- test/algorithms/reflection_basis/tst_transform.py
+++ test/algorithms/reflection_basis/tst_transform.py
@@ -15,10 +15,6 @@ class TestForward(object):
         self.gonio = self.sweep.get_goniometer()
         self.scan = self.sweep.get_scan()
 
-        print self.detector.get_d_matrix()
-        print self.gonio.get_rotation_axis()
-        print self.beam.get_direction()
-        print self.detector.get_beam_centre(self.beam.get_s0())
         self.beam.set_direction((0.0, 0.0, 1.0))
         self.gonio.set_rotation_axis((1.0, 0.0, 0.0))
         self.detector.set_frame((1.0, 0.0, 0.0),
@@ -29,7 +25,7 @@ class TestForward(object):
         self.sigma_divergence =self.beam.get_sigma_divergence(deg=False)
         self.mosaicity = 0.157 * pi / 180
         self.n_sigma = 3
-        self.grid_size = 7
+        self.grid_size = 4
         self.delta_divergence = self.n_sigma * self.sigma_divergence
 
         step_size = self.delta_divergence / self.grid_size
@@ -48,10 +44,15 @@ class TestForward(object):
             self.mosaicity, self.n_sigma, self.grid_size)
 
     def __call__(self):
+        self.tst_conservation_of_counts()
+        self.tst_transformed_centroid()
+
+    def tst_conservation_of_counts(self):
 
         from scitbx import matrix
         from random import uniform
         from dials.algorithms.reflection_basis import CoordinateSystem
+        from dials.algorithms.reflection_basis import transform
         from scitbx.array_family import flex
         from time import time
 
@@ -59,127 +60,70 @@ class TestForward(object):
         m2 = self.gonio.get_rotation_axis()
         s0_length = matrix.col(self.beam.get_s0()).length()
 
-        # Get random x, y, z
-        x = uniform(300, 1800)
-        y = uniform(300, 1800)
-        z = uniform(-10, 0)
-        print x, y, z
-
-#        x, y, z = 500, 500, 10
-        x, y, z = 993.376692565, 1272.1175661, -7.9419902089
-
-        # Get random s1, phi, panel
-        s1 = matrix.col(self.detector.get_pixel_lab_coord(
-            (x, y))).normalize() * s0_length
-        phi = self.scan.get_angle_from_array_index(z, deg=False)
-        panel = 0
-
-        # Calculate the bounding box
-        bbox = self.calculate_bbox(s1, phi, panel)
-        x0, x1, y0, y1, z0, z1 = bbox
-
-        from dials.algorithms.reflection_basis import FromDetector
-
-
-        print bbox, x, y, z
-
-        # Create the coordinate system
-        cs = CoordinateSystem(m2, s0, s1, phi)
-
-        from_detector = FromDetector(cs, self.detector.get_d_matrix())
-        step_size = self.delta_divergence / self.grid_size
-        e0, e1 = from_detector(self.detector.pixel_to_millimeter((x, y)))
-        e00, e01 = from_detector(self.detector.pixel_to_millimeter((x0, y0)))
-        e10, e11 = from_detector(self.detector.pixel_to_millimeter((x0, y1)))
-        e20, e21 = from_detector(self.detector.pixel_to_millimeter((x1, y0)))
-        e30, e31 = from_detector(self.detector.pixel_to_millimeter((x1, y1)))
-        offset = self.grid_size + 0.5
-        print e0 / step_size + offset, e1 / step_size + offset
-        print e00 / step_size + offset, e01 / step_size + offset
-        print e10 / step_size + offset, e11 / step_size + offset
-        print e20 / step_size + offset, e21 / step_size + offset
-        print e30 / step_size + offset, e31 / step_size + offset
-        print self.delta_divergence, self.delta_divergence2,
-
-        # Create the image
-        image = flex.double(flex.grid(z1 - z0, y1 - y0, x1 - x0), 1)
-        mask = flex.bool(flex.grid(image.all()), True)
-
-        # Transform the image to the grid
-        grid = self.transform(cs, bbox, image, mask)
-
-        #print flex.min(grid), flex.max(grid)
-        print grid.as_numpy_array()[4,:,:]
-        print grid.all()
-
-        from dials.algorithms.reflection_basis import transform
-        step_size = self.delta_divergence / self.grid_size
+        # Create an s1 map
         s1_map = transform.beam_vector_map(self.detector, self.beam, True)
-        index = transform.GridIndexGenerator(cs, x0, y0, (step_size, step_size),
-            self.grid_size, s1_map)
 
-        indices = []
-        labels = []
-        for j in range(y1 - y0 + 1):
-            for i in range(x1 - x0 + 1):
-                indices.append(index(j,i))
-                labels.append('{0}, {1}'.format(i, j))
-        x, y = zip(*indices)
+        for i in range(100):
 
-        #x = [xx - 0.5 for xx in x]
-        #y = [yy - 0.5 for yy in y]
-        #print x, y
-        from dials.algorithms.polygon import simple_area
-        indices = []
-        area = []
-        from_detector = FromDetector(cs, self.detector.get_d_matrix())
-        #index2 = lambda j, i: matrix.col(from_detector(self.detector.pixel_to_millimeter((i, j)))) / step_size + matrix.col((offset, offset))
+            # Get random x, y, z
+            x = uniform(300, 1800)
+            y = uniform(300, 1800)
+            z = uniform(-10, 0)
 
+            # Get random s1, phi, panel
+            s1 = matrix.col(self.detector.get_pixel_lab_coord(
+                (x, y))).normalize() * s0_length
+            phi = self.scan.get_angle_from_array_index(z, deg=False)
+            panel = 0
 
-        def index(j, i):
-            mm = self.detector.pixel_to_millimeter((i + x0, j + y0))
-            ee = matrix.col(from_detector(mm))
-            return (ee[0] / step_size + offset, ee[1] / step_size + offset)
+            # Calculate the bounding box
+            bbox = self.calculate_bbox(s1, phi, panel)
+            x0, x1, y0, y1, z0, z1 = bbox
 
-        for j in range(y1 - y0):
-            for i in range(x1 - x0):
-                gx00, gy00 = index(j, i)
-                gx01, gy01 = index(j, i+1)
-                gx10, gy10 = index(j+1,i)
-                gx11, gy11 = index(j+1,i+1)
-                poly = flex.vec2_double([
-                    (gx00, gy00),
-                    (gx01, gy01),
-                    (gx11, gy11),
-                    (gx10, gy10)])
-                indices.append((gx00, gy00))
-                area.append(simple_area(poly))
+            # Create the coordinate system
+            cs = CoordinateSystem(m2, s0, s1, phi)
 
-        from matplotlib import pylab
-        x, y = zip(*indices)
-        pylab.plot(y, area)
-        pylab.show()
-#        for i in range(9):
-#            pylab.subplot(3, 3, i)
-#            pylab.imshow(grid.as_numpy_array()[i,:,:], origin='bottom')
-#            pylab.scatter(x, y)
+            # The grid index generator
+            step_size = self.delta_divergence / self.grid_size
+            grid_index = transform.GridIndexGenerator(cs, x0, y0,
+                (step_size, step_size), self.grid_size, s1_map)
 
-        #pylab.imshow(grid.as_numpy_array()[4,:,:], origin='bottom', interpolation='none')
-#        pylab.scatter(x, y)
-#        for j in range(9):
-#            for i in range(9):
-#                ii = i - 0.5
-#                jj = j - 0.5
-#                px = [ii, ii + 1, ii + 1, ii, ii]
-#                py = [jj, jj, jj + 1, jj + 1, jj]
-#                pylab.plot(px, py, color='white')
-#        for ll, xx, yy in zip(labels, x, y):
-#            pylab.annotate(ll, xy = (xx, yy))
-        #pylab.show()
+            # Create the image
+            image = flex.double(flex.grid(z1 - z0, y1 - y0, x1 - x0), 1)
+            mask = flex.bool(flex.grid(image.all()), False)
+            for j in range(y1 - y0):
+                for i in range(x1 - x0):
+                    inside = False
+                    gx00, gy00 = grid_index(j, i)
+                    gx01, gy01 = grid_index(j, i+1)
+                    gx10, gy10 = grid_index(j+1, i)
+                    gx11, gy11 = grid_index(j+1, i+1)
+                    mingx = min([gx00, gx01, gx10, gx11])
+                    maxgx = max([gx00, gx01, gx10, gx11])
+                    mingy = min([gy00, gy01, gy10, gy11])
+                    maxgy = max([gy00, gy01, gy10, gy11])
+                    if (mingx >= 0 and maxgx <= 2 * self.grid_size + 1 and
+                        mingy >= 0 and maxgy <= 2 * self.grid_size + 1):
+                        inside = True
+                    for k in range(1, z1 - z0 - 1):
+                        mask[k,j,i] = inside
+
+            # Transform the image to the grid
+            grid = self.transform(cs, bbox, image, mask)
+
+            # Get the sums and ensure they're the same
+            eps = 1e-7
+            sum_grid = flex.sum(grid)
+            sum_image = flex.sum(flex.double(flex.select(image, flags=mask)))
+            assert(abs(sum_grid - sum_image) <= eps)
 
         # Test passed
         print 'OK'
 
+    def tst_transformed_centroid(self):
+
+        # Test passed
+        print 'OK'
 
 class TestReverse(object):
     def __init__(self, filename):
