7edd056c783562ca8377a3cba343eea9b080773d
James Parkhurst <james.parkhurst@diamond.ac.uk>
2013-07-29 11:07:14 +0000

    Removed shoebox bits from integration diractory

--- algorithms/integration/boost_python/integration_ext.cc
+++ algorithms/integration/boost_python/integration_ext.cc
@@ -16,10 +16,7 @@ namespace dials { namespace algorithms { namespace boost_python {
   using namespace boost::python;
 
   void export_helpers();
-  void export_bbox_calculator();
-  void export_find_overlapping_reflections();
   void export_reflection_profile_helpers();
-  void export_shoebox_masker();
   void export_luiso_s_2d_integration();
   void export_summation();
   void export_summation_reciprocal_space();
@@ -28,10 +25,7 @@ namespace dials { namespace algorithms { namespace boost_python {
   BOOST_PYTHON_MODULE(dials_algorithms_integration_ext)
   {
     export_helpers();
-    export_bbox_calculator();
-    export_find_overlapping_reflections();
     export_reflection_profile_helpers();
-    export_shoebox_masker();
     export_luiso_s_2d_integration();
     export_summation();
     export_summation_reciprocal_space();
