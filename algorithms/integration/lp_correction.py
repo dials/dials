91ccfd15c531c1f2020962bced675ead77509d37
Graeme Winter <graeme.winter@gmail.com>
2013-08-01 07:46:58 +0000

    Largely cosmetic changes

--- algorithms/integration/lp_correction.py
+++ algorithms/integration/lp_correction.py
@@ -6,16 +6,18 @@ def correct_intensity(sweep, crystal, reflections):
     return reflections
 
 def LP_calculations(sweep, crystal, reflection):
+    '''See Kabsch, J. Appl. Cryst 1988 21 916-924.'''
+
     tpl_n = sweep.get_beam().get_polarization_normal()
     tpl_s0 = sweep.get_beam().get_s0()
     tpl_m2 = sweep.get_goniometer().get_rotation_axis()
     tpl_s1 = reflection.beam_vector
     p = sweep.get_beam().get_polarization_fraction()
+    
     # FIXME hack for testing
     # p = 0.5
+    
     from scitbx import matrix
-    #import math
-    #import numpy as np
 
     n = matrix.col(tpl_n)
     s0 = matrix.col(tpl_s0)
@@ -25,11 +27,7 @@ def LP_calculations(sweep, crystal, reflection):
     L_f = abs(s.dot(u.cross(s0))) / (s.length() * s0.length())
 
     P_f = (1 - 2 * p) * (1 - (n.dot(s) / s.length()) ** 2.0) + \
-     p * (1 + (s.dot(s0) / (s.length() * s0.length())) ** 2.0)
-
-    # FIXED the polarization factor is an inverse correction see Kabsch
-    # Data Correction and Scaling (b) comment text
-    # J. Appl. Cryst 1988 21 916-924
+          p * (1 + (s.dot(s0) / (s.length() * s0.length())) ** 2.0)
 
     reflection.corrected_intensity = reflection.intensity * L_f / P_f
     reflection.corrected_intensity_variance = \
