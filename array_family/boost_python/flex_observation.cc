089a436e5c8d4d81ad07bacf0546f02ab024b124
James Parkhurst <james.parkhurst@diamond.ac.uk>
2013-09-02 09:35:35 +0000

    Added functions to flex.observation and flex.prediction

--- array_family/boost_python/flex_observation.cc
+++ array_family/boost_python/flex_observation.cc
@@ -10,6 +10,8 @@
  */
 #include <boost/python.hpp>
 #include <boost/python/def.hpp>
+#include <scitbx/vec2.h>
+#include <scitbx/vec3.h>
 #include <scitbx/array_family/boost_python/flex_wrapper.h>
 #include <scitbx/array_family/ref_reductions.h>
 #include <dials/model/data/observation.h>
@@ -17,12 +19,137 @@
 namespace dials { namespace af { namespace boost_python {
 
   using namespace boost::python;
+  
+  using scitbx::af::const_ref;
+  using scitbx::af::shared;
+  using scitbx::vec2;
+  using scitbx::vec3;
   using dials::model::Observation;
+
+  static
+  shared< vec3<double> > observation_centroid_px_position(
+      const const_ref<Observation> &obj) {
+     shared< vec3<double> > result(obj.size());
+     for (std::size_t i = 0; i < result.size(); ++i) {
+      result[i] = obj[i].centroid.px.position;
+     }
+     return result;
+  }
+
+  static
+  shared< vec3<double> > observation_centroid_px_variance(
+      const const_ref<Observation> &obj) {
+     shared< vec3<double> > result(obj.size());
+     for (std::size_t i = 0; i < result.size(); ++i) {
+      result[i] = obj[i].centroid.px.variance;
+     }
+     return result;
+  }
+
+  static
+  shared< vec3<double> > observation_centroid_px_std_err_sq(
+      const const_ref<Observation> &obj) {
+     shared< vec3<double> > result(obj.size());
+     for (std::size_t i = 0; i < result.size(); ++i) {
+      result[i] = obj[i].centroid.px.std_err_sq;
+     }
+     return result;
+  }
+
+  static
+  shared< vec3<double> > observation_centroid_mm_position(
+      const const_ref<Observation> &obj) {
+     shared< vec3<double> > result(obj.size());
+     for (std::size_t i = 0; i < result.size(); ++i) {
+      result[i] = obj[i].centroid.mm.position;
+     }
+     return result;
+  }
+
+  static
+  shared< vec3<double> > observation_centroid_mm_variance(
+      const const_ref<Observation> &obj) {
+     shared< vec3<double> > result(obj.size());
+     for (std::size_t i = 0; i < result.size(); ++i) {
+      result[i] = obj[i].centroid.mm.variance;
+     }
+     return result;
+  }
+
+  static
+  shared< vec3<double> > observation_centroid_mm_std_err_sq(
+      const const_ref<Observation> &obj) {
+     shared< vec3<double> > result(obj.size());
+     for (std::size_t i = 0; i < result.size(); ++i) {
+      result[i] = obj[i].centroid.mm.std_err_sq;
+     }
+     return result;
+  }
+
+  static
+  shared<double> observation_intensity_observed_value(
+      const const_ref<Observation> &obj) {
+     shared<double> result(obj.size());
+     for (std::size_t i = 0; i < result.size(); ++i) {
+      result[i] = obj[i].intensity.observed.value;
+     }
+     return result;
+  }
+
+  static
+  shared<double> observation_intensity_observed_variance(
+      const const_ref<Observation> &obj) {
+     shared<double> result(obj.size());
+     for (std::size_t i = 0; i < result.size(); ++i) {
+      result[i] = obj[i].intensity.observed.variance;
+     }
+     return result;
+  }
+
+  static
+  shared<double> observation_intensity_corrected_value(
+      const const_ref<Observation> &obj) {
+     shared<double> result(obj.size());
+     for (std::size_t i = 0; i < result.size(); ++i) {
+      result[i] = obj[i].intensity.corrected.value;
+     }
+     return result;
+  }
+
+  static
+  shared<double> observation_intensity_corrected_variance(
+      const const_ref<Observation> &obj) {
+     shared<double> result(obj.size());
+     for (std::size_t i = 0; i < result.size(); ++i) {
+      result[i] = obj[i].intensity.corrected.variance;
+     }
+     return result;
+  }
 
   void export_flex_observation()
   {
     scitbx::af::boost_python::flex_wrapper <
-        Observation, return_internal_reference<> >::plain("observation");
+        Observation, return_internal_reference<> >::plain("observation")
+      .def("centroid_px_position",
+        &observation_centroid_px_position)
+      .def("centroid_px_variance",
+        &observation_centroid_px_variance)
+      .def("centroid_px_std_err_eq",
+        &observation_centroid_px_std_err_sq)
+      .def("centroid_mm_position",
+        &observation_centroid_mm_position)
+      .def("centroid_mm_variance",
+        &observation_centroid_mm_variance)
+      .def("centroid_mm_std_err_eq",
+        &observation_centroid_mm_std_err_sq)
+      .def("intensity_observed_value",
+        &observation_intensity_observed_value)
+      .def("intensity_observed_variance",
+        &observation_intensity_observed_variance)
+      .def("intensity_corrected_value",
+        &observation_intensity_corrected_value)
+      .def("intensity_corrected_variance",
+        &observation_intensity_corrected_variance);
   }
 
 }}} // namespace dials::af::boost_python
--- array_family/boost_python/flex_prediction.cc
+++ array_family/boost_python/flex_prediction.cc
@@ -10,6 +10,7 @@
  */
 #include <boost/python.hpp>
 #include <boost/python/def.hpp>
+#include <cctbx/miller.h>
 #include <scitbx/array_family/boost_python/flex_wrapper.h>
 #include <scitbx/array_family/ref_reductions.h>
 #include <dials/model/data/prediction.h>
@@ -17,12 +18,138 @@
 namespace dials { namespace af { namespace boost_python {
 
   using namespace boost::python;
+  using scitbx::af::const_ref;
+  using scitbx::af::shared;
+  using scitbx::vec2;
+  using scitbx::vec3;
   using dials::model::Prediction;
+  
+  typedef cctbx::miller::index<> MillerIndex;
+
+  static
+  shared<MillerIndex> prediction_miller_index(
+      const const_ref<Prediction> &obj) {
+     shared<MillerIndex> result(obj.size());
+     for (std::size_t i = 0; i < result.size(); ++i) {
+      result[i] = obj[i].miller_index;
+     }
+     return result;
+  }
+
+  static
+  shared< vec3<double> > prediction_beam_vector(
+      const const_ref<Prediction> &obj) {
+     shared< vec3<double> > result(obj.size());
+     for (std::size_t i = 0; i < result.size(); ++i) {
+      result[i] = obj[i].beam_vector;
+     }
+     return result;
+  }
+
+  static
+  shared<int> prediction_panel(
+      const const_ref<Prediction> &obj) {
+     shared<int> result(obj.size());
+     for (std::size_t i = 0; i < result.size(); ++i) {
+      result[i] = obj[i].panel;
+     }
+     return result;
+  }
+
+  static
+  shared<bool> prediction_entering(
+      const const_ref<Prediction> &obj) {
+     shared<bool> result(obj.size());
+     for (std::size_t i = 0; i < result.size(); ++i) {
+      result[i] = obj[i].entering;
+     }
+     return result;
+  }
+
+  static
+  shared< vec3<double> > prediction_position_px(
+      const const_ref<Prediction> &obj) {
+     shared< vec3<double> > result(obj.size());
+     for (std::size_t i = 0; i < result.size(); ++i) {
+      result[i] = obj[i].position.px;
+     }
+     return result;
+  }
+
+  static
+  shared< vec2<double> > prediction_position_px_coord(
+      const const_ref<Prediction> &obj) {
+     shared< vec2<double> > result(obj.size());
+     for (std::size_t i = 0; i < result.size(); ++i) {
+      result[i] = vec2<double>(obj[i].position.px[0], obj[i].position.px[1]);
+     }
+     return result;
+  }
+
+  static
+  shared<double> prediction_position_frame(
+      const const_ref<Prediction> &obj) {
+     shared<double> result(obj.size());
+     for (std::size_t i = 0; i < result.size(); ++i) {
+      result[i] = obj[i].position.px[2];
+     }
+     return result;
+  }
+
+  static
+  shared< vec3<double> > prediction_position_mm(
+      const const_ref<Prediction> &obj) {
+     shared< vec3<double> > result(obj.size());
+     for (std::size_t i = 0; i < result.size(); ++i) {
+      result[i] = obj[i].position.mm;
+     }
+     return result;
+  }
+
+  static
+  shared< vec2<double> > prediction_position_mm_coord(
+      const const_ref<Prediction> &obj) {
+     shared< vec2<double> > result(obj.size());
+     for (std::size_t i = 0; i < result.size(); ++i) {
+      result[i] = vec2<double>(obj[i].position.mm[0], obj[i].position.mm[1]);
+     }
+     return result;
+  }
+
+  static
+  shared<double> prediction_position_angle(
+      const const_ref<Prediction> &obj) {
+     shared<double> result(obj.size());
+     for (std::size_t i = 0; i < result.size(); ++i) {
+      result[i] = obj[i].position.mm[2];
+     }
+     return result;
+  }
 
   void export_flex_prediction()
   {
     scitbx::af::boost_python::flex_wrapper <
-        Prediction, return_internal_reference<> >::plain("prediction");
+        Prediction, return_internal_reference<> >::plain("prediction")
+      .def("miller_index",
+        &prediction_miller_index)
+      .def("beam_vector",
+        &prediction_beam_vector)
+      .def("panel",
+        &prediction_panel)
+      .def("entering",
+        &prediction_entering)
+      .def("position_px",
+        &prediction_position_px)
+      .def("position_px_coord",
+        &prediction_position_px_coord)
+      .def("position_frame",
+        &prediction_position_frame)
+      .def("position_mm",
+        &prediction_position_mm)
+      .def("position_mm_coord",
+        &prediction_position_mm_coord)
+      .def("position_angle",
+        &prediction_position_angle);
   }
 
 }}} // namespace dials::af::boost_python
