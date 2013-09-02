1486bbec1cee1eae295dc48e96779f3282d0ee9e
James Parkhurst <james.parkhurst@diamond.ac.uk>
2013-09-02 09:35:30 +0000

    Added frame, angle and 2D image coord properties.

--- model/data/boost_python/prediction.cc
+++ model/data/boost_python/prediction.cc
@@ -37,6 +37,48 @@ namespace dials { namespace model { namespace boost_python {
   }
  
   static
+  vec2<double> position_data_get_mm_coord(const Prediction::PositionData& obj) {
+    return vec2<double>(obj.mm[0], obj.mm[1]);
+  }
+  
+  static
+  void position_data_set_mm_coord(Prediction::PositionData& obj, vec2<double> v) {
+    obj.mm[0] = v[0];
+    obj.mm[1] = v[1];
+  }
+  
+  static
+  double position_data_get_angle(const Prediction::PositionData& obj) {
+    return obj.mm[2];
+  }
+  
+  static
+  void position_data_set_angle(Prediction::PositionData& obj, double v) {
+    obj.mm[2] = v;
+  }
+  
+  static
+  vec2<double> position_data_get_px_coord(const Prediction::PositionData& obj) {
+    return vec2<double>(obj.px[0], obj.px[1]);
+  }
+  
+  static
+  void position_data_set_px_coord(Prediction::PositionData& obj, vec2<double> v) {
+    obj.px[0] = v[0];
+    obj.px[1] = v[1];
+  }
+ 
+  static
+  double position_data_get_frame(const Prediction::PositionData& obj) {
+    return obj.px[2];
+  }
+  
+  static
+  void position_data_set_frame(Prediction::PositionData& obj, double v) {
+    obj.px[2] = v;
+  }
+  
+  static
   MillerIndex prediction_get_miller_index(const Prediction &obj) {
     return obj.miller_index;
   }
@@ -64,7 +106,19 @@ namespace dials { namespace model { namespace boost_python {
         &position_data_set_px) 
       .add_property("mm",
         &position_data_get_mm,
-        &position_data_set_mm);     
+        &position_data_set_mm)
+      .add_property("mm_coord",
+        &position_data_get_mm_coord,
+        &position_data_set_mm_coord)
+      .add_property("angle",
+        &position_data_get_angle,
+        &position_data_set_angle)
+      .add_property("px_coord",
+        &position_data_get_px_coord,
+        &position_data_set_px_coord)
+      .add_property("frame",
+        &position_data_get_frame,
+        &position_data_set_frame);
     
     class_<Prediction>("Prediction")
       .add_property("miller_index",
