71a7df631dc87abd22db3a6aa77cd09ba5ed1603
James Parkhurst <james.parkhurst@diamond.ac.uk>
2013-10-15 14:40:05 +0000

    Refactored out some area interpolation code and put into polygon package,

---
+++ algorithms/polygon/boost_python/areal_interpolation_ext.cc
@@ -0,0 +1,42 @@
+/*
+ * areal_interpolation_ext.cc
+ *
+ *  Copyright (C) 2013 Diamond Light Source
+ *
+ *  Author: James Parkhurst
+ *
+ *  This code is distributed under the BSD license, a copy of which is
+ *  included in the root directory of this package.
+ */
+#include <boost/python.hpp>
+#include <boost/python/def.hpp>
+#include <scitbx/vec2.h>
+#include <dials/algorithms/polygon/areal_interpolation.h>
+#include <dials/array_family/scitbx_shared_and_versa.h>
+
+namespace dials { namespace algorithms { namespace polygon {
+  namespace areal_interpolation { namespace boost_python {
+
+  using namespace boost::python;
+  
+  void export_areal_interpolation() 
+  {
+    class_<Match>("Match")
+      .def(init<int, int, double>())
+      .def_readwrite("in", &Match::in)
+      .def_readwrite("out", &Match::out)
+      .def_readwrite("fraction", &Match::fraction);
+
+    def("irregular_grid_to_grid", &irregular_grid_to_grid);
+    def("grid_to_irregular_grid", &grid_to_irregular_grid);
+    
+    def("regrid_irregular_grid_to_grid", &regrid_irregular_grid_to_grid);
+    def("regrid_grid_to_irregular_grid", &regrid_grid_to_irregular_grid);
+  }
+
+  BOOST_PYTHON_MODULE(dials_algorithms_polygon_areal_interpolation_ext)
+  {
+    export_areal_interpolation();
+  }
+
+}}}}}
