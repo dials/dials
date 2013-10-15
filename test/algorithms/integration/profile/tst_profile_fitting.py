9b996ca92febed0947cc404052ac58eab22f1464
James Parkhurst <james.parkhurst@diamond.ac.uk>
2013-10-15 14:40:52 +0000

    Added tests for profile fitting with background.

--- test/algorithms/integration/profile/tst_profile_fitting.py
+++ test/algorithms/integration/profile/tst_profile_fitting.py
@@ -8,6 +8,8 @@ class Test(object):
     def run(self):
         self.tst_identical()
         self.tst_scaled()
+        self.tst_with_flat_background()
+        self.tst_with_noisy_flat_background()
 
     def tst_identical(self):
 
@@ -60,6 +62,63 @@ class Test(object):
         eps = 1e-7
         assert(abs(I - flex.sum(c)) < eps)
         assert(abs(V - I) < eps)
+
+        print 'OK'
+
+    def tst_with_flat_background(self):
+
+        from dials.algorithms.integration.profile import ProfileFitting
+        from scitbx.array_family import flex
+        from tst_profile_helpers import gaussian
+
+        # Create profile
+        p = gaussian((9, 9, 9), 1, (4, 4, 4), (2, 2, 2))
+        s = flex.sum(p)
+        p = p / s
+
+        # Copy profile
+        c0 = gaussian((9, 9, 9), 1, (4, 4, 4), (2, 2, 2))
+        b = flex.double(flex.grid(9, 9, 9), 5)
+        c = c0 + b
+
+        # Fit
+        fit = ProfileFitting(p, c, b)
+        I = fit.intensity()
+        V = fit.variance()
+
+        # Test intensity is the same
+        eps = 1e-7
+        assert(abs(I - flex.sum(c0)) < eps)
+        assert(abs(V - (flex.sum(c0) + flex.sum(b))) < eps)
+
+        print 'OK'
+
+    def tst_with_noisy_flat_background(self):
+
+        from dials.algorithms.integration.profile import ProfileFitting
+        from scitbx.array_family import flex
+        from tst_profile_helpers import gaussian
+
+        # Create profile
+        p = gaussian((9, 9, 9), 1, (4, 4, 4), (2, 2, 2))
+        s = flex.sum(p)
+        p = p / s
+
+        # Copy profile
+        c0 = gaussian((9, 9, 9), 1, (4, 4, 4), (2, 2, 2))
+        n = flex.random_double(9 * 9 * 9)
+        b = flex.double(flex.grid(9, 9, 9), 5) + n
+        c = c0 + b
+
+        # Fit
+        fit = ProfileFitting(p, c, b)
+        I = fit.intensity()
+        V = fit.variance()
+
+        # Test intensity is the same
+        eps = 1e-7
+        assert(abs(I - flex.sum(c0)) < eps)
+        assert(abs(V - (flex.sum(c0) + flex.sum(b))) < eps)
 
         print 'OK'
 
