538094bacd5f6d5f9f9207010988f0c18017a839
Richard Gildea <richard.gildea@diamond.ac.uk>
2013-10-31 19:59:05 +0000

    script to run indexing_api

---
+++ scratch/rjg/run_indexing_api.py
@@ -0,0 +1,49 @@
+import iotbx.phil
+
+master_params = ""
+master_phil_scope = iotbx.phil.parse(input_string=master_params)
+
+def run(args):
+  import time
+  from libtbx.phil import command_line
+  from dials.util.command_line import Importer
+
+  args = sys.argv[1:]
+  importer = Importer(args)
+  if len(importer.imagesets) == 0:
+      print "No sweep object could be constructed"
+      return
+  elif len(importer.imagesets) > 1:
+      raise RuntimeError("Only one imageset can be processed at a time")
+  sweeps = importer.imagesets
+  reflections = importer.reflections
+  assert len(reflections) > 0
+  args = importer.unhandled_arguments
+
+  sweep = sweeps[0]
+  cmd_line = command_line.argument_interpreter(master_params=master_phil_scope)
+  working_phil, args = cmd_line.process_and_fetch(
+      args=args, custom_processor="collect_remaining")
+  working_phil.show()
+
+  gonio = sweep.get_goniometer()
+  detector = sweep.get_detector()
+  scan = sweep.get_scan()
+  beam = sweep.get_beam()
+  print detector
+  print scan
+  print gonio
+  print beam
+
+  from dials.algorithms.indexing import indexer
+
+  strategies = None
+  idxr = indexer.Indexer(strategies, parameters=working_phil.extract())
+  idxr(reflections, detector, beam, goniometer=gonio, scan=scan)
+
+
+if __name__ == '__main__':
+  import sys
+  from libtbx.utils import show_times_at_exit
+  show_times_at_exit()
+  run(sys.argv[1:])
