51a6063666113024dbc3ae4030ac3d0d6c97d4da
James Parkhurst <james.parkhurst@diamond.ac.uk>
2013-10-31 22:42:06 +0000

    Removed sorting (assumed sorted)

--- array_family/boost_python/flex_pixel_list.cc
+++ array_family/boost_python/flex_pixel_list.cc
@@ -31,52 +31,41 @@ namespace dials { namespace af { namespace boost_python {
   using scitbx::vec3;
   using dials::model::PixelList;
   
-  struct PixelListLessPred {
-    PixelListLessPred(const af::const_ref<PixelList> a)
-      : pl_(a) {}
-     
-    template <typename T> 
-    bool operator()(T a, T b) const {
-      return pl_[a].first_frame() < pl_[b].last_frame();
-    }
-    
-    af::const_ref<PixelList> pl_;
-  };
-  
+  /**
+   * Merge a load of pixel lists
+   */
+  static 
   PixelList pixel_list_merge(const af::const_ref<PixelList> &a) {
     
-    // Sort by frames  
-    std::vector<int> index(a.size());
-    for (std::size_t i = 0; i < index.size(); ++i) {
-      index[i] = i;
-    }
-    std::sort(index.begin(), index.end(), PixelListLessPred(a));
-
+    // Number of pixel lists is > 0
+    DIALS_ASSERT(a.size() > 0);
+    
+    // Calculate the total number of pixels
     std::size_t num = 0;
     for (std::size_t i = 0; i < a.size(); ++i) {
       num += a[i].num_pixels();
     }
     
+    // Allocate arrays
     af::shared< vec3<int> > coords((af::reserve(num)));
     af::shared<int> values((af::reserve(num)));
     
-    // Check the frames are contiguous
-    int2 fr1(0, a[index[0]].first_frame());
-    int2 size = a[index[0]].size();
-    for (std::size_t i = 1; i < index.size(); ++i) {
-      const PixelList &pl = a[index[i]];
-      int2 fr2 = pl.frame_range();
+    // Check the frames are sequential
+    int2 fr1(0, a[0].first_frame());
+    int2 size = a[0].size();
+    for (std::size_t i = 1; i < a.size(); ++i) {
+      int2 fr2 = a[i].frame_range();
       DIALS_ASSERT(fr2[0] == fr1[1]);
-      DIALS_ASSERT(pl.size().all_eq(size));
+      DIALS_ASSERT(a[i].size().all_eq(size));
       fr1 = fr2;
-      af::shared<int> v = pl.values();
-      af::shared< vec3<int> > c = pl.coords();        
+      af::shared<int> v = a[i].values();
+      af::shared< vec3<int> > c = a[i].coords();        
       std::copy(v.begin(), v.end(), std::back_inserter(values));
       std::copy(c.begin(), c.end(), std::back_inserter(coords));
     }
     
-    int2 frame_range(a[index.front()].first_frame(), a[index.back()].last_frame());
-    
+    // Return the merged pixel list
+    int2 frame_range(a.front().first_frame(), a.back().last_frame());
     return PixelList(size, frame_range, values, coords); 
   }
   
