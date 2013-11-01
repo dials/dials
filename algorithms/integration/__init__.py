133522425be18dccb1f1ac81e285cf3aa7e1c439
James Parkhurst <james.parkhurst@diamond.ac.uk>
2013-11-01 16:13:25 +0000

    Removed import that no longer exists

--- algorithms/integration/__init__.py
+++ algorithms/integration/__init__.py
@@ -11,4 +11,3 @@ from summation2d import *
 from summation_reciprocal_space import *
 from profile_fitting_reciprocal_space import *
 from profile_extractor import *
-from reference_profile_factory import *
