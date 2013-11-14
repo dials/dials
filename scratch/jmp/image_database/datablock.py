240b6c7a1abc93c202e1816df4149df9a5c1e650
James Parkhurst <james.parkhurst@diamond.ac.uk>
2013-11-14 15:03:36 +0000

    Added some comments to the data block class.

--- scratch/jmp/image_database/datablock.py
+++ scratch/jmp/image_database/datablock.py
@@ -22,8 +22,20 @@ class ImageRecord(object):
 
 
 class DataBlock(object):
+    ''' High level container for blocks of sweeps and imagesets. '''
 
     def __init__(self, filenames=None, format_class=None):
+        ''' Instantiate from filenames or a format class.
+
+        If no format is given then the format is found from the first
+        filename. All files must use the same format, otherwise an
+        exception will be raised.
+
+        Params:
+            filenames The filenames to use
+            format_class The format class to use
+
+        '''
         from dxtbx.format.Registry import Registry
         from collections import OrderedDict
 
@@ -40,19 +52,25 @@ class DataBlock(object):
                 self.append(f)
 
     def format_class(self):
+        ''' Return the format class. '''
         return self._format_class
 
     def filenames(self):
+        ''' Return the list of filenames. '''
         return list(self._images.iterkeys())
 
     def metadata(self):
+        ''' Return the list of filenames and meta data. '''
         return list(self._images.iteritems())
 
     def extract_all(self):
+        ''' Extract all the images as an image set. '''
         from dxtbx.imageset import ImageSetFactory
-        return ImageSetFactory.make_imageset(self._images.keys(), self._format_class)
+        return ImageSetFactory.make_imageset(
+            self._images.keys(), self._format_class)
 
     def extract_stills(self):
+        ''' Extract all the still images as an image set. '''
         from dxtbx.imageset import ImageSetFactory
         stills = [f for f, i in self._images.iteritems() if i.template is None]
         if len(stills) == 0:
@@ -60,6 +78,7 @@ class DataBlock(object):
         return ImageSetFactory.make_imageset(stills, self._format_class)
 
     def extract_sweeps(self):
+        ''' Extract all the sweeps from the block. '''
         from dxtbx.imageset import ImageSetFactory
         from itertools import groupby
         sweeps = []
@@ -82,6 +101,8 @@ class DataBlock(object):
         return sweeps
 
     def append(self, filename):
+        ''' Add another image to the block. The image must use the same
+        format class, otherwise an exception will be raised. '''
         from os.path import abspath
 
         # Check the image is not already here and can be understood
@@ -100,6 +121,12 @@ class DataBlock(object):
 
 
     def understand(self, filename):
+        ''' Check if the data block format understands the given file. This
+        function checks the method resolution order for the format class and
+        calls the understand methods of each parent down to the bottom level.
+        Just calling the format class understand method directly can result
+        in problems. This is really a workaround for a bug in the way that
+        the format understand method works. '''
         from dxtbx.format.Format import Format
         mro = self._format_class.mro()[::-1]
         if len(mro) <= 2 or mro[0] != object or mro[1] != Format:
@@ -110,6 +137,7 @@ class DataBlock(object):
         return True
 
     def _create_record(self, filename, last=None):
+        ''' Get any information about the image we can and create a record. '''
         from dxtbx.sweep_filenames import template_regex
         from os.path import getmtime
 
@@ -154,9 +182,12 @@ class DataBlock(object):
             template=template, index=index)
 
     def __getstate__(self):
+        ''' Return the dict for pickling. '''
         return self.__dict__
 
     def __setstate__(self, state):
+        ''' Update the dict from pickling. On reload, update any records for
+        images that have changed since the class was pickled. '''
         from os.path import getmtime
         self.__dict__.update(state)
         last = None
@@ -167,24 +198,32 @@ class DataBlock(object):
 
 
 class DataBlockFactory(object):
+    ''' Class for creating DataBlock instances'''
+
+    @staticmethod
+    def from_filenames(filenames, verbose=False):
+        ''' Create a list of data blocks from a list of filenames. '''
+        return DataBlockFactory.create_list(filenames, verbose)
 
     @staticmethod
     def create_list(filenames, verbose=False):
+        ''' Create a list of data blocks from a list of filenames. '''
         datablock_list = []
         for f in filenames:
             if verbose: print 'Loading file: %s' % f
             try:
                 datablock_list[-1].append(f)
             except Exception:
-                datablock_list.append(DataBlockFactory.create([f]))
+                datablock_list.append(DataBlockFactory.create_single([f]))
         return datablock_list
 
     @staticmethod
-    def create(filenames, verbose=False):
+    def create_single(filenames, verbose=False):
+        ''' Create a single data blocks from a list of filenames. '''
 
         # Ensure we have a list of images
         if len(filenames) < 1:
-            raise RuntimeError('Need at least 1 image to create data block')
+            raise RuntimeError('Need at least 1 image to create a data block')
 
         # Create the datablock
         return DataBlock(filenames)
@@ -226,7 +265,7 @@ if __name__ == '__main__':
     from dxtbx import model
     from glob import glob
     from os.path import join
-    import cPickle as pickle
+    import pickle
     from time import time
     path1 = '/home/upc86896/Projects/cctbx/sources/dials_regression/centroid_test_data'
     path2 = '/home/upc86896/Data/TRP_M1S3_2_'
@@ -240,12 +279,12 @@ if __name__ == '__main__':
 
 #    st = time()
 
-#    datablock_list = DataBlockFactory.create_list(filenames, verbose=False)
+    datablock_list = DataBlockFactory.from_filenames(filenames, verbose=True)
 
 #    print time() - st
 
-#    print "Dumping"
-#    pickle.dump(datablock_list, open('temp.p', 'wb'))
+    print "Dumping"
+    pickle.dump(datablock_list, open('temp.p', 'wb'))
 #
     print "Loading"
     datablock_list2 = pickle.load(open('temp.p', 'rb'))
@@ -254,17 +293,23 @@ if __name__ == '__main__':
 #    print datablock_list2[0].filenames()
 #    print datablock_list2[0].metadata()
 
-#    imageset = datablock_list2[0].extract_all()
+    imageset = datablock_list2[0].extract_all()
 #    print len(imageset)
 #
 #
 #    for d in datablock_list:
 #        print type(d.extract_stills())
 
-    for d in datablock_list2:
-        d.extract_sweeps()
 
-#    imageset = datablock_list2[0].extract_sweeps()
+
+#    for d in datablock_list2:
+#        d.extract_sweeps()
+
+#    imageset = datablock_list2[0].extract_sweeps()[0]
+#    for i in imageset[0:2]:
+#        pass
+    pickle.dump(imageset, open("imageset.p", "wb"))
+    pickle.load(open("imageset.p", "rb"))
 #    print len(imageset)
 #    for i in imageset:
 #        print len(i)
