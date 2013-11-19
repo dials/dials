380ae6599398f16052e2306af6f58d42adc89c0c
James Parkhurst <james.parkhurst@diamond.ac.uk>
2013-11-19 15:50:02 +0000

    Added spatial indexing to reflection table.

--- scratch/jmp/container/reflection_table.py
+++ scratch/jmp/container/reflection_table.py
@@ -4,8 +4,9 @@ from table import Table
 
 class ReflectionTable(Table):
 
-  def spatial_index_map(self, column):
-    pass
+  def spatial_index(self, column):
+    from dials.algorithms.spatial_indexing import make_spatial_index
+    return make_spatial_index(self._columns[column])
 
 
 if __name__ == '__main__':
@@ -18,6 +19,9 @@ if __name__ == '__main__':
     ),
     ('intensity',
      flex.double([1, 2, 3, 4, 5])
+    ),
+    ('centroid_px',
+     flex.vec3_double([(0, 0, 0), (-1, -1, 0), (-1, 1, 0), (1, 1, 0), (1, -1, 0)])
     )
   ])
 
@@ -26,3 +30,18 @@ if __name__ == '__main__':
   for key, indices in indexer.iteritems():
     print "Miller Index: {0}, Intensities: {1}".format(
       key, [table[i].intensity for i in indices])
+
+  print '\nGet a list of reflections recorded in a volume'
+  indexer = table.spatial_index('centroid_px')
+
+  vol = (-1, 1, -1, 1, -1, 1)
+  print "Volume: ", vol
+  for i in indexer.query_range(vol):
+    print "  Miller Index: {0}, Centroid: {1}".format(
+      table[i].miller_index, table[i].centroid_px)
+
+  vol = (0, 1, -1, 1, -1, 1)
+  print "Volume: ", vol
+  for i in indexer.query_range(vol):
+    print "  Miller Index: {0}, Centroid: {1}".format(
+      table[i].miller_index, table[i].centroid_px)
