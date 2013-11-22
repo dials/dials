306e4bb89e469cc5ce8e9a83b9ab3a81450882b2
Richard Gildea <richard.gildea@diamond.ac.uk>
2013-11-22 11:10:12 +0000

    add dials.index command_line wrapper for convenience

---
+++ command_line/index.py
@@ -0,0 +1,7 @@
+from __future__ import division
+from dials.scratch.rjg import index_3D_FFT_simple
+
+if __name__ == '__main__':
+  import sys
+  args = sys.argv[1:]
+  index_3D_FFT_simple.run(args)
