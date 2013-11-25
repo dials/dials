a3ffa17dc7e1c050fc953a3ece58546c8e158f2a
Graeme Winter <graeme.winter@gmail.com>
2013-11-25 13:07:19 +0000

    making headway on the phil controls, can now steer what background and integration is used (nearly); cctbx.python reflgen.py nrefl=100 counts=1000 background=10 output.all=all_refl.pickle pixel_mask=none shoebox_size.x=10 shoebox_size.y=10 shoebox_size.z=10

--- scratch/gw/reflgen.py
+++ scratch/gw/reflgen.py
@@ -43,90 +43,7 @@ output {
 }
 """)
 
-def simple_gaussian_spots(num_refl, signal, background):
-  from dials.model.data import ReflectionList
-  from dials.algorithms import shoebox
-  import random
-  import math
-
-  rlist = ReflectionList(num_refl)
-
-  from dials.util.command_line import ProgressBar
-  p = ProgressBar(title = 'Generating reflections')
-
-  for j in range(num_refl):
-    p.update(j * 100.0 / num_refl)
-    rlist[j].miller_index = (random.randint(0, 20),
-                             random.randint(0, 20),
-                             random.randint(0, 20))
-    rlist[j].rotation_angle = 2 * math.pi * random.random()
-    rlist[j].beam_vector = (0, 0, 0)
-    rlist[j].image_coord_px = (0, 0)
-    rlist[j].image_coord_mm = (0, 0)
-    rlist[j].frame_number = 0
-    rlist[j].panel_number = 0
-    rlist[j].bounding_box = (0, 10, 0, 10, 0, 10)
-    rlist[j].centroid_position = (0, 0, 0)
-    rlist[j].centroid_variance = (0, 0, 0)
-    rlist[j].centroid_sq_width = (0, 0, 0)
-    rlist[j].intensity = 0
-    rlist[j].intensity_variance = 0
-    rlist[j].corrected_intensity = 0
-    rlist[j].corrected_intensity_variance = 0
-
-  p.finished('Generating %d reflections' % num_refl)
-  shoebox.allocate(rlist)
-
-  p = ProgressBar(title = 'Generating shoeboxes')
-
-  for _, refl in enumerate(rlist):
-
-    p.update(_ * 100.0 / num_refl)
-    sbox = refl.shoebox
-
-    x0 = 5
-    y0 = 5
-    z0 = 5
-
-    # reflection itself, including setting the peak region
-
-    counts_true = 0
-    for j in range(signal):
-      x = int(random.gauss(x0, 1))
-      y = int(random.gauss(y0, 1))
-      z = int(random.gauss(z0, 1))
-      if x < 0 or x >= 10:
-        continue
-      if y < 0 or y >= 10:
-        continue
-      if z < 0 or z >= 10:
-        continue
-      sbox[z, y, x] += 1
-      counts_true += 1
-
-    refl.intensity = counts_true
-
-    # background:flat
-
-    for j in range(background * len(sbox)):
-      x = random.randint(0, 9)
-      y = random.randint(0, 9)
-      z = random.randint(0, 9)
-      sbox[z, y, x] += 1
-
-  # set the mask - it's all good
-
-  from dials.algorithms.shoebox import MaskCode
-  mask_value = MaskCode.Valid|MaskCode.Foreground|MaskCode.Background
-  for refl in rlist:
-    mask = refl.shoebox_mask
-    for j in range(len(mask)):
-      mask[j] = mask_value
-  p.finished('Generating %d shoeboxes' % num_refl)
-
-  return rlist
-
-def simple_gaussian_spots_proper_mask(num_refl, signal, background):
+def simple_gaussian_spots(params):
   from dials.model.data import ReflectionList
   from dials.algorithms import shoebox
   import random
@@ -138,13 +55,13 @@ def simple_gaussian_spots_proper_mask(num_refl, signal, background):
   mask_peak = MaskCode.Valid|MaskCode.Foreground
   mask_back = MaskCode.Valid|MaskCode.Background
 
-  rlist = ReflectionList(num_refl)
+  rlist = ReflectionList(params.nrefl)
 
   from dials.util.command_line import ProgressBar
   p = ProgressBar(title = 'Generating reflections')
 
-  for j in range(num_refl):
-    p.update(j * 100.0 / num_refl)
+  for j in range(params.nrefl):
+    p.update(j * 100.0 / params.nrefl)
     rlist[j].miller_index = (random.randint(0, 20),
                              random.randint(0, 20),
                              random.randint(0, 20))
@@ -154,7 +71,8 @@ def simple_gaussian_spots_proper_mask(num_refl, signal, background):
     rlist[j].image_coord_mm = (0, 0)
     rlist[j].frame_number = 0
     rlist[j].panel_number = 0
-    rlist[j].bounding_box = (0, 10, 0, 10, 0, 10)
+    rlist[j].bounding_box = (0, params.shoebox_size.x, 0, params.shoebox_size.y, 
+                             0, params.shoebox_size.z)
     rlist[j].centroid_position = (0, 0, 0)
     rlist[j].centroid_variance = (0, 0, 0)
     rlist[j].centroid_sq_width = (0, 0, 0)
@@ -163,91 +81,93 @@ def simple_gaussian_spots_proper_mask(num_refl, signal, background):
     rlist[j].corrected_intensity = 0
     rlist[j].corrected_intensity_variance = 0
 
-  p.finished('Generating %d reflections' % num_refl)
+  p.finished('Generating %d reflections' % params.nrefl)
   shoebox.allocate(rlist)
 
   p = ProgressBar(title = 'Generating shoeboxes')
 
   for _, refl in enumerate(rlist):
 
-    p.update(_ * 100.0 / num_refl)
-
-    # flag everything as background
+    p.update(_ * 100.0 / params.nrefl)
     mask = refl.shoebox_mask
-    for j in range(len(mask)):
-      mask[j] = mask_back
+
+    if params.pixel_mask == 'precise':
+      # flag everything as background: peak will me assigned later
+      for j in range(len(mask)):
+        mask[j] = mask_back
+    elif params.pixel_mask == 'none':
+      # flag we have no idea what anything is
+      mask_none = MaskCode.Valid|MaskCode.Foreground|MaskCode.Background
+      for j in range(len(mask)):
+        mask[j] = mask_none
 
     sbox = refl.shoebox
-
-    x0 = 5
-    y0 = 5
-    z0 = 5
 
     # reflection itself, including setting the peak region
 
     counts_true = 0
-    for j in range(signal):
-      x = int(random.gauss(x0, 1))
-      y = int(random.gauss(y0, 1))
-      z = int(random.gauss(z0, 1))
-      if x < 0 or x >= 10:
+    for j in range(params.counts):
+      x = int(random.gauss(params.shoebox_size.x / 2, params.spot_size.x))
+      y = int(random.gauss(params.shoebox_size.y / 2, params.spot_size.y))
+      z = int(random.gauss(params.shoebox_size.z / 2, params.spot_size.z))
+      if x < 0 or x >= params.shoebox_size.x:
         continue
-      if y < 0 or y >= 10:
+      if y < 0 or y >= params.shoebox_size.y:
         continue
-      if z < 0 or z >= 10:
+      if z < 0 or z >= params.shoebox_size.z:
         continue
       sbox[z, y, x] += 1
-      mask[z, y, x] = mask_peak
       counts_true += 1
+      if params.pixel_mask == 'precise':
+        mask[z, y, x] = mask_peak
 
     refl.intensity = counts_true
 
-    # background:flat
+    # background:flat; FIXME can replace this with Poissonian added random 
+    # number, which will give the same answer...
 
-    for j in range(background * len(sbox)):
-      x = random.randint(0, 9)
-      y = random.randint(0, 9)
-      z = random.randint(0, 9)
+    for j in range(params.background * len(sbox)):
+      x = random.randint(0, params.shoebox_size.x - 1)
+      y = random.randint(0, params.shoebox_size.y - 1)
+      z = random.randint(0, params.shoebox_size.z - 1)
       sbox[z, y, x] += 1
 
-  p.finished('Generating %d shoeboxes' % num_refl)
+  p.finished('Generating %d shoeboxes' % params.nrefl)
 
   return rlist
 
-def integrate_xds_background_3d_summation(rlist):
+def background_xds(rlist):
   from dials.algorithms.background import XdsSubtractor
-  from dials.algorithms.integration import Summation3d
-
   background = XdsSubtractor()
   background(None, None, rlist)
+  return
+  
+def integrate_3d_summation(rlist):
+  from dials.algorithms.integration import Summation3d
   integration = Summation3d()
   integration(None, None, rlist)
   return
 
-def integrate_inclined_background_3d_summation(rlist):
+def background_inclined(rlist):
   from dials.algorithms.background import InclinedSubtractor
-  from dials.algorithms.integration import Summation3d
-
   background = InclinedSubtractor()
   background(None, None, rlist)
-  integration = Summation3d()
-  integration(None, None, rlist)
   return
 
 def main(params):
-  if params.pixel_mask == 'none':
-    rlist = simple_gaussian_spots(params.nrefl, params.counts, params.background)
-  elif params.pixel_mask == 'precise':
-    rlist = simple_gaussian_spots_proper_mask(params.nrefl, params.counts, 
-                                              params.background)
+  rlist = simple_gaussian_spots(params)
   correct_intensities = [r.intensity for r in rlist]
   for r in rlist:
     r.intensity = 0
 
   if params.background_method == 'xds':
-    integrate_xds_background_3d_summation(rlist)
+    background_xds(rlist)
   elif params.background_method == 'mosflm':
-    integrate_inclined_background_3d_summation(rlist)
+    assert(params.pixel_mask != 'none')
+    background_inclined(rlist)
+    
+  integrate_3d_summation(rlist)
+  
   integrated_intensities = [r.intensity for r in rlist]
 
   # now scan through the reflection list and find those where the integration
