ba0e1fe01925a335fb48181f72bdf7a1afb3f232
Graeme Winter <graeme.winter@gmail.com>
2013-11-26 15:15:41 +0000

    Reinstated main...

---
+++ command_line/generate_test_reflections.py
@@ -0,0 +1,14 @@
+
+def main(args):
+  from dials.algorithms.simulation.generate_test_reflections import main \
+    as _main
+  from dials.algorithms.simulation.generate_test_reflections import \
+    master_phil
+  from libtbx.phil import command_line
+  cmd = command_line.argument_interpreter(master_params = master_phil)
+  working_phil = cmd.process_and_fetch(args = sys.argv[1:])
+  _main(working_phil.extract())
+
+if __name__ == '__main__':
+  import sys
+  main(sys.argv[1:])
