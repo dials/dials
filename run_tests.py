12d82326e1c0ca061902561d103d87b00dc9c0ba
Graeme Winter <graeme.winter@gmail.com>
2013-11-26 15:22:17 +0000

    Generate test reflections test

--- run_tests.py
+++ run_tests.py
@@ -62,6 +62,7 @@ tst_list = (
     "$D/test/algorithms/shoebox/tst_partial_extractor.py",
     "$D/test/algorithms/shoebox/tst_helpers.py",
     "$D/test/algorithms/peak_finding/tst_spotfinder.py",
+    "$D/test/algorithms/generate_test_reflections/tst_generate_test_reflections.py",
     ["$D/scratch/rjg/tst_index.py", "1"],
     ["$D/scratch/rjg/tst_index.py", "2"],
     ["$D/scratch/rjg/tst_index.py", "3"],
