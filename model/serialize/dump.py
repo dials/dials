b75c7bf6fd9374141ee61f466ce6dd57efc17a9a
Richard Gildea <richard.gildea@diamond.ac.uk>
2013-11-29 12:59:23 +0000

    port dials crystal model to cctbx. NB. currently old imports of dials crystal model still work as before - TODO: deprecate these imports and move to direct cctbx imports

--- model/serialize/dump.py
+++ model/serialize/dump.py
@@ -15,52 +15,11 @@ from __future__ import division
 from dxtbx.serialize.dump import imageset as sweep
 from dxtbx.serialize.dump import imageset_to_string as sweep_to_string
 
-def crystal_to_string(obj, compact=False):
-  ''' Dump the given object to string.
 
-  Params:
-      obj The crystal model
-      compact Write in compact representation
+# FIXME These are only temporary - they should be removed ASAP
+from cctbx.crystal.crystal_model.serialize import crystal_to_string
+from cctbx.crystal.crystal_model.serialize import dump_crystal as crystal
 
-  Returns:
-      The JSON string
-
-  '''
-  import json
-  import textwrap
-  from dials.model.serialize.crystal import crystal_to_dict
-  from dxtbx.serialize.dump import compact_simple_lists
-
-  # Return as a JSON string
-  if compact == False:
-    string = json.dumps(crystal_to_dict(obj), indent=2)
-
-    # Hack to make more readable
-    string = compact_simple_lists(string)
-
-  else:
-    string = json.dumps(crystal_to_dict(obj), separators=(',',':'))
-
-  # Return the string
-  return string
-
-def crystal(obj, outfile, compact=False):
-  ''' Dump the given object to file.
-
-  Params:
-      obj The crystal to dump
-      outfile The output file name or file object
-      compact Write in compact representation
-
-  '''
-  # If the input is a string then open and write to that file
-  if isinstance(outfile, str):
-    with open(outfile, 'w') as outfile:
-      outfile.write(crystal_to_string(obj, compact))
-
-  # Otherwise assume the input is a file and write to it
-  else:
-    outfile.write(crystal_to_string(obj, compact))
 
 def reflections(obj, outfile):
   ''' Dump the given object to file
--- model/serialize/load.py
+++ model/serialize/load.py
@@ -14,38 +14,9 @@ from __future__ import division
 # Import to give access from here
 from dxtbx.serialize.load import imageset as sweep
 
-def crystal_from_string(string):
-  ''' Load the string and return the models.
-
-  Params:
-      string The JSON string to load
-
-  Returns:
-      The models
-
-  '''
-  import json
-  from dials.model.serialize.crystal import crystal_from_dict
-  return crystal_from_dict(json.loads(string))
-
-def crystal(infile):
-  ''' Load the given JSON file.
-
-  Params:
-      infile The input filename or file object
-
-  Returns:
-      The models
-
-  '''
-  # If the input is a string then open and read from that file
-  if isinstance(infile, str):
-    with open(infile, 'r') as infile:
-      return crystal_from_string(infile.read())
-
-  # Otherwise assume the input is a file and read from it
-  else:
-    return crystal_from_string(infile.read())
+# FIXME These are only temporary - they should be removed ASAP
+from cctbx.crystal.crystal_model.serialize import crystal_from_string
+from cctbx.crystal.crystal_model.serialize import load_crystal as crystal
 
 def reflections(infile):
   ''' Load the given reflection file.
