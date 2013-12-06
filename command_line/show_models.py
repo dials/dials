f2b6df0b0d8f4e2481f5d36e3ed738e11917e6b8
Richard Gildea <richard.gildea@diamond.ac.uk>
2013-12-06 09:59:11 +0000

    also print detector, beam, scan, goniometer objects

--- command_line/show_models.py
+++ command_line/show_models.py
@@ -7,6 +7,12 @@ def run(args):
     print crystal_model
   for imageset in importer.imagesets:
     print imageset
+    print imageset.get_detector()
+    print imageset.get_beam()
+    if imageset.get_scan() is not None:
+      print imageset.get_scan()
+    if imageset.get_goniometer() is not None:
+      print imageset.get_goniometer()
   return
 
 if __name__ == '__main__':
