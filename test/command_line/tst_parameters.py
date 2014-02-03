e8dfa869884fb0df025e7514354c980789056922
James Parkhurst <james.parkhurst@diamond.ac.uk>
2014-02-03 13:22:22 +0000

    Added a test for dials.parameters

---
+++ test/command_line/tst_parameters.py
@@ -0,0 +1,18 @@
+from __future__ import division
+
+def run_test():
+  from subprocess import call
+
+  # Call dials.parameters
+  call(["dials.parameters > parameters.txt"], shell=True)
+
+  # Read the parameters
+  with open("parameters.txt", "r") as infile:
+    output = infile.read()
+
+  # Check we have some output
+  assert(len(output) != 0)
+  print 'OK'
+
+if __name__ == '__main__':
+  run_test()
