33d838bfe6095c971118334da1347e1738890174
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2014-02-04 17:04:56 +0000

    adding tst_compare as a starting point for now part II

--- test/command_line/tst_compare.py
+++ test/command_line/tst_compare.py
@@ -13,22 +13,31 @@ class Test(object):
       print 'FAIL: dials_regression not configured'
       exit(0)
 
-    self.path = join(dials_regression, "centroid_test_data")
+    self.path = join(dials_regression, "integration_test_data/i04-weak-data")
 
   def run(self):
     from os.path import abspath, join
     from libtbx import easy_run
 
-    # Call dials.integrate
+
+    print "path =", self.path
+
+
+    # Call dials.compare_mosflm_dials
     easy_run.fully_buffered([
-      'dials.integrate',
-      join(self.path, 'experiments.json'),
-      'integration.algorithm=sum3d',
+      'dials.compare_mosflm_dials',
+      join(self.path, 'integrate.mtz'),
+      join(self.path, 'integrated.pickle'),
+      join(self.path, 'crystal.json'),
+      join(self.path, 'sweep.json'),
     ]).raise_if_errors()
 
-    import cPickle as pickle
-    table = pickle.load(open('integrated.pickle', 'rb'))
-    assert(len(table) == 360)
+
+    #import cPickle as pickle
+    #table = pickle.load(open('integrated.pickle', 'rb'))
+
+    # remember to uncomment the next line
+    #assert(len(table) == 361)
     print 'OK'
 
 if __name__ == '__main__':
