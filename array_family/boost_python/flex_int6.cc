6202ad7ff5ab00b3fa10316e304c3f7ed9979662
James Parkhurst <james.parkhurst@diamond.ac.uk>
2014-02-04 12:42:35 +0000

    Fixed bug in pickling

--- array_family/boost_python/flex_int6.cc
+++ array_family/boost_python/flex_int6.cc
@@ -18,7 +18,7 @@ namespace scitbx { namespace serialization { namespace single_buffered {
   {
     return
       to_string(to_string(to_string(to_string(to_string(to_string(
-        start, value[0]), value[1]), value[2]), value[0]), value[1]), value[2]);
+        start, value[0]), value[1]), value[2]), value[3]), value[4]), value[5]);
   }
 
   template <>
