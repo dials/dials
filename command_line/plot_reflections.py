aed3a3e452b931a03a96d82389b1ff02eefbae02
Richard Gildea <richard.gildea@diamond.ac.uk>
2014-02-11 13:13:44 +0000

    fix dials.plot_reflections

--- command_line/plot_reflections.py
+++ command_line/plot_reflections.py
@@ -27,10 +27,12 @@ output {
 def run(args):
   from dials.util.command_line import Importer
   from scitbx.array_family import flex
+  from scitbx import matrix
   from libtbx.phil import command_line
-  importer = Importer(args)
-  assert len(importer.imagesets) == 1
-  imageset = importer.imagesets[0]
+  importer = Importer(args, check_format=False)
+  assert len(importer.reflections) > 0
+  assert len(importer.datablocks) == 1
+  imageset = importer.datablocks[0].extract_imagesets()[0]
   detector = imageset.get_detector()
   scan = imageset.get_scan()
   args = importer.unhandled_arguments
@@ -39,32 +41,46 @@ def run(args):
   params = working_phil.extract()
   observed_xy = flex.vec2_double()
   predicted_xy = flex.vec2_double()
-  for reflection_list in importer.reflections:
-    if len(params.scan_range):
-      sel = flex.bool(reflection_list.size(), False)
-      centroid_positions = reflection_list.centroid_position()
-      if centroid_positions.norms().all_eq(0):
-        centroids_frame = reflection_list.frame_number()
-      else:
-        centroids_frame = centroid_positions.parts()[2]
-      reflections_in_range = False
-      for scan_range in params.scan_range:
-        if scan_range is None: continue
-        range_start, range_end = scan_range
-        sel |= ((centroids_frame >= range_start) & (centroids_frame < range_end))
-      reflection_list = reflection_list.select(sel)
-    if params.first_n_reflections is not None:
-      centroid_positions = reflection_list.centroid_position()
+  panel_origin_shifts = {0: (0,0,0)}
+  try:
+    hierarchy = detector.hierarchy()
+  except AttributeError, e:
+    hierarchy = None
+  for i_panel in range(1, len(detector)):
+    origin_shift = matrix.col(detector[0].get_origin()) \
+      - matrix.col(detector[i_panel].get_origin())
+    panel_origin_shifts[i_panel] = origin_shift
+  from dials.model.data import ReflectionList
+  reflection_list = ReflectionList.from_table(importer.reflections)
+  if len(params.scan_range):
+    sel = flex.bool(reflection_list.size(), False)
+    centroid_positions = reflection_list.centroid_position()
+    if (centroid_positions.norms().all_eq(0) or
+        not reflection_list.miller_index().all_eq((0,0,0))):
+      centroids_frame = reflection_list.frame_number()
+    else:
       centroids_frame = centroid_positions.parts()[2]
-      perm = flex.sort_permutation(centroids_frame)
-      perm = perm[:min(reflection_list.size(), params.first_n_reflections)]
-      print flex.max(centroids_frame.select(perm))
-      reflection_list = reflection_list.select(perm)
+    reflections_in_range = False
+    for scan_range in params.scan_range:
+      if scan_range is None: continue
+      range_start, range_end = scan_range
+      sel |= ((centroids_frame >= range_start) & (centroids_frame < range_end))
+    reflection_list = reflection_list.select(sel)
+  if params.first_n_reflections is not None:
+    centroid_positions = reflection_list.centroid_position()
+    centroids_frame = centroid_positions.parts()[2]
+    perm = flex.sort_permutation(centroids_frame)
+    perm = perm[:min(reflection_list.size(), params.first_n_reflections)]
+    print flex.max(centroids_frame.select(perm))
+    reflection_list = reflection_list.select(perm)
 
-    for refl in reflection_list:
-      centroid_position = refl.centroid_position
-      centroid_variance = refl.centroid_variance
-      if centroid_position != (0,0,0):
+  for refl in reflection_list:
+    centroid_position = refl.centroid_position
+    centroid_variance = refl.centroid_variance
+    if centroid_position != (0,0,0):
+      if refl.miller_index is not None and refl.image_coord_mm != (0,0):
+        x, y = refl.image_coord_mm
+      else:
         # just a quick check for now that the reflections haven't come from
         # somewhere else
         assert refl.image_coord_mm == (0,0)
@@ -75,11 +91,17 @@ def run(args):
           centroid_position,
           centroid_variance,
           (1,1,1))
+        if refl.panel_number > 0:
+          s1 = detector[refl.panel_number].get_lab_coord(centroid_position[:2])
+          if hierarchy is not None:
+            centroid_position = hierarchy.get_ray_intersection(s1)
+          else:
+            centroid_position = detector[0].get_ray_intersection(s1)
         x, y = centroid_position[:2]
-        observed_xy.append((x,y))
-      if refl.image_coord_px != (0, 0):
-        x, y = refl.image_coord_mm
-        predicted_xy.append((x,y))
+      observed_xy.append((x,y))
+    elif refl.image_coord_px != (0, 0):
+      x, y = refl.image_coord_mm
+      predicted_xy.append((x,y))
   obs_x, obs_y = observed_xy.parts()
   pred_x, pred_y = predicted_xy.parts()
 
@@ -99,12 +121,24 @@ def run(args):
   pyplot.axes().set_aspect('equal')
   pyplot.scatter(obs_x, obs_y, marker='o', c='white', s=10, alpha=1)
   pyplot.scatter(pred_x, pred_y, marker='+', c='blue')
-  assert len(detector) == 1
+  #assert len(detector) == 1
   panel = detector[0]
-  beam_centre = panel.get_beam_centre(imageset.get_beam().get_s0())
+  #if len(detector) > 1:
+  xmin = max([detector[i_panel].get_image_size_mm()[0] + panel_origin_shifts[i_panel][0]
+              for i_panel in range(len(detector))])
+  xmax = max([detector[i_panel].get_image_size_mm()[0] + panel_origin_shifts[i_panel][0]
+              for i_panel in range(len(detector))])
+  ymax = max([detector[i_panel].get_image_size_mm()[1] + panel_origin_shifts[i_panel][1]
+              for i_panel in range(len(detector))])
+  ymax = max([detector[i_panel].get_image_size_mm()[1] + panel_origin_shifts[i_panel][1]
+              for i_panel in range(len(detector))])
+  if hierarchy is not None:
+    beam_centre = hierarchy.get_beam_centre(imageset.get_beam().get_s0())
+  else:
+    beam_centre = detector[0].get_beam_centre(imageset.get_beam().get_s0())
   pyplot.scatter([beam_centre[0]], [beam_centre[1]], marker='+', c='blue', s=100)
-  pyplot.xlim(0, panel.get_image_size_mm()[0])
-  pyplot.ylim(0, panel.get_image_size_mm()[1])
+  pyplot.xlim(0, xmax)
+  pyplot.ylim(0, ymax)
   pyplot.gca().invert_yaxis()
   pyplot.title('Centroid x,y-coordinates')
   pyplot.xlabel('x-coordinate (mm)')
---
+++ test/tst_plot_reflections.py
@@ -0,0 +1,29 @@
+from __future__ import division
+import os
+import libtbx.load_env
+from libtbx import easy_run
+
+def exercise():
+  if not libtbx.env.has_module("dials_regression"):
+    print "Skipping test: dials_regression not available"
+    return
+  dials_regression = libtbx.env.find_in_repositories(
+    relative_path="dials_regression",
+    test=os.path.isdir)
+  data_dir = os.path.join(dials_regression, "indexing_test_data", "i04_weak_data")
+
+  cmd = " ".join(["dials.plot_reflections",
+                  os.path.join(data_dir, "datablock_orig.json"),
+                  os.path.join(data_dir, "full.pickle"),
+                  "scan_range=0,5",
+                  ])
+  print cmd
+  easy_run.fully_buffered(cmd).raise_if_errors()
+  assert os.path.exists("centroids.png")
+
+def run():
+  exercise()
+  print "OK"
+
+if __name__ == '__main__':
+  run()
