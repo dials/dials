d4a4a6b874c2bbf35bdecdccb8c04f277afd50bb
David Waterman <david.waterman@stfc.ac.uk>
2014-02-13 12:22:01 +0000

    Do not write out parameters and centroids tables based on verbosity level, but provide each with their own command line options

--- command_line/refine.py
+++ command_line/refine.py
@@ -29,12 +29,25 @@ class Script(ScriptRunner):
     # Initialise the base class
     ScriptRunner.__init__(self, usage=usage)
 
-    # Output filename option
+    # Output experiments filename option
     self.config().add_option(
         '--output-experiments-filename',
         dest = 'output_experiments_filename',
         type = 'string', default = 'refined_experiments.json',
         help = 'Set the filename for refined experimental models.')
+
+    self.config().add_option(
+        '--output-centroids-filename',
+        dest = 'output_centroids_filename',
+        type = 'string',
+        help = 'Set the filename for the table of centroids at the end of refinement.')
+
+    self.config().add_option(
+        '--output-parameters-filename',
+        dest = 'output_parameters_filename',
+        type = 'string',
+        help = 'Set the filename for the table of scan varying parameter values'
+               ' at the end of refinement.')
 
     # Add a verbosity option
     self.config().add_option(
@@ -42,11 +55,11 @@ class Script(ScriptRunner):
         action="count", default=0,
         help="set verbosity level; -vv gives verbosity level 2.")
 
-  def write_residuals_table(self, refiner):
+  def write_residuals_table(self, refiner, filename):
 
     matches = refiner.get_matches()
 
-    f = open("residuals.dat","w")
+    f = open(filename,"w")
     header = ("H\tK\tL\tFrame_obs\tX_obs\tY_obs\tPhi_obs\tX_calc\t"
         "Y_calc\tPhi_calc\n")
     f.write(header)
@@ -65,10 +78,10 @@ class Script(ScriptRunner):
     from dials.algorithms.refinement import RefinerFactory
     import cPickle as pickle
 
-    # Check the number of arguments is correct
-    #if len(args) != 2:
-    #  self.config().print_help()
-    #  return
+    # Check the number of arguments
+    if len(args) < 2:
+      self.config().print_help()
+      return
 
     importer = Importer(args, check_format=False, verbose=True)
 
@@ -94,20 +107,25 @@ class Script(ScriptRunner):
     # Refine and get the refinement history
     refined = refiner.run()
 
-    if options.verbosity > 1:
-      print "Writing residuals to file"
-      self.write_residuals_table(refiner)
+    if options.output_centroids_filename:
+      print "Writing table of centroids to '{0}'".format(
+        options.output_centroids_filename)
+      self.write_residuals_table(refiner, options.output_centroids_filename)
 
-      # Write scan-varying parameters to file, if there were any
+    # Write scan-varying parameters to file, if there were any
+    if options.output_parameters_filename:
       scan = refiner.get_scan()
       if scan:
         text = refiner.get_param_reporter().varying_params_vs_image_number(
             scan.get_array_range())
         if text:
-          print "Writing scan-varying parameter table to file"
-          f = open("varying_params.dat","w")
+          print "Writing scan-varying parameter table to '{0}'".format(
+            options.output_parameters_filename)
+          f = open(options.output_parameters_filename,"w")
           f.write(text)
           f.close()
+        else:
+          print "No scan-varying parameter table to write"
 
     # get the refined experiments
     experiments = refiner.get_experiments()
