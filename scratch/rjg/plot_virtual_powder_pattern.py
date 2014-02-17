9c7d1880d9dde0a152e4517eb4ace584e5cebc8c
Richard Gildea <richard.gildea@diamond.ac.uk>
2014-02-17 11:25:31 +0000

    save png

--- scratch/rjg/plot_virtual_powder_pattern.py
+++ scratch/rjg/plot_virtual_powder_pattern.py
@@ -8,8 +8,6 @@ def run(args):
   #datablocks = importer.datablocks
   #assert len(datablocks) == 1
   ##imagesets = datablocks[0].extract_imagesets()
-  fig = pyplot.figure()
-  pyplot.axes().set_aspect('equal')
   observed_xy = flex.vec2_double()
   for reflection_list in importer.reflections:
     for refl in reflection_list:
@@ -18,11 +16,14 @@ def run(args):
         x, y = centroid[:2]
         observed_xy.append((x,y))
   obs_x, obs_y = observed_xy.parts()
-  pyplot.scatter(obs_x, obs_y, marker='.', c='red', s=0.5)
+  fig = pyplot.figure()
+  pyplot.scatter(obs_x, obs_y, marker='.', c='black', lw=0, s=0.1)
   pyplot.axes().set_aspect('equal')
   pyplot.xlim((0, pyplot.xlim()[1]))
   pyplot.ylim((0, pyplot.ylim()[1]))
-  pyplot.show()
+  pyplot.savefig("virtual_powder.png",
+                 size_inches=(10,10), dpi=600)
+  #pyplot.show()
 
 
 if __name__ == '__main__':
