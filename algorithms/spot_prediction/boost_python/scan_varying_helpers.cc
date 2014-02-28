58047b9586a209897ac97a6ce5a97ef68634a597
David Waterman <david.waterman@stfc.ac.uk>
2014-02-28 16:48:47 +0000

    python bindings for solve_quad

---
+++ algorithms/spot_prediction/boost_python/scan_varying_helpers.cc
@@ -0,0 +1,25 @@
+/*
+ * scan_varying_helpers.cc
+ *
+ *  Copyright (C) 2014 STFC Rutherford Appleton Laboratory, UK.
+ *
+ *  Author: David Waterman
+ *
+ *  This code is distributed under the BSD license, a copy of which is
+ *  included in the root directory of this package.
+ */
+#include <boost/python.hpp>
+#include <boost/python/def.hpp>
+#include <dials/algorithms/spot_prediction/scan_varying_helpers.h>
+
+namespace dials { namespace algorithms { namespace boost_python {
+
+  using namespace boost::python;
+
+  void export_solve_quad()
+  {
+    def("solve_quad", reeke_detail::solve_quad,(
+      arg("a"), arg("b"), arg("c")));
+  }
+
+}}} // namespace = dials::algorithms::boost_python
--- algorithms/spot_prediction/boost_python/spot_prediction_ext.cc
+++ algorithms/spot_prediction/boost_python/spot_prediction_ext.cc
@@ -10,13 +10,17 @@
  */
 #include <boost/python.hpp>
 #include <boost/python/def.hpp>
+#include <scitbx/array_family/small.h>
+#include <scitbx/boost_python/container_conversions.h>
 
 namespace dials { namespace algorithms { namespace boost_python {
 
   using namespace boost::python;
+  using namespace scitbx::boost_python::container_conversions;
 
   void export_index_generator();
   void export_reeke_index_generator();
+  void export_solve_quad();
   void export_rotation_angles();
   void export_ray_predictor();
   void export_scan_varying_ray_predictor();
@@ -27,8 +31,11 @@ namespace dials { namespace algorithms { namespace boost_python {
 
   BOOST_PYTHON_MODULE(dials_algorithms_spot_prediction_ext)
   {
+    tuple_mapping_fixed_capacity<scitbx::af::small<double, 2> >();
+
     export_index_generator();
     export_reeke_index_generator();
+    export_solve_quad();
     export_rotation_angles();
     export_ray_predictor();
     export_scan_varying_ray_predictor();
