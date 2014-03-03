7343bb42f91132486cc295b5e8b58a765c4365ee
James Parkhurst <james.parkhurst@diamond.ac.uk>
2014-03-03 14:45:39 +0000

    Added phil parameters to threshold extension

--- extensions/kabsch_spotfinder_threshold_ext.py
+++ extensions/kabsch_spotfinder_threshold_ext.py
@@ -18,6 +18,34 @@ class KabschSpotFinderThresholdExt(SpotFinderThresholdIface):
 
   name = 'xds'
 
+  phil = '''
+    kernel_size = 3 3
+      .help = "The size of the local area around the spot in which"
+              "to calculate the mean and variance. The kernel is"
+              "given as a box of size (2 * nx + 1, 2 * ny + 1) centred"
+              "at the pixel."
+      .type = ints(size=2)
+
+    sigma_background = 6
+      .help = "The number of standard deviations of the coefficient of"
+              "variation (variance / mean) in the local area below"
+              "which the pixel will be classified as background."
+      .type = float
+
+    sigma_strong = 3
+      .help = "The number of standard deviations above the mean in the"
+              "local area above which the pixel will be classified as"
+              "strong."
+      .type = float
+
+    min_local=2
+      .help = "The number of pixels in the local area of each pixel needed"
+              "to do the thresholding. Setting to 0 or less means that all"
+              "the pixels under the kernel are needed. The minimum allowable"
+              "number is 2"
+      .type = int
+  '''
+
   def __init__(self, params):
     ''' Initialise the algorithm. '''
     from dials.algorithms.peak_finding.threshold import XDSThresholdStrategy
