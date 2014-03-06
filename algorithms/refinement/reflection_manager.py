69bacac1e41a969fb1a453aeb3087662e776bbf2
David Waterman <david.waterman@stfc.ac.uk>
2014-03-06 23:27:50 +0000

    check in WIP new reflection manager

--- algorithms/refinement/reflection_manager.py
+++ algorithms/refinement/reflection_manager.py
@@ -10,6 +10,10 @@
 """Contains classes used to manage the reflections used during refinement,
 principally ReflectionManager."""
 
+# constants
+TWO_PI = 2.0 * pi
+RAD_TO_DEG = 180. / pi
+
 class ObsPredMatch:
   """
   A bucket class containing data for a prediction that has been
@@ -101,8 +105,8 @@ class ReflectionManager(object):
                      experiments,
                      nref_per_degree=None,
                      min_num_obs=20,
-                     max_num_obs=None,
-                     minimum_sample_size=0,
+                     max_sample_size=None,
+                     min_sample_size=0,
                      close_to_spindle_cutoff=0.1,
                      iqr_multiplier=1.5,
                      verbosity=0):
@@ -114,16 +118,7 @@ class ReflectionManager(object):
     # set verbosity
     self._verbosity = verbosity
 
-    ####FIXME currently extracting quantities from the FIRST Experiment only
-    # DEPRECATED. KEEP THE EXPERIMENTS ENTIRE
-    #if experiments[0].scan:
-    #  sweep_range_rad = experiments[0].scan.get_oscillation_range(deg=False)
-    #else: sweep_range_rad = None
-    #gonio = experiments[0].goniometer
-    #beam = experiments[0].beam
-    ####
     self._experiments = experiments
-    self._goniometers = [e.goniometer for e in self._experiments]
     self._axes = [matrix.col(g.get_rotation_axis()) if g else None for g in goniometers]
     self._s0vecs = [matrix.col(e.beam.get_s0()) for e in self.experiments]
     # keep references to the beam, goniometer and sweep range (for
@@ -133,20 +128,50 @@ class ReflectionManager(object):
     #self._gonio = gonio
     #self._sweep_range_rad = sweep_range_rad
 
-    # find vector normal to the spindle-beam plane for the initial model
-    #vecn = self._spindle_beam_plane_normal()
-
     # set up the reflection inclusion cutoffs
-    self._close_to_spindle_cutoff = close_to_spindle_cutoff
-    self._iqr_multiplier = iqr_multiplier
+    self._close_to_spindle_cutoff = close_to_spindle_cutoff #too close to spindle
+    self._iqr_multiplier = iqr_multiplier #outlier rejection
+    self._nref_per_degree = nref_per_degree #random subsets
+    self._max_sample_size = max_sample_size #sample size ceiling
+    self._min_sample_size = min_sample_size #sample size floor
 
-    # exclude reflections that fail inclusion criteria
+    # exclude reflections that fail some inclusion criteria (currently just
+    # close to spindle)
     self._input_size = len(reflections)
     refs_to_keep = self._id_refs_to_keep(reflections)
     self._accepted_refs_size = len(refs_to_keep)
 
     # select only the accepted reflections to manage
     self._reflections = reflections.select(flex.size_t(refs_to_keep))
+
+    # keep minimum number of observations to allow as working set
+    self._min_num_obs = min_num_obs
+
+    # fail if there are too few reflections in the manager (the check also needs
+    # to be repeated after outlier rejection and subsetting)
+    self._check_too_few()
+
+    # set entering flags for all reflections
+    self._calculate_entering_flags()
+
+    # set observed frame numbers for all reflections
+    self._calculate_frame_numbers()
+
+    # set weights for all reflections
+    self._calculate_weights()
+
+    # create a 'use' flag
+    self._reflections['is_matched'] = flex.bool(len(self._reflections))
+    self.reset_accepted_reflections()
+
+    # not known until the manager is finalised
+    self._sample_size = None
+
+    return
+
+  def finalise(self):
+    """Complete initialisation by performing outlier rejection and any
+    requested subsetting. This function to be called by a Target object"""
 
     # choose a random subset of data for refinement
     # DO THIS LATER, AFTER PREDICTION AND OUTLIER REJECTION
@@ -156,49 +181,46 @@ class ReflectionManager(object):
     #                                        minimum_sample_size,
     #                                        max_num_obs)
 
-    # keep minimum number of observations to allow as working set
-    self._min_num_obs = min_num_obs
+    if self._verbosity > 1: print "Finalising the Reflection Manager"
 
-    # store observation information in a list of observation-prediction
-    # pairs (prediction information will go in here later)
-    #self._obs_pred_pairs = []
+    # NB This function does the job previously handled by the first_update clause in
+    # Target.predict()
 
-    # calculate entering flags for all reflections
-    self._calculate_entering_flags()
+    # print summary before outlier rejection
+    self._reflection_manager.print_stats_on_matches()
 
-    for i in refs_to_keep:
+    # flag potential outliers
+    rejection_occurred = self._reject_outliers()
 
-      ref = reflections[i]
-      exp_id = ref['id']
-      h = ref['miller_index']
-      s = matrix.col(ref['s1'])
-      entering = s.dot(vecn) < 0.
-      panel = ref['panel']
-      x = ref["xyzobs.mm.value"][0]
-      y = ref["xyzobs.mm.value"][1]
-      phi = ref["xyzobs.mm.value"][2]
-      if experiments[exp_id].scan:
-        frame = experiments[exp_id].scan.get_array_index_from_angle(phi, deg=False)
-      else:
-        frame = 0
-      sig_x, sig_y, sig_phi = [sqrt(e) for e in ref["xyzobs.mm.variance"]]
-      w_x = w_y = w_phi = 0
-      if ref["xyzobs.mm.variance"][0] != 0: w_x   = 1./ref["xyzobs.mm.variance"][0]
-      if ref["xyzobs.mm.variance"][1] != 0: w_y   = 1./ref["xyzobs.mm.variance"][1]
-      if ref["xyzobs.mm.variance"][2] != 0: w_phi = 1./ref["xyzobs.mm.variance"][2]
+    # delete all obs-pred pairs from the manager that do not
+    # have a prediction or were flagged as outliers
+    self._reflection_manager.strip_unmatched_observations()
 
-      self._obs_pred_pairs.append(ObsPredMatch(i, exp_id, h,
-                                               entering, frame, panel,
-                                               x, sig_x, w_x,
-                                               y, sig_y, w_y,
-                                               phi, sig_phi, w_phi))
+    # print summary after outlier rejection
+    if rejection_occurred: self._reflection_manager.print_stats_on_matches()
 
-    # fail if there are too few reflections in the manager
-    if len(self._obs_pred_pairs) < self._min_num_obs:
-      msg = ('Remaining number of reflections = {0}, which is below '+ \
-          'the configured limit for creating this reflection ' + \
-          'manager').format(len(self._obs_pred_pairs))
-      raise RuntimeError(msg)
+    # form working subset
+    refs_to_keep = self._create_working_set()
+    self._sample_size = len(self._reflections)
+
+    self._check_too_few()
+
+    if self._verbosity > 1:
+      print ("Working set size = %d observations"
+             % self.get_sample_size())
+
+    return
+
+  def _check_too_few():
+    # fail if any of the experiments has too few reflections
+    for iexp in range(len(self._experiments)):
+      nref = (self._reflections['id'] == iexp).count(True)
+      if nref < self._min_num_obs:
+        msg = ('Remaining number of reflections = {0}, for experiment {1}, ' + \
+               'which is below the configured limit for this reflection ' + \
+               'manager').format((nref, iexp))
+        raise RuntimeError(msg)
+    return
 
   def _calculate_entering_flags(self):
     """calculate entering flags for all reflections, and set them as a column
@@ -212,7 +234,48 @@ class ReflectionManager(object):
     vecs = [self._s0vecs[i].cross(e).normalize() \
             if e else None for i, e in enumerate(self._axes)]
 
-    enterings = [ref[''].dot(vecn) < 0.]
+    # Set entering flags. These are always False for experiments that have no
+    # rotation axis.
+    enterings = [ref['s1'].dot(vecn[ref['id']]) < 0. if vecn[ref['id']] \
+                 else False for ref in self._reflections]
+
+    self._reflections['entering'] = flex.bool(enterings)
+
+    return
+
+  def _calculate_frame_numbers(self):
+    """calculate observed frame numbers for all reflections, if not already
+    set"""
+
+    scans = [e.scan for e in self._experiments]
+    frames = [scans[ref['id']].get_array_index_from_angle(
+                ref["xyzobs.mm.value"][2], deg=False) \
+              if scans[ref['id']] else 0.0 for ref in self._reflections]
+    #TEMP Test: if frames are already set, see that we have the same values
+    # FIXME Remove the test if is is seen to pass in practice, and just return
+    # early
+    if self._reflections.has_key('xyzobs.px.value'):
+      from libtbx.test_utils import approx_equal
+      for a, b in zip(frames, self._reflections['xyzobs.px.value']):
+        assert a = b[2]
+    else: # Frames are not set, so set them, with dummy observed pixel values
+      xyzobs = [(0., 0., f) for f in frames]
+      self._reflections['xyzobs.px.value'] = flex.vec3_double(xyzobs)
+
+    return
+
+  def _calculate_weights(self):
+    """set 'statistical weights', that is w(x) = 1/var(x)"""
+    # TODO Plug-in to allow different weighting schemes
+
+    weights = ref['xyzobs.mm.variance'].deepcopy()
+    parts = weights.parts()
+    for w in parts:
+      sel = w >= 0.
+      w.set_selected(sel, 1./w)
+    ref['xyzobs.mm.weights'] = flex.vec3_double(*parts)
+
+    return
 
   def _id_refs_to_keep(self, obs_data):
     """Create a selection of observations that pass certain conditions.
@@ -220,7 +283,7 @@ class ReflectionManager(object):
     This step includes rejection of reflections too close to the spindle, and
     rejection of the (0,0,0) Miller index. Outlier rejection is done later."""
 
-
+    #TODO Should be possible to 'vectorise' this using flex array operations
 
     inc = [i for i, ref in enumerate(obs_data) if ref['miller_index'] != (0,0,0) \
            and self._inclusion_test(matrix.col(ref['s1']),
@@ -243,33 +306,43 @@ class ReflectionManager(object):
 
     return test
 
-  def _create_working_set(self, indices, nref_per_degree,
-                          minimum_sample_size, max_num_obs):
+  def _create_working_set(self):
     """Make a subset of the indices of reflections to use in refinement"""
 
-    working_indices = indices
-    sample_size = len(working_indices)
+    #working_indices = indices
 
-    # set sample size according to nref_per_degree
-    if nref_per_degree:
-      width = abs(self._sweep_range_rad[1] -
-                  self._sweep_range_rad[0]) * RAD_TO_DEG
-      sample_size = int(nref_per_degree * width)
+    working_isel = flex.size_t()
+    for iexp, exp in self._experiments:
 
-    # adjust if this is below the chosen limit
-    sample_size = max(sample_size, minimum_sample_size)
+      sel = self._reflections['id'] == iexp
+      isel = sel.iselection()
+      #refs = self._reflections.select(sel)
+      nrefs = sample_size = len(isel)
 
-    # set maximum sample size
-    if max_num_obs:
-      if sample_size > max_num_obs:
-        sample_size = max_num_obs
+      # set sample size according to nref_per_degree (per experiment)
+      if exp.scan and self._nref_per_degree:
+        sweep_range_rad = exp.scan.get_oscillation_range(deg=False)
+        width = abs(self._sweep_range_rad[1] -
+                    self._sweep_range_rad[0]) * RAD_TO_DEG
+        sample_size = int(nref_per_degree * width)
+      else: sweep_range_rad = None
 
-    # sample the data and record the sample size
-    if sample_size < len(working_indices):
-      self._sample_size = sample_size
-      working_indices = random.sample(working_indices,
-                                      self._sample_size)
-    return(working_indices)
+      # adjust sample size if below the chosen limit
+      sample_size = max(sample_size, self._min_sample_size)
+
+      # set maximum sample size if requested
+      if self._max_sample_size:
+        sample_size = min(sample_size, self._max_sample_size)
+
+      # determine subset and collect indices
+      if sample_size < nrefs:
+        isel = isel.select(flex.random_selection(nrefs, sample_size))
+      working_isel.extend(isel)
+
+    # create subset
+    self._reflections = self._reflections.select(working_isel)
+
+    return
 
   def get_input_size(self):
     """Return the number of observations in the initial list supplied
@@ -299,18 +372,19 @@ class ReflectionManager(object):
 
     """
 
+    sort_obs = deepcopy(obs)
     if angular:
-      k = lambda e: e.phi_resid
+      sort_obs.sort('phi_resid', reverse=True)
     else:
-      k = lambda e: e.x_resid**2 + e.y_resid**2
-    sort_obs = sorted(obs, key=k, reverse=True)
-
+      sort_obs['key'] = sort_obs['x_resid']**2 + sort_obs['y_resid']**2
+      sort_obs.sort('key', reverse=True)
+      del sort_obs['key']
     return sort_obs
 
   def get_matches(self):
     """For every observation matched with a prediction return all data"""
 
-    return [obs for obs in self._obs_pred_pairs if obs.is_matched]
+    return self._reflections.select[self._reflections['is_matched']]
 
   def print_stats_on_matches(self):
     """Print some basic statistics on the matches"""
@@ -319,12 +393,10 @@ class ReflectionManager(object):
     if self._verbosity > 1:
 
       from scitbx.math import five_number_summary
-      x_resid = [e.x_resid for e in l]
-      y_resid = [e.y_resid for e in l]
-      phi_resid = [e.phi_resid for e in l]
-      w_x = [e.weight_x_obs for e in l]
-      w_y = [e.weight_y_obs for e in l]
-      w_phi = [e.weight_phi_obs for e in l]
+      x_resid = l['x_resid']
+      y_resid = l['y_resid']
+      phi_resid = l['phi_resid']
+      w_x, w_y, w_phi = l['xyzobs.mm.weights'].parts()
 
       print "\nSummary statistics for observations matched to predictions:"
       print ("                      "
@@ -378,7 +450,7 @@ class ReflectionManager(object):
 
     return
 
-  def reject_outliers(self):
+  def _reject_outliers(self):
     """Unset the use flag on matches with extreme (outlying) residuals.
 
     Outlier detection finds values more than _iqr_multiplier times the
@@ -390,11 +462,11 @@ class ReflectionManager(object):
     if self._iqr_multiplier is None: return False
 
     from scitbx.math import five_number_summary
-    matches = [obs for obs in self._obs_pred_pairs if obs.is_matched]
+    matches = self._reflections.select(self._reflections['is_matched'])
 
-    x_resid = [e.x_resid for e in matches]
-    y_resid = [e.y_resid for e in matches]
-    phi_resid = [e.phi_resid for e in matches]
+    x_resid = matches['x_resid']
+    y_resid = matches['y_resid']
+    phi_resid = matches['phi_resid']
 
     min_x, q1_x, med_x, q3_x, max_x = five_number_summary(x_resid)
     min_y, q1_y, med_y, q3_y, max_y = five_number_summary(y_resid)
@@ -409,14 +481,14 @@ class ReflectionManager(object):
     cut_phi = self._iqr_multiplier * iqr_phi
 
     for m in matches:
-      if m.x_resid > q3_x + cut_x: m.is_matched = False
-      if m.x_resid < q1_x - cut_x: m.is_matched = False
-      if m.y_resid > q3_y + cut_y: m.is_matched = False
-      if m.y_resid < q1_y - cut_y: m.is_matched = False
-      if m.phi_resid > q3_phi + cut_phi: m.is_matched = False
-      if m.phi_resid < q1_phi - cut_phi: m.is_matched = False
+      if m['x_resid'] > q3_x + cut_x: m['is_matched'] = False
+      if m['x_resid'] < q1_x - cut_x: m['is_matched'] = False
+      if m['y_resid'] > q3_y + cut_y: m['is_matched'] = False
+      if m['y_resid'] < q1_y - cut_y: m['is_matched'] = False
+      if m['phi_resid'] > q3_phi + cut_phi: m['is_matched'] = False
+      if m['phi_resid'] < q1_phi - cut_phi: m['is_matched'] = False
 
-    nreject = [m.is_matched for m in matches].count(False)
+    nreject = matches['is_matched'].count(False)
 
     if nreject == 0: return False
 
@@ -450,7 +522,8 @@ class ReflectionManager(object):
     """Reset all observations to use=False in preparation for a new set of
     predictions"""
 
-    for obs in self._obs_pred_pairs: obs.reset()
+    self._reflections['is_matched'].fill(False)
+    return
 
   def get_obs(self):
     """Get the list of managed observations"""
@@ -533,10 +606,10 @@ class ReflectionManagerXY(ReflectionManager):
         fmt = "(%3d, %3d, %3d) %5.3f %5.3f %5.3f %5.3f"
         for i in xrange(20):
           e = sl[i]
-          msg = fmt % tuple(e.miller_index + (e.x_resid,
-                           e.y_resid,
-                           e.weight_x_obs,
-                           e.weight_y_obs))
+          msg = fmt % tuple(e['miller_index'] + (e['x_resid'],
+                           e['y_resid'],
+                           e['weight_x_obs'],
+                           e['weight_y_obs']))
           print msg
         print
 
