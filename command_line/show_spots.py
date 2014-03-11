51a007df0285347261d05d1087de8a48a175f1ed
Graeme Winter <graeme.winter@gmail.com>
2014-03-11 12:43:40 +0000

    Jiffy to show x, y, z, dx, dy, dz from strong.pickle, to look for any bias in the variance (I did not find any)

---
+++ command_line/show_spots.py
@@ -0,0 +1,39 @@
+from __future__ import division
+
+def show_spots(strong_spots):
+
+  import math
+
+  x, y, z = zip(*strong_spots['xyzobs.px.value'])
+  vx, vy, vz = zip(*strong_spots['xyzobs.px.variance'])
+  dx = flex.sqrt(flex.double(vx))
+  dy = flex.sqrt(flex.double(vy))
+  dz = flex.sqrt(flex.double(vz))
+
+  mdx = sum(dx) / len(dx)
+  vdx = sum([(v - mdx) ** 2 for v in dx]) / len(dx)
+
+  mdy = sum(dy) / len(dy)
+  vdy = sum([(v - mdy) ** 2 for v in dy]) / len(dy)
+
+  mdz = sum(dz) / len(dz)
+  vdz = sum([(v - mdz) ** 2 for v in dz]) / len(dz)
+
+  for j in range(len(strong_spots)):
+    print '%8.2f %8.2f %8.2f %8.4f %8.4f %8.4f' % (x[j], y[j], z[j],
+                                                   dx[j], dy[j], dz[j])
+
+  print '<dX>: %.4f %.4f' % (mdx, math.sqrt(vdx))
+  print '<dY>: %.4f %.4f' % (mdy, math.sqrt(vdy))
+  print '<dZ>: %.4f %.4f' % (mdz, math.sqrt(vdz))
+
+if __name__ == '__main__':
+  import sys
+  if len(sys.argv) != 2:
+    raise RuntimeError, '%s strong.pickle'
+
+  import cPickle as pickle
+  from dials.array_family import flex
+
+  strong_spots = pickle.load(open(sys.argv[1], 'rb'))
+  show_spots(strong_spots)
