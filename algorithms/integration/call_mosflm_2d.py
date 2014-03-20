6f89563c9a32a90f75264d766277de141a7727c8
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2014-03-20 11:53:19 +0000

    adding a bit of optional visualization tool that proves that 2D profile fitting needs 2D shoeboxes

--- algorithms/integration/call_mosflm_2d.py
+++ algorithms/integration/call_mosflm_2d.py
@@ -18,6 +18,14 @@ from dials.array_family import flex
 
 #def mosflm_caller(rlist, xmax, ymax, n_div):
 def mosflm_caller(ref_table_in, xmax, ymax, n_div):
+
+  imagin_stuff = '''
+  #################################################################################
+  t_intensity = ref_table_in['intensity.raw.value']
+  old_i_table = t_intensity[:]
+  #################################################################################
+  #'''
+
   ncol = n_div
   nrow = n_div
   arr_rlist = []
@@ -67,6 +75,28 @@ def mosflm_caller(ref_table_in, xmax, ymax, n_div):
       #arr_rlist[row][col] = fit_profile_2d(arr_rlist[row][col], ref_table_in
       #                      arr_proff, row, col,  xmax, ymax)
   print "profile fitting            ....       Done"
+
+  imagin_stuff = '''
+  ###############################################################################
+  t_intensity = ref_table_in['intensity.raw.value']
+  num_ref = len(t_intensity)
+  paint_compare = []
+  for i in range(num_ref):
+    paint_compare.append([ old_i_table[i], t_intensity[i]])
+  paint_compare_sort = sorted(paint_compare)
+  import numpy
+  data1d = numpy.zeros(num_ref, dtype = numpy.float64)
+  new_data1d = numpy.zeros(num_ref, dtype = numpy.float64)
+  for i in range(num_ref):
+    data1d[i] = paint_compare_sort[i][0]
+    new_data1d[i] = paint_compare_sort[i][1]
+
+  from matplotlib import pylab
+  pylab.plot(data1d)
+  pylab.plot(new_data1d)
+  pylab.show()
+  ###############################################################################
+  #'''
 
   #new_ref_table = flex.reflection_table()
   #return new_ref_table
