7a5b2c4b9d3cfd631f04138ab1804e6ec23d534b
James Parkhurst <james.parkhurst@diamond.ac.uk>
2014-04-04 10:52:42 +0000

    Doing summation along with profile fitting

--- algorithms/integration/profile_fitting_reciprocal_space.py
+++ algorithms/integration/profile_fitting_reciprocal_space.py
@@ -39,12 +39,19 @@ class ProfileFittingReciprocalSpace(object):
     '''
     from dials.model.serialize import dump
     assert("flags" in reflections)
+    self._integrate_by_summation(experiment, reflections)
     self._transform_profiles(experiment, reflections)
     self.learner = self._learn_references(experiment, reflections)
     counter = ProfileFittingReciprocalSpace.reference_counter
     dump.reference(self.learner.locate(), "reference_%d.pickle" % counter)
     ProfileFittingReciprocalSpace.reference_counter += 1
     return self._integrate_intensities(self.learner, reflections)
+
+  def _integrate_by_summation(self, experiment, reflections):
+    ''' Integrate the reflections by summation. '''
+    from dials.algorithms.integration import Summation3d
+    algorithm = Summation3d()
+    algorithm(experiment, reflections)
 
   def _transform_profiles(self, experiment, reflections):
     ''' Transform the reflection profiles to reciprocal space. '''
