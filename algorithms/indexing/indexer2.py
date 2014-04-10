3ac795990f7d299596047cbdc15b21a10f1d58b1
Richard Gildea <richard.gildea@diamond.ac.uk>
2014-04-10 13:27:58 +0000

    don't do nearest neighbour analysis for max_cell if target unit_cell is provided

--- algorithms/indexing/indexer2.py
+++ algorithms/indexing/indexer2.py
@@ -327,33 +327,38 @@ class indexer_base(object):
     self._map_spots_pixel_to_reciprocal_space_timer.start()
 
     if self.params.max_cell is libtbx.Auto:
-      # The nearest neighbour analysis gets fooled when the same part of
-      # reciprocal space has been measured twice as this introduced small
-      # random differences in position between reflections measured twice.
-      # Therefore repeat the nearest neighbour analysis several times in small
-      # wedges where there shouldn't be any overlap in reciprocal space
-      from rstbx.indexing_api.nearest_neighbor import neighbor_analysis
-      phi_deg = self.reflections['xyzobs.mm.value'].parts()[2] * (180/math.pi)
-      if (flex.max(phi_deg) - flex.min(phi_deg)) < 1e-3:
-        NN = neighbor_analysis(self.reciprocal_space_points)
-        self.params.max_cell = NN.max_cell
+      if self.params.known_symmetry.unit_cell is not None:
+        reference = self.target_symmetry_primitive.as_reference_setting()
+        uc_params  = reference.unit_cell().parameters()
+        self.params.max_cell = 1.5 * max(uc_params[:3])
       else:
-        phi_min = flex.min(phi_deg)
-        phi_max = flex.max(phi_deg)
-        step_size = 5 #degrees
-        d_phi = phi_max - phi_min
-        n_steps = int(math.ceil(d_phi / step_size))
-        max_cell = flex.double()
-        for n in range(n_steps):
-          sel = (phi_deg > (phi_min+n*step_size)) & (phi_deg < (phi_min+(n+1)*step_size))
-          rlp = self.reciprocal_space_points.select(sel)
-          if len(rlp) == 0:
-            continue
-          NN = neighbor_analysis(rlp)
-          max_cell.append(NN.max_cell)
-          #print NN.max_cell
-        self.params.max_cell = flex.mean(max_cell) # or max or median?
-      print "Found max_cell: %.1f Angstrom" %(self.params.max_cell)
+        # The nearest neighbour analysis gets fooled when the same part of
+        # reciprocal space has been measured twice as this introduced small
+        # random differences in position between reflections measured twice.
+        # Therefore repeat the nearest neighbour analysis several times in small
+        # wedges where there shouldn't be any overlap in reciprocal space
+        from rstbx.indexing_api.nearest_neighbor import neighbor_analysis
+        phi_deg = self.reflections['xyzobs.mm.value'].parts()[2] * (180/math.pi)
+        if (flex.max(phi_deg) - flex.min(phi_deg)) < 1e-3:
+          NN = neighbor_analysis(self.reciprocal_space_points)
+          self.params.max_cell = NN.max_cell
+        else:
+          phi_min = flex.min(phi_deg)
+          phi_max = flex.max(phi_deg)
+          step_size = 5 #degrees
+          d_phi = phi_max - phi_min
+          n_steps = int(math.ceil(d_phi / step_size))
+          max_cell = flex.double()
+          for n in range(n_steps):
+            sel = (phi_deg > (phi_min+n*step_size)) & (phi_deg < (phi_min+(n+1)*step_size))
+            rlp = self.reciprocal_space_points.select(sel)
+            if len(rlp) == 0:
+              continue
+            NN = neighbor_analysis(rlp)
+            max_cell.append(NN.max_cell)
+            #print NN.max_cell
+          self.params.max_cell = flex.mean(max_cell) # or max or median?
+        print "Found max_cell: %.1f Angstrom" %(self.params.max_cell)
 
     if self.params.sigma_phi_deg is not None:
       var_x, var_y, _ = self.reflections['xyzobs.mm.variance'].parts()
