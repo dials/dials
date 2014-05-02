900a046c08c6b3da64a50c33e8f3f44860bfc140
David Waterman <david.waterman@stfc.ac.uk>
2014-05-02 14:03:04 +0000

    Expand into a more useful test. The delta Psi values calculated by the predictor for all reflections near the Ewald sphere are verified by direct calculation of the angle between vectors r and q

--- test/algorithms/spot_prediction/tst_stills_reflection_predictor.py
+++ test/algorithms/spot_prediction/tst_stills_reflection_predictor.py
@@ -15,16 +15,21 @@ class Test(object):
 
   def __init__(self):
 
+    # Set up experimental models with regular geometry
     from dials.model.experiment import beam_factory
     from dials.model.experiment import goniometer_factory
     from dials.model.experiment import detector_factory
 
     from cctbx.crystal.crystal_model import crystal_model
 
+    # Beam along the Z axis
     self.beam = beam_factory.make_beam(unit_s0 = matrix.col((0, 0, 1)),
                                        wavelength = 1.0)
-    #self.goniometer = goniometer_factory.known_axis(matrix.col((1, 0, 0)))
 
+    # Goniometer (used only for index generation) along X axis
+    self.goniometer = goniometer_factory.known_axis(matrix.col((1, 0, 0)))
+
+    # Detector fast, slow along X, -Y; beam in the centre, 200 mm distance
     dir1 = matrix.col((1, 0, 0))
     dir2 = matrix.col((0, -1, 0))
     n = matrix.col((0, 0, 1))
@@ -39,43 +44,80 @@ class Test(object):
                         (npx_fast, npx_slow),
                         (0, 1.e6))
 
-    a = matrix.col((1, 0, 0))
-    b = matrix.col((0, 1, 0))
-    c = matrix.col((0, 0, 1))
+    # Cubic 100 A^3 crystal
+    a = matrix.col((100, 0, 0))
+    b = matrix.col((0, 100, 0))
+    c = matrix.col((0, 0, 100))
     self.crystal = crystal_model(a, b, c, space_group_symbol = "P 1")
 
-    from dials.model.experiment.experiment_list import ExperimentList, Experiment
-    self.experiments = ExperimentList()
-    self.experiments.append(Experiment(
-      beam=self.beam, detector=self.detector, goniometer=None,
-      scan=None, crystal=self.crystal, imageset=None))
+    # Collect these models in an Experiment (ignoring the goniometer)
+    from dials.model.experiment.experiment_list import Experiment
+    self.experiment = Experiment(beam=self.beam, detector=self.detector,
+      goniometer=None, scan=None, crystal=self.crystal, imageset=None)
 
+    # Generate some reflections
+    self.reflections = self.generate_reflections()
+
+    return
+
+  def generate_reflections(self):
+    """Use reeke_model to generate indices of reflections near to the Ewald
+    sphere that might be observed on a still image. Build a reflection_table
+    of these."""
+
+    # create a ReekeIndexGenerator
+    UB = self.crystal.get_U() * self.crystal.get_B()
+    axis = self.goniometer.get_rotation_axis()
+    s0 = self.beam.get_s0()
+    dmin = 1.5
+    # use the same UB at the beginning and end - the margin parameter ensures
+    # we still have indices close to the Ewald sphere generated
+    from dials.algorithms.spot_prediction import ReekeIndexGenerator
+    r = ReekeIndexGenerator(UB, UB, axis, s0, dmin=1.5, margin=1)
+
+    # generate indices
+    hkl = r.to_array()
+    nref = len(hkl)
+
+    # create a reflection table
     from dials.array_family import flex
     table = flex.reflection_table()
-    table['flags'] = flex.size_t([0, 0])
-    table['id']    = flex.size_t([0, 0])
-    table['panel'] = flex.size_t([0, 0])
+    table['flags'] = flex.size_t(nref, 0)
+    table['id']    = flex.size_t(nref, 0)
+    table['panel'] = flex.size_t(nref, 0)
+    table['miller_index'] = flex.miller_index(hkl)
+    table['entering']     = flex.bool(nref, True)
+    table['s1']           = flex.vec3_double(nref)
+    table['xyzcal.mm']    = flex.vec3_double(nref)
+    table['xyzcal.px']    = flex.vec3_double(nref)
 
-    # Predicted properties
-    table['miller_index'] = flex.miller_index([(1,0,0), (0,1,0)])
-    table['entering']     = flex.bool([True, True])
-    table['s1']           = flex.vec3_double(2)
-    table['xyzcal.mm']    = flex.vec3_double(2)
-    table['xyzcal.px']    = flex.vec3_double(2)
-
-    self.reflections = table
-    print self.reflections[0]
-    for k in self.reflections.keys(): print k
-
+    return table
 
   def run(self):
 
+    # create the predictor and predict for reflection table
     from dials.algorithms.spot_prediction import StillsReflectionPredictor
-
-    predictor = StillsReflectionPredictor(self.experiments[0])
-
+    predictor = StillsReflectionPredictor(self.experiment)
     predictor.for_reflection_table(self.reflections)
-    for r in self.reflections: print r
+
+    # cache objects from the model
+    UB = self.crystal.get_U() * self.crystal.get_B()
+    s0 = matrix.col(self.beam.get_s0())
+    es_radius = s0.length()
+
+    # for every reflection, reconstruct relp rotated to the Ewald sphere (vector
+    # r) and unrotated relp (vector q), calculate the angle between them and
+    # compare with delpsical.rad
+    from libtbx.test_utils import approx_equal
+    for ref in self.reflections:
+      r = s0 - matrix.col(ref['s1'])
+      q = UB * matrix.col(ref['miller_index'])
+      tst_radius = (s0 + q).length()
+      sgn = -1 if tst_radius > es_radius else 1
+      delpsi = sgn*r.accute_angle(q)
+      assert approx_equal(delpsi, ref['delpsical.rad'])
+
+    print "OK"
 
 
 if __name__ == '__main__':
