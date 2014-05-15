529059cbb9ca28db1d742790bec12ccddcec6be2
David Waterman <david.waterman@stfc.ac.uk>
2014-05-15 15:27:47 +0000

    RJG fixed it. Thanks!

--- scratch/asb/refine_quadrants.py
+++ scratch/asb/refine_quadrants.py
@@ -6,10 +6,11 @@ import sys, os
 from libtbx.phil import command_line, parse
 
 if len(sys.argv) != 2: exit("please pass the path to a phil file")
-with(open(sys.argv[1])) as f:
-  phil = f.read()
+#with(open(sys.argv[1])) as f:
+#  phil = f.read()
 
-print phil
+#print phil
+phil = sys.argv[1]
 
 master_phil = parse("""
   input
@@ -23,7 +24,7 @@ master_phil = parse("""
   """)
 
 cmd_line = command_line.argument_interpreter(master_params=master_phil)
-working_phil = cmd_line.process_and_fetch(args=[phil])
+working_phil = cmd_line.process_and_fetch(args=(phil,))
 working_params = working_phil.extract()
 for input in working_params.input:
   print input.experiments, input.reflections
