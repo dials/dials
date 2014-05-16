8ef610fd9dd2967341c4af3e75bb2bf92c102f77
David Waterman <david.waterman@stfc.ac.uk>
2014-05-16 15:05:07 +0000

    set target.jacobian_max_nref=100000

--- scratch/asb/refine_quadrants.py
+++ scratch/asb/refine_quadrants.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env dials.python
 from __future__ import division
+
 import sys, os
+
 from libtbx.phil import command_line, parse
 
 if len(sys.argv) != 2: exit("please pass the path to a phil file")
+#with(open(sys.argv[1])) as f:
+#  phil = f.read()
+
+#print phil
 phil = sys.argv[1]
 
 master_phil = parse("""
@@ -26,6 +32,8 @@ for input in working_params.input:
 
 from dials.model.serialize import load
 def load_input(exp_path, ref_path):
+  #from dials.model.experiment.experiment_list import ExperimentListFactory
+  #e = ExperimentListFactory().from_args(working_params.input[0].experiments)
   refs = load.reflections(ref_path)
   exp = load.experiment_list(exp_path , check_format=False)[0]
   return refs, exp
@@ -45,7 +53,7 @@ class ExperimentFromCrystal(object):
                       detector=self.reference_detector,
                       crystal=crystal)
 
-assert len(working_params.input) > 1
+assert len(working_params.input)
 e = enumerate(working_params.input)
 i, line = e.next()
 reflections, exp = load_input(line.experiments, line.reflections)
@@ -53,36 +61,19 @@ assert reflections['id'].all_eq(0)
 experiment_from_crystal=ExperimentFromCrystal(exp.beam, exp.detector)
 
 from dials.model.experiment.experiment_list import ExperimentList
+
 experiments=ExperimentList()
 experiments.append(experiment_from_crystal(exp.crystal))
 
 from scitbx.array_family import flex
+
 for i, line in e:
+  print i
+
   refs, exp = load_input(line.experiments, line.reflections)
-  refs['id'] = flex.int(len(refs),i)
+  refs['id'] = flex.size_t(len(refs),i)
   reflections.extend(refs)
   experiments.append(experiment_from_crystal(exp.crystal))
 
-# analysis of panel sampling
-#TODO
-
-# refinement
-from libtbx.phil import parse
-user_phil=parse("""
-refinement.parameterisation {
-  beam.fix=all
-  detector.hierarchy_level=1
-}""")
-from dials.data.refinement import phil_scope as master_phil
-working_phil = master_phil.fetch(
-  sources=[user_phil])
-working_phil.show()
-params = working_phil.extract()
-from dials.algorithms.refinement import RefinerFactory
-refiner = RefinerFactory.from_parameters_data_experiments(
-    params, reflections, experiments,
-    verbosity=2)
-refiner.run()
-
-
+from dials.util.command_line import interactive_console; interactive_console()
 
