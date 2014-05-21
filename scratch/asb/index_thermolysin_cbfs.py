79aa6fe94a96dbd04e532cba6c2f6184c7c4aa87
Aaron S. Brewster <asbrewster@lbl.gov>
2014-05-21 00:03:35 +0000

    Ooops.  Remove a debug return statement.

--- scratch/asb/index_thermolysin_cbfs.py
+++ scratch/asb/index_thermolysin_cbfs.py
@@ -15,7 +15,6 @@ def do_work(path):
 
   easy_run.call("dials.import %s --output %s"%(path,datablock))
   easy_run.call("dials.find_spots %s threshold.xds.sigma_strong=15 min_spot_size=2 -o %s"%(datablock, spots))
-  return
   easy_run.call("dials.index %s %s method=fft1d beam.fix=all detector.fix=all known_symmetry.unit_cell=93,93,130,90,90,120 known_symmetry.space_group=P6122 n_macro_cycles=5 d_min_final=0.5 experiments=%s reflections=%s"%(spots, datablock, experiments, reflections))
 
   if os.path.exists(reflections):
