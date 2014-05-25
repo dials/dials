77b6e1749a24fc978f8cbc362da22e78e28c8e5d
Graeme Winter <graeme.winter@gmail.com>
2014-05-25 11:35:20 +0000

    Now evaluating parallax corrections from XS output, seems I get a value about half of what XDS does in terms of the extreme ranges...
    dials.python parallax.py /Users/graeme/dials/i04-BAG-training/xds/DEFAULT/NATIVE/SWEEP1/index
    -0.466614045148 0.470084212858
    -0.454976966823 0.499378497183
    
    more to do...

--- scratch/gw/parallax.py
+++ scratch/gw/parallax.py
@@ -55,5 +55,95 @@ def work():
       math.log(0.5 + 0.5 * math.exp(- mu_cm_170 * t0 / math.cos(theta)))
     print j, o_127 / pixel, o_170 / pixel
 
+def read_xds_calibration_file(calibration_file):
+  '''Read XDS calibration file, return as flex array.'''
+
+  from scitbx.array_family import flex
+  from cbflib_adaptbx import uncompress, compress
+  import binascii
+
+  start_tag = binascii.unhexlify('0c1a04d5')
+
+  data = open(calibration_file, 'rb').read()
+  data_offset = data.find(start_tag) + 4
+  cbf_header = data[:data_offset - 4]
+
+  fast = 0
+  slow = 0
+  length = 0
+
+  for record in cbf_header.split('\n'):
+    if 'X-Binary-Size-Fastest-Dimension' in record:
+      fast = int(record.split()[-1])
+    elif 'X-Binary-Size-Second-Dimension' in record:
+      slow = int(record.split()[-1])
+    elif 'X-Binary-Number-of-Elements' in record:
+      length = int(record.split()[-1])
+
+  assert(length == fast * slow)
+
+  pixel_values = uncompress(packed = data[data_offset:],
+                            fast = fast, slow = slow)
+
+  return pixel_values
+
+def validate_against_xds(xds_directory):
+  '''Will look in xds_directory for (i) XPARM.XDS and (ii) X- and Y-CORRECTIONS.cbf
+  files, and will rederive from the former the values for the latter...'''
+
+  from rstbx.cftbx.coordinate_frame_converter import coordinate_frame_converter
+  import os
+  import math
+
+  # read and derive all of the model parameters
+
+  xparm = os.path.join(xds_directory, 'XPARM.XDS')
+  cfc = coordinate_frame_converter(xparm)
+  fast = cfc.get_c('detector_fast')
+  slow = cfc.get_c('detector_slow')
+  origin = cfc.get_c('detector_origin')
+  wavelength = cfc.get('wavelength')
+  normal = fast.cross(slow)
+
+  energy_kev = 12.3985 / wavelength
+  mu = derive_absorption_coefficient_Si(energy_kev)
+
+  detector_size = cfc.get('detector_size_fast_slow')
+  pixel_size = cfc.get('detector_pixel_size_fast_slow')
+
+  beam_centre_fast_slow = cfc.derive_beam_centre_pixels_fast_slow()
+  beam_centre = beam_centre_fast_slow[0] * pixel_size[0] * fast + \
+    beam_centre_fast_slow[1] * pixel_size[1] * slow + origin
+
+  t0 = 0.032
+  pixel = 0.0172
+
+  min_offset_x = 0
+  max_offset_x = 0
+  min_offset_y = 0
+  max_offset_y = 0
+
+  for k in range(0, detector_size[1], 10):
+    for j in range(0, detector_size[0], 10):
+      p = j * pixel_size[0] * fast + k * pixel_size[1] * slow + origin
+      theta = p.angle(normal)
+      offset = - (1.0 / mu) * math.sin(theta) * \
+        math.log(0.5 + 0.5 * math.exp(- mu * t0 / math.cos(theta))) / pixel
+      offset_vector = (p - beam_centre).normalize()
+      offset_x = fast.dot(offset_vector * offset)
+      offset_y = slow.dot(offset_vector * offset)
+      if offset_x < min_offset_x:
+        min_offset_x = offset_x
+      if offset_x > max_offset_x:
+        max_offset_x = offset_x
+      if offset_y < min_offset_y:
+        min_offset_y = offset_y
+      if offset_y > max_offset_y:
+        max_offset_y = offset_y
+
+  print min_offset_x, max_offset_x
+  print min_offset_y, max_offset_y
+
 if __name__ == '__main__':
-  work()
+  import sys
+  validate_against_xds(sys.argv[1])
