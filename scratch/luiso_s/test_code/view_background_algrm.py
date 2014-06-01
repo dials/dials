16108c99b6089071def5b640576ae30aea5e49db
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2014-06-01 19:21:31 +0000

    adjusting illustrative code

--- scratch/luiso_s/test_code/view_background_algrm.py
+++ scratch/luiso_s/test_code/view_background_algrm.py
@@ -6,14 +6,15 @@ from matplotlib import pylab
 
 import numpy
 
-nrow = 40
-ncol = 40
+nrow = 32
+ncol = 32
 
 ref_ang = 5
-local_i = 200
+local_i = 50
 
-data2d = model_2d(nrow, ncol, 3, 2, ref_ang, local_i, 0.5)
+data2d = model_2d(nrow, ncol, 6, 3, ref_ang, local_i, 0.5)
 
+ext_siz = 2
 mask2d = flex.int(flex.grid(nrow, ncol))
 for x_loc in range(ncol):
   for y_loc in range(nrow):
@@ -22,20 +23,27 @@ for x_loc in range(ncol):
     else:
       mask2d[y_loc, x_loc] = 3
 
-#tmp='''
+ext_siz = 3
+tmp_mask = mask2d[:,:]
+mask2d = mask2d.as_numpy_array()
+for x_loc in range(ncol):
+  for y_loc in range(nrow):
+    if( tmp_mask[y_loc, x_loc] == 5):
+      mask2d[y_loc - ext_siz:y_loc + ext_siz \
+      , x_loc - ext_siz : x_loc + ext_siz] = 5
+
+mask2d = flex.int(mask2d)
 print "adding noise ...."
 import random
 for x_loc in range(ncol):
   for y_loc in range(nrow):
     roll_the_dice = random.randint(1,50)
     data2d[y_loc, x_loc] += (y_loc + x_loc) * 0.3
-    noise_scale = .01
-    inclined_pl_scale = 30
+    noise_scale = .1
+    inclined_pl_scale = 300000
     data2d[y_loc, x_loc] += inclined_pl_scale * (nrow + ncol)
-    data2d[y_loc, x_loc] += roll_the_dice * noise_scale #* data2d[y_loc, x_loc]
+    data2d[y_loc, x_loc] += roll_the_dice * noise_scale
 
-    
-    
 print "adding noise .... done"
 
 from dials.algorithms.background import curved_background_flex_2d
