155f49f138d0908559911980e21c891ef1851f4b
Graeme Winter <graeme.winter@gmail.com>
2014-06-05 09:37:54 +0000

    By default use_all_reflections for the refinement so that the full reflection list is indexed (necessary for using fitrs for integration, should really work out a way of reindexing the spot list before running integration, would be tidier...)

--- doc/user/tutorial.sh
+++ doc/user/tutorial.sh
@@ -19,22 +19,23 @@ dials.find_spots min_spot_size=3 datablock.json
 # and refinement. can also specify unit cell here and also apply different
 # indexing algorithms
 
-dials.index space_group=P4 datablock.json strong.pickle
+dials.index space_group=P4 datablock.json strong.pickle use_all_reflections=true
 
 # run the refinement of the data - the indexing includes refinement so the result
 # will be refined already with a static model - however here we would like to use
 # a scan varying model so re-run the refinement (you should find that the R.M.S.
 # deviations are a little lower following the scan varying refinement)
 
-dials.refine experiments.json indexed.pickle scan_varying=true
+dials.refine experiments.json indexed.pickle scan_varying=true \
+  use_all_reflections=true
 
 # now run the integration - complex choices of algorithms are shown here in
-# terms of the peak measurement (actually no) and background determination
+# terms of the peak measurement (fitrs) and background determination
 # methods. pass reference reflections from indexing in to determine the
 # profile parameters...
 
-dials.integrate outlier.algorithm=null refined_experiments.json \
-  -r indexed.pickle
+dials.integrate outlier.algorithm=null intensity.algorithm=fitrs \
+  refined_experiments.json -r indexed.pickle
 
 # finally export the integrated measurements in an MTZ file - this should be
 # properly formatted for immediate use in pointless / aimless
