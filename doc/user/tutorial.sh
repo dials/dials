40e89b4773cdd8c7db406a050ec880a560688f92
Graeme Winter <graeme.winter@gmail.com>
2014-06-05 07:10:21 +0000

    Fix tutorial documentation with new API (I think...)

--- doc/user/tutorial.sh
+++ doc/user/tutorial.sh
@@ -2,28 +2,28 @@
 # DIALS tutorial script - associated with data doi:10.5281/zenodo.10271
 ############################################################################
 
-# first import the data - this reads the image headers and creates a file which 
-# describes the sweeps found therein. here there should only be one sweep found 
+# first import the data - this reads the image headers and creates a file which
+# describes the sweeps found therein. here there should only be one sweep found
 
 dials.import ~/data/th_8_2_0*.cbf
 
-# find spots on the images - this will take a while! also accept small spots 
-# in this case as the data are good (N.B. to explore DIALS options you can 
-# run dials.parameters - this shows you the entire parameter set which is 
+# find spots on the images - this will take a while! also accept small spots
+# in this case as the data are good (N.B. to explore DIALS options you can
+# run dials.parameters - this shows you the entire parameter set which is
 # available for DIALS)
 
 dials.find_spots min_spot_size=3 datablock.json
 
-# index these found spots - in this case the crystal is thaumatin which is known 
+# index these found spots - in this case the crystal is thaumatin which is known
 # to be tetragonal, so impose appropriate lattice constraints in the indexing
 # and refinement. can also specify unit cell here and also apply different
 # indexing algorithms
 
 dials.index space_group=P4 datablock.json strong.pickle
 
-# run the refinement of the data - the indexing includes refinement so the result 
+# run the refinement of the data - the indexing includes refinement so the result
 # will be refined already with a static model - however here we would like to use
-# a scan varying model so re-run the refinement (you should find that the R.M.S. 
+# a scan varying model so re-run the refinement (you should find that the R.M.S.
 # deviations are a little lower following the scan varying refinement)
 
 dials.refine experiments.json indexed.pickle scan_varying=true
@@ -34,15 +34,23 @@ dials.refine experiments.json indexed.pickle scan_varying=true
 # complex choices of algorithms are shown here in terms of the peak measurement
 # and background determination methods.
 
+# OLD command line
+
 dials.integrate n_blocks=6 outlier.algorithm=null \
   intensity.algorithm=fitrs refined_experiments.json indexed.pickle
 
-# finally export the integrated measurements in an MTZ file - this should be 
+# NEW command-line
+
+dials.integrate block_size=13.5 outlier.algorithm=null \
+  intensity.algorithm=fitrs refined_experiments.json -r indexed.pickle
+
+
+# finally export the integrated measurements in an MTZ file - this should be
 # properly formatted for immediate use in pointless / aimless
 
 dials.export_mtz integrated.pickle refined_experiments.json integrated.mtz
 
-# and as if to make a point, here is all we need to do is to sort the data with 
+# and as if to make a point, here is all we need to do is to sort the data with
 # pointless and then scale the data (ignoring anomalous differences) to 1.3A
 
 pointless hklin integrated.mtz hklout sorted.mtz
@@ -51,6 +59,3 @@ aimless hklin sorted.mtz hklout scaled.mtz << eof
 resolution 1.3
 anomalous off
 eof
-
-
-
