9ac464775d2ab86f8c592bc0a44bdc5a64ad17ea
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2014-06-10 15:19:53 +0000

    first exercise with wxpython

---
+++ scratch/luiso_s/wx_toys/wx_h_world.py
@@ -0,0 +1,6 @@
+import wx
+
+app = wx.App(False)  # Create a new app, don't redirect stdout/stderr to a window.
+frame = wx.Frame(None, wx.ID_ANY, "Hello World") # A Frame is a top-level window.
+frame.Show(True)     # Show the frame.
+app.MainLoop()
