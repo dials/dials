13e080f63e1c7c29fc968221b1e2dde32a4671f0
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2014-06-13 13:26:47 +0000

    last adjustments to this compatibility exercise between matplotlib, wx and numpy

--- scratch/luiso_s/wx_toys/bitmap_from_numpy_w_matplotlib_one_more_try.py
+++ scratch/luiso_s/wx_toys/bitmap_from_numpy_w_matplotlib_one_more_try.py
@@ -3,24 +3,26 @@ import numpy
 import matplotlib.pyplot as plt
 
 def GetBitmap_from_np_array(np_img_2d):
-  fig = plt.figure( )
-  plt.imshow(np_img_2d, interpolation = "nearest")
-  fig.canvas.draw ( )
-  w,h = fig.canvas.get_width_height()
+  fig = plt.figure()
+  # remember to make sure this is our convention in (x, y) vs (row, col)
+  plt.imshow(numpy.transpose(np_img_2d), interpolation = "nearest")
+  fig.canvas.draw()
+  width, height = fig.canvas.get_width_height()
   np_buf = numpy.fromstring ( fig.canvas.tostring_rgb(), dtype=numpy.uint8 )
-  np_buf.shape = ( w, h, 3)
-  np_buf = numpy.roll ( np_buf, 3, axis = 2 )
-  image = wx.EmptyImage(w,h)
+  np_buf.shape = (width, height, 3)
+  np_buf = numpy.roll(np_buf,3 , axis = 2)
+  image = wx.EmptyImage(width, height)
   image.SetData( np_buf.tostring())
   wxBitmap = image.ConvertToBitmap()
   return wxBitmap
 
-def build_np_img(width=64, height=64):
+def build_np_img(width = 64, height = 64):
   data2d = numpy.zeros( (width, height), 'float')
   print "width, height =", width, height
   for x in range(0, width):
     for y in range(0, height):
-      data2d[x,y] = numpy.sqrt(x*x + y*y)
+      #data2d[x,y] = numpy.sqrt(x*x + y*y)
+      data2d[x,y] = x + y
   data2d[width/4:width*3/4,height/4:height*3/4] = 0
   print "data2d.max =", data2d.max()
   return data2d
@@ -42,7 +44,7 @@ class MyFrame(wx.Frame):
     # Attributes
     self.panel = wx.Panel(self)
 
-    data2d = build_np_img(width=60, height=20)
+    data2d = build_np_img(width = 60, height = 20)
     bitmap = GetBitmap_from_np_array(data2d)
 
     self.bitmap = wx.StaticBitmap(self.panel, bitmap=bitmap)
