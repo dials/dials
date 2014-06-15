14d06e8912fb78e99e092a98dff754ab74139c2a
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2014-06-15 09:09:50 +0000

    working with sizers

--- scratch/luiso_s/wx_toys/bitmap__w_sizers.py
+++ scratch/luiso_s/wx_toys/bitmap__w_sizers.py
@@ -15,21 +15,24 @@ class MyFrame(wx.Frame):
                                   pos, size, style, name)
     # Attributes
     self.panel = wx.Panel(self, wx.ID_ANY)
-    img_path = os.path.abspath("../../../../../../53977.png")
+    #img_path = os.path.abspath("../../../../../../53977.png")
+    img_path = os.path.abspath("../../../../../../Pictures/duccati.png")
     bitmap = wx.Bitmap(img_path, type=wx.BITMAP_TYPE_PNG)
     self.bitmap = wx.StaticBitmap(self.panel, bitmap=bitmap)
 
-
-
+    topSizer        = wx.BoxSizer(wx.VERTICAL)
+    btnSizer        = wx.BoxSizer(wx.HORIZONTAL)
+    
     okBtn = wx.Button(self.panel, wx.ID_ANY, 'OK')
-    #cancelBtn = wx.Button(self.panel, wx.ID_ANY, 'Cancel')
+    cancelBtn = wx.Button(self.panel, wx.ID_ANY, 'Cancel')
     self.Bind(wx.EVT_BUTTON, self.onOK, okBtn)
-    #self.Bind(wx.EVT_BUTTON, self.onCancel, cancelBtn)
-
-    btnSizer        = wx.BoxSizer(wx.VERTICAL)
+    self.Bind(wx.EVT_BUTTON, self.onCancel, cancelBtn)
 
     btnSizer.Add(okBtn, 0, wx.ALL, 5)
-    #btnSizer.Add(cancelBtn, 0, wx.ALL, 5)
+    btnSizer.Add(cancelBtn, 0, wx.ALL, 5)
+    topSizer.Add(btnSizer, 0, wx.ALL|wx.CENTER, 5)
+    self.panel.SetSizer(topSizer)
+    topSizer.Fit(self)
 
   def onOK(self, event):
     # Do something
