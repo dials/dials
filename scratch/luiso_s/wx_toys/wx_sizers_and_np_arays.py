6581ca2061578041ad90b4cf9e1ed6a2c69deacc
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2014-06-16 20:16:43 +0000

    intermediate semistable mixed step

--- scratch/luiso_s/wx_toys/wx_sizers_and_np_arays.py
+++ scratch/luiso_s/wx_toys/wx_sizers_and_np_arays.py
@@ -10,8 +10,8 @@ Test most recently on OS-X wxPython 2.9.3.1
 But it been reported to work on lots of other platforms/versions
 
 """
-import wx, os
-
+import wx, os, numpy
+import matplotlib.pyplot as plt
 class TestFrame(wx.Frame):
     def __init__(self, *args, **kwargs):
         wx.Frame.__init__(self, *args, **kwargs)
@@ -84,11 +84,49 @@ class TestFrame(wx.Frame):
 
     def DisplayPrev(self, event = None):
         print "test"
+        np_img = build_np_img(width = 164, height = 64)
+
+        Img = GetBitmap_from_np_array(np_img)
+
+        self.Image.SetBitmap(Img)
+        self.Fit()
+        self.Layout()
+        self.Refresh()
 
     def OnCloseWindow(self, event):
         self.Destroy()
 
+def GetBitmap_from_np_array(np_img_2d):
+  fig = plt.figure()
+  # remember to make sure this is our convention in (x, y) vs (row, col)
+  plt.imshow(numpy.transpose(np_img_2d), interpolation = "nearest")
+  fig.canvas.draw()
+  width, height = fig.canvas.get_width_height()
+  np_buf = numpy.fromstring ( fig.canvas.tostring_rgb(), dtype=numpy.uint8 )
+  np_buf.shape = (width, height, 3)
+  np_buf = numpy.roll(np_buf, 3, axis = 2)
+  image = wx.EmptyImage(width, height)
+  image.SetData( np_buf )
+  #image.SetData( np_buf.tostring()) # looks like there is no need to convert
+  wxBitmap = image.ConvertToBitmap()
 
+  return wxBitmap
+
+  
+  
+def build_np_img(width = 64, height = 64):
+  data2d = numpy.zeros( (width, height), 'float')
+  print "width, height =", width, height
+  for x in range(0, width):
+    for y in range(0, height):
+      #data2d[x,y] = numpy.sqrt(x*x + y*y)
+      data2d[x,y] = x + y
+  data2d[width/4:width*3/4,height/4:height*3/4] = 0
+  print "data2d.max =", data2d.max()
+  return data2d
+  
+  
+  
 def GetJpgList(dir):
     jpgs = [f for f in os.listdir(dir) if f[-4:] == ".jpg"]
     # print "JPGS are:", jpgs
