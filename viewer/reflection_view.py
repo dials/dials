f19c68a0ad6aca976eb80ffa56d88f2e7434e796
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2014-06-20 16:17:20 +0000

    starting to work with sizes

--- viewer/reflection_view.py
+++ viewer/reflection_view.py
@@ -12,9 +12,11 @@ import wx, os
 from dials.viewer.viewer_utilities import GetBitmap_from_np_array, build_np_img
 from dials.viewer.reflection_data_navigator import table_s_navigator
 
+
 class ReflectionFrame(wx.Frame):
   def __init__(self, *args, **kwargs):
     wx.Frame.__init__(self, *args, **kwargs)
+
 
     self.MaxImageSizeX = 320
     self.MaxImageSizeY = 240
@@ -27,19 +29,18 @@ class ReflectionFrame(wx.Frame):
     btn_nxt_slice = wx.Button(self, -1, "Next slice ")
     btn_prv_slice = wx.Button(self, -1, "Previous slice")
     btn_tst = wx.Button(self, -1, "tst btn")
+    btn_tst1 = wx.Button(self, -1, "tst btn1")
 
     btn_nxt_slice.Bind(wx.EVT_BUTTON, self.DisplayNext_slice)
     btn_prv_slice.Bind(wx.EVT_BUTTON, self.DisplayPrev_slice)
     btn_tst.Bind(wx.EVT_BUTTON, self.B_tst)
-
+    btn_tst1.Bind(wx.EVT_BUTTON, self.B_tst1)
     self.Image_01 = wx.StaticBitmap(self, bitmap=wx.EmptyBitmap(
                                  self.MaxImageSizeX, self.MaxImageSizeY))
     self.Image_02 = wx.StaticBitmap(self, bitmap=wx.EmptyBitmap(
                                  self.MaxImageSizeX, self.MaxImageSizeY))
     self.Image_03 = wx.StaticBitmap(self, bitmap=wx.EmptyBitmap(
                                  self.MaxImageSizeX, self.MaxImageSizeY))
-    self.Bind(wx.EVT_SIZE, self.OnSize)
-    # Using a Sizers to handle the layout
 
     v_box = wx.BoxSizer(wx.VERTICAL)
     h_box = wx.BoxSizer(wx.HORIZONTAL)
@@ -65,6 +66,7 @@ class ReflectionFrame(wx.Frame):
     r_box.AddSpacer(50)
 
     r_box.Add(btn_tst, 0, wx.CENTER | wx.ALL,5)
+    r_box.Add(btn_tst1, 0, wx.CENTER | wx.ALL,5)
 
     h_box.Add(r_box)
     v_box.Add(h_box)
@@ -72,7 +74,7 @@ class ReflectionFrame(wx.Frame):
     self.frame_scale = 0.5
 
     self.SetSizerAndFit(v_box)
-    wx.EVT_CLOSE(self, self.OnCloseWindow)
+    wx.EVT_CLOSE(self, self.On_Close_Window)
 
   def tabl_to_frame(self, loc_tabl):
     self.tabl = table_s_navigator(loc_tabl)
@@ -95,20 +97,18 @@ class ReflectionFrame(wx.Frame):
     self.My_Update()
   def B_tst(self, event = None):
     print "Here tst"
-    self.frame_scale = self.frame_scale * 1.2
+    self.frame_scale = self.frame_scale * 1.1
+    self.My_Update()
+    print "self.GetSize() =", self.GetSize()
+  def B_tst1(self, event = None):
+    print "Here tst1"
+    self.frame_scale = self.frame_scale * 0.9
     self.My_Update()
     print "self.GetSize() =", self.GetSize()
 
-  def OnSize(self, event = None):
-    siz_data = self.GetSize()
-    print "New size of window =", siz_data
-    #self.frame_scale = float(siz_data[0] * siz_data[1]) * 0.5 / 320100.0
-    #self.My_Update()
-
   def My_Update(self):
-    #self.Unbind(wx.EVT_SIZE)
-    bkg, dat, msk = self.tabl()
 
+    bkg, dat, msk = self.tabl()
     I_max = self.tabl.Get_Max()
 
     My_Img = GetBitmap_from_np_array(np_img_2d = dat, Intst_max = I_max
@@ -120,23 +120,30 @@ class ReflectionFrame(wx.Frame):
     My_Img = GetBitmap_from_np_array(np_img_2d = msk, Intst_max = -1
                                      , img_scale = self.frame_scale)
     self.Image_03.SetBitmap(My_Img)
+
     self.Fit()
     self.Layout()
     self.Refresh()
 
-  def OnCloseWindow(self, event):
+  def On_Close_Window(self, event):
     self.Destroy()
 
 class App(wx.App):
   def OnInit(self):
+    original = '''
     self.frame = ReflectionFrame(None, -1, "DIALS Reflections Viewer"
     , wx.DefaultPosition,(550,200))
+    '''
+    self.frame = ReflectionFrame(None, -1, "DIALS Reflections Viewer"
+    , wx.DefaultPosition,(550,200))
+
     self.SetTopWindow(self.frame)
     self.frame.Show(True)
     return True
 
   def table_in(self, loc_tabl):
     self.frame.tabl_to_frame(loc_tabl)
+
 
 if __name__ == "__main__":
 
@@ -151,4 +158,4 @@ if __name__ == "__main__":
   app.table_in(table)
 
   app.MainLoop()
-#'''
+
