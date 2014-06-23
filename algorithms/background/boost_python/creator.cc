cbc384066e347474a710e2e24ebb7a08915a64e4
James Parkhurst <james.parkhurst@diamond.ac.uk>
2014-06-23 15:14:45 +0000

    Fixed test

--- algorithms/background/boost_python/creator.cc
+++ algorithms/background/boost_python/creator.cc
@@ -17,9 +17,16 @@ namespace dials { namespace algorithms { namespace background {
 
   using namespace boost::python;
 
+  af::shared<bool> call_for_shoeboxes(
+      const Creator &self, 
+      const af::const_ref< Shoebox<> > &sbox) {
+    af::shared<double> mse(sbox.size());
+    return self(sbox, mse.ref());
+  }
+
   void export_creator()
   {
-    typedef af::shared<bool>(Creator::*call_for_shoeboxes)(
+    typedef af::shared<bool>(Creator::*call_for_shoeboxes_w_err)(
         const af::const_ref< Shoebox<> >&,
         af::ref<double>)const;
 
@@ -39,7 +46,8 @@ namespace dials { namespace algorithms { namespace background {
           boost::shared_ptr<OutlierRejector>
         >((arg("modeller"),
            arg("rejector"))))
-      .def("__call__", (call_for_shoeboxes)&Creator::operator())
+      .def("__call__", &call_for_shoeboxes)
+      .def("__call__", (call_for_shoeboxes_w_err)&Creator::operator())
       .def("__call__", (call_for_shoebox)&Creator::operator())
       .def("__call__", (call_for_data)&Creator::operator());
   }
