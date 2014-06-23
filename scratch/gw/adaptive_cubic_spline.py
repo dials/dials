fe0c141132799046a330035252eca0264dcaeca6
Graeme Winter <graeme.winter@gmail.com>
2014-06-23 12:17:36 +0000

    A first cut attempt at some generic cubic spline interpolation code - why this? to help accelerate the parallax correction lookups ...

---
+++ scratch/gw/adaptive_cubic_spline.py
@@ -0,0 +1,132 @@
+class bicubic_bilinear_spline(object):
+  '''A class to provide support for bicubic spline interpolation falling back
+  on bilinear spline interpolation at the corners. To avoid the fallback, pad
+  the incoming parameter array appropriately.'''
+
+  def __init__(self, point_array):
+    self._point_array = point_array
+    self._ny, self._nx = point_array.focus()
+
+    from scitbx.array_family import flex
+    self._44 = flex.grid(4, 4)
+    self._aij_pij = self._make_aij_pij_coefficients_table()
+
+    return
+
+  def _make_aij_pij_coefficients_table(self):
+    from scitbx.array_family import flex
+    aij_pij = flex.double((
+      (0.00, 0.00, 0.00, 0.00, 0.00, 1.00, 0.00, 0.00,
+       0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00),
+      (0.00, 0.00, 0.00, 0.00, -0.50, 0.00, 0.50, 0.00,
+       0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00),
+      (0.00, 0.00, 0.00, 0.00, 1.00, -2.50, 2.00, -0.50,
+       0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00),
+      (0.00, 0.00, 0.00, 0.00, -0.50, 1.50, -1.50, 0.50,
+       0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00),
+      (0.00, -0.50, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00,
+       0.00, 0.50, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00),
+      (0.25, 0.00, -0.25, 0.00, 0.00, 0.00, 0.00, 0.00,
+       -0.25, 0.00, 0.25, 0.00, 0.00, 0.00, 0.00, 0.00),
+      (-0.50, 1.25, -1.00, 0.25, 0.00, 0.00, 0.00, 0.00,
+       0.50, -1.25, 1.00, -0.25, 0.00, 0.00, 0.00, 0.00),
+      (0.25, -0.75, 0.75, -0.25, 0.00, 0.00, 0.00, 0.00,
+       -0.25, 0.75, -0.75, 0.25, 0.00, 0.00, 0.00, 0.00),
+      (0.00, 1.00, 0.00, 0.00, 0.00, -2.50, 0.00, 0.00,
+       0.00, 2.00, 0.00, 0.00, 0.00, -0.50, 0.00, 0.00),
+      (-0.50, 0.00, 0.50, 0.00, 1.25, 0.00, -1.25, 0.00,
+       -1.00, 0.00, 1.00, 0.00, 0.25, 0.00, -0.25, 0.00),
+      (1.00, -2.50, 2.00, -0.50, -2.50, 6.25, -5.00, 1.25,
+       2.00, -5.00, 4.00, -1.00, -0.50, 1.25, -1.00, 0.25),
+      (-0.50, 1.50, -1.50, 0.50, 1.25, -3.75, 3.75, -1.25,
+       -1.00, 3.00, -3.00, 1.00, 0.25, -0.75, 0.75, -0.25),
+      (0.00, -0.50, 0.00, 0.00, 0.00, 1.50, 0.00, 0.00,
+       0.00, -1.50, 0.00, 0.00, 0.00, 0.50, 0.00, 0.00),
+      (0.25, 0.00, -0.25, 0.00, -0.75, 0.00, 0.75, 0.00,
+       0.75, 0.00, -0.75, 0.00, -0.25, 0.00, 0.25, 0.00),
+      (-0.50, 1.25, -1.00, 0.25, 1.50, -3.75, 3.00, -0.75,
+       -1.50, 3.75, -3.00, 0.75, 0.50, -1.25, 1.00, -0.25),
+      (0.25, -0.75, 0.75, -0.25, -0.75, 2.25, -2.25, 0.75,
+       0.75, -2.25, 2.25, -0.75, -0.25, 0.75, -0.75, 0.25)
+      ))
+
+    return aij_pij
+
+  def _aij(self, pij):
+    '''Derive coefficient matrix aij from 4 x 4 matrix pij'''
+
+    assert(pij.focus() == (4, 4))
+
+    # compute aij from pij by matrix operations on 16-vectors not 4x4 matrices
+
+    aij = aij_pij * (pij.as_1d())
+    aij.reshape(self._44)
+
+    return aij
+
+  def _evaluate_bilinear(self, x, y):
+    x0 = int(x)
+    y0 = int(y)
+    _x = x - x0
+    _y = y - y0
+
+    f00 = self._point_array[y0, x0]
+    f01 = self._point_array[y0 + 1, x0]
+    f10 = self._point_array[y0, x0 + 1]
+    f11 = self._point_array[y0 + 1, x0 + 1]
+
+    return f00 + (f10 - f00) * x + (f01 - f00) * y + \
+      (f00 - f10 - f01 + f11) * x * y
+
+  def _evaluate_bicubic(self, x, y):
+    x0 = int(x)
+    y0 = int(y)
+    _x = x - x0
+    _y = y - y0
+
+    from scitbx.array_family import flex
+    X = flex.double(flex.grid(4, 4))
+
+    for i in range(4):
+      for j in range(4):
+        X[i,j] = (_x ** j) * (_y ** j)
+
+    aij = self._aij(self._point_array[y0-1:y0+2,x0-1:x0+2])
+
+    return aij.as_1d.dot(X.as_1d)
+
+  def evaluate(self, x, y):
+    assert(x > 0 and x < self._nx)
+    assert(y > 0 and y < self._ny)
+
+    if x > 1 and x < (self._nx - 1) and \
+      y > 1 and y < (self._ny - 1):
+      return self._evaluate_bicubic(x, y)
+
+    return self._evaluate_bilinear(x, y)
+
+  def __call__(self, x, y):
+    return self.evaluate(x, y)
+
+def tst_bicubic_binear_spline():
+  from scitbx.array_family import flex
+  import math
+  cos_x_sin_y = flex.double(flex.grid(11, 11))
+  for i in range(11):
+    for j in range(11):
+      cos_x_sin_y[i,j] = math.sin(math.pi * i * 0.1) * math.cos(math.pi * j * 0.1)
+
+  bbs = bicubic_bilinear_spline(cos_x_sin_y)
+
+  import random
+
+  for j in range(1000):
+    x = random.random()
+    y = random.random()
+    cxsy = math.sin(math.pi * y * 0.1) * math.cos(math.pi * x * 0.1)
+    print bbs(x, y), cxsy
+
+
+
+if __name__ == '__main__':
+  tst_bicubic_binear_spline()
