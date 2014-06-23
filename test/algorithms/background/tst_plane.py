d0cecb7e5fdca5830867023a06320e5d5265819e
James Parkhurst <james.parkhurst@diamond.ac.uk>
2014-06-23 14:59:37 +0000

    Fixed for summation.

--- test/algorithms/background/tst_plane.py
+++ test/algorithms/background/tst_plane.py
@@ -26,7 +26,8 @@ class Test(object):
   def run(self):
     from dials.array_family import flex
     import cPickle as pickle
-    from math import floor
+    from math import floor, ceil
+    from dials.algorithms.shoebox import MaskCode
 
     # Read the data
     rtable = flex.reflection_table.from_pickle(self.reflection_filename)
@@ -48,6 +49,9 @@ class Test(object):
       nrx = rtable[i]['nrx']
       nry = rtable[i]['nry']
       bbox = rtable[i]['bbox']
+      I = rtable[i]['intensity.sum.value']
+      Ivar = rtable[i]['intensity.sum.variance']
+      lp = rtable[i]['lp']
       data = shoeboxes[i]
       mask = masks[i]
       fraction = 1.0
@@ -70,6 +74,7 @@ class Test(object):
         assert(abs(c1 - c2) < 0.1)
       except Exception:
         count += 1
+        continue
         #print "BG %d:(%.2f, %.2f, %.1f), (%.2f, %.2f, %.1f): %d" % \
           #(i, a1, b1, c1, a2, b2, c2, n)
         #print "X, Y: ", xdet, ydet
@@ -83,6 +88,61 @@ class Test(object):
         #print data.as_numpy_array().transpose()[::-1,::-1]
         #print mask.as_numpy_array().transpose()[::-1,::-1]
         #raise
+
+      background = data.as_double()
+      hy = background.all()[0] // 2
+      hx = background.all()[1] // 2
+      for jj in range(background.all()[0]):
+        for ii in range(background.all()[1]):
+          x = ii - hx
+          y = jj - hy
+          background[jj,ii] = a1 * x + b1 * y + c1
+
+      # Test the summation results. Edge reflections use profile fitted
+      # intensity in MOSFLM. Therefore ignore these. There also appears to be a
+      # some discrepancy with very low <= 0 reflections where an extra 0.5 is
+      # added. Not sure why this is so ignore these reflections as well.
+      from dials.algorithms.integration import integrate_by_summation
+      intensity = integrate_by_summation(data.as_double(), background, mask)
+      I2 = intensity.intensity()
+      Ivar2 = intensity.variance()
+      I1 = I
+      Ivar1 = Ivar
+      if mask.count(0) == 0 and mask.count(2) == 0 and I1 > 0:
+        try:
+          assert(abs(I1 - I2) < 1.0)
+          assert(abs(Ivar1 - Ivar2) < 1.0)
+        except Exception:
+          count += 1
+          print Ivar1, Ivar2
+          continue
+          #import numpy
+          #numpy.set_printoptions(precision=4, linewidth=200)
+          #print "# %d" % i
+          #print "I: %f, %f, %f" % (I2, I1, lp)
+          #print "DEBUG: ", c1 * 25
+          #print "PF: %f" % rtable[i]['intensity.prf.value']
+          #print "BG (%.4f, %.4f, %.4f), (%.2f, %.2f, %.1f): %d" % \
+            #(a1, b1, c1, a2, b2, c2, n)
+          #print "X, Y: ", xdet, ydet
+          #print "NX, NY: ", nx, ny
+          #print "NRX, NRY, NC", nrx, nry, nc
+          #print int(floor(xdet + 0.5)) - nx // 2, int(floor(ydet + 0.5)) - ny // 2
+          #print "BBOX: ", bbox
+          #print "N Outliers: ", model.noutlier()
+          #print "N Background: ", model.nbackground()
+          #print "Max DIff: ", model.maxdiff()
+          #temp = (mask == MaskCode.Valid | MaskCode.Foreground).as_1d().as_int()
+          #temp.resize(flex.grid(*data.all()))
+          #temp = temp.as_double()
+          #print data.as_numpy_array().transpose()[::-1,::-1]
+          #print (background * temp).as_numpy_array().transpose()[::-1,::-1]
+          #print mask.as_numpy_array().transpose()[::-1,::-1]
+          #print ((data.as_double() - background) * temp).as_numpy_array().transpose()[::-1,::-1]
+          #raise
+
+    # Only 1 should fail
+    print count
     assert(count == 1)
     print 'OK'
 
