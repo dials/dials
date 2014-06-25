39455d3f120d421fca57c2b53a4df0e35f94887a
Aaron S. Brewster <asbrewster@lbl.gov>
2014-06-25 19:02:16 +0000

    Add AnnAdapter to shoebox build

--- algorithms/shoebox/__init__.py
+++ algorithms/shoebox/__init__.py
@@ -1,4 +1,5 @@
 from __future__ import division
-from dials.array_family import flex
+from annlib_ext import AnnAdaptor # import dependency
+from dials.array_family import flex # import dependency
 from dials_algorithms_shoebox_ext import *
 from dials.algorithms.shoebox.masker import *
