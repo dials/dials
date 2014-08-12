55d443e4621807064af1eee9720b073b754dcbbf
Richard Gildea <richard.gildea@diamond.ac.uk>
2014-08-12 12:17:10 +0000

    partial support for multi-experiment json: option to choose which experiment to refine

--- command_line/refine_bravais_settings.py
+++ command_line/refine_bravais_settings.py
@@ -1,8 +1,10 @@
 from __future__ import division
 
 from libtbx.phil import command_line
+from libtbx.utils import Sorry
 import iotbx.phil
 from dials.util.command_line import Importer
+from dials.array_family import flex
 
 master_phil_scope = iotbx.phil.parse("""
 include scope dials.data.refinement.phil_scope
@@ -12,6 +14,8 @@ verbosity = 0
   .type = int(value_min=0)
 nproc = Auto
   .type = int(value_min=1)
+experiment_id = None
+  .type = int(value_min=0)
 """, process_includes=True)
 
 master_params = master_phil_scope.fetch().extract()
@@ -32,20 +36,23 @@ Parameters:
     usage_message += s.getvalue()
     raise Usage(usage_message)
   importer = Importer(args, check_format=False)
-  if len(importer.experiments) == 0:
-    print "No ExperimentList could be constructed"
-    return
-  elif len(importer.experiments) > 1:
-    raise RuntimeError("Only one Experiment can be processed at a time")
-  experiments = importer.experiments
-  experiment = experiments[0]
-  assert len(importer.reflections) == 1
-  reflections = importer.reflections[0]
   args = importer.unhandled_arguments
   cmd_line = command_line.argument_interpreter(master_params=master_phil_scope)
   working_phil = cmd_line.process_and_fetch(args=args)
   working_phil.show()
   params = working_phil.extract()
+  if len(importer.experiments) == 0:
+    print "No ExperimentList could be constructed"
+    return
+  elif len(importer.experiments) > 1 and params.experiment_id is None:
+    raise Sorry("More than one experiment present: set experiment_id to choose experiment.")
+  experiments = importer.experiments
+  if params.experiment_id is None:
+    params.experiment_id = 0
+  assert params.experiment_id < len(experiments)
+  experiment = experiments[params.experiment_id]
+  assert len(importer.reflections) == 1
+  reflections = importer.reflections[0]
 
   from dials.algorithms.indexing.symmetry \
        import refined_settings_factory_from_refined_triclinic
@@ -55,15 +62,16 @@ Parameters:
     .change_of_basis_op_to_primitive_setting()
   if experiment.crystal.get_space_group().n_ltr() > 1:
     effective_group = experiment.crystal.get_space_group()\
-      .build_derived_reflection_intensity_group(anomalous_flag=False)
+      .build_derived_reflection_intensity_group(anomalous_flag=True)
     sys_absent_flags = effective_group.is_sys_absent(
       reflections['miller_index'])
     reflections = reflections.select(~sys_absent_flags)
   experiment.crystal = experiment.crystal.change_basis(cb_op_to_primitive)
-  miller_indices = reflections.select(reflections['id'] == 0)['miller_index']
+  reflections = reflections.select(reflections['id'] == params.experiment_id)
+  miller_indices = reflections['miller_index']
   miller_indices = cb_op_to_primitive.apply(miller_indices)
-  reflections['miller_index'].set_selected(
-    reflections['id'] == 0, miller_indices)
+  reflections['miller_index'] = miller_indices
+  reflections['id'] = flex.int(len(reflections), 0)
 
   Lfat = refined_settings_factory_from_refined_triclinic(
     params, experiment, reflections, lepage_max_delta=params.lepage_max_delta,
