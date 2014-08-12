7201c97ac6844c99e1ed1a4a4a0721d925802f20
Richard Gildea <richard.gildea@diamond.ac.uk>
2014-08-12 14:02:44 +0000

    slightly more convenient behaviour of dials.export_mtz

--- command_line/export_mtz.py
+++ command_line/export_mtz.py
@@ -1,18 +1,33 @@
 from __future__ import division
 from dials.util.export_mtz import export_mtz
 
+def run(args):
+  if len(args) not in (2, 3):
+    from libtbx.utils import Usage
+    import libtbx.load_env
+    usage_message = """\
+%s integrated.pickle experiments.json [hklout.mtz] [options]\
+""" %libtbx.env.dispatcher_name
+    raise Usage(usage_message)
+
+  from dials.util.command_line import Importer
+  importer = Importer(args, check_format=False)
+  assert importer.experiments is not None and len(importer.experiments) > 0
+  assert importer.reflections is not None and len(importer.reflections) == 1
+
+  args = importer.unhandled_arguments
+  if len(args) == 0:
+    hklout = "hklout.mtz"
+  else:
+    assert len(args) == 1
+    hklout = args[0]
+
+  integrated_data = importer.reflections[0]
+  experiment_list = importer.experiments
+  m = export_mtz(integrated_data, experiment_list, hklout)
+  m.show_summary()
+
+
 if __name__ == '__main__':
   import sys
-  if len(sys.argv) != 4:
-    raise RuntimeError, '%s integrated.pickle experiments.json hklout.mtz'
-
-  import cPickle as pickle
-  from dials.array_family import flex
-  from dxtbx.model.experiment.experiment_list import ExperimentListFactory
-
-  integrated_data = pickle.load(open(sys.argv[1], 'rb'))
-  experiment_list = ExperimentListFactory.from_json_file(sys.argv[2],
-                                                         check_format=False)
-
-  m = export_mtz(integrated_data, experiment_list, sys.argv[3])
-  m.show_summary()
+  run(sys.argv[1:])
