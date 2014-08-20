2653126f03ecd7b5e8add042200aa0d9ecad6864
David Waterman <david.waterman@stfc.ac.uk>
2014-08-20 12:41:16 +0000

    fixes ImportErrors in various tests

--- algorithms/integration/__init__.py
+++ algorithms/integration/__init__.py
@@ -1,4 +1,5 @@
 from __future__ import division
+from cctbx import sgtbx # import dependency
 from dials.array_family import flex # import dependency
 from dials.model.data import AdjacencyList # import dependency
 from dials.algorithms.shoebox import *
