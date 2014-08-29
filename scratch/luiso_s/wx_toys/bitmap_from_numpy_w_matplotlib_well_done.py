d53c90b086c0c266e0c12b4b8e3421e49e030d95
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2014-08-29 16:23:06 +0000

    exercising

--- scratch/luiso_s/wx_toys/bitmap_from_numpy_w_matplotlib_well_done.py
+++ scratch/luiso_s/wx_toys/bitmap_from_numpy_w_matplotlib_well_done.py
@@ -1,4 +1,4 @@
-#import os, io, Image, PIL
+from __future__ import division
 import wx
 import numpy as np
 # set backend before importing pyplot
@@ -11,41 +11,47 @@ def GetBitmap_from_np_array(data2d):
 
   xmax = len(data2d[:,1])
   ymax = len(data2d[1,:])
-
-  #new_way = '''
-
-  plt.imshow(np.transpose(data2d), interpolation = "nearest")
-  lc_fig.set_size_inches(xmax / 2.0, ymax / 2.0)
-  ax = plt.Axes(lc_fig, [0., 0., 1., 1.])
-  ax.set_axis_off()
-  lc_fig.add_axes(ax)
-
-  ax.imshow(np.transpose(data2d), aspect = 'normal', interpolation = "nearest", cmap = 'hot')
+  vl_max = np.amax(data2d)
+  vl_min = np.amin(data2d)
+  d = vl_max - vl_min
+  vl_mid_low = vl_min + d / 3.0
+  vl_mid_hig = vl_max - d / 3.0
 
 
-  old_way = '''
+  print "vl_max =", vl_max
+  print "vl_min =", vl_min
+  print "d =", d
+  print "vl_mid_low =", vl_mid_low
+  print "vl_mid_hig =", vl_mid_hig
+
+
+
+  #vl_md = (vl_max + vl_min) / 2.0
 
   lc_fig.set_size_inches(xmax * .6, ymax * .6)
   ax = plt.Axes(lc_fig, [0., 0., 1., 1.])
   ax.set_axis_off()
   lc_fig.add_axes(ax)
-
   plt.imshow(np.transpose(data2d), interpolation = "nearest", cmap = 'hot')
-  #'''
-
 
   print "xmax =", xmax
   print "ymax =", ymax
   for xpos in range(xmax):
     for ypos in range(ymax):
       print "[xpos,ypos] =", [xpos,ypos]
-      #txt_dat = str(data2d[xpos,ypos])
 
       f_num = data2d[xpos,ypos]
       g = float("{0:.2f}".format(f_num))
       txt_dat = str(g)
+      if( g < vl_mid_low ):
+        clr_chr = 'yellow'
+      elif(g > vl_mid_hig):
+        clr_chr = 'black'
+        print "got black"
+      else:
+        clr_chr = 'blue'
       plt.annotate(txt_dat, xy = (xpos - 0.3, ypos + 0.3), xycoords = 'data'
-                   , color = 'green', size = 12.)
+                   , color = clr_chr, size = 12.)
 
   lc_fig.canvas.draw()
   width, height = lc_fig.canvas.get_width_height()
@@ -66,7 +72,7 @@ def build_np_img(width=64, height=64):
   for x in range(0, width):
     for y in range(0, height):
       data2d[x,y] = np.sqrt(x*x + y*y)
-  #data2d[width/4:width*3/4,height/4:height*3/4] = 0
+
   print "data2d.max =", data2d.max()
   return data2d
 
@@ -87,7 +93,7 @@ class MyFrame(wx.Frame):
     # Attributes
     self.panel = wx.Panel(self)
 
-    data2d = build_np_img(width=5, height=4)
+    data2d = build_np_img(width=18, height=18)
     print data2d
     bitmap = GetBitmap_from_np_array(data2d)
 
