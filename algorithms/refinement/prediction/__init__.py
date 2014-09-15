70cd40f653c43d39a00f7c08bc801b60ab218328
David Waterman <david.waterman@stfc.ac.uk>
2014-09-15 11:44:51 +0000

    remove Reflection, ReflectionList

--- algorithms/refinement/prediction/__init__.py
+++ algorithms/refinement/prediction/__init__.py
@@ -1,4 +1,3 @@
 from __future__ import division
 from managed_predictors import ScansRayPredictor # import dependency
-from managed_predictors import StillsRayPredictor # import dependency
 from managed_predictors import ExperimentsPredictor # import dependency
--- algorithms/refinement/prediction/managed_predictors.py
+++ algorithms/refinement/prediction/managed_predictors.py
@@ -22,8 +22,6 @@ from math import pi
 from scitbx import matrix
 from dials.algorithms.spot_prediction import RayPredictor
 
-from dials.model.data import Reflection, ReflectionList
-
 from dials.algorithms.spot_prediction import ScanStaticReflectionPredictor
 from dials.algorithms.spot_prediction import StillsReflectionPredictor
 
@@ -64,56 +62,6 @@ class ScansRayPredictor(object):
     UB_ = UB if UB else self._UBs[experiment_id]
 
     return self._ray_predictors[experiment_id](hkl, UB_)
-
-class StillsRayPredictor(object):
-  """
-  Predict for a relp based on the current states of models of the
-  experimental geometry. Here we assume the crystal UB already puts hkl
-  in reflecting position, so no rotation is required.
-
-  Generally, this assumption is not true: most relps are not touching the
-  Ewald sphere on a still image, but are observed due to their finite
-  mosaicity and the finite width of the Ewald sphere. Rather than employing
-  a complicated polychromatic and mosaic model, here we take a naive
-  approach, which is likely to introduce (small?) errors in the direction of
-  predicted rays.
-
-  """
-
-  def __init__(self, experiments):
-    """Construct by linking to instances of experimental model classes"""
-
-    self._experiments = experiments
-    self.update()
-
-  def update(self):
-    """Cache s0 and UB"""
-
-    self._s0s = [matrix.col(e.beam.get_s0()) for e in self._experiments]
-    self._s0_lengths = [s0.length() for s0 in self._s0s]
-    self._UBs = [e.crystal.get_U() * e.crystal.get_B() for e in self._experiments]
-
-  def predict(self, hkl, experiment_id=0):
-    """Predict for hkl under the assumption it is in reflecting position"""
-
-    s0 = self._s0s[experiment_id]
-    s0_length = self._s0_lengths[experiment_id]
-    r = self._UBs[experiment_id] * matrix.col(hkl)
-    s1 = (s0 + r).normalize() * s0_length
-
-    # create the Reflections and set properties. The relp is
-    # neither entering nor exiting the Ewald sphere, but we need
-    # both to ensure we match whatever this flag is set to for
-    # the observation.
-    ref1, ref2 = Reflection(hkl), Reflection(hkl)
-    ref1.beam_vector, ref2.beam_vector = s1, s1
-    ref1.rotation_angle, ref2.rotation_angle = 0.0, 0.0
-    ref1.entering, ref2.entering = True, False
-
-    rl = ReflectionList(2)
-    rl[0] = ref1
-    rl[1] = ref2
-    return rl
 
 class ExperimentsPredictor(object):
   """
