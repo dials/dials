c15e3216bfb01c30f866f11b2581895accd56897
James Parkhurst <james.parkhurst@diamond.ac.uk>
2014-09-16 11:11:36 +0000

    Removed old redundant stuff

--- algorithms/integration/__init__.py
+++ algorithms/integration/__init__.py
@@ -4,9 +4,7 @@ from dials.array_family import flex # import dependency
 from dials.model.data import AdjacencyList # import dependency
 from dials.algorithms.shoebox import *
 from dials_algorithms_integration_ext import *
-from reflection_extractor import *
 from integrator import *
 from integrator_stills import *
 from summation import *
-from call_mosflm_2d import *
 from profile_fitting_reciprocal_space import *
--- algorithms/integration/boost_python/integration_ext.cc
+++ algorithms/integration/boost_python/integration_ext.cc
@@ -15,24 +15,16 @@ namespace dials { namespace algorithms { namespace boost_python {
 
   using namespace boost::python;
 
-  void export_luiso_s_2d_integration();
   void export_summation();
   void export_profile_fitting_reciprocal_space();
-  void export_flattened_2d_profile_fitting();
-  void export_interpolate_profile2d();
-  void export_shoebox_flattener();
   void export_preprocessor();
   void export_interface();
   void export_corrections();
 
   BOOST_PYTHON_MODULE(dials_algorithms_integration_ext)
   {
-    export_luiso_s_2d_integration();
     export_summation();
     export_profile_fitting_reciprocal_space();
-    export_flattened_2d_profile_fitting();
-    export_interpolate_profile2d();
-    export_shoebox_flattener();
     export_preprocessor();
     export_interface();
     export_corrections();
