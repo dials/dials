b154eef7d36359d0fbcc11a165371d3a54f40682
James Parkhurst <james.parkhurst@diamond.ac.uk>
2014-09-17 08:45:52 +0000

    Delete *.pyc files from dials/extensions when doing libtbx.refresh

--- libtbx_refresh.py
+++ libtbx_refresh.py
@@ -9,3 +9,15 @@ try:
   run()
 except Exception, e:
   pass
+
+try:
+  from glob import glob
+  import os
+  filenames = glob("extensions/*.pyc")
+  if len(filenames) > 0:
+    print "Cleaning up 'dials/extensions':"
+    for filename in glob("extensions/*.pyc"):
+      print " Deleting %s" % filename
+      os.remove(filename)
+except Exception, e:
+  pass
