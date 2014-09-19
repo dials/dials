66192765953163ff29f05d8a4de57e4fea489c8d
James Parkhurst <james.parkhurst@diamond.ac.uk>
2014-09-19 10:14:57 +0000

    First commit of image space fitting algorithm

---
+++ algorithms/integration/fit/fit.h
@@ -0,0 +1,38 @@
+/*
+ * fitrs.h
+ *
+ *  Copyright (C) 2013 Diamond Light Source
+ *
+ *  Author: James Parkhurst
+ *
+ *  This code is distributed under the BSD license, a copy of which is
+ *  included in the root directory of this package.
+ */
+
+#ifndef DIALS_ALGORITHMS_INTEGRATION_FIT_FIT_H
+#define DIALS_ALGORITHMS_INTEGRATION_FIT_FIT_H
+
+#include <dials/array_family/reflection_table.h>
+
+namespace dials { namespace algorithms {
+
+  /**
+   * A class to perform image space profile fitting.
+   */
+  class ImageSpaceProfileFitting {
+  public:
+
+    ImageSpaceProfileFitting() {}
+
+    void exec(af::reflection_table data) const {
+
+
+
+
+    }
+  };
+
+}} // namespace dials::algorithms
+
+
+#endif // DIALS_ALGORITHMS_INTEGRATION_FIT_FIT_H
