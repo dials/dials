2b08dd6796ad9bad636f6dc1a676d8e1d842a467
James Parkhurst <james.parkhurst@diamond.ac.uk>
2014-09-19 10:15:43 +0000

    Compute the profile for each experiment independantly

--- algorithms/integration/fit_image/fit.h
+++ algorithms/integration/fit_image/fit.h
@@ -27,6 +27,7 @@ namespace dials { namespace algorithms {
   using dxtbx::model::Detector;
   using dxtbx::model::Goniometer;
   using dxtbx::model::Scan;
+  using dials::af::ReferenceSpot;
   using dials::af::DontIntegrate;
   using dials::af::IntegratedPrf;
   using dials::model::Shoebox;
@@ -77,20 +78,23 @@ namespace dials { namespace algorithms {
    * A class to compute a single reference profile.
    */
   template <typename FloatType>
-  class SingleReferenceProfile {
+  class SingleProfileLearner {
   public:
 
+    typedef FloatType float_type;
     typedef af::versa< FloatType, af::c_grid<3> > profile_type;
 
-    SingleReferenceProfile(af::const_ref<Spec> spec, af::reflection_table data) {
+    SingleProfileLearner(const Spec spec)
+      : spec_(spec) {}
+
+    void add(const Shoebox<> &sbox, const vec3<double> &xyz) {
 
     }
 
     profile_type get(
-        std::size_t id,
         std::size_t panel,
         vec3<double> xyz,
-        int6 bbox) {
+        int6 bbox) const {
       DIALS_ASSERT(bbox[1] > bbox[0]);
       DIALS_ASSERT(bbox[3] > bbox[2]);
       DIALS_ASSERT(bbox[5] > bbox[4]);
@@ -98,11 +102,80 @@ namespace dials { namespace algorithms {
       std::size_t ysize = bbox[3] - bbox[2];
       std::size_t zsize = bbox[5] - bbox[4];
       af::c_grid<3> accessor(zsize, ysize, xsize);
-
-
       return profile_type(accessor, 1);
     }
 
+  private:
+
+    Spec spec_;
+  };
+
+
+  /**
+   * A class to compute a single reference profile per experiment.
+   */
+  template <typename FloatType>
+  class MultiExpSingleProfileLearner {
+  public:
+
+    typedef FloatType float_type;
+    typedef SingleProfileLearner<FloatType> learner_type;
+    typedef typename learner_type::profile_type profile_type;
+
+    /**
+     * Compute the reference profiles.
+     * @param spec The list of experiment specifications
+     * @param data The list of reflections.
+     */
+    MultiExpSingleProfileLearner(
+        af::const_ref<Spec> spec,
+        af::reflection_table data) {
+
+      // Check the input
+      DIALS_ASSERT(spec.size() > 0);
+      DIALS_ASSERT(data.size() > 0);
+
+      // Create the array of learners
+      learner_.reserve(spec.size());
+      for (std::size_t i = 0; i < spec.size(); ++i) {
+        learner_.push_back(learner_type(spec[i]));
+      }
+
+      // Get the data we need
+      af::const_ref<std::size_t>    id      = data["id"];
+      af::const_ref< Shoebox<> >    shoebox = data["shoebox"];
+      af::const_ref< vec3<double> > xyzcal  = data["xyzcal.px"];
+      af::ref<std::size_t>          flags   = data["flags"];
+
+      // Loop through all the reflections
+      for (std::size_t i = 0; i < data.size(); ++i) {
+        DIALS_ASSERT(id[i] < learner_.size());
+        if (flags[i] & ReferenceSpot) {
+          learner_[id[i]].add(shoebox[i], xyzcal[i]);
+        }
+      }
+    }
+
+    /**
+     * Get the reference profile for the reflection.
+     * @param id The experiment ID
+     * @param panel The panel number
+     * @param xyz The calculated xyz pixel coordinate
+     * @param bbox The bounding box
+     * @returns The reference profile for the reflection.
+     */
+    profile_type get(
+        std::size_t id,
+        std::size_t panel,
+        vec3<double> xyz,
+        int6 bbox) const {
+      DIALS_ASSERT(id < learner_.size());
+      return learner_[id].get(panel, xyz, bbox);
+    }
+
+  private:
+
+    std::vector<learner_type> learner_;
   };
 
 
@@ -113,23 +186,32 @@ namespace dials { namespace algorithms {
   public:
 
     typedef Shoebox<>::float_type float_type;
-    typedef SingleReferenceProfile<float_type> reference_type;
-    typedef reference_type::profile_type profile_type;
+    typedef MultiExpSingleProfileLearner<float_type> reference_learner_type;
+    typedef reference_learner_type::profile_type profile_type;
     typedef af::versa< bool, af::c_grid<3> > mask_type;
     typedef ProfileFitting<float_type> fitting_type;
 
     ImageSpaceProfileFitting() {}
 
+    /**
+     * Add some experiment data
+     * @param spec The experiment specification
+     */
     void add(const Spec &spec) {
       spec_.push_back(spec);
     }
 
+    /**
+     * Perform the profile fitting on the input data
+     * @param data The reflection data
+     */
     void execute(af::reflection_table data) const {
 
       // Check we have experimental setup data
       DIALS_ASSERT(spec_.size() > 0);
 
       // Check the input contains expected fields
+      DIALS_ASSERT(data.size() > 0);
       DIALS_ASSERT(data.is_consistent());
       DIALS_ASSERT(data.contains("id"));
       DIALS_ASSERT(data.contains("shoebox"));
@@ -137,7 +219,7 @@ namespace dials { namespace algorithms {
       DIALS_ASSERT(data.contains("flags"));
 
       // Compute the reference profile
-      reference_type reference(spec_.const_ref(), data);
+      reference_learner_type reference(spec_.const_ref(), data);
 
       // Get the data we need
       af::const_ref<std::size_t>    id      = data["id"];
