6a8169b75f36fe0dd6792201295013371ecd2d1a
James Parkhurst <james.parkhurst@diamond.ac.uk>
2014-09-25 09:30:51 +0000

    Fixed input to image_viewer

--- command_line/image_viewer.py
+++ command_line/image_viewer.py
@@ -39,30 +39,64 @@ class Script(object):
 
 if __name__ == '__main__':
   import sys
-  from dials.util.command_line import Importer
-  args = sys.argv[1:]
-  if len(args) == 0:
-    from libtbx.utils import Usage
-    import libtbx.load_env
-    usage_message = """\
-%s datablock.json reflections.pickle""" %libtbx.env.dispatcher_name
-    raise Usage(usage_message)
-  importer = Importer(args, check_format=True)
-  crystals = None
-  if importer.datablocks is not None and len(importer.datablocks) == 1:
-    imagesets = importer.datablocks[0].extract_imagesets()
-  elif importer.datablocks is not None and len(importer.datablocks) > 1:
-    raise RuntimeError("Only one DataBlock can be processed at a time")
-  elif len(importer.experiments.imagesets()) > 0:
-    imagesets = importer.experiments.imagesets()[:1]
-    crystals = importer.experiments.crystals()
+
+  from dials.util.options import OptionParser
+  from dials.util.options import flatten_datablocks
+  from dials.util.options import flatten_experiments
+  from dials.util.options import flatten_reflections
+  import libtbx.load_env
+  usage_message = """
+    %s datablock.json reflections.pickle
+  """ %libtbx.env.dispatcher_name
+  parser = OptionParser(
+    usage=usage_message,
+    read_datablocks=True,
+    read_experiments=True,
+    read_reflections=True)
+  params, options = parser.parse_args(show_diff_phil=True)
+  datablocks = flatten_datablocks(params.input.datablock)
+  experiments = flatten_experiments(params.input.experiments)
+  reflections = flatten_reflections(params.input.reflections)
+
+  if len(datablocks) == 0 and len(experiments) == 0:
+    parser.print_help()
+    exit(0)
+
+  if len(datablocks) > 0:
+    assert(len(datablocks) == 1)
+    imagesets = datablocks[0].extract_imagesets()
+    crystals = None
+  elif len(experiments.imagesets()) > 0:
+    assert(len(experiments.imagesets()) == 1)
+    imagesets = experiments.imagesets()
+    crystals = experiments.crystals()
   else:
     raise RuntimeError("No imageset could be constructed")
-  assert len(importer.unhandled_arguments) == 0
 
-  reflections = importer.reflections
-  if reflections is None:
-    reflections = []
+  # from dials.util.command_line import Importer
+  # args = sys.argv[1:]
+  # if len(args) == 0:
+  #   from libtbx.utils import Usage
+  #   import libtbx.load_env
+  #   usage_message = """\
+# %s datablock.json reflections.pickle""" %libtbx.env.dispatcher_name
+  #   raise Usage(usage_message)
+  # importer = Importer(args, check_format=True)
+  # crystals = None
+  # if importer.datablocks is not None and len(importer.datablocks) == 1:
+  #   imagesets = importer.datablocks[0].extract_imagesets()
+  # elif importer.datablocks is not None and len(importer.datablocks) > 1:
+  #   raise RuntimeError("Only one DataBlock can be processed at a time")
+  # elif len(importer.experiments.imagesets()) > 0:
+  #   imagesets = importer.experiments.imagesets()[:1]
+  #   crystals = importer.experiments.crystals()
+  # else:
+  #   raise RuntimeError("No imageset could be constructed")
+  # assert len(importer.unhandled_arguments) == 0
+
+  # reflections = importer.reflections
+  # if reflections is None:
+  #   reflections = []
 
   runner = Script(
       reflections=reflections,
