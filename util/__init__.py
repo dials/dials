5ba2909ffdb644360411a5bb406253cd346003ca
Graeme Winter <graeme.winter@gmail.com>
2014-09-26 07:30:55 +0000

    Slightly more boring but perhaps more helpful error reporting; kept original definitions but clobbered them... now gives:
    Please report this error to dials-dev@cci.lbl.gov: Uninterpretable command line argument: /tmp/xyz/*cbf
    '

--- util/__init__.py
+++ util/__init__.py
@@ -11,6 +11,31 @@
 
 from __future__ import division
 
+class UsefulError(RuntimeError):
+  '''Error message to direct user to report to dials developers.'''
+
+  def __init__(self, message=''):
+    if message:
+      text = 'Error: "%s" - please report this to dials-dev@cci.lbl.gov' % message
+    else:
+      text = 'An error has occurred with no message'
+
+    RuntimeError.__init__(self, text)
+    return
+
+def usefulraiser(e):
+  ''' Function to re-raise an exception with a useful message. '''
+
+  text = 'Please report this error to dials-dev@cci.lbl.gov:'
+
+  if len(e.args) == 0:
+    e.args = (text,)
+  elif len(e.args) == 1:
+    e.args = (text + ' ' + str(e.args[0]),)
+  else:
+    e.args = (text,) + e.args
+
+  raise
 
 class HalError(RuntimeError):
   def __init__(self, string=''):
@@ -28,7 +53,6 @@ class HalError(RuntimeError):
 
     # Init base class
     RuntimeError.__init__(self, text)
-
 
 def halraiser(e):
   ''' Function to re-raise an exception with a Hal message. '''
@@ -53,3 +77,7 @@ def halraiser(e):
 
   # Reraise the exception
   raise
+
+# clobber existing definitions
+HalError = UsefulError
+halraiser = usefulraiser
