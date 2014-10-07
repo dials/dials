dd155b103b638612129eb51854cce9e2a2642631
David Waterman <david.waterman@stfc.ac.uk>
2014-10-07 10:23:30 +0000

    ought to add STFC here

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -53,7 +53,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'DIALS'
-copyright = u'2014, Diamond Light Source'
+copyright = u'2014, Diamond Light Source and STFC'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
