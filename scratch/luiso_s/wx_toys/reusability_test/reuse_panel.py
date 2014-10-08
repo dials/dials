da7d6e81903ba663b581673c4d563acb06106f2f
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2014-10-08 14:35:11 +0000

    first exercise of two panels but one of them is a scrollable panel

--- scratch/luiso_s/wx_toys/reusability_test/reuse_panel.py
+++ scratch/luiso_s/wx_toys/reusability_test/reuse_panel.py
@@ -1,8 +1,28 @@
 import os
 import wx
 import numpy as np
-
+import wx.lib.scrolledpanel as scroll_pan
 from bitmap_from_numpy import GetBitmap_from_np_array
+
+class multi_img(scroll_pan.ScrolledPanel):
+    def __init__(self, outer_frame):
+        scroll_pan.ScrolledPanel.__init__(self, outer_frame)
+
+        print "Hi from scroll_pan"
+
+        data2d = np.arange( 7 * 6, dtype = 'uintc').reshape( 7, 6 )
+        bmp = GetBitmap_from_np_array(data2d)
+        local_bmp = wx.StaticBitmap(self, bitmap=bmp)
+
+        data2d_ag = np.arange( 6 * 7, dtype = 'uintc').reshape( 6, 7 )
+        bmp_ag = GetBitmap_from_np_array(data2d_ag)
+        local_bmp_ag = wx.StaticBitmap(self, bitmap=bmp_ag)
+
+        self.top_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.top_sizer.Add(local_bmp, 1, wx.RIGHT)
+        self.top_sizer.Add(local_bmp_ag, 1, wx.LEFT)
+        self.SetSizer(self.top_sizer)
+        self.SetupScrolling()
 
 class iner_panel(wx.Panel):
     def __init__(self, outer_frame):
@@ -30,7 +50,7 @@ class MyFrame(wx.Frame):
 
         # Attributes
         self.panel_01 = iner_panel(self)
-        self.panel_02 = iner_panel(self)
+        self.panel_02 = multi_img(self)
         # Layout
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(self.panel_01, 1, wx.EXPAND)
