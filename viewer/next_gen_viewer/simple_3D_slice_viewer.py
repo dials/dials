7314bdfd9361d52ae1837a26e6718694d2a0b30d
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2014-10-08 21:43:32 +0000

    now that the simple 3d slice viewer behaves better we can replace the old one

--- viewer/next_gen_viewer/simple_3D_slice_viewer.py
+++ viewer/next_gen_viewer/simple_3D_slice_viewer.py
@@ -8,13 +8,15 @@
 #
 #  This code is distributed under the BSD license, a copy of which is
 #  included in the root directory of this package.
+
+
 from __future__ import division
 
 from dials.array_family import flex
 from from_flex_to_wxbitmap import wxbitmap_convert
 import wx
+import numpy as np
 import wx.lib.scrolledpanel as scroll_pan
-
 
 class show_3d(object):
   def __init__(self, data_xyz_in):
@@ -25,41 +27,68 @@ class show_3d(object):
 
 class show_3d_wx_app(wx.App):
   def OnInit(self):
-    self.frame = MyFrame(None, title="Bitmaps")
+    self.frame = My_3d_flex_arr_frame(None, title="Bitmaps")
     return True
 
 
   def in_lst(self, lst):
-    self.frame.set_bmp(lst)
+    self.frame.ini_n_intro(lst)
     self.SetTopWindow(self.frame)
     self.frame.Show()
 
+class multi_img_scrollable(scroll_pan.ScrolledPanel):
+    def __init__(self, outer_frame, bmp_lst_in):
+        scroll_pan.ScrolledPanel.__init__(self, outer_frame)
 
-class MyFrame(wx.Frame):
+        my_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        for bmp_lst in bmp_lst_in:
+          local_bitmap = wx.StaticBitmap(self, bitmap = bmp_lst)
+          my_sizer.Add(local_bitmap, 0, wx.LEFT | wx.ALL, 3)
+
+        self.SetSizer(my_sizer)
+        self.SetupScrolling()
+
+class buttons_panel(wx.Panel):
+    def __init__(self, outer_frame):
+        wx.Panel.__init__(self, outer_frame)
+
+        Hide_I_Button = wx.Button(self, label="Hide I")
+        Hide_I_Button.Bind(wx.EVT_BUTTON, self.OnShwIBut)
+
+        Show_I_Button = wx.Button(self, label="Show I")
+        Show_I_Button.Bind(wx.EVT_BUTTON, self.OnHidIBut)
+        my_sizer = wx.BoxSizer(wx.VERTICAL)
+        my_sizer.Add(Show_I_Button, 0, wx.LEFT | wx.ALL,8)
+        my_sizer.Add(Hide_I_Button, 0, wx.LEFT | wx.ALL,8)
+        my_sizer.SetMinSize((90, 250))
+
+        self.SetSizer(my_sizer)
+
+    def OnShwIBut(self, event):
+        print "OnShwIBut"
+
+    def OnHidIBut(self, event):
+        print "OnHidIBut"
+
+class My_3d_flex_arr_frame(wx.Frame):
   def __init__(self, parent, id = wx.ID_ANY, title = "",
                pos=wx.DefaultPosition, size=wx.DefaultSize,
                style = wx.DEFAULT_FRAME_STYLE):
-    super(MyFrame, self).__init__(parent, id, title,
+    super(My_3d_flex_arr_frame, self).__init__(parent, id, title,
                                   pos, size, style)
 
-  def set_bmp(self, bmp_in):
-    scrl_panel = scroll_pan.ScrolledPanel(self)
+  def ini_n_intro(self, bmp_lst_in):
 
-    img_sizer = wx.BoxSizer(wx.HORIZONTAL)
-    for bmp_lst in bmp_in:
-      local_bitmap = wx.StaticBitmap(scrl_panel, bitmap = bmp_lst)
-      img_sizer.Add(local_bitmap, 0, wx.LEFT | wx.ALL,3)
+    # Attributes
+    self.panel_01 = buttons_panel(self)
+    self.panel_02 = multi_img_scrollable(self, bmp_lst_in)
+    # Layout
+    sizer = wx.BoxSizer(wx.HORIZONTAL)
+    sizer.Add(self.panel_01, 0, wx.EXPAND)
+    sizer.Add(self.panel_02, 1, wx.EXPAND)
+    self.SetSizer(sizer)
 
-
-    ToggleButton = wx.Button(scrl_panel, label="Toggle Show I")
-    img_sizer.Add(ToggleButton, 0, wx.LEFT | wx.ALL,8)
-    ToggleButton.Bind(wx.EVT_BUTTON, self.OnTglBut)
-
-    scrl_panel.SetSizer(img_sizer)
-    scrl_panel.SetupScrolling()
-
-  def OnTglBut(self, event):
-    print "OnTstBut"
+    self.Show(True)
 
 if(__name__ == "__main__"):
   size_xy = 7
