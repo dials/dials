29aa2530187b25e3ef9895f8ee3f5f89009e2610
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2014-10-10 14:03:25 +0000

    this intermediate version is capable to update the bitmaps, it just puts the same bitmaps again

--- viewer/next_gen_viewer/simple_3D_slice_viewer.py
+++ viewer/next_gen_viewer/simple_3D_slice_viewer.py
@@ -41,7 +41,9 @@ class multi_img_scrollable(scroll_pan.ScrolledPanel):
     def __init__(self, outer_frame, bmp_lst_in):
       super(multi_img_scrollable, self).__init__(outer_frame)
 
-      self.set_scroll_content(bmp_lst_in)
+      self.local_bmp_lst = bmp_lst_in
+
+      self.set_scroll_content(self.local_bmp_lst)
 
       self.Bind(wx.EVT_MOUSEWHEEL, self.OnMouseWheel)
       self.SetupScrolling()
@@ -49,22 +51,25 @@ class multi_img_scrollable(scroll_pan.ScrolledPanel):
 
     def set_scroll_content(self, bmp_lst_in):
 
-      my_sizer = wx.BoxSizer(wx.HORIZONTAL)
+      self.my_img_sizer = wx.BoxSizer(wx.HORIZONTAL)
       for bmp_lst in bmp_lst_in:
         local_bitmap = wx.StaticBitmap(self, bitmap = bmp_lst)
-        my_sizer.Add(local_bitmap, 0, wx.LEFT | wx.ALL, 3)
+        self.my_img_sizer.Add(local_bitmap, 0, wx.LEFT | wx.ALL, 3)
 
-      self.SetSizer(my_sizer)
-
-      to_be_used_soon = '''
-        for child in panel.GetChildren():
-          child.Destroy()
-      '''
+      self.SetSizer(self.my_img_sizer)
 
 
     def OnMouseWheel(self, event):
-        print "Weel event"
-        print "event.GetWheelRotation() =", event.GetWheelRotation()
+      print "Weel event"
+      print "event.GetWheelRotation() =", event.GetWheelRotation()
+
+
+    def Waipeando(self):
+      for child in self.GetChildren():
+        child.Destroy()
+      self.set_scroll_content(self.local_bmp_lst)
+      self.Layout()
+      self.Refresh()
 
 
 class buttons_panel(wx.Panel):
@@ -78,12 +83,12 @@ class buttons_panel(wx.Panel):
         Show_I_Button = wx.Button(self, label="Show I")
         Show_I_Button.Bind(wx.EVT_BUTTON, self.OnHidIBut)
 
-        my_sizer = wx.BoxSizer(wx.VERTICAL)
-        my_sizer.Add(Show_I_Button, 0, wx.LEFT | wx.ALL,8)
-        my_sizer.Add(Hide_I_Button, 0, wx.LEFT | wx.ALL,8)
-        my_sizer.SetMinSize((90, 250))
+        self.my_sizer = wx.BoxSizer(wx.VERTICAL)
+        self.my_sizer.Add(Show_I_Button, 0, wx.LEFT | wx.ALL,8)
+        self.my_sizer.Add(Hide_I_Button, 0, wx.LEFT | wx.ALL,8)
+        self.my_sizer.SetMinSize((90, 250))
 
-        self.SetSizer(my_sizer)
+        self.SetSizer(self.my_sizer)
 
     def OnShwIBut(self, event):
         print "OnShwIBut"
@@ -98,6 +103,7 @@ class My_3d_flex_arr_frame(wx.Frame):
                style = wx.DEFAULT_FRAME_STYLE):
     super(My_3d_flex_arr_frame, self).__init__(parent, id, title,
                                   pos, size, style)
+    self.show_nums = True
 
   def ini_n_intro(self, bmp_lst_in):
 
@@ -112,7 +118,8 @@ class My_3d_flex_arr_frame(wx.Frame):
 
     self.Show(True)
   def tst(self):
-    print "testing"
+    self.show_nums = False
+    self.panel_02.Waipeando()
 
 if(__name__ == "__main__"):
   size_xy = 6
