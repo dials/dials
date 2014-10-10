3d2e48af5dec00f692a414ec377eabfb0f68f427
James Parkhurst <james.parkhurst@diamond.ac.uk>
2014-10-10 14:32:52 +0000

    Removed importer from export mtz

--- command_line/export_mtz.py
+++ command_line/export_mtz.py
@@ -2,29 +2,36 @@ from __future__ import division
 from dials.util.export_mtz import export_mtz
 
 def run(args):
-  if len(args) not in (2, 3):
-    from libtbx.utils import Usage
-    import libtbx.load_env
-    usage_message = """\
-%s integrated.pickle experiments.json [hklout.mtz] [options]\
-""" %libtbx.env.dispatcher_name
-    raise Usage(usage_message)
 
-  from dials.util.command_line import Importer
-  importer = Importer(args, check_format=False)
-  assert importer.experiments is not None and len(importer.experiments) > 0
-  assert importer.reflections is not None and len(importer.reflections) == 1
+  import libtbx.load_env
+  from dials.util.options import OptionParser
+  from dials.util.options import flatten_experiments
+  from dials.util.options import flatten_reflections
+  from libtbx.phil import parse
+  phil_scope = parse('''
+    hklout = hklout.mtz
+      .type = str
+      .help = "The output MTZ file"
+  ''')
 
-  args = importer.unhandled_arguments
-  if len(args) == 0:
-    hklout = "hklout.mtz"
-  else:
-    assert len(args) == 1
-    hklout = args[0]
+  usage = '%s integrated.pickle experiments.json [hklout.mtz] [options]' % (
+              libtbx.env.dispatcher_name)
 
-  integrated_data = importer.reflections[0]
-  experiment_list = importer.experiments
-  m = export_mtz(integrated_data, experiment_list, hklout)
+  parser = OptionParser(
+    usage = usage,
+    read_experiments=True,
+    read_reflections=True,
+    check_format=False,
+    phil=phil_scope)
+  params, options = parser.parse_args(show_diff_phil=True)
+  experiments = flatten_experiments(params.input.experiments)
+  reflections = flatten_reflections(params.input.reflections)
+  assert(len(experiments) > 0)
+  assert(len(reflections) == 1)
+
+  integrated_data = reflections[0]
+  experiment_list = experiments
+  m = export_mtz(integrated_data, experiment_list, params.hklout)
   m.show_summary()
 
 
