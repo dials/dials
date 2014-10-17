a1fb0d540d19287568193d560cb00fa817772867
Aaron S. Brewster <asbrewster@lbl.gov>
2014-10-17 17:39:58 +0000

    Add obs vs. preds display, if a reflection table is passed in.

--- scratch/asb/display_detectors.py
+++ scratch/asb/display_detectors.py
@@ -2,15 +2,16 @@ from __future__ import division
 from dials.util.options import OptionParser
 from dials.util.options import flatten_datablocks
 from dials.util.options import flatten_experiments
+from dials.util.options import flatten_reflections
 from matplotlib import pyplot as plt
 from matplotlib.patches import Polygon
 from scitbx.matrix import col
 
-def display_detectors(detectors, save=False):
+def display_detectors(detectors, save=False, reflections=None):
   fig = plt.figure()
   ax = fig.add_subplot(111, aspect='equal')
 
-  for detector in detectors:
+  for d_id, detector in enumerate(detectors):
     for i, panel in enumerate(detector):
       size = panel.get_image_size()
       p0 = col(panel.get_pixel_lab_coord((0,0)))
@@ -24,6 +25,16 @@ def display_detectors(detectors, save=False):
 
       ax.add_patch(Polygon((p0[0:2],p1[0:2],p2[0:2],p3[0:2]), closed=True, color='green', fill=False, hatch='/'))
       ax.annotate(i, vcen[0:2])
+
+      if reflections is not None:
+        refs = reflections.select(reflections['id'] == d_id)
+        obs = [detector[refs[i]['panel']].get_pixel_lab_coord(refs[i]['xyzobs.px.value'][0:2]) for i in xrange(len(refs))]
+        cal = [detector[refs[i]['panel']].get_pixel_lab_coord(refs[i]['xyzcal.px'][0:2]) for i in xrange(len(refs))]
+
+        ax.scatter([pt[0] for pt in obs],
+                   [pt[1] for pt in obs], c='b')
+        ax.scatter([pt[0] for pt in cal],
+                   [pt[1] for pt in cal], c='r')
 
   ax.set_xlim((-100, 100))
   ax.set_ylim((-100, 100))
@@ -50,6 +61,7 @@ class Script(object):
     self.parser = OptionParser(
       read_experiments=True,
       read_datablocks=True,
+      read_reflections=True,
       read_datablocks_from_images=True,
       check_format=False)
 
@@ -57,13 +69,14 @@ class Script(object):
     params, options = self.parser.parse_args(show_diff_phil=True)
     datablocks  = flatten_datablocks(params.input.datablock)
     experiments = flatten_experiments(params.input.experiments)
+    reflections = flatten_reflections(params.input.reflections)[0]
 
     all_detectors = []
     for db in datablocks:
       all_detectors.extend(db.unique_detectors())
 
     all_detectors.extend(experiments.detectors())
-    display_detectors(all_detectors[:min(len(all_detectors),10)])
+    display_detectors(all_detectors[:min(len(all_detectors),10)], reflections=reflections)
 
 
 if __name__ == '__main__':
