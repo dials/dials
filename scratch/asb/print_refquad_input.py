4b6b3292a4281b06658d59387ec6821d53bf4da6
Aaron S. Brewster <asbrewster@lbl.gov>
2014-10-18 02:45:21 +0000

    Better input (accept multiple paths)

--- scratch/asb/print_refquad_input.py
+++ scratch/asb/print_refquad_input.py
@@ -1,12 +1,13 @@
 from __future__ import division
 import os, sys
 
-path = sys.argv[1]
+paths = sys.argv[1:]
 
-for filename in os.listdir(path):
-  if "integrated" in filename:
-    print "input {"
-    print "  experiments =", filename.rstrip("_integrated.pickle") + "_refined_experiments.json"
-    print "  reflections =", filename
-    print "}"
+for path in paths:
+  for filename in os.listdir(path):
+    if "integrated" in filename:
+      print "input {"
+      print "  experiments =", os.path.join(path, filename.rstrip("_integrated.pickle") + "_refined_experiments.json")
+      print "  reflections =", os.path.join(path, filename)
+      print "}"
 
