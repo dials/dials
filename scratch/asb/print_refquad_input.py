7534664ed21b58045ffbb8c9868c7695e8e25313
Aaron S. Brewster <asbrewster@lbl.gov>
2014-10-29 20:36:26 +0000

    Bugfix

--- scratch/asb/print_refquad_input.py
+++ scratch/asb/print_refquad_input.py
@@ -6,8 +6,10 @@ paths = sys.argv[1:]
 for path in paths:
   for filename in os.listdir(path):
     if "indexed" in filename:
+      exp_path = os.path.join(path, filename.rstrip("_indexed.pickle") + "_refined_experiments.json")
+      if not os.path.exists(exp_path): continue
       print "input {"
-      print "  experiments =", os.path.join(path, filename.rstrip("_indexed.pickle") + "_refined_experiments.json")
+      print "  experiments =", exp_path
       print "  reflections =", os.path.join(path, filename)
       print "}"
 
