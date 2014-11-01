8a1fcdb8ff70276ab9eeb0b2b331e1fa95a4d67d
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2014-11-01 10:30:53 +0000

    making the viewer tester to rely on the proper app

--- viewer/new_viewer_tst.py
+++ viewer/new_viewer_tst.py
@@ -1,5 +1,5 @@
 #
-#  DIALS viewer
+#  DIALS viewer_tester
 #
 #  Copyright (C) 2014 Diamond Light Source
 #
@@ -13,23 +13,11 @@ from __future__ import division
 import wx
 from dials.viewer.viewer_frame import ReflectionFrame
 
-class viewer_App(wx.App):
-  def OnInit(self):
-    self.frame = ReflectionFrame(None, -1, "DIALS Reflections Viewer"
-    , wx.DefaultPosition,(550,200))
-
-    self.SetTopWindow(self.frame)
-    self.frame.Show(True)
-    return True
-
-  def table_in(self, loc_tabl):
-    self.frame.tabl_to_frame(loc_tabl)
 
 
 def next_gen_viewer_test(table):
   print "table(ptr) =", table
   from dials.viewer.next_gen_viewer.multi_3D_slice_viewer import show_3d
-  print "Here Yes"
 
 
   table_row = table[8037]
@@ -50,6 +38,8 @@ if __name__ == "__main__":
   import cPickle as pickle
   import sys
   from dials.array_family import flex
+  from dials.viewer.reflection_view import viewer_App
+
   table = flex.reflection_table.from_pickle(sys.argv[1])
 
   app = viewer_App(redirect=False)
