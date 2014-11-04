fd6488df9edcb9eed7fc9ce2c1035dd939ebd073
James Parkhurst <james.parkhurst@diamond.ac.uk>
2014-11-04 14:12:11 +0000

    Hopefully fixed stdin check

--- util/options.py
+++ util/options.py
@@ -336,7 +336,7 @@ class OptionParserBase(optparse.OptionParser, object):
 
   def parse_args(self, args=None):
     '''Parse the command line arguments and get system configuration.'''
-    import sys
+    import sys, os
 
     # Parse the command line arguments, this will separate out
     # options (e.g. -o, --option) and positional arguments, in
@@ -344,7 +344,7 @@ class OptionParserBase(optparse.OptionParser, object):
     options, args = super(OptionParserBase, self).parse_args(args=args)
 
     # Read stdin if data is available
-    if not sys.stdin.isatty():
+    if not sys.stdin.isatty() and os.fstat(sys.stdin.fileno()).st_size > 0:
       args.extend([l.strip() for l in sys.stdin.readlines()])
 
     # Maybe sort the data
