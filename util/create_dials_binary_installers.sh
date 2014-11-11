61cd2c0c23b8b68e557910b9f24654113583feee
Nathaniel Echols <nathaniel.echols@gmail.com>
2014-11-11 22:53:06 +0000

    refactored installer packaging

--- util/create_dials_binary_installers.sh
+++ util/create_dials_binary_installers.sh
@@ -30,49 +30,20 @@ if [ -z "$HOST_TAG" ]; then
   echo "HOST_TAG must be third argument!"
   exit 1
 fi
-BUILD_DIR="`libtbx.find_in_repositories dials`/../.."
-echo "BUILD_DIR is ${BUILD_DIR}"
-DEV_BUILD=`echo ${DIALS_VERSION} | grep -c dev`
-PYTHON_BIN=${BUILD_DIR}/build/bin/libtbx.python
-$PYTHON_BIN ${BUILD_DIR}/modules/dials/util/assemble_installer.py \
-  --version=${DIALS_VERSION} --host-tag=${HOST_TAG} $BUILD_DIR
-DIALS_INSTALLER="dials-installer-${DIALS_VERSION}"
-DIALS_TAR="dials-installer-${DIALS_VERSION}-${HOST_TAG}.tar.gz"
-if [ ! -d "$DIALS_INSTALLER" ]; then
-  echo "Directory ${DIALS_INSTALLER} not found!"
-  exit 1
-fi
-if [ ! -f "${DIALS_TAR}" ]; then
-  tar czf ${DIALS_TAR} ${DIALS_INSTALLER}
-fi
+DIALS="`libtbx.find_in_repositories dials`"
+MISC_OPTIONS=""
 if [ ! -z "${DIALS_DEST_DIR}" ]; then
-  rsync -avz ${DIALS_TAR} ${DIALS_DEST_DIR}/${DIALS_VERSION}
+  MISC_OPTIONS="--destination=${DIALS_DEST_DIR}"
 fi
-#
-# MAC GRAPHICAL INSTALLER
-#
-if [ "`uname`" = "Darwin" ]; then
-  cd $DIALS_INSTALLER
-  # slightly buried for a cleaner top-level directory, although this may cause
-  # more user confusion than it prevents
-  ./install --prefix=/Applications --compact --no-app
-  if [ $? -ne 0 ]; then
-    echo "Fatal installer error"
-    exit 1
-  fi
-  ${PYTHON_BIN} ./lib/libtbx/auto_build/create_mac_pkg.py \
-    --package_name=DIALS --version=${DIALS_VERSION} \
-    --background=${BUILD_DIR}/modules/dials/util/installer_background.jpg \
-    --license=${BUILD_DIR}/modules/dials/license.txt \
-    --organization=gov.lbl.dials /Applications/dials-${DIALS_VERSION}
-  DIALS_PKG="DIALS-${DIALS_VERSION}-${HOST_TAG}.pkg.zip"
-  if [ ! -z "${DIALS_DEST_DIR}" ]; then
-    rsync -avz ${DIALS_PKG} ${DIALS_DEST_DIR}/${DIALS_VERSION}
-  fi
-  cd ..
+if [ "${DIALS_DEBUG}" != "0" ]; then
+  MISC_OPTIONS="${MISC_OPTIONS} --debug"
 fi
-# get rid of bulky directories
-if [ $DIALS_DEBUG -eq 0 ] && [ $DEV_BUILD -eq 1 ]; then
-  rm -rf $INST_DIR
-  rm -rf ${DIALS_INSTALLER}
+libtbx.make_dist \
+  --version=${DIALS_VERSION} \
+  --host-tag=${HOST_TAG} \
+  ${MISC_OPTIONS} \
+  ${DIALS}/util/make_dist.phil
+if [ $? -ne 0 ]; then
+  echo "Fatal error assembling installer"
+  exit 1
 fi
