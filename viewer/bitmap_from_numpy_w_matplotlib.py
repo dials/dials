3915b6f0016fa9951500c0cb688d447ea86264db
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2014-11-17 16:07:26 +0000

    removing leftover print to console

--- viewer/bitmap_from_numpy_w_matplotlib.py
+++ viewer/bitmap_from_numpy_w_matplotlib.py
@@ -95,8 +95,6 @@ class wxbmp_from_np_array(object):
           loc_mask = int(np_2d_mask[xpos, ypos])
 
           if( loc_mask != 0 ):
-            print "loc_mask =", loc_mask
-
 
             # drawing box
             plt.vlines(xpos - 0.45, ypos - 0.45, ypos + 0.45,
@@ -109,8 +107,6 @@ class wxbmp_from_np_array(object):
                        color = 'gray', linewidth=2)
 
             if( (loc_mask & MaskCode.Background) == MaskCode.Background ):
-              print "MaskCode.Background =", int(MaskCode.Background)
-
               # drawing v_lines
               plt.vlines(xpos, ypos - 0.45, ypos + 0.45,
                          color = 'gray', linewidth=2)
@@ -121,7 +117,6 @@ class wxbmp_from_np_array(object):
 
             if( (loc_mask & MaskCode.Foreground) == MaskCode.Foreground ):
               # drawing lines from 1p5 to 7p5
-              print "MaskCode.Foreground =", int(MaskCode.Foreground)
               plt.plot([xpos - dst_diag, xpos - 0.45], [ypos - 0.45, ypos - dst_diag],
                        color='gray', linewidth=2)
               plt.plot([xpos + dst_diag, xpos + 0.45], [ypos + 0.45, ypos + dst_diag],
@@ -131,7 +126,6 @@ class wxbmp_from_np_array(object):
 
             if( (loc_mask &  MaskCode.Valid) == MaskCode.Valid ):
               # drawing lines from 4p5 to 10p5
-              print "MaskCode.Valid =", int(MaskCode.Valid)
               plt.plot([xpos + dst_diag, xpos - 0.45], [ypos + 0.45, ypos - dst_diag],
                        color='gray', linewidth=2)
               plt.plot([xpos + dst_diag, xpos + 0.45], [ypos - 0.45, ypos - dst_diag],
@@ -139,18 +133,14 @@ class wxbmp_from_np_array(object):
               plt.plot([xpos + 0.45, xpos - 0.45], [ypos + 0.45, ypos - 0.45],
                        color='gray', linewidth=2)
 
-
             if( (loc_mask & MaskCode.BackgroundUsed) == MaskCode.BackgroundUsed ):
               # drawing h_lines
-              print "MaskCode.BackgroundUsed =", int(MaskCode.BackgroundUsed)
               plt.hlines(ypos , xpos - 0.45, xpos + 0.45,
                          color = 'gray', linewidth=2)
               plt.hlines(ypos + 0.225, xpos - 0.45, xpos + 0.45,
                          color = 'gray', linewidth=2)
               plt.hlines(ypos - 0.225, xpos - 0.45, xpos + 0.45,
                          color = 'gray', linewidth=2)
-
-
 
 
     if( show_nums != 0 ):
