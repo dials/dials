6a5f921dfe73797d6d26e18e05df510369309793
James Parkhurst <james.parkhurst@diamond.ac.uk>
2014-11-21 14:00:37 +0000

    Added test for partial profile fit

--- test/algorithms/integration/profile/tst_profile_fitting.py
+++ test/algorithms/integration/profile/tst_profile_fitting.py
@@ -10,6 +10,10 @@ class Test(object):
     self.tst_scaled()
     self.tst_with_flat_background()
     self.tst_with_noisy_flat_background()
+    self.tst_identical_partial()
+    self.tst_scaled_partial()
+    self.tst_with_flat_background_partial()
+    self.tst_with_noisy_flat_background_partial()
 
   def tst_identical(self):
 
@@ -123,6 +127,147 @@ class Test(object):
     eps = 1e-7
     assert(abs(I - flex.sum(c0)) < eps)
     assert(abs(V - (flex.sum(c0) + flex.sum(b))) < eps)
+
+    print 'OK'
+
+  def tst_identical_partial(self):
+
+    from dials.algorithms.integration.profile import fit_profile
+    from scitbx.array_family import flex
+    from tst_profile_helpers import gaussian
+
+    # Create profile
+    p = gaussian((9, 9, 9), 1, (4, 4, 4), (2, 2, 2))
+    s = flex.sum(p)
+    p = p / s
+
+    # Copy profile
+    c = p.deep_copy()
+    b = flex.double(flex.grid(9, 9, 9), 0)
+    m = flex.bool(flex.grid(9,9,9), True)
+
+    # Get the partial profiles
+    pp = p[0:5,:,:]
+    mp = m[0:5,:,:]
+    cp = c[0:5,:,:]
+    bp = b[0:5,:,:]
+
+    # Fit
+    fit = fit_profile(pp, mp, cp, bp)
+    I = fit.intensity()
+    V = fit.variance()
+
+    # Test intensity is the same
+    eps = 1e-7
+    assert(abs(I - flex.sum(p)) < eps)
+    assert(abs(V - flex.sum(pp)) < eps)
+
+    print 'OK'
+
+  def tst_scaled_partial(self):
+
+    from dials.algorithms.integration.profile import fit_profile
+    from scitbx.array_family import flex
+    from tst_profile_helpers import gaussian
+
+    # Create profile
+    p = gaussian((9, 9, 9), 1, (4, 4, 4), (2, 2, 2))
+    s = flex.sum(p)
+    p = p / s
+
+    # Copy profile
+    c = gaussian((9, 9, 9), 1, (4, 4, 4), (2, 2, 2))
+    b = flex.double(flex.grid(9, 9, 9), 0)
+    m = flex.bool(flex.grid(9,9,9), True)
+
+    # Get the partial profiles
+    pp = p[0:5,:,:]
+    mp = m[0:5,:,:]
+    cp = c[0:5,:,:]
+    bp = b[0:5,:,:]
+
+    # Fit
+    fit = fit_profile(pp, mp, cp, bp)
+    I = fit.intensity()
+    V = fit.variance()
+
+    # Test intensity is the same
+    eps = 1e-7
+    assert(abs(I - flex.sum(c)) < eps)
+    assert(abs(V - flex.sum(cp)) < eps)
+
+    print 'OK'
+
+  def tst_with_flat_background_partial(self):
+
+    from dials.algorithms.integration.profile import fit_profile
+    from scitbx.array_family import flex
+    from tst_profile_helpers import gaussian
+
+    # Create profile
+    p = gaussian((9, 9, 9), 1, (4, 4, 4), (2, 2, 2))
+    s = flex.sum(p)
+    p = p / s
+
+    # Copy profile
+    c0 = gaussian((9, 9, 9), 1, (4, 4, 4), (2, 2, 2))
+    b = flex.double(flex.grid(9, 9, 9), 5)
+    m = flex.bool(flex.grid(9,9,9), True)
+    c = c0 + b
+
+    # Get the partial profiles
+    pp = p[0:5,:,:]
+    mp = m[0:5,:,:]
+    cp = c[0:5,:,:]
+    bp = b[0:5,:,:]
+    c0p = c0[0:5,:,:]
+
+    # Fit
+    fit = fit_profile(pp, mp, cp, bp)
+    I = fit.intensity()
+    V = fit.variance()
+
+    # Test intensity is the same
+    eps = 1e-7
+    assert(abs(I - flex.sum(c0)) < eps)
+    assert(abs(V - (flex.sum(c0p) + flex.sum(bp))) < eps)
+
+    print 'OK'
+
+  def tst_with_noisy_flat_background_partial(self):
+
+    from dials.algorithms.integration.profile import fit_profile
+    from scitbx.array_family import flex
+    from tst_profile_helpers import gaussian
+
+    # Create profile
+    p = gaussian((9, 9, 9), 1, (4, 4, 4), (2, 2, 2))
+    s = flex.sum(p)
+    p = p / s
+
+    # Copy profile
+    c0 = gaussian((9, 9, 9), 1, (4, 4, 4), (2, 2, 2))
+    n = flex.random_double(9 * 9 * 9)
+    b = flex.double(flex.grid(9, 9, 9), 5) + n
+    m = flex.bool(flex.grid(9,9,9), True)
+    c = c0 + b
+
+    # Get the partial profiles
+    pp = p[0:5,:,:]
+    mp = m[0:5,:,:]
+    cp = c[0:5,:,:]
+    bp = b[0:5,:,:]
+    c0p = c0[0:5,:,:]
+
+    # Fit
+    fit = fit_profile(pp, mp, cp, bp)
+    I = fit.intensity()
+    V = fit.variance()
+
+    # Test intensity is the same
+    eps = 1e-7
+    assert(abs(I - flex.sum(c0)) < eps)
+    assert(abs(V - (flex.sum(c0p) + flex.sum(bp))) < eps)
 
     print 'OK'
 
