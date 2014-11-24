4810deafabbbbbccc8aa1a65db6ff76c7f000bb4
Graeme Winter <graeme.winter@gmail.com>
2014-11-24 08:36:47 +0000

    Added stop function

--- scratch/gw/test_client.py
+++ scratch/gw/test_client.py
@@ -9,8 +9,27 @@ def work(filename, params):
   response = conn.getresponse()
   return response.read()
 
+def stop(nproc):
+  for j in range(nproc):
+    conn = httplib.HTTPConnection('localhost', 1701)
+    path = '/Ctrl-C'
+    conn.request('GET', path)
+    response = conn.getresponse()
+  return
+
+
 if __name__ == '__main__':
   import sys
-  filename = sys.argv[1]
-  params = sys.argv[2:]
-  print work(filename, params)
+
+  if len(sys.argv) < 2:
+    raise RuntimeError, '%s [filename] [param=value]'
+
+  if sys.argv[1] == 'stop':
+    if len(sys.argv) < 3:
+      raise RuntimeError, '%s stop nproc'
+    nproc = int(sys.argv[2])
+    stop(nproc)
+  else:
+    filename = sys.argv[1]
+    params = sys.argv[2:]
+    print work(filename, params)
