083df8741942fcdb67db668439260ac26f15d2ca
Graeme Winter <graeme.winter@gmail.com>
2014-12-05 15:14:43 +0000

    Thread (pool) the client; can now pass a hill of calls all at the same time (ie for wedges)

--- command_line/findspots_client.py
+++ command_line/findspots_client.py
@@ -1,6 +1,7 @@
-import httplib
+from __future__ import division
 
 def work(host, filename, params):
+  import httplib
   conn = httplib.HTTPConnection(host, 1701)
   path = filename
   for param in params:
@@ -9,14 +10,30 @@ def work(host, filename, params):
   response = conn.getresponse()
   return response.read()
 
+def nproc():
+  from libtbx.introspection import number_of_processors
+  return number_of_processors(return_value_if_unknown=-1)
+
+def work_all(host, filenames, params):
+  from multiprocessing.pool import ThreadPool as thread_pool
+  pool = thread_pool(processes=nproc())
+  threads = { }
+  for filename in filenames:
+    threads[filename] = pool.apply_async(work, (host, filename, params))
+  results = { }
+  for filename in filenames:
+    results[filename] = threads[filename].get()
+    print results[filename]
+  return
+
 def stop(host, nproc):
+  import httplib
   for j in range(nproc):
     conn = httplib.HTTPConnection(host, 1701)
     path = '/Ctrl-C'
     conn.request('GET', path)
     response = conn.getresponse()
   return
-
 
 if __name__ == '__main__':
   import sys
@@ -32,6 +49,14 @@ if __name__ == '__main__':
     stop(host, nproc)
   else:
     host = sys.argv[1]
-    filename = sys.argv[2]
-    params = sys.argv[3:]
-    print work(host, filename, params)
+    filenames = []
+    params = []
+    for arg in sys.argv[2:]:
+      if '=' in arg:
+        params.append(arg)
+      else:
+        filenames.append(arg)
+    if len(filenames) == 1:
+      print work(host, filenames[0], params)
+    else:
+      work_all(host, filenames, params)
