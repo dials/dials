0bda0a8c1b5efefc18788dde0a4cdb7ba9cb1917
Ian Rees <ian@ianrees.net>
2015-01-06 23:02:13 +0000

    bug fix

--- installer/dials_installer.sh
+++ installer/dials_installer.sh
@@ -2,7 +2,7 @@ build/bin/libtbx.python modules/cctbx_project/libtbx/auto_build/create_installer
 	--dest            tmp/dials-installer-dev \
 	--install_script  modules/dials/installer/dials_installer.py \
   --host-tag        intel-linux-2.6-x86_64 \
-	--version         dev
+	--version         dev \
 	--binary
 
 cd tmp
