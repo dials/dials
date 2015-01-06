c1de67bafe3c1f33726ea6ac5cbb8a06db82f8f9
Ian Rees <ian@ianrees.net>
2015-01-06 22:56:59 +0000

    Default host tag is linux

--- installer/dials_installer.sh
+++ installer/dials_installer.sh
@@ -1,7 +1,6 @@
-# Setup installer.
-# chem_data,dials_regression
 build/bin/libtbx.python modules/cctbx_project/libtbx/auto_build/create_installer.py \
 	--dest            tmp/dials-installer \
 	--install_script  modules/dials/installer/dials_installer.py \
-  --host-tag        mac-intel-osx-x86_64 \
+  --host-tag        intel-linux-2.6-x86_64 \
+	--version         dev
 	--binary
