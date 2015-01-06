efacdb1b5b2e8c90e756fc01eaf54c6c17f8e74f
Ian Rees <ian@ianrees.net>
2015-01-06 23:00:06 +0000

    tar the resulting package

--- installer/dials_installer.sh
+++ installer/dials_installer.sh
@@ -1,6 +1,16 @@
 build/bin/libtbx.python modules/cctbx_project/libtbx/auto_build/create_installer.py \
-	--dest            tmp/dials-installer \
+	--dest            tmp/dials-installer-dev \
 	--install_script  modules/dials/installer/dials_installer.py \
   --host-tag        intel-linux-2.6-x86_64 \
 	--version         dev
 	--binary
+
+cd tmp
+tar -cvzf 
+	dials-installer-dev.tar.gz \
+	dials-installer-dev \
+	--exclude dials-installer-dev/build \
+	--exclude dials-installer-dev/modules \
+	--exclude dials-installer-dev/base \
+	--exclude dials-installer-dev/tmp
+
