a618c3f4f672acbab3e2ebf34aac51f724c05b69
Ian Rees <ian@ianrees.net>
2015-01-07 10:26:54 +0000

    Host tag no longer needed

--- installer/dials_installer.sh
+++ installer/dials_installer.sh
@@ -1,7 +1,6 @@
 build/bin/libtbx.python modules/cctbx_project/libtbx/auto_build/create_installer.py \
 	--dest            tmp/dials-installer-dev \
 	--install_script  modules/dials/installer/dials_installer.py \
-  --host-tag        intel-linux-2.6-x86_64 \
 	--version         dev \
 	--binary
 
