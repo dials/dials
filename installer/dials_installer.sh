027ec610d03bd621b86e9f66dddcf9f5738558b1
Ian Rees <ian@ianrees.net>
2015-01-07 12:53:37 +0000

    Updates to installer. Work in progress.

--- installer/dials_installer.sh
+++ installer/dials_installer.sh
@@ -1,15 +1,7 @@
+#!/bin/sh
 build/bin/libtbx.python modules/cctbx_project/libtbx/auto_build/create_installer.py \
 	--dest            tmp/dials-installer-dev \
 	--install_script  modules/dials/installer/dials_installer.py \
 	--version         dev \
 	--binary
-
-cd tmp
-tar -cvzf \
-	dials-installer-dev.tar.gz \
-	dials-installer-dev \
-	--exclude dials-installer-dev/build \
-	--exclude dials-installer-dev/modules \
-	--exclude dials-installer-dev/base \
-	--exclude dials-installer-dev/tmp
 
