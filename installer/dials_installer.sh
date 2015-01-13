af0c917aa12a7a45f447360974f2f89da8db3451
Ian Rees <ian@ianrees.net>
2015-01-13 23:13:11 +0000

    Reordered arguments

--- installer/dials_installer.sh
+++ installer/dials_installer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 build/bin/libtbx.python modules/cctbx_project/libtbx/auto_build/create_installer.py \
-	--dest            tmp/dials-installer-dev \
 	--install_script  modules/dials/installer/dials_installer.py \
 	--version         dev \
-	--binary
+	--binary \
+	tmp/dials-installer-dev
 
