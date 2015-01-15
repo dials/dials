3efeb9221e2032bfc28de24cbf4c60752b3e62ef
Ian Rees <ian@ianrees.net>
2015-01-15 10:51:02 +0000

    Updated to current argument names

--- installer/dials_installer.sh
+++ installer/dials_installer.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+# Debuging tool for building installers.
 build/bin/libtbx.python modules/cctbx_project/libtbx/auto_build/create_installer.py \
-        --install_script  modules/dials/installer/dials_installer.py \
-        --version         dev \
-        --binary \
-        tmp/dials-installer-dev
+  --install_script  modules/dials/installer/dials_installer.py \
+  --version         dev \
+  --binary \
+  tmp/dials-installer-dev
