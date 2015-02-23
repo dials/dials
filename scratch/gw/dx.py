b11a8d4ae38a3e5206e6306ec4f1f61e21508a12
Graeme Winter <graeme.winter@gmail.com>
2015-02-23 21:45:12 +0000

    Now have analytical derivatives of Rodrigues rotation formula agreeing with finite differences, detail in header including sage script showing calculus process (allows recovery of source code)

--- scratch/gw/dx.py
+++ scratch/gw/dx.py
@@ -1,35 +1,32 @@
-sage_calculations = '''
-sage: k1 = var('k1')
-sage: k2 = var('k2')
-sage: k3 = var('k3')
-sage: K = matrix([[0, -k3, k2], [k3, 0, -k1], [-k2, k1, 0]])
-sage: K
-[  0 -k3  k2]
-[ k3   0 -k1]
-[-k2  k1   0]
-sage: t = var('t')
-sage: I = matrix.identity(3)
-sage: R = I + sin(t) * K + (1 - cos(t)) * K * K
-sage: dR1 = derivative(R, k1)
-sage: dR2 = derivative(R, k2)
-sage: dR3 = derivative(R, k3)
-sage: dR1
-[                0  -k2*(cos(t) - 1)  -k3*(cos(t) - 1)]
-[ -k2*(cos(t) - 1) 2*k1*(cos(t) - 1)           -sin(t)]
-[ -k3*(cos(t) - 1)            sin(t) 2*k1*(cos(t) - 1)]
-sage: dR2
-[2*k2*(cos(t) - 1)  -k1*(cos(t) - 1)            sin(t)]
-[ -k1*(cos(t) - 1)                 0  -k3*(cos(t) - 1)]
-[          -sin(t)  -k3*(cos(t) - 1) 2*k2*(cos(t) - 1)]
-sage: dR3
-[2*k3*(cos(t) - 1)           -sin(t)  -k1*(cos(t) - 1)]
-[           sin(t) 2*k3*(cos(t) - 1)  -k2*(cos(t) - 1)]
-[ -k1*(cos(t) - 1)  -k2*(cos(t) - 1)                 0]
-sage: dRt = derivative(R, t)
-sage: dRt
-[-k2^2*sin(t) - k3^2*sin(t)   k1*k2*sin(t) - k3*cos(t)   k1*k3*sin(t) + k2*cos(t)]
-[  k1*k2*sin(t) + k3*cos(t) -k1^2*sin(t) - k3^2*sin(t)   k2*k3*sin(t) - k1*cos(t)]
-[  k1*k3*sin(t) - k2*cos(t)   k2*k3*sin(t) + k1*cos(t) -k1^2*sin(t) - k2^2*sin(t)]
+#!/usr/bin/env dials.python
+#
+# Play area trying to derive useful derivatives for Rodrigues rotation formula
+# http://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula
+
+sage_script = '''
+r = var('r')
+s = var('s')
+
+k1 = cos(r) * sin(s)
+k2 = sin(r) * sin(s)
+k3 = cos(s)
+K = matrix([[0, -k3, k2], [k3, 0, -k1], [-k2, k1, 0]])
+t = var('t')
+I = matrix.identity(3)
+
+R = I + sin(t) * K + (1 - cos(t)) * K * K
+
+dRdr = diff(R, r)
+dRds = diff(R, s)
+
+for i in range(3):
+  for j in range(3):
+    print dRdr[i,j].trig_reduce().full_simplify()
+
+for i in range(3):
+  for j in range(3):
+    print dRds[i,j].trig_reduce().full_simplify()
+
 '''
 
 def skew_symm(v):
@@ -45,78 +42,40 @@ def skew_symm(v):
 
   return v1 * L1 + v2 * L2 + v3 * L3
 
-def dR_dki_gw(t, k):
+def dR_dki_gw(r, s, t):
   from scitbx import matrix
   from math import sin, cos
 
-  k1, k2, k3 = k.elems
-  
-  dR1 = matrix.sqr((0, -k2*(cos(t) - 1), -k3*(cos(t) - 1),
-                    -k2*(cos(t) - 1), 2*k1*(cos(t) - 1), -sin(t),
-                    -k3*(cos(t) - 1), sin(t), 2*k1*(cos(t) - 1)))
-  dR2 = matrix.sqr((2*k2*(cos(t) - 1), -k1*(cos(t) - 1), sin(t),
-                    -k1*(cos(t) - 1), 0, -k3*(cos(t) - 1),
-                    -sin(t), -k3*(cos(t) - 1), 2*k2*(cos(t) - 1)))
-  dR3 = matrix.sqr((2*k3*(cos(t) - 1), -sin(t), -k1*(cos(t) - 1),
-                    sin(t), 2*k3*(cos(t) - 1), -k2*(cos(t) - 1),
-                    -k1*(cos(t) - 1), -k2*(cos(t) - 1), 0))
+  cr = cos(r)
+  sr = sin(r)
+  cs = cos(s)
+  ss = sin(s)
+  ct = cos(t)
+  st = sin(t)
 
-  return dR1, dR2, dR3
+  dRdr = matrix.sqr([
+    -1/2*(cos(2*s)*sin(2*r) - sin(2*r))*ct + 1/2*cos(2*s)*sin(2*r) - cr*sr,
+    cr**2 - 1/2*cos(2*r)*cos(2*s) + 1/2*(cos(2*r)*cos(2*s) - cos(2*r))*ct - 1/2,
+    1/2*ct*sr*sin(2*s) + cr*ss*st - 1/2*sr*sin(2*s),
+    cr**2 - 1/2*cos(2*r)*cos(2*s) + 1/2*(cos(2*r)*cos(2*s) - cos(2*r))*ct - 1/2,
+    1/2*(cos(2*s)*sin(2*r) - sin(2*r))*ct - 1/2*cos(2*s)*sin(2*r) + cr*sr,
+    -1/2*cr*ct*sin(2*s) + sr*ss*st + 1/2*cr*sin(2*s),
+    1/2*ct*sr*sin(2*s) - cr*ss*st - 1/2*sr*sin(2*s),
+    -1/2*cr*ct*sin(2*s) - sr*ss*st + 1/2*cr*sin(2*s),
+    0])
 
-def dR_dki_7(t, k):
-  '''Compute derivative matrices dR/dk_i for rotation about axis k of
-  angle t. Equation (7) from Gallego & Yezzi paper, though that is derivative
-  of R as function of v = t * k => remove one factor of t = |v|.'''
+  dRds = matrix.sqr([
+    -1/2*(cos(2*r) + 1)*ct*sin(2*s) + 1/2*cos(2*r)*sin(2*s) + cs*ss,
+    -1/2*ct*sin(2*r)*sin(2*s) + 1/2*sin(2*r)*sin(2*s) + ss*st,
+    -cr*cos(2*s)*ct + cs*sr*st + cr*cos(2*s),
+    -1/2*ct*sin(2*r)*sin(2*s) + 1/2*sin(2*r)*sin(2*s) - ss*st,
+    1/2*(cos(2*r) - 1)*ct*sin(2*s) - 1/2*cos(2*r)*sin(2*s) + cs*ss,
+    -cos(2*s)*ct*sr - cr*cs*st + cos(2*s)*sr,
+    -cr*cos(2*s)*ct - cs*sr*st + cr*cos(2*s),
+    -cos(2*s)*ct*sr + cr*cs*st + cos(2*s)*sr,
+    ct*sin(2*s) - 2*cs*ss])
 
-  import scitbx.matrix
-  import math
-
-  K = skew_symm(k)
-  I = scitbx.matrix.sqr((1, 0, 0, 0, 1, 0, 0, 0, 1))
-
-  e1 = scitbx.matrix.col((1, 0, 0))
-  e2 = scitbx.matrix.col((0, 1, 0))
-  e3 = scitbx.matrix.col((0, 0, 1))
-
-  e = (e1, e2, e3)
-
-  c = math.cos(t)
-  s = math.sin(t)
-
-  result = []
-
-  for j in range(3):
-    result.append((t * c * k.elems[j] * K + t * s * k.elems[j] * K * K +
-                   s * skew_symm(e[j] - k.elems[j] * k) +
-                   (1 - c) * (e[j] * k.transpose() +
-                              k * e[j].transpose() -
-                              2 * k.elems[j] * k * k.transpose())))
-  return result
-
-def dR_dki_9(t, k):
-  '''Compute derivative matrices dR/dk_i for rotation about axis k of
-  angle t. Equation (9) from Gallego & Yezzi paper, though that is derivative
-  of R as function of v = t * k => remove one factor of t = |v|.'''
-
-  import scitbx.matrix
-
-  R = Rtk(t, k)
-  v = t * k
-  V = skew_symm(v)
-  I = scitbx.matrix.sqr((1, 0, 0, 0, 1, 0, 0, 0, 1))
-
-  e1 = scitbx.matrix.col((1, 0, 0))
-  e2 = scitbx.matrix.col((0, 1, 0))
-  e3 = scitbx.matrix.col((0, 0, 1))
-
-  e = (e1, e2, e3)
-
-  result = []
-
-  for j in range(3):
-    result.append((v.elems[j] * V + skew_symm(v.cross((I - R) * e[j]))) * R / t)
-
-  return result
+  return dRdr, dRds
 
 def Rtk(t, k):
   import scitbx.matrix
@@ -131,49 +90,64 @@ def Rtk(t, k):
   R = I + s * K + (1 - c) * K * K
 
   # make sure answer is right
-
-  zero = R.inverse() * k.axis_and_angle_as_r3_rotation_matrix(t) - I
-  assert sum(zero.elems) < 1.0e-8
+  # zero = R.inverse() * k.axis_and_angle_as_r3_rotation_matrix(t) - I
+  # assert sum(zero.elems) < 1.0e-8
 
   return R
+
+def krs(r, s):
+  from math import sin, cos
+  import scitbx.matrix
+  k1 = cos(r) * sin(s)
+  k2 = sin(r) * sin(s)
+  k3 = cos(s)
+  return scitbx.matrix.col((k1, k2, k3))
 
 def dx():
   import random
   import math
   import scitbx.matrix
 
-  # pick random rotation axis & angle - does around the houses to get values
-  # to use in finite difference calculation
+  # pick random rotation axis & angle - pick spherical coordinate basis to make
+  # life easier in performing finite difference calculations
 
   t = math.pi * random.random()
-  k = scitbx.matrix.col((random.random(),
-                         random.random(),
-                         random.random())).normalize()
-  k1, k2, k3 = k.elems
+  r = 2 * math.pi * random.random()
+  s = math.pi * random.random()
+  k = krs(r, s)
 
   # perform finite difference calculation of dR/dk1 as
   # (1/2 eps) * (R(k1+eps) - R(k1-eps))
 
   eps = 1.0e-8
 
-  Rpls = Rtk(t, scitbx.matrix.col((k1 + eps, k2, k3)).normalize())
-  Rmns = Rtk(t, scitbx.matrix.col((k1 - eps, k2, k3)).normalize())
+  Rpls = Rtk(t, krs(r + eps, s))
+  Rmns = Rtk(t, krs(r - eps, s))
+  ndRr = (0.5 / eps) * (Rpls - Rmns)
 
-  ndR1 = (0.5 / eps) * (Rpls - Rmns)
-
-  print 'Numerical:'
-  print ndR1
-
-  # now call on derivative calculation
-  dR1 = dR_dki_gw(t, k)[0]
-
-  print 'Sums:'
-  print dR1
+  Rpls = Rtk(t, krs(r, s + eps))
+  Rmns = Rtk(t, krs(r, s - eps))
+  ndRs = (0.5 / eps) * (Rpls - Rmns)
 
   # now call on derivative calculation
-  dR1 = dR_dki_7(t, k)[0]
+  dRr, dRs = dR_dki_gw(r, s, t)
 
-  print 'Sums7:'
-  print dR1
+  print 'Numerical dR/dr:'
+  print ndRr
+
+  print 'Sums dR/dr:'
+  print dRr
+
+  print 'Numerical dR/ds:'
+  print ndRs
+
+  print 'Sums dR/ds:'
+  print dRs
+
+  I = scitbx.matrix.sqr((1, 0, 0, 0, 1, 0, 0, 0, 1))
+
+  print 'Summed differences:'
+  print sum((ndRr - dRr).elems)
+  print sum((ndRs - dRs).elems)
 
 dx()
