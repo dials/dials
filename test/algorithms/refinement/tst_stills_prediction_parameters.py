8465f89ec9a02bb7d7e0a6443f50d15c42df1f02
David Waterman <david.waterman@stfc.ac.uk>
2015-02-26 15:05:00 +0000

    Make this test somewhat more useful and expose to run_tests.py.
    run(verbose=True) to see summary output. Certain small subclasses of
    of reflections have poor gradient calculations compared with finite
    differences, for beam and crystal parameters. However for the majority
    of reflections the agreement is good.
    
    Perhaps this indicates a breakdown in the applicability of the
    calculations, analogous to the 'too close to spindle' case for rotation
    scans. This is not fully understood. In addition this test uses data
    from a simulated narrow rotation scan in place of true stills data,
    meaning that the 'observed' centroids are subject to systematic error.

--- test/algorithms/refinement/tst_stills_prediction_parameters.py
+++ test/algorithms/refinement/tst_stills_prediction_parameters.py
@@ -43,170 +43,229 @@ from dials.algorithms.refinement.parameterisation.crystal_parameters import \
     CrystalOrientationParameterisation, \
     CrystalUnitCellParameterisation
 
-from time import time
-start_time = time()
+def run(verbose = False):
+  #### Create models
 
-#### Create models
+  # build models, with a larger crystal than default in order to get plenty of
+  # reflections on the 'still' image
+  overrides = """
+  geometry.parameters.crystal.a.length.range=40 50;
+  geometry.parameters.crystal.b.length.range=40 50;
+  geometry.parameters.crystal.c.length.range=40 50;
+  geometry.parameters.random_seed = 42"""
 
-args = sys.argv[1:]
+  master_phil = parse("""
+      include scope dials.test.algorithms.refinement.geometry_phil
+      """, process_includes=True)
 
-# build models, with a larger crystal than default in order to get enough
-# reflections on the 'still' image
-overrides = """
-geometry.parameters.crystal.a.length.range=40 50;
-geometry.parameters.crystal.b.length.range=40 50;
-geometry.parameters.crystal.c.length.range=40 50;
-geometry.parameters.random_seed = 42"""
+  models = Extract(master_phil, overrides)
 
-master_phil = parse("""
-    include scope dials.test.algorithms.refinement.geometry_phil
-    """, process_includes=True)
+  mydetector = models.detector
+  mygonio = models.goniometer
+  mycrystal = models.crystal
+  mybeam = models.beam
 
-models = Extract(master_phil, overrides, cmdline_args = args)
+  # Build a mock scan for a 3 degree sweep
+  from dxtbx.model.scan import scan_factory
+  sf = scan_factory()
+  myscan = sf.make_scan(image_range = (1,1),
+                        exposure_times = 0.1,
+                        oscillation = (0, 3.0),
+                        epochs = range(1),
+                        deg = True)
+  sweep_range = myscan.get_oscillation_range(deg=False)
 
-mydetector = models.detector
-mygonio = models.goniometer
-mycrystal = models.crystal
-mybeam = models.beam
+  #### Create parameterisations of these models
 
-# Build a mock scan for a 72 degree sweep
+  det_param = DetectorParameterisationSinglePanel(mydetector)
+  s0_param = BeamParameterisation(mybeam, mygonio)
+  xlo_param = CrystalOrientationParameterisation(mycrystal)
+  xluc_param = CrystalUnitCellParameterisation(mycrystal)
 
-from dxtbx.model.scan import scan_factory
-sf = scan_factory()
-myscan = sf.make_scan(image_range = (1,1),
-                      exposure_times = 0.1,
-                      oscillation = (0, 3.0),
-                      epochs = range(1),
-                      deg = True)
-sweep_range = myscan.get_oscillation_range(deg=False)
+  # Create a scans ExperimentList, only for generating reflections
+  experiments = ExperimentList()
+  experiments.append(Experiment(
+        beam=mybeam, detector=mydetector, goniometer=mygonio, scan=myscan,
+        crystal=mycrystal, imageset=None))
 
-#### Create parameterisations of these models
+  # Create a stills ExperimentList
+  stills_experiments = ExperimentList()
+  stills_experiments.append(Experiment(
+        beam=mybeam, detector=mydetector, crystal=mycrystal, imageset=None))
 
-det_param = DetectorParameterisationSinglePanel(mydetector)
-s0_param = BeamParameterisation(mybeam, mygonio)
-xlo_param = CrystalOrientationParameterisation(mycrystal)
-xluc_param = CrystalUnitCellParameterisation(mycrystal)
+  #### Generate reflections
 
-# Create a scans ExperimentList, only for generating reflections
-experiments = ExperimentList()
-experiments.append(Experiment(
-      beam=mybeam, detector=mydetector, goniometer=mygonio, scan=myscan,
-      crystal=mycrystal, imageset=None))
+  # Create a ScansRayPredictor
+  ray_predictor = ScansRayPredictor(experiments, sweep_range)
 
-# Create a stills ExperimentList
-stills_experiments = ExperimentList()
-stills_experiments.append(Experiment(
-      beam=mybeam, detector=mydetector, crystal=mycrystal, imageset=None))
+  # Generate rays
+  resolution = 2.0
+  index_generator = IndexGenerator(mycrystal.get_unit_cell(),
+                        space_group(space_group_symbols(1).hall()).type(),
+                        resolution)
+  indices = index_generator.to_array()
+  rays = ray_predictor.predict(indices)
 
-#### Unit tests
+  # Make a standard reflection_table and copy in the ray data
+  reflections = flex.reflection_table.empty_standard(len(rays))
+  reflections.update(rays)
 
-# Create a ScansRayPredictor
-ray_predictor = ScansRayPredictor(experiments, sweep_range)
+  # Build a prediction parameterisation for the stills experiment
+  pred_param = StillsPredictionParameterisation(stills_experiments,
+                 detector_parameterisations = [det_param],
+                 beam_parameterisations = [s0_param],
+                 xl_orientation_parameterisations = [xlo_param],
+                 xl_unit_cell_parameterisations = [xluc_param])
 
-# Generate rays
-resolution = 2.0
-index_generator = IndexGenerator(mycrystal.get_unit_cell(),
-                      space_group(space_group_symbols(1).hall()).type(),
-                      resolution)
-indices = index_generator.to_array()
-rays = ray_predictor.predict(indices)
+  # use a ReflectionManager to keep track of predictions
+  from dials.algorithms.refinement.reflection_manager import \
+    StillsReflectionManager
+  refman = StillsReflectionManager(reflections, stills_experiments,
+                                   iqr_multiplier=None)
 
-# Make a standard reflection_table and copy in the ray data
-reflections = flex.reflection_table.empty_standard(len(rays))
-reflections.update(rays)
+  # Make a reflection predictor of the type expected by the Target class
+  from dials.algorithms.refinement.prediction import ExperimentsPredictor
+  ref_predictor = ExperimentsPredictor(stills_experiments)
 
-# Build a prediction parameterisation
-pred_param = StillsPredictionParameterisation(stills_experiments,
-               detector_parameterisations = [det_param],
-               beam_parameterisations = [s0_param],
-               xl_orientation_parameterisations = [xlo_param],
-               xl_unit_cell_parameterisations = [xluc_param])
+  # Make a target to ensure reflections are predicted with the stills predictor.
+  # This sets the "delpsical.rad" column.
+  from dials.algorithms.refinement.target_stills import \
+    LeastSquaresStillsResidualWithRmsdCutoff
+  target = LeastSquaresStillsResidualWithRmsdCutoff(stills_experiments,
+      ref_predictor, refman, pred_param)
 
-# use a ReflectionManager to exclude reflections too close to the spindle
-from dials.algorithms.refinement.reflection_manager import StillsReflectionManager
-refman = StillsReflectionManager(reflections, stills_experiments, iqr_multiplier=None)
+  # get predictions from the reflection manager
+  reflections = refman.get_matches()
 
-# Redefine the reflection predictor to use the type expected by the Target class
-from dials.algorithms.refinement.prediction import ExperimentsPredictor
-ref_predictor = ExperimentsPredictor(stills_experiments)
+  # get analytical gradients
+  an_grads = pred_param.get_gradients(reflections)
 
-# make a target to ensure reflections are predicted and refman is finalised
-from dials.algorithms.refinement.target_stills import \
-  LeastSquaresStillsResidualWithRmsdCutoff
-target = LeastSquaresStillsResidualWithRmsdCutoff(stills_experiments,
-    ref_predictor, refman, pred_param)
+  # get finite difference gradients
+  p_vals = pred_param.get_param_vals()
+  deltas = [1.e-7] * len(p_vals)
 
-# keep only those reflections that pass inclusion criteria and have predictions
-reflections = refman.get_matches()
+  p_names = pred_param.get_param_names()
+  for i in range(len(deltas)):
 
-# get analytical gradients
-an_grads = pred_param.get_gradients(reflections)
+    # save parameter value
+    val = p_vals[i]
 
-# get finite difference gradients
-p_vals = pred_param.get_param_vals()
-deltas = [1.e-7] * len(p_vals)
+    # calc reverse state
+    p_vals[i] -= deltas[i] / 2.
+    pred_param.set_param_vals(p_vals)
 
-from scitbx.array_family import flex
-for i in range(len(deltas)):
+    ref_predictor.update()
+    ref_predictor.predict(reflections)
 
-  val = p_vals[i]
+    x, y, _ = reflections['xyzcal.mm'].deep_copy().parts()
+    delpsi = reflections['delpsical.rad'].deep_copy()
+    rev_state = flex.vec3_double(x, y, delpsi)
 
-  p_vals[i] -= deltas[i] / 2.
-  pred_param.set_param_vals(p_vals)
+    # calc forward state
+    p_vals[i] += deltas[i]
+    pred_param.set_param_vals(p_vals)
 
-  ref_predictor.update()
-  ref_predictor.predict(reflections)
+    ref_predictor.update()
+    ref_predictor.predict(reflections)
 
-  x, y, _ = reflections['xyzcal.mm'].deep_copy().parts()
-  delpsi = reflections['delpsical.rad'].deep_copy()
-  rev_state = flex.vec3_double(x, y, delpsi)
+    x, y, _ = reflections['xyzcal.mm'].deep_copy().parts()
+    delpsi = reflections['delpsical.rad'].deep_copy()
+    fwd_state = flex.vec3_double(x, y, delpsi)
 
-  p_vals[i] += deltas[i]
-  pred_param.set_param_vals(p_vals)
+    # reset parameter to saved value
+    p_vals[i] = val
 
-  ref_predictor.update()
-  ref_predictor.predict(reflections)
+    # finite difference
+    fd = (fwd_state - rev_state)
+    x_grads, y_grads, delpsi_grads = fd.parts()
+    x_grads /= deltas[i]
+    y_grads /= deltas[i]
+    delpsi_grads /= deltas[i]
 
-  x, y, _ = reflections['xyzcal.mm'].deep_copy().parts()
-  delpsi = reflections['delpsical.rad'].deep_copy()
-  fwd_state = flex.vec3_double(x, y, delpsi)
-  p_vals[i] = val
+    # compare FD with analytical calculations
+    from scitbx.math import five_number_summary
+    if verbose: print "\n\nParameter {0}: {1}". format(i,  p_names[i])
+    grads = (x_grads, y_grads, delpsi_grads)
 
-  fd = (fwd_state - rev_state)
-  x_grads, y_grads, delpsi_grads = fd.parts()
-  x_grads /= deltas[i]
-  y_grads /= deltas[i]
-  delpsi_grads /= deltas[i]
+    for idx, name in enumerate(["dX/dp", "dY/dp", "d[DeltaPsi]/dp"]):
+      if verbose: print name
+      a = grads[idx]
+      b = an_grads[idx][i]
 
-  # compare FD with analytical calculations
-  from scitbx.math import five_number_summary
-  print "\n\nParameter", i
-  grads = (x_grads, y_grads, delpsi_grads)
+      abs_error = a - b
+      denom = a + b
 
-  for name, idx in zip(["X", "Y", "DeltaPsi"], (0, 1, 2)):
-    print name
-    rel_error = []
-    abs_error = []
-    for j, (a, b) in enumerate(zip(grads[idx], an_grads[idx][i])):
-      abs_error.append(a - b)
-      if abs(a + b) < 1.e-8:
+      fns = five_number_summary(abs_error)
+      if verbose: print "  summary of absolute errors: %9.6f %9.6f %9.6f " + \
+        "%9.6f %9.6f" % fns
+      assert flex.max(flex.abs(abs_error)) < 0.0003
+      # largest absolute error found to be about 0.00025 for dX/dp of
+      # Crystal0g_param_3. Reject outlying absolute errors and test again.
+      iqr = fns[3] - fns[1]
+      if iqr > 0.:
+        sel1 = abs_error < fns[3] + 1.5 * iqr
+        sel2 = abs_error > fns[1] - 1.5 * iqr
+        sel = sel1 & sel2
+        tst = flex.abs(abs_error.select(sel))
+        n_outliers = sel.count(False)
+        if verbose: print "  {0} outliers rejected, leaving greatest " + \
+          "absolute error: {1:9.6f}".format(n_outliers, flex.max(tst))
+        # largest absolute error now 0.000062
+        assert flex.max(tst) < 0.00007
+
+      # Completely skip parameters with FD gradients all zero (e.g. gradients of
+      # DeltaPsi for detector parameters)
+      sel1 = flex.abs(a) < 1.e-20
+      if sel1.all_eq(True):
         continue
-      else:
-        rel_error.append(2.*(a - b)/(a + b))
-        #rel_error.append((a - b)/max(abs(a), abs(b)))
-    if rel_error:
-      print "summary of relative errors: %9.6f %9.6f %9.6f %9.6f %9.6f" % five_number_summary(rel_error)
-    print "summary of absolute errors: %9.6f %9.6f %9.6f %9.6f %9.6f" % five_number_summary(abs_error)
 
-  #assert approx_equal(x_grads, an_grads[0][i], eps=5.e-6)
-  #assert approx_equal(y_grads, an_grads[1][i], eps=5.e-6)
-  #assert approx_equal(delpsi_grads, an_grads[2][i], eps=5.e-6)
+      # otherwise just remove particular reflections with FD or analytical
+      # gradients of zero from the relative error calculation. The calculation
+      # can't be done for these reflections
+      sel2 = flex.abs(b) < 1.e-20
+      sel = sel1 | sel2
+      remove_refs = reflections.select(sel)
+      # check not too many reflections were removed this way (< 5%)
+      assert len(remove_refs) < 0.05 * len(reflections)
 
-# return to the initial state
-pred_param.set_param_vals(p_vals)
+      # calculate relative errors
+      abs_error = abs_error.select(sel == False)
+      denom = denom.select(sel == False)
+      rel_error = 2. * abs_error/denom
 
-finish_time = time()
-print "Time Taken: ",finish_time - start_time
+      fns = five_number_summary(rel_error)
+      if verbose: print "  summary of relative errors: %9.6f %9.6f %9.6f " + \
+        "%9.6f %9.6f" % fns
 
-# if we got this far,
-print "OK"
+      # NB certain reflections give bad relative errors for parameters
+      # of the crystal model. For instance, reflections directed largely along
+      # the fast axis of the detector give poor dY/dp for Crystal0Phi1, the
+      # misset around the X axis. Other reflections are much better, indicating
+      # that the problem is not with the gradient calculation per se, but that
+      # the calculation breaks down for certain subclasses of reflection. This
+      # is not well understood yet.
+
+      # largest relative error found to be about 2.067 for dY/dp of
+      # Crystal0g_param_2.
+      assert flex.max(flex.abs(rel_error)) < 2.07
+      # Reject outlying relative errors and test again
+      iqr = fns[3] - fns[1]
+      if iqr > 0.:
+        sel1 = rel_error < fns[3] + 1.5 * iqr
+        sel2 = rel_error > fns[1] - 1.5 * iqr
+        sel = sel1 & sel2
+        tst = flex.abs(rel_error.select(sel))
+        n_outliers = sel.count(False)
+        if verbose: print "  {0} outliers rejected, leaving greatest " + \
+          "relative error: {1:9.6f}".format(n_outliers, flex.max(tst))
+        # largest relative error now 0.007169 for dX/dp of Beam0Mu1
+        assert flex.max(tst) < 0.0075
+
+  # return to the initial state
+  pred_param.set_param_vals(p_vals)
+
+if __name__ == "__main__":
+
+  # switch this to true to see summary output
+  run(verbose=False)
+  print "OK"
