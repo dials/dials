4b3df9c4ac7761edfc97706f6ffd729ade20ddf1
Graeme Winter <graeme.winter@gmail.com>
2015-03-26 10:42:47 +0000

    Make this work properly, usage:
    Graemes-MacBook-Pro:HotPixel graeme$ dials.find_hot_pixels datablock.json strong.pickle 
    The following parameters have been modified:
    
    input {
      datablock = datablock.json
      reflections = strong.pickle
    }
    
    Found 2 hot pixels

--- command_line/find_hot_pixels.py
+++ command_line/find_hot_pixels.py
@@ -1,36 +1,101 @@
 from __future__ import division
 
-def print_refl(refl):
+from libtbx.phil import command_line
+import iotbx.phil
+from dials.util.options import OptionParser
+from dials.util.options import flatten_reflections
+from dials.util.options import flatten_datablocks
+from dials.util.options import flatten_experiments
 
-  sbox = refl['shoebox'].data
-  mask = refl['shoebox'].mask
-  ndim = sbox.nd()
-  dims = sbox.focus()
+phil_scope = iotbx.phil.parse("""\
+output {
+  mask = hot_pixels.pickle
+    .type = path
+}
 
-  s = refl['shoebox']
+verbosity = 1
+  .type = int(value_min=0)
+  .help = "The verbosity level"
+""", process_includes=True)
 
-  if s.xsize() != 1 or s.ysize() != 1:
-    return
+help_message = '''
+Find hot pixels from custom dials.find_spots run, i.e.
 
-  print s.xoffset(), s.yoffset()
+  dials.find_hot_pixels datablock.json strong.pickle
 
+Will return a hot_pixel.pickle mask for use in integration.
+'''
+
+
+def run(args):
+  import libtbx.load_env
+  from libtbx.utils import Sorry
+  from dials.util import log
+  from logging import info
+  import cPickle as pickle
+  usage = "%s [options] datablock.json strong.pickle" % \
+    libtbx.env.dispatcher_name
+
+  parser = OptionParser(
+    usage=usage,
+    phil=phil_scope,
+    read_reflections=True,
+    read_datablocks=True,
+    check_format=False,
+    epilog=help_message)
+
+  params, options = parser.parse_args(show_diff_phil=True)
+
+  log.config(
+    params.verbosity, info='dials.find_hot_pixels.log',
+      debug='dials.find_hot_pixels.debug.log')
+
+  datablocks = flatten_datablocks(params.input.datablock)
+  reflections = flatten_reflections(params.input.reflections)
+
+  if len(datablocks) > 1:
+    raise Sorry("Only one DataBlock can be processed at a time")
+  else:
+    imagesets = datablocks[0].extract_imagesets()
+
+  if len(reflections) == 0:
+    raise Sorry("No reflection lists found in input")
+  if len(reflections) > 1:
+    raise Sorry("Multiple reflections lists provided in input")
+
+  assert(len(reflections) == 1)
+  reflections = reflections[0]
+
+  mask = hot_pixel_mask(imagesets[0], reflections)
+  pickle.dump(mask, open(params.output.mask, 'w'))
   return
 
+def hot_pixel_mask(imageset, reflections):
+  depth = imageset.get_array_range()[1] - imageset.get_array_range()[0]
+  xylist = filter_reflections(reflections, depth)
+
+  from dials.array_family import flex
+
+  mask = flex.bool(flex.grid(imageset.get_image_size()), True)
+
+  for x, y in xylist:
+    mask[x, y] = False
+
+  print 'Found %d hot pixels' % len(xylist)
+
+  return mask
+
+def filter_reflections(reflections, depth):
+  xylist = []
+
+  for i in range(len(reflections)):
+    refl = reflections[i]
+    s = refl['shoebox']
+    if s.zsize() == depth:
+      xylist.append((s.xoffset(), s.yoffset()))
+
+  return xylist
 
 if __name__ == '__main__':
-  import cPickle as pickle
   import sys
-  from dials.array_family import flex # import dependency
-  from libtbx.utils import Abort
-
-  if len(sys.argv) != 2:
-    raise Abort('exactly 1 reflection table must be specified')
-
-  try:
-    table = pickle.load(open(sys.argv[1]))
-  except Exception:
-    raise Abort('Error loading reflection table')
-
-
-  for i in range(len(table)):
-    print_refl(table[i])
+  run(sys.argv[1:])
