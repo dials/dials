58d44ee984ed5e6807e5d32c8e67e3bd4d6e1f68
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2015-04-07 13:02:15 +0000

    adding an exercise that will become the replacement of matplotlib for viewing 2D arrays

--- scratch/luiso_s/wx_toys/bitmap_from_numpy_hot_colour_palete.py
+++ scratch/luiso_s/wx_toys/bitmap_from_numpy_hot_colour_palete.py
@@ -10,15 +10,30 @@ def GetBitmap_from_np_array(data2d):
   img_array = numpy.zeros( (height ,width, 3),'uint8')
   print "data2d.max =", data2d.max()
 
-  div_scale = 255.0 / data2d.max()
+  div_scale = 256.0 * 3.0 / data2d.max()
   data2d_scale = numpy.multiply(data2d, div_scale)
   print "div_scale =", div_scale
   print "data2d_scale.max = ", data2d_scale.max()
-  int_data2d_scale = data2d_scale.astype(numpy.uint8)
-  #a.astype(numpy.int64)
-  img_array[:,:,0] = int_data2d_scale[:,:]
-  img_array[:,:,1] = int_data2d_scale[:,:]
-  img_array[:,:,2] = int_data2d_scale[:,:]
+  #int_data2d_scale = data2d_scale.astype(numpy.uint8)
+
+  for col in range(0, width):
+    for row in range(0, height):
+      if(data2d_scale[row,col] <= 255):
+        img_array[row,col,0] = data2d_scale[row,col]
+        img_array[row,col,1] = 0
+        img_array[row,col,2] = 0
+
+      elif( data2d_scale[row,col] > 255 and
+            data2d_scale[row,col] < 256 * 2):
+        img_array[row,col,0] = 255
+        img_array[row,col,1] = data2d_scale[row,col] - 256
+        img_array[row,col,2] = 0
+
+      else:
+        img_array[row,col,0] = 255
+        img_array[row,col,1] = 255
+        img_array[row,col,2] = data2d_scale[row,col] - 256 * 2 - 1
+
 
   print "img_array.max =", img_array.max()
   image = wx.EmptyImage(width,height)
@@ -31,8 +46,9 @@ def build_np_img(width=64, height=64):
   for x in range(0, width):
     for y in range(0, height):
       data2d[x,y] = numpy.sqrt(x*x + y*y)
-  #data2d[width/4:width*3/4,height/4:height*3/4] = 0
-  print "data2d.max =", data2d.max()
+
+  data2d[width*1/2:width*3/4,height/4:height*3/4] = data2d.max()
+  data2d[width/4:width*1/2,height/4:height*3/4] = 0
   return data2d
 
 class MyApp(wx.App):
@@ -52,7 +68,7 @@ class MyFrame(wx.Frame):
     # Attributes
     self.panel = wx.Panel(self)
 
-    data2d = build_np_img(width = 100, height = 200)
+    data2d = build_np_img(width = 500, height = 1200)
     bitmap = GetBitmap_from_np_array(data2d)
 
     self.bitmap = wx.StaticBitmap(self.panel, bitmap=bitmap)
