e9b284ec058472ef86e5329ddf6af400b2f29cb6
David Waterman <david.waterman@stfc.ac.uk>
2015-04-10 13:58:31 +0000

    accept two sets of reflections - the second will be matched to the first and only those that do not match will be displayed in red

--- scratch/dgw/prediction_relp_viewer.py
+++ scratch/dgw/prediction_relp_viewer.py
@@ -6,6 +6,10 @@ from dials.command_line.reciprocal_lattice_viewer import *
 
 class PredRelpViewer(ReciprocalLatticeViewer):
 
+  reflections2 = None
+  def load_reflections2(self, reflections2):
+    self.reflections2 = reflections2
+
   # modified from dials.algorithms.indexing.indexer to map predictions
   # from pixels to mm, rather than observations
   @staticmethod
@@ -62,11 +66,11 @@ class PredRelpViewer(ReciprocalLatticeViewer):
     if goniometer is not None and self.settings.reverse_phi:
       goniometer.set_rotation_axis([-i for i in goniometer.get_rotation_axis()])
 
-
     # set parameters to latest values
     self.dp.set_param_vals(self.detector_parameters)
     self.bp.set_param_vals(self.beam_parameters)
 
+    # map first set of points
     reflections = self.map_spots_pixel_to_mm_rad(
       self.reflections, self.detector, self.scan)
     self.map_centroids_to_reciprocal_space(reflections, self.detector,
@@ -80,6 +84,47 @@ class PredRelpViewer(ReciprocalLatticeViewer):
       self.settings_panel.d_min_ctrl.SetValue(self.settings.d_min)
     points = reflections['rlp'] * 100
     self.viewer.set_points(points)
+
+    # map second set of points
+    if self.reflections2 is not None:
+      reflections2 = self.map_spots_pixel_to_mm_rad(
+        self.reflections2, self.detector, self.scan)
+      self.map_centroids_to_reciprocal_space(reflections2, self.detector,
+        self.beam, goniometer)
+      points2 = reflections2['rlp'] * 100
+      self.viewer.set_points2(points2)
+
+  def create_viewer_panel(self):
+    self.viewer = MyGLWindow2(settings=self.settings, parent=self, size=(800,600),
+      style=wx.glcanvas.WX_GL_DOUBLEBUFFER,
+      #orthographic=True
+      )
+
+class MyGLWindow2(MyGLWindow):
+
+  points2 = flex.vec3_double()
+  def set_points2(self, points):
+    self.points2 = points
+    #self.points_display_list = None
+    #self.draw_points()
+    #self._compute_minimum_covering_sphere()
+    #if not self.GL_uninitialised:
+    #  self.fit_into_viewport()
+
+  def draw_points(self):
+    if (self.points_display_list is None):
+      self.points_display_list = gltbx.gl_managed.display_list()
+      self.points_display_list.compile()
+      glLineWidth(1)
+      print "first set of points:", len(self.points)
+      for point in self.points:
+        self.draw_cross_at(point)
+      print "second set of points:", len(self.points2)
+      for point in self.points2:
+        self.draw_cross_at(point, color=(1,0,0))
+      self.points_display_list.end()
+    self.points_display_list.call()
+
 
 def run(args):
 
@@ -99,6 +144,16 @@ def run(args):
   datablocks = flatten_datablocks(params.input.datablock)
   experiments = flatten_experiments(params.input.experiments)
   reflections = flatten_reflections(params.input.reflections)[0]
+  if len(params.input.reflections) == 2:
+    reflections2 = flatten_reflections(params.input.reflections)[1]
+  else:
+    reflections2 = None
+
+  # find the reflections in the second set that DO NOT match those in the
+  # first set
+  mask, _ = reflections2.match_with_reference(reflections)
+  reflections2 = reflections2.select(~mask)
+  print len(reflections2)
 
   if len(datablocks) == 0:
     if len(experiments) > 0:
@@ -120,6 +175,7 @@ def run(args):
   a.settings = params
   f = PredRelpViewer(
     None, -1, "Prediction reciprocal lattice viewer", size=(1024,768))
+  f.load_reflections2(reflections2)
   f.load_models(imageset, reflections)
   f.Show()
   a.SetTopWindow(f)
