3f59af89a0af038da415803066eafad888018f25
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2015-04-20 16:31:04 +0000

    adding missing file

---
+++ viewer/rgb_2D.h
@@ -0,0 +1,25 @@
+/*
+ * rgb_2d.h
+ *
+ *  Copyright (C) 2015 Diamond Light Source
+ *
+ *  Author: Luis Fuentes-Montero (Luiso)
+ *
+ *  This code is distributed under the BSD license, a copy of which is
+ *  included in the root directory of this package.
+ */
+
+#ifndef DIALS_RGB_IMG_BUILDER_H
+#define DIALS_RGB_IMG_BUILDER_H
+#include <iostream>
+namespace dials { namespace viewer { namespace boost_python {
+
+  int hello_tst_vw() {
+        std::cout << "hi there \n";
+        int a=15;
+        return a;
+  }
+
+}}}
+
+#endif
