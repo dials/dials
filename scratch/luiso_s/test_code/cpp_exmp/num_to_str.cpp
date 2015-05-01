e18842eba4d74da209d90986b67083989a7dca46
Luis Fuentes-Montero <luisodls@users.noreply.github.com>
2015-05-01 20:46:10 +0000

    adding a couple of examples of cpp code to my scratch area

---
+++ scratch/luiso_s/test_code/cpp_exmp/num_to_str.cpp
@@ -0,0 +1,18 @@
+/* sprintf example */
+#include <stdio.h>
+#include <iostream>
+//#include <string>     // std::string, std::to_string
+int main ()
+{
+  char asc_str [50];
+
+  int i_nm = 1235434567;
+  sprintf (asc_str, "%f ", i_nm);
+  std::cout << "\nasc_str = " << asc_str << "\n\n";
+
+  double dl_nm = 8876576.465654654;
+  sprintf (asc_str, "%f ", dl_nm);
+  std::cout << "\nasc_str = " << asc_str << "\n\n";
+
+  return 0;
+}
