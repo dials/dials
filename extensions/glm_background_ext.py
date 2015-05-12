0d5aa958b2fa3a1c2761f099c8d01782469e1f1b
James Parkhurst <james.parkhurst@diamond.ac.uk>
2015-05-12 13:26:43 +0000

    Added an extension

---
+++ extensions/glm_background_ext.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+#
+# simple_background_ext.py
+#
+#  Copyright (C) 2013 Diamond Light Source
+#
+#  Author: James Parkhurst
+#
+#  This code is distributed under the BSD license, a copy of which is
+#  included in the root directory of this package.
+from __future__ import division
+
+from dials.interfaces import BackgroundIface
+
+
+class SimpleBackgroundExt(BackgroundIface):
+  ''' An extension class implementing XDS background subtraction. '''
+
+  name = 'glm'
+
+  default = True
+
+  @classmethod
+  def phil(cls):
+    from libtbx.phil import parse
+    phil = parse('''
+                 
+      robust {
+        tuning_constant = 1.345
+          .type = float
+          .help = "The tuning constant for robust estimation"
+      }
+
+    ''')
+    return phil
+
+  def __init__(self, params, experiments):
+    '''
+    Initialise the algorithm.
+
+    :param params: The input parameters
+    :param experiments: The list of experiments
+
+    '''
+    from libtbx.phil import parse
+    from dials.algorithms.background.glm import BackgroundAlgorithm
+
+    # Create some default parameters
+    if params is None:
+      params = self.phil().fetch(parse('')).extract()
+    else:
+      params = params.integration.background.glm
+
+    # Create the algorithm
+    self._algorithm = BackgroundAlgorithm(
+      experiments,
+      tuning_constant=params.robust.tuning_constant)
+
+  def compute_background(self, reflections):
+    '''
+    Compute the background.
+
+    :param reflections: The list of reflections
+
+    '''
+    self._algorithm.compute_background(reflections)
