fba95134076d6f2caf17ede433371b762f7a415f
David Waterman <david.waterman@stfc.ac.uk>
2015-05-18 11:58:50 +0000

    Use angle along X axis, not image number. When multiple experiments are provided, instead of plotting to separate files, overlay each plot on the same file

--- command_line/plot_scan_varying_crystal.py
+++ command_line/plot_scan_varying_crystal.py
@@ -78,7 +78,6 @@ class Script(object):
       self.parser.print_help()
       raise Sorry("No experiments found in the input")
     experiments = flatten_experiments(params.input.experiments)
-    crystals = experiments.crystals()
 
     # Determine output path
     self._directory = os.path.join(params.output.directory,
@@ -94,29 +93,54 @@ class Script(object):
     self._e2 = params.orientation_decomposition.e2
     self._e3 = params.orientation_decomposition.e3
 
-    for icrystal, crystal in enumerate(crystals):
+    # cell plot
+    dat = []
+    for iexp, exp in enumerate(experiments):
 
-      icrystal_suffix = icrystal if len(crystals) > 1 else None
+      crystal = exp.crystal
+      scan = exp.scan
 
       if crystal.num_scan_points == 0:
         print "Ignoring scan-static crystal"
         continue
 
-      # cell plot
       scan_pts = range(crystal.num_scan_points)
       cells = [crystal.get_unit_cell_at_scan_point(t) for t in scan_pts]
-      dat = [(t,) + e.parameters() + (e.volume(),) \
-             for (t, e) in zip(scan_pts, cells)]
-      self.plot_cell(dat, icrystal_suffix)
-
+      cell_params = [e.parameters() for e in cells]
+      a,b,c,aa,bb,cc = zip(*cell_params)
+      phi = [scan.get_angle_from_array_index(t) for t in scan_pts]
+      vol = [e.volume() for e in cells]
+      cell_dat = {'phi':phi,
+                  'a':a,
+                  'b':b,
+                  'c':c,
+                  'alpha':aa,
+                  'beta':bb,
+                  'gamma':cc,
+                  'volume':vol}
       if self._debug:
-        print "Crystal {0}".format(icrystal)
-        print "Image\ta\tb\tc\talpha\tbeta\tgamma\tVolume"
-        msg = "\t".join(["%.3f"] * 8)
-        for line in dat:
-          print msg % line
+        print "Crystal in Experiment {0}".format(iexp)
+        print "Phi\ta\tb\tc\talpha\tbeta\tgamma\tVolume"
+        msg = "{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}"
+        line_dat = zip(phi, a, b, c, aa, bb, cc, vol)
+        for line in line_dat:
+          print msg.format(*line)
+      dat.append(cell_dat)
+    self.plot_cell(dat)
 
-      # orientation plot
+    # orientation plot
+    dat = []
+    for iexp, exp in enumerate(experiments):
+
+      crystal = exp.crystal
+      scan = exp.scan
+
+      if crystal.num_scan_points == 0:
+        print "Ignoring scan-static crystal"
+        continue
+
+      scan_pts = range(crystal.num_scan_points)
+      phi = [scan.get_angle_from_array_index(t) for t in scan_pts]
       Umats = [crystal.get_U_at_scan_point(t) for t in scan_pts]
       if params.orientation_decomposition.relative_to_static_orientation:
         # factor out static U
@@ -127,17 +151,22 @@ class Script(object):
       # method
       angles = [solve_r3_rotation_for_angles_given_axes(U,
         self._e3, self._e2, self._e1, deg=True) for U in Umats]
-      dat = [(t,) + a for (t, a) in zip(scan_pts, angles)]
-      self.plot_orientation(dat, icrystal_suffix)
-
+      phi3, phi2, phi1 = zip(*angles)
+      angle_dat = {'phi':phi,
+                   'phi3':phi3,
+                   'phi2':phi2,
+                   'phi1':phi1}
       if self._debug:
-        print "Crystal {0}".format(icrystal)
+        print "Crystal in Experiment {0}".format(iexp)
         print "Image\tphi3\tphi2\tphi1"
-        msg = "\t".join(["%.6f"] * 4)
-        for line in dat:
-          print msg % line
+        msg = "{0}\t{1}\t{2}\t{3}"
+        line_dat = zip(phi, phi3, phi2, phi1)
+        for line in line_dat:
+          print msg.format(*line)
+      dat.append(angle_dat)
+    self.plot_orientation(dat)
 
-  def plot_cell(self, dat, icrystal=None):
+  def plot_cell(self, dat):
     try:
       import matplotlib.pyplot as plt
       import matplotlib.gridspec as gridspec
@@ -146,68 +175,72 @@ class Script(object):
       return None
 
     from math import floor, ceil
-    image, a, b, c, alpha, beta, gamma, volume = zip(*dat)
     fig = plt.figure(figsize=(13, 10))
     gs = gridspec.GridSpec(4, 2, wspace=0.4, hspace=0.6)
 
     ax = plt.subplot(gs[0, 0])
     ax.ticklabel_format(useOffset=False)
-    plt.plot(image, a)
-    plt.xlabel('Image')
+    for cell in dat:
+      plt.plot(cell['phi'], cell['a'])
+    plt.xlabel(r'rotation angle $\left(^\circ\right)$')
     plt.ylabel(r'length $\left(\AA\right)$')
     plt.title('a')
 
     ax = plt.subplot(gs[0, 1])
     ax.ticklabel_format(useOffset=False)
-    plt.plot(image, alpha)
-    plt.axis(ymin=floor(min(alpha) - 0.05), ymax=ceil(max(alpha) + 0.05))
-    plt.xlabel('Image')
+    for cell in dat:
+      plt.plot(cell['phi'], cell['alpha'])
+      plt.axis(ymin=floor(min(cell['alpha']) - 0.05), ymax=ceil(max(cell['alpha']) + 0.05))
+    plt.xlabel(r'rotation angle $\left(^\circ\right)$')
     plt.ylabel(r'angle $\left(^\circ\right)$')
     plt.title(r'$\alpha$')
 
     ax = plt.subplot(gs[1, 0])
     ax.ticklabel_format(useOffset=False)
-    plt.plot(image, b)
-    plt.xlabel('Image')
+    for cell in dat:
+      plt.plot(cell['phi'], cell['b'])
+    plt.xlabel(r'rotation angle $\left(^\circ\right)$')
     plt.ylabel(r'length $\left(\AA\right)$')
     plt.title('b')
 
     ax = plt.subplot(gs[1, 1])
     ax.ticklabel_format(useOffset=False)
-    plt.plot(image, beta)
-    plt.axis(ymin=floor(min(beta)- 0.05), ymax=ceil(max(beta) + 0.05))
-    plt.xlabel('Image')
+    for cell in dat:
+      plt.plot(cell['phi'], cell['beta'])
+      plt.axis(ymin=floor(min(cell['beta'])- 0.05), ymax=ceil(max(cell['beta']) + 0.05))
+    plt.xlabel(r'rotation angle $\left(^\circ\right)$')
     plt.ylabel(r'angle $\left(^\circ\right)$')
     plt.title(r'$\beta$')
 
     ax = plt.subplot(gs[2, 0])
     ax.ticklabel_format(useOffset=False)
-    plt.plot(image, c)
-    plt.xlabel('Image')
+    for cell in dat:
+      plt.plot(cell['phi'], cell['c'])
+    plt.xlabel(r'rotation angle $\left(^\circ\right)$')
     plt.ylabel(r'length $\left(\AA\right)$')
     plt.title('c')
 
     ax = plt.subplot(gs[2, 1])
     ax.ticklabel_format(useOffset=False)
-    plt.plot(image, gamma)
-    plt.axis(ymin=floor(min(gamma) - 0.05), ymax=ceil(max(gamma) + 0.05))
-    plt.xlabel('Image')
+    for cell in dat:
+      plt.plot(cell['phi'], cell['gamma'])
+      plt.axis(ymin=floor(min(cell['gamma']) - 0.05), ymax=ceil(max(cell['gamma']) + 0.05))
+    plt.xlabel(r'rotation angle $\left(^\circ\right)$')
     plt.ylabel(r'angle $\left(^\circ\right)$')
     plt.title(r'$\gamma$')
 
     ax = plt.subplot2grid((4,2), (3, 0), colspan=2)
     ax.ticklabel_format(useOffset=False)
-    plt.plot(image, volume)
-    plt.xlabel('Image')
+    for cell in dat:
+      plt.plot(cell['phi'], cell['volume'])
+    plt.xlabel(r'rotation angle $\left(^\circ\right)$')
     plt.ylabel(r'volume $\left(\AA^3\right)$')
     plt.title('Cell volume')
 
     basename = os.path.join(self._directory, "unit_cell")
-    if icrystal is not None: basename += "_{0}".format(
-      icrystal)
     plt.savefig(basename + self._format)
 
-  def plot_orientation(self, dat, icrystal=None):
+  def plot_orientation(self, dat):
     try:
       import matplotlib.pyplot as plt
       import matplotlib.gridspec as gridspec
@@ -216,34 +249,34 @@ class Script(object):
       return None
 
     from math import floor, ceil
-    image, phi3, phi2, phi1 = zip(*dat)
     fig = plt.figure(figsize=(13, 10))
     gs = gridspec.GridSpec(3, 1, wspace=0.4, hspace=0.6)
 
     ax = plt.subplot(gs[0, 0])
     ax.ticklabel_format(useOffset=False)
-    plt.plot(image, phi1)
-    plt.xlabel('Image')
+    for ori in dat:
+      plt.plot(ori['phi'], ori['phi1'])
+    plt.xlabel(r'rotation angle $\left(^\circ\right)$')
     plt.ylabel(r'angle $\left(^\circ\right)$')
     plt.title(r'$\phi_1$')
 
     ax = plt.subplot(gs[1, 0])
     ax.ticklabel_format(useOffset=False)
-    plt.plot(image, phi2)
-    plt.xlabel('Image')
+    for ori in dat:
+      plt.plot(ori['phi'], ori['phi2'])
+    plt.xlabel(r'rotation angle $\left(^\circ\right)$')
     plt.ylabel(r'angle $\left(^\circ\right)$')
     plt.title(r'$\phi_2$')
 
     ax = plt.subplot(gs[2, 0])
     ax.ticklabel_format(useOffset=False)
-    plt.plot(image, phi3)
-    plt.xlabel('Image')
+    for ori in dat:
+      plt.plot(ori['phi'], ori['phi3'])
+    plt.xlabel(r'rotation angle $\left(^\circ\right)$')
     plt.ylabel(r'angle $\left(^\circ\right)$')
     plt.title(r'$\phi_3$')
 
     basename = os.path.join(self._directory, "orientation")
-    if icrystal is not None: basename += "_{0}".format(
-      icrystal)
     plt.savefig(basename + self._format)
 
 if __name__ == '__main__':
