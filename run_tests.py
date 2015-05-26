56f344a573005828419ddb923c0e14d4ec5204ee
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2015-05-26 20:34:19 +0000

    Run libtbx.import_all_ext in dials

--- run_tests.py
+++ run_tests.py
@@ -7,6 +7,7 @@ tst_list = (
     "$D/test/command_line/tst_find_spots_server_client.py",
     ["$D/test/command_line/tst_dials_process.py", "1"],
     ["$D/test/command_line/tst_dials_process.py", "2"],
+    "$D/test/tst_libtbx_import.py",
     "$D/test/tst_scan_varying_integration_bug.py",
     "$D/test/tst_phil.py",
     "$D/test/framework/tst_interface.py",
