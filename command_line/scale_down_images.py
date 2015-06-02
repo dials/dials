4b31d7350835b68bcf50f1c8cd71a0dd595d44a1
Graeme Winter <graeme.winter@gmail.com>
2015-06-02 12:35:07 +0000

    Threaded version of image scaling; much quicker

---
+++ command_line/scale_down_images.py
@@ -0,0 +1,33 @@
+from __future__ import division
+
+def nproc():
+  from libtbx.introspection import number_of_processors
+  return number_of_processors(return_value_if_unknown=-1)
+
+def joiner(args):
+  from dials.util.scale_down_image import scale_down_image
+  scale_down_image(*args)
+
+def scale_down_images(in_template, out_template, start, end, scale_factor):
+  from multiprocessing import Pool
+
+  jobs = [(in_template % j, out_template % j, scale_factor) for j in
+          range(start, end + 1)]
+
+  pool = Pool(processes=nproc())
+  result = pool.map_async(joiner, jobs)
+  result.get()
+
+  return result
+
+if __name__ == '__main__':
+  import sys
+  if len(sys.argv) != 6:
+    raise RuntimeError, '%s in_\%04d.cbf out_\%04d.cbf start end scale' % \
+      sys.argv[0]
+  in_template = sys.argv[1]
+  out_template = sys.argv[2]
+  start = int(sys.argv[3])
+  end = int(sys.argv[4])
+  scale_factor = float(sys.argv[5])
+  scale_down_images(in_template, out_template, start, end, scale_factor)
