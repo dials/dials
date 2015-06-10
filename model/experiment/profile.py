04e6c83a7eeab9262c2256da5a83260f2cff8c81
James Parkhurst <james.parkhurst@diamond.ac.uk>
2015-06-10 11:09:51 +0000

    Updated interface a bit

--- model/experiment/profile.py
+++ model/experiment/profile.py
@@ -10,18 +10,31 @@
 #  included in the root directory of this package.
 
 from __future__ import division
-from dxtbx.model.profile import Profile as ProfileBase
+from dxtbx.model.profile import ProfileModelBaseIface
 from abc import abstractmethod
 
 
-class ProfileModelIface(ProfileBase):
+class ProfileModelIface(ProfileBaseIface):
   '''
   The interface definition for a list of profile models.
 
   '''
 
+  @classmethod
+  def create(Class, params, experiment, reflections):
+    '''
+    Create the profile model from data.
+
+    :param params: The phil parameters
+    :param experiment: The experiment
+    :param reflections: The reflections
+    :return: An instance of the profile model
+
+    '''
+    return None
+
   @abstractmethod
-  def predict_reflections(self, experiment, **kwargs):
+  def predict_reflections(self, experiment):
     '''
     Given an experiment, predict the reflections.
 
@@ -31,7 +44,7 @@ class ProfileModelIface(ProfileBase):
     pass
 
   @abstractmethod
-  def compute_partiality(self, experiment, reflections, **kwargs):
+  def compute_partiality(self, experiment, reflections):
     '''
     Given an experiment and list of reflections, compute the partiality of the
     reflections
@@ -43,15 +56,18 @@ class ProfileModelIface(ProfileBase):
     pass
 
   @abstractmethod
-  def compute_bbox(self, experiment, reflections, **kwargs):
+  def compute_bbox(self, experiment, reflections):
     ''' Given an experiment and list of reflections, compute the
     bounding box of the reflections on the detector (and image frames).
+
+    :param experiment: The experiment
+    :param reflections: The reflection table
 
     '''
     pass
 
   @abstractmethod
-  def compute_mask(self, experiment, reflections, **kwargs):
+  def compute_mask(self, experiment, reflections):
     '''
     Given an experiment and list of reflections, compute the
     foreground/background mask of the reflections.
@@ -62,3 +78,12 @@ class ProfileModelIface(ProfileBase):
     '''
     pass
 
+  @classmethod
+  def profile_fitting_class(Class):
+    '''
+    Get the profile fitting algorithm associated with this profile model
+
+    :return: The profile fitting class
+
+    '''
+    return None
