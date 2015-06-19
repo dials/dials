f644710182c2c36860d0ee7a4adcabe9134329be
David Waterman <david.waterman@stfc.ac.uk>
2015-06-19 13:22:50 +0000

    A more interesting test of Mahalanobis distance with 3 dimensions and 10 observations. Result matches the R function mahalanobis, but we are calculating inefficiently

--- scratch/dgw/fast_mcd.py
+++ scratch/dgw/fast_mcd.py
@@ -3,24 +3,27 @@
 """Testing functions for multivariate outlier rejection by the FAST-MCD
 algorithm"""
 
-# Want implementation of Mahalanobis distance that matches the test in R:
+# Want implementation of Mahalanobis distance that matches this R session:
 
-#> ma <- cbind(1:6, 1:3)
-#> ma
-#     [,1] [,2]
-#[1,]    1    1
-#[2,]    2    2
-#[3,]    3    3
-#[4,]    4    1
-#[5,]    5    2
-#[6,]    6    3
-#> (S <-  var(ma))
-#     [,1] [,2]
-#[1,]  3.5  0.8
-#[2,]  0.8  0.8
-#> ?var
-#> mahalanobis(c(0, 0), 1:2, S)
-#[1] 5.37037
+#> x1 <- round(rnorm(10,3), 3)
+#> x2 <- round(x1 + rnorm(10), 3)
+#> x3 <- round(x2 + runif(10), 3)
+#> x1
+# [1] 3.853 2.401 2.253 3.067 1.887 3.293 3.995 2.559 2.785 2.228
+#> x2
+# [1] 4.294 1.915 1.315 4.641 1.611 2.838 3.696 1.337 2.853 2.434
+#> x3
+# [1] 4.785 2.352 2.023 4.978 2.329 3.101 4.494 2.204 3.468 3.075
+#> obs <- cbind(x1, x2, x3)
+#> S <- var(obs)
+#> S
+#          x1        x2       x3
+#x1 0.5020374 0.6667232 0.633355
+#x2 0.6667232 1.4434718 1.326026
+#x3 0.6333550 1.3260262 1.248315
+#> mahalanobis(obs, c(mean(x1), mean(x2), mean(x3)), S)
+# [1] 2.1838336 1.9673401 1.3335029 4.9191627 2.1246818 5.3297995 4.9022487
+# [8] 2.5335913 0.1952562 1.5105832
 
 from scitbx.array_family import flex
 
@@ -31,8 +34,10 @@ def sample_covariance(a, b):
   assert len(b) == N
   return flex.sum((a - flex.mean(a)) * (b - flex.mean(b))) / (N - 1)
 
-a = flex.double((1, 2, 3, 4, 5, 6))
-b = flex.double((1, 2, 3, 1, 2, 3))
+x1 = flex.double((3.853, 2.401, 2.253, 3.067, 1.887, 3.293, 3.995, 2.559, 2.785, 2.228))
+x2 = flex.double((4.294, 1.915, 1.315, 4.641, 1.611, 2.838, 3.696, 1.337, 2.853, 2.434))
+x3 = flex.double((4.785, 2.352, 2.023, 4.978, 2.329, 3.101, 4.494, 2.204, 3.468, 3.075))
+
 
 def cov(*args):
   """Calculate covariance matrix between the arguments (should be flex.double
@@ -50,23 +55,25 @@ def cov(*args):
   cov.matrix_copy_upper_to_lower_triangle_in_place()
   return cov
 
-# observation matrix and its transpose
-# FIXME probably better to create the output matrix once and use paste column in
-# place to avoid excessive copying when ndim > 2
-obs_t = flex.double.concatenate(a,b)
-obs_t.reshape(flex.grid(2,6))
-obs = obs_t.matrix_transpose()
+# observation matrix (shifted by means) and its transpose
+obs = flex.double((flex.grid(len(x1), 3)))
+obs.matrix_paste_column_in_place(x1 - flex.mean(x1), 0)
+obs.matrix_paste_column_in_place(x2 - flex.mean(x2), 1)
+obs.matrix_paste_column_in_place(x3 - flex.mean(x3), 2)
+obs_t = obs.matrix_transpose()
 
 # covariance matrix
-covmat = cov(a,b)
+covmat = cov(x1, x2, x3)
 
-# the R test looks at a single observation of (0,0) with centres of (1,2)
-vec = flex.double((0-1, 0-2))
-vec.reshape(flex.grid(1,2))
-vec_t = vec.matrix_transpose()
+# test Mahalanobis distance. This is an inefficient approach to calculating this
+# because we do a full matrix product when we only want the diagonal elements of
+# the result. More efficient approaches exist. See e.g.
+# http://blogs.sas.com/content/iml/2012/02/22/how-to-compute-mahalanobis-distance-in-sas.html
+maha=((obs.matrix_multiply(covmat.matrix_inversion())).matrix_multiply(obs_t)).matrix_diagonal()
 
-# test Mahalanobis distance
-maha=(vec.matrix_multiply(covmat.matrix_inversion())).matrix_multiply(vec_t)
-assert round(maha[0],6) == 5.37037
+# NB This is actually the squared Mahalanobis distance!
 
-
+from libtbx.test_utils import approx_equal
+R_result = [2.1838336, 1.9673401, 1.3335029, 4.9191627, 2.1246818,
+            5.3297995, 4.9022487, 2.5335913, 0.1952562, 1.5105832]
+assert approx_equal(list(maha), R_result)
