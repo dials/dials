aa6673dec744aa8ab48e514f8b0f940526027470
Richard Gildea <richard.gildea@diamond.ac.uk>
2015-06-20 17:44:17 +0000

    work on automatically determining a suitable global_threshold that excludes most of the non-signal pixels without excluding the signal pixels: may be useful for certain ccd detectors

--- extensions/kabsch_spotfinder_threshold_ext.py
+++ extensions/kabsch_spotfinder_threshold_ext.py
@@ -12,6 +12,7 @@
 from __future__ import division
 from libtbx.utils import Sorry
 from dials.interfaces import SpotFinderThresholdIface
+from logging import info
 
 
 class KabschSpotFinderThresholdExt(SpotFinderThresholdIface):
@@ -79,15 +80,7 @@ class KabschSpotFinderThresholdExt(SpotFinderThresholdIface):
     :param params: The input parameters
 
     '''
-    from dials.algorithms.peak_finding.threshold import XDSThresholdStrategy
-    self._algorithm = XDSThresholdStrategy(
-      kernel_size=params.spotfinder.threshold.xds.kernel_size,
-      gain=params.spotfinder.threshold.xds.gain,
-      mask=params.spotfinder.lookup.mask,
-      n_sigma_b=params.spotfinder.threshold.xds.sigma_background,
-      n_sigma_s=params.spotfinder.threshold.xds.sigma_strong,
-      min_count=params.spotfinder.threshold.xds.min_local,
-      global_threshold=params.spotfinder.threshold.xds.global_threshold)
+    self.params = params
 
   def compute_threshold(self, image, mask):
     '''
@@ -98,4 +91,90 @@ class KabschSpotFinderThresholdExt(SpotFinderThresholdIface):
     :returns: A boolean mask showing foreground/background pixels
 
     '''
+
+    import libtbx
+    params = self.params
+    if params.spotfinder.threshold.xds.global_threshold is libtbx.Auto:
+      params.spotfinder.threshold.xds.global_threshold \
+        = int(estimate_global_threshold(image, mask))
+      info("Setting global_threshold: %i" %(
+        params.spotfinder.threshold.xds.global_threshold))
+
+    from dials.algorithms.peak_finding.threshold import XDSThresholdStrategy
+    self._algorithm = XDSThresholdStrategy(
+      kernel_size=params.spotfinder.threshold.xds.kernel_size,
+      gain=params.spotfinder.threshold.xds.gain,
+      mask=params.spotfinder.lookup.mask,
+      n_sigma_b=params.spotfinder.threshold.xds.sigma_background,
+      n_sigma_s=params.spotfinder.threshold.xds.sigma_strong,
+      min_count=params.spotfinder.threshold.xds.min_local,
+      global_threshold=params.spotfinder.threshold.xds.global_threshold)
+
     return self._algorithm(image, mask)
+
+def estimate_global_threshold(image, mask=None):
+
+  from scitbx.array_family import flex
+  from scitbx import matrix
+
+  n_above_threshold = flex.size_t()
+  threshold = flex.double()
+  for i in range(1, 20):
+    g = 1.5**i
+    g = int(g)
+    n_above_threshold.append((image > g).count(True))
+    threshold.append(g)
+
+  # Find the elbow point of the curve, in the same manner as that used by
+  # distl spotfinder for resolution method 1 (Zhang et al 2006).
+  # See also dials/algorithms/peak_finding/per_image_analysis.py
+
+  x = threshold.as_double()
+  y = n_above_threshold.as_double()
+  slopes = (y[-1] - y[:-1])/(x[-1] - x[:-1])
+  p_m = flex.min_index(slopes)
+
+  x1 = matrix.col((x[p_m], y[p_m]))
+  x2 = matrix.col((x[-1], y[-1]))
+
+  gaps = flex.double()
+  v = matrix.col(((x2[1] - x1[1]), -(x2[0] - x1[0]))).normalize()
+
+  for i in range(p_m, len(x)):
+    x0 = matrix.col((x[i], y[i]))
+    r = x1 - x0
+    g = abs(v.dot(r))
+    gaps.append(g)
+
+  mv = flex.mean_and_variance(gaps)
+  s = mv.unweighted_sample_standard_deviation()
+
+  p_k = flex.max_index(gaps)
+  g_k = gaps[p_k]
+  p_g = p_k
+
+  #x_g = x[p_g + p_m]
+  #y_g = y[p_g + p_m]
+
+  #x_g = x[p_g + p_m -1]
+  #y_g = y[p_g + p_m -1]
+
+  # more conservative, choose point 2 left of the elbow point
+  x_g = x[p_g + p_m -2]
+  y_g = y[p_g + p_m -2]
+
+  #from matplotlib import pyplot
+  #pyplot.scatter(threshold, n_above_threshold)
+  ##for i in range(len(threshold)-1):
+    ##pyplot.plot([threshold[i], threshold[-1]],
+                ##[n_above_threshold[i], n_above_threshold[-1]])
+  ##for i in range(1, len(threshold)):
+    ##pyplot.plot([threshold[0], threshold[i]],
+                ##[n_above_threshold[0], n_above_threshold[i]])
+  #pyplot.plot(
+    #[threshold[p_m], threshold[-1]], [n_above_threshold[p_m], n_above_threshold[-1]])
+  #pyplot.plot(
+    #[x_g, threshold[-1]], [y_g, n_above_threshold[-1]])
+  #pyplot.show()
+
+  return x_g
