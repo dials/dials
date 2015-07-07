675451255fb748c8d9334cc05caed329e2a3a27e
David Waterman <david.waterman@stfc.ac.uk>
2015-07-07 10:30:41 +0000

    MCD version of CentroidOutlier

---
+++ algorithms/refinement/outlier_detection/mcd.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+#
+#  mcd.py
+#
+#  Copyright (C) 2015 Diamond Light Source and STFC Rutherford Appleton
+#                     Laboratory, UK.
+#
+#  Author: David Waterman
+#
+#  This code is distributed under the BSD license, a copy of which is
+#  included in the root directory of this package.
+
+from __future__ import division
+from dials.array_family import flex
+from dials.algorithms.refinement.outlier_detection import CentroidOutlier
+from dials.algorithms.statistics.fast_mcd import FastMCD, maha_dist_sq
+from dials_refinement_helpers_ext import qchisq
+
+class MCD(CentroidOutlier):
+  """Implementation of the CentroidOutlier class using Mahalanobis distance
+  calculated using the robust location and scatter estimate from the Minimum
+  Covariance Determinant estimate."""
+
+  def __init__(self, cols=["x_resid", "y_resid", "phi_resid"],
+               min_num_obs=20,
+               alpha=0.5,
+               max_n_groups=5,
+               min_group_size=300,
+               n_trials=500,
+               k1=2,
+               k2=2,
+               k3=100,
+               threshold_probability=0.975):
+
+    CentroidOutlier.__init__(self,
+      cols=["x_resid", "y_resid", "phi_resid"],
+      min_num_obs=20)
+
+    # Keep the FastMCD options here
+    self._alpha = alpha
+    self._max_n_groups = max_n_groups
+    self._min_group_size = min_group_size
+    self._n_trials = n_trials
+    self._k1 = k1
+    self._k2 = k2
+    self._k3 = k3
+
+    # Calculate Mahalanobis distance threshold
+    df = len(cols)
+    self._mahasq_cutoff = qchisq(threshold_probability, df)
+
+    return
+
+  def _detect_outliers(self, cols):
+
+    outliers = flex.bool(len(cols[0]), False)
+
+    fast_mcd = FastMCD(cols,
+                       alpha = self._alpha,
+                       max_n_groups = self._max_n_groups,
+                       min_group_size = self._min_group_size,
+                       n_trials = self._n_trials,
+                       k1 = self._k1,
+                       k2 = self._k2,
+                       k3 = self._k3)
+
+    # get location and MCD scatter estimate
+    T, S = fast_mcd.get_corrected_T_and_S()
+
+    # get squared Mahalanobis distances
+    d2s = maha_dist_sq(cols, T, S)
+
+    # compare to the threshold
+    outliers = d2s > self._mahasq_cutoff
+
+    return outliers
