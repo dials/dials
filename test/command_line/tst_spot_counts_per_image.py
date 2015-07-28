d325f5168f1fe83f51e66fcc71dc7ee42ee6eb43
Richard Gildea <richard.gildea@diamond.ac.uk>
2015-07-28 14:16:24 +0000

    fix test

--- test/command_line/tst_spot_counts_per_image.py
+++ test/command_line/tst_spot_counts_per_image.py
@@ -29,7 +29,7 @@ def run():
   assert os.path.exists("spot_counts.png")
 
   assert (
-    "|image | #spots | #spots_no_ice | #spots_4A | total_intensity |" + \
+    "|image | #spots | #spots_no_ice | total_intensity |" + \
     " d_min | d_min (distl method 1) | d_min (distl method 2)|"
     in result.stdout_lines), result.stdout_lines
 
