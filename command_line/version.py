524c3106ffed08ee1eda838a44dccfd788d41c65
Graeme Winter <graeme.winter@gmail.com>
2015-09-14 12:42:29 +0000

    Also print location where DIALS is coming from

--- command_line/version.py
+++ command_line/version.py
@@ -1,5 +1,11 @@
 from __future__ import division
 
-from dials.util.version import dials_version
+def version():
+  from dials.util.version import dials_version
+  import dials
+  import os
 
-print dials_version()
+  print dials_version()
+  print "Installed in: %s" % os.path.split(dials.__file__)[0]
+
+version()
