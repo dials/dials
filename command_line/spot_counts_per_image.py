8b357f7c9b4514e19380c5298da2517f378fd22f
Richard Gildea <richard.gildea@diamond.ac.uk>
2015-10-02 12:39:59 +0000

    allow experiments.json as alternative input to datablock.json

--- command_line/spot_counts_per_image.py
+++ command_line/spot_counts_per_image.py
@@ -2,7 +2,8 @@ from __future__ import division
 # LIBTBX_PRE_DISPATCHER_INCLUDE_SH export BOOST_ADAPTBX_FPE_DEFAULT=1
 
 from dials.util.options import OptionParser
-from dials.util.options import flatten_reflections, flatten_datablocks
+from dials.util.options \
+     import flatten_reflections, flatten_datablocks, flatten_experiments
 from dials.algorithms.peak_finding import per_image_analysis
 
 import iotbx.phil
@@ -23,6 +24,7 @@ def run(args):
   parser = OptionParser(
     read_reflections=True,
     read_datablocks=True,
+    read_experiments=True,
     phil=phil_scope,
     check_format=False)
   from libtbx.utils import Abort
@@ -30,14 +32,21 @@ def run(args):
   params, options = parser.parse_args(show_diff_phil=False)
   reflections = flatten_reflections(params.input.reflections)
   datablocks = flatten_datablocks(params.input.datablock)
+  experiments = flatten_experiments(params.input.experiments)
 
   if len(reflections) != 1:
     raise Abort('exactly 1 reflection table must be specified')
   if len(datablocks) != 1:
-    raise Abort('exactly 1 datablock must be specified')
+    if len(experiments):
+      if len(experiments.imagesets()) != 1:
+        raise Abort('exactly 1 datablock must be specified')
+      imageset = experiments.imagesets()[0]
+    else:
+      raise Abort('exactly 1 datablock must be specified')
+  else:
+    imageset = datablocks[0].extract_imagesets()[0]
 
   reflections = reflections[0]
-  imageset = datablocks[0].extract_imagesets()[0]
 
   if params.id is not None:
     reflections = reflections.select(reflections['id'] == params.id)
