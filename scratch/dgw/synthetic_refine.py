21b8ba30e15f326121c1dd6c0047f5b8c8d6778f
David Waterman <dgwaterman@gmail.com>
2015-10-15 15:32:28 +0100

    More general version of synthetic_refine, for use in investigating error propagation

--- scratch/dgw/synthetic_refine.py
+++ scratch/dgw/synthetic_refine.py
@@ -24,6 +24,10 @@ from libtbx.test_utils import not_approx_equal
 from scitbx import matrix
 from scitbx.array_family import flex
 from logging import info, debug
+from copy import deepcopy
+import random
+
+from dials.algorithms.refinement import RefinerFactory
 
 # Model parameterisations
 from dials.algorithms.refinement.parameterisation.detector_parameters import \
@@ -45,115 +49,109 @@ from dials.algorithms.refinement.prediction import ScansRayPredictor, \
 from dials.algorithms.spot_prediction import ray_intersection
 from cctbx.sgtbx import space_group, space_group_symbols
 
-def generate_reflections(experiment):
+class ExperimentsPerturber(object):
+  '''Perturb the models in an experiment list. For simplicity create a complete
+  Refiner for this, which will just be used as a carrier for the parameterised
+  models'''
 
-  # copy the experiment to avoid modifying the original models
-  from copy import deepcopy
-  experiment = deepcopy(experiment)
+  def __init__(self, experiments):
 
-  detector = experiment.detector
-  beam = experiment.beam
-  goniometer = experiment.goniometer
-  crystal = experiment.crystal
-  scan = experiment.scan
-  assert [goniometer, scan].count(None) == 0
+    self.dummy_reflections = generate_reflections(experiments)
 
-  from dxtbx.model.experiment.experiment_list import \
-    ExperimentList
-  experiments = ExperimentList()
-  experiments.append(experiment)
+    # use default options for Refiners built internally
+    phil_scope = parse('''
+    include scope dials.algorithms.refinement.refiner.phil_scope
+    ''', process_includes=True)
+    self.params = phil_scope.extract()
 
-  ###########################
-  # Parameterise the models #
-  ###########################
+    # changes for building internal Refiners
+    self.params.refinement.reflections.outlier.algorithm="null"
+    self.params.refinement.reflections.random_seed=None
+    self.params.refinement.verbosity=0
 
-  det_param = DetectorParameterisationSinglePanel(detector)
-  s0_param = BeamParameterisation(beam, goniometer)
-  xlo_param = CrystalOrientationParameterisation(crystal)
-  xluc_param = CrystalUnitCellParameterisation(crystal)
+    self.original_experiments = experiments
+    return
 
-  # Fix beam to the X-Z plane (imgCIF geometry), fix wavelength
-  s0_param.set_fixed([True, False, True])
+  def random_perturbation(self, fraction=0.05):
+    '''randomly perturb each model parameter value by an amount drawn from a
+    normal distribution with sigma equal to fraction*value'''
 
-  ################################
-  # Apply known parameter shifts #
-  ################################
+    refiner = RefinerFactory.from_parameters_data_experiments(
+      self.params, self.dummy_reflections, self.original_experiments)
 
-  # shift detector by 1.0 mm each translation and 2 mrad each rotation
-  det_p_vals = det_param.get_param_vals()
-  p_vals = [a + b for a, b in zip(det_p_vals,
-                                  [1.0, 1.0, 1.0, 2., 2., 2.])]
-  det_param.set_param_vals(p_vals)
+    pp = refiner._pred_param
+    vals = pp.get_param_vals()
+    sigmas = [fraction * val for val in vals]
+    new_vals = [random.gauss(mu, sig) for (mu, sig) in zip(vals, sigmas)]
+    pp.set_param_vals(new_vals)
 
-  # shift beam by 2 mrad in free axis
-  s0_p_vals = s0_param.get_param_vals()
-  p_vals = list(s0_p_vals)
+    return refiner.get_experiments()
 
-  p_vals[0] += 2.
-  s0_param.set_param_vals(p_vals)
+  def known_perturbation(self, fraction=0.05):
+    '''Perturb each model parameter value by a known relative amount given by
+    fraction*value'''
 
-  # rotate crystal a bit (=10 mrad each rotation)
-  xlo_p_vals = xlo_param.get_param_vals()
-  p_vals = [a + b for a, b in zip(xlo_p_vals, [10., 10., 10.])]
-  xlo_param.set_param_vals(p_vals)
+    refiner = RefinerFactory.from_parameters_data_experiments(
+      self.params, self.dummy_reflections, self.original_experiments)
 
-  # work out what unit cell parameters we can vary by apparent lattice symmetry
-  xluc_p_vals = xluc_param.get_param_vals()
-  (a,b,c,aa,bb,cc) = crystal.get_unit_cell().parameters()
-  a += 0.1
-  b += 0.1
-  c += 0.1
-  if not_approx_equal(aa, 90) or not_approx_equal(aa, 120): aa += 0.1
-  if not_approx_equal(bb, 90) or not_approx_equal(bb, 120): bb += 0.1
-  if not_approx_equal(cc, 90) or not_approx_equal(cc, 120): cc += 0.1
-  new_uc = unit_cell((a,b,c,aa,bb,cc))
-  newB = matrix.sqr(new_uc.fractionalization_matrix()).transpose()
-  S = symmetrize_reduce_enlarge(crystal.get_space_group())
-  S.set_orientation(orientation=newB)
-  X = tuple([e * 1.e5 for e in S.forward_independent_parameters()])
-  xluc_param.set_param_vals(X)
+    pp = refiner._pred_param
+    vals = pp.get_param_vals()
+    shifts = [fraction * val for val in vals]
+    new_vals = [val + shift for (val, shift) in zip(vals, shifts)]
+    pp.set_param_vals(new_vals)
 
-  #############################
-  # Generate some reflections #
-  #############################
+    return refiner.get_experiments()
 
-  # All indices in a 1.0 Angstrom sphere
-  resolution = 1.5
-  index_generator = IndexGenerator(crystal.get_unit_cell(),
-                  space_group(space_group_symbols(1).hall()).type(), resolution)
-  indices = index_generator.to_array()
+def generate_reflections(experiments):
 
-  # Predict rays within the sweep range
-  ray_predictor = ScansRayPredictor(experiments,
-    scan.get_oscillation_range(deg=False))
-  obs_refs = ray_predictor.predict(indices)
+  refs = []
+  for iexp, exp in enumerate(experiments):
 
-  print "Total number of reflections excited", len(obs_refs)
+    info("Generating reflections for experiment {0}".format(iexp))
 
-  # Take only those rays that intersect the detector
-  intersects = ray_intersection(detector, obs_refs)
-  obs_refs = obs_refs.select(intersects)
+    # All indices in a 1.5 Angstrom sphere
+    resolution = 1.5
+    index_generator = IndexGenerator(exp.crystal.get_unit_cell(),
+                    space_group(space_group_symbols(1).hall()).type(), resolution)
+    indices = index_generator.to_array()
+
+    # Predict rays within the sweep range
+    ray_predictor = ScansRayPredictor(experiments,
+      exp.scan.get_oscillation_range(deg=False))
+    obs_refs = ray_predictor.predict(indices, experiment_id=iexp)
+
+    info("Total number of reflections excited: {0}".format(len(obs_refs)))
+
+    # Take only those rays that intersect the detector
+    intersects = ray_intersection(exp.detector, obs_refs)
+    obs_refs = obs_refs.select(intersects)
+    obs_refs['id'] = flex.size_t(len(obs_refs), iexp)
+    refs.append(obs_refs)
+
+    info("Total number of impacts: {0}".format(len(obs_refs)))
+
+  # Concatenate reflections
+  obs_refs = reduce(lambda x, y: x.extend(y), refs)
 
   # Make a reflection predictor and re-predict for all these reflections. The
   # result is the same, but we gain also the flags and xyzcal.px columns
   ref_predictor = ExperimentsPredictor(experiments)
-  obs_refs['id'] = flex.size_t(len(obs_refs), 0)
   obs_refs = ref_predictor.predict(obs_refs)
 
   # Set 'observed' centroids from the predicted ones
   obs_refs['xyzobs.mm.value'] = obs_refs['xyzcal.mm']
 
   # Invent some variances for the centroid positions of the simulated data
-  im_width = scan.get_oscillation()[1] * pi / 180.
-  px_size = detector[0].get_pixel_size()
+  im_width = exp.scan.get_oscillation()[1] * pi / 180.
+  px_size = exp.detector[0].get_pixel_size()
   var_x = flex.double(len(obs_refs), (px_size[0] / 2.)**2)
   var_y = flex.double(len(obs_refs), (px_size[1] / 2.)**2)
   var_phi = flex.double(len(obs_refs), (im_width / 2.)**2)
   obs_refs['xyzobs.mm.variance'] = flex.vec3_double(var_x, var_y, var_phi)
 
-  print "Total number of observations made", len(obs_refs)
+  info("Total number of observations made: {0}".format(len(obs_refs)))
 
-  return obs_refs, experiment
+  return obs_refs
 
 
 if __name__ == "__main__":
@@ -174,32 +172,29 @@ if __name__ == "__main__":
 
   # Parse the command line arguments
   params, options = parser.parse_args(show_diff_phil=True)
-  print params.input.experiments
+  #print params.input.experiments
   from dials.util.options import flatten_experiments
   experiments = flatten_experiments(params.input.experiments)
-
-  try:
-    # only allow single experiment at the moment
-    assert len(experiments) == 1
-  except AssertionError:
-    print usage
-    raise
 
   from dials.util import log
   log.config(params.refinement.verbosity,
       info='synthetic_refine.log', debug='synthetic_refine.debug.log')
 
-  reflections, perturbed_exp = generate_reflections(experiments[0])
+  info("Perturbing original experiments")
+  exp_perturber = ExperimentsPerturber(experiments)
+  perturbed_experiments = exp_perturber.random_perturbation()
 
-  from dials.algorithms.refinement import RefinerFactory
+  info("Generating 'observations' to refine against")
+  reflections = generate_reflections(perturbed_experiments)
+
+  info("Running refinement starting from original experiments")
   refiner = RefinerFactory.from_parameters_data_experiments(
     params, reflections, experiments)
   refiner.run()
-
   refined_experiments = refiner.get_experiments()
 
   # quick check on refined detector geometry using panel 0
-  old_detector = perturbed_exp.detector
+  old_detector = experiments[0].detector
   new_detector = refined_experiments[0].detector
   old_origin = matrix.col(old_detector[0].get_origin())
   new_origin = matrix.col(new_detector[0].get_origin())
