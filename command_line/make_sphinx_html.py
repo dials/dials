2e159df8674d1a7bff9c6e6cdb6e25641ef8abd7
rjgildea <richard.gildea@diamond.ac.uk>
2015-10-16 20:14:12 +0100

    update dest_dir for copying generated html

--- command_line/make_sphinx_html.py
+++ command_line/make_sphinx_html.py
@@ -7,6 +7,23 @@ import os.path as op
 import shutil
 import os
 
+def recursive_overwrite(src, dest, ignore=None):
+  if os.path.isdir(src):
+    if not os.path.isdir(dest):
+      os.makedirs(dest)
+    files = os.listdir(src)
+    if ignore is not None:
+      ignored = ignore(src, files)
+    else:
+      ignored = set()
+    for f in files:
+      if f not in ignored:
+        recursive_overwrite(os.path.join(src, f),
+                            os.path.join(dest, f),
+                            ignore)
+  else:
+    shutil.copyfile(src, dest)
+
 if (__name__ == "__main__") :
   cctbx_base = libtbx.env.find_in_repositories("cctbx_project")
   dials_dir = libtbx.env.find_in_repositories("dials")
@@ -16,11 +33,9 @@ if (__name__ == "__main__") :
   assert (dials_htdocs is not None)
   assert (cctbx_base is not None)
   base_dir = op.dirname(cctbx_base)
-  dest_dir = op.join(dials_htdocs, "doc")
-  if op.exists(dest_dir) :
-    shutil.rmtree(dest_dir)
+  dest_dir = dials_htdocs
   os.chdir(op.join(dials_dir, "doc", "sphinx"))
   easy_run.call("make clean")
   easy_run.call("make html")
   print "Moving HTML pages to", dest_dir
-  shutil.move("build/html", dest_dir)
+  recursive_overwrite("build/html", dest_dir)
