e55d2eb294800917aae2e7cf107780bc252aad40
rjgildea <richard.gildea@diamond.ac.uk>
2015-10-16 17:13:39 +0100

    use correct license in installer

--- installer/dials_installer.sh
+++ installer/dials_installer.sh
@@ -3,5 +3,7 @@
 build/bin/libtbx.python modules/cctbx_project/libtbx/auto_build/create_installer.py \
   --install_script  modules/dials/installer/dials_installer.py \
   --version         dev \
+  --license         modules/dials/license.txt \
+  --readme          modules/dials/license.txt \
   --binary \
   tmp/dials-installer-dev
