12e367a3449fed2d9e9b00af3122e12085acfaaa
graeme-winter <graeme.winter@gmail.com>
2015-11-19 07:03:59 +0000

    Code to allow dump of intensities to file for plotting

--- command_line/check_indexing_symmetry.py
+++ command_line/check_indexing_symmetry.py
@@ -52,6 +52,15 @@ normalise_bins = 0
   .help = "Number of resolution bins for normalisation"
 """, process_includes=True)
 
+def dump_text(filename, set0, set1):
+  i0 = set0.as_double()
+  i1 = set1.as_double()
+  fout = open(filename), 'w')
+  for _0, _1 in zip(i0, i1):
+    assert(_0[0] == _1[0])
+    fout.write('%f %f\n' % (_0[1], _1[1]))
+  fout.close()
+  return
 
 def get_symop_correlation_coefficients(miller_array):
   from copy import deepcopy
