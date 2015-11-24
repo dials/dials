40f42a2256be71108c18f3103f6a95ccc66f6174
James Parkhurst <james.parkhurst@diamond.ac.uk>
2015-11-24 13:41:54 +0000

    Moved to dev

--- command_line/display_reference_profiles.py
+++ command_line/display_reference_profiles.py
@@ -9,6 +9,8 @@
 #  This code is distributed under the BSD license, a copy of which is
 #  included in the root directory of this package.
 
+# LIBTBX_SET_DISPATCHER_NAME dev.dials.display_reference_profiles
+
 from __future__ import division
 import math
 
