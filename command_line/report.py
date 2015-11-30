ab0028d210df68fd1a7e889c63007d98aad48963
rjgildea <richard.gildea@diamond.ac.uk>
2015-11-30 15:50:49 +0000

    add unicode encoding to html header

--- command_line/report.py
+++ command_line/report.py
@@ -2113,7 +2113,7 @@ class Analyser(object):
 <!-- Plotly.js -->
 <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
 
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1" charset="UTF-8">
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
