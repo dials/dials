6d0cb2e9b9f2ffdce1c4bd633b582ff0d79b76f4
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-03-02 08:22:56 +0000

    keep unindexed (id<0) points white

--- command_line/reciprocal_lattice_viewer.py
+++ command_line/reciprocal_lattice_viewer.py
@@ -144,14 +144,15 @@ class render_3d(object):
     colors = flex.vec3_double(len(points), (1,1,1))
 
     if len(points):
-      # suggested colorblind color pallet
+      # suggested colorblind color palette
       # sorry if you have > 8 lattices!
       palette = flex.vec3_double((
         (255,255,255), (230,159,0), (86,180,233), (0,158,115),
         (240,228,66), (0,114,178), (213,94,0), (204,121,167)))
       palette *= (1/255)
       n = palette.size() - 1
-      for i in range(-1, flex.max(reflections['id'])+1):
+      colors.set_selected(reflections['id'] == -1, palette[0])
+      for i in range(0, flex.max(reflections['id'])+1):
         colors.set_selected(reflections['id'] == i, palette[(i%n)+1])
     self.viewer.set_colors(colors)
 
