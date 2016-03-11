df0ba7bb4c2a08175ec73347306e79f275d02a1c
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-03-11 14:20:46 +0000

    Allow & enable treating merges as a single commit for version number purposes. This is mostly relevant for stable release branches

--- util/version.py
+++ util/version.py
@@ -8,6 +8,45 @@ __dials_version_default = "1.1"
 #  3. a dash followed by the number of commits since that tag
 #  4. a dash followed by a lowercase 'g' and the current commit id
 
+def get_git_version(dials_path, treat_merges_as_single_commit=True):
+  import os
+  import subprocess
+  version = None
+  with open(os.devnull, 'w') as devnull:
+    # Obtain name of the current branch. If this fails then the other commands will probably also fail
+    branch = subprocess.check_output(["git", "describe", "--contains", "--all", "HEAD"],
+      cwd=dials_path, stderr=devnull).rstrip()
+
+    # Get descriptive version string, eg. v1.1.0-1-g56f9cd7
+    if treat_merges_as_single_commit:
+      try:
+        # Get a 'correct' depth, which should be the shortest path to the most recent tag
+        version = subprocess.check_output(["git", "describe", "--long", "--first-parent"],
+          cwd=dials_path, stderr=devnull).rstrip()
+      except Exception:
+        pass # This is not supported on older git versions < 1.8.4.
+    if version is None:
+      # Find the most recent tag
+      version = subprocess.check_output(["git", "describe", "--long"],
+        cwd=dials_path, stderr=devnull).rstrip()
+      if treat_merges_as_single_commit:
+        tag = version[:version.rindex('-', 0, version.rindex('-'))]
+        commit = version[version.rindex('-') + 1:] # 'gxxxxxxx'
+        # Now find the first-parent-path
+        depth = subprocess.check_output(["git", "rev-list", "%s..HEAD" % tag, "--first-parent"],
+          cwd=dials_path, stderr=devnull).rstrip()
+        depth = depth.strip().count("\n") + 1
+        version = "%s-%d-%s" % (tag, depth, commit)
+
+    # Turn descriptive version string into proper version number
+    if version[0] == 'v':
+      version = version[1:].replace('.0-','-')
+    version = version.replace('-', '.', 1)
+    # If we are on a release branch, then append a '-release'-tag
+    if 'release' in branch:
+      version = version + '-release'
+  return version
+
 # When run from a development installation the version information is extracted
 # from the git repository. Otherwise it is read from the file '.gitversion' in the
 # DIALS module directory.
@@ -27,19 +66,7 @@ def dials_version():
     #    Regenerate .gitversion if possible
     if os.path.exists(os.path.join(dials_path, '.git')):
       try:
-        import subprocess
-        with open(os.devnull, 'w') as devnull:
-          version = subprocess.check_output(
-            ["git", "describe", "--long"], cwd=dials_path, stderr=devnull).rstrip()
-          if version[0] == 'v':
-            version = version[1:].replace('.0-','-')
-          version = version.replace('-', '.', 1)
-          try:
-            branch = subprocess.check_output(["git", "describe", "--contains", "--all", "HEAD"], cwd=dials_path, stderr=devnull).rstrip()
-            if 'release' in branch:
-              version = version + '-release'
-          except Exception:
-            pass
+        version = get_git_version(dials_path)
         with open(version_file, 'w') as gv:
           gv.write(version)
       except Exception:
