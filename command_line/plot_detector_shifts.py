34740a0e9d7403ebfa601e336c8018905e55c54c
David Waterman <dgwaterman@gmail.com>
2016-03-24 16:22:10 +0000

    Implement grid_size, guess for known detectors, bug fix for scale of colour bars for panel_grid plots

--- command_line/plot_detector_shifts.py
+++ command_line/plot_detector_shifts.py
@@ -19,8 +19,9 @@ each panel that take the first detector model to the second one as a heatmap
 """
 from __future__ import division
 
+from dxtbx.model.experiment.experiment_list import Experiment
 from scitbx.array_family import flex
-from libtbx import phil
+from libtbx import phil, Auto
 from libtbx.utils import Sorry
 import matplotlib
 # Offline backend
@@ -48,9 +49,13 @@ level1.json.
 '''
 
 phil_scope = phil.parse('''
-plot_type = *panel_stack spherical_polar
+plot_type = *panel_grid spherical_polar
   .type = choice
-  .help = "choose type of plot"
+  .help = "Choose type of plot"
+grid_size = Auto
+  .type = ints(size=2)
+  .help = "Grid size (nrow, ncol) for panel_grid plots. Set automatically for"
+          "known detector types if set to Auto"
 tag = None
   .type = str
   .help = "output files will be pre-pended with this string"
@@ -115,35 +120,54 @@ class PlotData(object):
             'z_offset':z_off,
             'fast_offset':f_off,
             'slow_offset':s_off,
-            'normal_offset':n_off}
+            'normal_offset':n_off,
+            'size_fast':size_fast,
+            'size_slow':size_slow}
 
-def plot_stack_of_panels(panel_data, direction='fast', tag = ''):
+def plot_grid_of_panels(panel_data, nrow, ncol, direction='fast', tag = ''):
   '''Plot data for each panel in a stack of subplots, with the first panel
   at the top. This is appropriate for e.g. the 24 panel model for the I23
   P12M detector, as it is simply unrolling the barrel to make a flat plot'''
 
-  fig, axarr = plt.subplots(len(panel_data), sharex=True)
-  axarr[0].set_title(r"$\Delta " + direction + "$")
-  plt.xlabel("fast (mm)")
+  fig, axarr = plt.subplots(nrow, ncol, sharex=True)
+  # kludge just in case nrow=ncol=1 and axarr is not actually an array
+  try:
+    len(axarr)
+  except TypeError:
+    import numpy as np
+    axarr = np.array(axarr)
+  plt.suptitle(r"$\Delta " + direction + "$" + " shifts", y=0.95)
+  plt.figtext(0.425, 0.05, "fast (mm)")
+
   plt.setp(axarr.flat, aspect=1.0, adjustable='box-forced')
 
-  for ipanel in range(len(panel_data)):
-    pnl_data = panel_data[ipanel]
+  clim = []
+  imarr = []
+  for ipanel, (pnl_data, ax) in enumerate(zip(panel_data, axarr.flatten())):
     f, s, offset = (pnl_data['fast'], pnl_data['slow'],
                     pnl_data[direction + '_offset'])
-    ax=axarr[ipanel]
     im = ax.hexbin(f.as_numpy_array(),
-                   s.as_numpy_array(),
-                   offset.as_numpy_array(),
-                   gridsize=30)
+                           s.as_numpy_array(),
+                           offset.as_numpy_array(),
+                           gridsize=30)
+    imarr.append(im)
+    clim.append(im.get_clim())
     ax.invert_yaxis()
     ax.set_yticks([]) # don't show axis labels on the stacked side
+    ax.set_xticks([0, round(pnl_data['size_fast'])])
     ax.tick_params('y', labelsize='x-small')
 
+  clim_low, clim_high = zip(*clim)
+  clim = (min(clim_low), max(clim_high))
+  if (clim[1] - clim[0]) < 1e-12:
+    print "...skipping plot with shift too small to show"
+    return
+  for im in imarr: im.set_clim(clim)
   fig.subplots_adjust(right=0.8)
   cbar_ax = fig.add_axes([0.85, 0.15, 0.03, 0.7])
   fig.colorbar(im, cax=cbar_ax)
   cbar_ax.set_ylabel("$" + direction + "_{2} - " + direction + "_{1}$ (mm)")
+  fig.set_size_inches(10,10)
   plt.savefig(tag + direction + "_diff.png")
   plt.clf()
 
@@ -212,6 +236,43 @@ def plot_spherical_polar(panel_data, beam, direction='fast', tag = ''):
 
   return
 
+def guess_grid_size(detector):
+  '''Guess the right grid size for the detector based on comparison with
+  known types'''
+
+  n_panels = len(detector)
+  pxl_sizes = [p.get_pixel_size() for p in detector]
+  pnl_sizes = [p.get_image_size() for p in detector]
+
+  # check all sizes are equal
+  if not all([s == pxl_sizes[0] for s in pxl_sizes[1:]]):
+    return None
+  if not all([s == pnl_sizes[0] for s in pnl_sizes[1:]]):
+    return None
+
+  # take the first
+  pxl_size = [round(s, 3) for s in pxl_sizes[0]]
+  pnl_size = list(pnl_sizes[0])
+
+  if n_panels == 24 and pxl_size == [0.172, 0.172] and pnl_size == [2463, 195]:
+    # guessed P12M 24 row model
+    print "Guessed P12M detector modelled with 24 rows"
+    grid_size = (24, 1)
+  elif n_panels == 120 and pxl_size == [0.172, 0.172] and pnl_size == [487, 195]:
+    # guessed P12M 24 row, 5 column model
+    print "Guessed P12M detector modelled with 24 rows in 5 columns"
+    grid_size = (24, 5)
+  elif n_panels == 60 and pxl_size == [0.172, 0.172] and pnl_size == [487, 195]:
+    print "Guessed P6M detector modelled with 60 separate panels"
+    grid_size = (12, 5)
+  elif n_panels == 64 and pxl_size == [0.110, 0.110] and pnl_size == [194, 185]:
+    print "Guessed CS-PAD detector. WARNING plot_type=spherical_polar is probably more useful!"
+    grid_size = (8, 8)
+  else:
+    grid_size = None
+
+  return grid_size
+
 class Script(object):
 
   def __init__(self):
@@ -227,28 +288,53 @@ class Script(object):
     parser = OptionParser(
       usage=usage,
       phil=phil_scope,
+      read_datablocks=True,
       read_experiments=True,
       check_format=False,
       epilog=help_message)
 
     params, options = parser.parse_args(show_diff_phil=True)
 
-    if len(params.input.experiments) != 2:
-      raise Sorry("Please provide two experiment lists as input")
+    if params.input.datablock:
+      if params.input.experiments:
+        raise Sorry("Please provide either datablocks or experiment lists, "
+                    "not a combination of the two")
+      if len(params.input.datablock) != 2:
+        raise Sorry("Please provide two datablocks or experiment lists as input")
 
-    warnmsg = ("WARNING: The {0} experiment list contains more than one "
-               "detector. Only the first will be considered.")
-    detector1 = params.input.experiments[0].data.detectors()
-    if len(detector1) > 1:
-      print warnmsg.format("first")
-    detector1 = detector1[0]
-    experiment1 = params.input.experiments[0].data[0]
+      warnmsg = ("WARNING: The {0} datablock contains more than one "
+                 "detector. Only the first will be considered.")
+      detector1 = params.input.datablock[0].data[0].unique_detectors()
+      if len(detector1) > 1:
+        print warnmsg.format("first")
+      detector1 = detector1[0]
+      beam1 = params.input.datablock[0].data[0].unique_beams()[0]
+      experiment1 = Experiment(beam=beam1, detector=detector1)
 
-    detector2 = params.input.experiments[1].data.detectors()
-    if len(detector2) > 1:
-      print warnmsg.format("second")
-    detector2 = detector2[0]
-    experiment2 = params.input.experiments[1].data[0]
+      detector2 = params.input.datablock[1].data[0].unique_detectors()
+      if len(detector2) > 1:
+        print warnmsg.format("second")
+      detector2 = detector2[0]
+      beam2 = params.input.datablock[1].data[0].unique_beams()[0]
+      experiment2 = Experiment(beam=beam2, detector=detector2)
+
+    else:
+      if len(params.input.experiments) != 2:
+        raise Sorry("Please provide two datablocks or experiment lists as input")
+
+      warnmsg = ("WARNING: The {0} experiment list contains more than one "
+                 "detector. Only the first will be considered.")
+      detector1 = params.input.experiments[0].data.detectors()
+      if len(detector1) > 1:
+        print warnmsg.format("first")
+      detector1 = detector1[0]
+      experiment1 = params.input.experiments[0].data[0]
+
+      detector2 = params.input.experiments[1].data.detectors()
+      if len(detector2) > 1:
+        print warnmsg.format("second")
+      detector2 = detector2[0]
+      experiment2 = params.input.experiments[1].data[0]
 
     if len(detector1) != len(detector2):
       raise Sorry("The detectors do not contain the same number of panels")
@@ -270,29 +356,30 @@ class Script(object):
       print "Calc for panel", ipanel
       dat.append(plot_data(ipanel))
 
-    # set limits on colour scale to shifts of 2 pixels, using first panel of
-    # the reference detector to determine pixel size
-    #px_size = min(det1[0].get_pixel_size())
-    #extrema = 1*px_size
-
     if self.params.tag is None:
       tag = ''
     else:
       tag = "%s_"%self.params.tag
 
+    grid_size = self.params.grid_size
+    if self.params.plot_type == 'panel_grid' and grid_size is Auto:
+      grid_size = guess_grid_size(det1)
+      if grid_size is None:
+        raise Sorry("Unable to guess the grid_size for this detector")
+
     # first the plots using offsets in local fast, slow, normal frames
     for direction in ['fast', 'slow', 'normal']:
       print "Doing plot of offsets in the {0} direction".format(direction)
-      if self.params.plot_type == 'panel_stack':
-        plot_stack_of_panels(dat, direction, tag)
+      if self.params.plot_type == 'panel_grid':
+        plot_grid_of_panels(dat, grid_size[0], grid_size[1], direction, tag)
       elif self.params.plot_type == 'spherical_polar':
         plot_spherical_polar(dat, self.experiment1.beam, direction, tag)
 
     # now plots using offsets in the laboratory frame
     for direction in ['x', 'y', 'z']:
       print "Doing plot of offsets in the laboratory {0} direction".format(direction)
-      if self.params.plot_type == 'panel_stack':
-        plot_stack_of_panels(dat, direction, tag)
+      if self.params.plot_type == 'panel_grid':
+        plot_grid_of_panels(dat, grid_size[0], grid_size[1], direction, tag)
       elif self.params.plot_type == 'spherical_polar':
         plot_spherical_polar(dat, self.experiment1.beam, direction, tag)
 
