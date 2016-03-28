52a46a8243811f4d225787f41746a096bfb5b972
graeme-winter <graeme.winter@gmail.com>
2016-03-28 07:20:14 +0100

    First cut at raytracing, it is not fast or clever, but should give an illustration. Usage: dev.dials.duelling_profiles integrated_experiments.json integrated_shoeboxes.pickle.gz method=rt0 id=19363 output: Removed 5621 invalid reflections, 21825 remain Observed reflection (flattened in Z):
       1    0    0    1    1    1    0    1    2    1
       0    0    1    3    1    1    1    0    0    1
       2    1    0    0    2    1    2    0    1    0
       1    1    0    0   10    3    2    1    0    0
       0    1    1    6 1743  451    2    1    0    0
       0    0    1    8  311   55    4    2    0    1
       2    2    0    1    1    3    1    0    0    2
       2    0    1    1    0    1    2    3    3    1
       1    1    1    0    1    0    1    1    2    2
       1    0    0    0    0    1    0    2    0    1
    2587 rays
    Simulated reflection (flattened in Z):
    
       0    0    0    0    0    0    0    0    0    0
       0    0    0    0    0    0    0    0    0    0
       0    0    0    0    0    0    0    0    0    0
       0    0    0    0    0    0    0    0    0    0
       0    0    4  157 1899  462   12    0    0    0
       0    0    0    8   41    4    0    0    0    0
       0    0    0    0    0    0    0    0    0    0
       0    0    0    0    0    0    0    0    0    0
       0    0    0    0    0    0    0    0    0    0
       0    0    0    0    0    0    0    0    0    0
    
    This is modelling one reflection; not considering penetration depth at the moment or anything like that; reflection ID in list passed on CL

--- command_line/duelling_profiles.py
+++ command_line/duelling_profiles.py
@@ -5,7 +5,7 @@ from __future__ import division
 import iotbx.phil
 
 phil_scope = iotbx.phil.parse("""\
-  method = *example nonsense flat
+  method = *example rt0 flat
     .type = choice
   id = None
     .type = int
@@ -30,22 +30,22 @@ def model_background(shoebox, mean_bg):
         shoebox[k, j, i] += g.next()
   return
 
-def random_vector_2D(vector, sd=0.5):
+def random_vector_2D(vector, sd):
   '''Form random orthonormal basis including vector, rotate vector by random
   amount sd in both normal directions.'''
   import random
   o0 = vector.ortho()
   o1 = vector.cross(o0)
-  return vector.rotate(o0, random.gauss(0, sd),  deg = True).rotate(
-    o1, random.gauss(0, sd),  deg = True)
+  return vector.rotate(o0, random.gauss(0, sd)).rotate(o1, random.gauss(0, sd))
 
-def random_vector_cone(vector, sd=0.5):
+def random_vector_cone(vector, sd):
   '''Pick orthogonal axis to vector, rotate by random angle about vector,
-  rotate vector about this by sd.'''
+  rotate vector about this by sd in radians.'''
   import random
+  import math
   o0 = vector.ortho()
   o1 = vector.rotate(o0, random.random() * 2.0 * math.pi)
-  return vector.rotate(o1, random.gauss(0, sd),  deg = True)
+  return vector.rotate(o1, random.gauss(0, sd))
 
 def model_reflection_example(reflection, experiment):
   hkl = reflection['miller_index']
@@ -59,7 +59,7 @@ def model_reflection_example(reflection, experiment):
   Amat = crystal.get_A_at_scan_point(int(xyz[2]))
   return
 
-def predict_angles(p0_star, experiment):
+def predict_angles(p0_star, experiment, s0=None):
   '''Predict Ewald sphere crossing angle for RLP x'''
 
   from scitbx import matrix
@@ -67,7 +67,10 @@ def predict_angles(p0_star, experiment):
 
   # Kabsch frame
   a = matrix.col(experiment.goniometer.get_rotation_axis())
-  b = matrix.col(experiment.beam.get_s0())
+  if s0 is None:
+    b = matrix.col(experiment.beam.get_s0())
+  else:
+    b = s0
 
   m2 = a.normalize()
   m1 = m2.cross(b.normalize())
@@ -90,11 +93,12 @@ def predict_angles(p0_star, experiment):
 
   return math.atan2(sp1, cp1), math.atan2(sp2, cp2)
 
-def model_reflection_nonsense(reflection, experiment):
+def model_reflection_rt0(reflection, experiment):
   import math
   from scitbx import matrix
+  from dials.array_family import flex
 
-  d2r = 180.0 / math.pi
+  d2r = math.pi / 180.0
 
   hkl = reflection['miller_index']
   xyz = reflection['xyzcal.px']
@@ -106,21 +110,62 @@ def model_reflection_nonsense(reflection, experiment):
   angle = angles[0] if (abs(angles[0] - xyz_mm[2]) <
                         abs(angles[1] - xyz_mm[2])) else angles[1]
 
+  i0 = reflection['intensity.sum.value']# / reflection['dqe']
+  s1 = reflection['s1']
+  a = matrix.col(experiment.goniometer.get_rotation_axis())
+  s0 = matrix.col(experiment.beam.get_s0())
+
   pixels = reflection['shoebox']
   pixels.flatten()
   data = pixels.data
   dz, dy, dx = data.focus()
 
-  print 'Observed reflection (flattened in Z)'
+  print 'Observed reflection (flattened in Z):'
+  print
   for j in range(dy):
     for i in range(dx):
       print '%4d' % data[(0, j, i)],
     print
 
-  good = data.as_1d().select(data.as_1d() > 0)
-  from dials.array_family import flex
+  sigma_m = experiment.profile.sigma_m() * d2r
+  sigma_b = experiment.profile.sigma_b() * d2r
 
-  return flex.sum(good)
+  r0 = xyz_mm[2]
+
+  detector = experiment.detector
+
+  patch = flex.double(dy * dx, 0)
+  patch.reshape(flex.grid(dy, dx))
+
+  bbox = reflection['bbox']
+
+  scale = 1
+  print '%d rays' % (int(round(i0 * scale)))
+  for i in range(int(round(i0 * scale))):
+    b = random_vector_cone(s0, sigma_b)
+    p0 = random_vector_cone(Amat * hkl, sigma_m)
+    angles = predict_angles(p0, experiment, b)
+    r = angles[0] if (abs(angles[0] - r0) <
+                      abs(angles[1] - r0)) else angles[1]
+    p = p0.rotate(a, r)
+    s1 = p + b
+    panel, xy = detector.get_ray_intersection(s1)
+    x, y = detector[panel].millimeter_to_pixel(xy)
+    x -= bbox[0]
+    y -= bbox[2]
+    patch[(int(y), int(x))] += 1.0 / scale
+
+  print 'Simulated reflection (flattened in Z):'
+  print
+  for j in range(dy):
+    for i in range(dx):
+      print '%4d' % int(patch[(j, i)]),
+    print
+
+
+
+
+  return
 
 def model_reflection_flat(reflection, experiment):
   pixels = reflection['shoebox']
