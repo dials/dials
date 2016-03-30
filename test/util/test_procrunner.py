581cc32edaeed12ae21c088b93bba91dc52031f9
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-03-30 15:13:12 +0100

    Move procrunner from dials_regression and i19 module to dials

---
+++ test/util/test_procrunner.py
@@ -0,0 +1,91 @@
+from __future__ import division
+
+import dials.util.procrunner
+import mock
+import pytest
+
+@pytest.mark.skipif(dials.util.procrunner.dummy, reason='procrunner class set to dummy mode')
+@mock.patch('dials.util.procrunner._NonBlockingStreamReader')
+@mock.patch('dials.util.procrunner.time')
+@mock.patch('dials.util.procrunner.subprocess')
+def test_run_command_aborts_after_timeout(mock_subprocess, mock_time, mock_streamreader):
+  mock_process = mock.Mock()
+  mock_process.returncode = None
+  mock_subprocess.Popen.return_value = mock_process
+  task = ['___']
+
+  with pytest.raises(Exception):
+    dials.util.procrunner.run_process(task, -1, False)
+
+  assert mock_subprocess.Popen.called
+  assert mock_process.terminate.called
+  assert mock_process.kill.called
+
+
+@pytest.mark.skipif(dials.util.procrunner.dummy, reason='procrunner class set to dummy mode')
+@mock.patch('dials.util.procrunner._NonBlockingStreamReader')
+@mock.patch('dials.util.procrunner.subprocess')
+def test_run_command_runs_command_and_directs_pipelines(mock_subprocess, mock_streamreader):
+  (mock_stdout, mock_stderr) = (mock.Mock(), mock.Mock())
+  mock_stdout.get_output.return_value = mock.sentinel.proc_stdout
+  mock_stderr.get_output.return_value = mock.sentinel.proc_stderr
+  (stream_stdout, stream_stderr) = (mock.sentinel.stdout, mock.sentinel.stderr)
+  mock_process = mock.Mock()
+  mock_process.stdout = stream_stdout
+  mock_process.stderr = stream_stderr
+  mock_process.returncode = 99
+  command = ['___']
+  def streamreader_processing(*args, **kwargs):
+    return {(stream_stdout,): mock_stdout, (stream_stderr,): mock_stderr}[args]
+  mock_streamreader.side_effect = streamreader_processing
+  mock_subprocess.Popen.return_value = mock_process
+
+  expected = {
+    'stderr': mock.sentinel.proc_stderr,
+    'stdout': mock.sentinel.proc_stdout,
+    'exitcode': mock_process.returncode,
+    'command': command,
+    'runtime': mock.ANY,
+    'timeout': False,
+    'time_start': mock.ANY,
+    'time_end': mock.ANY
+  }
+
+  actual = dials.util.procrunner.run_process(command, 0.5, False)
+
+  assert mock_subprocess.Popen.called
+  mock_streamreader.assert_has_calls([mock.call(stream_stdout, output=mock.ANY), mock.call(stream_stderr, output=mock.ANY)], any_order=True)
+  assert not mock_process.terminate.called
+  assert not mock_process.kill.called
+  assert actual == expected
+
+
+def test_nonblockingstreamreader_can_read():
+  import time
+  class _stream:
+    def __init__(self):
+      self.data = []
+      self.closed = False
+    def write(self, string):
+      self.data.append(string)
+    def readline(self):
+      while (len(self.data) == 0) and not self.closed:
+        time.sleep(0.3)
+      return self.data.pop(0) if len(self.data) > 0 else ''
+    def close(self):
+      self.closed=True
+
+  teststream = _stream()
+  testdata = ['a', 'b', 'c']
+
+  streamreader = dials.util.procrunner._NonBlockingStreamReader(teststream, output=False)
+  for d in testdata:
+    teststream.write(d)
+  assert not streamreader.has_finished()
+
+  teststream.close()
+  time.sleep(0.6)
+
+  assert streamreader.has_finished()
+  assert streamreader.get_output() == ''.join(testdata)
+
---
+++ util/procrunner.py
@@ -0,0 +1,183 @@
+from __future__ import division
+import cStringIO as StringIO
+import subprocess
+import time
+import timeit
+from threading import Thread
+
+'''Function to run an external process, print and retain stdout and stderr,
+   enforce a timeout, keep the exit code, etc.'''
+
+dummy = False
+
+class _NonBlockingStreamReader:
+  '''Reads a stream in a thread to avoid blocking/deadlocks'''
+  def __init__(self, stream, output=True):
+    self._stream = stream
+    self._buffer = StringIO.StringIO()
+    self._terminated = False
+    self._closed = False
+
+    def _thread_write_stream_to_buffer():
+      line = True
+      while line:
+        line = self._stream.readline()
+        if line:
+          self._buffer.write(line)
+          if output:
+            print line,
+      self._terminated = True
+
+    self._thread = Thread(target = _thread_write_stream_to_buffer)
+    self._thread.daemon = True
+    self._thread.start()
+
+  def has_finished(self):
+    return self._terminated
+
+  def get_output(self):
+    if not self.has_finished():
+      raise Exception('thread did not terminate')
+    if self._closed:
+      raise Exception('streamreader double-closed')
+    self._closed = True
+    data = self._buffer.getvalue()
+    self._buffer.close()
+    return data
+
+class _NonBlockingStreamWriter:
+  '''Writes to a stream in a thread to avoid blocking/deadlocks'''
+  def __init__(self, stream, data, debug=False):
+    self._buffer = data
+    self._buffer_len = len(data)
+    self._buffer_pos = 0
+    self._debug = debug
+    self._max_block_len = 4096
+    self._stream = stream
+    self._terminated = False
+
+    def _thread_write_buffer_to_stream():
+      while self._buffer_pos < self._buffer_len:
+        if (self._buffer_len - self._buffer_pos) > self._max_block_len:
+          block = self._buffer[self._buffer_pos:(self._buffer_pos + self._max_block_len)]
+        else:
+          block = self._buffer[self._buffer_pos:]
+        try:
+          self._stream.write(block)
+        except IOError, e:
+          if e.errno == 32: # broken pipe, ie. process terminated without reading entire stdin
+            self._stream.close()
+            self._terminated = True
+            return
+          raise
+        self._buffer_pos += len(block)
+        if debug:
+          print "wrote %d bytes to stream" % len(block)
+      self._stream.close()
+      self._terminated = True
+
+    self._thread = Thread(target = _thread_write_buffer_to_stream)
+    self._thread.daemon = True
+    self._thread.start()
+
+  def has_finished(self):
+    return self._terminated
+
+  def bytes_sent(self):
+    return self._buffer_pos
+
+  def bytes_remaining(self):
+    return self._buffer_len - self._buffer_pos
+
+def run_process(command, timeout=None, debug=False, stdin=None, print_stdout=True, print_stderr=True):
+  ''' run an external process, command line specified as array,
+      optionally enforces a timeout specified in seconds,
+      obtains STDOUT, STDERR and exit code
+      and returns summary dictionary. '''
+
+  time_start = time.strftime("%Y-%m-%d %H:%M:%S GMT", time.gmtime())
+  if debug:
+    print "Starting external process:", command
+
+  if stdin is None:
+    stdin_pipe = None
+  else:
+    stdin_pipe = subprocess.PIPE
+
+  if dummy:
+    return { 'exitcode': 0, 'command': command,
+             'stdout': '', 'stderr': '',
+             'timeout': False, 'runtime': 0,
+             'time_start': time_start, 'time_end': time_start }
+
+  start_time = timeit.default_timer()
+  if timeout is not None:
+    max_time = start_time + timeout
+
+  p = subprocess.Popen(command, shell=False, stdin=stdin_pipe, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  stdout = _NonBlockingStreamReader(p.stdout, output=print_stdout)
+  stderr = _NonBlockingStreamReader(p.stderr, output=print_stderr)
+  if stdin is not None:
+    stdin = _NonBlockingStreamWriter(p.stdin, data=stdin)
+
+  timeout_encountered = False
+
+  while (p.returncode is None) and \
+        ((timeout is None) or (timeit.default_timer() < max_time)):
+    if debug and timeout is not None:
+      print "still running (T%.2fs)" % (timeit.default_timer() - max_time)
+
+    # sleep some time
+    try:
+      time.sleep(0.5)
+    except KeyboardInterrupt:
+      p.kill() # if user pressed Ctrl+C we won't be able to produce a proper report anyway
+               # but at least make sure the child process dies with us
+      raise
+
+    # check if process is still running
+    p.poll()
+
+  if p.returncode is None:
+    # timeout condition
+    timeout_encountered = True
+    if debug:
+      print "timeout (T%.2fs)" % (timeit.default_timer() - max_time)
+
+    # send terminate signal and wait some time for buffers to be read
+    p.terminate()
+    time.sleep(0.5)
+    if (not stdout.has_finished() or not stderr.has_finished()):
+      time.sleep(2)
+    p.poll()
+
+  if p.returncode is None:
+    # thread still alive
+    # send kill signal and wait some more time for buffers to be read
+    p.kill()
+    time.sleep(0.5)
+    if (not stdout.has_finished() or not stderr.has_finished()):
+      time.sleep(5)
+    p.poll()
+
+  if p.returncode is None:
+    raise Exception("Process won't terminate")
+
+  runtime = timeit.default_timer() - start_time
+  if debug:
+    print "Process ended after %.1f seconds with exit code %d (T%.2fs)" % \
+      (runtime, p.returncode, timeit.default_timer() - max_time)
+
+  stdout = stdout.get_output()
+  stderr = stderr.get_output()
+  time_end = time.strftime("%Y-%m-%d %H:%M:%S GMT", time.gmtime())
+
+  result = { 'exitcode': p.returncode, 'command': command,
+             'stdout': stdout, 'stderr': stderr,
+             'timeout': timeout_encountered, 'runtime': runtime,
+             'time_start': time_start, 'time_end': time_end }
+  if stdin is not None:
+    result.update({ 'stdin_bytes_sent': stdin.bytes_sent(),
+                    'stdin_bytes_remain': stdin.bytes_remaining() })
+
+  return result
