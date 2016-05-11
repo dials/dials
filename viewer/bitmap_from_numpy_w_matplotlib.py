a6795e44ea6ee346563429c8e08aad5ac128b540
luisodls <luisodls@users.noreply.github.com>
2016-05-11 15:48:59 +0100

    removing old use of matplotlib

--- viewer/bitmap_from_numpy_w_matplotlib.py
+++ viewer/bitmap_from_numpy_w_matplotlib.py
@@ -58,13 +58,7 @@ class wxbmp_from_np_array(object):
           else:
             tmp_data2d_mask = None
 
-          traditional_generator_of_images_with_matplotlib = '''
-          data_sigle_img = self._wx_img(tmp_data2d, show_nums, palette, tmp_data2d_mask)
-          #'''
-
-          #new_generator_of_images_with_cpp = '''
           data_sigle_img = self._wx_img_w_cpp(tmp_data2d, show_nums, palette, tmp_data2d_mask)
-          #'''
 
           single_block_lst_01.append(data_sigle_img)
 
@@ -79,7 +73,6 @@ class wxbmp_from_np_array(object):
 
       wx_image = wx.EmptyImage(NewW, NewW)
       wxBitmap = wx_image.ConvertToBitmap()
-
 
       dc = wx.MemoryDC(wxBitmap)
       text = 'No Shoebox data'
@@ -103,11 +96,7 @@ class wxbmp_from_np_array(object):
     return wx_bmp_lst
 
 
-
   def _wx_img_w_cpp(self, np_2d_tmp, show_nums, palette, np_2d_mask = None):
-
-    #xmax = np_2d_tmp.shape[0]
-    #ymax = np_2d_tmp.shape[1]
 
     xmax = np_2d_tmp.shape[1]
     ymax = np_2d_tmp.shape[0]
@@ -117,9 +106,6 @@ class wxbmp_from_np_array(object):
 
     transposed_data = np.zeros( (ymax, xmax), 'double')
     transposed_mask = np.zeros( (ymax, xmax), 'double')
-
-    #transposed_data[:,:] = np.transpose(np_2d_tmp)
-    #transposed_mask[:,:] = np.transpose(np_2d_mask)
 
     transposed_data[:,:] = np_2d_tmp
     transposed_mask[:,:] = np_2d_mask
@@ -169,117 +155,6 @@ class wxbmp_from_np_array(object):
 
 
 
-  def _wx_img(self, np_2d_tmp, show_nums, palette, np_2d_mask = None):
-
-    d = self.vl_max - self.vl_min
-    vl_mid_low = self.vl_min + d / 3.0
-    vl_mid_hig = self.vl_max - d / 3.0
-
-    lc_fig = plt.figure(frameon=False)
-
-    xmax = np_2d_tmp.shape[0]
-    ymax = np_2d_tmp.shape[1]
-
-    lc_fig.set_size_inches(xmax * .5, ymax * .5)
-
-    ax = plt.Axes(lc_fig, [0., 0., 1., 1.])
-    ax.set_axis_off()
-    lc_fig.add_axes(ax)
-
-    a=lc_fig.gca()
-    a.set_frame_on(False)
-    a.set_xticks([])
-    a.set_yticks([])
-    plt.axis('off')
-
-    plt.imshow(np.transpose(np_2d_tmp), interpolation = "nearest", cmap = 'hot',
-               vmin = self.vl_min, vmax = self.vl_max)
-
-    l_wdt = 1.2
-
-    if( np_2d_mask != None):
-      dst_diag = 0.015
-      for xpos in range(xmax):
-        for ypos in range(ymax):
-          loc_mask = int(np_2d_mask[xpos, ypos])
-
-          if( loc_mask != 0 ):
-
-            # drawing box
-            plt.vlines(xpos - 0.45, ypos - 0.45, ypos + 0.45,
-                       color = 'gray', linewidth=l_wdt)
-            plt.vlines(xpos + 0.45, ypos - 0.45, ypos + 0.45,
-                       color = 'gray', linewidth=l_wdt)
-            plt.hlines(ypos - 0.45, xpos - 0.45, xpos + 0.45,
-                       color = 'gray', linewidth=l_wdt)
-            plt.hlines(ypos + 0.45, xpos - 0.45, xpos + 0.45,
-                       color = 'gray', linewidth=l_wdt)
-
-            if( (loc_mask & MaskCode.Background) == MaskCode.Background ):
-              # drawing v_lines
-              plt.vlines(xpos, ypos - 0.45, ypos + 0.45,
-                         color = 'gray', linewidth=l_wdt)
-              plt.vlines(xpos + 0.225, ypos - 0.45, ypos + 0.45,
-                         color = 'gray', linewidth=l_wdt)
-              plt.vlines(xpos - 0.225, ypos - 0.45, ypos + 0.45,
-                         color = 'gray', linewidth=l_wdt)
-
-            if( (loc_mask & MaskCode.BackgroundUsed) == MaskCode.BackgroundUsed ):
-              # drawing h_lines
-              plt.hlines(ypos , xpos - 0.45, xpos + 0.45,
-                         color = 'gray', linewidth=l_wdt)
-              plt.hlines(ypos + 0.225, xpos - 0.45, xpos + 0.45,
-                         color = 'gray', linewidth=l_wdt)
-              plt.hlines(ypos - 0.225, xpos - 0.45, xpos + 0.45,
-                         color = 'gray', linewidth=l_wdt)
-
-            if( (loc_mask & MaskCode.Foreground) == MaskCode.Foreground ):
-              # drawing lines from 1p5 to 7p5
-              plt.plot([xpos - dst_diag, xpos - 0.45], [ypos - 0.45, ypos - dst_diag],
-                       color='gray', linewidth=l_wdt)
-              plt.plot([xpos + dst_diag, xpos + 0.45], [ypos + 0.45, ypos + dst_diag],
-                       color='gray', linewidth=l_wdt)
-              plt.plot([xpos - 0.45, xpos + 0.45], [ypos + 0.45, ypos - 0.45],
-                       color='gray', linewidth=l_wdt)
-
-            if( (loc_mask &  MaskCode.Valid) == MaskCode.Valid ):
-              # drawing lines from 4p5 to 10p5
-              plt.plot([xpos - dst_diag, xpos - 0.45], [ypos + 0.45, ypos + dst_diag],
-                       color='gray', linewidth=l_wdt)
-              plt.plot([xpos + dst_diag, xpos + 0.45], [ypos - 0.45, ypos - dst_diag],
-                       color='gray', linewidth=l_wdt)
-              plt.plot([xpos + 0.45, xpos - 0.45], [ypos + 0.45, ypos - 0.45],
-                       color='gray', linewidth=l_wdt)
-
-    if( show_nums != 0 ):
-      for xpos in range(xmax):
-        for ypos in range(ymax):
-          f_num = np_2d_tmp[xpos,ypos]
-          g = float("{0:.3f}".format(float(f_num)))
-
-          txt_dat = str(g)
-          if( f_num < vl_mid_low ):
-            clr_chr = 'yellow'
-          elif(f_num > vl_mid_hig):
-            clr_chr = 'black'
-          else:
-            clr_chr = 'blue'
-
-          plt.annotate(txt_dat, xy = (xpos - 0.5, ypos + 0.1), xycoords = 'data',
-                       color = clr_chr, size = 9.)
-
-    lc_fig.canvas.draw()
-    width, height = lc_fig.canvas.get_width_height()
-    np_buf = np.fromstring (lc_fig.canvas.tostring_rgb(), dtype=np.uint8)
-    np_buf.shape = (width, height, 3)
-    np_buf = np.roll(np_buf, 3, axis = 2)
-    self._wx_image = wx.EmptyImage(width, height)
-    self._wx_image.SetData( np_buf )
-    data_to_become_bmp = (self._wx_image, width, height)
-
-    plt.close(lc_fig)
-
-    return data_to_become_bmp
 
 
   def _wx_bmp_scaled(self, data_to_become_bmp, scale):
