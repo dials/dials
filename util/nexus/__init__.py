401fc910b35607f530187edcbad40c1e24ef485e
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-05-18 08:58:45 +0100

    resolve naming conflict with python built-in function

--- util/nexus/__init__.py
+++ util/nexus/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import division
 
-def open(filename, mode='a'):
+def get_entry(filename, mode='a'):
   import h5py
   handle = h5py.File(filename, mode)
   if 'entry' in handle:
@@ -13,13 +13,13 @@ def open(filename, mode='a'):
 
 def load(filename):
   from dials.util.nexus import nx_diffraction, nx_mx
-  entry = open(filename, "r")
+  entry = get_entry(filename, "r")
   ref, exp_index = nx_diffraction.load(entry)
   exp = nx_mx.load(entry, exp_index)
   return exp, ref
 
 def dump(experiments, reflections, filename):
   from dials.util.nexus import nx_diffraction, nx_mx
-  entry = open(filename, "w")
+  entry = get_entry(filename, "w")
   experiments = nx_mx.dump(entry, experiments)
   nx_diffraction.dump(entry, reflections, experiments)
