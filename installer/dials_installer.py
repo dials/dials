937a23146e01a9bef75a93ec69e8149f9fe20df9
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-06-08 10:55:11 +0100

    defluff installer, fixes #149

--- installer/dials_installer.py
+++ installer/dials_installer.py
@@ -1,13 +1,12 @@
 from __future__ import division
+import os
 import shutil
-import os.path
 import sys
 libtbx_path = os.path.join(
   os.path.abspath(os.path.dirname(os.path.dirname(__file__))), "lib")
 if libtbx_path not in sys.path:
   sys.path.append(libtbx_path)
 from libtbx.auto_build import install_distribution
-from libtbx.auto_build.installer_utils import *
 
 class installer(install_distribution.installer):
   product_name = "DIALS"
@@ -42,6 +41,50 @@ class installer(install_distribution.installer):
     flags.remove('create_versioned_dispatchers')
   except ValueError:
     pass
+
+  def product_specific_prepackage_hook(self, directory):
+    """
+    Remove irrelevant files from installer.
+    """
+    self.print_header('Deflating installer')
+
+    suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+    def humansize(nbytes):
+      if nbytes == 0: return '0 B'
+      i = 0
+      while nbytes >= 1024 and i < len(suffixes)-1:
+        nbytes /= 1024.
+        i += 1
+      f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
+      return '%s %s' % (f, suffixes[i])
+
+    def rmdir(subdir):
+      fullpath = os.path.join(directory, subdir)
+      num_files, total_size = 0, 0
+      if not os.path.exists(fullpath):
+        print "Skipping", subdir
+        return
+      for dirpath, dirnames, filenames in os.walk(fullpath):
+        for f in filenames:
+          fp = os.path.join(dirpath, f)
+          total_size += os.path.getsize(fp)
+          num_files += 1
+      print "Removing %s" % subdir
+      print "  %s, %d files" % (humansize(total_size), num_files)
+      shutil.rmtree(fullpath)
+      print
+
+    rmdir('base/lib/python2.7/site-packages/matplotlib-1.3.1-py2.7-linux-x86_64.egg/matplotlib/tests')
+    # TODO: deduce path using something like
+    # import matplotlib
+    # import inspect
+    # inspect.getsourcefile(matplotlib)
+    # => '/scratch/wra62962/files/dials/base/lib/python2.7/site-packages/matplotlib-1.3.1-py2.7-linux-x86_64.egg/matplotlib/__init__.py'
+
+    rmdir('base/lib/python2.7/test')
+    rmdir('base/man')
+    rmdir('base/share/gtk-doc')
+    rmdir('modules/cbflib/doc')
 
 if __name__ == "__main__":
   installer(sys.argv[1:]).install()
