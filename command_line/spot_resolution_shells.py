6a3e17404c0b931c6815ff6eec751599db299ec0
graeme-winter <graeme.winter@gmail.com>
2016-06-17 13:51:11 +0100

    Tool to generate histogram of #spots in resolution shells, scaled as 1/d**2

---
+++ command_line/spot_resolution_shells.py
@@ -0,0 +1,93 @@
+from __future__ import division
+import libtbx.phil
+
+help_message = '''
+
+Compute resolution-wise distribution of spots
+
+Examples::
+
+  dials.spot_resolution_shells datablock.json strong.pickle
+
+'''
+
+phil_scope= libtbx.phil.parse("""
+  shells = 100
+    .type = int(value_min=1)
+""")
+
+def settings():
+  return phil_scope.fetch().extract()
+
+def spot_resolution_shells(imagesets, reflections, params):
+  goniometer = imagesets[0].get_goniometer()
+  from dials.algorithms.indexing import indexer
+  from dials.array_family import flex
+  mapped_reflections = flex.reflection_table()
+  for i, imageset in enumerate(imagesets):
+    if 'imageset_id' in reflections:
+      sel = (reflections['imageset_id'] == i)
+    else:
+      sel = (reflections['id'] == i)
+    if isinstance(reflections['id'], flex.size_t):
+      reflections['id'] = reflections['id'].as_int()
+    refl = indexer.indexer_base.map_spots_pixel_to_mm_rad(
+      reflections.select(sel),
+      imageset.get_detector(), imageset.get_scan())
+
+    indexer.indexer_base.map_centroids_to_reciprocal_space(
+      refl, imageset.get_detector(), imageset.get_beam(),
+      imageset.get_goniometer())
+    mapped_reflections.extend(refl)
+  reflections = mapped_reflections
+  two_theta_array = reflections['rlp'].norms()
+  h0 = flex.weighted_histogram(two_theta_array ** 2, n_slots=params.shells)
+  n = h0.slots()
+  d = 1.0 / flex.sqrt(h0.slot_centers())
+
+  for j in range(params.shells):
+    print '%d %f %d' % (j, d[j], n[j])
+
+def run(args):
+
+  from dials.util.options import OptionParser
+  from dials.util.options import flatten_datablocks
+  from dials.util.options import flatten_experiments
+  from dials.util.options import flatten_reflections
+  import libtbx.load_env
+
+  usage = "%s [options] datablock.json reflections.pickle" %(
+    libtbx.env.dispatcher_name)
+
+  parser = OptionParser(
+    usage=usage,
+    phil=phil_scope,
+    read_datablocks=True,
+    read_experiments=True,
+    read_reflections=True,
+    check_format=False,
+    epilog=help_message)
+
+  params, options = parser.parse_args(show_diff_phil=True)
+  datablocks = flatten_datablocks(params.input.datablock)
+  experiments = flatten_experiments(params.input.experiments)
+  reflections = flatten_reflections(params.input.reflections)
+
+  if (len(datablocks) == 0 and len(experiments) == 0) or len(reflections) == 0:
+    parser.print_help()
+    exit(0)
+
+  reflections = reflections[0]
+
+  if len(datablocks) == 0 and len(experiments) > 0:
+    imagesets = experiments.imagesets()
+  else:
+    imagesets = []
+    for datablock in datablocks:
+      imagesets.extend(datablock.extract_imagesets())
+
+  spot_resolution_shells(imagesets, reflections, params)
+
+if __name__ == '__main__':
+  import sys
+  run(sys.argv[1:])
