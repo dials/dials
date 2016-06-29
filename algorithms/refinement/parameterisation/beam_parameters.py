353225150541c60f8d55bc97569c81ac21e40054
David Waterman <dgwaterman@gmail.com>
2016-06-29 16:35:56 +0100

    Abstract shareable code to BeamMixin

--- algorithms/refinement/parameterisation/beam_parameters.py
+++ algorithms/refinement/parameterisation/beam_parameters.py
@@ -15,32 +15,16 @@ from scitbx import matrix
 from dials.algorithms.refinement.refinement_helpers \
     import dR_from_axis_and_angle
 
-class BeamParameterisation(ModelParameterisation):
-  """Implementation of parameterisation for the beam with angles expressed in
-  mrad and wavenumber in inverse Angstroms.
+class BeamMixin(object):
+  """Mix-in class defining some functionality unique to beam parameterisations
+  that can be shared by static and scan-varying versions"""
 
-  Pass in a goniometer (if present) to ensure consistent definition of the
-  beam rotation angles with respect to the spindle-beam plane."""
+  @staticmethod
+  def _build_p_list(s0, goniometer, parameter_type=Parameter):
+    """Build the list of parameters, using the parameter_type callback to
+    select between versions of the Parameter class"""
 
-  def __init__(self, beam, goniometer=None, experiment_ids=None):
-
-    # The state of the beam model consists of the s0 vector that it is
-    # modelling. The initial state is the direction of this vector at the point
-    # of initialisation. Future states are composed by rotations around axes
-    # perpendicular to that direction and normalisation specified by the
-    # wavenumber (inverse wavelength).
-    #
-    # The 'models' attribute refers to the beam vector contained by this
-    # model.
-
-    ### Set up the initial state
-    if experiment_ids is None:
-      experiment_ids = [0]
-    s0 = matrix.col(beam.get_s0())
-    s0dir = matrix.col(beam.get_unit_s0())
-    istate = s0dir
-
-    ### Set up the parameters
+    # Set up the parameters
     if goniometer:
       spindle = matrix.col(goniometer.get_rotation_axis())
       s0_plane_dir2 = s0.cross(spindle).normalize()
@@ -59,6 +43,68 @@ class BeamParameterisation(ModelParameterisation):
     # build the parameter list in a specific,  maintained order
     p_list = [mu1, mu2, nu]
 
+    return p_list
+
+  @staticmethod
+  def _compose_core(is0, mu1, mu2, nu, mu1_axis, mu2_axis):
+
+    # convert angles to radians
+    mu1rad, mu2rad = mu1 / 1000., mu2 / 1000.
+
+    # compose rotation matrices and their first order derivatives
+    Mu1 = (mu1_axis).axis_and_angle_as_r3_rotation_matrix(mu1rad, deg=False)
+    dMu1_dmu1 = dR_from_axis_and_angle(mu1_axis, mu1rad, deg=False)
+
+    Mu2 = (mu2_axis).axis_and_angle_as_r3_rotation_matrix(mu2rad, deg=False)
+    dMu2_dmu2 = dR_from_axis_and_angle(mu2_axis, mu2rad, deg=False)
+
+    # compose new state
+    Mu21 = Mu2 * Mu1
+    s0_new_dir = (Mu21 * is0).normalize()
+    s0 = nu * s0_new_dir
+
+    # calculate derivatives of the beam direction wrt angles:
+    #  - derivative wrt mu1
+    dMu21_dmu1 = Mu2 * dMu1_dmu1
+    ds0_new_dir_dmu1 = dMu21_dmu1 * is0
+
+    #  - derivative wrt mu2
+    dMu21_dmu2 = dMu2_dmu2 * Mu1
+    ds0_new_dir_dmu2 = dMu21_dmu2 * is0
+
+    # calculate derivatives of the attached beam vector, converting
+    # parameters back to mrad, and store
+    ds0_dval = [ds0_new_dir_dmu1 * nu / 1000.,
+                ds0_new_dir_dmu2 * nu / 1000.,
+                s0_new_dir]
+
+    return s0, ds0_dval
+
+class BeamParameterisation(ModelParameterisation, BeamMixin):
+  """Implementation of parameterisation for the beam with angles expressed in
+  mrad and wavenumber in inverse Angstroms.
+
+  Pass in a goniometer (if present) to ensure consistent definition of the
+  beam rotation angles with respect to the spindle-beam plane."""
+
+  def __init__(self, beam, goniometer=None, experiment_ids=None):
+
+    # The state of the beam model consists of the s0 vector that it is
+    # modelling. The initial state is the direction of this vector at the point
+    # of initialisation. Future states are composed by rotations around axes
+    # perpendicular to that direction and normalisation specified by the
+    # wavenumber (inverse wavelength).
+
+    # Set up the initial state
+    if experiment_ids is None:
+      experiment_ids = [0]
+    s0 = matrix.col(beam.get_s0())
+    s0dir = matrix.col(beam.get_unit_s0())
+    istate = s0dir
+
+    # build the parameter list
+    p_list = self._build_p_list(s0, goniometer)
+
     # set up the base class
     ModelParameterisation.__init__(self, beam, istate, p_list,
                                    experiment_ids=experiment_ids)
@@ -76,41 +122,12 @@ class BeamParameterisation(ModelParameterisation):
     # extract parameters from the internal list
     mu1, mu2, nu = self._param
 
-    # convert angles to radians
-    mu1rad, mu2rad = mu1.value / 1000., mu2.value / 1000.
-
-    # compose rotation matrices and their first order derivatives
-    Mu1 = (mu1.axis).axis_and_angle_as_r3_rotation_matrix(mu1rad, deg=False)
-    dMu1_dmu1 = dR_from_axis_and_angle(mu1.axis, mu1rad, deg=False)
-
-    Mu2 = (mu2.axis).axis_and_angle_as_r3_rotation_matrix(mu2rad, deg=False)
-    dMu2_dmu2 = dR_from_axis_and_angle(mu2.axis, mu2rad, deg=False)
-
-    Mu21 = Mu2 * Mu1
-
-    ### Compose new state
-    s0_new_dir = (Mu21 * is0).normalize()
+    # calculate new s0 and derivatives
+    s0, self._dstate_dp = self._compose_core(is0, mu1.value, mu2.value,
+      nu.value, mu1_axis=mu1.axis, mu2_axis=mu2.axis)
 
     # now update the model with its new s0
-    self._model.set_s0(nu.value * s0_new_dir)
-
-    ### calculate derivatives of the beam direction wrt angles
-    # derivative wrt mu1
-    dMu21_dmu1 = Mu2 * dMu1_dmu1
-    ds0_new_dir_dmu1 = dMu21_dmu1 * is0
-
-    # derivative wrt mu2
-    dMu21_dmu2 = dMu2_dmu2 * Mu1
-    ds0_new_dir_dmu2 = dMu21_dmu2 * is0
-
-    ### calculate derivatives of the attached beam vector, converting
-    ### parameters back to mrad, and store
-    # derivative wrt mu1
-    self._dstate_dp[0] = ds0_new_dir_dmu1 * nu.value / 1000.
-    # derivative wrt mu2
-    self._dstate_dp[1] = ds0_new_dir_dmu2 * nu.value / 1000.
-    # derivative wrt nu
-    self._dstate_dp[2] = s0_new_dir
+    self._model.set_s0(s0)
 
     return
 
