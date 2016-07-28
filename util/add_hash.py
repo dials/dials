0ba2c94cfd320ea0db66a6797b4a057f7830c067
graeme-winter <graeme.winter@gmail.com>
2016-07-28 07:23:05 +0100

    Print duplicates for testing; add enhash / dehash functions

--- util/add_hash.py
+++ util/add_hash.py
@@ -1,5 +1,15 @@
 from __future__ import division
 
+def enhash(e, h, k, l):
+  return e * (2 ** 30) + (h+512) * (2 ** 20) + (k+512) * (2 ** 10) + (l+512)
+
+def dehash(hash):
+  e = hash // 2 ** 30
+  h = (hash % (2 ** 30)) // (2 ** 20) - 512
+  k = (hash % (2 ** 20)) // (2 ** 10) - 512
+  l = (hash % (2 ** 10)) - 512
+  return e, h, k, l
+
 def add_hash(integrated_data):
   '''Add hash = 2^30 * entering + 2^20 * (h+512) + 2^10 * (k+512) + (l+512)
   as new column to reflection table - should be P1 unique for 360 degree
@@ -17,7 +27,7 @@ def add_hash(integrated_data):
   l = l.iround()
   e = integrated_data['entering'].as_int()
 
-  hash = e * 2 ** 30 + (h+512) * 2 ** 20 + (k+512) * 2 ** 10 + (l+512)
+  hash = enhash(e, h, k, l)
   integrated_data['hash'] = hash
 
   return integrated_data
@@ -37,4 +47,8 @@ if __name__ == '__main__':
 
   for h in hash:
     sel = hash == h
-    print sel.count(True)
+    if sel.count(True) > 1:
+      print 'Duplicates:', dehash(h)
+      isel = sel.iselection()
+      for i in isel:
+        print integrated_data['miller_index'][i], integrated_data['entering'][i]
