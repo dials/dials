0d4b5094482794210f2ea64ad01ad8a16a177f06
graeme-winter <graeme.winter@gmail.com>
2016-07-29 06:33:00 +0100

    Generalise tool to work on any reflection table with indices / entering flag & value / variance columns

--- command_line/semisynthetic_variance_analysis.py
+++ command_line/semisynthetic_variance_analysis.py
@@ -22,7 +22,8 @@ def npp(values, input_mean_variance):
 
   return expected, scaled
 
-def semisynthetic_variance_analysis(semisynthetic_integrated_data_files):
+def semisynthetic_variance_analysis(semisynthetic_integrated_data_files,
+                                    value_column):
   import cPickle as pickle
   import math
   from dials.array_family import flex
@@ -30,6 +31,17 @@ def semisynthetic_variance_analysis(semisynthetic_integrated_data_files):
 
   integrated_data_sets = [pickle.load(open(data_file, 'rb')) for
                           data_file in semisynthetic_integrated_data_files]
+
+  # check column, find variance
+  integrated_data = integrated_data_sets[0]
+  assert value_column in integrated_data
+  variance_column = None
+  for column in ['%s.variance' % value_column,
+                 value_column.replace('value', 'variance')]:
+    if column in integrated_data:
+      variance_column = column
+      break
+  assert(variance_column)
 
   # first prepare the data files i.e. remove partials, keep only integrated
   # reflections, add the hash column, add weight column
@@ -39,10 +51,11 @@ def semisynthetic_variance_analysis(semisynthetic_integrated_data_files):
   hashed_data_sets = []
 
   for integrated_data in integrated_data_sets:
-    sel = integrated_data.get_flags(integrated_data.flags.integrated)
-    integrated_data = integrated_data.select(sel)
-    sel = integrated_data['partiality'] > 0.99
-    integrated_data = integrated_data.select(sel)
+    if 'intensity' in value_column:
+      sel = integrated_data.get_flags(integrated_data.flags.integrated)
+      integrated_data = integrated_data.select(sel)
+      sel = integrated_data['partiality'] > 0.99
+      integrated_data = integrated_data.select(sel)
     integrated_data = add_hash(integrated_data)
     hashed_data_sets.append(integrated_data)
     if hash_set is None:
@@ -60,8 +73,8 @@ def semisynthetic_variance_analysis(semisynthetic_integrated_data_files):
       sel = i['hash'] == h
       isel = sel.iselection()
       assert(len(isel) == 1)
-      values_profile.append(i[isel[0]]['intensity.prf.value'])
-      variances_profile.append(i[isel[0]]['intensity.prf.variance'])
+      values_profile.append(i[isel[0]][value_column])
+      variances_profile.append(i[isel[0]][variance_column])
     weighted_mean, weighted_variance = weighted_mean_variance(values_profile,
                                                               variances_profile)
     expected, scaled = npp(values_profile, (weighted_mean, weighted_variance))
@@ -71,4 +84,5 @@ def semisynthetic_variance_analysis(semisynthetic_integrated_data_files):
 
 if __name__ == '__main__':
   import sys
-  semisynthetic_variance_analysis(sys.argv[1:])
+  value_column = sys.argv[1]
+  semisynthetic_variance_analysis(sys.argv[2:], value_column)
