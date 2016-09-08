822d6cd4bf21ae0fd2e25cacfe2d24def7f7c576
James Parkhurst <james.parkhurst@diamond.ac.uk>
2016-09-08 15:27:15 +0100

    Added command to import from eiger stream

---
+++ command_line/import_stream.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+#
+# import_stream.py
+#
+#  Copyright (C) 2013 Diamond Light Source
+#
+#  Author: James Parkhurst
+#
+#  This code is` distributed under the BSD license, a copy of which is
+#  included in the root directory of this package.
+from __future__ import division
+from dxtbx.datablock import DataBlockFactory, DataBlockDumper
+from libtbx.utils import Sorry
+
+help_message = '''
+
+
+'''
+
+# Create the phil parameters
+from libtbx.phil import parse
+phil_scope = parse('''
+
+  output {
+
+    datablock = datablock.json
+      .type = str
+      .help = "The output JSON or pickle file"
+
+    log = 'dials.import_stream.log'
+      .type = str
+      .help = "The log filename"
+
+    debug_log = 'dials.import_stream.debug.log'
+      .type = str
+      .help = "The debug log filename"
+
+    compact = False
+      .type = bool
+      .help = "For JSON output use compact representation"
+
+    directory = auto
+      .type = str
+      .help = "The output directory for streaming data"
+
+    image_template = "%05d.image"
+      .type = str
+      .help = "The image template"
+
+  }
+
+  verbosity = 1
+    .type = int(value_min=0)
+    .help = "The verbosity level"
+
+  input {
+
+    host = localhost
+      .type = str
+      .help = "The input host"
+
+    port = 9999
+      .type = int
+      .help = "The input port"
+
+  }
+
+''')
+
+
+
+
+class Script(object):
+  ''' Class to parse the command line options. '''
+
+  def __init__(self):
+    ''' Set the expected options. '''
+    from dials.util.options import OptionParser
+    import libtbx.load_env
+
+    # Create the option parser
+    usage = "usage: %s [options]" % libtbx.env.dispatcher_name
+    self.parser = OptionParser(
+      usage=usage,
+      sort_options=True,
+      phil=phil_scope,
+      epilog=help_message)
+
+  def run(self):
+    ''' Parse the options. '''
+    from dials.util import log
+    from logging import info, debug
+    import libtbx
+    from uuid import uuid4
+    from dials.util.stream import ZMQStream, Decoder
+    from os.path import join, exists
+    import os
+    import json
+    from dxtbx.datablock import DataBlock
+
+    # Parse the command line arguments in two passes to set up logging early
+    params, options = self.parser.parse_args(show_diff_phil=False, quick_parse=True)
+
+    # Configure logging
+    log.config(
+      params.verbosity,
+      info=params.output.log,
+      debug=params.output.debug_log)
+    from dials.util.version import dials_version
+    info(dials_version())
+
+    # Parse the command line arguments completely
+    params, options = self.parser.parse_args(show_diff_phil=False)
+
+    # Log the diff phil
+    diff_phil = self.parser.diff_phil.as_str()
+    if diff_phil is not '':
+      info('The following parameters have been modified:\n')
+      info(diff_phil)
+
+    # Check a stream is given
+    if params.input.host is None:
+      raise Sorry("An input host needs to be given")
+
+    # Check the directory
+    if params.output.directory is None:
+      raise Sorry("An output directory needs to be given")
+    elif params.output.directory is libtbx.Auto:
+      params.output.directory = "/dev/shm/dials-%s" % uuid4()
+
+    # Make the output directory
+    if exists(params.output.directory):
+      raise Sorry('Directory "%s" already exists' % (params.output.directory))
+
+    # Make the directory
+    os.mkdir(params.output.directory)
+
+    # Initialise the stream
+    stream = ZMQStream(params.input.host, params.input.port)
+    decoder = Decoder(
+      params.output.directory,
+      params.output.image_template)
+    imageset = None
+    while True:
+
+      # Get the frames from zmq
+      frames = stream.receive()
+
+      # Decode the frames
+      obj = decoder.decode(frames)
+
+      # Process the object
+      if obj.is_header():
+        filename = join(params.output.directory, "metadata.json")
+        with open(filename, "w") as outfile:
+          json.dump(obj.header, outfile)
+        imageset = obj.as_imageset(filename)
+        datablocks = [DataBlock([imageset])]
+        self.write_datablocks(datablocks, params)
+      elif obj.is_image():
+        assert imageset is not None
+        filename = join(
+          params.output.directory,
+          params.output.image_template % obj.count)
+        with open(filename, "wb") as outfile:
+          outfile.write(obj.data)
+        filename = join(
+          params.output.directory,
+          "%s.info" % (params.output.image_template % obj.count))
+        with open(filename, "w") as outfile:
+          json.dump(obj.info, outfile)
+      elif obj.is_endofseries():
+        assert imageset is not None
+        break
+      else:
+        raise RuntimeError("Unknown object")
+
+    # Close the stream
+    stream.close()
+
+  def write_datablocks(self, datablocks, params):
+    '''
+    Output the datablock to file.
+
+    '''
+    from logging import info
+    if params.output.datablock:
+      info("-" * 80)
+      info('Writing datablocks to %s' % params.output.datablock)
+      dump = DataBlockDumper(datablocks)
+      dump.as_file(params.output.datablock, compact=params.output.compact)
+
+
+
+
+if __name__ == '__main__':
+  from dials.util import halraiser
+  try:
+    script = Script()
+    script.run()
+  except Exception as e:
+    halraiser(e)
---
+++ util/stream.py
@@ -0,0 +1,281 @@
+from __future__ import division
+
+
+class ZMQStream():
+  '''
+  A class to listen to a zeromq stream
+
+  '''
+  def __init__(self, host, port=9999):
+    """
+    create stream listener object
+
+    """
+    self.host = host
+    self.port = port
+
+    # Connect to the stream
+    self.connect()
+
+  def connect(self):
+    """
+    Enable stream, connect to zmq host
+
+    :return: receiver object
+
+    """
+    import zmq
+    from logging import info
+
+    # Set the URL
+    url = "tcp://{0}:{1}".format(self.host, self.port)
+    info("Connecting to ZMQ stream at %s" % url)
+
+    # Get the zmq context
+    context = zmq.Context()
+
+    # Create the reciever
+    self.receiver = context.socket(zmq.PULL)
+    self.receiver.connect(url)
+    return self.receiver
+
+  def receive(self):
+    """
+    Receive and return zmq frames
+
+    """
+    from logging import info
+    info("Waiting for frame")
+    frames = self.receiver.recv_multipart(copy = False)
+    return frames
+
+  def close(self):
+    """
+    Close and disable stream
+
+    """
+    from logging import info
+    info("Closing stream")
+    return self.receiver.close()
+
+
+class Result(object):
+  '''
+  A class to represent a result
+
+  '''
+  def __init__(self):
+    pass
+
+  def is_header(self):
+    '''
+    Return not a header
+
+    '''
+    return False
+
+  def is_image(self):
+    '''
+    Return not an image
+
+    '''
+    return False
+
+  def is_endofseries(self):
+    '''
+    Return not an endofseries
+
+    '''
+    return False
+
+
+class Header(Result):
+  '''
+  A class representing a header
+
+  '''
+
+  def __init__(self, frames, directory, image_template):
+    '''
+    Create a header object
+
+    '''
+    import json
+
+    super(Header, self).__init__()
+
+    # Load the header
+    head = json.loads(frames[0].bytes)
+
+    # Get info
+    if head['header_detail'] == 'basic':
+      conf = json.loads(frames[1].bytes)
+    elif head['header_detail'] == 'all':
+      conf = json.loads(frames[1].bytes)
+      # TODO use flatfield, pixel mask and count rate
+      # flat_head = json.loads(frames[2].bytes)
+      # flat_data = frames[3].bytes
+      # mask_head = json.loads(frames[5].bytes)
+      # mask_data = frames[6].bytes
+      # rate_head = json.loads(frames[7].bytes)
+      # rate_data = frames[8].bytes
+    else:
+      raise RuntimeError('Need header_detail to be "all" or "basic"')
+    print conf
+
+    # Save the header data
+    self.header = {
+      'htype' : 'eiger-stream',
+      'directory' : directory,
+      'image_template' : image_template,
+      'configuration' : conf
+    }
+
+  def is_header(self):
+    '''
+    This is a header object
+
+    '''
+    return True
+
+  def as_imageset(self, filename):
+    '''
+    Return as an imageset
+
+    '''
+    from dxtbx.format.FormatEigerStream import FormatEigerStream
+    from dxtbx.imageset import MultiFileReader, ImageSweep
+
+    # Create the reader
+    reader = MultiFileReader(FormatEigerStream, [filename])
+
+    # Create the sweep
+    return ImageSweep(reader)
+
+
+class Image(Result):
+  '''
+  A class to represent an image
+
+  '''
+
+  def __init__(self, frames, header):
+    '''
+    Create the image object
+
+    '''
+    import json
+
+    super(Image, self).__init__()
+
+    # Load stuff
+    head = json.loads(frames[0].bytes)
+    info = json.loads(frames[1].bytes)
+    data = frames[2].bytes
+    time = json.loads(frames[3].bytes)
+
+    # The image number and data
+    self.count = head['frame']
+    self.data = data
+    self.info = info
+
+    # The dimensions
+    shape = info['shape']
+    dtype = info['type']
+    denco = info['encoding']
+    dsize = info['size']
+
+    # The timing info
+    self.start_time = time['start_time']
+    self.stop_time = time['stop_time']
+    self.real_time = time['real_time']
+
+    # Check the sizes
+    assert shape[1] == header.header['configuration']['x_pixels_in_detector']
+    assert shape[0] == header.header['configuration']['y_pixels_in_detector']
+
+    # # Check the image data hash
+    # assert hashlib.md5(data).hexdigest() == head['hash']
+    # if header.compression == 'lz4':
+    #   assert denco == 'lz4<'
+    # elif header.compression == 'bslz4':
+    #   assert denco == 'bs32-lz4<'
+    # else:
+    #   raise RuntimeError('Unknown compression')
+
+  def is_image(self):
+    '''
+    Return that the object is an image
+
+    '''
+    return True
+
+
+class EndOfSeries(Result):
+  '''
+  Class to represent the end of a series
+
+  '''
+
+  def is_endofseries(self):
+    '''
+    Get the end of series
+
+    '''
+    return True
+
+
+class Decoder(object):
+  """
+  Decodes zmq frames from EIGER ZMQ stream into dxtbx format object
+
+  """
+
+  def __init__(self, directory, image_template):
+    '''
+    Initialize the processor
+
+    '''
+    self.header = None
+    self.directory = directory
+    self.image_template = image_template
+
+  def decode(self, frames):
+    """
+    Decode and process EIGER ZMQ stream frames
+
+    """
+    import json
+    header = json.loads(frames[0].bytes)
+    if header["htype"].startswith("dheader-"):
+      return self.decode_header(frames)
+    elif header["htype"].startswith("dimage-"):
+      return self.decode_image(frames)
+    elif header["htype"].startswith("dseries_end"):
+      return self.decode_endofseries(frames)
+    else:
+      raise RuntimeError("No EIGER ZMQ message received")
+
+  def decode_header(self, frames):
+    '''
+    Decode a header message
+
+    '''
+    self.header = Header(frames, self.directory, self.image_template)
+    return self.header
+
+  def decode_image(self, frames):
+    '''
+    Decode an image object
+
+    '''
+    if self.header is None:
+      raise RuntimeError('Need header information before reading images')
+    return Image(frames, self.header)
+
+  def decode_endofseries(self, frames):
+    '''
+    Decode an endofseries object
+
+    '''
+    return EndOfSeries()
