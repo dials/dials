02314b72134e9f70e7bd046ed85cd3357b21e5f1
James Parkhurst <james.parkhurst@diamond.ac.uk>
2016-09-16 09:42:18 +0100

    removed old redundant code

--- algorithms/background/const_d/__init__.py
+++
@@ -1,4 +0,0 @@
-from __future__ import division
-from dials.array_family import flex # import dependency
-from dials_algorithms_background_const_d_ext import *
-from algorithm import BackgroundAlgorithm
