e04076e620da5771cd3651b1fabb535d4772b7bc
graeme-winter <graeme.winter@gmail.com>
2016-10-13 20:58:59 +0100

    Added much more diagnostic information; compute RUB for every unique reflection; still no closer to something which avoids this from sadabs:
     ** Inconsistent 2-theta values in same scan **
    
     - more helpful diagnostics would be useful...

--- util/export_hkl.py
+++ util/export_hkl.py
@@ -12,6 +12,7 @@ def export_hkl(integrated_data, experiment_list, hklout, run=0,
 
   from logging import info
   from dials.array_family import flex
+  import math
 
   # for the moment assume (and assert) that we will convert data from exactly
   # one lattice...
@@ -75,10 +76,6 @@ def export_hkl(integrated_data, experiment_list, hklout, run=0,
   perm = sorted(indices, key=lambda k: integrated_data['miller_index'][k])
   integrated_data = integrated_data.select(flex.size_t(perm))
 
-  # tricky bit here will be calculating direction cosines of the incoming
-  # beam and the outgoing beam w.r.t. the crystal unit cell axes at the time
-  # so need to compute R U B for each reflection then - cache these in RUBs
-
   from scitbx import matrix
 
   assert (not experiment.goniometer is None)
@@ -94,23 +91,20 @@ def export_hkl(integrated_data, experiment_list, hklout, run=0,
   S = matrix.sqr(experiment.goniometer.get_setting_rotation())
   unit_cell = experiment.crystal.get_unit_cell()
 
+  info('Unit cell parameters from experiment: %.2f %.2f %.2f %.2f %.2f %.2f' %
+       unit_cell.parameters())
+
+  m_format = '%6.3f%6.3f%6.3f\n%6.3f%6.3f%6.3f\n%6.3f%6.3f%6.3f'
+
+  info('Goniometer fixed matrix:\n%s' % (m_format % F.elems))
+  info('Goniometer setting matrix:\n%s' % (m_format % S.elems))
+  info('Goniometer scan axis:\n%6.3f%6.3f%6.3f' % (axis.elems))
+
   from scitbx.array_family import flex
   from math import floor, sqrt, pi
 
   assert(not experiment.scan is None)
   image_range = experiment.scan.get_image_range()
-
-  # somewhere to stash R*U*B for each frame where R is full goniometer
-  # matrix i.e. setting * scan * fixed matrix
-
-  RUBs = { }
-
-  for b in range(image_range[0], image_range[1] + 1):
-    UB = experiment.crystal.get_A_at_scan_point(b - image_range[0])
-    phi_start, phi_range = experiment.scan.get_image_oscillation(b)
-    phi = phi_start + 0.5 * phi_range
-    R = axis.axis_and_angle_as_r3_rotation_matrix(phi, deg=True)
-    RUBs[b - image_range[0]] = S * R * F * UB
 
   from cctbx.array_family import flex as cflex # implicit import
   from cctbx.miller import map_to_asu_isym # implicit import
@@ -119,9 +113,6 @@ def export_hkl(integrated_data, experiment_list, hklout, run=0,
 
   nref = len(integrated_data['miller_index'])
   zdet = flex.double(integrated_data['xyzcal.px'].parts()[2])
-
-  # only using this to index into RUBs above
-  iframe = flex.floor(zdet).iround()
 
   miller_index = integrated_data['miller_index']
 
@@ -168,6 +159,14 @@ def export_hkl(integrated_data, experiment_list, hklout, run=0,
   panel = experiment.detector[0]
   dims = panel.get_image_size()
   pixel = panel.get_pixel_size()
+  fast_axis = matrix.col(panel.get_fast_axis())
+  slow_axis = matrix.col(panel.get_slow_axis())
+
+  info('Detector axes:')
+  info('%6.3f%6.3f%6.3f' % (fast_axis.elems))
+  info('%6.3f%6.3f%6.3f' % (slow_axis.elems))
+
+  origin = matrix.col(panel.get_origin())
   scl_x = 512.0 / (dims[0] * pixel[0])
   scl_y = 512.0 / (dims[1] * pixel[1])
   phi_start, phi_range = experiment.scan.get_image_oscillation(image_range[0])
@@ -175,25 +174,37 @@ def export_hkl(integrated_data, experiment_list, hklout, run=0,
   for j in range(nref):
     h, k, l = miller_index[j]
     x_mm, y_mm, z_rad = integrated_data['xyzobs.mm.value'][j]
-    z0 = integrated_data['xyzcal.px'][j][2] % 1
+    z0 = integrated_data['xyzcal.px'][j][2]
     istol = int(round(10000 * unit_cell.stol((h, k, l))))
-    # weighted average UB based on how far through the image the spot is
-    RUB = (1 - z0) * RUBs[iframe[j] - image_range[0]] + \
-      z0 * RUBs[iframe[j] - image_range[0] + 1]
+
+    # properly compute RUB for every reflection
+    UB = experiment.crystal.get_A_at_scan_point(int(round(z0)))
+    phi = phi_start + z0 * phi_range
+    R = axis.axis_and_angle_as_r3_rotation_matrix(phi, deg=True)
+    RUB = S * R * F * UB
+
     x = RUB * (h, k, l)
     s = (s0 + x).normalize()
+
+    # can also compute s based on centre of mass of spot
+    # s = (origin + x_mm * fast_axis + y_mm * slow_axis).normalize()
+
     astar = (RUB * (1, 0, 0)).normalize()
     bstar = (RUB * (0, 1, 0)).normalize()
     cstar = (RUB * (0, 0, 1)).normalize()
+
     ix = beam.dot(astar)
     iy = beam.dot(bstar)
     iz = beam.dot(cstar)
+
     dx = s.dot(astar)
     dy = s.dot(bstar)
     dz = s.dot(cstar)
+
     x = x_mm * scl_x
     y = y_mm * scl_y
     z = (z_rad * 180 / pi - phi_start) / phi_range
+
     fout.write('%4d%4d%4d%8.2f%8.2f%4d%8.5f%8.5f%8.5f%8.5f%8.5f%8.5f' % \
                (h, k, l, I[j], sigI[j], run, ix, dx, iy, dy, iz, dz))
     fout.write('%7.2f%7.2f%8.2f%7.3f%5d\n' % (x, y, z, scl[j], istol))
