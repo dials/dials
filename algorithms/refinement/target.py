c39d3582dddf33e8dc902c4c920716b5b08d7b18
dgwaterman <dgwaterman@gmail.com>
2016-10-27 11:58:25 +0100

    avoid zero blocks

--- algorithms/refinement/target.py
+++ algorithms/refinement/target.py
@@ -345,6 +345,7 @@ class Target(object):
       nblocks = nproc
     # ensure at least 100 reflections per block
     nblocks = min(nblocks, int(len(self._matches)/100))
+    nblocks = max(nblocks, 1)
     blocksize = int(floor(len(self._matches) / nblocks))
     blocks = []
     for block_num in range(nblocks - 1):
