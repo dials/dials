757018d2c751aed323cf898f85594eb3a9cc332f
rjgildea <richard.gildea@diamond.ac.uk>
2016-11-29 13:08:19 +0000

    add help for dials.align_crystal

--- command_line/align_crystal.py
+++ command_line/align_crystal.py
@@ -8,6 +8,26 @@ import iotbx.phil
 from cctbx import sgtbx
 
 help_message = '''
+Calculation of possible goniometer settings for re-alignment of crystal axes.
+
+By default the program will attempt to calculate possible goniometer settings to
+align the primary crystal axes with the principle goniometer axis. Optionally
+vectors to align may be specified in pairs using the vector= parameter. If
+mode=main (default), then the first vector of a pair will be aligned along the
+principle goniometer axis, with the second vector placed in the plane containing
+the beam vector and the principle goniometer axis. This allows for rotation of
+the crystal around a principle crystal axis. If mode=cusp, then the first vector
+is aligned perpendicular to the beam and the principle goniometer axis, with the
+second vector placed in the plane containing the first crystal vector and the
+principle goniometer axis.
+
+Examples::
+
+dials.align_crystal experiments.json
+
+dials.align_crystal experiments.json vector=0,0,1 vector=0,1,0
+
+dials.align_crystal experiments.json frame=direct
 
 '''
 
