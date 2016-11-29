540869d16974880f93af9c47eeade2569120d969
rjgildea <richard.gildea@diamond.ac.uk>
2016-11-29 12:23:15 +0000

    add help for dials.goniometer_calibration; add test

--- run_tests.py
+++ run_tests.py
@@ -121,6 +121,7 @@ tst_list = (
     "$D/test/command_line/tst_stills_process.py",
     "$D/test/command_line/tst_two_theta_refine.py",
     "$D/test/command_line/tst_align_crystal.py",
+    "$D/test/command_line/tst_goniometer_calibration.py",
     "$D/test/framework/tst_interface.py",
     "$D/test/model/data/tst_observation.py",
     "$D/test/model/data/tst_pixel_list.py",
