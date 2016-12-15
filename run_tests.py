2dd27e6fa1caccda20daea3f559644000b9f385f
rjgildea <richard.gildea@diamond.ac.uk>
2016-12-15 10:54:32 +0000

    add another small molecule indexing test case

--- run_tests.py
+++ run_tests.py
@@ -150,6 +150,7 @@ tst_list = (
     ["$D/test/algorithms/indexing/tst_index.py", "15"],
     ["$D/test/algorithms/indexing/tst_index.py", "16"],
     ["$D/test/algorithms/indexing/tst_index.py", "17"],
+    ["$D/test/algorithms/indexing/tst_index.py", "18"],
     "$D/test/algorithms/indexing/tst_assign_indices.py",
     "$D/test/command_line/tst_refine_bravais_settings.py",
     #["$D/test/command_line/tst_discover_better_experimental_model.py", "1"],
