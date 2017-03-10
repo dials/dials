62a54da76a430b9a82310ca4f20824eabab7bbcf
rjgildea <richard.gildea@diamond.ac.uk>
2017-03-10 11:24:29 +0000

    test stereographic projections json output

--- test/command_line/tst_stereographic_projections.py
+++ test/command_line/tst_stereographic_projections.py
@@ -11,11 +11,25 @@ def run():
     exit(0)
 
   path = os.path.join(dials_regression, "experiment_test_data")
-  cmd = "dials.stereographic_projection %s/experiment_1.json hkl_limit=4 plot.show=False plot.filename=proj.png" %(path)
+  cmd = " ".join((
+    "dials.stereographic_projection",
+    "%s/experiment_1.json" %path,
+    "hkl_limit=4",
+    "plot.show=False",
+    "plot.filename=proj.png",
+    "json.filename=proj.json"))
+  print cmd
   result = easy_run.fully_buffered(cmd).raise_if_errors()
   assert os.path.exists("projections.txt")
   assert os.path.exists("proj.png")
+  assert os.path.exists("proj.json")
 
+  import json
+  with open("proj.json", "rb") as f:
+    d = json.load(f)
+    assert d.keys() == ['data', 'layout']
+    assert d['data'][0]['name'] == 'stereographic_projections'
+    assert len(d['data'][0]['x']) == 578
 
 if __name__ == '__main__':
   from dials.test import cd_auto
