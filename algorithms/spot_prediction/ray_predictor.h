18f8e3bc551d439e70a9f91bb4215f9671689334
James Parkhurst <james.parkhurst@diamond.ac.uk>
2017-03-23 12:07:35 +0000

    Added ray predictor for fractional indices

--- algorithms/spot_prediction/ray_predictor.h
+++ algorithms/spot_prediction/ray_predictor.h
@@ -133,6 +133,89 @@ namespace dials { namespace algorithms {
   typedef cctbx::miller::index <> miller_index;
 
 
+  class RayPredictor {
+  public:
+
+    // Typedef the miller_index type
+    typedef cctbx::miller::index <> miller_index;
+
+    /**
+     * Initialise the ray predictor.
+     * @param s0 The incident beam vector
+     * @param m2 The rotation axis
+     * @param dphi The total oscillation range
+     */
+    RayPredictor(vec3 <double> m2,
+                 mat3 <double> fixed_rotation,
+                 mat3 <double> setting_rotation)
+      : fixed_rotation_(fixed_rotation),
+        setting_rotation_(setting_rotation),
+        setting_rotation_inv_(setting_rotation_.inverse()),
+        m2_(m2.normalize()) {}
+
+    /** Virtual destructor to allow inheritance */
+    virtual ~RayPredictor() {}
+
+    /**
+     * Predict the spot locations on the image detector.
+     *
+     * The algorithm performs the following procedure:
+     *
+     *  - For the miller index, the rotation angle at which the diffraction
+     *    conditions are met is calculated.
+     *
+     *  - The rotation angles are then checked to see if they are within the
+     *    rotation range.
+     *
+     *  - The reciprocal lattice vectors are then calculated, followed by the
+     *    diffracted beam vector for each reflection.
+     *
+     *  - The diffracted beam vectors are then classified according to whether
+     *    the reciprocal lattice point is entering or exiting the Ewald sphere.
+     *
+     * @param h The miller index
+     * @returns An array of predicted reflections
+     */
+    Ray operator()(mat3<double> UB, vec3<double> s0, vec3<double> h, bool entering_in) const {
+
+      vec3<double> s0_m2_plane = s0.cross(setting_rotation_ * m2_).normalize();
+
+      // Calculate the reciprocal space vector
+      vec3 <double> pstar0 = fixed_rotation_ * UB * h;
+
+      // Try to calculate the diffracting rotation angles
+      vec2<double> phi = rotation_angles(setting_rotation_inv_ * s0, m2_, pstar0);
+
+      // Loop through the 2 rotation angles
+      for (std::size_t i = 0; i < phi.size(); ++i) {
+
+        // Calculate the reciprocal space vector and diffracted beam vector
+        vec3 <double> pstar = setting_rotation_ * pstar0.unit_rotate_around_origin(m2_, phi[i]);
+        vec3 <double> s1 = s0_ + pstar;
+
+        double small = 1.0e-8;
+
+        DIALS_ASSERT(std::abs(s1.length() - s0_.length()) < small);
+
+        // Calculate the direction of reflection passage
+        bool entering = s1 * s0_m2_plane < 0.;
+        if (entering == entering_in) {
+          return Ray(s1, mod_2pi(phi[i]), entering);
+        }
+      }
+      DIALS_ERROR("No ray");
+      return Ray();
+    }
+
+  private:
+    mat3 <double> fixed_rotation_;
+    mat3 <double> setting_rotation_;
+    mat3 <double> setting_rotation_inv_;
+    vec3 <double> s0_;
+    vec3 <double> m2_;
+    vec3 <double> s0_m2_plane;
+  };
+
 }} // namespace dials::algorithms
 
 #endif // DIALS_ALGORITHMS_SPOT_PREDICTION_RAY_PREDICTOR_H
