2357d1e89fb03a7aee9fa764180b62236622e622
graeme-winter <graeme.winter@gmail.com>
2017-05-23 07:55:54 +0100

    Rest of commit

---
+++ algorithms/shadowing/filter.py
@@ -0,0 +1,38 @@
+from __future__ import absolute_import, division
+
+def filter_shadowed_reflections(experiments, reflections,
+                                experiment_goniometer=False):
+  from dials.util import is_inside_polygon
+  from scitbx.array_family import flex
+  shadowed = flex.bool(reflections.size(), False)
+  for expt_id in range(len(experiments)):
+    expt = experiments[expt_id]
+    imgset = expt.imageset
+    if experiment_goniometer:
+      masker = imgset.reader().get_format().get_goniometer_shadow_masker(
+        goniometer=expt.goniometer)
+    else:
+      masker = imgset.reader().get_format().get_goniometer_shadow_masker()
+    detector = expt.detector
+    sel = reflections['id'] == expt_id
+    isel = sel.iselection()
+    x,y,z = reflections['xyzcal.px'].select(isel).parts()
+    start, end = expt.scan.get_array_range()
+    for i in range(start, end):
+      shadow = masker.project_extrema(
+        detector, expt.scan.get_angle_from_array_index(i))
+      img_sel = (z >= i) & (z < (i+1))
+      img_isel = img_sel.iselection()
+      for p_id in range(len(detector)):
+        panel = reflections['panel'].select(img_isel)
+        if shadow[p_id].size() < 4:
+          continue
+        panel_isel = img_isel.select(panel == p_id)
+        inside = is_inside_polygon(
+          shadow[p_id],
+          flex.vec2_double(x.select(isel.select(panel_isel)),
+                           y.select(isel.select(panel_isel))))
+        shadowed.set_selected(panel_isel, inside)
+
+  return shadowed
+
