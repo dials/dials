a5c6344acab54c594c0fbbfdc5ece186addb569b
James Parkhurst <james.parkhurst@diamond.ac.uk>
2017-05-26 11:03:11 +0100

    Fixed tests for changesa in profile model

--- test/command_line/tst_create_profile_model.py
+++ test/command_line/tst_create_profile_model.py
@@ -15,6 +15,40 @@ class Test(object):
     self.path = join(dials_regression, "integration_test_data", "i04-weak-data2")
 
   def run(self):
+
+    self.tst_basic()
+    self.tst_extended()
+
+  def tst_basic(self):
+    from os.path import join
+    from libtbx import easy_run
+    from dxtbx.model.experiment_list import ExperimentListFactory
+
+    # Call dials.create_profile_model
+    easy_run.fully_buffered([
+      'dials.create_profile_model',
+      join(self.path, 'experiments.json'),
+      join(self.path, 'indexed.pickle'),
+      'sigma_m_algorithm=basic',
+    ]).raise_if_errors()
+
+
+    experiments =  ExperimentListFactory.from_json_file(
+      "experiments_with_profile_model.json",
+      check_format=False)
+    sigma_b = experiments[0].profile.sigma_b(deg=True)
+    sigma_m = experiments[0].profile.sigma_m(deg=True)
+    eps = 1e-3
+    try:
+      assert(abs(sigma_b - 0.02195) < eps)
+      assert(abs(sigma_m - 0.06833) < eps)
+    except Exception:
+      print sigma_b
+      print sigma_m
+      raise
+    print 'OK'
+
+  def tst_extended(self):
     from os.path import join
     from libtbx import easy_run
     from dxtbx.model.experiment_list import ExperimentListFactory
@@ -35,7 +69,7 @@ class Test(object):
     eps = 1e-3
     try:
       assert(abs(sigma_b - 0.02195) < eps)
-      assert(abs(sigma_m - 0.06833) < eps)
+      assert(abs(sigma_m - 0.04187) < eps)
     except Exception:
       print sigma_b
       print sigma_m
