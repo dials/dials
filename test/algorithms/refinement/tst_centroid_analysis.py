8a8cf11cf771dd3a2cf5a9b6d1057e6efa5672b4
dgwaterman <dgwaterman@gmail.com>
2017-06-23 15:52:56 +0100

    Tests for CentroidAnalyser. This shows that automatic interval width determination for #162 is not yet robust or fully understood, so should not yet be made default behaviour

--- test/algorithms/refinement/tst_centroid_analysis.py
+++ test/algorithms/refinement/tst_centroid_analysis.py
@@ -34,7 +34,7 @@ def test1():
     test=os.path.isdir)
 
   # Use dials_regression/indexing_test_data/i04_weak_data/indexed.pickle for
-  # this test. This file is slightly malformed, in that the observed and
+  # this test. This file is somewhat malformed, in that the observed and
   # calculated phi centroids are identical. We should not produce a phi
   # periodogram in that case
   data_dir = os.path.join(dials_regression, "indexing_test_data", "i04_weak_data")
@@ -91,12 +91,119 @@ def test1():
   assert results['x_periodogram'] is not None
   assert results['y_periodogram'] is not None
   assert results['phi_periodogram'] is None
+
   print "OK"
+  return
+
+
+def test2():
+  """Test on simulated data"""
+
+  # Get models for reflection prediction
+  import dials.test.algorithms.refinement.setup_geometry as setup_geometry
+
+  from libtbx.phil import parse
+  overrides = """geometry.parameters.crystal.a.length.value = 77
+  geometry.parameters.crystal.b.length.value = 77
+  geometry.parameters.crystal.c.length.value = 37"""
+
+  master_phil = parse("""
+      include scope dials.test.algorithms.refinement.geometry_phil
+      """, process_includes=True)
+
+  from dxtbx.model import Crystal
+  models = setup_geometry.Extract(master_phil)
+  crystal = Crystal(
+    real_space_a=(2.62783398111729, -63.387215823567125, -45.751375737456975),
+    real_space_b=(15.246640559660356, -44.48254330406616, 62.50501032727026),
+    real_space_c=(-76.67246874451074, -11.01804131886244, 10.861322446352226),
+    space_group_symbol="I 2 3")
+  detector = models.detector
+  goniometer = models.goniometer
+  beam = models.beam
+
+  # Build a mock scan for a 180 degree sweep
+  from dxtbx.model import ScanFactory
+  sf = ScanFactory()
+  scan = sf.make_scan(image_range = (1,1800),
+                        exposure_times = 0.1,
+                        oscillation = (0, 0.1),
+                        epochs = range(1800),
+                        deg = True)
+
+  # Build an experiment list
+  from dxtbx.model.experiment_list import ExperimentList, Experiment
+  experiments = ExperimentList()
+  experiments.append(Experiment(
+        beam=beam, detector=detector, goniometer=goniometer,
+        scan=scan, crystal=crystal, imageset=None))
+
+  # Generate all indices in a 1.5 Angstrom sphere
+  from dials.algorithms.spot_prediction import IndexGenerator
+  from cctbx.sgtbx import space_group, space_group_symbols
+  resolution = 1.5
+  index_generator = IndexGenerator(crystal.get_unit_cell(),
+                  space_group(space_group_symbols(1).hall()).type(), resolution)
+  indices = index_generator.to_array()
+
+  # Predict rays within the sweep range
+  from dials.algorithms.refinement.prediction import ScansRayPredictor
+  sweep_range = scan.get_oscillation_range(deg=False)
+  ray_predictor = ScansRayPredictor(experiments, sweep_range)
+  obs_refs = ray_predictor(indices)
+
+  # Take only those rays that intersect the detector
+  from dials.algorithms.spot_prediction import ray_intersection
+  intersects = ray_intersection(detector, obs_refs)
+  obs_refs = obs_refs.select(intersects)
+
+  # Make a reflection predictor and re-predict for all these reflections. The
+  # result is the same, but we gain also the flags and xyzcal.px columns
+  from dials.algorithms.refinement.prediction import ExperimentsPredictor
+  ref_predictor = ExperimentsPredictor(experiments)
+  obs_refs['id'] = flex.int(len(obs_refs), 0)
+  obs_refs = ref_predictor(obs_refs)
+
+  # Copy 'observed' centroids from the predicted ones, applying sinusoidal
+  # offsets
+  obs_x, obs_y, obs_z = obs_refs['xyzcal.mm'].parts()
+
+  # obs_z is in range (0, pi). Calculate offsets for phi at twice that
+  # frequency
+  im_width = scan.get_oscillation(deg=False)[1]
+  z_off = flex.sin(2 * obs_z) * im_width
+  obs_z += z_off
+
+  # Calculate offsets for x
+  pixel_size = detector[0].get_pixel_size()
+  x_off = flex.sin(20 * obs_z) * pixel_size[0]
+
+  # Calculate offsets for y with a phase-shifted sine wave
+  from math import pi
+  y_off = flex.sin(4 * obs_z + pi/6) * pixel_size[1]
+
+  # Incorporate the offsets into the 'observed' centroids
+  obs_z += z_off
+  obs_x += x_off
+  obs_y += y_off
+  obs_refs['xyzobs.mm.value'] = flex.vec3_double(obs_x, obs_y, obs_z)
+
+  # Now do centroid analysis of the residuals
+  results = CentroidAnalyser(obs_refs, debug=True)()
+
+  # FIXME this test shows that the suggested interval width heuristic is not
+  # yet robust. This simulation function seems a useful direction to proceed
+  # in though
+  raise RuntimeError('test2 failed')
+
+  print "OK"
+  return
 
 if __name__ == '__main__':
 
   # simple test on an indexed.pickle
   test1()
 
-  # fixme, need another test on a pickle that produces suggested interval
-  # widths
+  # A test using simulated data with sinusodial residuals
+  test2()
+
