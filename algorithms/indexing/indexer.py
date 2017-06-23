a67fa86e4fb859ed80cdddf3167973b2d6e6ac06
rjgildea <richard.gildea@diamond.ac.uk>
2017-06-23 10:12:25 -0500

    nearest_neighbour analysis: expose max_height_fraction via phil parameter

--- algorithms/indexing/indexer.py
+++ algorithms/indexing/indexer.py
@@ -64,6 +64,9 @@ max_cell_estimation
     .type = choice
     .help = "Choose between linear or logarithmic bins for nearest neighbour"
             "histogram analysis."
+  max_height_fraction = 0.25
+    .type = float(value_min=0, value_max=1)
+    .expert_level=2
 }
 '''
 
@@ -1019,6 +1022,7 @@ class indexer_base(object):
           step_size=params.step_size,
           nearest_neighbor_percentile=params.nearest_neighbor_percentile,
           histogram_binning=params.histogram_binning,
+          max_height_fraction=params.max_height_fraction,
           filter_ice=params.filter_ice,
           filter_overlaps=params.filter_overlaps,
           overlaps_border=params.overlaps_border).max_cell
@@ -1945,6 +1949,7 @@ def detect_non_primitive_basis(miller_indices, threshold=0.9):
 
 def find_max_cell(reflections, max_cell_multiplier, step_size,
                   nearest_neighbor_percentile, histogram_binning='linear',
+                  max_height_fraction=0.25,
                   filter_ice=True, filter_overlaps=True, overlaps_border=0):
   logger.debug('Finding suitable max_cell based on %i reflections' % len(reflections))
   # Exclude potential ice-ring spots from nearest neighbour analysis if needed
@@ -2013,6 +2018,7 @@ def find_max_cell(reflections, max_cell_multiplier, step_size,
 
   NN = neighbor_analysis(reflections,
                          step_size=step_size,
+                         max_height_fraction=max_height_fraction,
                          tolerance=max_cell_multiplier,
                          percentile=nearest_neighbor_percentile,
                          histogram_binning=histogram_binning)
