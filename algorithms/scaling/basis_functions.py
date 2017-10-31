Development towards scaling against fixed Ih.

algorithms/scaling/basis_functions.py
@@ -26,6 +26,25 @@ class basis_function(object):
     in minimisation'''
     return self.calculate_scale_factors(), self.calculate_derivatives()
 
+class KB_basis_function(basis_function):
+  def calculate_scale_factors(self):
+    scale_term = self.parameters[0:1]
+    decay_term = self.parameters[1:2]
+    self.data_manager.g_scale.set_scale_factors(scale_term)
+    self.data_manager.g_decay.set_scale_factors(decay_term)
+    d = self.data_manager.g_decay.d_values
+    return scale_term * flex.double(np.exp(decay_term/(2.0*(d**2))))
+
+  def calculate_derivatives(self):
+    d = self.data_manager.g_decay.d_values
+    B = self.data_manager.g_decay.scale_factors
+    decay_term = flex.double(np.exp(B/(2.0*(d**2))))
+    scale_term = self.data_manager.g_scale.scale_factors
+    derivatives = flex.double([])
+    derivatives.extend(decay_term)
+    derivatives.extend(scale_term * decay_term / (2*(d**2)))
+    return derivatives
+
 class xds_basis_function(basis_function):
   '''Subclass of basis_function for xds parameterisation'''
   def calculate_scale_factors(self):
@@ -38,8 +57,10 @@ class xds_basis_function(basis_function):
   def calculate_derivatives(self):
     '''Derivatives are fixed by the parameterisation'''
     derivatives = self.data_manager.g_parameterisation[
-      self.data_manager.active_parameterisation].calculate_smooth_derivatives()
-    return derivatives
+      self.data_manager.active_parameterisation].calculate_smooth_derivatives() 
+    #should this be multiplied by constant_g_values?
+    return derivatives * flex.double(np.tile(self.data_manager.constant_g_values, 
+                                             self.data_manager.n_active_params))
 
 class aimless_basis_function(basis_function):
   '''Subclass of basis_function for aimless parameterisation'''

algorithms/scaling/data_plotter.py
@@ -221,7 +221,7 @@ def plot_absorption_surface(data_man, outputfile=None):
   from scitbx import math
   from scitbx.array_family import flex
   import math as pymath
-  STEPS = 100
+  STEPS = 50
   theta = np.linspace(0, 2 * np.pi, 2*STEPS)
   phi = np.linspace(0, np.pi, STEPS)
   THETA, PHI = np.meshgrid(theta, phi)
@@ -250,7 +250,7 @@ def plot_absorption_surface(data_man, outputfile=None):
   Y = Intensity * np.sin(PHI) * np.sin(THETA)
   Z = Intensity * np.cos(PHI)
   from matplotlib import cm
-
+ 
   rel_Int = (Intensity - Intensity.min())/(Intensity.max() - Intensity.min())
   #print "max, min absorption factors are (%s,%s)" % (Intensity.max(),Intensity.min())
   #my_col = cm.jet(Intensity)