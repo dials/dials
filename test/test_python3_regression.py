ae47e4240f00eec0b38208c7a3d574595d1bfe9c
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-11-23 21:28:53 +0000

    Enforce Python3 syntax for all new files and all files that are currently Python3 compatible.
    See #459 for more details.

---
+++ test/test_python3_regression.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import, division, print_function
+
+def test_no_new_python3_incompatible_code_is_introduced_into_this_module():
+  import dials
+  import pytest
+  import dials.test.python3_regression as py3test
+  result = py3test.find_new_python3_incompatible_code(dials)
+  if result is None:
+    pytest.skip('No python3 interpreter available')
+  elif result:
+    pytest.fail(result)
