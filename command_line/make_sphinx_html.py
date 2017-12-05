e032db0c65bc9a394b241d7ef081b8bd23160f98
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-12-05 14:12:56 +0000

    Add '--strict' option to fix all Sphinx warnings.
    Now everything becomes an error.
    
    Using this option fixes #479.

--- command_line/make_sphinx_html.py
+++ command_line/make_sphinx_html.py
@@ -1,9 +1,10 @@
 # LIBTBX_SET_DISPATCHER_NAME dev.dials.make_sphinx_html
 
-from __future__ import absolute_import, division
-from dials.util.procrunner import run_process
+from __future__ import absolute_import, division, print_function
+from libtbx.procrunner import run_process
 import libtbx.load_env
 import shutil
+from optparse import SUPPRESS_HELP, OptionParser
 import os
 
 # Disable all HTTPS verification. This is to work around an issue
@@ -37,7 +38,7 @@ def update_dials_download_links():
   from datetime import datetime
   import json
   import re
-  print "Checking DIALS release status: ",
+  print("Checking DIALS release status: ", end="")
   if Toolbox().download_to_file('https://api.github.com/repos/dials/dials/releases/latest', release_json, cache=False):
     with open(release_json, 'r') as json_data:
       release_info = json.load(json_data)
@@ -48,16 +49,16 @@ def update_dials_download_links():
     caption = "Stable Release"
     if 'name' in release_info:
       caption = caption + ": " + release_info['name']
-      print "Most recent major DIALS release is:", release_info['name']
+      print("Most recent major DIALS release is:", release_info['name'])
     else:
-      print "Could not determine most recent major DIALS release"
+      print("Could not determine most recent major DIALS release")
     release.write(caption + "\n" + '=' * len(caption) + "\n\n")
 
     release.write('The current stable release can be downloaded from `Github <https://github.com/dials/dials/releases/latest>`_,\n')
     release.write('where you can also find further `release notes <https://github.com/dials/dials/releases/latest>`_.\n\n')
 
     def download_button(text, version, link):
-      print "  %s %s -> %s" % (version, text, link)
+      print("  %s %s -> %s" % (version, text, link))
       return ".. button::\n   :text: DIALS %s %s\n   :link: %s\n\n" % (version, text, link)
 
     assets = {}
@@ -83,20 +84,34 @@ def update_dials_download_links():
     release.write("".join(sorted(buttons)))
 
 if __name__ == "__main__":
+
+  parser = OptionParser(description="Generate documentation website for DIALS")
+  parser.add_option("-?", action="help", help=SUPPRESS_HELP)
+  parser.add_option("-s", "--strict", dest="strict",
+      action="store_true", default=False,
+      help="Run in strict mode and stop on encountering any errors or warnings")
+  options, args = parser.parse_args()
+
   update_dials_download_links()
   cctbx_base = libtbx.env.find_in_repositories("cctbx_project")
   dials_dir = libtbx.env.find_in_repositories("dials")
   dials_github_io = libtbx.env.find_in_repositories("dials.github.io")
-  assert (dials_github_io is not None)
-  assert (cctbx_base is not None)
+  assert dials_github_io is not None, \
+      "Repository dials.github.io needs to be checked out and configured in modules directory"
+  assert cctbx_base is not None
   base_dir = os.path.dirname(cctbx_base)
   dest_dir = dials_github_io
   os.chdir(os.path.join(dials_dir, "doc", "sphinx"))
-  result = run_process(["make", "clean"])
+
+  env = {}
+  if options.strict:
+    env['SPHINXOPTS'] = '-W'
+
+  result = run_process(["make", "clean"], environment_override=env)
   assert result['exitcode'] == 0, \
       'make clean failed with exit code %d' % result['exitcode']
-  result = run_process(["make", "html"])
+  result = run_process(["make", "html"], environment_override=env)
   assert result['exitcode'] == 0, \
       'make html failed with exit code %d' % result['exitcode']
-  print "Copying HTML pages to", dest_dir
+  print("Copying HTML pages to", dest_dir)
   recursive_overwrite("build/html", dest_dir)
