15b9c6cf23b8083a64db92b7b766ed21b7b9f64c
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-12-15 10:20:55 +0000

    update license file location references

--- installer/dials_installer.sh
+++ installer/dials_installer.sh
@@ -3,7 +3,7 @@
 build/bin/libtbx.python modules/cctbx_project/libtbx/auto_build/create_installer.py \
   --install_script  modules/dials/installer/dials_installer.py \
   --version         dev \
-  --license         modules/dials/license.txt \
-  --readme          modules/dials/license.txt \
+  --license         modules/dials/LICENSE \
+  --readme          modules/dials/LICENSE \
   --binary \
   tmp/dials-installer-dev
