d3504576412260f17b3f7fb3098b5c1124bd3098
rjgildea <richard.gildea@diamond.ac.uk>
2018-02-22 16:47:48 +0000

    pytest

--- test/command_line/test_compare_orientation_matrices.py
+++ test/command_line/test_compare_orientation_matrices.py
@@ -1,33 +1,21 @@
 from __future__ import absolute_import, division
 
-def run():
-  import os
-  import libtbx.load_env
-  from libtbx import easy_run
-  from libtbx.test_utils import show_diff
-  try:
-    dials_regression = libtbx.env.dist_path('dials_regression')
-  except KeyError:
-    print 'FAIL: dials_regression not configured'
-    exit(0)
+import os
+from libtbx import easy_run
+
+def test_compare_orientation_matrices(dials_regression, tmpdir):
+  tmpdir.chdir()
 
   path = os.path.join(dials_regression, "refinement_test_data", "i04_weak_data")
   cmd = "dials.compare_orientation_matrices %s/experiments.json %s/regression_experiments.json" %(path, path)
   result = easy_run.fully_buffered(cmd).raise_if_errors()
   out = "\n".join(result.stdout_lines[7:])
   out = out.replace("-0", "0")
-  assert not show_diff(out, """\
+  assert out == """\
 Change of basis op: a,b,c
 Rotation matrix to transform crystal 1 to crystal 2:
 {{1.000, 0.000, 0.000},
  {0.000, 1.000, 0.000},
  {0.000, 0.000, 1.000}}
 Rotation of 0.002 degrees about axis (0.916, 0.081, 0.393)
-""")
-
-
-if __name__ == '__main__':
-  from dials.test import cd_auto
-  with cd_auto(__file__):
-    run()
-    print "OK"
+"""
--- test/command_line/test_reindex.py
+++ test/command_line/test_reindex.py
@@ -1,23 +1,14 @@
-from __future__ import absolute_import, division
+from __future__ import absolute_import, division, print_function
+
+import pytest
 import os
-import libtbx.load_env
 from libtbx import easy_run
 from libtbx import easy_pickle
-from libtbx.test_utils import approx_equal
-from libtbx.test_utils import open_tmp_directory
+from cctbx import sgtbx
 from dxtbx.serialize import load
 
-have_dials_regression = libtbx.env.has_module("dials_regression")
-if have_dials_regression:
-  dials_regression = libtbx.env.find_in_repositories(
-    relative_path="dials_regression",
-    test=os.path.isdir)
-
-
-def run():
-  if not have_dials_regression:
-    print "Skipping tst_reindex: dials_regression not available."
-    return
+def test_reindex(dials_regression, tmpdir):
+  tmpdir.chdir()
 
   data_dir = os.path.join(dials_regression, "indexing_test_data", "i04_weak_data")
   pickle_path = os.path.join(data_dir, "indexed.pickle")
@@ -28,10 +19,8 @@ def run():
               "change_of_basis_op=2a,b,c",
               "space_group=P1"]
   command = " ".join(commands)
-  print command
-  cwd = os.path.abspath(os.curdir)
-  tmp_dir = open_tmp_directory()
-  os.chdir(tmp_dir)
+  print(command)
+
   result = easy_run.fully_buffered(command=command).raise_if_errors()
   old_reflections = easy_pickle.load(pickle_path)
   assert os.path.exists('reindexed_reflections.pickle')
@@ -42,18 +31,17 @@ def run():
     'reindexed_experiments.json', check_format=False)
   h1,k1,l1 = old_reflections['miller_index'].as_vec3_double().parts()
   h2,k2,l2 = new_reflections['miller_index'].as_vec3_double().parts()
-  assert approx_equal(2*h1, h2)
-  assert approx_equal(k1, k2)
-  assert approx_equal(l1, l2)
+  assert 2*h1 == pytest.approx(h2)
+  assert k1 == pytest.approx(k2)
+  assert l1 == pytest.approx(l2)
   old_uc_params = old_experiments[0].crystal.get_unit_cell().parameters()
   new_uc_params = new_experiments[0].crystal.get_unit_cell().parameters()
-  assert approx_equal(2*old_uc_params[0], new_uc_params[0])
-  assert approx_equal(old_uc_params[1:], new_uc_params[1:])
+  assert new_uc_params[0] == pytest.approx(2*old_uc_params[0])
+  assert new_uc_params[1:] == pytest.approx(old_uc_params[1:])
   assert old_experiments[0].crystal.get_space_group().type().hall_symbol() == ' P 1'
   assert new_experiments[0].crystal.get_space_group().type().hall_symbol() == ' P 1'
 
   # set space group P4
-  from cctbx import sgtbx
   cb_op = sgtbx.change_of_basis_op('a,b,c')
   commands = ["dials.reindex",
               experiments_path,
@@ -61,7 +49,7 @@ def run():
               "change_of_basis_op=%s" %str(cb_op),
               "output.experiments=P4.json"]
   command = " ".join(commands)
-  print command
+  print(command)
   result = easy_run.fully_buffered(command=command).raise_if_errors()
   # apply one of the symops from the space group
   cb_op = sgtbx.change_of_basis_op('-x,-y,z')
@@ -70,12 +58,11 @@ def run():
               "change_of_basis_op=%s" %str(cb_op),
               "output.experiments=P4_reindexed.json"]
   command = " ".join(commands)
-  print command
+  print(command)
   result = easy_run.fully_buffered(command=command).raise_if_errors()
   new_experiments1 = load.experiment_list(
     'P4_reindexed.json', check_format=False)
-  assert approx_equal(
-    new_experiments1[0].crystal.get_A(),
+  assert new_experiments1[0].crystal.get_A() == pytest.approx(
     old_experiments[0].crystal.change_basis(cb_op).get_A())
   #
   cb_op = sgtbx.change_of_basis_op('-x,-y,z')
@@ -85,16 +72,9 @@ def run():
               "reference=P4_reindexed.json",
               "output.experiments=P4_reindexed2.json"]
   command = " ".join(commands)
-  print command
+  print(command)
   result = easy_run.fully_buffered(command=command).raise_if_errors()
   new_experiments2 = load.experiment_list(
     'P4_reindexed2.json', check_format=False)
-  assert approx_equal(
-    new_experiments1[0].crystal.get_A(),
+  assert new_experiments1[0].crystal.get_A() == pytest.approx(
     new_experiments2[0].crystal.get_A())
-
-
-
-if __name__ == '__main__':
-  run()
-  print "OK"
