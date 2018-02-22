df3c0ea8133f3c10c19f81e5ede5d49b53d0f863
rjgildea <richard.gildea@diamond.ac.uk>
2018-02-22 15:03:18 +0000

    more pytestification

---
+++ test/command_line/test_rl_png.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import, division
+import os
+from libtbx import easy_run
+
+
+def test_rl_png_datablock(dials_regression, tmpdir):
+  tmpdir.chdir()
+
+  data_dir = os.path.join(dials_regression, "centroid_test_data")
+  datablock_path = os.path.join(data_dir, "datablock.json")
+  strong_pickle = os.path.join(data_dir, "strong.pickle")
+
+  cmd = 'dials.rl_png %s %s' %(datablock_path, strong_pickle)
+  result = easy_run.fully_buffered(command=cmd).raise_if_errors()
+
+  for s in ('beam_vector', 'e3', 'rotation_axis',
+            'solution_1', 'solution_2','solution_3'):
+    assert os.path.exists('rl_%s.png' %s), s
+
+
+def test_rl_png_experiments(dials_regression, tmpdir):
+  tmpdir.chdir()
+
+  data_dir = os.path.join(dials_regression, "refinement_test_data", "i04_weak_data")
+  datablock_path = os.path.join(data_dir, "experiments.json")
+  indexed_pickle = os.path.join(data_dir, "indexed_strong.pickle")
+
+  cmd = 'dials.rl_png %s %s' %(datablock_path, indexed_pickle)
+  result = easy_run.fully_buffered(command=cmd).raise_if_errors()
+
+  for s in ('beam_vector', 'e3', 'rotation_axis', 'a', 'b','c'):
+    assert os.path.exists('rl_%s.png' %s), s
--- test/command_line/test_show.py
+++ test/command_line/test_show.py
@@ -1,21 +1,17 @@
 from __future__ import absolute_import, division
 
-def run():
-  import os
-  import libtbx.load_env
-  from libtbx import easy_run
-  from libtbx.test_utils import show_diff
-  try:
-    dials_regression = libtbx.env.dist_path('dials_regression')
-  except KeyError:
-    print 'FAIL: dials_regression not configured'
-    exit(0)
+import glob
+import os
+from libtbx import easy_run
+from libtbx.test_utils import show_diff
+
+def test_dials_show(dials_regression):
 
   path = os.path.join(dials_regression, "experiment_test_data")
 
   cmd = "dials.show %s/experiment_1.json" %path
   result = easy_run.fully_buffered(cmd).raise_if_errors()
-  assert not show_diff("\n".join(result.stdout_lines[6:]), """\
+  assert "\n".join(s.rstrip() for s in result.stdout_lines[6:]) == """\
 Experiment 0:
 Detector:
 Panel:
@@ -71,13 +67,13 @@ Crystal:
                 {-0.0043, -0.0008, -0.0248},
                 { 0.0124,  0.0200, -0.0032}}
     Mosaicity:  0.157000
-""", strip_trailing_whitespace=True)
+"""
 
   path = os.path.join(
     dials_regression, "indexing_test_data", "i04_weak_data")
   cmd = "dials.show %s/datablock_orig.json" %path
   result = easy_run.fully_buffered(cmd).raise_if_errors()
-  assert not show_diff("\n".join(result.stdout_lines[8:]), """\
+  assert "\n".join(s.rstrip() for s in result.stdout_lines[8:]) == """\
 Detector:
 Panel:
   name: Panel
@@ -118,11 +114,10 @@ Goniometer:
     Rotation axis:   {1,0,0}
     Fixed rotation:  {1,0,0,0,1,0,0,0,1}
     Setting rotation:{1,0,0,0,1,0,0,0,1}
-""", strip_trailing_whitespace=True)
+"""
 
   path = os.path.join(
     dials_regression, "centroid_test_data", "centroid_*.cbf")
-  import glob
   g = glob.glob(path)
   assert len(g) > 0, path
   cmd = "dials.show %s" %(' '.join(g))
@@ -130,7 +125,7 @@ Goniometer:
   assert (
     "Format: <class 'dxtbx.format.FormatCBFMiniPilatus.FormatCBFMiniPilatus'>"
     in result.stdout_lines), result.show_stdout()
-  assert not show_diff("\n".join(result.stdout_lines[8:]), """\
+  assert "\n".join(s.rstrip() for s in result.stdout_lines[8:]) == """\
 Detector:
 Panel:
   name: Panel
@@ -173,11 +168,4 @@ Goniometer:
     Rotation axis:   {1,0,0}
     Fixed rotation:  {1,0,0,0,1,0,0,0,1}
     Setting rotation:{1,0,0,0,1,0,0,0,1}
-""", strip_trailing_whitespace=True)
-
-
-if __name__ == '__main__':
-  from dials.test import cd_auto
-  with cd_auto(__file__):
-    run()
-    print "OK"
+"""
