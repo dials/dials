29eab9db607c3048a9df8c44cb7a8cb550ae1eda
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-03-10 12:36:30 +0000

    add Jinja2 dependency for dials.report

--- libtbx_refresh.py
+++ libtbx_refresh.py
@@ -29,8 +29,9 @@ except Exception:
 try:
   import libtbx.pkg_utils
   libtbx.pkg_utils.require('mock', '>=2.0')
-  libtbx.pkg_utils.require('pytest', '>=3.1')
   libtbx.pkg_utils.require('orderedset')
+  libtbx.pkg_utils.require('pytest', '>=3.1')
+  libtbx.pkg_utils.require('Jinja2')
 except ImportError:
   print("\n" * 10 + "Could not verify dependencies: cctbx sources out of date" + "\n" * 10)
 
