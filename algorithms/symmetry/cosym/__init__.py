9f2d080b6d30c21d96b3add9305c395df25ed706
rjgildea <richard.gildea@diamond.ac.uk>
2018-04-05 14:34:56 +0100

    Add "seed" clustering algorithm:
    - for i in range(m):
        - select only those points that don't already belong to a cluster
        - choose high density point as seed for cluster
        - for each dataset find closest copy to centroid of current cluster and assign to current cluster
    - hierarchical clustering of cluster centroids using cosine metric
    - silhouette analysis of valid (equal-sized) clusterings

--- algorithms/symmetry/cosym/__init__.py
+++ algorithms/symmetry/cosym/__init__.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, division, print_function
 import logging
 logger = logging.getLogger(__name__)
 
+import copy
 from collections import OrderedDict
 
 from libtbx import Auto
@@ -62,7 +63,7 @@ termination_params {
 }
 
 cluster {
-  method = dbscan bisect minimize_divide *agglomerative
+  method = dbscan bisect minimize_divide *agglomerative seed
     .type = choice
   dbscan {
     eps = 0.5
@@ -77,6 +78,10 @@ cluster {
   agglomerative {
     n_clusters = 2
       .type = int(value_min=1)
+  }
+  seed {
+    min_silhouette_score = 0.2
+      .type = float(value_min=-1, value_max=1)
   }
 }
 
@@ -277,7 +282,6 @@ class analyse_datasets(object):
         sym_ops_cos_angle[op].append(cos_angle[ref_idx, comp_idx])
 
     # print symops sorted by average cos(angle)
-    import copy
     sg = copy.deepcopy(self.input_space_group)
     rows = [['symop', 'order', 'sg', 'mean(cos(angle))', 'median(cos(angle))']]
     perm = flex.sort_permutation(
@@ -306,7 +310,6 @@ class analyse_datasets(object):
       #plt.clf()
 
   def cluster_analysis(self):
-    import copy
     from cctbx.sgtbx import cosets
 
     if self.params.cluster.method == 'dbscan':
@@ -317,6 +320,9 @@ class analyse_datasets(object):
       self.minimize_divide_clustering()
     elif self.params.cluster.method == 'agglomerative':
       self.agglomerative_clustering()
+    elif self.params.cluster.method == 'seed':
+      self.seed_clustering()
+
 
 
     # Number of clusters in labels, ignoring noise if present.
@@ -420,6 +426,139 @@ class analyse_datasets(object):
       linkage='average', affinity='cosine')
     model.fit(X)
     self.cluster_labels = flex.int(model.labels_.astype(np.int32))
+
+  def seed_clustering(self):
+    eps = 1e-6
+    X_orig = self.coords.as_numpy_array()
+
+    import numpy as np
+    from scipy.cluster import hierarchy
+    import scipy.spatial.distance as ssd
+    from sklearn.neighbors import NearestNeighbors
+    from sklearn import metrics
+
+    # initialise cluster labels: -1 signifies doesn't belong to a cluster
+    self.cluster_labels = flex.int(self.coords.all()[0], -1)
+
+    cluster_id = 0
+    while self.cluster_labels.count(-1) > 0:
+      dataset_ids = (flex.int_range(
+        len(self.datasets) * len(self.target.get_sym_ops())) % len(self.datasets)
+                     ).as_numpy_array()
+      coord_ids = flex.int_range(dataset_ids.size).as_numpy_array()
+
+      # select only those points that don't already belong to a cluster
+      sel = np.where(self.cluster_labels == -1)
+      X = X_orig[sel]
+      dataset_ids = dataset_ids[sel]
+      coord_ids = coord_ids[sel]
+
+      # choose a high density point as seed for cluster
+      nbrs = NearestNeighbors(
+        n_neighbors=min(11, len(X)), algorithm='brute', metric='cosine').fit(X)
+      distances, indices = nbrs.kneighbors(X)
+      average_distance = flex.double([dist[1:].mean() for dist in distances])
+      i = flex.min_index(average_distance)
+
+      d_id = dataset_ids[i]
+      cluster = np.array([coord_ids[i]])
+      cluster_dataset_ids = np.array([d_id])
+      xis = np.array([X[i]])
+
+      for j in range(len(self.datasets)-1):
+        # select only those rows that don't correspond to a dataset already
+        # present in current cluster
+        sel = np.where(dataset_ids != d_id)
+        X = X[sel]
+        dataset_ids = dataset_ids[sel]
+        coord_ids = coord_ids[sel]
+
+        assert len(X) > 0
+
+        # Find nearest neighbour in cosine-space to the current cluster centroid
+        nbrs = NearestNeighbors(
+          n_neighbors=min(1, len(X)), algorithm='brute', metric='cosine').fit(X)
+        distances, indices = nbrs.kneighbors([xis.mean(axis=0)])
+        k = indices[0][0]
+        d_id = dataset_ids[k]
+        cluster = np.append(cluster, coord_ids[k])
+        cluster_dataset_ids = np.append(cluster_dataset_ids, d_id)
+        xis = np.append(xis, [X[k]], axis=0)
+
+      # label this cluster
+      self.cluster_labels.set_selected(flex.size_t(cluster.tolist()), cluster_id)
+      cluster_id += 1
+
+    cluster_centroids = []
+    X = self.coords.as_numpy_array()
+    for i in set(self.cluster_labels):
+      sel = self.cluster_labels == i
+      cluster_centroids.append(
+        X[(self.cluster_labels == i).iselection().as_numpy_array()].mean(axis=0))
+
+    # hierarchical clustering of cluster centroids, using cosine metric
+    dist_mat = ssd.pdist(cluster_centroids, metric='cosine')
+    linkage_matrix = hierarchy.linkage(dist_mat, method='average')
+
+    # compare valid equal-sized clustering using silhouette scores
+    # https://en.wikipedia.org/wiki/Silhouette_(clustering)
+    # http://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_silhouette_analysis.html
+    distances = linkage_matrix[::, 2]
+    distances = np.insert(distances, 0, 0)
+    silhouette_scores = flex.double()
+    thresholds = flex.double()
+    for threshold in distances:
+      cluster_labels = self.cluster_labels.deep_copy()
+      labels = hierarchy.fcluster(
+        linkage_matrix, threshold+eps, criterion='distance').tolist()
+      counts = [labels.count(l) for l in set(labels)]
+      if len(set(counts)) > 1:
+        # only equal-sized clusters are valid
+        continue
+
+      n_clusters = len(set(labels))
+      if n_clusters == 1: continue
+      for i in range(len(labels)):
+        cluster_labels.set_selected(self.cluster_labels == i, int(labels[i]-1))
+      silhouette_avg = metrics.silhouette_score(
+        X, cluster_labels.as_numpy_array(), metric='cosine')
+      # Compute the silhouette scores for each sample
+      sample_silhouette_values = metrics.silhouette_samples(
+        X, cluster_labels.as_numpy_array(), metric='cosine')
+      silhouette_avg = sample_silhouette_values.mean()
+      silhouette_scores.append(silhouette_avg)
+      thresholds.append(threshold)
+
+      if self.params.save_plot:
+        plot_silhouette(
+          sample_silhouette_values, cluster_labels.as_numpy_array(),
+          file_name='silhouette_%i.png' % n_clusters)
+
+    logger.info('Silhouette scores: %s' %(list(silhouette_scores)))
+
+    idx = flex.max_index(silhouette_scores)
+    if silhouette_scores[idx] < self.params.cluster.seed.min_silhouette_score:
+      # assume single cluster
+      self.cluster_labels = flex.int(self.cluster_labels.size(), 0)
+    else:
+      threshold = thresholds[idx] + eps
+      labels = hierarchy.fcluster(
+        linkage_matrix, threshold, criterion='distance')
+      cluster_labels = flex.double(self.cluster_labels.size(), -1)
+      for i in range(len(labels)):
+        cluster_labels.set_selected(self.cluster_labels == i, labels[i]-1)
+      self.cluster_labels = cluster_labels
+
+    if self.params.save_plot:
+      plot_matrix(
+        1 - ssd.squareform(dist_mat), linkage_matrix,
+        'seed_clustering_cos_angle_matrix.png',
+        color_threshold=threshold)
+      plot_dendrogram(
+        linkage_matrix, 'seed_clustering_cos_angle_dendrogram.png',
+        color_threshold=threshold)
+
+    return self.cluster_labels
 
   def plot(self):
     self.target.plot_rij_matrix(plot_name='rij.png')
@@ -588,18 +727,17 @@ def plot_angles(coords, labels=None, show=False, plot_name=None):
   plt.close()
 
 
-def plot_matrix(correlation_matrix, linkage_matrix, file_name, labels=None):
+def plot_matrix(correlation_matrix, linkage_matrix, file_name, labels=None,
+                color_threshold=0.05):
   from matplotlib import pyplot as plt
   from scipy.cluster import hierarchy
-
-  ind = hierarchy.fcluster(linkage_matrix, t=0.05, criterion='distance')
 
   # Compute and plot dendrogram.
   fig = plt.figure(dpi=200, figsize=(16,12))
   axdendro = fig.add_axes([0.09,0.1,0.2,0.8])
   Y = linkage_matrix
   Z = hierarchy.dendrogram(Y,
-                           color_threshold=0.05,
+                           color_threshold=color_threshold,
                            orientation='right')
   axdendro.set_xticks([])
   axdendro.set_yticks([])
@@ -627,17 +765,63 @@ def plot_matrix(correlation_matrix, linkage_matrix, file_name, labels=None):
   fig.clear()
 
 
-def plot_dendrogram(linkage_matrix, file_name, labels=None):
+def plot_dendrogram(linkage_matrix, file_name, labels=None,
+                    color_threshold=0.05):
   from matplotlib import pyplot as plt
+
   fig = plt.figure(dpi=200, figsize=(16,12))
 
   from scipy.cluster import hierarchy
   ddict = hierarchy.dendrogram(linkage_matrix,
-                               #truncate_mode='lastp',
-                               color_threshold=0.05,
+                               color_threshold=color_threshold,
                                labels=labels,
-                               #leaf_rotation=90,
                                show_leaf_counts=False)
   locs, labels = plt.xticks()
   plt.setp(labels, rotation=70)
   fig.savefig(file_name)
+
+
+def plot_silhouette(sample_silhouette_values, cluster_labels, file_name):
+  import numpy as np
+  from matplotlib import pyplot as plt
+  import matplotlib.cm as cm
+
+  fig = plt.figure()
+  ax1 = fig.gca()
+  y_lower = 10
+
+  n_clusters = len(set(cluster_labels))
+  silhouette_avg = sample_silhouette_values.mean()
+  for i in range(n_clusters):
+    # Aggregate the silhouette scores for samples belonging to
+    # cluster i, and sort them
+    ith_cluster_silhouette_values = \
+      sample_silhouette_values[cluster_labels == i]
+
+    ith_cluster_silhouette_values.sort()
+
+    size_cluster_i = ith_cluster_silhouette_values.shape[0]
+    y_upper = y_lower + size_cluster_i
+
+    color = cm.spectral(float(i) / n_clusters)
+    ax1.fill_betweenx(np.arange(y_lower, y_upper),
+                      0, ith_cluster_silhouette_values,
+                      facecolor=color, edgecolor=color, alpha=0.7)
+
+    # Label the silhouette plots with their cluster numbers at the middle
+    ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+
+    # Compute the new y_lower for next plot
+    y_lower = y_upper + 10  # 10 for the 0 samples
+
+  ax1.set_title("The silhouette plot for the various clusters.")
+  ax1.set_xlabel("The silhouette coefficient values")
+  ax1.set_ylabel("Cluster label")
+
+  # The vertical line for average silhouette score of all the values
+  ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
+
+  ax1.set_yticks([])  # Clear the yaxis labels / ticks
+  ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
+  fig.savefig(file_name)
+  plt.clf()
