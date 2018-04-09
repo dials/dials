655a54e0d4835809bb0d46de03cffe8a1de7fdd1
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-04-09 08:54:56 +0100

    Only need to evaluate package location fixtures once

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ from __future__ import absolute_import, division, print_function
 import os
 import pytest
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def dials_regression():
   '''Return the absolute path to the dials_regression module as a string.
      Skip the test if dials_regression is not installed.'''
@@ -18,7 +18,7 @@ def dials_regression():
     pytest.skip("dials_regression required for this test")
   return os.path.dirname(dr.__file__)
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def xia2_regression():
   '''Return the absolute path to the xia2_regression module as a string.
      Skip the test if dials_regression is not installed.'''
@@ -28,7 +28,7 @@ def xia2_regression():
     pytest.skip("xia2_regression required for this test")
   return os.path.dirname(xr.__file__)
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def xia2_regression_build():
   '''Return the absolute path to the xia2_regression directory within the build
      path as a string. Skip the test if xia2_regression is not installed.'''
