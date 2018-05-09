7f613c02dc71aaa0a3b2d7d37b7d5117add1cbe2
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-05-09 10:59:13 +0100

    Workaround for some libtbx path conversion feature
    which breaks these tests on Windows. They work fine if run by pytest.

--- test/test_cpp_components.py
+++ test/test_cpp_components.py
@@ -5,13 +5,15 @@ import os
 import procrunner
 import pytest
 
-@pytest.mark.parametrize('executable', [
+cpp_tests = [
     # Paths are under /build/
     "test/algorithms/spatial_indexing/tst_collision_detection",
     "test/algorithms/spatial_indexing/tst_octree",
     "test/algorithms/spatial_indexing/tst_quadtree",
     "test/algorithms/spot_prediction/tst_reeke_model",
-])
+]
+
+@pytest.mark.parametrize('executable', cpp_tests, ids=[p.replace('/', '-') for p in cpp_tests])
 def test_cpp_program(executable):
   full_path = os.path.join(os.environ["LIBTBX_BUILD"], 'dials', *(executable.split('/')))
   print(full_path)
