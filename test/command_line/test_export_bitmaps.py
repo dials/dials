d01261bb86b65a1a6ddd49d7a42abf2c4cf7025f
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-06-22 16:19:59 +0100

    remove test from DIALS release branch. Required file not in release version of dials_regression

--- test/command_line/test_export_bitmaps.py
+++ test/command_line/test_export_bitmaps.py
@@ -20,12 +20,3 @@ def test_export_bitmaps(dials_regression, tmpdir):
 
   for i in range(1, 8):
     assert os.path.exists('variance_000%i.png' %i)
-
-def test_still_image(dials_regression, tmpdir):
-  tmpdir.chdir()
-  data_dir = os.path.join(dials_regression, 'image_examples/DLS_I24_stills')
-
-  cmd = 'dials.export_bitmaps %s/still_0001.cbf' %data_dir
-  result = easy_run.fully_buffered(cmd).raise_if_errors()
-
-  assert os.path.exists('image0001.png')
