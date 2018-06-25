cf3a6a98297835f0f5a42b67fdc68271d2493e52
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-06-25 09:56:57 +0100

    split up tests
    and resolve SCI-7504

--- test/command_line/test_export_bitmaps.py
+++ test/command_line/test_export_bitmaps.py
@@ -4,7 +4,7 @@ import os
 import pytest
 from libtbx import easy_run
 
-def test_export_bitmaps(dials_regression, tmpdir):
+def test_export_single_bitmap(dials_regression, tmpdir):
   tmpdir.chdir()
   data_dir = os.path.join(dials_regression, 'centroid_test_data')
 
@@ -13,6 +13,9 @@ def test_export_bitmaps(dials_regression, tmpdir):
 
   assert os.path.exists('image0001.png')
 
+def test_export_multiple_bitmaps(dials_regression, tmpdir):
+  tmpdir.chdir()
+  data_dir = os.path.join(dials_regression, 'centroid_test_data')
   cmd = ' '.join([
     'dials.export_bitmaps', '%s/datablock.json' %data_dir, 'prefix=variance_',
     'binning=2', 'display=variance', 'colour_scheme=inverse_greyscale',
@@ -22,29 +25,41 @@ def test_export_bitmaps(dials_regression, tmpdir):
   for i in range(1, 8):
     assert os.path.exists('variance_000%i.png' %i)
 
+def test_export_bitmap_with_prefix_and_no_padding(dials_regression, tmpdir):
+  tmpdir.chdir()
+  data_dir = os.path.join(dials_regression, 'centroid_test_data')
   cmd = 'dials.export_bitmaps %s/centroid_0001.cbf prefix=img_ padding=0' %data_dir
   result = easy_run.fully_buffered(cmd).raise_if_errors()
   assert os.path.exists('img_1.png')
 
+def test_export_bitmap_with_prefix_and_extra_padding(dials_regression, tmpdir):
+  tmpdir.chdir()
+  data_dir = os.path.join(dials_regression, 'centroid_test_data')
   cmd = 'dials.export_bitmaps %s/centroid_0001.cbf prefix=img_ padding=5' %data_dir
   result = easy_run.fully_buffered(cmd).raise_if_errors()
   assert os.path.exists('img_00001.png')
 
+def test_export_bitmap_with_specified_output_filename(dials_regression, tmpdir):
+  tmpdir.chdir()
+  data_dir = os.path.join(dials_regression, 'centroid_test_data')
   cmd = 'dials.export_bitmaps %s/centroid_0001.cbf output_file=kittens.png' %data_dir
   result = easy_run.fully_buffered(cmd).raise_if_errors()
   assert os.path.exists('kittens.png')
 
+def test_export_multiple_bitmaps_with_specified_output_filename_fails(dials_regression, tmpdir):
+  tmpdir.chdir()
+  data_dir = os.path.join(dials_regression, 'centroid_test_data')
   with pytest.raises(RuntimeError):
     # setting output filename not allowed with >1 image
     cmd = ' '.join([
       'dials.export_bitmaps', '%s/datablock.json' %data_dir, 'output_file=kittens.png'])
     result = easy_run.fully_buffered(cmd).raise_if_errors()
 
-def test_still_image(dials_regression, tmpdir):
+def test_export_still_image(dials_regression, tmpdir):
   tmpdir.chdir()
-  data_dir = os.path.join(dials_regression, 'image_examples/DLS_I24_stills')
+  image = os.path.join(dials_regression, 'image_examples', 'DLS_I24_stills', 'still_0001.cbf')
 
-  cmd = 'dials.export_bitmaps %s/still_0001.cbf' %data_dir
+  cmd = 'dials.export_bitmaps %s' % image
   result = easy_run.fully_buffered(cmd).raise_if_errors()
 
   assert os.path.exists('image0001.png')
