fa70b0d4f237d44e5898b47b89a2a53173bce79c
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-07-10 15:56:07 +0100

    For DIALS commands extend easy_mp error handling
    When easy_mp parallelization is used for DIALS commands, extend the
    existing error handling to print out the subprocess stack trace to
    aid in debugging.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,19 @@
 from __future__ import absolute_import, division, print_function
 
 import logging
-
 logging.getLogger('dials').addHandler(logging.NullHandler())
+
+# Intercept easy_mp exceptions to extract stack traces before they are lost at
+# the libtbx process boundary/the easy_mp API. In the case of a subprocess
+# crash we print the subprocess stack trace, which will be most useful for
+# debugging parallelized sections of DIALS code.
+import libtbx.scheduling.stacktrace as _lss
+def _stacktrace_tracer(error, trace, intercepted_call=_lss.set_last_exception):
+  '''Intercepts and prints ephemeral stacktraces.'''
+  if error and traces:
+    print("\n\neasy_mp crash detected; subprocess trace: ----\n%s%s\n%s\n\n" \
+          % (''.join(trace), error, '-'*46))
+  return intercepted_call(error, trace)
+if _lss.set_last_exception.__doc__ != _stacktrace_tracer.__doc__:
+  # ensure function is only redirected once
+  _lss.set_last_exception = _stacktrace_tracer
