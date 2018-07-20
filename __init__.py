da591892d0af7b1e1473f4da65db71a963b23110
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-07-20 14:50:05 +0100

    fix typo

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ logging.getLogger('dials').addHandler(logging.NullHandler())
 import libtbx.scheduling.stacktrace as _lss
 def _stacktrace_tracer(error, trace, intercepted_call=_lss.set_last_exception):
   '''Intercepts and prints ephemeral stacktraces.'''
-  if error and traces:
+  if error and trace:
     print("\n\neasy_mp crash detected; subprocess trace: ----\n%s%s\n%s\n\n" \
           % (''.join(trace), error, '-'*46))
   return intercepted_call(error, trace)
