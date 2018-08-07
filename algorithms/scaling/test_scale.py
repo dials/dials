88895ea3a66384d109dd8efc9dade4e453aea9bb
James Beilsten-Edmands <jbeilstenedmands@gmail.com>
2018-08-07 12:40:20 +0100

    Update dataset tests to inspect the results, checking Rpim and CC1/2

--- algorithms/scaling/test_scale.py
+++ algorithms/scaling/test_scale.py
@@ -40,6 +40,22 @@ class run_one_scaling(object):
       print(command)
       _ = easy_run.fully_buffered(command=command).raise_if_errors()
 
+def get_merging_stats(scaled_unmerged_mtz, anomalous=False, n_bins=20,
+                  use_internal_variance=False, eliminate_sys_absent=False,
+                  data_labels=None):
+  import iotbx.merging_statistics
+  i_obs = iotbx.merging_statistics.select_data(
+    scaled_unmerged_mtz, data_labels=data_labels)
+  i_obs = i_obs.customized_copy(anomalous_flag=False, info=i_obs.info())
+  result = iotbx.merging_statistics.dataset_statistics(
+    i_obs=i_obs,
+    n_bins=n_bins,
+    anomalous=anomalous,
+    use_internal_variance=use_internal_variance,
+    eliminate_sys_absent=eliminate_sys_absent,
+  )
+  return result
+
 @pytest.mark.dataset_test
 def test_scale_physical(dials_regression, tmpdir):
   """Test standard scaling of one dataset."""
@@ -48,21 +64,37 @@ def test_scale_physical(dials_regression, tmpdir):
   pickle_path = os.path.join(data_dir, "20_integrated.pickle")
   sweep_path = os.path.join(data_dir, "20_integrated_experiments.json")
   extra_args = ["model=physical", "merged_mtz=merged.mtz",
-    "unmerged_mtz=unmerged.mtz", "use_free_set=1"]
+    "unmerged_mtz=unmerged.mtz", "use_free_set=1", "outlier_rejection=simple"]
 
   with tmpdir.as_cwd():
     _ = run_one_scaling([pickle_path], [sweep_path], extra_args)
     assert os.path.exists("unmerged.mtz")
     assert os.path.exists("merged.mtz")
 
+    # Now inspect output, check it hasn't changed drastically, or if so verify
+    # that the new behaviour is more correct and update test accordingly.
+    result = get_merging_stats("unmerged.mtz")
+    assert result.overall.r_pim < 0.024 #at 07/08/18, value was 0.0234517
+    assert result.overall.cc_one_half > 0.9955 # at 07/08/18, value was 0.99597
+    assert result.overall.n_obs > 2300 # at 07/08/18, was 2309
+
   # run again with the concurrent scaling option turned off and the 'standard'
   # outlier rejection
-  extra_args += ["concurrent=False", "outlier_rejection=standard"]
+  extra_args = ["model=physical", "merged_mtz=merged.mtz",
+    "unmerged_mtz=unmerged.mtz", "use_free_set=1", "outlier_rejection=standard",
+    "concurrent=False"]
   with tmpdir.as_cwd():
     _ = run_one_scaling([pickle_path], [sweep_path], extra_args)
     assert os.path.exists("scale_model.png")
     assert os.path.exists("absorption_surface.png")
     assert os.path.exists("outliers.png")
+
+    # Now inspect output, check it hasn't changed drastically, or if so verify
+    # that the new behaviour is more correct and update test accordingly.
+    result = get_merging_stats("unmerged.mtz")
+    assert result.overall.r_pim < 0.035 #at 07/08/18, value was 0.034104
+    assert result.overall.cc_one_half > 0.9935 # at 07/08/18, value was 0.99388
+    assert result.overall.n_obs > 2310 # at 07/08/18, was 2319
 
 @pytest.mark.dataset_test
 def test_scale_optimise_errors(dials_regression, tmpdir):
@@ -99,7 +131,7 @@ def test_multi_scale(dials_regression, tmpdir):
   sweep_path_1 = os.path.join(data_dir, "20_integrated_experiments.json")
   pickle_path_2 = os.path.join(data_dir, "25_integrated.pickle")
   sweep_path_2 = os.path.join(data_dir, "25_integrated_experiments.json")
-  extra_args = []
+  extra_args = ["unmerged_mtz=unmerged.mtz"]
 
   with tmpdir.as_cwd():
     _ = run_one_scaling([pickle_path_1, pickle_path_2],
@@ -111,10 +143,26 @@ def test_multi_scale(dials_regression, tmpdir):
     assert os.path.exists("outliers_1.png")
     assert os.path.exists("outliers_2.png")
 
+    # Now inspect output, check it hasn't changed drastically, or if so verify
+    # that the new behaviour is more correct and update test accordingly.
+    result = get_merging_stats("unmerged.mtz")
+    expected_nobs = 5460
+    assert abs(result.overall.n_obs - expected_nobs) < 10
+    assert result.overall.r_pim < 0.022 #at 07/08/18, value was 0.021837
+    assert result.overall.cc_one_half > 0.9975 # at 07/08/18, value was 0.99810
+
     #run again, optimising errors, and continuing from where last run left off.
-    extra_args = ["optimise_errors=True"]
+    extra_args = ["optimise_errors=True", "unmerged_mtz=unmerged.mtz"]
     _ = run_one_scaling(["scaled.pickle"], ["scaled_experiments.json"],
       extra_args)
+    # Now inspect output, check it hasn't changed drastically, or if so verify
+    # that the new behaviour is more correct and update test accordingly.
+    # Note: error optimisation currently appears to give worse results here!
+    result = get_merging_stats("unmerged.mtz")
+    expected_nobs = 5459
+    assert abs(result.overall.n_obs - expected_nobs) < 10
+    assert result.overall.r_pim < 0.023 #at 07/08/18, value was 0.022722
+    assert result.overall.cc_one_half > 0.9965 # at 07/08/18, value was 0.996925
 
 @pytest.mark.dataset_test
 def test_targeted_scaling(dials_regression, tmpdir):
