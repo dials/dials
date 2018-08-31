add2749d776999a88e4b08baa995079127557ef8
James Beilsten-Edmands <jbeilstenedmands@gmail.com>
2018-08-31 14:22:29 +0100

    Update dials.show test to not test exact string output

--- test/command_line/test_show.py
+++ test/command_line/test_show.py
@@ -166,45 +166,18 @@ def test_dials_show_reflection_table(dials_regression):
   result = procrunner.run(["dials.show", path], environment_override={'DIALS_NOBANNER': '1'})
   assert not result['exitcode'] and not result['stderr']
   output = list(filter(None, (s.rstrip() for s in result['stdout'].split('\n'))))
-  assert "\n".join(output[4:]) == """
-Reflection list contains 2269 reflections
----------------------------------------------------------------------------------------------------------------------------------------------
-| Column                     | min                                | max                                | mean                               |
----------------------------------------------------------------------------------------------------------------------------------------------
-| background.mean            | 0.0                                | 3.6                                | 0.5                                |
-| background.sum.value       | 0.0                                | 925.1                              | 226.1                              |
-| background.sum.variance    | 0.0                                | 1143.6                             | 265.7                              |
-| d                          | 0.00                               | 13.68                              | 1.50                               |
-| dqe                        | 0.000                              | 0.904                              | 0.709                              |
-| flags                      | 769                                | 2228260                            | 502517                             |
-| id                         | -1                                 | 0                                  | 0                                  |
-| imageset_id                | 0                                  | 0                                  | 0                                  |
-| intensity.prf.value        | -11.3                              | 40839.8                            | 426.6                              |
-| intensity.prf.variance     | -1.0                               | 41045.5                            | 518.2                              |
-| intensity.sum.value        | -52.4                              | 41476.6                            | 547.4                              |
-| intensity.sum.variance     | 0.0                                | 42223.9                            | 816.1                              |
-| lp                         | 0.000                              | 0.920                              | 0.486                              |
-| miller_index               | -31, -36, -28                      | 6, 20, 32                          | -7, -4, 1                          |
-| num_pixels.background      | 0                                  | 15219                              | 3195                               |
-| num_pixels.background_used | 0                                  | 15219                              | 3195                               |
-| num_pixels.foreground      | 0                                  | 1809                               | 484                                |
-| num_pixels.valid           | 0                                  | 17028                              | 3680                               |
-| panel                      | 0                                  | 0                                  | 0                                  |
-| partial_id                 | 0                                  | 1990                               | 873                                |
-| partiality                 | 0.0000                             | 0.9998                             | 0.7450                             |
-| profile.correlation        | -0.235                             | 0.912                              | 0.199                              |
-| profile.rmsd               | 0.000                              | 0.228                              | 0.101                              |
-| rlp                        | -0.6434, -0.5749, 0.0000           | 0.5587, 0.6052, 0.2326             | -0.0042, 0.0034, 0.0121            |
-| s1                         | -0.7430, -0.7560, -1.0209          | 0.7523, 0.7665, -0.5510            | -0.0042, 0.0055, -0.8036           |
-| shoebox                    |                                    |                                    |                                    |
-|   summed I                 | 0.0                                | 10514.0                            | 73.5                               |
-|   N pix                    | 0.0                                | 378.0                              | 2.4                                |
-|   N valid foreground pix   | 0.0                                | 87.0                               | 1.0                                |
-| xyzcal.mm                  | 0.00, 0.00, -0.04                  | 422.90, 434.48, 0.08               | 204.72, 210.72, 0.01               |
-| xyzcal.px                  | -0.23, 0.00, -11.21                | 2459.34, 2526.64, 23.88            | 1190.23, 1225.10, 4.18             |
-| xyzobs.mm.value            | 0.61, 0.79, 0.00                   | 422.67, 433.87, 0.03               | 212.02, 218.32, 0.02               |
-| xyzobs.mm.variance         | 0.0000e+00, 0.0000e+00, 0.0000e+00 | 7.9419e-02, 1.0688e-02, 1.5462e-05 | 3.4148e-03, 3.3928e-03, 1.2491e-06 |
-| xyzobs.px.value            | 2.94, 3.96, 0.50                   | 2458.01, 2523.09, 8.50             | 1232.67, 1269.32, 4.46             |
-| xyzobs.px.variance         | 0.0000, 0.0000, 0.0000             | 2.6845, 0.3613, 1.2690             | 0.1154, 0.1147, 0.1025             |
-| zeta                       | -1.000                             | 1.000                              | -0.003                             |
----------------------------------------------------------------------------------------------------------------------------------------------""".strip()
+  assert output[4] == 'Reflection list contains 2269 reflections'
+  headers = ['Column', 'min', 'max', 'mean']
+  for header in headers:
+    assert header in output[6]
+  row_names = ['background.mean', 'background.sum.value',
+    'background.sum.variance', 'd', 'dqe', 'flags', 'id', 'imageset_id',
+    'intensity.prf.value', 'intensity.prf.variance', 'intensity.sum.value',
+    'intensity.sum.variance', 'lp', 'miller_index', 'num_pixels.background',
+    'num_pixels.background_used', 'num_pixels.foreground', 'num_pixels.valid',
+    'panel', 'partial_id', 'partiality', 'profile.correlation', 'profile.rmsd',
+    'rlp', 's1', 'shoebox', 'summed I', 'N pix', 'N valid foreground pix',
+    'xyzcal.mm', 'xyzcal.px', 'xyzobs.mm.value', 'xyzobs.mm.variance',
+    'xyzobs.px.value', 'xyzobs.px.variance', 'zeta']
+  for (name, out) in zip(row_names, output[8:-1]):
+    assert name in out
