e55b5f5ed089aaca97ab54d0af3c43bf302bd49f
James Parkhurst <james.parkhurst@diamond.ac.uk>
2018-09-06 13:10:09 +0100

    Added pack and convert for tiny<T,N> and c_grid<N> to/from msgpack array rather than binary type

--- test/test_phil.py
+++ test/test_phil.py
@@ -6,43 +6,45 @@ import dials.util.phil
 import mock
 
 # Modules the phil parser uses, that we want to mock
-import dials.array_family.flex
+from dials.array_family import flex
 
 @mock.patch("os.path.exists", mock.Mock(return_value=True))
-@mock.patch("dials.array_family.flex")
 @mock.patch("dxtbx.model.experiment_list.ExperimentListFactory")
 @mock.patch("dxtbx.datablock.DataBlockFactory")
-def test(DataBlockFactory, ExperimentListFactory, flex):
+def test(DataBlockFactory, ExperimentListFactory, dials_regression):
   # Only use these filenames for verification
   path = "centroid_test_data"
-  datablock_path = os.path.join(path, "datablock.json")
-  experiments_path = os.path.join(path, "experiments.json")
-  reflections_path = os.path.join(path, "integrated.pickle")
-
+  datablock_path = os.path.join(dials_regression, path, "datablock.json")
+  experiments_path = os.path.join(dials_regression, path, "experiments.json")
+  reflections_path1 = os.path.join(dials_regression, path, "integrated.pickle")
+  reflections_path2 = os.path.join(dials_regression, path, "integrated.mpack")
 
   phil_scope = dials.util.phil.parse('''
     input {
-      reflections = %s
+      reflections1 = %s
+        .type = reflection_table
+      reflections2 = %s
         .type = reflection_table
       datablock = %s
         .type = datablock
       experiments = %s
         .type = experiment_list
     }
-  ''' % (reflections_path,
+  ''' % (reflections_path1,
+         reflections_path2,
          datablock_path,
          experiments_path))
 
   params = phil_scope.extract()
+
   # Check the right filenames were parsed
-  assert(params.input.reflections.filename == reflections_path)
+  assert(params.input.reflections1.filename == reflections_path1)
+  assert(params.input.reflections2.filename == reflections_path2)
   assert(params.input.datablock.filename == datablock_path)
   assert(params.input.experiments.filename == experiments_path)
   # Check that we got the expected objects back
-  assert isinstance(params.input.reflections.data, mock.Mock)
   assert isinstance(params.input.datablock.data, mock.Mock)
   assert isinstance(params.input.experiments.data, mock.Mock)
   # Check we had the correct calls made
-  flex.reflection_table.from_pickle.assert_called_once_with(reflections_path)
   assert DataBlockFactory.from_json_file.call_args[0] == (datablock_path,)
   assert ExperimentListFactory.from_json_file.call_args[0] == (experiments_path,)
