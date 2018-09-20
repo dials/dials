a94b5d6b16cc5053d351ef62b27a287360a05cff
James Parkhurst <james.parkhurst@diamond.ac.uk>
2018-09-20 15:42:29 +0100

    Changed recursion to iteration

--- algorithms/scaling/scaling_utilities.py
+++ algorithms/scaling/scaling_utilities.py
@@ -90,11 +90,9 @@ def get_next_unique_id(unique_id, used_ids):
   where unique_id is an integer. Returns the input unique id if it is not in
   the used_ids list, else it increments the unique_id by one until the value is
   not found in the list and then returns that."""
-  if not str(unique_id) in used_ids:
-    return unique_id
-  else:
+  while str(unique_id) in used_ids:
     unique_id += 1
-    return get_next_unique_id(unique_id, used_ids)
+  return unique_id
 
 def assign_unique_identifiers(experiments, reflections):
   """Read in an experiment list and a list of reflection tables containing
