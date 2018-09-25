32f868a2ed16e7252c27ac25122597be2023354d
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-09-25 12:33:29 +0100

    Remove further references to xia2_regression

--- conftest.py
+++ conftest.py
@@ -35,40 +35,6 @@ def dials_regression():
   return os.path.dirname(dr.__file__)
 
 @pytest.fixture(scope="session")
-def xia2_regression():
-  '''Return the absolute path to the xia2_regression module as a string.
-     Skip the test if xia2_regression is not installed.'''
-  try:
-    import xia2_regression as xr
-  except ImportError:
-    pytest.skip("xia2_regression required for this test")
-  return os.path.dirname(xr.__file__)
-
-@pytest.fixture(scope="session")
-def xia2_regression_build():
-  '''Return the absolute path to the xia2_regression directory within the build
-     path as a string. Skip the test if xia2_regression is not installed.'''
-  try:
-    import xia2_regression as xr
-  except ImportError:
-    pytest.skip("xia2_regression required for this test")
-  try:
-    dls_common_copy = '/dls/science/groups/scisoft/DIALS/repositories/current/xia2_regression_data'
-    if os.path.exists(dls_common_copy):
-      return dls_common_copy
-  except Exception:
-    pass
-  try:
-    x2rpath = os.path.join(os.environ.get('LIBTBX_BUILD'), 'xia2_regression')
-  except AttributeError:
-    x2rpath = ''
-  if not os.path.exists(x2rpath):
-    pytest.skip("xia2_regression required for this test")
-  if 'test_data' not in os.listdir(x2rpath):
-    pytest.skip("xia2_regression files need to be downloaded for this test. Run xia2_regression.fetch_test_data")
-  return x2rpath
-
-@pytest.fixture(scope="session")
 def regression_data():
   '''Return the location of a regression data set as py.path object.
      Skip the test if the data are not present.
@@ -87,7 +53,7 @@ def regression_data():
   df = dials.util.regression_data.DataFetcher(target_dir, read_only=True)
   def skip_test_if_lookup_failed(result):
     if not result:
-      pytest.skip('Regression data is required to run this test. Run xia2.fetch_test_data')
+      pytest.skip('Regression data is required to run this test. Run dials.fetch_test_data')
     return result
   setattr(df, 'result_filter', skip_test_if_lookup_failed)
   return df
