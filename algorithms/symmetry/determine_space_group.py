02d1eaba3ec06b17f695377f845b08487e2e6676
rjgildea <richard.gildea@diamond.ac.uk>
2018-12-14 10:44:57 +0000

    bug fix to the bug fix in f03dc8288903be3aeba5a43d8ff3e06aac24921a

--- algorithms/symmetry/determine_space_group.py
+++ algorithms/symmetry/determine_space_group.py
@@ -295,8 +295,7 @@ class ScoreSymmetryElement(object):
         reindexed_intensities, assert_is_similar_symmetry=False)
       tmp_sg = sgtbx.space_group().expand_smx(self.sym_op) \
         .info().as_reference_setting().group()
-      sel = tmp_sg.is_sys_absent(x.indices())
-      sel = flex.bool(x.size(), True)
+      sel = ~(tmp_sg.is_sys_absent(x.indices()))
       sel &= tmp_sg.epsilon(x.indices()) == 1
       x = x.select(sel)
       y = y.select(sel)
