dials/dials

commit 86f27a4f36843e566d326f4fa9e5f8708761d7f3

Synch 2to3 pickle/xrange changes from 04edf365642b1fa84b0c63d2f2f9bea22ef96af8

util/image_viewer/slip_viewer/pyslip.py
@@ -22,6 +22,7 @@ difficulty for most uses is to generate the map tiles.
 [1] http://wiki.openstreetmap.org/index.php/Slippy_Map
 """
 from __future__ import division
+from six.moves import range
 
 
 # Copyright (c) 2010, Ross Wilson (rzzzwilson@gmail.com). All rights reserved.
@@ -51,10 +52,7 @@ from __future__ import division
 import os
 import sys
 import glob
-try:
-    import cPickle as pickle
-except ImportError:
-    import pickle
+from six.moves import cPickle as pickle
 import wx
 from scitbx.matrix import col
 import math
@@ -925,7 +923,7 @@ class PySlip(_BufferedCanvas):
         default_offset_y = kwargs.get('offset_y', self.DefaultPolygonOffsetY)
 
         draw_data = []
-        for x in xrange(0,len(data),5):
+        for x in range(0,len(data),5):
           # Calculate ellipse center, major and minor axes.
           side1=col(( (data[x][0]+data[x+1][0])/2., (data[x][1]+data[x+1][1])/2.))
           side2=col(( (data[x+1][0]+data[x+2][0])/2., (data[x+1][1]+data[x+2][1])/2.))

util/image_viewer/rstbx_frame.py
@@ -1,4 +1,5 @@
 from __future__ import division
+from six.moves import range
 
 import rstbx.viewer.display
 import wxtbx.plots
@@ -204,7 +205,7 @@ class XrayFrame(wx.Frame):
     """
 
     key = self.get_key(file_name_or_data)
-    for i in xrange(self.image_chooser.GetCount()):
+    for i in range(self.image_chooser.GetCount()):
       if (key == self.image_chooser.GetClientData(i)):
         return i
     if (self.image_chooser.GetCount() >= self.CHOOSER_SIZE):

util/image_viewer/slip_viewer/calibration_frame.py
@@ -4,6 +4,7 @@
 
 from __future__ import division
 from __future__ import print_function
+from six.moves import range
 
 import wx
 from scitbx.matrix import col
@@ -45,7 +46,7 @@ class SBSettingsPanel(wx.Panel):
     img = self.GetParent().GetParent().pyslip.tiles.raw_image
     d = img.get_detector()
     self._quad_spinners = []
-    for serial in xrange(4):
+    for serial in range(4):
       fast, slow = d.hierarchy()[serial].get_origin()[0:2]
       name_quadrant = ["Q0", "Q1", "Q2", "Q3"][serial]
       box = wx.BoxSizer(wx.HORIZONTAL)
@@ -140,7 +141,7 @@ class SBSettingsPanel(wx.Panel):
 
         # Update the controls, remember to reset the default values
         # for the spinners.
-        for serial in xrange(4):
+        for serial in range(4):
           fast, slow = img.get_panel_fast_slow(serial)
           name_quadrant = ["Q0", "Q1", "Q2", "Q3"][serial]
 

util/image_viewer/slip_viewer/frame.py
@@ -8,6 +8,7 @@
 from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
+from six.moves import range
 
 import os
 import wx
@@ -241,7 +242,7 @@ class XrayFrame(AppFrame,XFBaseClass):
       """
 
       key = self.get_key(file_name_or_data)
-      for i in xrange(self.image_chooser.GetCount()):
+      for i in range(self.image_chooser.GetCount()):
         if (key == str(self.image_chooser.GetClientData(i))):
           return i
       if (self.image_chooser.GetCount() >= self.CHOOSER_SIZE):

util/image_viewer/slip_viewer/ring_frame.py
@@ -22,7 +22,7 @@ class RingSettingsFrame(wx.MiniFrame):
 
 
 class RingSettingsPanel(wx.Panel):
-  def __init__ (self, *args, **kwds) :
+  def __init__ (self, *args, **kwds):
     # XXX Support several rings.  Plot radial distribution somewhere
     # (not here), but maybe distribution along ring.  Drop-down menu
     # for ring center, and button to reset to beam center.

util/image_viewer/slip_viewer/score_frame.py
@@ -4,6 +4,7 @@
 
 from __future__ import division
 from __future__ import print_function
+from six.moves import range
 
 import wx
 
@@ -88,7 +89,7 @@ class ScoreSettingsPanel(wx.Panel):
     self.Bind(wx.EVT_UPDATE_UI, self.OnUpdatePrevious, id=self._id_previous)
     self.Bind(wx.EVT_UPDATE_UI, self.OnUpdateText, id=self._id_text)
 
-    for i in xrange(self._root_frame.image_chooser.GetCount()):
+    for i in range(self._root_frame.image_chooser.GetCount()):
       _scores[self._root_frame.get_key(self._root_frame.image_chooser.GetClientData(i))] = None
 
   def OnNext(self, event):

util/image_viewer/slip_viewer/tile_generation.py
@@ -3,6 +3,7 @@
 # $Id$
 
 from __future__ import division
+from six.moves import range
 
 import math
 import wx
@@ -547,8 +548,8 @@ class _Tiles(object):
         for key, asic in self.raw_image._tiles.iteritems():
           key_count += 1
           focus = asic.focus()
-          for slow in xrange(0,focus[0],20):
-            for fast in xrange(0,focus[1],20):
+          for slow in range(0,focus[0],20):
+            for fast in range(0,focus[1],20):
               slowpic,fastpic = self.flex_image.tile_readout_to_picture(key_count,slow,fast)
               mr1,mr2 = self.picture_fast_slow_to_map_relative(fastpic,slowpic)
               pointdata.append((mr1,mr2,{"data":key}))
@@ -591,7 +592,7 @@ class _Tiles(object):
       text_data = []
       if hasattr(self.raw_image, 'get_tile_manager'):
         IT = self.raw_image.get_tile_manager(params).effective_tiling_as_flex_int()
-        for i in xrange(len(IT) // 4):
+        for i in range(len(IT) // 4):
           tile = IT[4*i:4*i+4]
           attributes = {'color': '#0000FFA0', 'width': 1, 'closed': False}
           box_data.append(

commit 15488a37ad44433966b9d8c121855c0ba0ac4d27

Fix for image viewer mask tool broken by 86f27a4f36843e566d326f4fa9e5f8708761d7f3
Change to importing range from six.moves meant that range now returns an xrange
instance, which is not sliceable:

>>> range(10)
[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
>>> range(10)[:]
[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
>>> from six.moves import range
>>> range(10)
xrange(10)
>>> range(10)[:]
Traceback (most recent call last):
  Python Shell, prompt 19, line 1
TypeError: sequence index must be integer, not 'slice'

util/image_viewer/slip_viewer/pyslip.py
@@ -1265,7 +1265,7 @@ class PySlip(_BufferedCanvas):
 
         # prepare the show_level value
         if show_levels is None:
-            show_levels = range(self.min_level, self.max_level + 1)[:]
+            show_levels = range(self.min_level, self.max_level + 1)
 
         # create layer, add unique ID to Z order list
         l = _Layer(id=id, painter=render, data=data, map_rel=map_rel,