39e3d89155fd16dd0cea801baad345e89b251485
Ben Williams <benjaminhwilliams@users.noreply.github.com>
2019-03-08 17:04:57 +0000

    Convert dials.generate_mask to functional interface
    Follows boilerplate agreed in [the wiki](https://github.com/dials/dials/wiki/Boilerplate-(or-the-standard-framework-of-a-DIALS-program)).
    Addresses part of #691.
    Details:
      - Adds encoding declaration.
      - Uses help string as module docstring.
      - Moves imports to top of file.
      - Sets up logger, incluldes logging-related parameters in PHIL scope, converts print calls to logging calls.
      - Breaks Script class into script function, containing functionality of the program, and run function for parsing command-line args and calling script.
      - Fleshes out docstrings, including type hints.
      - Removes Sorrys, raises built-in exceptions.
      - Adds return values to script.
      - Uses dials.util.show_mail_on_error() in place of halraiser.

--- command_line/generate_mask.py
+++ command_line/generate_mask.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-#
+# -*- coding: utf-8 -*-
+
 # generate_mask.py
 #
 #  Copyright (C) 2013 Diamond Light Source
@@ -9,11 +10,8 @@
 #  This code is distributed under the BSD license, a copy of which is
 #  included in the root directory of this package.
 
-from __future__ import absolute_import, division, print_function
-
-from iotbx.phil import parse
-
-help_message = """
+"""
+Mask images to remove unwanted pixels.
 
 This program is used to generate mask to specify which pixels should be
 considered "invalid" during spot finding and integration. It provides a few
@@ -32,108 +30,157 @@ Examples::
 
 """
 
+from __future__ import absolute_import, division, print_function
+
+import sys
+import logging
+import six.moves.cPickle as pickle
+
+import dials.util
+import dials.util.log
+from iotbx.phil import parse
+import libtbx.load_env
+from dials.util.options import OptionParser, flatten_experiments
+from dials.util.masking import MaskGenerator
+from dxtbx.format.image import ImageBool
+from dxtbx.model.experiment_list import ExperimentListDumper
+
+help_message = __doc__
+
+log = logging.getLogger('dials.generate_mask')
+
 phil_scope = parse(
     """
-  output {
-    mask = mask.pickle
-      .type = path
-      .help = "Name of output mask file"
-    experiments = None
-      .type = path
-      .help = "Save the modified experiments. (usually only modified with the"
-              "generated pixel mask)"
-  }
+    output {
+        mask = mask.pickle
+            .type = path
+            .help = "Name of output mask file."
+        experiments = None
+            .type = path
+            .help = "Name of output experiment list file.  If this is set, a copy of "
+                    "the experiments, modified with the generated pixel masks, " 
+                    "will be saved to this location."
+        log = 'dials.generate_masks.log'
+            .type = str
+            .help = "The log filename."
+    }
 
-  include scope dials.util.masking.phil_scope
-""",
+    include scope dials.util.masking.phil_scope
+    
+    verbosity = 1
+        .type = int(value_min=0)
+        .help = "The verbosity level."
+    """,
     process_includes=True,
 )
 
 
-class Script(object):
-    """ A class to encapsulate the script. """
+def script(experiments, params):
+    """
+    Generate a pixel mask for each image in an experiment.
 
-    def __init__(self):
-        """ Initialise the script. """
-        from dials.util.options import OptionParser
-        import libtbx.load_env
+    Use the masking parameters :param:`params` and an experiment in the experiment
+    list :param:`experiments` to define pixel masks for the associated imagesets.
+    The masks are generated using :mod:`dials.util.masking`.
 
-        # Create the parser
-        usage = "usage: %s [options] experiments.json" % libtbx.env.dispatcher_name
-        self.parser = OptionParser(
-            usage=usage, phil=phil_scope, epilog=help_message, read_experiments=True
-        )
+    The masks will be saved to disk at the location specified by
+    :attr:`params.output.mask`.  Optionally, if a path
+    :attr:`params.output.experiments` is set, a modified copy of :param:`experiments`
+    with the masks applied will be saved to that location.
 
-    def run(self):
-        """ Run the script. """
-        from dials.util.masking import MaskGenerator
-        from dials.util.options import flatten_experiments
-        from dials.util import Sorry
-        import six.moves.cPickle as pickle
-        from dials.util import log
-        from dxtbx.format.image import ImageBool
+    Args:
+        experiments (:type:`dxtbx_model_ext.ExperimentList`): An experiment list
+            containing only one :type:`dxtbx_model_ext.Experiment` object.
+        params (:type:`libtbx.phil.scope_extract`): Masking parameters, having the
+            structure defined in :data:`dials.util.masking.phil_scope`.
 
-        # Parse the command line arguments
-        params, options = self.parser.parse_args(show_diff_phil=True)
-        experiments = flatten_experiments(params.input.experiments)
+    Returns:
+        Tuple[:rtype:`scitbx_array_family_flex_ext.bool`]: A tuple containing the
+            generated pixel masks.
+        :rtype:`dxtbx_model_ext.ExperimentList` (optional): A copy of
+            :param:`experiments` with the masks applied.  Only returned if
+            :attr:`params.output.experiments` is set.
+    """
+    imagesets = experiments.imagesets()
+    if len(imagesets) > 1:
+        # Check beams (for resolution) and detectors are equivalent in each case
+        # otherwise the mask may not be appropriate across all imagesets
+        detectors = experiments.detectors()
+        beams = experiments.beams()
+        for d in detectors[1:]:
+            if not d.is_similar_to(detectors[0]):
+                raise ValueError(
+                    "Multiple imagesets are present, but their detector models differ."
+                )
+        for b in beams[1:]:
+            if not b.is_similar_to(beams[0]):
+                raise ValueError(
+                    "Multiple imagesets are present, but their beam models differ."
+                )
 
-        # Configure logging
-        log.config()
+    imageset = imagesets[0]
 
-        # Check number of args
-        if len(experiments) == 0:
-            self.parser.print_help()
-            return
+    # Generate the mask
+    generator = MaskGenerator(params)
+    mask = generator.generate(imageset)
 
-        if len(experiments) != 1:
-            raise Sorry("exactly 1 experiments must be specified")
-        imagesets = experiments.imagesets()
-        if len(imagesets) > 1:
-            # Check beams (for resolution) and detectors are equivalent in each case
-            # otherwise the mask may not be appropriate across all imagesets
-            detectors = experiments.detectors()
-            beams = experiments.beams()
-            for d in detectors[1:]:
-                if not d.is_similar_to(detectors[0]):
-                    raise Sorry(
-                        "multiple imagesets are present, but their detector"
-                        " models differ"
-                    )
-            for b in beams[1:]:
-                if not b.is_similar_to(beams[0]):
-                    raise Sorry(
-                        "multiple imagesets are present, but their beam"
-                        " models differ"
-                    )
+    # Save the mask to file
+    log.info("Writing mask to %s", params.output.mask)
+    with open(params.output.mask, "wb") as fh:
+        pickle.dump(mask, fh)
 
-        imageset = imagesets[0]
+    # Save the experiment list
+    if params.output.experiments:
+        for imageset in imagesets:
+            imageset.external_lookup.mask.data = ImageBool(mask)
+            imageset.external_lookup.mask.filename = params.output.mask
 
-        # Generate the mask
-        generator = MaskGenerator(params)
-        mask = generator.generate(imageset)
+        log.info("Saving experiments to %s", params.output.experiments)
+        dump = ExperimentListDumper(experiments)
+        dump.as_file(params.output.experiments)
 
-        # Save the mask to file
-        print("Writing mask to %s" % params.output.mask)
-        with open(params.output.mask, "wb") as fh:
-            pickle.dump(mask, fh)
+        return mask, experiments
+    else:
+        return mask
 
-        # Save the experiment list
-        if params.output.experiments is not None:
-            for imageset in imagesets:
-                imageset.external_lookup.mask.data = ImageBool(mask)
-                imageset.external_lookup.mask.filename = params.output.mask
-            from dxtbx.model.experiment_list import ExperimentListDumper
 
-            print("Saving experiments to {0}".format(params.output.experiments))
-            dump = ExperimentListDumper(experiments)
-            dump.as_file(params.output.experiments)
+def run(phil=phil_scope, args=None):
+    """
+    Parse command-line arguments, run the script.
+
+    Use the DIALS option parser to extract an experiment list and parameters.  Pass
+    these to :func:`script`.  If :param:`args` is None (default), the option parser
+    defaults to :data:`sys.argv[1:]`.
+
+    Args:
+        phil (:type:`libtbx.phil.scope`): PHIL scope for option parser.
+        args (:type:`list`): Arguments to parse.  Defaults to :data:`sys.argv[1:]`.
+    """
+    # Create the parser
+    usage = "usage: %s [options] experiments.json" % libtbx.env.dispatcher_name
+    parser = OptionParser(
+        usage=usage, phil=phil, epilog=help_message, read_experiments=True
+    )
+
+    # Parse the command line arguments
+    params, options = parser.parse_args(args=args, show_diff_phil=True)
+    experiments = flatten_experiments(params.input.experiments)
+
+    # Configure logging
+    dials.util.log.config(params.verbosity, info=params.output.log)
+
+    # Check number of args
+    if len(experiments) == 0:
+        parser.print_help()
+        sys.exit(1)
+
+    if len(experiments) != 1:
+        sys.exit("Exactly 1 experiment must be specified.")
+
+    # Run the script
+    script(experiments, params)
 
 
 if __name__ == "__main__":
-    from dials.util import halraiser
-
-    try:
-        script = Script()
-        script.run()
-    except Exception as e:
-        halraiser(e)
+    with dials.util.show_mail_on_error():
+        run()
