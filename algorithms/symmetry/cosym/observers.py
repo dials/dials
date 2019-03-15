83d1db0d2b116c0db47af05863951ae19809550b
rjgildea <richard.gildea@diamond.ac.uk>
2019-03-15 15:12:35 +0000

    cosym observers

---
+++ algorithms/symmetry/cosym/observers.py
@@ -0,0 +1,117 @@
+"""
+Observers for the cosym procedure.
+"""
+from collections import OrderedDict
+from dials.util.observer import Observer, singleton
+from dials.algorithms.symmetry.cosym.plots import plot_coords, plot_rij_histogram
+from dials.algorithms.clustering.observers import UnitCellAnalysisObserver
+from jinja2 import Environment, ChoiceLoader, PackageLoader
+
+
+def register_default_cosym_observers(script):
+    """Register the standard observers to the cosym script."""
+    script.cosym_analysis.register_observer(
+        event="analysed_symmetry", observer=SymmetryAnalysisObserver()
+    )
+    script.cosym_analysis.register_observer(
+        event="analysed_clusters", observer=CosymClusterAnalysisObserver()
+    )
+    script.register_observer(event="run_cosym", observer=UnitCellAnalysisObserver())
+    script.register_observer(
+        event="run_cosym", observer=CosymHTMLGenerator(), callback="make_html"
+    )
+
+
+@singleton
+class CosymHTMLGenerator(Observer):
+
+    """
+    Observer to make a html report
+    """
+
+    def make_html(self, cosym_script):
+        """Collect data from the individual observers and write the html."""
+        self.data.update(CosymClusterAnalysisObserver().make_plots())
+        self.data.update(UnitCellAnalysisObserver().make_plots())
+        self.data.update(SymmetryAnalysisObserver().make_tables())
+        filename = cosym_script.params.output.html
+        print("Writing html report to: %s" % filename)
+        loader = ChoiceLoader(
+            [
+                PackageLoader("dials", "templates"),
+                PackageLoader("dials", "static", encoding="utf-8"),
+            ]
+        )
+        env = Environment(loader=loader)
+        template = env.get_template("cosym_report.html")
+        html = template.render(
+            page_title="DIALS cosym report",
+            cosym_graphs=self.data["cosym_graphs"],
+            unit_cell_graphs=self.data["unit_cell_graphs"],
+            # symmetry_analysis=self.data["symmetry_analysis"],
+            symmetry_analysis=[
+                self.data["symmetry_analysis"]["sym_ops_table"],
+                self.data["symmetry_analysis"]["subgroups_table"],
+                self.data["symmetry_analysis"]["summary_table"],
+            ],
+        )
+        with open(filename, "wb") as f:
+            f.write(html.encode("ascii", "xmlcharrefreplace"))
+
+
+@singleton
+class CosymClusterAnalysisObserver(Observer):
+
+    """
+    Observer to record scaling model data and make model plots.
+    """
+
+    def update(self, cosym):
+        """Update the data in the observer."""
+        self.data["coordinates"] = cosym.coords
+        self.data["labels"] = cosym.cluster_labels
+        self.data["rij_matrix"] = cosym.target.rij_matrix
+
+    def make_plots(self):
+        """Generate scaling model component plot data."""
+        d = OrderedDict()
+        d.update(plot_rij_histogram(self.data["rij_matrix"]))
+        d.update(plot_coords(self.data["coordinates"], self.data["labels"]))
+        graphs = {"cosym_graphs": d}
+        return graphs
+
+
+@singleton
+class SymmetryAnalysisObserver(Observer):
+
+    """
+    Observer to record symmetry analysis data and make tables.
+    """
+
+    def update(self, cosym):
+        self.data["sym_ops_table"] = cosym._symmetry_analysis.sym_ops_table()
+        self.data["subgroups_table"] = cosym._symmetry_analysis.subgroups_table()
+        d = cosym._symmetry_analysis.as_dict()
+        from cctbx import sgtbx
+
+        best_subgroup = d["subgroup_scores"][0]
+        self.data["summary_table"] = (
+            (
+                "Best solution",
+                str(
+                    sgtbx.space_group(
+                        hall_symbol=best_subgroup["patterson_group"]
+                    ).info()
+                ),
+            ),
+            ("Unit cell", "%.3f %.3f %.3f %.1f %.1f %.1f" % best_subgroup["unit_cell"]),
+            ("Reindex operator", best_subgroup["cb_op"]),
+            ("Laue group probability", "%.3f" % best_subgroup["likelihood"]),
+            ("Laue group confidence", "%.3f" % best_subgroup["confidence"]),
+        )
+
+    def make_tables(self):
+        """Generate tables of overall and resolution-binned merging statistics."""
+        d = {"symmetry_analysis": self.data}
+
+        return d
---
+++ algorithms/symmetry/cosym/test_observers.py
@@ -0,0 +1,62 @@
+import mock
+
+from scitbx.array_family import flex
+from dials.algorithms.symmetry.cosym import observers
+
+
+def test_SymmetryAnalysisObserver():
+    # setup script
+    script = mock.Mock()
+    script._symmetry_analysis = mock.Mock()
+    script._symmetry_analysis.sym_ops_table = mock.Mock()
+    script._symmetry_analysis.subgroups_table = mock.Mock()
+    script._symmetry_analysis.as_dict = mock.Mock(
+        return_value={
+            "subgroup_scores": [
+                {
+                    "patterson_group": "-P 1",
+                    "unit_cell": (10, 10, 10, 90, 90, 90),
+                    "cb_op": "x,y,z",
+                    "likelihood": 0.9,
+                    "confidence": 0.9,
+                }
+            ]
+        }
+    )
+
+    # test the observer
+    observer = observers.SymmetryAnalysisObserver()
+    observer.update(script)
+    d = observer.make_tables()
+    assert "symmetry_analysis" in d
+
+
+def test_CosymClusterAnalysisObserver():
+    rij_matrix = flex.random_double(16)
+    rij_matrix.reshape(flex.grid(4, 4))
+    coords = flex.random_double(8)
+    coords.reshape(flex.grid(4, 2))
+
+    # setup script
+    script = mock.Mock()
+    script.target = mock.Mock()
+    script.target.rij_matrix = rij_matrix
+    script.coords = coords
+    script.cluster_labels = flex.int(4, 0)
+
+    # test the observer
+    observer = observers.CosymClusterAnalysisObserver()
+    observer.update(script)
+    d = observer.make_plots()
+    assert "cosym_graphs" in d
+
+
+def test_CosymHTMLGenerator():
+    pass
+    # script = mock.Mock()
+    # script.params.output.html = "test.html"
+
+    ## Test that CosymHTMLGenerator works if all data is empty.
+    # observer = observers.CosymHTMLGenerator()
+    # observer.make_html(script)
+    # assert os.path.exists("test.html")
