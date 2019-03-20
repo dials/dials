026ab8ab3f90c819424c1867936d9edbc78a6950
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-03-20 20:18:23 +0000

    Unmask test
    The first of these two tests was masked by both tests having the same
    name. It therefore was never run. It is currently broken.

--- test/command_line/test_rl_png.py
+++ test/command_line/test_rl_png.py
@@ -5,7 +5,7 @@ import os
 from libtbx import easy_run
 
 
-def test_rl_png_experiments(dials_regression, run_in_tmpdir):
+def test_rl_png_centroid_data(dials_regression, run_in_tmpdir):
     data_dir = os.path.join(dials_regression, "centroid_test_data")
     experiments_path = os.path.join(data_dir, "experiments.json")
     strong_pickle = os.path.join(data_dir, "strong.pickle")
@@ -24,7 +24,7 @@ def test_rl_png_experiments(dials_regression, run_in_tmpdir):
         assert os.path.exists("rl_%s.png" % s), s
 
 
-def test_rl_png_experiments(dials_regression, run_in_tmpdir):
+def test_rl_png_refinement_data(dials_regression, run_in_tmpdir):
     data_dir = os.path.join(dials_regression, "refinement_test_data", "i04_weak_data")
     experiments_path = os.path.join(data_dir, "experiments.json")
     indexed_pickle = os.path.join(data_dir, "indexed_strong.pickle")
