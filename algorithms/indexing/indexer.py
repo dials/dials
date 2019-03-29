d3a44c2e21165b5c046abf7a208efd4f9e900273
Richard Gildea <richard.gildea@diamond.ac.uk>
2019-03-29 16:50:39 +0000

    this code has already been moved elsewhere

--- algorithms/indexing/indexer.py
+++ algorithms/indexing/indexer.py
@@ -439,55 +439,6 @@ def filter_reflections_by_scan_range(reflections, scan_range):
     return reflections.select(reflections_in_scan_range)
 
 
-class vector_group(object):
-    def __init__(self):
-        self.vectors = []
-        self.lengths = []
-        self.volumes = []
-        self._mean = None
-
-    def append(self, vector, length, volume):
-        self.vectors.append(vector)
-        self.lengths.append(length)
-        self.volumes.append(volume)
-        self._mean = self.compute_mean()
-
-    def mean(self):
-        if self._mean is None:
-            self._mean = self.compute_mean()
-        return self._mean
-
-    def compute_mean(self):
-        sum_x = 0
-        sum_y = 0
-        sum_z = 0
-        for v in self.vectors:
-            sum_x += v.elems[0]
-            sum_y += v.elems[1]
-            sum_z += v.elems[2]
-        return matrix.col((sum_x, sum_y, sum_z)) / len(self.vectors)
-
-
-def is_approximate_integer_multiple(
-    vec_a, vec_b, relative_tolerance=0.2, angular_tolerance=5.0
-):
-    length_a = vec_a.length()
-    length_b = vec_b.length()
-    # assert length_b >= length_a
-    if length_a > length_b:
-        vec_a, vec_b = vec_b, vec_a
-        length_a, length_b = length_b, length_a
-    angle = vec_a.angle(vec_b, deg=True)
-    if angle < angular_tolerance or abs(180 - angle) < angular_tolerance:
-        n = length_b / length_a
-        if abs(round(n) - n) < relative_tolerance:
-            return True
-    return False
-
-
-deg_to_radians = math.pi / 180
-
-
 class indexer_base(object):
     def __init__(self, reflections, experiments, params=None):
         self.reflections = reflections
