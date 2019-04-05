53fe1f5f586279be892464615f9888713c0d7895
James Beilsten-Edmands <jbeilstenedmands@gmail.com>
2019-04-05 15:52:16 +0100

    Fix bug in calculation of scaling error-model RMSD for output.

--- algorithms/scaling/error_model/error_model_target.py
+++ algorithms/scaling/error_model/error_model_target.py
@@ -38,12 +38,13 @@ class ErrorModelTarget(object):
         """calculate unweighted RMSDs for the matches"""
         # cache rmsd calculation for achieved test
         R = self.calculate_residuals()
+        n = R.size()
         R.extend(
             flex.double(
                 [self.compute_restraints_functional_gradients_and_curvatures()[0]]
             )
         )
-        self._rmsds = [(flex.sum((R)) / self.error_model.Ih_table.size) ** 0.5]
+        self._rmsds = [(flex.sum((R)) / n) ** 0.5]
         return self._rmsds
 
     def achieved(self):
