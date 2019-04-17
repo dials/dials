a26482ca7b59e4b0577340a8764de74cd227f2c1
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-04-17 18:11:53 +0100

    lgtm: fix newly added alert

--- command_line/reindex.py
+++ command_line/reindex.py
@@ -16,7 +16,6 @@ from __future__ import absolute_import, division, print_function
 
 import os
 import copy
-from libtbx import easy_pickle
 import iotbx.phil
 from cctbx import sgtbx
 from dxtbx.model import Crystal
