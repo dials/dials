8c7a169950b41b2fa1ad979ac51d70fa82047953
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-04-18 07:58:18 +0100

    Make test show xfail reason and not use local data
    Test was introduced for #693 to improve handling of unexpected files.
    Ticket is still open.

--- test/util/test_options.py
+++ test/util/test_options.py
@@ -3,22 +3,15 @@ Tests for the functions in dials.util.options
 """
 from __future__ import absolute_import, division, print_function
 
-import os
 import pytest
 from mock import Mock
 from dials.util.options import flatten_reflections, flatten_experiments, OptionParser
 from dials.array_family import flex
 
 
-@pytest.mark.skipif(
-    not os.access("/dls/i04/data/2019/cm23004-1/20190109/Eiger", os.R_OK),
-    reason="Test images not available",
-)
-@pytest.mark.xfail
-def test_not_master_h5():
-    data_h5 = (
-        "/dls/i04/data/2019/cm23004-1/20190109/Eiger/gw/Thaum/Thau_4/Thau_4_1_000001.h5"
-    )
+@pytest.mark.xfail(reason="https://github.com/dials/dials/issues/693")
+def test_not_master_h5(dials_data):
+    data_h5 = dials_data("vmxi_thaumatin").join("image_15799_data_000001.h5").strpath
     parser = OptionParser(read_experiments=True, read_experiments_from_images=True)
     params, options = parser.parse_args([data_h5])
     experiments = flatten_experiments(params.input.experiments)
