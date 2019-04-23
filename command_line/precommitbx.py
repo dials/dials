b0f567645a4ef9aa1a89886a5bf1b1355dd8d536
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-04-23 22:04:21 +0100

    add new command: libtbx.precommit, #715
    TL;DR: just run 'libtbx.precommit install'
    
    Summary:
    libtbx.precommit installs a fully fledged pre-commit environment
    supporting the full range of possible pre-commit plugins, and
    enables them for all supported repositories. This means you can have
    code blackening, syntax validation, static code verification, etc.
    before you commit.
    
    Approach:
    This environment is separate from the cctbx python environment, and
    installed into build/precommitbx. This approach requires neither
    patching of pre-commit plugins nor of pre-commit itself, and the
    pre-commit yaml files are fully compatible with normal pre-commit
    installations.
    
    Background:
    The pre-commit dependencies are installed into said environment via the
    precommitbx python package, which lives inside dials/precommitbx.
    libtbx.precommit takes care of installing and maintaining the git hook.
    The pre-commit environment may not be relocatable, but can be easily
    restored by removing the build/precommitbx directory and re-running the
    installer.
    
    Replaces the previously available precommit installation command.
    Fixes #715

---
+++ command_line/precommitbx.py
@@ -0,0 +1,7 @@
+# LIBTBX_SET_DISPATCHER_NAME libtbx.precommit
+
+from __future__ import absolute_import, division, print_function
+
+import dials.precommitbx.installer
+
+dials.precommitbx.installer.main()
