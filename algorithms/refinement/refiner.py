dials/dials

commit a403602d809fdc1d1e52ee8805f7bea3e4617925

Do less when not asking for scan-varying errors.
The default is not to calculate scan-varying errors as it bloats the
.json file size and is expensive. Previously, some calculations were
being done even if the errors were not being written. Now avoid any
scan-varying error calculation unless requested. Speeds up the tail
end of scan-varying refinement jobs.

algorithms/refinement/refiner.py
@@ -1101,6 +1101,10 @@ class ScanVaryingRefiner(Refiner):
             if A_list is not None:
                 exp.crystal.set_A_at_scan_points(A_list)
 
+            # Return early if not calculating scan-varying errors
+            if not self._pred_param.set_scan_varying_errors:
+                return
+
             # get state covariance matrices the whole range of images. We select
             # the first element of this at each image because crystal scan-varying
             # parameterisations are not multi-state

algorithms/refinement/parameterisation/configure.py
@@ -98,6 +98,15 @@ phil_str = (
       .type = float(value_min = 0.)
       .expert_level = 1
 
+    set_scan_varying_errors = False
+      .type = bool
+      .help = "If scan-varying refinement is done, and if the estimated"
+              "covariance of the model states have been calculated by the"
+              "minimiser, choose whether to return this to the models or"
+              "not. The default is not to, in order to keep the file size"
+              "of the serialized model small. At the moment, this only has"
+              "an effect for crystal unit cell (B matrix) errors."
+
     debug_centroid_analysis = False
       .help = "Set True to write out a file containing the reflections used"
               "for centroid analysis for automatic setting of the  scan-varying"
@@ -163,14 +172,6 @@ phil_str = (
           .help = "Force a static parameterisation for the crystal unit cell"
                   "when doing scan-varying refinement"
           .expert_level = 1
-
-        set_scan_varying_errors = False
-          .type = bool
-          .help = "If scan-varying refinement is done, and if the estimated"
-                  "covariance of the B matrix has been calculated by the"
-                  "minimiser, choose whether to return this to the model or"
-                  "not. The default is not to, in order to keep the file size"
-                  "of the serialized model small."
 
         %(sv_phil)s
       }
@@ -508,7 +509,7 @@ def _parameterise_crystals(options, experiments, analysis):
             n_intervals = _set_n_intervals(
                 options.crystal.unit_cell.smoother, analysis, scan, exp_ids
             )
-            set_errors = options.crystal.unit_cell.set_scan_varying_errors
+            set_errors = options.set_scan_varying_errors
             xl_uc_param = ScanVaryingCrystalUnitCellParameterisation(
                 crystal,
                 array_range,
@@ -835,6 +836,8 @@ def build_prediction_parameterisation(
                 xl_uc_params,
                 gon_params,
             )
+            pred_param.set_scan_varying_errors = options.set_scan_varying_errors
+
         else:
             if options.sparse:
                 PredParam = XYPhiPredictionParameterisationSparse

algorithms/refinement/parameterisation/scan_varying_prediction_parameters.py
@@ -185,6 +185,10 @@ class ScanVaryingPredictionParameterisation(XYPhiPredictionParameterisation):
             goniometer_parameterisations=goniometer_parameterisations,
         )
 
+        # Avoid calculation in calculate_model_state_uncertainties unless this
+        # is set to True
+        self.set_scan_varying_errors = False
+
     def _get_xl_orientation_parameterisation(self, experiment_id):
         """Return the crystal orientation parameterisation for the requested
         experiment number (or None if the crystal orientation in that experiment
@@ -628,7 +632,7 @@ class ScanVaryingPredictionParameterisation(XYPhiPredictionParameterisation):
         experiment_id to calculate for a particular crystal at a particular
         scan-point"""
 
-        # first call, only a variance-covariance matrix is supplied
+        # First call, only a variance-covariance matrix is supplied
         if var_cov is not None:
             assert [obs_image_number, experiment_id].count(None) == 2
             super(
@@ -636,7 +640,7 @@ class ScanVaryingPredictionParameterisation(XYPhiPredictionParameterisation):
             ).calculate_model_state_uncertainties(var_cov)
             return
 
-        # later calls, only an experiment and image number are supplied
+        # Later calls, only an experiment and image number are supplied for
         # identify the crystal parameterisations for this experiment
         xl_op = self._get_xl_orientation_parameterisation(experiment_id)
         xl_ucp = self._get_xl_unit_cell_parameterisation(experiment_id)

commit 93d2e843409b7dc9bdb63801f51f6596358a702a

Ensure all scan-varying models are updated.
Regression introduced in a403602. Fixes #798, but an additional
test would be useful

algorithms/refinement/refiner.py
@@ -1100,28 +1100,29 @@ class ScanVaryingRefiner(Refiner):
             if A_list is not None:
                 exp.crystal.set_A_at_scan_points(A_list)
 
-            # Return early if not calculating scan-varying errors
-            if not self._pred_param.set_scan_varying_errors:
-                return
+            # Calculate scan-varying errors if requested
+            if self._pred_param.set_scan_varying_errors:
 
-            # get state covariance matrices the whole range of images. We select
-            # the first element of this at each image because crystal scan-varying
-            # parameterisations are not multi-state
-            state_cov_list = [
-                self._pred_param.calculate_model_state_uncertainties(
-                    obs_image_number=t, experiment_id=iexp
+                # get state covariance matrices the whole range of images. We select
+                # the first element of this at each image because crystal scan-varying
+                # parameterisations are not multi-state
+                state_cov_list = [
+                    self._pred_param.calculate_model_state_uncertainties(
+                        obs_image_number=t, experiment_id=iexp
+                    )
+                    for t in range(ar_range[0], ar_range[1] + 1)
+                ]
+                if "U_cov" in state_cov_list[0]:
+                    u_cov_list = [e["U_cov"] for e in state_cov_list]
+                else:
+                    u_cov_list = None
+
+                if "B_cov" in state_cov_list[0]:
+                    b_cov_list = [e["B_cov"] for e in state_cov_list]
+                else:
+                    b_cov_list = None
+
+                # return these to the model parameterisations to be set in the models
+                self._pred_param.set_model_state_uncertainties(
+                    u_cov_list, b_cov_list, iexp
                 )
-                for t in range(ar_range[0], ar_range[1] + 1)
-            ]
-            if "U_cov" in state_cov_list[0]:
-                u_cov_list = [e["U_cov"] for e in state_cov_list]
-            else:
-                u_cov_list = None
-
-            if "B_cov" in state_cov_list[0]:
-                b_cov_list = [e["B_cov"] for e in state_cov_list]
-            else:
-                b_cov_list = None
-
-            # return these to the model parameterisations to be set in the models
-            self._pred_param.set_model_state_uncertainties(u_cov_list, b_cov_list, iexp)