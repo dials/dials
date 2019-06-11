20744e1ad0e72875d3a18447f47e6027d5355fc9
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-06-11 13:02:19 +0100

    Copy over dxtbx test setup for future Python3 work
    This will auto-xfail tests when running under Python 3

--- conftest.py
+++ conftest.py
@@ -8,6 +8,7 @@ from __future__ import absolute_import, division, print_function
 import os
 
 import pytest
+import six
 
 
 def pytest_addoption(parser):
@@ -26,6 +27,14 @@ def pytest_collection_modifyitems(config, items):
         for item in items:
             if "slow" in item.keywords:
                 item.add_marker(skip_slow)
+
+
+def pytest_configure(config):
+    if six.PY3:
+        import dxtbx.tests.python3_test_filter as ptf
+
+        exp = ptf.Python3TestFailureExpectationPlugin(config)
+        config.pluginmanager.register(exp)
 
 
 @pytest.fixture(scope="session")
