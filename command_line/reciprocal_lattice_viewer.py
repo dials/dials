dials/dials

commit 01921d5feac6fdda61bd3c196c741a2e8bdcc8d6

Fix dials.rl_png command
due to recent changes in RLV. Some campsite improvements.

command_line/reciprocal_lattice_viewer.py
@@ -1,5 +1,4 @@
 # LIBTBX_PRE_DISPATCHER_INCLUDE_SH export PHENIX_GUI_ENVIRONMENT=1
-# LIBTBX_PRE_DISPATCHER_INCLUDE_SH export BOOST_ADAPTBX_FPE_DEFAULT=1
 
 # DIALS_ENABLE_COMMAND_LINE_COMPLETION
 from __future__ import absolute_import, division, print_function
@@ -775,10 +774,10 @@ class RLVWindow(wx_viewer.show_points_and_lines_mixin):
         self.beam_vector = beam
 
     def set_reciprocal_lattice_vectors(self, vectors_per_crystal):
-        self.recip_latt_vectors = []
         # the points are scaled by 100 so must do that here too
-        for vec_set in vectors_per_crystal:
-            self.recip_latt_vectors.append([100.0 * matrix.col(vec) for vec in vec_set])
+        self.recip_latt_vectors = [
+            [100.0 * matrix.col(vec) for vec in vectors_per_crystal]
+        ]
 
     # --- user input and settings
     def update_settings(self):

command_line/rl_png.py
@@ -1,8 +1,10 @@
 # LIBTBX_PRE_DISPATCHER_INCLUDE_SH export PHENIX_GUI_ENVIRONMENT=1
-# LIBTBX_PRE_DISPATCHER_INCLUDE_SH export BOOST_ADAPTBX_FPE_DEFAULT=1
 
 # DIALS_ENABLE_COMMAND_LINE_COMPLETION
 from __future__ import absolute_import, division, print_function
+
+import logging
+import math
 
 try:
     import matplotlib
@@ -12,10 +14,6 @@ except ImportError:
 # Offline backend
 matplotlib.use("Agg")
 
-import logging
-
-logger = logging.getLogger("dials.command_line.rl_png")
-
 import libtbx.phil
 from scitbx import matrix
 from scitbx.array_family import flex
@@ -23,7 +21,7 @@ from scitbx.array_family import flex
 from dials.command_line.reciprocal_lattice_viewer import render_3d
 from dials.command_line.reciprocal_lattice_viewer import help_message
 
-
+logger = logging.getLogger("dials.command_line.rl_png")
 phil_scope = libtbx.phil.parse(
     """
 include scope dials.command_line.reciprocal_lattice_viewer.phil_scope
@@ -73,14 +71,16 @@ class PngScene(object):
         self.points = points
 
     def set_colors(self, colors):
-        import math
-
         # convert whites to black (background is white)
         colors.set_selected((colors.norms() == math.sqrt(3)), (0, 0, 0))
         self.colors = colors
 
     def set_palette(self, palette):
         self.palette = palette
+
+    def set_reciprocal_lattice_vectors(self, *args, **kwargs):
+        # we do not draw reciprocal lattice vectors at this time
+        pass
 
     def project_2d(self, n):
         d = self.points.dot(n.elems)
@@ -121,17 +121,13 @@ class PngScene(object):
         pyplot.close()
 
 
-def run(args):
-
+def run():
     from dials.util.options import OptionParser
     from dials.util.options import flatten_experiments
     from dials.util.options import flatten_reflections
     from dials.util import log
-    import libtbx.load_env
 
-    usage = "%s [options] experiments.json reflections.pickle" % (
-        libtbx.env.dispatcher_name
-    )
+    usage = "dials.rl_png [options] experiments.json reflections.pickle"
 
     parser = OptionParser(
         usage=usage,
@@ -155,7 +151,7 @@ def run(args):
 
     # Log the diff phil
     diff_phil = parser.diff_phil.as_str()
-    if diff_phil is not "":
+    if diff_phil != "":
         logger.info("The following parameters have been modified:\n")
         logger.info(diff_phil)
 
@@ -164,7 +160,7 @@ def run(args):
     imagesets = experiments.imagesets()
 
     f = ReciprocalLatticePng(settings=params)
-    f.load_models(imagesets, reflections, None)
+    f.load_models(experiments, reflections)
 
     imageset = imagesets[0]
     rotation_axis = matrix.col(imageset.get_goniometer().get_rotation_axis())
@@ -173,9 +169,6 @@ def run(args):
     e1 = rotation_axis.normalize()
     e2 = s0.normalize()
     e3 = e1.cross(e2).normalize()
-    # print e1
-    # print e2
-    # print e3
 
     f.viewer.plot("rl_rotation_axis.png", n=e1.elems)
     f.viewer.plot("rl_beam_vector", n=e2.elems)
@@ -186,9 +179,9 @@ def run(args):
     if experiments.crystals().count(None) < len(experiments):
         for i, c in enumerate(experiments.crystals()):
             A = matrix.sqr(c.get_A())
-            astar = A[:3]
-            bstar = A[3:6]
-            cstar = A[6:9]
+            # astar = A[:3]
+            # bstar = A[3:6]
+            # cstar = A[6:9]
 
             direct_matrix = A.inverse()
             a = direct_matrix[:3]
@@ -208,8 +201,6 @@ def run(args):
 
         hardcoded_phil = dps_phil_scope.extract()
         hardcoded_phil.d_min = params.d_min
-
-        imageset = imagesets[0]
 
         if "imageset_id" not in reflections:
             reflections["imageset_id"] = reflections["id"]
@@ -247,6 +238,4 @@ def run(args):
 
 
 if __name__ == "__main__":
-    import sys
-
-    run(sys.argv[1:])
+    run()

commit bae377643dd2243fa1aafb59ef5b56b8b0e4a03f

fix RLV for lattice vectors broken by 01921d5feac6fdda61bd3c196c741a2e8bdcc8d6

command_line/reciprocal_lattice_viewer.py
@@ -776,7 +776,11 @@ class RLVWindow(wx_viewer.show_points_and_lines_mixin):
     def set_reciprocal_lattice_vectors(self, vectors_per_crystal):
         # the points are scaled by 100 so must do that here too
         self.recip_latt_vectors = [
-            [100.0 * matrix.col(vec) for vec in vectors_per_crystal]
+            [
+                100.0 * matrix.col(vec)
+                for vectors in vectors_per_crystal
+                for vec in vectors
+            ]
         ]
 
     # --- user input and settings