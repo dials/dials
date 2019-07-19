b706981af00dd851f6bf04eaa249aeb8ed6f7362
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-07-19 09:02:12 +0100

    Run test in temporary directory
    instead of leaving droppings in working directory

--- test/command_line/test_split_experiments.py
+++ test/command_line/test_split_experiments.py
@@ -1,5 +1,7 @@
+from __future__ import absolute_import, division, print_function
+
 """Tests for dials.split_experiments when experiment ids are set"""
-import os
+
 import procrunner
 from dxtbx.model import Beam, Experiment, ExperimentList
 from dxtbx.model.experiment_list import ExperimentListFactory
@@ -13,7 +15,7 @@ def generate_exp(wavelength=1):
     return exp
 
 
-def test_split_by_wavelength():
+def test_split_by_wavelength(tmpdir):
     """Test the split_by_wavelength option of dials.split_experiments"""
     experiments = ExperimentList()
     exp = generate_exp(wavelength=1.0)
@@ -29,24 +31,27 @@ def test_split_by_wavelength():
     reflections.experiment_identifiers()[0] = "0"
     reflections.experiment_identifiers()[1] = "1"
 
-    experiments.as_json("tmp.expt")
-    reflections.as_pickle("tmp.refl")
+    experiments.as_json(tmpdir.join("tmp.expt").strpath)
+    reflections.as_pickle(tmpdir.join("tmp.refl").strpath)
 
     result = procrunner.run(
-        ["dials.split_experiments", "tmp.expt", "tmp.refl", "by_wavelength=True"]
+        ["dials.split_experiments", "tmp.expt", "tmp.refl", "by_wavelength=True"],
+        working_directory=tmpdir,
     )
-    assert result["exitcode"] == 0
-    assert result["stderr"] == ""
+    assert result.returncode == 0
+    assert result.stderr == ""
 
     for i, (wl, ids, intensity) in enumerate(
         zip([0.5, 1.0], ["1", "0"], [200.0, 100.0])
     ):
-        assert os.path.exists("split_%d.expt" % i)
-        assert os.path.exists("split_%d.refl" % i)
+        assert tmpdir.join("split_%d.expt" % i).check()
+        assert tmpdir.join("split_%d.refl" % i).check()
         exp_single = ExperimentListFactory.from_json_file(
-            "split_%d.expt" % i, check_format=False
+            tmpdir.join("split_%d.expt" % i).strpath, check_format=False
         )
-        ref_single = flex.reflection_table.from_pickle("split_%d.refl" % i)
+        ref_single = flex.reflection_table.from_pickle(
+            tmpdir.join("split_%d.refl" % i).strpath
+        )
         assert exp_single[0].beam.get_wavelength() == wl
         assert exp_single[0].identifier == ids
         id_ = ref_single["id"][0]
@@ -56,21 +61,23 @@ def test_split_by_wavelength():
     # Now test for successful error handling if no identifiers set.
     experiments[0].identifier = ""
     experiments[1].identifier = ""
-    experiments.as_json("tmp.expt")
+    experiments.as_json(tmpdir.join("tmp.expt").strpath)
     result = procrunner.run(
-        ["dials.split_experiments", "tmp.expt", "tmp.refl", "by_wavelength=True"]
+        ["dials.split_experiments", "tmp.expt", "tmp.refl", "by_wavelength=True"],
+        working_directory=tmpdir,
     )
-    assert result["exitcode"] == 1
-    assert result["stderr"].startswith("Sorry")
+    assert result.returncode == 1
+    assert result.stderr.startswith("Sorry")
 
     experiments[0].identifier = "0"
     experiments[1].identifier = "1"
     del reflections.experiment_identifiers()[0]
     del reflections.experiment_identifiers()[1]
-    experiments.as_json("tmp.expt")
-    reflections.as_pickle("tmp.refl")
+    experiments.as_json(tmpdir.join("tmp.expt").strpath)
+    reflections.as_pickle(tmpdir.join("tmp.refl").strpath)
     result = procrunner.run(
-        ["dials.split_experiments", "tmp.expt", "tmp.refl", "by_wavelength=True"]
+        ["dials.split_experiments", "tmp.expt", "tmp.refl", "by_wavelength=True"],
+        working_directory=tmpdir,
     )
-    assert result["exitcode"] == 1
-    assert result["stderr"].startswith("Sorry")
+    assert result.returncode == 1
+    assert result.stderr.startswith("Sorry")
