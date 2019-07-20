e99c7a0929e2ccbbebba24937e0cf5a86b094a32
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-07-20 01:45:10 +0100

    create temporary files in temporary place

--- test/command_line/test_goniometer_calibration.py
+++ test/command_line/test_goniometer_calibration.py
@@ -2,17 +2,17 @@ from __future__ import absolute_import, division, print_function
 
 import os
 
-from libtbx import easy_run
+import procrunner
 
 
-def test_goniometer_calibration(dials_regression):
+def test_goniometer_calibration(dials_regression, tmpdir):
     data_dir = os.path.join(dials_regression, "rotation_calibration")
     o0_k0_p0 = os.path.join(data_dir, "experiments_o0_k0_p0.json")
     o0_k0_p48 = os.path.join(data_dir, "experiments_o0_k0_p48.json")
     o0_k48_p48 = os.path.join(data_dir, "experiments_o0_k48_p48.json")
     o48_k48_p48 = os.path.join(data_dir, "experiments_o48_k48_p48.json")
 
-    args = [
+    command = [
         "dials.goniometer_calibration",
         o0_k0_p0,
         o0_k0_p48,
@@ -22,9 +22,9 @@ def test_goniometer_calibration(dials_regression):
         "xoalign=xoalign_config.py",
     ]
 
-    command = " ".join(args)
     print(command)
-    result = easy_run.fully_buffered(command=command).raise_if_errors()
+    result = procrunner.run(command, working_directory=tmpdir)
+    assert not result.returncode and not result.stderr
 
     expected_output = """
 Goniometer axes and angles (ImgCIF coordinate system):
@@ -52,15 +52,15 @@ loop_
   GON_OMEGA  rotation  goniometer  .          1.0000   0.0000   0.0000  .  .  .
   GON_KAPPA  rotation  goniometer  GON_OMEGA  0.9131   0.2794  -0.2968  .  .  .
   GON_PHI    rotation  goniometer  GON_KAPPA  1.0000  -0.0059  -0.0059  .  .  .
-"""
-    assert "\n".join(result.stdout_lines[12:]) == "".join(expected_output)
 
-    assert os.path.exists("xoalign_config.py")
+"""
+    assert result.stdout.endswith(expected_output)
+
+    assert tmpdir.join("xoalign_config.py").check()
     expected_xoalign_config = """\
 GONIOMETER_AXES_NAMES = ('GON_OMEGA', 'GON_KAPPA', 'GON_PHI')
 GONIOMETER_AXES = [(-0.00092, 0.00000, 1.00000), (0.29597, 0.27943, 0.91341), (0.00494, -0.00588, 0.99997)]
 GONIOMETER_DATUM = (0,0,0) # in degrees
 """
-    with open("xoalign_config.py", "rb") as f:
-        text = f.read()
-        assert text == expected_xoalign_config
+    xoalign_config = tmpdir.join("xoalign_config.py").read()
+    assert xoalign_config == expected_xoalign_config
