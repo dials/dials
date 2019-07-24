828f647ee2d6cddea0678c7628b1332c4fe45a62
David Waterman <dgwaterman@gmail.com>
2019-07-24 15:08:45 +0100

    Remove deprecated ExperimentListDumper.

--- command_line/stills_process.py
+++ command_line/stills_process.py
@@ -662,10 +662,8 @@ class Processor(object):
             not self.params.output.composite_output
             and self.params.output.experiments_filename
         ):
-            from dxtbx.model.experiment_list import ExperimentListDumper
 
-            dump = ExperimentListDumper(experiments)
-            dump.as_json(self.params.output.experiments_filename)
+            experiments.as_json(self.params.output.experiments_filename)
 
         # Do the processing
         try:
@@ -914,10 +912,8 @@ class Processor(object):
         else:
             # Dump experiments to disk
             if self.params.output.refined_experiments_filename:
-                from dxtbx.model.experiment_list import ExperimentListDumper
 
-                dump = ExperimentListDumper(experiments)
-                dump.as_json(self.params.output.refined_experiments_filename)
+                experiments.as_json(self.params.output.refined_experiments_filename)
 
             if self.params.output.indexed_filename:
                 self.save_reflections(centroids, self.params.output.indexed_filename)
@@ -1039,10 +1035,8 @@ class Processor(object):
         else:
             # Dump experiments to disk
             if self.params.output.integrated_experiments_filename:
-                from dxtbx.model.experiment_list import ExperimentListDumper
 
-                dump = ExperimentListDumper(experiments)
-                dump.as_json(self.params.output.integrated_experiments_filename)
+                experiments.as_json(self.params.output.integrated_experiments_filename)
 
             if self.params.output.integrated_filename:
                 # Save the reflections
@@ -1287,10 +1281,10 @@ class Processor(object):
                 len(self.all_indexed_experiments) > 0
                 and self.params.output.refined_experiments_filename
             ):
-                from dxtbx.model.experiment_list import ExperimentListDumper
 
-                dump = ExperimentListDumper(self.all_indexed_experiments)
-                dump.as_json(self.params.output.refined_experiments_filename)
+                self.all_indexed_experiments.as_json(
+                    self.params.output.refined_experiments_filename
+                )
 
             if (
                 len(self.all_indexed_reflections) > 0
@@ -1304,10 +1298,10 @@ class Processor(object):
                 len(self.all_integrated_experiments) > 0
                 and self.params.output.integrated_experiments_filename
             ):
-                from dxtbx.model.experiment_list import ExperimentListDumper
 
-                dump = ExperimentListDumper(self.all_integrated_experiments)
-                dump.as_json(self.params.output.integrated_experiments_filename)
+                self.all_integrated_experiments.as_json(
+                    self.params.output.integrated_experiments_filename
+                )
 
             if (
                 len(self.all_integrated_reflections) > 0
