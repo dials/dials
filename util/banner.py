52449290e53ea336cad2d3d2b898afde1426c12b
Markus Gerstel <2102431+Anthchirp@users.noreply.github.com>
2019-07-24 13:54:44 +0100

    Overhaul DIALS logging mechanism, #832 (#866)
    * write logs into a single file and on screen
    * remove debug log file, you can use '-v' to get debug level logging
    * add deprecation warnings for unused configuration functions and
      parameters
    * change the DIALS banner printing logic from an action implicit on
      import to an explicit function call
    * added a technology demonstration feature: if dlstbx is installed can
      run with environment variable COLOURLOG=1 to get coloured log output
      on stdout
    
    Fixes #832

--- util/banner.py
+++ util/banner.py
@@ -1,23 +1,11 @@
 from __future__ import absolute_import, division, print_function
 
-import logging
-import os
+import dials.util.log
+import warnings
 
-banner = (
-    "DIALS (2018) Acta Cryst. D74, 85-97. https://doi.org/10.1107/S2059798317017235"
+dials.util.log.print_banner()
+warnings.warn(
+    "dials.util.banner is deprecated. use dials.util.log.print_banner()",
+    DeprecationWarning,
+    stacklevel=2,
 )
-
-
-def print_banner():
-    if os.getenv("DIALS_NOBANNER"):
-        return
-    d = logging.getLogger("dials")
-    d.info(banner)
-    logging_to_stdout = any(
-        map(lambda h: isinstance(h, logging.StreamHandler), d.handlers)
-    )
-    if not logging_to_stdout:
-        print(banner)
-
-
-print_banner()
