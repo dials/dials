436dd9fb1398bd447e0b40530263efbc8c59aa6c
Richard Gildea <richard.gildea@diamond.ac.uk>
2019-07-24 14:45:10 +0100

    add LoggingContext manager as suggested by @Anthchirp

---
+++ test/util/test_log.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import, division
+
+import logging
+
+from dials.util.log import config_simple_stdout, LoggingContext
+
+
+def test_LoggingContext():
+    # configure logging
+    config_simple_stdout()
+
+    # get some loggers
+    idx_logger = logging.getLogger("dials.algorithms.indexing")
+    dials_logger = logging.getLogger("dials")
+
+    # check the logging level is as expected
+    assert idx_logger.getEffectiveLevel() == logging.DEBUG
+    assert dials_logger.getEffectiveLevel() == logging.DEBUG
+
+    # selectively change logging level and check this has worked
+    with LoggingContext(idx_logger, logging.ERROR):
+        assert idx_logger.getEffectiveLevel() == logging.ERROR
+        assert dials_logger.getEffectiveLevel() == logging.DEBUG
+
+    # check logging levels are as they were before
+    assert idx_logger.getEffectiveLevel() == logging.DEBUG
+    assert dials_logger.getEffectiveLevel() == logging.DEBUG
