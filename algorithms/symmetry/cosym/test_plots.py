42117247f698f82131f51c3ebee38893b0333d60
Richard Gildea <richard.gildea@diamond.ac.uk>
2019-07-31 15:07:39 +0100

    update expected test output

--- algorithms/symmetry/cosym/test_plots.py
+++ algorithms/symmetry/cosym/test_plots.py
@@ -9,16 +9,16 @@ def test_plot_coords():
     coords.reshape(flex.grid((4, 2)))
     labels = flex.int([0, 0, 1, 1])
     d = plots.plot_coords(coords, labels=labels)
-    assert "coordinates" in d
-    assert set(d["coordinates"]) == {"layout", "data"}
-    assert d["coordinates"]["data"][0]["x"] == [0.0, 0.0]
-    assert d["coordinates"]["data"][0]["y"] == [1.0, 1.0]
-    assert d["coordinates"]["data"][1]["x"] == [1.0, 1.0]
-    assert d["coordinates"]["data"][1]["y"] == [0.0, 0.0]
+    assert "cosym_coordinates" in d
+    assert set(d["cosym_coordinates"]) == {"layout", "data"}
+    assert d["cosym_coordinates"]["data"][0]["x"] == [0.0, 0.0]
+    assert d["cosym_coordinates"]["data"][0]["y"] == [1.0, 1.0]
+    assert d["cosym_coordinates"]["data"][1]["x"] == [1.0, 1.0]
+    assert d["cosym_coordinates"]["data"][1]["y"] == [0.0, 0.0]
 
 
 def test_plot_rij_histogram():
     rij_matrix = flex.random_double(16)
     d = plots.plot_rij_histogram(rij_matrix)
-    assert "rij_histogram" in d
-    sum(d["rij_histogram"]["data"][0]["y"]) == 16
+    assert "cosym_rij_histogram" in d
+    sum(d["cosym_rij_histogram"]["data"][0]["y"]) == 16
