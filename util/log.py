133405b15f33b0dcbe9a21e2d2b59b5d10b70db0
Richard Gildea <richard.gildea@diamond.ac.uk>
2019-08-08 10:20:10 +0100

    remove handler/close functionality as not used/tested

--- util/log.py
+++ util/log.py
@@ -165,24 +165,16 @@ def print_banner(force=False, use_logging=False):
 
 class LoggingContext(object):
     # https://docs.python.org/3/howto/logging-cookbook.html#using-a-context-manager-for-selective-logging
-    def __init__(self, logger, level=None, handler=None, close=True):
+    def __init__(self, logger, level=None):
         self.logger = logger
         self.level = level
-        self.handler = handler
-        self.close = close
 
     def __enter__(self):
         if self.level is not None:
             self.old_level = self.logger.level
             self.logger.setLevel(self.level)
-        if self.handler:
-            self.logger.addHandler(self.handler)
 
     def __exit__(self, et, ev, tb):
         if self.level is not None:
             self.logger.setLevel(self.old_level)
-        if self.handler:
-            self.logger.removeHandler(self.handler)
-        if self.handler and self.close:
-            self.handler.close()
         # implicit return of None => don't swallow exceptions
