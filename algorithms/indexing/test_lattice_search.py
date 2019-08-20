5897078efe902ad070022ae5a36154b615d8fb3a
David Waterman <dgwaterman@gmail.com>
2019-08-20 13:14:18 +0100

    Add a direct test of LatticeSearch.

--- algorithms/indexing/test_lattice_search.py
+++ algorithms/indexing/test_lattice_search.py
@@ -99,3 +99,37 @@ def test_stills_indexer_BasisVectorSearch_i04_weak_data(
     assert indexed_experiments[0].crystal.get_unit_cell().parameters() == pytest.approx(
         (57.752, 57.776, 150.013, 90.0101, 89.976, 90.008), rel=1e-2
     )
+
+
+@pytest.mark.parametrize(
+    "indexing_method,space_group,unit_cell",
+    (("low_res_spot_match", "P422", (57.8, 57.8, 150.0, 90, 90, 90)),),
+)
+def test_stills_indexer_LatticeSearch_i04_weak_data(
+    i04_weak_data, indexing_method, space_group, unit_cell
+):
+    reflections = slice_reflections(i04_weak_data["reflections"], [(1, 2)])
+    experiments = slice_experiments(i04_weak_data["experiments"], [(1, 2)])
+    for experiment in experiments:
+        experiment.imageset = ImageSet(
+            experiment.imageset.data(), experiment.imageset.indices()
+        )
+        experiment.imageset.set_scan(None)
+        experiment.imageset.set_goniometer(None)
+        experiment.scan = None
+        experiment.goniometer = None
+    params = phil_scope.fetch().extract()
+    params.indexing.method = indexing_method
+    params.indexing.basis_vector_combinations.max_refine = 5
+    if unit_cell is not None:
+        params.indexing.known_symmetry.unit_cell = uctbx.unit_cell(unit_cell)
+    if space_group is not None:
+        params.indexing.known_symmetry.space_group = sgtbx.space_group_info(space_group)
+    idxr = stills_indexer.StillsIndexerLatticeSearch(reflections, experiments, params)
+    idxr.index()
+
+    indexed_experiments = idxr.refined_experiments
+    assert len(indexed_experiments) == 1
+    assert indexed_experiments[0].crystal.get_unit_cell().parameters() == pytest.approx(
+        (57.752, 57.776, 150.013, 90.0101, 89.976, 90.008), rel=1e-2
+    )
