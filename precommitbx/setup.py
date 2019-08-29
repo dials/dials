acb12b9ece8cde7e6d6f3796aa41e624ca2358c4
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-08-29 22:24:29 +0100

    add missing regex-group->string conversion

--- precommitbx/setup.py
+++ precommitbx/setup.py
@@ -6,19 +6,20 @@ from setuptools import find_packages, setup
 with open("_precommitbx/__init__.py") as fh:
     init_file = fh.read()
     version_match = re.search(r"^__version__ = \"([0-9.]+)\"", init_file, re.M)
-    pc_min_version = re.search(
+    pc_version_match = re.search(
         r"^__precommit_min_version__ = \"([0-9.]+)\"", init_file, re.M
     )
-    if not version_match or not pc_min_version:
+    if not version_match or not pc_version_match:
         raise RuntimeError("Unable to find version string")
 
 version = version_match.group(1)
+pre_commit_min_version = pc_version_match.group(1)
 
 setup(
     author="DIALS",
     author_email="dials-support@lists.sourceforge.net",
     description="A cctbx-adapter for pre-commit",
-    install_requires=["pre-commit>=" + pc_min_version],
+    install_requires=["pre-commit>=" + pre_commit_min_version],
     python_requires=">=3.5",
     license="BSD license",
     include_package_data=True,
