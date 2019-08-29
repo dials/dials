db6eea47ceeab2b1d24fa9ae0fd3d5702a9727a3
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-08-29 09:15:40 +0100

    disable libtbx testing for DIALS
    closes #890

--- run_tests.py
+++ run_tests.py
@@ -1,18 +1,10 @@
 from __future__ import absolute_import, division, print_function
 
-from libtbx.test_utils.pytest import discover
+exit(
+    """
+To run dials tests please run 'pytest' inside the dials directory
 
-tst_list = discover()
-
-# To write tests for dials:
-
-# 1. Test file should be named test_*.py
-# 2. Test methods should be named test_*()
-# 3. Nothing else needed. Rest happens by magic.
-
-# To run dials tests:
-
-# run 'pytest' inside dials directory
-
-# For more information see:
-#   https://github.com/dials/dials/wiki/pytest
+For more information see:
+  https://github.com/dials/dials/wiki/pytest
+"""
+)
