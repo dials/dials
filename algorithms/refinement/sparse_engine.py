88de6e5df1578f7d899284d9856c321386d75856
Nicholas Devenish <ndevenish@gmail.com>
2019-09-09 17:11:02 +0100

    Remove missed copyright notice not at top of file

--- algorithms/refinement/sparse_engine.py
+++ algorithms/refinement/sparse_engine.py
@@ -1,13 +1,5 @@
 from __future__ import absolute_import, division, print_function
 
-#
-#  Copyright (C) (2016) Lawrence Berkeley National Laboratory
-#
-#  Author: Nicholas Sauter.
-#
-#  This code is distributed under the BSD license, a copy of which is
-#  included in the root directory of this package.
-#
 import libtbx
 from dials.algorithms.refinement import DialsRefineConfigError
 from scitbx.array_family import flex
