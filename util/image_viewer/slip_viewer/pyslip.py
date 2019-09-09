20c4f1939c11b28c9f73bf5fcbd4aecef99afe0a
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-09-09 09:15:00 +0100

    silence lgtm alerts
    These variables are neither directly used nor can they be removed.
    Pyslip uses them in self-modifying code.
    Silence all possible warnings lgtm can come up with.

--- util/image_viewer/slip_viewer/pyslip.py
+++ util/image_viewer/slip_viewer/pyslip.py
@@ -1713,8 +1713,8 @@ class PySlip(_BufferedCanvas):
                         dc.DrawCircle(x + x_off, y + y_off, radius)
         else:
             (dc_w, dc_h) = dc.GetSize()
-            dc_w2 = dc_w / 2  # noqa; lgtm[py/unused-local-variable] self-modifying code
-            dc_h2 = dc_h / 2  # noqa; lgtm[py/unused-local-variable] self-modifying code
+            dc_w2 = dc_w / 2  # noqa; lgtm; self-modifying code
+            dc_h2 = dc_h / 2  # noqa; lgtm; self-modifying code
             dc_h -= 1
             dc_w -= 1
             dc = wx.GCDC(dc)  # allow transparent colours
@@ -1781,8 +1781,8 @@ class PySlip(_BufferedCanvas):
                     lines.append(p_lonlat)
         else:
             (dc_w, dc_h) = dc.GetSize()
-            dc_w2 = dc_w / 2  # noqa; lgtm[py/unused-local-variable] self-modifying code
-            dc_h2 = dc_h / 2  # noqa; lgtm[py/unused-local-variable] self-modifying code
+            dc_w2 = dc_w / 2  # noqa; lgtm; self-modifying code
+            dc_h2 = dc_h / 2  # noqa; lgtm; self-modifying code
             dc_w -= 1
             dc_h -= 1
             dc = wx.GCDC(dc)  # allow transparent colours
@@ -1835,8 +1835,8 @@ class PySlip(_BufferedCanvas):
         # draw images on map/view
         if map_rel:
             for (lon, lat, bmap, w, h, place, x_off, y_off, idata) in images:
-                w2 = w / 2  # noqa; lgtm[py/unused-local-variable] self-modifying code
-                h2 = h / 2  # noqa; lgtm[py/unused-local-variable] self-modifying code
+                w2 = w / 2  # noqa; lgtm; self-modifying code
+                h2 = h / 2  # noqa; lgtm; self-modifying code
                 pt = self.ConvertGeo2ViewMasked((lon, lat))
                 if pt:
                     (x, y) = pt
@@ -1844,11 +1844,11 @@ class PySlip(_BufferedCanvas):
                     dc.DrawBitmap(bmap, x, y, False)
         else:
             (dc_w, dc_h) = dc.GetSize()
-            dc_w2 = dc_w / 2  # noqa; lgtm[py/unused-local-variable] self-modifying code
-            dc_h2 = dc_h / 2  # noqa; lgtm[py/unused-local-variable] self-modifying code
+            dc_w2 = dc_w / 2  # noqa; lgtm; self-modifying code
+            dc_h2 = dc_h / 2  # noqa; lgtm; self-modifying code
             for (x, y, bmap, w, h, place, x_off, y_off, idata) in images:
-                w2 = w / 2  # noqa; lgtm[py/unused-local-variable] self-modifying code
-                h2 = h / 2  # noqa; lgtm[py/unused-local-variable] self-modifying code
+                w2 = w / 2  # noqa; lgtm; self-modifying code
+                h2 = h / 2  # noqa; lgtm; self-modifying code
                 exec(self.image_view_placement[place])
                 dc.DrawBitmap(bmap, x, y, False)
 
@@ -1904,20 +1904,16 @@ class PySlip(_BufferedCanvas):
 
                     # place the text relative to hotpoint
                     (w, h, _, _) = dc.GetFullTextExtent(tdata)
-                    w2 = (
-                        w / 2
-                    )  # noqa; lgtm[py/unused-local-variable] self-modifying code
-                    h2 = (
-                        h / 2
-                    )  # noqa; lgtm[py/unused-local-variable] self-modifying code
+                    w2 = w / 2  # noqa; lgtm; self-modifying code
+                    h2 = h / 2  # noqa; lgtm; self-modifying code
                     exec(self.text_map_placement[place])
                     dc.SetTextForeground(textcolour)
                     dc.DrawText(tdata, x, y)
         else:
             # we need the size of the DC
             (dc_w, dc_h) = dc.GetSize()
-            dc_w2 = dc_w / 2  # noqa; lgtm[py/unused-local-variable] self-modifying code
-            dc_h2 = dc_h / 2  # noqa; lgtm[py/unused-local-variable] self-modifying code
+            dc_w2 = dc_w / 2  # noqa; lgtm; self-modifying code
+            dc_h2 = dc_h / 2  # noqa; lgtm; self-modifying code
             dc_w -= 1
             dc_h -= 1
             dc = wx.GCDC(dc)  # allow transparent colours
@@ -1957,8 +1953,8 @@ class PySlip(_BufferedCanvas):
 
                 # place the text relative to hotpoint
                 (w, h, _, _) = dc.GetFullTextExtent(tdata)  # size of text
-                w2 = w / 2  # noqa; lgtm[py/unused-local-variable] self-modifying code
-                h2 = h / 2  # noqa; lgtm[py/unused-local-variable] self-modifying code
+                w2 = w / 2  # noqa; lgtm; self-modifying code
+                h2 = h / 2  # noqa; lgtm; self-modifying code
                 exec(self.text_view_placement[place])
                 dc.SetTextForeground(textcolour)
                 dc.DrawText(tdata, x, y)
@@ -2593,12 +2589,8 @@ class PySlip(_BufferedCanvas):
             for p in layer.data:
                 dc = wx.BufferedPaintDC(self, self.buffer)
                 (dc_w, dc_h) = dc.GetSize()
-                dc_w2 = (  # noqa; lgtm[py/unused-local-variable] self-modifying code
-                    dc_w / 2
-                )
-                dc_h2 = (  # noqa; lgtm[py/unused-local-variable] self-modifying code
-                    dc_h / 2
-                )
+                dc_w2 = dc_w / 2  # noqa; lgtm; self-modifying code
+                dc_h2 = dc_h / 2  # noqa; lgtm; self-modifying code
                 dc_h -= 1
                 dc_w -= 1
                 (x, y, place, _, _, x_off, y_off, pdata) = p
@@ -2648,12 +2640,8 @@ class PySlip(_BufferedCanvas):
             for p in layer.data:
                 dc = wx.BufferedPaintDC(self, self.buffer)
                 (dc_w, dc_h) = dc.GetSize()
-                dc_w2 = (  # noqa; lgtm[py/unused-local-variable] self-modifying code
-                    dc_w / 2
-                )
-                dc_h2 = (  # noqa; lgtm[py/unused-local-variable] self-modifying code
-                    dc_h / 2
-                )
+                dc_w2 = dc_w / 2  # noqa; lgtm; self-modifying code
+                dc_h2 = dc_h / 2  # noqa; lgtm; self-modifying code
                 dc_h -= 1
                 dc_w -= 1
                 (x, y, place, _, _, x_off, y_off, pdata) = p
