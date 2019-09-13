a9cf1f10fb756fe8973f2715826cf54bef08ca3d
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-09-13 14:57:08 +0100

    Update release information
    and in future update this outside of documentation build

--- command_line/make_sphinx_html.py
+++ command_line/make_sphinx_html.py
@@ -2,90 +2,12 @@
 
 from __future__ import absolute_import, division, print_function
 
-import json
-import re
 import sys
-from datetime import datetime
 from optparse import SUPPRESS_HELP, OptionParser
 
 import dials
 import procrunner
 import py
-
-try:  # Python 3
-    from urllib.request import urlopen, Request
-except ImportError:  # Python 2
-    from urllib2 import urlopen, Request
-
-
-def update_dials_download_links():
-    print("Checking DIALS release status")
-    url_request = Request("https://api.github.com/repos/dials/dials/releases/latest")
-    conn = urlopen(url_request)
-    try:
-        release_info = json.load(conn)
-    finally:
-        conn.close()
-
-    dials_dir = py.path.local(dials.__file__).dirpath()
-    release_file = dials_dir / "doc" / "sphinx" / "installation.stable_release"
-
-    with release_file.open("w") as release:
-        caption = "Stable Release"
-        if "name" in release_info:
-            caption = caption + ": " + release_info["name"]
-            print("Most recent major DIALS release is:", release_info["name"])
-        else:
-            print("Could not determine most recent major DIALS release")
-        release.write(caption + "\n" + "=" * len(caption) + "\n\n")
-
-        release.write(
-            "The current stable release can be downloaded from `Github <https://github.com/dials/dials/releases/latest>`_,\n"
-        )
-        release.write(
-            "where you can also find further `release notes <https://github.com/dials/dials/releases/latest>`_.\n\n"
-        )
-
-        def download_button(text, version, link):
-            print("  %s %s -> %s" % (version, text, link))
-            return ".. button::\n   :text: DIALS %s %s\n   :link: %s\n\n" % (
-                version,
-                text,
-                link,
-            )
-
-        assets = {}
-        for a in release_info.get("assets", []):
-            tag = re.search("dials-v([^-]+)-([^-]+)-([^-]+)-(.+)", a["name"])
-            if tag:
-                shortname = tag.group(4)
-                version = ".".join(tag.group(1, 2, 3))
-                last_update = datetime.strptime(
-                    a["updated_at"], "%Y-%m-%dT%H:%M:%SZ"
-                )  # - datetime(1970,1,1)).total_seconds()
-                if shortname not in assets or assets[shortname][0] < last_update:
-                    assets[shortname] = (
-                        last_update,
-                        version,
-                        a.get("browser_download_url"),
-                    )
-
-        long_names = {
-            "macosx.pkg": "Mac installer",
-            "macosx.tar.gz": "Mac tar archive",
-            "macosx-10.6.pkg": "Mac installer (OS X 10.6)",
-            "macosx-10.6.tar.gz": "Mac tar archive (OS X 10.6)",
-            "linux-x86_64.tar.xz": "Linux installer",
-            "source.tar.xz": "Source installer",
-        }
-
-        buttons = [
-            download_button(long_names.get(asset, asset), _version, link)
-            for asset, (_, _version, link) in assets.items()
-        ]
-
-        release.write("".join(sorted(buttons)))
-
 
 if __name__ == "__main__":
     dials_dir = py.path.local(dials.__file__).dirpath()
@@ -152,13 +74,6 @@ if __name__ == "__main__":
         help="Build documentation in parallel",
     )
     options, _ = parser.parse_args()
-
-    try:
-        update_dials_download_links()
-    except Exception as e:
-        if options.strict:
-            raise
-        print("Ignoring error:", e)
 
     output_dir = py.path.local(options.output)
     if options.clean:
