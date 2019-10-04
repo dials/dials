4da2bf2dc018cfe4567c084bef67f5a9aa0e0c3a
Takanori Nakane <nakane.t@gmail.com>
2019-10-04 11:58:10 +0100

    Use a mask from dxtbx in dials.rs_mapper.
    Also removed FIXMEs and added more checks.

--- command_line/rs_mapper.py
+++ command_line/rs_mapper.py
@@ -3,7 +3,8 @@ from __future__ import absolute_import, division, print_function
 import math
 
 import dials.algorithms.rs_mapper as recviewer
-import dials.util
+from dials.util import Sorry, show_mail_on_error
+from dials.util.options import flatten_experiments, OptionParser
 from cctbx import sgtbx, uctbx
 from iotbx import ccp4_map, phil
 from scitbx.array_family import flex
@@ -27,11 +28,11 @@ phil_scope = phil.parse(
 rs_mapper
   .short_caption = Reciprocal space mapper
 {
-  map_file = rs_mapper_output.ccp4
+  map_file = rs_mapper_output.mrc
     .type = path
     .optional = False
     .multiple= False
-    .short_caption = Map file
+    .short_caption = Output map file
   max_resolution = 6
     .type = float
     .optional = True
@@ -42,6 +43,10 @@ rs_mapper
   reverse_phi = False
     .type = bool
     .optional = True
+  ignore_mask = False
+    .type = bool
+    .optional = True
+    .short_caption = Ignore masks from dxtbx class
 }
 """,
     process_includes=True,
@@ -51,7 +56,6 @@ rs_mapper
 class Script(object):
     def __init__(self):
         """Initialise the script."""
-        from dials.util.options import OptionParser
 
         # The script usage
         usage = (
@@ -66,8 +70,6 @@ class Script(object):
         )
 
     def run(self):
-        from dials.util.options import flatten_experiments
-
         # Parse the command line
         params, options = self.parser.parse_args(show_diff_phil=True)
 
@@ -86,18 +88,20 @@ class Script(object):
         self.reverse_phi = params.rs_mapper.reverse_phi
         self.grid_size = params.rs_mapper.grid_size
         self.max_resolution = params.rs_mapper.max_resolution
+        self.ignore_mask = params.rs_mapper.ignore_mask
 
         self.grid = flex.double(
             flex.grid(self.grid_size, self.grid_size, self.grid_size), 0
         )
-        self.cnts = flex.int(
+        self.counts = flex.int(
             flex.grid(self.grid_size, self.grid_size, self.grid_size), 0
         )
 
         for experiment in self.experiments:
             self.process_imageset(experiment.imageset)
 
-        recviewer.normalize_voxels(self.grid, self.cnts)
+        recviewer.normalize_voxels(self.grid, self.counts)
+
         # Let's use 1/(100A) as the unit so that the absolute numbers in the
         # "cell dimensions" field of the ccp4 map are typical for normal
         # MX maps. The values in 1/A would give the "cell dimensions" around
@@ -117,38 +121,44 @@ class Script(object):
     def process_imageset(self, imageset):
         rec_range = 1 / self.max_resolution
 
+        if len(imageset.get_detector()) != 1:
+            raise Sorry("This program does not support multi-panel detectors.")
+
         panel = imageset.get_detector()[0]
         beam = imageset.get_beam()
         s0 = beam.get_s0()
         pixel_size = panel.get_pixel_size()
         xlim, ylim = imageset.get_raw_data(0)[0].all()
+        if pixel_size[0] != pixel_size[1]:
+            raise Sorry("This program does not support non-square pixels.")
 
         # cache transformation
         xy = recviewer.get_target_pixels(panel, s0, xlim, ylim, self.max_resolution)
-
-        s1 = panel.get_lab_coord(xy * pixel_size[0])  # FIXME: assumed square pixel
+        s1 = panel.get_lab_coord(xy * pixel_size[0])
         s1 = s1 / s1.norms() * (1 / beam.get_wavelength())
         S = s1 - s0
 
         for i in range(len(imageset)):
             axis = imageset.get_goniometer().get_rotation_axis()
             osc_range = imageset.get_scan(i).get_oscillation_range()
-            print("Oscillation range: %.1f - %.1f" % (osc_range[0], osc_range[1]))
+            print("Oscillation range: %.2f - %.2f" % (osc_range[0], osc_range[1]))
             angle = (osc_range[0] + osc_range[1]) / 2 / 180 * math.pi
-            if not self.reverse_phi:  # FIXME: ???
+            if not self.reverse_phi:
+                # the pixel is in S AFTER rotation. Thus we have to rotate BACK.
                 angle *= -1
             rotated_S = S.rotate_around_origin(axis, angle)
+
+            data = imageset.get_raw_data(i)[0]
+            if not self.ignore_mask:
+                mask = imageset.get_mask(i)[0]
+                data.set_selected(~mask, 0)
+
             recviewer.fill_voxels(
-                imageset.get_raw_data(i)[0],
-                self.grid,
-                self.cnts,
-                rotated_S,
-                xy,
-                rec_range,
+                data, self.grid, self.counts, rotated_S, xy, rec_range
             )
 
 
 if __name__ == "__main__":
-    with dials.util.show_mail_on_error():
+    with show_mail_on_error():
         script = Script()
         script.run()
