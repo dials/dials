d7cfb635286d3d64fbb3990db4c740d47a992ebb
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-10-19 09:29:56 +0100

    Remove unused code

--- algorithms/polygon/spatial_interpolation.py
+++ algorithms/polygon/spatial_interpolation.py
@@ -1,5 +1,3 @@
 from __future__ import absolute_import, division, print_function
 
-from dials_algorithms_polygon_ext import *
 from dials_algorithms_polygon_spatial_interpolation_ext import *
-from scitbx.array_family import *
--- array_family/shared.py
+++
@@ -1,3 +0,0 @@
-from __future__ import absolute_import, division, print_function
-
-from scitbx.array_family.shared import *
