426039827859155bd8402235ae433dddbc4dcade
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-10-28 08:15:46 +0000

    add sanity check

---
+++ test/test_scitbx.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import, division, print_function
+
+from scitbx.array_family import flex
+
+
+def test_complex_double_none_comparison():
+    """For unknown reasons this can return a flex.bool() object on some builds."""
+    assert (flex.complex_double() == None) is False  # noqa:E711
