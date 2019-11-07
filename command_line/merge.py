c40920ac6e24ef66bd03a4fc310d4a1178ce3c37
James Beilsten-Edmands <jbeilstenedmands@gmail.com>
2019-11-07 11:31:15 +0000

    Catch Merging stats error in dials.merge  Fixes #1002

--- command_line/merge.py
+++ command_line/merge.py
@@ -16,6 +16,7 @@ from dials.algorithms.merging.merge import (
     make_merged_mtz_file,
     merge_and_truncate,
 )
+from dials.algorithms.scaling.scaling_utilities import DialsMergingStatisticsError
 from libtbx import phil
 from six.moves import cStringIO as StringIO
 
@@ -161,7 +162,7 @@ Only scaled data can be processed with dials.merge"""
 
     try:
         mtz_file = merge_data_to_mtz(params, experiments, reflections)
-    except ValueError as e:
+    except (ValueError, DialsMergingStatisticsError) as e:
         raise Sorry(e)
 
     logger.info("\nWriting reflections to %s", (params.output.mtz))
