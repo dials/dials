d26f0288b961d3bf78c2ed4af203c4e3a719b102
Graeme Winter <graeme.winter@gmail.com>
2019-12-03 06:38:48 +0000

    Tests for trimming either end, verify scan OK

--- test/command_line/test_refine.py
+++ test/command_line/test_refine.py
@@ -249,3 +249,77 @@ def test_scan_varying_multi_scan_one_crystal(dials_data, tmpdir):
     ]
     for a, b in zip(history["rmsd"], expected_rmsds):
         assert a == pytest.approx(b, abs=1e-6)
+
+
+def test_trim_start(dials_regression, tmpdir):
+    """Trim the start of i04 bag training data"""
+    data_dir = os.path.join(dials_regression, "refinement_test_data", "i04_weak_data")
+    experiments_path = os.path.join(data_dir, "experiments.json")
+    pickle_path = os.path.join(data_dir, "indexed_strong.pickle")
+
+    expt_in = ExperimentListFactory.from_json_file(
+        experiments_path, check_format=False
+    )[0]
+
+    scan_in = expt_in.scan
+
+    data = flex.reflection_table.from_file(pickle_path)
+    keep = data["xyzcal.px"].parts()[2] > 100
+    data = data.select(keep)
+
+    refl_out = tmpdir.join("trimmed.refl").strpath
+    data.as_file(refl_out)
+
+    for pth in (experiments_path, pickle_path):
+        assert os.path.exists(pth)
+
+    cmd = ("dials.refine", experiments_path, refl_out)
+    result = procrunner.run(cmd, working_directory=tmpdir)
+    assert not result.returncode and not result.stderr
+
+    # verify scan
+
+    ref_exp = ExperimentListFactory.from_json_file(
+        tmpdir.join("refined.expt").strpath, check_format=False
+    )[0]
+
+    scan = ref_exp.scan
+
+    assert scan.get_image_range()[1] == scan_in.get_image_range()[1]
+
+
+def test_trim_end(dials_regression, tmpdir):
+    """Trim the end of i04 bag training data"""
+    data_dir = os.path.join(dials_regression, "refinement_test_data", "i04_weak_data")
+    experiments_path = os.path.join(data_dir, "experiments.json")
+    pickle_path = os.path.join(data_dir, "indexed_strong.pickle")
+
+    expt_in = ExperimentListFactory.from_json_file(
+        experiments_path, check_format=False
+    )[0]
+
+    scan_in = expt_in.scan
+
+    data = flex.reflection_table.from_file(pickle_path)
+    keep = data["xyzcal.px"].parts()[2] < 440
+    data = data.select(keep)
+
+    refl_out = tmpdir.join("trimmed.refl").strpath
+    data.as_file(refl_out)
+
+    for pth in (experiments_path, pickle_path):
+        assert os.path.exists(pth)
+
+    cmd = ("dials.refine", experiments_path, refl_out)
+    result = procrunner.run(cmd, working_directory=tmpdir)
+    assert not result.returncode and not result.stderr
+
+    # verify scan
+
+    ref_exp = ExperimentListFactory.from_json_file(
+        tmpdir.join("refined.expt").strpath, check_format=False
+    )[0]
+
+    scan = ref_exp.scan
+
+    assert scan.get_image_range()[0] == scan_in.get_image_range()[0]
