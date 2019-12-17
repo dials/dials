4b7195e689697b48148b145a8789403f0336045f
James Beilsten-Edmands <jbeilstenedmands@gmail.com>
2019-12-17 16:24:28 +0000

    Update dials.merge

--- command_line/merge.py
+++ command_line/merge.py
@@ -8,7 +8,7 @@ import logging
 import sys
 
 from dials.util import log, show_mail_on_error, Sorry
-from dials.util.options import OptionParser, flatten_reflections, flatten_experiments
+from dials.util.options import OptionParser, reflections_and_experiments_from_files
 from dials.util.version import dials_version
 from dials.util.export_mtz import match_wavelengths
 from dials.algorithms.merging.merge import (
@@ -126,8 +126,9 @@ def run(args=None):
         parser.print_help()
         sys.exit()
 
-    reflections = flatten_reflections(params.input.reflections)
-    experiments = flatten_experiments(params.input.experiments)
+    reflections, experiments = reflections_and_experiments_from_files(
+        params.input.reflections, params.input.experiments
+    )
 
     log.config(verbosity=options.verbose, logfile=params.output.log)
     logger.info(dials_version())
