50d6250e887b867c3e511e458e81d74c25c66a91
Markus Gerstel <2102431+Anthchirp@users.noreply.github.com>
2020-01-30 12:14:01 +0000

    Stream reflectiontable msgpack directly to file (#1115)
    The previous implementation held the reflection table up to 3 times in
    memory during the output phase. This implementation passes the python
    filehandle into C++ space and then writes the msgpack output directly
    to the file. As a result memory consumption is significantly reduced.

--- util/boost_python/ext.cc
+++ util/boost_python/ext.cc
@@ -3,8 +3,36 @@
 #include <dials/util/scale_down_array.h>
 #include <dials/util/masking.h>
 #include <dials/util/export_mtz_helpers.h>
+#include <dials/util/python_streambuf.h>
 
+std::size_t dials::util::streambuf::default_buffer_size = 1024;
 namespace dials { namespace util { namespace boost_python {
+  struct python_streambuf_wrapper {
+    typedef dials::util::streambuf wt;
+
+    static void wrap() {
+      using namespace boost::python;
+      class_<wt, boost::noncopyable>("streambuf", no_init)
+        .def(init<boost::python::object &, std::size_t>(
+          (arg("python_file_obj"), arg("buffer_size") = 0)))
+        .def_readwrite("default_buffer_size",
+                       wt::default_buffer_size,
+                       "The default size of the buffer sitting "
+                       "between a Python file object and a C++ stream.");
+    }
+  };
+
+  struct python_ostream_wrapper {
+    typedef dials::util::ostream wt;
+
+    static void wrap() {
+      using namespace boost::python;
+      class_<std::ostream, boost::noncopyable>("std_ostream", no_init);
+      class_<wt, boost::noncopyable, bases<std::ostream> >("ostream", no_init)
+        .def(init<boost::python::object &, std::size_t>(
+          (arg("python_file_obj"), arg("buffer_size") = 0)));
+    }
+  };
 
   using namespace boost::python;
   BOOST_PYTHON_MODULE(dials_util_ext) {
@@ -32,5 +60,8 @@ namespace dials { namespace util { namespace boost_python {
     class_<ResolutionMaskGenerator>("ResolutionMaskGenerator", no_init)
       .def(init<const BeamBase &, const Panel &>())
       .def("apply", &ResolutionMaskGenerator::apply);
+
+    python_streambuf_wrapper::wrap();
+    python_ostream_wrapper::wrap();
   }
 }}}  // namespace dials::util::boost_python
