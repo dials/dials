8b354d74f3dde5dc99d553f00a50994915476f5a
Richard Gildea <richard.gildea@diamond.ac.uk>
2020-02-03 18:13:43 +0000

    Fix plot functionality for multiple experiments

--- command_line/spot_counts_per_image.py
+++ command_line/spot_counts_per_image.py
@@ -85,7 +85,8 @@ def run(args):
     for s in all_stats:
         for attrib, value in s._asdict().items():
             summary_table[attrib].extend(value)
-    per_image_analysis.print_table(per_image_analysis.Stats(**summary_table))
+    stats = per_image_analysis.Stats(**summary_table)
+    per_image_analysis.print_table(stats)
 
     overall_stats = per_image_analysis.stats_for_reflection_table(
         reflections, resolution_analysis=params.resolution_analysis
