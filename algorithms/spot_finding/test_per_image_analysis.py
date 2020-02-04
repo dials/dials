8bbf3c779c9c4062eef1a7e0956ccd8987875b2f
Richard Gildea <richard.gildea@diamond.ac.uk>
2020-02-04 10:23:37 +0000

    Move loading of experiments/reflections to shared fixture

--- algorithms/spot_finding/test_per_image_analysis.py
+++ algorithms/spot_finding/test_per_image_analysis.py
@@ -7,7 +7,8 @@ from dials.algorithms.spot_finding import per_image_analysis
 from dials.array_family import flex
 
 
-def test_estimate_resolution_limit_distl_method1(dials_data, tmpdir):
+@pytest.fixture
+def centroid_test_data(dials_data):
     reflections = flex.reflection_table.from_file(
         dials_data("centroid_test_data").join("strong.pickle").strpath
     )
@@ -16,6 +17,11 @@ def test_estimate_resolution_limit_distl_method1(dials_data, tmpdir):
     )
     reflections.centroid_px_to_mm(experiments)
     reflections.map_centroids_to_reciprocal_space(experiments)
+    return experiments, reflections
+
+
+def test_estimate_resolution_limit_distl_method1(centroid_test_data, tmpdir):
+    experiments, reflections = centroid_test_data
     plot_file = tmpdir.join("distl1.png")
     d_min, noisiness = per_image_analysis.estimate_resolution_limit_distl_method1(
         reflections=reflections, plot_filename=plot_file.strpath
@@ -25,15 +31,8 @@ def test_estimate_resolution_limit_distl_method1(dials_data, tmpdir):
     assert plot_file.check()
 
 
-def test_estimate_resolution_limit_distl_method2(dials_data, tmpdir):
-    reflections = flex.reflection_table.from_file(
-        dials_data("centroid_test_data").join("strong.pickle").strpath
-    )
-    experiments = load.experiment_list(
-        dials_data("centroid_test_data").join("imported_experiments.json")
-    )
-    reflections.centroid_px_to_mm(experiments)
-    reflections.map_centroids_to_reciprocal_space(experiments)
+def test_estimate_resolution_limit_distl_method2(centroid_test_data, tmpdir):
+    experiments, reflections = centroid_test_data
     plot_file = tmpdir.join("distl2.png")
     d_min, noisiness = per_image_analysis.estimate_resolution_limit_distl_method2(
         reflections=reflections, plot_filename=plot_file.strpath
@@ -43,15 +42,8 @@ def test_estimate_resolution_limit_distl_method2(dials_data, tmpdir):
     assert plot_file.check()
 
 
-def test_estimate_resolution(dials_data, tmpdir):
-    reflections = flex.reflection_table.from_file(
-        dials_data("centroid_test_data").join("strong.pickle").strpath
-    )
-    experiments = load.experiment_list(
-        dials_data("centroid_test_data").join("imported_experiments.json")
-    )
-    reflections.centroid_px_to_mm(experiments)
-    reflections.map_centroids_to_reciprocal_space(experiments)
+def test_estimate_resolution(centroid_test_data, tmpdir):
+    experiments, reflections = centroid_test_data
     ice_sel = per_image_analysis.ice_rings_selection(reflections, width=0.004)
     assert ice_sel.count(True) == 76
     plot_file = tmpdir.join("i_over_sigi_vs_resolution.png")
