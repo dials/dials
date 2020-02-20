dials/dials

commit 7879db4c1f49f74d8315f979d088bf116bc168fa

Update search_beam_centre to use experiments (#1145)
- Replace imagesets with experiments. Since imagesets aren't relevant at to search_beam_position and mainly a legacy of its pre-experiment datablock-past, use experiments to pass around/access the experimental models directly.
- Extend test_search_small_molecule to check that the same detector (i.e. same fast/slow axes) is returned for each experiment. Previously all experiments were output with the same detector model, which was a bug.
- Use concurrent.futures instead of libtbx.easy_mp
- In tests call run method directly instead of via procrunner
- Fix n_macro_cycles option: ensure that the updated geometry is used for subsequent macro-cycles. Add test to assert that the shift in the final cycle is small.
- Move hardcoded dps phil params to lowest level

Fixes #1126, fixes #1127

test/command_line/test_search_beam_position.py
@@ -1,15 +1,22 @@
 from __future__ import absolute_import, division, print_function
 
+import mock
 import os
-
 import procrunner
 import pytest
+import py.path
+import sys
 import scitbx
+from cctbx import uctbx
 
 from dxtbx.serialize import load
 
+import dials.command_line.dials_import
+from dials.algorithms.indexing.test_index import run_indexing
+from dials.command_line import search_beam_position
 
-def test_search_i04_weak_data_image_range(run_in_tmpdir, dials_regression):
+
+def test_search_i04_weak_data_image_range(mocker, run_in_tmpdir, dials_regression):
     """Perform a beam-centre search and check that the output is sane."""
 
     data_dir = os.path.join(dials_regression, "indexing_test_data", "i04_weak_data")
@@ -17,24 +24,30 @@ def test_search_i04_weak_data_image_range(run_in_tmpdir, dials_regression):
     experiments_file = os.path.join(data_dir, "experiments_import.json")
 
     args = [
-        "dials.search_beam_position",
         experiments_file,
         reflection_file,
         "image_range=1,10",
         "image_range=251,260",
         "image_range=531,540",
+        "n_macro_cycles=2",
     ]
+    from rstbx.indexing_api import dps_extended
 
-    print(args)
-    result = procrunner.run(args)
-    assert not result.returncode and not result.stderr
+    mocker.spy(dps_extended, "get_new_detector")
+    search_beam_position.run(args)
+    # Check that the last call to get_new_detector was with an offset of close to zero.
+    # The final call was to apply the "best" shift to the detector model before
+    # returning the updated experiments.
+    assert dps_extended.get_new_detector.call_args[0][1].elems == pytest.approx(
+        (0, 0, 0), abs=3e-2
+    )
     assert os.path.exists("optimised.expt")
 
+    # Compare the shifts between the start and final detector models
     experiments = load.experiment_list(experiments_file, check_format=False)
-    original_imageset = experiments.imagesets()[0]
-    optimized_experiments = load.experiment_list("optimised.expt", check_format=False)
-    detector_1 = original_imageset.get_detector()
-    detector_2 = optimized_experiments.detectors()[0]
+    optimised_experiments = load.experiment_list("optimised.expt", check_format=False)
+    detector_1 = experiments[0].detector
+    detector_2 = optimised_experiments[0].detector
     shift = scitbx.matrix.col(detector_1[0].get_origin()) - scitbx.matrix.col(
         detector_2[0].get_origin()
     )
@@ -59,31 +72,21 @@ def test_search_multiple(run_in_tmpdir, dials_regression):
     experiments_path1 = os.path.join(data_dir, "datablock_P1_X6_1.json")
     experiments_path2 = os.path.join(data_dir, "datablock_P1_X6_2.json")
 
-    args = [
-        "dials.search_beam_position",
-        experiments_path1,
-        experiments_path2,
-        pickle_path1,
-        pickle_path2,
-    ]
-
-    print(args)
-    result = procrunner.run(args)
-    assert not result.returncode and not result.stderr
+    args = [experiments_path1, experiments_path2, pickle_path1, pickle_path2]
+    search_beam_position.run(args)
     assert os.path.exists("optimised.expt")
 
     experiments = load.experiment_list(experiments_path1, check_format=False)
-    original_imageset = experiments.imagesets()[0]
-    optimized_experiments = load.experiment_list("optimised.expt", check_format=False)
-    detector_1 = original_imageset.get_detector()
-    detector_2 = optimized_experiments.detectors()[0]
+    optimised_experiments = load.experiment_list("optimised.expt", check_format=False)
+    detector_1 = experiments[0].detector
+    detector_2 = optimised_experiments[0].detector
     shift = scitbx.matrix.col(detector_1[0].get_origin()) - scitbx.matrix.col(
         detector_2[0].get_origin()
     )
     assert shift.elems == pytest.approx((0.037, 0.061, 0.0), abs=1e-1)
 
 
-def test_index_after_search(dials_data, tmpdir):
+def test_index_after_search(dials_data, run_in_tmpdir):
     """Integrate the beam centre search with the rest of the toolchain
 
     Do the following:
@@ -102,21 +105,15 @@ def test_index_after_search(dials_data, tmpdir):
     args = ["dials.import", "mosflm_beam_centre=207,212"] + g
     print(args)
     if os.name != "nt":
-        result = procrunner.run(args, working_directory=tmpdir)
+        result = procrunner.run(args)
         assert not result.returncode and not result.stderr
     else:
         # Can't run this command on Windows,
         # as it will exceed the maximum Windows command length limits.
         # So, instead:
-        import mock
-        import sys
-
-        with tmpdir.as_cwd():
-            with mock.patch.object(sys, "argv", args):
-                import dials.command_line.dials_import
-
-                dials.command_line.dials_import.Script().run()
-    assert tmpdir.join("imported.expt").check()
+        with mock.patch.object(sys, "argv", args):
+            dials.command_line.dials_import.Script().run()
+    assert os.path.exists("imported.expt")
 
     # spot-finding, just need a subset of the data
     args = [
@@ -126,27 +123,18 @@ def test_index_after_search(dials_data, tmpdir):
         "scan_range=531,540",
     ]
     print(args)
-    result = procrunner.run(args, working_directory=tmpdir)
+    result = procrunner.run(args)
     assert not result.returncode and not result.stderr
-    assert tmpdir.join("strong.refl").check()
+    assert os.path.exists("strong.refl")
 
     # actually run the beam centre search
-    args = ["dials.search_beam_position", "imported.expt", "strong.refl"]
-    print(args)
-    result = procrunner.run(args, working_directory=tmpdir)
-    assert not result.returncode and not result.stderr
-    assert tmpdir.join("optimised.expt").check()
+    search_beam_position.run(["imported.expt", "strong.refl"])
+    assert os.path.exists("optimised.expt")
 
     # look at the results
-    from dxtbx.serialize import load
-
-    experiments = load.experiment_list(
-        tmpdir.join("imported.expt").strpath, check_format=False
-    )
+    experiments = load.experiment_list("imported.expt", check_format=False)
     original_imageset = experiments.imagesets()[0]
-    optimized_experiments = load.experiment_list(
-        tmpdir.join("optimised.expt").strpath, check_format=False
-    )
+    optimized_experiments = load.experiment_list("optimised.expt", check_format=False)
     detector_1 = original_imageset.get_detector()
     detector_2 = optimized_experiments.detectors()[0]
     shift = scitbx.matrix.col(detector_1[0].get_origin()) - scitbx.matrix.col(
@@ -155,9 +143,6 @@ def test_index_after_search(dials_data, tmpdir):
     print(shift)
 
     # check we can actually index the resulting optimized experiments
-    from cctbx import uctbx
-    from dials.algorithms.indexing.test_index import run_indexing
-
     expected_unit_cell = uctbx.unit_cell(
         (57.780, 57.800, 150.017, 89.991, 89.990, 90.007)
     )
@@ -166,7 +151,7 @@ def test_index_after_search(dials_data, tmpdir):
     run_indexing(
         "strong.refl",
         "optimised.expt",
-        tmpdir,
+        py.path.local(os.path.curdir),
         [],
         expected_unit_cell,
         expected_rmsds,
@@ -190,13 +175,8 @@ def test_search_single(run_in_tmpdir, dials_regression):
     pickle_path = os.path.join(data_dir, "strong.pickle")
     experiments_path = os.path.join(data_dir, "datablock.json")
 
-    args = ["dials.search_beam_position", experiments_path, pickle_path]
-    print(args)
-    result = procrunner.run(args)
-    assert not result.returncode and not result.stderr
+    search_beam_position.run([experiments_path, pickle_path])
     assert os.path.exists("optimised.expt")
-
-    from dxtbx.serialize import load
 
     experiments = load.experiment_list(experiments_path, check_format=False)
     original_imageset = experiments.imagesets()[0]
@@ -223,24 +203,24 @@ def test_search_small_molecule(dials_data, run_in_tmpdir):
     """
 
     data = dials_data("l_cysteine_dials_output")
-    datablock_path = data.join("datablock.json").strpath
-    pickle_path = data.join("strong.pickle").strpath
+    experiments_path = data.join("imported.expt").strpath
+    refl_path = data.join("strong.refl").strpath
 
-    args = ["dials.search_beam_position", datablock_path, pickle_path]
-    print(args)
-    result = procrunner.run(args)
-    assert not result.returncode and not result.stderr
+    search_beam_position.run([experiments_path, refl_path])
     assert os.path.exists("optimised.expt")
 
-    from dxtbx.serialize import load
-
-    datablocks = load.datablock(datablock_path, check_format=False)
-    original_imageset = datablocks[0].extract_imagesets()[0]
-    detector_1 = original_imageset.get_detector()
-    optimized_experiments = load.experiment_list("optimised.expt", check_format=False)
-    detector_2 = optimized_experiments[0].detector
-    shift = scitbx.matrix.col(detector_1[0].get_origin()) - scitbx.matrix.col(
-        detector_2[0].get_origin()
-    )
-    print(shift)
-    assert shift.elems == pytest.approx((0.11, -1.03, 0.0), abs=1e-1)
+    experiments = load.experiment_list(experiments_path, check_format=False)
+    optimised_experiments = load.experiment_list("optimised.expt", check_format=False)
+    for old_expt, new_expt in zip(experiments, optimised_experiments):
+        # assert that the detector fast/slow axes are unchanged from the input experiments
+        # the last experiment actually does have a different detector model
+        assert (
+            old_expt.detector[0].get_slow_axis() == new_expt.detector[0].get_slow_axis()
+        )
+        assert (
+            old_expt.detector[0].get_fast_axis() == new_expt.detector[0].get_fast_axis()
+        )
+        shift = scitbx.matrix.col(
+            old_expt.detector[0].get_origin()
+        ) - scitbx.matrix.col(new_expt.detector[0].get_origin())
+        assert shift.elems == pytest.approx((-0.292, -1.063, 0.0), abs=1e-2)

commit 8ce7d8e3771e01905b16aa07083afeeaaf8845d5

Fix beam centre search for non-identify fixed rotation
Original fix was broken by 7879db4 and then removed entirely by 563c345.
This time add a test to try to ensure that it stays fixed .

Fixes #1164

command_line/search_beam_position.py
@@ -267,8 +267,9 @@ def _get_origin_offset_score(
 
     # Key point for this is that the spots must correspond to detector
     # positions not to the correct RS position => reset any fixed rotation
-    # to identity - copy in case called from elsewhere
+    # to identity
 
+    experiment.goniometer.set_fixed_rotation((1, 0, 0, 0, 1, 0, 0, 0, 1))
     spots_mm.map_centroids_to_reciprocal_space([experiment])
     return _sum_score_detail(spots_mm["rlp"], solutions, amax=amax)
 

test/command_line/test_search_beam_position.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, division, print_function
 
+import glob
 import mock
 import os
 import procrunner
@@ -10,6 +11,7 @@ import scitbx
 from cctbx import uctbx
 
 from dxtbx.serialize import load
+from dxtbx.model import ExperimentList
 
 import dials.command_line.dials_import
 from dials.algorithms.indexing.test_index import run_indexing
@@ -224,3 +226,30 @@ def test_search_small_molecule(dials_data, run_in_tmpdir):
             old_expt.detector[0].get_origin()
         ) - scitbx.matrix.col(new_expt.detector[0].get_origin())
         assert shift.elems == pytest.approx((-0.292, -1.063, 0.0), abs=1e-2)
+
+
+def test_multi_sweep_fixed_rotation(dials_regression):
+    data_dir = os.path.join(dials_regression, "indexing_test_data", "multi_sweep")
+    reflection_files = sorted(
+        glob.glob(os.path.join(data_dir, "SWEEP[1,2]", "index", "*_strong.pickle"))
+    )
+    experiment_files = sorted(
+        glob.glob(
+            os.path.join(data_dir, "SWEEP[1,2]", "index", "*_datablock_import.json")
+        )
+    )
+
+    search_beam_position.run(reflection_files + experiment_files)
+    assert os.path.exists("optimised.expt")
+
+    experiments = ExperimentList()
+    for path in experiment_files:
+        experiments.extend(load.experiment_list(path, check_format=False))
+
+    optimised_experiments = load.experiment_list("optimised.expt", check_format=False)
+    for orig_expt, new_expt in zip(experiments, optimised_experiments):
+        shift = scitbx.matrix.col(
+            orig_expt.detector[0].get_origin()
+        ) - scitbx.matrix.col(new_expt.detector[0].get_origin())
+        print(shift)
+        assert shift.elems == pytest.approx((2.310, -0.365, 0), abs=1e-2)