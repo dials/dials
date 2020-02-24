094720091567c0717b2565ed56a2131f74aecb1e
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-02-24 16:11:36 +0000

    ensure exception decorator can handle anything
    including fancy 'x' strings

---
+++ test/util/test_exception_handler.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import dials.util
+
+import pytest
+import six
+
+
+class _SomeError(RuntimeError):
+    pass
+
+
+def test_crash_with_plain_text():
+    with pytest.raises(_SomeError) as exc:
+        with dials.util.show_mail_on_error():
+            raise _SomeError("This is a string")
+    assert "report this error" in str(exc.value)
+    assert "This is a string" in str(exc.value)
+
+
+def test_crash_with_unicode():
+    with pytest.raises(_SomeError) as exc:
+        with dials.util.show_mail_on_error():
+            raise _SomeError(u"This is a 👹♔  𝓕คｎｃ¥ ｕηเᶜόＤ𝔼  🏆🍔 string")
+    assert u"report this error" in six.text_type(exc.value)
+    assert u"This is a" in six.text_type(exc.value)
+
+
+def test_crash_with_bytestring():
+    with pytest.raises(_SomeError) as exc:
+        with dials.util.show_mail_on_error():
+            raise _SomeError(b"This is a byte string")
+    assert "report this error" in repr(exc.value)
+    assert "This is a" in repr(exc.value)
