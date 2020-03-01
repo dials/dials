bf06a92af100ff8d830963ea7d6c3c58cd293f0a
DiamondLightSource-build-server <DiamondLightSource-build-server@users.noreply.github.com>
2020-03-01 05:10:54 +0000

    Clean clutter

--- test/command_line/test_stills_process.py
+++ test/command_line/test_stills_process.py
@@ -125,7 +125,13 @@ def test_sacla_h5(dials_regression, run_in_tmpdir, use_mpi, in_memory=False):
 
     # Call dials.stills_process
     if use_mpi:
-        command = ["mpirun", "-n", "4", "dials.stills_process", "mp.method=mpi mp.composite_stride=4"]
+        command = [
+            "mpirun",
+            "-n",
+            "4",
+            "dials.stills_process",
+            "mp.method=mpi mp.composite_stride=4",
+        ]
     else:
         command = ["dials.stills_process"]
     command += [image_path, "process_sacla.phil"]
