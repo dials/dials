0ffe6f85d08c0907eaa906bbdf1e32af10f17cd5
Graeme Winter <graeme.winter@gmail.com>
2020-03-06 16:12:07 +0000

    auto-format

--- algorithms/event_mode/boost_python/event_mode_ext.cc
+++ algorithms/event_mode/boost_python/event_mode_ext.cc
@@ -10,57 +10,52 @@
 #include <cctype>
 #include <boost/random.hpp>
 
-namespace dials { 
-  namespace algorithms { 
-    namespace event_mode {
+namespace dials { namespace algorithms { namespace event_mode {
 
-      // Create event list
+  // Create event list
 
-      static boost::python::tuple event_list(int image_n,
-					     scitbx::af::shared<size_t> idx, scitbx::af::shared<int> counts)
-      {
-	scitbx::af::shared<int> position, time;
-	boost::random::mt19937 rng;
-	boost::random::uniform_real_distribution<double> dist(0, 1);
-	for (size_t j = 0; j < idx.size(); j++) {  
-	  for (size_t c = 0; c < counts[j]; c++) {
-	    position.push_back(idx[j]);
-            time.push_back(1000*(image_n + dist(rng)));
-	  }
-	}
-	return boost::python::make_tuple(position, time);
-      }
-
-      // Get image from event list
-
-      static scitbx::af::shared<double> image_coord(int n, int expT,
-						    scitbx::af::shared<int> pos, scitbx::af::shared<int> t)
-      {
-	scitbx::af::shared<double> coord;
-	int t_i = expT*n;
-	int t_f = expT*(n+1);
-	//scitbx::af::shared<bool> deltaT;
-	//scitbx::af::shared<size_t> idx;
-	for (size_t j = 0; j < t.size(); j++) {
-          if ((t[j] >= t_i) && (t[j] < t_f)) {
-	    coord.push_back(pos[j]);
-          }
-	}
-                
-	return coord;
-      }
-
-      void init_module()
-      {
-	using namespace boost::python;
-	def("event_list", event_list, (arg("image_n"), arg("idx"), arg("counts")));
-	def("image_coord", image_coord, (arg("n"), arg("expT"), arg("pos"), arg("t")));
+  static boost::python::tuple event_list(int image_n,
+                                         scitbx::af::shared<size_t> idx,
+                                         scitbx::af::shared<int> counts) {
+    scitbx::af::shared<int> position, time;
+    boost::random::mt19937 rng;
+    boost::random::uniform_real_distribution<double> dist(0, 1);
+    for (size_t j = 0; j < idx.size(); j++) {
+      for (size_t c = 0; c < counts[j]; c++) {
+        position.push_back(idx[j]);
+        time.push_back(1000 * (image_n + dist(rng)));
       }
     }
+    return boost::python::make_tuple(position, time);
   }
-} // namespace dials::algorithms::event_mode
 
-BOOST_PYTHON_MODULE(dials_algorithms_event_mode_ext)
-{
+  // Get image from event list
+
+  static scitbx::af::shared<double> image_coord(int n,
+                                                int expT,
+                                                scitbx::af::shared<int> pos,
+                                                scitbx::af::shared<int> t) {
+    scitbx::af::shared<double> coord;
+    int t_i = expT * n;
+    int t_f = expT * (n + 1);
+    // scitbx::af::shared<bool> deltaT;
+    // scitbx::af::shared<size_t> idx;
+    for (size_t j = 0; j < t.size(); j++) {
+      if ((t[j] >= t_i) && (t[j] < t_f)) {
+        coord.push_back(pos[j]);
+      }
+    }
+
+    return coord;
+  }
+
+  void init_module() {
+    using namespace boost::python;
+    def("event_list", event_list, (arg("image_n"), arg("idx"), arg("counts")));
+    def("image_coord", image_coord, (arg("n"), arg("expT"), arg("pos"), arg("t")));
+  }
+}}}  // namespace dials::algorithms::event_mode
+
+BOOST_PYTHON_MODULE(dials_algorithms_event_mode_ext) {
   dials::algorithms::event_mode::init_module();
 }
