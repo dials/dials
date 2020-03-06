e28cbf62293370601f1fa68e62a3c6d94b78ac24
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-03-06 09:28:36 +0000

    add python version to output

--- command_line/version.py
+++ command_line/version.py
@@ -5,8 +5,10 @@ def version():
     from dials.util.version import dials_version
     import dials
     import os
+    import sys
 
     print(dials_version())
+    print("Python {0.major}.{0.minor}.{0.micro}".format(sys.version_info))
     print("Installed in: %s" % os.path.split(dials.__file__)[0])
 
 
