2d515a3340b7ce39a7f0e21897c5c7aae5962e81
Aaron Brewster <asbrewster@lbl.gov>
2020-03-10 09:16:14 -0700

    Code review

--- util/image_viewer/slip_viewer/rotate_detector.py
+++ util/image_viewer/slip_viewer/rotate_detector.py
@@ -1,13 +1,13 @@
 from __future__ import division, print_function
 import sys
-from dxtbx.model.experiment_list import ExperimentListFactory
+from dxtbx.model.experiment_list import ExperimentList
 from scitbx.matrix import col
 
 """
 Example script for rotating a detector in an experiment.json
 """
 
-experiments = ExperimentListFactory.from_json_file(sys.argv[1])
+experiments = ExperimentList.from_file(sys.argv[1])
 print("Original detector")
 detector = experiments[0].detector
 print(detector)
@@ -18,7 +18,7 @@ fast = col(h.get_fast_axis())
 slow = col(h.get_slow_axis())
 origin = col(h.get_origin())
 
-normal = fast.cross(slow)
+normal = col(h.get_normal())
 rotation = normal.axis_and_angle_as_r3_rotation_matrix(90, deg=True)
 
 h.set_frame(rotation * fast, rotation * slow, rotation * origin)
