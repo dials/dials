fdf98e0002e812471995019d87f41bdd8018c81f
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-03-15 12:43:07 +0000

    Prepare branch for DIALS 2.2 release

--- installer/bootstrap.py
+++ installer/bootstrap.py
@@ -793,31 +793,33 @@ MODULES = {}
 for module in (
     "cctbx/annlib_adaptbx",
     "cctbx/boost",
-    "cctbx/cctbx_project",
-    "cctbx/dxtbx",
     "xia2/xia2",
 ):
     modulename = module.split("/")[1]
     MODULES[modulename] = [
+        "-b dials-2.2",
         "git@github.com:%s.git" % module,
         "https://github.com/%s.git" % module,
-        "https://github.com/%s/archive/master.zip" % module,
+        "https://github.com/%s/archive/dials-2.2.zip" % module,
     ]
 for module in (
     "annlib",
     "cbflib",
     "ccp4io",
     "ccp4io_adaptbx",
+    "cctbx_project",
     "clipper",
     "dials",
+    "dxtbx",
     "eigen",
     "gui_resources",
     "tntbx",
 ):
     MODULES[module] = [
+        "-b dials-2.2",
         "git@github.com:dials/%s.git" % module,
         "https://github.com/dials/%s.git" % module,
-        "https://github.com/dials/%s/archive/master.zip" % module,
+        "https://github.com/dials/%s/archive/dials-2.2.zip" % module,
     ]
 
 
