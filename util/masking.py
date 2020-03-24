10fd633b1237f83271f8c6a1bdc1f6faa980acf2
David Waterman <dgwaterman@gmail.com>
2020-03-24 18:46:21 +0000

    Convert exception to logger.warning

--- util/masking.py
+++ util/masking.py
@@ -214,7 +214,7 @@ class MaskGenerator(object):
             # the trusted range. This identifies bad pixels, but does not include
             # pixels that are overloaded on some images.
             if self.params.use_trusted_range:
-                raise DeprecationWarning(
+                logger.warn(
                     "Checking for hot pixels using the trusted_range is deprecated. https://github.com/dials/dials/issues/1156"
                 )
                 trusted_mask = None
