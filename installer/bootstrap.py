adc74ed395693983697312452a5ccc5df15f6746
David Waterman <dgwaterman@gmail.com>
2020-03-25 00:37:00 +0000

    OSError is too broad. Revert to None, but maybe be problematic if ssl.SSLError is not available
    This reverts commit c32cbb0a04fb48dac78ac1954425f5d4d48335ce.

--- installer/bootstrap.py
+++ installer/bootstrap.py
@@ -395,7 +395,7 @@ def download_to_file(url, file, quiet=False, cache=True):
     try:
         from ssl import SSLError
     except ImportError:
-        SSLError = OSError
+        SSLError = None
 
     # Open connection to remote server
     try:
