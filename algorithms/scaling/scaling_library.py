7d9fafab3a98f72b82aff50ab1b31135767a0f85
DiamondLightSource-build-server <DiamondLightSource-build-server@users.noreply.github.com>
2020-04-12 05:10:47 +0100

    Clean clutter

--- algorithms/scaling/scaling_library.py
+++ algorithms/scaling/scaling_library.py
@@ -381,7 +381,9 @@ def determine_best_unit_cell(experiments):
     experiments."""
     uc_params = [flex.double() for i in range(6)]
     for exp in experiments:
-        unit_cell = exp.crystal.get_recalculated_unit_cell() or exp.crystal.get_unit_cell()
+        unit_cell = (
+            exp.crystal.get_recalculated_unit_cell() or exp.crystal.get_unit_cell()
+        )
         for i, p in enumerate(unit_cell.parameters()):
             uc_params[i].append(p)
     best_unit_cell = uctbx.unit_cell(parameters=[flex.median(p) for p in uc_params])
