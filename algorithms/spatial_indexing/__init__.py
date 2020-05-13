5d60ec676097425184e1750e450dc97b8ca83833
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-05-13 10:50:59 +0100

    Remove unused spatial indexing extension

--- algorithms/spatial_indexing/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import, division, print_function
-
-from dials_algorithms_spatial_indexing_ext import *  # noqa: F403; lgtm
-
-__all__ = ("make_spatial_index",)  # noqa: F405
