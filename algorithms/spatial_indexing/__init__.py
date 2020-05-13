d54333eed247f444d28519d9b8b952b358655a8c
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-05-13 10:41:17 +0100

    Remove unused octree/quadtree code
    and related tests that failed in #1258

--- algorithms/spatial_indexing/__init__.py
+++ algorithms/spatial_indexing/__init__.py
@@ -2,10 +2,4 @@ from __future__ import absolute_import, division, print_function
 
 from dials_algorithms_spatial_indexing_ext import *  # noqa: F403; lgtm
 
-__all__ = (  # noqa: F405
-    "OctreeVec3Double",
-    "OctreeVec3int",
-    "QuadtreeVec2Double",
-    "QuadtreeVec2int",
-    "make_spatial_index",
-)
+__all__ = ("make_spatial_index",)  # noqa: F405
