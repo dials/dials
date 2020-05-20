7ff21bbedf876e1cfac255809f91cbd13f7fbe18
Graeme Winter <graeme.winter@gmail.com>
2020-05-20 14:30:01 +0100

    First cut of error assessment script
    Demonstrates use of filter_reflections which is also critical

--- src/dials/command_line/assess_error_models.py
+++ src/dials/command_line/assess_error_models.py
@@ -4,12 +4,22 @@ from dials.util.command_line import OptionParser
 from dials.algorithms.indexing.compare_orientation_matrices import (
     difference_rotation_matrix_axis_angle,
 )
+from dials.util.filter_reflections import filter_reflection_table
+from dials.array_family import flex
 from libtbx.phil import parse
+import math
 
 master_scope = parse(
     """
 check_format = False
   .type = bool
+d_min = None
+  .type = float
+  .help = "High resolution limit for analysis"
+d_max = None
+  .type = float
+  .help = "Low resolution limit for analysis"
+
 """
 )
 
@@ -19,7 +29,8 @@ def reflections_to_keys(reflections):
         p.iround() for p in reflections["miller_index"].as_vec3_double().parts()
     )
     e = reflections["entering"]
-    return list(zip(*hkl + (e,)))
+    c = flex.floor(reflections["xyzcal.mm"].parts()[2] / (math.pi)).iround()
+    return list(zip(*(hkl + (e, c))))
 
 
 def assess_error_model(experiments, input_reflections):
@@ -31,8 +42,12 @@ def assess_error_model(experiments, input_reflections):
     reflections = []
 
     for j, expt in enumerate(experiments):
-        refl = input_reflections[j]
-        refl = refl.select(refl.get_flags(refl.flags.integrated))
+        refl = filter_reflection_table(
+            input_reflections[j],
+            intensity_choice=["scale"],
+            partiality_threshold=0.99,
+            combine_partials=True,
+        )
         c = expt.crystal
         cb_op = difference_rotation_matrix_axis_angle(c, c0)[-1]
         if str(cb_op) == "a,b,c":
@@ -42,20 +57,34 @@ def assess_error_model(experiments, input_reflections):
         refl["miller_index"] = cb_op.apply(refl["miller_index"])
         reflections.append(refl)
 
-    r0 = reflections[0]
-    for refl in reflections[1:]:
-        m = r0.match(refl)
-        r0 = r0.select(m[0])
+    nn = len(reflections)
+    keys = [reflections_to_keys(refl) for refl in reflections]
 
-    print(f"{r0.size()} reflections found to be common")
+    # check keys unique in each list
+    for k in keys:
+        assert len(set(k)) == len(k)
 
-    for refl in reflections:
-        m = r0.match(refl)
+    # find the set of common keys
+    k0 = set(keys[0])
+    for k in keys[1:]:
+        k0 = k0.intersection(set(k))
 
-    matched = [refl.select(r0.match(refl)[1]) for refl in reflections]
+    print(f"{len(k0)} reflections found to be common")
 
-    for m in matched:
-        print(m["miller_index"][100])
+    # put all of the reflection lists into a consistent sort order - really
+    # slowly...
+    k0 = sorted(k0)
+    for j, k in enumerate(keys):
+        sel = flex.size_t([k.index(_) for _ in k0])
+        reflections[j] = reflections[j].select(sel)
+
+    for n, _ in enumerate(k0):
+        obs = [reflections[j][n]["intensity.scale.value"] for j in range(nn)]
+        mean = sum(obs) / len(obs)
+        var = sum((o - mean) * (o - mean) for o in obs) * (1.0 / (nn - 1))
+        vars = [reflections[j][n]["intensity.scale.variance"] for j in range(nn)]
+        mvar = sum(vars) / len(vars)
+        print(mean, mvar, var)
 
 
 def main():
@@ -63,10 +92,20 @@ def main():
 
     print(op)
 
+    params = op.params()
+
     experiments = op.read_experiments(
-        op.input_experiments(), check_format=op.params().check_format
+        op.input_experiments(), check_format=params.check_format
     )
+
     reflections = op.read_reflections(op.input_reflections())
+
+    if params.d_min is not None:
+        reflections = [refl.select(refl["d"] >= params.d_min) for refl in reflections]
+
+    if params.d_max is not None:
+        reflections = [refl.select(refl["d"] <= params.d_max) for refl in reflections]
+
     assess_error_model(experiments, reflections)
 
 
