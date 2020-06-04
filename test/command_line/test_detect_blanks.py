5d9381abeca0b693236dfb822c6b6dc82b5e167f
Richard Gildea <richard.gildea@diamond.ac.uk>
2020-06-04 15:24:07 +0100

    Add test for stills raises sys.exit

--- test/command_line/test_detect_blanks.py
+++ test/command_line/test_detect_blanks.py
@@ -1,4 +1,7 @@
 import json
+import procrunner
+import pytest
+
 from dials.array_family import flex
 from dials.command_line import detect_blanks
 
@@ -10,12 +13,10 @@ def test_strong(dials_data, capsys, run_in_tmpdir):
     z = refl["xyzobs.px.value"].parts()[2]
     refl_subset = refl.select((z < 10) | (z > 20))
     refl_subset.as_file("mod.refl")
-    detect_blanks.run(
-        [expts_file.strpath, "mod.refl", "output.json=detect_blanks.json"]
-    )
+    detect_blanks.run([expts_file.strpath, "mod.refl"])
     captured = capsys.readouterr()
     assert "Potential blank images: 11 -> 21" in captured.out
-    json_file = run_in_tmpdir / "detect_blanks.json"
+    json_file = run_in_tmpdir / "blanks.json"
     assert json_file.check(file=1)
     with json_file.open() as fh:
         d = json.load(fh)
@@ -58,3 +59,25 @@ Potential blank images: 41 -> 45
         assert d["strong"]["blank_regions"] == [[40, 45]]
         assert d["indexed"]["blank_regions"] == [[40, 45]]
         assert d["integrated"]["blank_regions"] == [[0, 9], [40, 45]]
+
+
+def test_still_raises_sysexit(dials_data, run_in_tmpdir):
+    path = dials_data("thaumatin_grid_scan")
+
+    # Import the data
+    result = procrunner.run(
+        ["dials.import", "output.experiments=imported.expt"] + path.listdir("*.cbf*")
+    )
+    assert not result.returncode and not result.stderr
+    expts_file = run_in_tmpdir.join("imported.expt")
+    assert expts_file.check(file=1)
+
+    # Find the spots
+    result = procrunner.run(["dials.find_spots", "imported.expt", "min_spot_size=3"],)
+    assert not result.returncode and not result.stderr
+    refl_file = run_in_tmpdir.join("strong.refl")
+    assert refl_file.check(file=1)
+
+    # Now test that passing stills raises a sys.exit
+    with pytest.raises(SystemExit):
+        detect_blanks.run([expts_file.strpath, refl_file.strpath])
