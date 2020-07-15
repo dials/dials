daf580996210b562d78264db1e74a698d9937c40
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-07-15 09:52:04 +0100

    Make anyone importing DIALS aware of !2.7 support
    Warning is only shown on first import, and can be silenced in Python 2.7 with
    
    import warnings
    with warnings.catch_warnings():
        warnings.simplefilter("ignore")
        import dials
    
    cf. #1175

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ if sys.version_info.major == 2:
     warnings.warn(
         "Python 2 is no longer fully supported. Please consider using the DIALS 2.2 release branch. "
         "For more information on Python 2.7 support please go to https://github.com/dials/dials/issues/1175.",
-        DeprecationWarning,
+        UserWarning,
     )
 
 logging.getLogger("dials").addHandler(logging.NullHandler())
