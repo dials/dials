37d56a1f6f06f7604452f1773fa884b1dfcc6514
Richard Gildea <richard.gildea@diamond.ac.uk>
2020-08-14 15:32:51 +0100

    Don't override default nbins for resolution analysis
    20 bins is insufficient if the true resolution limit of the data
    is much lower than the resolution limit to which data were integrated.
    Better to stick with the default nbins=100.

--- algorithms/symmetry/__init__.py
+++ algorithms/symmetry/__init__.py
@@ -348,7 +348,6 @@ class symmetry_base(object):
 def resolution_filter_from_array(intensities, min_i_mean_over_sigma_mean, min_cc_half):
     """Run the resolution filter using miller array data format."""
     rparams = resolution_analysis.phil_defaults.extract().resolution
-    rparams.nbins = 20
     resolutionizer = resolution_analysis.Resolutionizer(intensities, rparams)
     return _resolution_filter(resolutionizer, min_i_mean_over_sigma_mean, min_cc_half)
 
@@ -358,7 +357,6 @@ def resolution_filter_from_reflections_experiments(
 ):
     """Run the resolution filter using native dials data formats."""
     rparams = resolution_analysis.phil_defaults.extract().resolution
-    rparams.nbins = 20
     resolutionizer = resolution_analysis.Resolutionizer.from_reflections_and_experiments(
         reflections, experiments, rparams
     )
