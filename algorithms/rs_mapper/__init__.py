91bec7aef8e7a7eeb40e2b696e1a8b804d5a75fa
Nicholas Devenish <ndevenish@gmail.com>
2020-08-20 16:04:19 +0100

    Update boost.* → boost_adaptbx.boost.* imports (#1379)
    This is to reflect upstream changes added in a series of commits merged
    in pull cctbx/cctbx_project#516. The changes were made with:
    
    find . -type f -name "*.py" -print0 | xargs -0 sed -i '' 's/boost\.python/boost_adaptbx.boost.python/g'
    find . -type f -name "*.py" -print0 | xargs -0 sed -i '' 's/boost\.optional/boost_adaptbx.boost.optional/g'
    find . -type f -name "*.py" -print0 | xargs -0 sed -i '' 's/boost\.std_pair/boost_adaptbx.boost.std_pair/g'
    find . -type f -name "*.py" -print0 | xargs -0 sed -i '' 's/boost\.rational/boost_adaptbx.boost.rational/g'
    find . -type f -name "*.py" -print0 | xargs -0 sed -i '' 's/boost\.tuple/boost_adaptbx.boost.tuple/g'
    find . -type f -name "*.py" -print0 | xargs -0 sed -i '' 's/from boost import/from boost_adaptbx.boost import/g'

--- algorithms/rs_mapper/__init__.py
+++ algorithms/rs_mapper/__init__.py
@@ -1,11 +1,11 @@
 from __future__ import absolute_import, division, print_function
 
 try:
-    import boost.python
+    import boost_adaptbx.boost.python
 except Exception:
     ext = None
 else:
-    ext = boost.python.import_ext("recviewer_ext", optional=False)
+    ext = boost_adaptbx.boost.python.import_ext("recviewer_ext", optional=False)
 
 if ext is not None:
     from recviewer_ext import *
