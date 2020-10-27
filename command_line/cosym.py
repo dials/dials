6743e86c2cedb9b55bbac0fa61a9253636711525
Richard Gildea <richard.gildea@diamond.ac.uk>
2020-10-27 09:42:18 +0000

    Minor improvement to log message

--- command_line/cosym.py
+++ command_line/cosym.py
@@ -131,8 +131,8 @@ class cosym(Subject):
         ]
         if len(exclude):
             logger.info(
-                f"Rejecting {len(exclude)} datasets from cosym analysis:"
-                f"couldn't determine consistent cb_op to minimum cell:\n"
+                f"Rejecting {len(exclude)} datasets from cosym analysis "
+                f"(couldn't determine consistent cb_op to minimum cell):\n"
                 f"{exclude}",
             )
             self._experiments, self._reflections = select_datasets_on_identifiers(
