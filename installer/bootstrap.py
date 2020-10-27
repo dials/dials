49514b0a00bc0890dee3b61ad5b69e579af5e309
Nicholas Devenish <ndevenish@gmail.com>
2020-10-27 13:13:12 +0000

    Prepare branch for DIALS 3.5 release

--- installer/bootstrap.py
+++ installer/bootstrap.py
@@ -961,11 +961,10 @@ def update_sources(options):
             pass
 
     repositories = {
-        source.split("/")[1]: {"base-repository": source, "branch-local": branch}
+        source.split("/")[1]: {"base-repository": source, "branch-local": "dials-3.5"}
         for source, branch in (
-            ("cctbx/annlib_adaptbx", "master"),
-            ("cctbx/cctbx_project", "master"),
-            ("cctbx/dxtbx", "main"),
+            ("dials/annlib_adaptbx", "master"),
+            ("dials/dxtbx", "main"),
             ("dials/annlib", "master"),
             ("dials/cbflib", "master"),
             ("dials/ccp4io", "master"),
@@ -976,10 +975,8 @@ def update_sources(options):
         )
     }
     repositories["cctbx_project"] = {
-        "base-repository": "cctbx/cctbx_project",
-        "effective-repository": "dials/cctbx",
-        "branch-remote": "master",
-        "branch-local": "stable",
+        "base-repository": "dials/cctbx",
+        "branch-local": "dials-3.5",
     }
 
     for source, setting in options.branch:
