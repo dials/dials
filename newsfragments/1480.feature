06c0d7dddae24de54f887bcad1ff420c7c4f032e
David Waterman <dagewa@users.noreply.github.com>
2020-11-13 09:30:15 +0000

    Compress exported mmcif on-the-fly (#1482)
    Allow export of compressed mmcif.
    
    Use `compress=(gz | bz2 | xz)` to use the specified compression format and
    automatically set the file name extension. Note that `gz` compression is
    surprisingly slow and `bz2` gives the best performance (see #1485).
    
    Also improve performance of mmcif output by preparing a format string in
    advance, as suggested by @rjgildea.

---
+++ newsfragments/1480.feature
@@ -0,0 +1,2 @@
+dials.export: Allow on-the-fly bzip2 or gzip compression for mmCIF
+output, because unmerged mmCIF reflection files are large.
--- util/export_mmcif.py
+++ util/export_mmcif.py
@@ -1,7 +1,10 @@
 from __future__ import absolute_import, division, print_function
 
+import bz2
 import datetime
+import gzip
 import logging
+import lzma
 import math
 import time
 
@@ -27,7 +30,7 @@ class MMCIFOutputFile(object):
 
     def __init__(self, params):
         """
-        Init with the filename
+        Init with the parameters
         """
         self._cif = iotbx.cif.model.cif()
         self.params = params
@@ -205,17 +208,37 @@ class MMCIFOutputFile(object):
             "_pdbx_diffrn_unmerged_refln.index_l",
         )
 
-        headernames = {
-            "scales": "_pdbx_diffrn_unmerged_refln.scale_value",
-            "intensity.scale.value": "_pdbx_diffrn_unmerged_refln.intensity_meas",
-            "intensity.scale.sigma": "_pdbx_diffrn_unmerged_refln.intensity_sigma",
-            "intensity.sum.value": "_pdbx_diffrn_unmerged_refln.intensity_sum",
-            "intensity.sum.sigma": "_pdbx_diffrn_unmerged_refln.intensity_sum_sigma",
-            "intensity.prf.value": "_pdbx_diffrn_unmerged_refln.intensity_prf",
-            "intensity.prf.sigma": "_pdbx_diffrn_unmerged_refln.intensity_prf_sigma",
-            "angle": "_pdbx_diffrn_unmerged_refln.scan_angle_reflection",
-            "partiality": "_pdbx_diffrn_unmerged_refln.partiality",
+        extra_items = {
+            "scales": ("_pdbx_diffrn_unmerged_refln.scale_value", "%5.3f"),
+            "intensity.scale.value": (
+                "_pdbx_diffrn_unmerged_refln.intensity_meas",
+                "%8.3f",
+            ),
+            "intensity.scale.sigma": (
+                "_pdbx_diffrn_unmerged_refln.intensity_sigma",
+                "%8.3f",
+            ),
+            "intensity.sum.value": (
+                "_pdbx_diffrn_unmerged_refln.intensity_sum",
+                "%8.3f",
+            ),
+            "intensity.sum.sigma": (
+                "_pdbx_diffrn_unmerged_refln.intensity_sum_sigma",
+                "%8.3f",
+            ),
+            "intensity.prf.value": (
+                "_pdbx_diffrn_unmerged_refln.intensity_prf",
+                "%8.3f",
+            ),
+            "intensity.prf.sigma": (
+                "_pdbx_diffrn_unmerged_refln.intensity_prf_sigma",
+                "%8.3f",
+            ),
+            "angle": ("_pdbx_diffrn_unmerged_refln.scan_angle_reflection", "%7.4f"),
+            "partiality": ("_pdbx_diffrn_unmerged_refln.partiality", "%7.4f"),
         }
+
+        fmt = "%6i %2i %5i %5i %-2i %-2i %-2i"
 
         variables_present = []
         if "scale" in self.params.intensity:
@@ -246,7 +269,10 @@ class MMCIFOutputFile(object):
 
         for name in variables_present:
             if name in reflections:
-                header += (headernames[name],)
+                header += (extra_items[name][0],)
+                fmt += " " + extra_items[name][1]
+
+        loop_format_strings = {"_pdbx_diffrn_unmerged_refln": fmt}
 
         if "scale" in self.params.intensity:
             # Write dataset_statistics - first make a miller array
@@ -295,8 +321,18 @@ class MMCIFOutputFile(object):
         self._cif["dials"] = cif_block
 
         # Print to file
-        with open(filename, "w") as fh:
-            self._cif.show(out=fh)
+        if self.params.mmcif.compress and not filename.endswith("." + self.params.mmcif.compress):
+            filename += "." + self.params.mmcif.compress
+        if self.params.mmcif.compress == "gz":
+            open_fn = gzip.open
+        elif self.params.mmcif.compress == "bz2":
+            open_fn = bz2.open
+        elif self.params.mmcif.compress == "xz":
+            open_fn = lzma.open
+        else:
+            open_fn = open
+        with open_fn(filename, "wt") as fh:
+            self._cif.show(out=fh, loop_format_strings=loop_format_strings)
 
         # Log
         logger.info("Wrote reflections to %s" % filename)
