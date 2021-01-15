2443d1388371079bec6ca37dfe14669a6281fb7a
David Waterman <dagewa@users.noreply.github.com>
2021-01-15 10:48:52 +0000

    dials.background plot to file (#1537)
    Add an option to dials.background to allow plotting to image files instead of
    interactively, to facilitate usage in scripts. Also, add a test and fix so that
    images can be read, not just .expt files.

---
+++ test/command_line/test_background.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import, division, print_function
+
+import procrunner
+
+
+def test(dials_data, tmpdir):
+    experiments = dials_data("centroid_test_data") / "experiments.json"
+
+    result = procrunner.run(
+        [
+            "dials.background",
+            "output.plot=background.png",
+            "image=1",
+            str(experiments),
+        ],
+        working_directory=tmpdir,
+    )
+
+    assert not result.returncode and not result.stderr
+    assert tmpdir.join("background.png").check()
+
+    for line in result.stdout.splitlines():
+        if line.startswith(b"Mean background"):
+            assert line.endswith(b"0.426")
+            break
