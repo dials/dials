9d60a536706e5dd7acb073839868d6cde8060909
Nicholas Devenish <ndevenish@gmail.com>
2021-01-16 12:29:10 +0000

    dials.background: Fix error writing out file
    If the X server isn't configured properly or available, then don't
    crash when trying to write a file out, in which case it needn't
    connect to the X server to begin with.

--- test/command_line/test_background.py
+++ test/command_line/test_background.py
@@ -23,3 +23,21 @@ def test(dials_data, tmpdir):
         if line.startswith(b"Mean background"):
             assert line.endswith(b"0.426")
             break
+
+
+def test_file_output_bad_display(dials_data, tmpdir):
+    experiments = dials_data("centroid_test_data") / "experiments.json"
+
+    result = procrunner.run(
+        [
+            "dials.background",
+            "output.plot=background.png",
+            "image=1",
+            str(experiments),
+        ],
+        working_directory=tmpdir,
+        environment_override={"DISPLAY": "invalid_host:0"},
+    )
+
+    assert not result.returncode and not result.stderr
+    assert tmpdir.join("background.png").check()
