b056387c6578527970aedab525e5497ab68b301b
David Waterman <dgwaterman@gmail.com>
2021-02-09 16:18:18 +0000

    Catch zero weighted reflections early before entering refinement

--- algorithms/refinement/weighting_strategies.py
+++ algorithms/refinement/weighting_strategies.py
@@ -20,6 +20,10 @@ class StatisticalWeightingStrategy(object):
             sel = w > 0.0
             w.set_selected(sel, 1.0 / w.select(sel))
         reflections["xyzobs.mm.weights"] = flex.vec3_double(*parts)
+        if any(reflections["xyzobs.mm.weights"].norms() == 0.0):
+            raise DialsRefineConfigError(
+                "Cannot set statistical weights as some reflections have observed variances equal to zero"
+            )
 
         return reflections
 
