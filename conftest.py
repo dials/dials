3c0405d52ef1ecae1b2c0ddbad5f63235f54cb1f
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2021-05-10 16:07:42 +0100

    Remove pytest '--runslow' option
    The masked tests aren't that slow any more. We also have changed our
    development model, so this option is no longer required.

--- conftest.py
+++ conftest.py
@@ -21,24 +21,6 @@ if sys.version_info[:2] == (3, 7) and sys.platform == "darwin":
 collect_ignore = []
 
 
-def pytest_addoption(parser):
-    """Add a '--runslow' option to py.test."""
-    parser.addoption(
-        "--runslow", action="store_true", default=False, help="run slow tests"
-    )
-
-
-def pytest_collection_modifyitems(config, items):
-    """Tests marked as slow will not be run unless slow tests are enabled with
-    the '--runslow' parameter or the test is selected specifically. The
-    latter allows running slow tests via the libtbx compatibility layer."""
-    if not config.getoption("--runslow") and len(items) > 1:
-        skip_slow = pytest.mark.skip(reason="need --runslow option to run")
-        for item in items:
-            if "slow" in item.keywords:
-                item.add_marker(skip_slow)
-
-
 def pytest_configure(config):
     if not config.pluginmanager.hasplugin("dials_data"):
 
