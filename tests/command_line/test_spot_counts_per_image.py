5c023e56d8af681b220ca9bbbf737a9f54517593
Richard Gildea <rjgildea@users.noreply.github.com>
2021-05-24 15:55:40 +0100

    dials.spot_counts_per_image: explicitly fail given indexed reflections (#1690)
    Since behaviour isn't currently well-defined on indexed reflections,
    ensure that we are only given output of spotfinding.
    
    Fixes #1689.

--- tests/command_line/test_spot_counts_per_image.py
+++ tests/command_line/test_spot_counts_per_image.py
@@ -45,3 +45,17 @@ def test_spot_counts_per_image(dataset, dials_data, tmpdir):
             b"d_min (distl method 2)",
         )
     ), result.stdout
+
+
+def test_spot_counts_per_image_fails_cleanly_on_indexed(dials_data, tmp_path):
+    path = dials_data("insulin_processed")
+    result = procrunner.run(
+        [
+            "dials.spot_counts_per_image",
+            path / "indexed.expt",
+            path / "indexed.refl",
+        ],
+        working_directory=tmp_path,
+    )
+    assert result.returncode
+    assert b"Only unindexed reflections are currently supported" in result.stderr
