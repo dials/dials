b86371b0fb35d96cbe62d58079ff41475f2bbfa9
Richard Gildea <richard.gildea@diamond.ac.uk>
2021-07-13 12:20:04 +0100

    Use pathlib objects in test

--- tests/algorithms/scaling/test_scale.py
+++ tests/algorithms/scaling/test_scale.py
@@ -561,10 +561,10 @@ def test_scale_and_filter_image_group_mode(dials_data, tmpdir):
     assert analysis_results["termination_reason"] == "max_percent_removed"
 
 
-def test_scale_and_filter_termination(dials_data, tmpdir):
+def test_scale_and_filter_termination(dials_data, tmp_path):
     """Test the scale and filter command line program,
     when it terminates with a cycle of no reflections removed."""
-    location = dials_data("multi_crystal_proteinase_k")
+    location = dials_data("multi_crystal_proteinase_k", pathlib=True)
 
     command = [
         "dials.scale",
@@ -579,21 +579,20 @@ def test_scale_and_filter_termination(dials_data, tmpdir):
         "full_matrix=False",
     ]
     for i in [1, 2, 3, 4, 5, 7, 10]:
-        command.append(location.join("experiments_" + str(i) + ".json").strpath)
-        command.append(location.join("reflections_" + str(i) + ".pickle").strpath)
+        command.append(location / f"experiments_{i}.json")
+        command.append(location / f"reflections_{i}.pickle")
 
-    result = procrunner.run(command, working_directory=tmpdir)
+    result = procrunner.run(command, working_directory=tmp_path)
     assert not result.returncode and not result.stderr
-    assert tmpdir.join("scaled.refl").check()
-    assert tmpdir.join("scaled.expt").check()
-    assert tmpdir.join("analysis_results.json").check()
+    assert (tmp_path / "scaled.refl").is_file()
+    assert (tmp_path / "scaled.expt").is_file()
+    assert (tmp_path / "analysis_results.json").is_file()
 
-    with open(tmpdir.join("analysis_results.json").strpath) as f:
-        analysis_results = json.load(f)
+    analysis_results = json.load((tmp_path / "analysis_results.json").open())
     assert analysis_results["termination_reason"] == "no_more_removed"
     assert len(analysis_results["cycle_results"]["1"]["removed_datasets"]) == 1
     assert analysis_results["cycle_results"]["2"]["removed_datasets"] == []
-    refls = flex.reflection_table.from_file(tmpdir.join("scaled.refl"))
+    refls = flex.reflection_table.from_file(tmp_path / "scaled.refl")
     assert len(set(refls["id"])) == 6
     assert len(set(refls["imageset_id"])) == 6
 
