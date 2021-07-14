f075c96d7720030525b253775b27f8a3c81c927a
Richard Gildea <richard.gildea@diamond.ac.uk>
2021-07-14 21:19:15 +0100

    Set wavelength in scaled miller array
    Set wavelength as mean of wavelengths of
    input experiments. Optionally override by
    passing wavelength.

---
+++ newsfragments/XXX.feature
@@ -0,0 +1,1 @@
+``dials.algorithms.scaling.scaling_library.scaled_data_as_miller_array``: Set wavelength in returned ``miller.array``
--- tests/algorithms/scaling/test_scaling_library.py
+++ tests/algorithms/scaling/test_scaling_library.py
@@ -20,6 +20,7 @@ from dials.algorithms.scaling.scaling_library import (
     create_scaling_model,
     determine_best_unit_cell,
     scale_single_dataset,
+    scaled_data_as_miller_array,
 )
 from dials.array_family import flex
 from dials.util.options import OptionParser
@@ -345,3 +346,21 @@ def test_determine_best_unit_cell(test_experiments):
     assert determine_best_unit_cell(test_experiments).parameters() == pytest.approx(
         recalc_uc.parameters()
     )
+
+
+def test_scaled_data_as_miller_array(dials_data):
+    location = dials_data("l_cysteine_4_sweeps_scaled", pathlib=True)
+    reflections = flex.reflection_table.from_file(location / "scaled_20_25.refl")
+    experiments = ExperimentList.from_file(
+        location / "scaled_20_25.expt", check_format=False
+    )
+    reflections = [reflections.select(reflections["id"] == i) for i in range(2)]
+    print([refl.size() for refl in reflections])
+
+    miller_array = scaled_data_as_miller_array(reflections, experiments)
+    assert miller_array.size() == 5503
+    assert miller_array.info().wavelength == experiments[0].beam.get_wavelength()
+
+    miller_array = scaled_data_as_miller_array(reflections, experiments, wavelength=1)
+    assert miller_array.size() == 5503
+    assert miller_array.info().wavelength == 1
