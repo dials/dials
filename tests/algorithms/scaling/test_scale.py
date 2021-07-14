dials/dials

commit 78a696a6591e224e73204846f39771ebac0c2668

Handle imageset_id correctly in scaling, cosym, symmetry (#1763)
Before combining multiple single-sweep reflection tables, call the
helper function to renumber the imageset_id to match the order of the
imagesets in the experiment list. This is called just before combining
and saving, in case any sweeps are removed during processing.

Towards fixing #1093.

algorithms/scaling/algorithm.py
@@ -42,6 +42,7 @@ from dials.util.multi_dataset_handling import (
     assign_unique_identifiers,
     parse_multiple_datasets,
     select_datasets_on_ids,
+    update_imageset_ids,
 )
 
 logger = logging.getLogger("dials")
@@ -306,7 +307,8 @@ class ScalingAlgorithm:
             for component in experiment.scaling_model.components.keys():
                 del experiment.scaling_model.components[component].data
         gc.collect()
-
+        # update imageset ids before combining reflection tables.
+        self.reflections = update_imageset_ids(self.experiments, self.reflections)
         joint_table = flex.reflection_table()
         for i in range(len(self.reflections)):
             joint_table.extend(self.reflections[i])

tests/algorithms/scaling/test_scale.py
@@ -716,6 +716,11 @@ def test_multi_scale(dials_data, tmpdir):
     assert n_scaled == refls.get_flags(refls.flags.bad_for_scaling, all=False).count(
         False
     )
+    assert len(set(refls["id"])) == 2
+    assert len(set(refls["imageset_id"])) == 2
+    for id_ in range(2):
+        sel = refls["id"] == id_
+        assert set(refls["imageset_id"].select(sel)) == set([id_])
 
     # run again, optimising errors, and continuing from where last run left off.
     extra_args = [

command_line/cosym.py
@@ -24,6 +24,7 @@ from dials.util.multi_dataset_handling import (
     assign_unique_identifiers,
     parse_multiple_datasets,
     select_datasets_on_identifiers,
+    update_imageset_ids,
 )
 from dials.util.observer import Subject
 from dials.util.options import OptionParser, reflections_and_experiments_from_files
@@ -191,6 +192,7 @@ class cosym(Subject):
         This includes the cosym.json, reflections and experiments files."""
 
         reindexed_reflections = flex.reflection_table()
+        self._reflections = update_imageset_ids(self._experiments, self._reflections)
         for refl in self._reflections:
             reindexed_reflections.extend(refl)
         reindexed_reflections.reset_ids()

command_line/symmetry.py
@@ -35,6 +35,7 @@ from dials.util.filter_reflections import filtered_arrays_from_experiments_refle
 from dials.util.multi_dataset_handling import (
     assign_unique_identifiers,
     parse_multiple_datasets,
+    update_imageset_ids,
 )
 from dials.util.options import OptionParser, reflections_and_experiments_from_files
 from dials.util.version import dials_version
@@ -486,6 +487,7 @@ def _reindex_experiments_reflections(experiments, reflections, space_group, cb_o
         experiments, cb_op, space_group=space_group
     )
     reindexed_reflections = flex.reflection_table()
+    reflections = update_imageset_ids(experiments, reflections)
     for i in range(len(reindexed_experiments)):
         reindexed_refl = copy.deepcopy(reflections[i])
         reindexed_refl["miller_index"] = cb_op.apply(reindexed_refl["miller_index"])

tests/command_line/test_cosym.py
@@ -35,6 +35,14 @@ def test_cosym(dials_data, run_in_tmpdir, space_group, engine):
             experiments[0].crystal.get_space_group().type().lookup_symbol()
             == space_group
         )
+    joint_reflections = flex.reflection_table.from_file("symmetrized.refl")
+    # check that there are 8 unique id and imageset_ids, and that these
+    # correctly correspond to each experiment
+    assert len(set(joint_reflections["id"])) == 8
+    assert len(set(joint_reflections["imageset_id"])) == 8
+    for id_ in range(8):
+        sel = joint_reflections["id"] == id_
+        assert set(joint_reflections["imageset_id"].select(sel)) == set([id_])
 
 
 def test_cosym_partial_dataset(dials_data, run_in_tmpdir):

tests/command_line/test_symmetry.py
@@ -47,6 +47,16 @@ def test_symmetry_laue_only(dials_data, tmpdir):
         tmpdir.join("symmetrized.expt").strpath, check_format=False
     )
     assert str(exps[0].crystal.get_space_group().info()) == "P 2 2 2"
+    joint_reflections = flex.reflection_table.from_file(
+        tmpdir.join("symmetrized.refl").strpath
+    )
+    # check that there are 2 unique id and imageset_ids, and that these
+    # correctly correspond to each experiment
+    assert len(set(joint_reflections["id"])) == 2
+    assert len(set(joint_reflections["imageset_id"])) == 2
+    for id_ in range(2):
+        sel = joint_reflections["id"] == id_
+        assert set(joint_reflections["imageset_id"].select(sel)) == set([id_])
 
 
 def test_symmetry_basis_changes_for_C2(run_in_tmpdir):

tests/util/test_multi_dataset_handling.py
@@ -5,6 +5,7 @@ Tests for dials.util.multi_dataset_handling functions
 import pytest
 
 from dxtbx.model import Experiment, ExperimentList
+from dxtbx.serialize import load
 
 from dials.array_family import flex
 from dials.tests.util import (
@@ -17,6 +18,7 @@ from dials.util.multi_dataset_handling import (
     renumber_table_id_columns,
     select_datasets_on_ids,
     sort_tables_to_experiments_order,
+    update_imageset_ids,
 )
 
 
@@ -381,3 +383,35 @@ def test_sort_tables_to_experiments_order_single_dataset_files():
     assert refls[1] is reflection_tables[1]
     assert list(refls[0].experiment_identifiers().values()) == ["0"]
     assert list(refls[1].experiment_identifiers().values()) == ["1"]
+
+
+def test_update_imageset_ids(dials_data):
+    expts = ExperimentList()
+    refls = []
+    for i in [1, 2, 3, 4, 5, 7, 8, 10]:
+        refls.append(
+            flex.reflection_table.from_file(
+                dials_data("multi_crystal_proteinase_k", pathlib=True)
+                / f"reflections_{i}.pickle"
+            )
+        )
+        expts.extend(
+            load.experiment_list(
+                dials_data("multi_crystal_proteinase_k", pathlib=True)
+                / f"experiments_{i}.json",
+                check_format=False,
+            )
+        )
+    # first make sure ids are set up correctly.
+    experiments, reflections = assign_unique_identifiers(expts, refls)
+    reflections = update_imageset_ids(experiments, reflections)
+    joint_reflections = flex.reflection_table()
+    for refls in reflections:
+        joint_reflections.extend(refls)
+    # check that there are 8 unique id and imageset_ids, and that these
+    # correctly correspond to each experiment
+    assert len(set(joint_reflections["id"])) == 8
+    assert len(set(joint_reflections["imageset_id"])) == 8
+    for id_ in range(8):
+        sel = joint_reflections["id"] == id_
+        assert set(joint_reflections["imageset_id"].select(sel)) == set([id_])

util/multi_dataset_handling.py
@@ -8,6 +8,8 @@ import copy
 import logging
 import uuid
 
+from orderedset import OrderedSet
+
 from dials.array_family import flex
 
 logger = logging.getLogger("dials")
@@ -368,3 +370,28 @@ are not found in the experiment list / reflection tables."""
                 list_of_reflections.append(reflection_table)
         experiments.remove_on_experiment_identifiers(exclude_datasets)
     return experiments, list_of_reflections
+
+
+def update_imageset_ids(experiments, reflections):
+    """For a list of input experiments and reflections (each containing one
+    sweep), update or add the imageset_id column to the data to match the order
+    in the experiment list.
+
+    This means that when the reflection tables are combined, the data is correct.
+    """
+    # input a list of ordered matching experiments and reflection tables.
+
+    next_iset_id = 0
+    imagesets_found = OrderedSet()
+    for expt, table in zip(experiments, reflections):
+        if "imageset_id" in table:
+            assert len(set(table["imageset_id"])) == 1
+        iset = expt.imageset
+        if iset not in imagesets_found:
+            imagesets_found.add(iset)
+            table["imageset_id"] = flex.int(table.size(), next_iset_id)
+            next_iset_id += 1
+        else:
+            iset_id = imagesets_found.index(iset)
+            table["imageset_id"] = flex.int(table.size(), iset_id)
+    return reflections

commit a47ae60843f4b587d54f00f636bba9fe095ea2cb

Fix imageset_id bug in scaling and filtering (#1805)
Introduced by 78a696a / #1763, causing a crash at the end of dials.scale.

Co-authored-by: Richard Gildea <richard.gildea@diamond.ac.uk>

algorithms/scaling/algorithm.py
@@ -418,13 +418,11 @@ multi-dataset scaling mode (not single dataset or scaling against a reference)""
                     logger.info(
                         "Finishing scaling and filtering as no data removed in this cycle."
                     )
+                    self.reflections = parse_multiple_datasets(
+                        [script.filtered_reflection_table]
+                    )
                     if self.params.scaling_options.full_matrix:
-                        self.reflections = parse_multiple_datasets(
-                            [script.filtered_reflection_table]
-                        )
                         results = self._run_final_scale_cycle(results)
-                    else:
-                        self.reflections = [script.filtered_reflection_table]
                     results.finish(termination_reason="no_more_removed")
                     break
 

tests/algorithms/scaling/test_scale.py
@@ -561,6 +561,42 @@ def test_scale_and_filter_image_group_mode(dials_data, tmpdir):
     assert analysis_results["termination_reason"] == "max_percent_removed"
 
 
+def test_scale_and_filter_termination(dials_data, tmp_path):
+    """Test the scale and filter command line program,
+    when it terminates with a cycle of no reflections removed."""
+    location = dials_data("multi_crystal_proteinase_k", pathlib=True)
+
+    command = [
+        "dials.scale",
+        "filtering.method=deltacchalf",
+        "stdcutoff=2.0",
+        "max_percent_removed=40.0",
+        "max_cycles=8",
+        "d_min=2.0",
+        "unmerged_mtz=unmerged.mtz",
+        "scale_and_filter_results=analysis_results.json",
+        "error_model=None",
+        "full_matrix=False",
+    ]
+    for i in [1, 2, 3, 4, 5, 7, 10]:
+        command.append(location / f"experiments_{i}.json")
+        command.append(location / f"reflections_{i}.pickle")
+
+    result = procrunner.run(command, working_directory=tmp_path)
+    assert not result.returncode and not result.stderr
+    assert (tmp_path / "scaled.refl").is_file()
+    assert (tmp_path / "scaled.expt").is_file()
+    assert (tmp_path / "analysis_results.json").is_file()
+
+    analysis_results = json.load((tmp_path / "analysis_results.json").open())
+    assert analysis_results["termination_reason"] == "no_more_removed"
+    assert len(analysis_results["cycle_results"]["1"]["removed_datasets"]) == 1
+    assert analysis_results["cycle_results"]["2"]["removed_datasets"] == []
+    refls = flex.reflection_table.from_file(tmp_path / "scaled.refl")
+    assert len(set(refls["id"])) == 6
+    assert len(set(refls["imageset_id"])) == 6
+
+
 def test_scale_and_filter_image_group_single_dataset(dials_data, tmpdir):
     """Test the scale and filter deltacchalf.mode=image_group on a
     single data set."""