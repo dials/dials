28f638b3ea5d1861d5acdfe6a916f052b1f1daa6
Richard Gildea <richard.gildea@diamond.ac.uk>
2021-08-17 20:21:59 +0100

    Remove some additional traces of Python 3.6

--- installer/bootstrap.py
+++ installer/bootstrap.py
@@ -1298,8 +1298,6 @@ be passed separately with quotes to avoid confusion (e.g
     )
 
     options = parser.parse_args()
-    if os.name == "nt" and options.python == "3.6":
-        sys.exit("Python 3.6 is not supported on Windows")
 
     print("Performing actions:", " ".join(options.actions))
 
