1f254048fdea6bfdbbaf94523c96846b492d6193
Graeme Winter <graeme.winter@gmail.com>
2021-09-07 09:57:02 +0100

    Rudimentary tests

---
+++ tests/util/test_command_line.py
@@ -0,0 +1,20 @@
+import random
+
+from dials.util.command_line import OptionParser
+
+
+def test_guess_input_file_type():
+    experiment_files = ["foo.expt", "bar.expt"]
+    image_files = ["foo.nxs"] + ["bar_%04d.cbf" % j for j in range(1, 11)]
+    reflection_files = ["strong.refl", "indexed.refl"]
+    others = ["kitten.jpg", "proposal_final_final_9aug2021.docx"]
+
+    filenames = experiment_files + image_files + reflection_files + others
+    random.shuffle(filenames)
+
+    split = OptionParser.guess_input_file_type(filenames)
+
+    assert set(experiment_files) == set(split["experiments"])
+    assert set(image_files) == set(split["images"])
+    assert set(reflection_files) == set(split["reflections"])
+    assert set(others) == set(split["unknown"])
