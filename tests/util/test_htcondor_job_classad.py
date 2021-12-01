bd9809170f53a165895436a45767d3e51bdb8210
Richard Gildea <rjgildea@users.noreply.github.com>
2021-12-01 19:42:41 +0000

    Read _CONDOR_JOB_AD not _CONDOR_MACHINE_AD (#1945)
    * Read _CONDOR_JOB_AD not _CONDOR_MACHINE_AD
    
    * Rename newsfragments/XXX.bugfix to newsfragments/1945.bugfix
    
    Co-authored-by: DiamondLightSource-build-server <DiamondLightSource-build-server@users.noreply.github.com>

--- tests/util/test_htcondor_job_classad.py
+++ tests/util/test_htcondor_job_classad.py
@@ -1,8 +1,8 @@
-from dials.util import parse_htcondor_classad
+from dials.util import parse_htcondor_job_classad
 
 
-def test_parse_htcondor_classad(tmp_path):
-    machine_ad = tmp_path / "condor_machine_ad"
+def test_parse_htcondor_job_classad(tmp_path):
+    machine_ad = tmp_path / "condor_job_ad"
     machine_ad.write_text(
         """\
 NumShadowStarts = 1
@@ -11,6 +11,6 @@ CpusProvisioned = 8
 TransferIn = false
 """
     )
-    class_ad = parse_htcondor_classad(machine_ad)
+    class_ad = parse_htcondor_job_classad(machine_ad)
     assert class_ad.cpus_provisioned == 8
     assert class_ad.memory_provisioned == 4096
