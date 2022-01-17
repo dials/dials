253a544ca7ca00d874c69bd4bb3e8d8e9e15d5ac
Markus Gerstel <2102431+Anthchirp@users.noreply.github.com>
2022-01-17 11:52:12 +0000

    Drop Python 3.7 support and allow 3.10 (#1866)
    As not all our dependencies have Python 3.10 builds yet this will likely
    fail for another few weeks/months. However, after that you'll benefit
    from much improved exception messages and all the other 3.10 goodness.

--- installer/bootstrap.py
+++ installer/bootstrap.py
@@ -1264,7 +1264,7 @@ be passed separately with quotes to avoid confusion (e.g
         "--python",
         help="Install this minor version of Python (default: %(default)s)",
         default="3.9",
-        choices=("3.7", "3.8", "3.9"),
+        choices=("3.8", "3.9", "3.10"),
     )
     parser.add_argument(
         "--branch",
