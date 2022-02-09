72a59f62c09cb37a4427552399a8e121e8577c7d
David Waterman <dgwaterman@gmail.com>
2022-02-09 13:56:12 +0000

    Basic algorithm seems to work. Need to tune parameters though.

--- extensions/radial_profile_spotfinder_threshold_ext.py
+++ extensions/radial_profile_spotfinder_threshold_ext.py
@@ -1,10 +1,8 @@
 from __future__ import annotations
 
-import logging
-
 from scitbx.array_family import flex
 
-logger = logging.getLogger("dials.extensions.radial_profile_spotfinder_threshold_ext")
+import dials.extensions
 
 
 class RadialProfileSpotFinderThresholdExt:
@@ -53,8 +51,64 @@ class RadialProfileSpotFinderThresholdExt:
             raise ValueError("Missing required arguments")
         region_of_interest = kwargs.get("region_of_interest")
 
-        # TODO - algorithm goes here
-        result = flex.bool(len(image))
-        result.reshape(image.accessor())
+        # Initial dispersion threshold to determine likely background pixels
+        Dispersion = dials.extensions.SpotFinderThreshold.load("dispersion")
+        threshold_function = Dispersion(self.params)
+        peak_pixels = threshold_function.compute_threshold(image, mask)
+        background_pixels = mask & ~peak_pixels
+        background = image.select(background_pixels.iselection())
 
-        return result
+        # compute histogram of two-theta values, then same weighted
+        # by pixel values, finally divide latter by former to get
+        # the radial profile out, need to set the number of bins
+        # sensibly; inspired by method in PyFAI
+        panel = imageset.get_detector()[i_panel]
+        beam = imageset.get_beam()
+
+        # Get 2θ array for the background pixels
+        full_two_theta_array = panel.get_two_theta_array(beam.get_s0())
+        if region_of_interest:
+            x0, x1, y0, y1 = region_of_interest
+            full_two_theta_array = full_two_theta_array[y0:y1, x0:x1]
+        two_theta_array = full_two_theta_array.as_1d().select(
+            background_pixels.iselection()
+        )
+
+        # Use flex.weighted_histogram
+        n_bins = self.params.spotfinder.threshold.radial_profile.n_bins
+        h0 = flex.weighted_histogram(two_theta_array, n_slots=n_bins)
+        h1 = flex.weighted_histogram(two_theta_array, background, n_slots=n_bins)
+        h2 = flex.weighted_histogram(
+            two_theta_array, background * background, n_slots=n_bins
+        )
+
+        d0 = h0.slots()
+        d1 = h1.slots()
+        d2 = h2.slots()
+
+        I = d1 / d0
+        I2 = d2 / d0
+        sig = flex.sqrt(I2 - flex.pow2(I))
+
+        # Determine the threshold value for each bin
+        n_sigma = self.params.spotfinder.threshold.radial_profile.n_sigma
+        threshold = I + n_sigma * sig
+
+        # convert full_two_theta_array into a lookup into the threshold
+        min_max_mean = flex.min_max_mean_double(two_theta_array)
+        lower_bound = min_max_mean.min
+        upper_bound = min_max_mean.max - lower_bound
+
+        lookup = full_two_theta_array - lower_bound
+        lookup /= upper_bound
+        lookup.set_selected(lookup < 0, 0)
+        lookup.set_selected(lookup > upper_bound, upper_bound)
+        lookup *= n_bins
+        lookup = flex.floor(lookup).iround().as_size_t()
+
+        # Now construct a threshold image
+        thresh_im = threshold.select(lookup.as_1d())
+        peaks = image > thresh_im
+        peaks.reshape(image.accessor())
+
+        return peaks
