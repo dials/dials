92daf13ed8c9807cc032bf958ded23e6f482a6ba
James Beilsten-Edmands <30625594+jbeilstenedmands@users.noreply.github.com>
2022-02-21 12:43:13 +0000

    Refactor tests in attempt to avoid testing issues (#2015)

--- tests/command_line/test_ssx_integrate.py
+++ tests/command_line/test_ssx_integrate.py
@@ -38,8 +38,11 @@ def test_ssx_integrate_fullprocess(dials_data, tmp_path):
     assert tmp_path.joinpath("data.json").is_file()
 
 
+@pytest.mark.parametrize(
+    "algorithm,expected_n_refls", [("stills", 614), ("ellipsoid", 1258)]
+)
 @pytest.mark.xdist_group(name="group1")
-def test_ssx_integrate_algorithms(dials_data):
+def test_ssx_integrate_algorithms(dials_data, algorithm, expected_n_refls):
     # Download data set and the internally referenced images
     ssx = dials_data("cunir_serial_processed", pathlib=True)
     dials_data("cunir_serial", pathlib=True)
@@ -52,39 +55,14 @@ def test_ssx_integrate_algorithms(dials_data):
     parser = ArgumentParser(phil=working_phil, check_format=False)
     params, _ = parser.parse_args(args=[], quick_parse=True)
 
-    params.algorithm = "stills"
+    params.algorithm = algorithm
     params.nproc = 1
     params.image_range = "1:2"
 
     results = list(run_integration(indexed_refl, indexed_expts, params))
-    assert len(results) == 1
+    assert len(results) == 1  # only one batch expected
     experiments = results[0][0]
     reflections = results[0][1]
 
     assert len(experiments) == 2
-    expected_n_refls = 614
     assert len(reflections) == pytest.approx(expected_n_refls, abs=9)
-
-    params.algorithm = "ellipsoid"
-    results = list(run_integration(indexed_refl, indexed_expts, params))
-    assert len(results) == 1
-    experiments = results[0][0]
-    reflections = results[0][1]
-    assert len(experiments) == 2
-    assert experiments.profiles()[0].name == "ellipsoid"
-    expected_n_refls = 1258
-    assert len(reflections) == pytest.approx(expected_n_refls, abs=9)
-    assert indexed_expts[0].crystal != experiments[0].crystal
-
-    params.profile.ellipsoid.unit_cell.fixed = True
-    params.profile.ellipsoid.orientation.fixed = True
-
-    results = list(run_integration(indexed_refl, indexed_expts, params))
-    assert len(results) == 1
-    experiments = results[0][0]
-    reflections = results[0][1]
-    assert len(experiments) == 2
-    assert experiments.profiles()[0].name == "ellipsoid"
-    expected_n_refls = 1266
-    assert len(reflections) == pytest.approx(expected_n_refls, abs=9)
-    assert indexed_expts[0].crystal == experiments[0].crystal
