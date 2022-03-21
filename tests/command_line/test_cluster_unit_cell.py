7621172c3c855921e0a443052661a75e92a7eefc
David Waterman <dagewa@users.noreply.github.com>
2022-03-21 15:47:14 +0000

    Run test on windows (#2028)
    - Avoid command line too long errors by writing `input.phil`
    - Also use `reflections.pickle` rather than `indexed.pickle` to
      match other tests using this data (to reduce new dependencies on
      files in dials.regression)

--- tests/command_line/test_cluster_unit_cell.py
+++ tests/command_line/test_cluster_unit_cell.py
@@ -39,13 +39,17 @@ def test_dials_cluster_unit_cell_command_line_output_files(dials_regression, tmp
         pathlib.Path(dials_regression) / "refinement_test_data" / "multi_narrow_wedges"
     )
     experiments = list(data_dir.glob("data/sweep_*/experiments.json"))
-    reflections = list(data_dir.glob("data/sweep_*/indexed.pickle"))
+    reflections = list(data_dir.glob("data/sweep_*/reflections.pickle"))
 
-    # first combine experiments
+    # Combine experiments. Write PHIL file to avoid "command line is too long" error on Windows
+    with open(tmp_path / "input.phil", "w") as f:
+        f.writelines((f"input.reflections={i}" + os.linesep for i in reflections))
+        f.writelines((f"input.experiments={i}" + os.linesep for i in experiments))
     result = procrunner.run(
-        command=["dials.combine_experiments"] + experiments + reflections,
+        command=["dials.combine_experiments", "input.phil"],
         working_directory=tmp_path,
     )
+
     assert not result.returncode
     assert (tmp_path / "combined.refl").is_file()
     assert (tmp_path / "combined.expt").is_file()
@@ -88,15 +92,18 @@ def test_dials_cluster_unit_cell_command_line_output_files(dials_regression, tmp
     experiments = list(tmp_path.glob("split_*.expt"))
     reflections = list(tmp_path.glob("split_*.refl"))
 
+    # Write PHIL file to avoid "command line is too long" error on Windows
+    with open(tmp_path / "input.phil", "w") as f:
+        f.writelines((f"input.reflections={i}" + os.linesep for i in reflections))
+        f.writelines((f"input.experiments={i}" + os.linesep for i in experiments))
     result = procrunner.run(
         command=[
             "dials.cluster_unit_cell",
             "output.clusters=True",
             "threshold=40",
             "plot.show=False",
-        ]
-        + experiments
-        + reflections,
+            "input.phil",
+        ],
         working_directory=tmp_path,
     )
     assert not result.returncode
