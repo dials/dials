09ec32f8303b2df4551d0e24118b8063d6ac4552
Elena Pascal <elenadpascal@gmail.com>
2022-03-22 14:36:25 +0000

    boilerplate it

--- command_line/modify_geometry.py
+++ command_line/modify_geometry.py
@@ -1,8 +1,13 @@
 from __future__ import annotations
 
+from typing import List
+
 import libtbx.phil
+from dxtbx.model import ExperimentList
 
 import dials.util
+from dials.command_line.dials_import import ManualGeometryUpdater
+from dials.util.options import ArgumentParser, flatten_experiments
 
 help_message = """
 """
@@ -19,9 +24,31 @@ output {
 )
 
 
+def update(
+    experiments: ExperimentList, new_params: libtbx.phil.scope_extract
+) -> ExperimentList:
+
+    """
+    Modify detector, beam, goniometer and scan in experiments with the values in new_params
+    """
+
+    update_geometry = ManualGeometryUpdater(new_params)
+
+    if len(experiments):
+        imagesets = experiments.imagesets()
+
+    for imageset in imagesets:
+        imageset_new = update_geometry(imageset)
+        imageset.set_detector(imageset_new.get_detector())
+        imageset.set_beam(imageset_new.get_beam())
+        imageset.set_goniometer(imageset_new.get_goniometer())
+        imageset.set_scan(imageset_new.get_scan())
+
+    return experiments
+
+
 @dials.util.show_mail_handle_errors()
-def run(args=None):
-    from dials.util.options import ArgumentParser, flatten_experiments
+def run(args: List[str] = None, phil: libtbx.phil.scope = phil_scope) -> None:
 
     usage = "dials.modify_geometry [options] models.expt"
 
@@ -53,6 +80,8 @@ def run(args=None):
         imageset.set_beam(imageset_new.get_beam())
         imageset.set_goniometer(imageset_new.get_goniometer())
         imageset.set_scan(imageset_new.get_scan())
+
+    experiments = update(experiments, params)
 
     if len(experiments):
         print(f"Saving modified experiments to {params.output.experiments}")
