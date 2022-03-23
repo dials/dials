be0c50260c938501aa2b4c2331df5aa1ed3fdd80
Elena Pascal <elenadpascal@gmail.com>
2022-03-23 09:11:59 +0000

    Use tmp_path instead

--- tests/command_line/test_modify_geometry.py
+++ tests/command_line/test_modify_geometry.py
@@ -8,23 +8,23 @@ import pytest
 from dxtbx.serialize import load
 
 
-def test_modify_geometry(dials_regression, tmpdir):
+def test_modify_geometry_run(dials_regression, tmp_path):
     orig_expt_json = os.path.join(
         dials_regression, "experiment_test_data/kappa_experiments.json"
     )
-
-    new_expt_json = tmpdir.join("modified.expt")
+    assert orig_expt_json.is_file()
+    orig_expt = load.experiment_list(orig_expt_json, check_format=False)
 
     result = procrunner.run(
         ["dials.modify_geometry", orig_expt_json, "angles=10,20,30"],
-        working_directory=tmpdir,
+        working_directory=tmp_path,
     )
     assert not result.returncode and not result.stderr
 
-    assert os.path.exists(orig_expt_json), orig_expt_json
-    orig_expt = load.experiment_list(orig_expt_json, check_format=False)
-    assert new_expt_json.check()
-    new_expt = load.experiment_list(new_expt_json.strpath, check_format=False)
+    new_expt_json = tmp_path / "modified.expt"
+    assert new_expt_json.is_file()
+
+    new_expt = load.experiment_list(new_expt_json, check_format=False)
 
     orig_gonio = orig_expt.goniometers()[0]
     new_gonio = new_expt.goniometers()[0]
