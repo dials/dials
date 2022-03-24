a20db35c6d9f2205e2fdbc04192bc3c3997278d6
Elena Pascal <elenadpascal@gmail.com>
2022-03-24 08:59:24 +0000

    boilerplate it (#2041)
    * boilerplate it
    
    * Magic spell for phil handling
    
    * Don't parse unnecessarily
    
    Co-authored-by: James Beilsten-Edmands <jbeilstenedmands@gmail.com>

--- command_line/modify_geometry.py
+++ command_line/modify_geometry.py
@@ -1,8 +1,13 @@
 from __future__ import annotations
 
+from typing import List
+
 import libtbx.phil
+from dxtbx.model import ExperimentList
 
 import dials.util
+from dials.command_line.dials_import import ManualGeometryUpdater
+from dials.util.options import ArgumentParser, flatten_experiments
 
 help_message = """
 """
@@ -19,9 +24,30 @@ output {
 )
 
 
+def update(
+    experiments: ExperimentList, new_params: libtbx.phil.scope_extract
+) -> ExperimentList:
+
+    """
+    Modify detector, beam, goniometer and scan in experiments with the values in new_params
+    """
+
+    update_geometry = ManualGeometryUpdater(new_params)
+
+    imagesets = experiments.imagesets()
+
+    for imageset in imagesets:
+        imageset_new = update_geometry(imageset)
+        imageset.set_detector(imageset_new.get_detector())
+        imageset.set_beam(imageset_new.get_beam())
+        imageset.set_goniometer(imageset_new.get_goniometer())
+        imageset.set_scan(imageset_new.get_scan())
+
+    return experiments
+
+
 @dials.util.show_mail_handle_errors()
-def run(args=None):
-    from dials.util.options import ArgumentParser, flatten_experiments
+def run(args: List[str] = None, phil: libtbx.phil.scope = phil_scope) -> None:
 
     usage = "dials.modify_geometry [options] models.expt"
 
@@ -33,30 +59,18 @@ def run(args=None):
         epilog=help_message,
     )
 
-    params, options = parser.parse_args(args, show_diff_phil=True)
+    params, _ = parser.parse_args(args, show_diff_phil=True)
     experiments = flatten_experiments(params.input.experiments)
 
     if len(experiments) == 0:
         parser.print_help()
         exit(0)
 
-    from dials.command_line.dials_import import ManualGeometryUpdater
+    new_experiments = update(experiments, params)
 
-    update_geometry = ManualGeometryUpdater(params)
-
-    if len(experiments):
-        imagesets = experiments.imagesets()
-
-    for imageset in imagesets:
-        imageset_new = update_geometry(imageset)
-        imageset.set_detector(imageset_new.get_detector())
-        imageset.set_beam(imageset_new.get_beam())
-        imageset.set_goniometer(imageset_new.get_goniometer())
-        imageset.set_scan(imageset_new.get_scan())
-
-    if len(experiments):
+    if len(new_experiments):
         print(f"Saving modified experiments to {params.output.experiments}")
-        experiments.as_file(params.output.experiments)
+        new_experiments.as_file(params.output.experiments)
 
 
 if __name__ == "__main__":
