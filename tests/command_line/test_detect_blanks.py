f7862651494e2fcd5bdaf34a3bb99a99cd9e433e
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2022-03-31 16:40:08 +0100

    pathlib

--- tests/command_line/test_detect_blanks.py
+++ tests/command_line/test_detect_blanks.py
@@ -10,27 +10,29 @@ from dials.command_line import detect_blanks
 
 
 def test_strong(dials_data, capsys, run_in_tmp_path):
-    expts_file = dials_data("insulin_processed") / "imported.expt"
-    refl_file = dials_data("insulin_processed") / "strong.refl"
+    expts_file = dials_data("insulin_processed", pathlib=True) / "imported.expt"
+    refl_file = dials_data("insulin_processed", pathlib=True) / "strong.refl"
     refl = flex.reflection_table.from_file(refl_file)
     z = refl["xyzobs.px.value"].parts()[2]
     refl_subset = refl.select((z < 10) | (z > 20))
-    refl_subset.as_file("mod.refl")
-    detect_blanks.run([expts_file.strpath, "mod.refl"])
+    refl_subset.as_file(run_in_tmp_path / "mod.refl")
+
+    detect_blanks.run([str(expts_file), str(run_in_tmp_path / "mod.refl")])
+
     captured = capsys.readouterr()
     assert "Potential blank images: 11 -> 21" in captured.out
     json_file = run_in_tmp_path / "blanks.json"
     assert json_file.is_file()
     with json_file.open() as fh:
         d = json.load(fh)
-        assert d["strong"]["blank_regions"] == [[10, 21]]
-        assert not d["indexed"]
-        assert not d["integrated"]
+    assert d["strong"]["blank_regions"] == [[10, 21]]
+    assert not d["indexed"]
+    assert not d["integrated"]
 
 
 def test_integrated(dials_data, capsys, run_in_tmp_path):
-    expts_file = dials_data("insulin_processed") / "integrated.expt"
-    refl_file = dials_data("insulin_processed") / "integrated.refl"
+    expts_file = dials_data("insulin_processed", pathlib=True) / "integrated.expt"
+    refl_file = dials_data("insulin_processed", pathlib=True) / "integrated.refl"
     refl = flex.reflection_table.from_file(refl_file)
     z = refl["xyzobs.px.value"].parts()[2]
     sel = z < 40
@@ -38,10 +40,16 @@ def test_integrated(dials_data, capsys, run_in_tmp_path):
     refl_subset["intensity.prf.value"].set_selected(
         z.select(sel) < 10, refl_subset["intensity.prf.value"] * 0.05
     )
-    refl_subset.as_file("mod.refl")
+    refl_subset.as_file(run_in_tmp_path / "mod.refl")
+
     detect_blanks.run(
-        [expts_file.strpath, "mod.refl", "output.json=detect_blanks.json"]
+        [
+            str(expts_file),
+            str(run_in_tmp_path / "mod.refl"),
+            "output.json=detect_blanks.json",
+        ]
     )
+
     captured = capsys.readouterr()
     assert (
         """\
@@ -59,17 +67,19 @@ Potential blank images: 41 -> 45
     assert json_file.is_file()
     with json_file.open() as fh:
         d = json.load(fh)
-        assert d["strong"]["blank_regions"] == [[40, 45]]
-        assert d["indexed"]["blank_regions"] == [[40, 45]]
-        assert d["integrated"]["blank_regions"] == [[0, 9], [40, 45]]
+    assert d["strong"]["blank_regions"] == [[40, 45]]
+    assert d["indexed"]["blank_regions"] == [[40, 45]]
+    assert d["integrated"]["blank_regions"] == [[0, 9], [40, 45]]
 
 
-def test_still_raises_sysexit(dials_data, run_in_tmp_path):
-    path = dials_data("thaumatin_grid_scan")
+def test_passing_still_images_raises_sysexit(dials_data, run_in_tmp_path):
+    path = dials_data("thaumatin_grid_scan", pathlib=True)
 
     # Import the data
     result = procrunner.run(
-        ["dials.import", "output.experiments=imported.expt"] + path.listdir("*.cbf*")
+        ["dials.import", "output.experiments=imported.expt"]
+        + list(path.glob("*.cbf*")),
+        working_directory=run_in_tmp_path,
     )
     assert not result.returncode and not result.stderr
     expts_file = run_in_tmp_path / "imported.expt"
@@ -78,6 +88,7 @@ def test_still_raises_sysexit(dials_data, run_in_tmp_path):
     # Find the spots
     result = procrunner.run(
         ["dials.find_spots", "imported.expt", "min_spot_size=3", "nproc=1"],
+        working_directory=run_in_tmp_path,
     )
     assert not result.returncode and not result.stderr
     refl_file = run_in_tmp_path / "strong.refl"
