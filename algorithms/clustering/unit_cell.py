9a31151a1a983c9418ac33fe4d26c2561550142e
Richard Gildea <richard.gildea@diamond.ac.uk>
2022-04-14 15:58:12 +0100

    Remove cosym xfel.clustering module dependency

--- algorithms/clustering/unit_cell.py
+++ algorithms/clustering/unit_cell.py
@@ -1,154 +1,233 @@
 from __future__ import annotations
 
+import collections
 import logging
+from dataclasses import dataclass
 
-# modified version of the ab_cluster function so we can access the scipy dendrogram object
-from xfel.clustering.cluster import Cluster
+import numpy as np
+import scipy.spatial.distance as ssd
+from scipy.cluster import hierarchy
+
+from cctbx import crystal, uctbx
+from cctbx.sgtbx.lattice_symmetry import metric_subgroups
+from cctbx.uctbx.determine_unit_cell import NCDist
+from dxtbx.util import format_float_with_standard_uncertainty
 
 logger = logging.getLogger(__name__)
 
 
-class UnitCellCluster(Cluster):
-    def ab_cluster(
+class Cluster:
+    def __init__(
         self,
-        threshold=10000,
-        method="distance",
-        linkage_method="single",
-        log=False,
-        ax=None,
-        write_file_lists=True,
-        schnell=False,
-        doplot=True,
-        labels="default",
+        crystal_symmetries: list[crystal.symmetry],
+        lattice_ids: list[int],
+        name="",
     ):
-        """
-        Hierarchical clustering using the unit cell dimensions.
+        self.crystal_symmetries = crystal_symmetries
+        self.lattice_ids = lattice_ids
+        assert len(crystal_symmetries) == len(lattice_ids)
+        self.unit_cells = np.array(
+            [cs.unit_cell().parameters() for cs in crystal_symmetries]
+        )
+        self.median_cell = np.median(self.unit_cells, axis=0).tolist()
+        self.cell_std = np.std(self.unit_cells, axis=0).tolist()
+        self.mean_cell = np.mean(self.unit_cells, axis=0).tolist()
 
-        :param threshold: the threshold to use for prunning the tree into clusters.
-        :param method: which clustering method from scipy to use when creating the tree (see scipy.cluster.hierarchy)
-        :param linkage_method: which linkage method from scipy to use when creating the linkages. x (see scipy.cluster.hierarchy)
-        :param log: if True, use log scale on y axis.
-        :param ax: if a matplotlib axes object is provided, plot to this.
-                   Otherwise, create a new axes object and display on screen.
-        :param write_file_lists: if True, write out the files that make up each cluster.
-        :param schnell: if True, use simple euclidean distance, otherwise, use Andrews-Bernstein
-                        distance from Andrews & Bernstein J Appl Cryst 47:346 (2014) on the Niggli cells.
-        :param doplot: Boolean flag for if the plotting should be done at all.
-                       Runs faster if switched off.
-        :param labels: 'default' will not display any labels for more than 100 images, but will display
-                       file names for fewer. This can be manually overridden with a boolean flag.
-        :return: A list of Clusters ordered by largest Cluster to smallest
-
-        .. note::
-          Use 'schnell' option with caution, since it can cause strange behaviour
-          around symmetry boundaries.
-        """
-
-        import numpy as np
-
-        from cctbx.uctbx.determine_unit_cell import NCDist
-        from xfel.clustering.singleframe import SingleFrame
-
-        logger.info("Hierarchical clustering of unit cells")
-        import scipy.cluster.hierarchy as hcluster
-        import scipy.spatial.distance as dist
-
-        # 1. Create a numpy array of G6 cells
-        g6_cells = np.array([SingleFrame.make_g6(image.uc) for image in self.members])
-
-        # 2. Do hierarchichal clustering, using the find_distance method above.
-        if schnell:
-            logger.info("Using Euclidean distance")
-            metric = "euclidean"
-        else:
-            logger.info(
-                "Using Andrews-Bernstein distance from Andrews & Bernstein "
-                "J Appl Cryst 47:346 (2014)"
-            )
-            metric = NCDist
-        pair_distances = dist.pdist(g6_cells, metric=metric)
-        if len(pair_distances) > 0:
-            logger.info("Distances have been calculated")
-            this_linkage = hcluster.linkage(
-                pair_distances, method=linkage_method, metric=metric
-            )
-            cluster_ids = hcluster.fcluster(this_linkage, threshold, criterion=method)
-            logger.debug("Clusters have been calculated")
-        else:
-            logger.debug("No distances were calculated. Aborting clustering.")
-            return [], None
-
-        # 3. Create an array of sub-cluster objects from the clustering
-        sub_clusters = []
-        for cluster in range(max(cluster_ids)):
-            info_string = f"Made using ab_cluster with t={threshold}, {method} method, and {linkage_method} linkage"
-            sub_clusters.append(
-                self.make_sub_cluster(
-                    [
-                        self.members[i]
-                        for i in range(len(self.members))
-                        if cluster_ids[i] == cluster + 1
-                    ],
-                    f"cluster_{cluster + 1}",
-                    info_string,
-                )
-            )
-
-        sub_clusters = sorted(sub_clusters, key=lambda x: len(x.members))
-        # Rename to order by size
-        for num, cluster in enumerate(sub_clusters):
-            cluster.cname = f"cluster_{num + 1}"
-
-        # 3.5 optionally write out the clusters to files.
-        if write_file_lists:
-            for cluster in sub_clusters:
-                if len(cluster.members) > 1:
-                    cluster.dump_file_list(out_file_name=f"{cluster.cname}.lst")
-
-        if labels is True:
-            labels = [image.name for image in self.members]
-        elif labels is False:
-            labels = ["" for _ in self.members]
-        elif labels == "default":
-            if len(self.members) > 100:
-                labels = ["" for _ in self.members]
-            else:
-                labels = [image.name for image in self.members]
-        else:
-            labels = [getattr(v, labels, "") for v in self.members]
-
-        if doplot:
-            import matplotlib.pyplot as plt
-
-            # 4. Plot a dendogram to the axes if no axis is passed, otherwise just
-            #    return the axes object
-            if ax is None:
-                fig = plt.figure("Distance Dendogram")
-                ax = fig.gca()
-                direct_visualisation = True
-            else:
-                direct_visualisation = False
-
-        dendrogram = hcluster.dendrogram(
-            this_linkage,
-            labels=labels,
-            p=200,
-            truncate_mode="lastp",  # show only the last p merged clusters
-            leaf_font_size=8,
-            leaf_rotation=90.0,
-            color_threshold=threshold,
-            ax=ax,
-            no_plot=not doplot,
+        self.pg_composition = collections.Counter(
+            cs.space_group().type().lookup_symbol() for cs in crystal_symmetries
         )
 
-        if doplot:
-            if log:
-                ax.set_yscale("symlog", linthreshx=(-1, 1))
+    def __len__(self):
+        return len(self.crystal_symmetries)
+
+    def __str__(self):
+        cell_str = ", ".join(
+            format_float_with_standard_uncertainty(v, e, minimum=1e-5)
+            for (v, e) in zip(self.median_cell, self.cell_std)
+        )
+        return "\n".join(
+            [
+                f"Median cell: {cell_str} ",
+            ]
+        )
+
+
+@dataclass
+class ClusteringResult:
+    clusters: list[Cluster]
+    dendrogram: dict
+
+    def __str__(self):
+        text = [
+            "\n{:<16} {:<8} {:<13} {:<13} {:<13} {:<12} {:<12} {:<12}{:<8}".format(
+                "Cluster_id",
+                "N_xtals",
+                "Med_a",
+                "Med_b",
+                "Med_c",
+                "Med_alpha",
+                "Med_beta",
+                "Med_gamma",
+                "Delta(deg)",
+            )
+        ]
+        singletons = []
+        for cluster in self.clusters:
+            if len(cluster) > 1:
+                input_symmetry = crystal.symmetry(
+                    unit_cell=uctbx.unit_cell(cluster.median_cell),
+                    space_group_symbol="P 1",
+                )
+                groups = metric_subgroups(
+                    input_symmetry, 3.00, enforce_max_delta_for_generated_two_folds=True
+                )
+                group = groups.result_groups[0]
+                uc_params_conv = group["best_subsym"].unit_cell().parameters()
+                sorted_pg_comp = sorted(
+                    cluster.pg_composition.items(), key=lambda x: -1 * x[1]
+                )
+                pg_strings = ["{} in {}".format(pg[1], pg[0]) for pg in sorted_pg_comp]
+                point_group_string = ", ".join(pg_strings) + "."
+                text.append(point_group_string)
+                text.append(
+                    (
+                        "{:<16} {:<8} {:<6.2f}({:<5.2f}) {:<6.2f}({:<5.2f})"
+                        " {:<6.2f}({:<5.2f}) {:<6.2f}({:<4.2f}) {:<6.2f}"
+                        "({:<4.2f}) {:<6.2f}({:<4.2f})"
+                    ).format(
+                        cluster.name,
+                        len(cluster),
+                        cluster.median_cell[0],
+                        cluster.cell_std[0],
+                        cluster.median_cell[1],
+                        cluster.cell_std[1],
+                        cluster.median_cell[2],
+                        cluster.cell_std[2],
+                        cluster.median_cell[3],
+                        cluster.cell_std[3],
+                        cluster.median_cell[4],
+                        cluster.cell_std[4],
+                        cluster.median_cell[5],
+                        cluster.cell_std[5],
+                    )
+                )
+                text.append(
+                    (
+                        "{:>24}  {:<6.2f}{:<7} {:<6.2f}{:<7}"
+                        " {:<6.2f}{:<7} {:<6.2f}{:<6} {:<6.2f}"
+                        "{:<6} {:<6.2f}{:<6}  {:<6.2}"
+                    ).format(
+                        group["best_subsym"].space_group_info().symbol_and_number(),
+                        uc_params_conv[0],
+                        "",
+                        uc_params_conv[1],
+                        "",
+                        uc_params_conv[2],
+                        "",
+                        uc_params_conv[3],
+                        "",
+                        uc_params_conv[4],
+                        "",
+                        uc_params_conv[5],
+                        "",
+                        group["max_angular_difference"],
+                    )
+                )
             else:
-                ax.set_ylim(-ax.get_ylim()[1] / 100, ax.get_ylim()[1])
+                singletons.append(
+                    "".join(
+                        [
+                            (
+                                "{:<14} {:<11.2f} {:<11.2f} {:<11.2f}"
+                                "{:<12.1f} {:<12.1f} {:<12.1f}"
+                            ).format(
+                                list(cluster.pg_composition.keys())[0],
+                                cluster.median_cell[0],
+                                cluster.median_cell[1],
+                                cluster.median_cell[2],
+                                cluster.median_cell[3],
+                                cluster.median_cell[4],
+                                cluster.median_cell[5],
+                            ),
+                        ]
+                    )
+                )
+        text.append("Standard deviations are in brackets.")
+        text.append(
+            """Each cluster:
+        Input lattice count, with integration Bravais setting space group.
+        Cluster median with Niggli cell parameters (std dev in brackets).
+        Highest possible metric symmetry and unit cell using LePage (J Appl Cryst 1982, 15:255) method, maximum delta 3deg."""
+        )
+        n_clusters = len(self.clusters) - len(singletons)
+        text.insert(0, f"\n{n_clusters} cluster{'s'[:n_clusters^1]}")
+        n_singletons = len(singletons)
+        text[:0] = [
+            f"{n_singletons} singleton{'s'[:n_singletons^1]}:\n",
+            "{:<14} {:<11} {:<11} {:<11}{:<12} {:<12} {:<12}".format(
+                "Point group", "a", "b", "c", "alpha", "beta", "gamma"
+            ),
+            "".join(singletons),
+        ]
+        return "\n".join(text)
 
-            if direct_visualisation:
-                fig.savefig(f"{self.cname}_dendogram.pdf")
-                plt.show()
 
-        return sub_clusters, dendrogram, ax
+def cluster_unit_cells(
+    crystal_symmetries: list[crystal.symmetry],
+    lattice_ids: list[int],
+    threshold: int = 10000,
+):
+    cluster = Cluster(crystal_symmetries, lattice_ids)
+    uc = cluster.unit_cells
+
+    a = uc[:, 0] ** 2
+    b = uc[:, 1] ** 2
+    c = uc[:, 2] ** 2
+    d = 2 * uc[:, 1] * uc[:, 2] * np.cos(np.radians(uc[:, 3]))
+    e = 2 * uc[:, 0] * uc[:, 2] * np.cos(np.radians(uc[:, 4]))
+    f = 2 * uc[:, 0] * uc[:, 1] * np.cos(np.radians(uc[:, 5]))
+    g6_cells = np.array([a, b, c, d, e, f]).transpose()
+
+    logger.info(
+        "Using Andrews-Bernstein distance from Andrews & Bernstein "
+        "J Appl Cryst 47:346 (2014)"
+    )
+    metric = NCDist
+    pair_distances = ssd.pdist(g6_cells, metric=metric)
+    if len(pair_distances) > 0:
+        logger.info("Distances have been calculated")
+        this_linkage = hierarchy.linkage(pair_distances, method="single", metric=metric)
+        cluster_ids = hierarchy.fcluster(this_linkage, threshold, criterion="distance")
+        logger.debug("Clusters have been calculated")
+    else:
+        logger.debug("No distances were calculated. Aborting clustering.")
+        return [], None
+
+    # Create an array of sub-cluster objects from the clustering
+    sub_clusters: list[Cluster] = []
+    for i_cluster in set(cluster_ids):
+        sel = np.where(cluster_ids == i_cluster)[0]
+        cluster = Cluster(
+            [crystal_symmetries[i] for i in sel],
+            [lattice_ids[i] for i in sel],
+        )
+        sub_clusters.append(cluster)
+
+    # Order clusters by size
+    sub_clusters = sorted(sub_clusters, key=len)
+    for i, cluster in enumerate(sub_clusters):
+        cluster.name = f"cluster_{i + 1}"
+
+    dendrogram = hierarchy.dendrogram(
+        this_linkage,
+        # labels=labels,
+        p=200,
+        truncate_mode="lastp",  # show only the last p merged clusters
+        leaf_font_size=8,
+        leaf_rotation=90.0,
+        color_threshold=threshold,
+        # ax=ax,
+        no_plot=True,
+    )
+
+    return ClusteringResult(clusters=sub_clusters, dendrogram=dendrogram)
