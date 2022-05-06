762454ff133b719bf6e0afe3ef51a0f6a85f26f6
Daniel Paley <dwpaley@gmail.com>
2022-05-06 14:26:42 -0700

    add missing import

--- src/dials/algorithms/spot_finding/finder.py
+++ src/dials/algorithms/spot_finding/finder.py
@@ -10,6 +10,7 @@ import pickle
 from typing import Iterable, Tuple
 
 import libtbx
+import uctbx
 from dxtbx.format.image import ImageBool
 from dxtbx.imageset import ImageSequence, ImageSet
 from dxtbx.model import ExperimentList
