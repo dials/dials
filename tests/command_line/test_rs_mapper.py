fffcb1baa2bacffc3b61400f7fb9b5d46f22a253
Richard Gildea <richard.gildea@diamond.ac.uk>
2022-06-20 09:04:34 +0100

    Remove failing rs_mapper test
    This has been failing since cctbx/dxtbx#455 was merged, however I
    don't think the test makes sense as is, either before or after these
    changes. dials/dxtbx does not handle screening data collections from
    eigers gracefully - before this change it interprets as 3 x 45-degree
    images (so the starting angle is correct, but the oscillation range
    of each image is wrong), and after as 3 x 0.15-degree oscillations
    (so the starting angles are incorrect, but the oscillation ranges
    are correct). See also cctbx/dxtbx#13.

--- tests/command_line/test_rs_mapper.py
+++ tests/command_line/test_rs_mapper.py
@@ -31,23 +31,3 @@ def test_rs_mapper(dials_data, tmp_path):
 
     assert m.header_mean == pytest.approx(0.018924040719866753, abs=1e-6)
     assert flex.mean(m.data) == pytest.approx(0.01892407052218914, abs=1e-6)
-
-
-def test_masked(dials_data, tmp_path):
-    procrunner.run(
-        [
-            "dials.import",
-            dials_data("thaumatin_eiger_screen", pathlib=True) / "Therm_6_1_master.h5",
-        ],
-        working_directory=tmp_path,
-    )
-    result = procrunner.run(
-        ["dials.rs_mapper", "imported.expt", "map_file=junk.ccp4"],
-        working_directory=tmp_path,
-    )
-    assert not result.returncode and not result.stderr
-    assert (tmp_path / "junk.ccp4").is_file()
-
-    # load results
-    m = ccp4_map.map_reader(file_name=str(tmp_path / "junk.ccp4"))
-    assert m.header_max == pytest.approx(6330.33350)
