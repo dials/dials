a97bb3a16835e3c10b2a2821b3fc2b0f1995cc60
David Waterman <dgwaterman@gmail.com>
2022-08-08 16:37:16 +0100

    The OverloadChecker is still used by adding trusted_range[1] values, but internally these are now treated as saturation (ok) not overload (bad)

--- src/dials/algorithms/shoebox/overload_checker.h
+++ src/dials/algorithms/shoebox/overload_checker.h
@@ -32,10 +32,10 @@ namespace dials { namespace algorithms { namespace shoebox {
       typedef Shoebox<>::float_type float_type;
 
       /**
-       * @param overload - The overload values for each pixel
+       * @param saturation - The saturation values for each pixel
        */
-      Checker(const af::const_ref<double> &overload)
-          : overload_(overload.begin(), overload.end()) {}
+      Checker(const af::const_ref<double> &saturation)
+          : saturation_(saturation.begin(), saturation.end()) {}
 
       /**
        * Mark all pixels that are overloaded as invalid
@@ -46,13 +46,13 @@ namespace dials { namespace algorithms { namespace shoebox {
       bool operator()(std::size_t panel,
                       const af::const_ref<float_type, af::c_grid<3> > &data,
                       af::ref<int, af::c_grid<3> > mask) const {
-        DIALS_ASSERT(panel < overload_.size());
+        DIALS_ASSERT(panel < saturation_.size());
         DIALS_ASSERT(data.accessor().all_eq(mask.accessor()));
         DIALS_ASSERT(data.size() == mask.size());
         bool result = false;
-        double overload_value = overload_[panel];
+        double saturation_value = saturation_[panel];
         for (std::size_t i = 0; i < data.size(); ++i) {
-          if (data[i] >= overload_value) {
+          if (data[i] > saturation_value) {
             mask[i] &= ~Valid;
             result = true;
           }
@@ -61,16 +61,16 @@ namespace dials { namespace algorithms { namespace shoebox {
       }
 
     private:
-      af::shared<double> overload_;
+      af::shared<double> saturation_;
     };
 
   public:
     /**
-     * Add the overload values for this detector
-     * @param overload The overloads for each panel
+     * Add the saturation values for this detector
+     * @param saturation The saturation value for each panel
      */
-    void add(const af::const_ref<double> &overload) {
-      checker_.push_back(Checker(overload));
+    void add(const af::const_ref<double> &saturation) {
+      checker_.push_back(Checker(saturation));
     }
 
     /**
