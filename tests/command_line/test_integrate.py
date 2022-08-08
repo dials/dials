39040df4434b3c05a9dec511d5339bbe1cdd915a
David Waterman <dgwaterman@gmail.com>
2022-08-08 17:37:29 +0100

    Patch trusted_range in old experiment list files.
    The test data should be replaced if this branch is merged.

--- tests/command_line/test_integrate.py
+++ tests/command_line/test_integrate.py
@@ -22,6 +22,12 @@ def test_basic_integrate(dials_data, tmpdir):
         dials_data("centroid_test_data", pathlib=True) / "experiments.json"
     )
     exp[0].identifier = "foo"
+
+    panel = exp[0].detector[0]
+    max_trusted = panel.get_trusted_range()[1]
+    panel.set_trusted_range((0, max_trusted))
+    # exp.as_json(tmpdir.join("trusted_range_patch.expt"))
+
     exp.as_json(tmpdir.join("modified_input.json"))
 
     result = procrunner.run(
@@ -74,6 +80,13 @@ def test_basic_integrate(dials_data, tmpdir):
     j["experiment"][0]["identifier"] = "bar"
     with tmpdir.join("models.expt").open("w") as fh:
         json.dump(j, fh)
+
+    exp = load.experiment_list(tmpdir.join("models.expt"))
+    panel = exp[0].detector[0]
+    max_trusted = panel.get_trusted_range()[1]
+    panel.set_trusted_range((0, max_trusted))
+    # exp.as_json(tmpdir.join("trusted_range_patch.expt"))
+    exp.as_json(tmpdir.join("models.expt"))
 
     # Call dials.integrate
     result = procrunner.run(
@@ -148,6 +161,12 @@ def test_basic_threaded_integrate(dials_data, tmp_path):
     expts = dials_data("centroid_test_data", pathlib=True) / "indexed.expt"
     refls = dials_data("centroid_test_data", pathlib=True) / "indexed.refl"
 
+    exp = load.experiment_list(expts)
+    panel = exp[0].detector[0]
+    max_trusted = panel.get_trusted_range()[1]
+    panel.set_trusted_range((0, max_trusted))
+    exp.as_json(tmp_path / "trusted_range_patch.expt")
+
     result = procrunner.run(
         [
             "dials.integrate",
@@ -157,7 +176,7 @@ def test_basic_threaded_integrate(dials_data, tmp_path):
             "njobs=2",
             "nproc=2",
             refls,
-            expts,
+            "trusted_range_patch.expt",
         ],
         working_directory=tmp_path,
     )
@@ -177,6 +196,12 @@ def test_basic_integrate_output_integrated_only(dials_data, tmpdir):
         dials_data("centroid_test_data", pathlib=True) / "experiments.json"
     )
     exp[0].identifier = "bar"
+
+    panel = exp[0].detector[0]
+    max_trusted = panel.get_trusted_range()[1]
+    panel.set_trusted_range((0, max_trusted))
+    # exp.as_json(tmp_path / "trusted_range_patch.expt")
+
     exp.as_json(tmpdir.join("modified_input.json"))
 
     result = procrunner.run(
@@ -213,6 +238,12 @@ def test_integration_with_sampling(dials_data, tmpdir):
         dials_data("centroid_test_data", pathlib=True) / "experiments.json"
     )
     exp[0].identifier = "foo"
+
+    panel = exp[0].detector[0]
+    max_trusted = panel.get_trusted_range()[1]
+    panel.set_trusted_range((0, max_trusted))
+    # exp.as_json(tmp_path / "trusted_range_patch.expt")
+
     exp.as_json(tmpdir / "modified_input.json")
 
     result = procrunner.run(
@@ -244,6 +275,12 @@ def test_integration_with_sample_size(dials_data, tmpdir):
         dials_data("centroid_test_data", pathlib=True) / "experiments.json"
     )
     exp[0].identifier = "foo"
+
+    panel = exp[0].detector[0]
+    max_trusted = panel.get_trusted_range()[1]
+    panel.set_trusted_range((0, max_trusted))
+    # exp.as_json(tmp_path / "trusted_range_patch.expt")
+
     exp.as_json(tmpdir / "modified_input.json")
 
     result = procrunner.run(
@@ -338,11 +375,17 @@ def test_basic_integration_with_profile_fitting(dials_data, tmpdir):
     expts = dials_data("centroid_test_data", pathlib=True) / "indexed.expt"
     refls = dials_data("centroid_test_data", pathlib=True) / "indexed.refl"
 
+    exp = load.experiment_list(expts)
+    panel = exp[0].detector[0]
+    max_trusted = panel.get_trusted_range()[1]
+    panel.set_trusted_range((0, max_trusted))
+    exp.as_json(tmpdir.join("trusted_range_patch.expt"))
+
     result = procrunner.run(
         [
             "dials.integrate",
             "nproc=1",
-            expts,
+            "trusted_range_patch.expt",
             refls,
             "profile.fitting=True",
             "sampling.integrate_all_reflections=False",
@@ -370,6 +413,12 @@ def test_multi_sweep(dials_regression, tmpdir):
     experiments = load.experiment_list(expts)
     for i, expt in enumerate(experiments):
         expt.identifier = str(100 + i)
+
+    panel = experiments[0].detector[0]
+    max_trusted = panel.get_trusted_range()[1]
+    panel.set_trusted_range((0, max_trusted))
+    # experiments.as_json(tmpdir.join("trusted_range_patch.expt"))
+
     experiments.as_json(tmpdir / "modified_input.json")
 
     refls = os.path.join(
