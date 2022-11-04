08cea9f327bd54c35022347258783a1dce9203e6
Nicholas Devenish <ndevenish@gmail.com>
2022-11-04 12:09:04 +0000

    Move reciprocal_lattice_viewer test into tests/ folder

--- tests/util/test_render_3d.py
+++ tests/util/test_render_3d.py
@@ -1,9 +1,42 @@
 from __future__ import annotations
 
+import os
 import random
+
+import pytest
+
+from dxtbx.serialize import load
 
 from dials.array_family import flex
 from dials.util.reciprocal_lattice import Render3d
+
+
+@pytest.fixture
+def multi_sequence_data(dials_regression):
+    experiments = load.experiment_list(
+        os.path.join(
+            dials_regression, "indexing_test_data", "multi_sweep", "experiments.json"
+        ),
+        check_format=False,
+    )
+    reflections = flex.reflection_table.from_file(
+        os.path.join(
+            dials_regression, "indexing_test_data", "multi_sweep", "indexed.pickle"
+        )
+    )
+    return {"reflections": reflections, "experiments": experiments}
+
+
+@pytest.fixture
+def centroid_test_data(dials_data):
+    experiments = load.experiment_list(
+        dials_data("centroid_test_data", pathlib=True) / "experiments.json",
+        check_format=False,
+    )
+    reflections = flex.reflection_table.from_file(
+        dials_data("centroid_test_data", pathlib=True) / "integrated.pickle"
+    )
+    return {"reflections": reflections, "experiments": experiments}
 
 
 def test_Render3d(mocker, multi_sequence_data):
