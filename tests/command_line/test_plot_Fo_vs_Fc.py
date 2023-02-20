86cd72b25ec3c5294db13464b9ef43be0ae809c4
Richard Gildea <richard.gildea@diamond.ac.uk>
2023-02-20 15:00:47 +0000

    Alternative fix for 8d3612d1fd776b759b6039258195873c1d0b76b7

--- tests/command_line/test_plot_Fo_vs_Fc.py
+++ tests/command_line/test_plot_Fo_vs_Fc.py
@@ -1,25 +1,17 @@
 from __future__ import annotations
 
-import os
-import shutil
-import subprocess
+from dials.command_line import plot_Fo_vs_Fc
 
 
-def test(dials_data, tmp_path):
-
+def test(dials_data, tmp_path, capsys):
     mtz_file = (
         dials_data("lysozyme_electron_diffraction", pathlib=True) / "refmac_final.mtz"
     )
-    env = os.environ.copy()
-    env[
-        "PYTHONDEVMODE"
-    ] = ""  # disable a developermode warning from pyparsing from mathtext in matplotlib
-    result = subprocess.run(
-        [shutil.which("dials.plot_Fo_vs_Fc"), f"hklin={mtz_file}"],
-        cwd=tmp_path,
-        env=env,
-        capture_output=True,
+    plot_file = tmp_path / "Fo_vs_Fc.pdf"
+    plot_Fo_vs_Fc.run(
+        [f"hklin={mtz_file}", f"plot_filename={plot_file}"],
     )
-    assert not result.returncode and not result.stderr
-    assert tmp_path.joinpath("Fo_vs_Fc.pdf").is_file()
-    assert "|Fe| = 42.0" in result.stdout.decode()
+    captured = capsys.readouterr()
+    assert not captured.err
+    assert plot_file.is_file()
+    assert "|Fe| = 42.0" in captured.out
