3d698eb1f19498bf87113121c4026860276a9041
David Waterman <dagewa@users.noreply.github.com>
2023-03-20 15:05:33 +0000

    Update src/dials/util/exclude_images.py
    Co-authored-by: James Beilsten-Edmands <30625594+jbeilstenedmands@users.noreply.github.com>

--- src/dials/util/exclude_images.py
+++ src/dials/util/exclude_images.py
@@ -196,7 +196,7 @@ def set_invalid_images(experiments, exclude_images):
         for image_range in valid_image_ranges:
             # Need to index into the imageset's 0-based array
             accepts = (
-                flex.size_t(list(range(image_range[0], image_range[1] + 1))) - first
+                flex.size_t_range(image_range[0], image_range[1] + 1) - first
             )
             rejects.set_selected(accepts, False)
         for index in rejects.iselection():
