6ff0f1983ae611dc7d58f559e7e464f340eddfdd
davidmcdonagh <davidnmcdonagh@gmail.com>
2023-04-28 14:51:15 +0100

    Hide refinement error again.

--- src/dials/algorithms/indexing/model_evaluation.py
+++ src/dials/algorithms/indexing/model_evaluation.py
@@ -337,7 +337,6 @@ class ModelEvaluation(Strategy):
                 )
                 refiner.run()
             except (RuntimeError, ValueError):
-                raise
                 return
             else:
                 rmsds = refiner.rmsds()
