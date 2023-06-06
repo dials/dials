Fix error in derivatives

src/dials/algorithms/profile_model/ellipsoid/model.py
@@ -685,7 +685,16 @@ class Simple6Angular1ProfileModel(AngularProfileModelBase):
         """
         return Class.from_params(
             np.array(
-                [sigma_d, 0, sigma_d, 0, 0, sigma_d, sigma_d / 10.0], dtype=np.float64
+                [
+                    sigma_d,
+                    sigma_d / 100.0,
+                    sigma_d,
+                    sigma_d / 100.0,
+                    sigma_d / 100.0,
+                    sigma_d,
+                    sigma_d / 100.0,
+                ],
+                dtype=np.float64,
             )
         )
 
@@ -732,14 +741,14 @@ class Simple6Angular3ProfileModel(AngularProfileModelBase):
             np.array(
                 [
                     sigma_d,
-                    0,
+                    sigma_d / 100.0,
                     sigma_d,
-                    0,
-                    0,
+                    sigma_d / 100.0,
+                    sigma_d / 100.0,
                     sigma_d,
-                    sigma_d / 10.0,
-                    0.0,
-                    sigma_d / 10.0,
+                    sigma_d / 100.0,
+                    sigma_d / 100.0,
+                    sigma_d / 100.0,
                 ],
                 dtype=np.float64,
             )
@@ -763,7 +772,7 @@ class Simple6Angular3ProfileModel(AngularProfileModelBase):
 
         # Setup the parameters
         return Class.from_params(
-            flex.double((LL[0], LL[1], LL[2], LL[3], LL[4], LL[5], LL[5], 0, LL[5]))
+            flex.double((LL[0], LL[1], LL[2], LL[3], LL[4], LL[5], LL[5], LL[5], LL[5]))
         )
 
 

src/dials/algorithms/profile_model/ellipsoid/parameterisation.py
@@ -1000,15 +1000,16 @@ class ReflectionModelState(object):
             n_M_params = dM_dp.shape[0]  # state.M_params.size
             if state.is_mosaic_spread_angular:
                 # first add the derivative of the static component
-                QTMQ = np.einsum("ij,mjk,kl->ilm", self._Q.T, dM_dp, self._Q)
-                self._ds_dp[:, :, n_tot : n_tot + n_M_params] = QTMQ
+                self._ds_dp[:, :, n_tot : n_tot + n_M_params] = np.transpose(
+                    dM_dp, axes=(1, 2, 0)
+                )
                 n_tot += n_M_params
                 # now add the derivative of the angular component
                 dM_dp_A = self.state.dM_dp_A
                 n_M_A_params = dM_dp_A.shape[0]
                 normr = norm(self._r)
                 A = np.array(
-                    [[normr**2, 0, 0], [0, normr**2, 0], [0, 0, 1]],
+                    [[normr**2, 0, 0], [0, normr**2, 0], [0, 0, 0]],
                     dtype=np.float64,
                 ).reshape(3, 3)
                 AdM = np.einsum("ij,mjk->mik", A, dM_dp_A)