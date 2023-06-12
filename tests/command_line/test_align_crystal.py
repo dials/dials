fb376d73327c560ca8e1892b5b4c06bebd35431e
Richard Gildea <richard.gildea@diamond.ac.uk>
2023-06-12 16:36:33 +0100

    test_align_crystal: procrunner -> subprocess

--- tests/command_line/test_align_crystal.py
+++ tests/command_line/test_align_crystal.py
@@ -1,15 +1,16 @@
 from __future__ import annotations
 
 import os
-
-import procrunner
+import shutil
+import subprocess
 
 
 def test_align_crystal(dials_regression, tmpdir):
     path = os.path.join(dials_regression, "experiment_test_data")
-    result = procrunner.run(
-        ("dials.align_crystal", f"{path}/kappa_experiments.json"),
-        working_directory=tmpdir,
+    result = subprocess.run(
+        [shutil.which("dials.align_crystal"), f"{path}/kappa_experiments.json"],
+        cwd=tmpdir,
+        capture_output=True,
     )
     assert not result.returncode and not result.stderr
     assert result.stdout.replace(b"\r\n", b"\n").endswith(
