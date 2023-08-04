4fb2a86d13126247f806aa6b8cfbb9a93d2fde44
David Waterman <dagewa@users.noreply.github.com>
2023-08-04 09:45:52 +0100

    Fix `dials.image_viewer` crash when the detector swing angle is larger than 90° (#2479)
    Fix the image viewer for cases where the detector is in front of the crystal.

--- src/dials/util/image_viewer/slip_viewer/flex_image.py
+++ src/dials/util/image_viewer/slip_viewer/flex_image.py
@@ -110,7 +110,8 @@ def get_flex_image_multipanel(
             npanels += 1
         except RuntimeError:  # catch DXTBX_ASSERT for no intersection
             pass
-    beam_center /= npanels / 1e-3
+    if npanels:
+        beam_center /= npanels / 1e-3
 
     # XXX If a point is contained in two panels simultaneously, it will
     # be assigned to the panel defined first.  XXX Use a Z-buffer
