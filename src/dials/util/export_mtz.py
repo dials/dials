ff563b3b713b6319c3cdcc2ce1d324f6ccd58956
David Waterman <dgwaterman@gmail.com>
2023-09-20 10:48:52 +0100

    Proper way (I think) to get the gemmi space group from the cctbx one

--- src/dials/util/export_mtz.py
+++ src/dials/util/export_mtz.py
@@ -13,6 +13,7 @@ import numpy as np
 import pandas as pd
 
 from cctbx import uctbx
+from cctbx.sgtbx import space_group_symbols
 from dxtbx import flumpy
 from iotbx import mtz
 from libtbx import env
@@ -974,9 +975,14 @@ def export_mtz(
     mtz.history += [
         f"From {dials_version()}, run on {date_str}",
     ]
-    mtz.spacegroup = gemmi.find_spacegroup_by_name(
-        str(experiment_list[0].crystal.get_space_group().info())
-    )  # XXX is this sufficient?
+
+    # Create the right gemmi spacegroup from the crystal's cctbx space_group.
+    # We need to get an extended Hermann-Maugin symbol from the space_group,
+    # which is surprisingly convoluted
+    symbols = space_group_symbols(
+        experiment_list[0].crystal.get_space_group().type().lookup_symbol()
+    )
+    mtz.spacegroup = gemmi.find_spacegroup_by_name(symbols.universal_hermann_mauguin())
     mtz_writer = UnmergedMTZWriter(experiment_list[0].crystal.get_space_group())
 
     # FIXME TODO for more than one experiment into an MTZ file:
