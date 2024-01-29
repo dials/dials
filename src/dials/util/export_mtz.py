43e9e0fe56ce8f4f75f349e83da756dce3be27e4
Daniel Tchon <dantch@onet.eu>
2024-01-29 12:06:34 -0800

    Hide gemmi imports so `ImportError` is not raised unless gemmi is used

--- src/dials/util/export_mtz.py
+++ src/dials/util/export_mtz.py
@@ -8,7 +8,7 @@ from dataclasses import dataclass, field
 from math import isclose
 from typing import List, Optional
 
-import gemmi
+
 import numpy as np
 import pandas as pd
 
@@ -310,6 +310,7 @@ def add_batch_list(
     if max_batch_number > batch_offset:
         batch_offset = max_batch_number
 
+    import gemmi
     batch = gemmi.Mtz.Batch()
 
     # Setting fields that are the same for all batches
@@ -670,6 +671,7 @@ def export_mtz(
         )
 
     # Create the mtz file
+    import gemmi
     mtz = gemmi.Mtz(with_base=True)
     mtz.title = f"From {env.dispatcher_name}"
     date_str = time.strftime("%Y-%m-%d at %H:%M:%S %Z")
