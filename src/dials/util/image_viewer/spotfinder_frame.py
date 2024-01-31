dials/dials

commit 8f16d0a9a751f7e875e2737bd33bb24da8960d6d

Use ImageSequences in ssx workflow (#2556)

src/dials/util/image_viewer/spotfinder_frame.py
@@ -257,9 +257,17 @@ class SpotFrame(XrayFrame):
                 detector.projection = self.params.projection
 
         self.viewing_stills = True
+        self.viewing_still_scans = True
         for experiment_list in self.experiments:
             if any(exp.scan or exp.goniometer for exp in experiment_list):
                 self.viewing_stills = False
+                break
+        for experiment_list in self.experiments:
+            if any(
+                exp.scan and (exp.scan.get_oscillation()[1] != 0.0)
+                for exp in experiment_list
+            ):
+                self.viewing_still_scans = False
                 break
 
         if self.viewing_stills:
@@ -1715,6 +1723,15 @@ class SpotFrame(XrayFrame):
                 n = self.params.stack_images - 1
                 if self.viewing_stills:
                     selected = ref_list
+                elif self.viewing_still_scans:
+                    identifiers = []
+                    for elist in self.experiments:
+                        for scan in elist.scans():
+                            if scan.get_array_range()[0] == i_frame:
+                                sel_expts = elist.where(scan=scan)
+                                for i in sel_expts:
+                                    identifiers.append(elist[i].identifier)
+                    selected = ref_list.select_on_experiment_identifiers(identifiers)
                 else:
                     bbox_sel = ~((i_frame >= z1) | ((i_frame + n) < z0))
                     selected = ref_list.select(bbox_sel)
@@ -1858,15 +1875,33 @@ class SpotFrame(XrayFrame):
                     scan = self.pyslip.tiles.raw_image.get_scan()
                     frame_numbers = scan.get_array_index_from_angle(math.degrees(phi))
                 n = self.params.stack_images
-                for i_expt in range(flex.max(ref_list["id"]) + 1):
+                for i_expt in set(ref_list["id"]):
                     expt_sel = ref_list["id"] == i_expt
                     frame_predictions_sel = (frame_numbers >= (i_frame)) & (
                         frame_numbers < (i_frame + n)
                     )
 
                     sel = expt_sel
-                    if not self.viewing_stills:
+                    if not (self.viewing_stills or self.viewing_still_scans):
                         sel = frame_predictions_sel & expt_sel
+                    elif self.viewing_still_scans:
+                        if i_expt not in ref_list.experiment_identifiers():
+                            sel = flex.bool(ref_list.size(), False)
+                            break
+                        identifier = ref_list.experiment_identifiers()[i_expt]
+                        for expt_list in self.experiments:
+                            if identifier in expt_list.identifiers():
+                                idx = (
+                                    expt_list.identifiers() == identifier
+                                ).iselection()[0]
+                                scan = expt_list[idx].scan.get_array_range()
+                                if scan[0] == i_frame:
+                                    # this scan corresponds to this frame: good!
+                                    if not expt_list[idx].crystal:
+                                        sel = flex.bool(ref_list.size(), False)
+                                else:
+                                    sel = flex.bool(ref_list.size(), False)
+                                break
                     selected = ref_list.select(sel)
                     for reflection in selected.rows():
                         if (

src/dials/algorithms/indexing/ssx/processing.py
@@ -35,6 +35,7 @@ class InputToIndex:
     image_no: int = 0
     method_list: List[str] = field(default_factory=list)
     known_crystal_models: Optional[List[Crystal]] = None
+    imageset_no: int = 0
 
 
 @dataclass
@@ -49,6 +50,8 @@ class IndexingResult:
     rmsd_x: List[float] = field(default_factory=list)
     rmsd_y: List[float] = field(default_factory=list)
     rmsd_dpsi: List[float] = field(default_factory=list)
+    imageset_no: int = 0
+    unindexed_experiment: Experiment = None
 
 
 loggers_to_disable = [
@@ -167,6 +170,8 @@ def wrap_index_one(input_to_index: InputToIndex) -> IndexingResult:
             table,
             expts,
             n_strong=n_strong,
+            imageset_no=input_to_index.imageset_no,
+            unindexed_experiment=input_to_index.experiment,
         )
         for id_, identifier in table.experiment_identifiers():
             selr = table.select(table["id"] == id_)
@@ -187,6 +192,8 @@ def wrap_index_one(input_to_index: InputToIndex) -> IndexingResult:
             input_to_index.image_identifier,
             input_to_index.image_no,
             n_strong=n_strong,
+            imageset_no=input_to_index.imageset_no,
+            unindexed_experiment=input_to_index.experiment,
         )
 
     # If chosen, output a message to json to show live progress
@@ -222,16 +229,13 @@ def index_all_concurrent(
 
     # Create a suitable iterable for passing to pool.map
     n = 0
-    for iset in experiments.imagesets():
+    original_isets = list(experiments.imagesets())
+    identifiers_to_scans = {expt.identifier: expt.scan for expt in experiments}
+    for n_iset, iset in enumerate(experiments.imagesets()):
         for i in range(len(iset)):
             refl_index = i + n
             if reflections[refl_index]:
                 expt = experiments[refl_index]
-                scan = iset.get_scan()
-                if scan:
-                    idx_0 = i  # slicing index
-                    new_iset = iset[idx_0 : idx_0 + 1]
-                    expt.imageset = new_iset
                 input_iterable.append(
                     InputToIndex(
                         reflection_table=reflections[refl_index],
@@ -242,6 +246,7 @@ def index_all_concurrent(
                         ).name,
                         image_no=refl_index,
                         method_list=method_list,
+                        imageset_no=n_iset,
                     )
                 )
             else:  # experiments that have already been filtered
@@ -273,7 +278,9 @@ def index_all_concurrent(
 
     sys.stdout = sys.__stdout__
     # prepare tables for output
-    indexed_experiments, indexed_reflections = _join_indexing_results(results)
+    indexed_experiments, indexed_reflections = _join_indexing_results(
+        results, experiments, original_isets, identifiers_to_scans
+    )
 
     results_summary = _add_results_to_summary_dict(results_summary, results)
 
@@ -282,12 +289,36 @@ def index_all_concurrent(
 
 def _join_indexing_results(
     results: List[IndexingResult],
+    experiments,
+    original_isets,
+    identifiers_to_scans,
 ) -> Tuple[ExperimentList, flex.reflection_table]:
     indexed_experiments = ExperimentList()
     indexed_reflections = flex.reflection_table()
+
+    use_beam = None
+    use_gonio = None
+    if len(experiments.beams()) == 1:
+        use_beam = experiments.beams()[0]
+    if len(experiments.goniometers()):  # need a placeholder gonio
+        use_gonio = experiments.goniometers()[0]
+
     n_tot = 0
     for res in results:
         if res.n_indexed:
+            identifier = res.unindexed_experiment.identifier
+            scan = identifiers_to_scans[identifier]
+            for expt in res.experiments:
+                expt.scan = scan
+                expt.imageset = original_isets[res.imageset_no]
+                res.reflection_table["imageset_id"] = flex.int(
+                    res.reflection_table.size(), res.imageset_no
+                )
+                if use_beam:
+                    expt.beam = use_beam
+                if use_gonio:
+                    expt.goniometer = use_gonio
+
             indexed_experiments.extend(res.experiments)
             table = res.reflection_table
             ids_map = dict(table.experiment_identifiers())

src/dials/algorithms/refinement/refiner.py
@@ -974,10 +974,10 @@ class Refiner:
                 "to determine the image width for reporting RMSDs"
             )
         scan = self._experiments.scans()[0]
-        try:
-            images_per_rad = 1.0 / abs(scan.get_oscillation(deg=False)[1])
-        except AttributeError:
-            images_per_rad = None
+        images_per_rad = None
+        if scan:
+            if scan.get_oscillation(deg=False)[1] != 0.0:
+                images_per_rad = 1.0 / abs(scan.get_oscillation(deg=False)[1])
 
         for idetector, detector in enumerate(self._experiments.detectors()):
             if len(detector) == 1:

src/dials/algorithms/scaling/observers.py
@@ -305,7 +305,7 @@ def make_outlier_plots(reflection_tables, experiments):
     for j, (table, expt) in enumerate(zip(reflection_tables, experiments)):
         outliers = table.get_flags(table.flags.outlier_in_scaling)
         x, y, z = table["xyzobs.px.value"].select(outliers).parts()
-        if expt.scan:
+        if expt.scan and (expt.scan.get_oscillation()[1] != 0.0):
             zrange = [
                 i / expt.scan.get_oscillation()[1]
                 for i in expt.scan.get_oscillation_range()

src/dials/algorithms/scaling/scaler_factory.py
@@ -187,7 +187,7 @@ class SingleScalerFactory(ScalerFactory):
             experiment.scaling_model.id_ == "physical"
             and "absorption" in experiment.scaling_model.components
         ):
-            if experiment.scan:
+            if experiment.scan and (experiment.scan.get_oscillation()[1] != 0.0):
                 reflection_table = calc_crystal_frame_vectors(
                     reflection_table, experiment
                 )

src/dials/algorithms/scaling/scaling_library.py
@@ -44,7 +44,7 @@ logger = logging.getLogger("dials")
 def set_image_ranges_in_scaling_models(experiments):
     """Set the batch range in scaling models if not already set."""
     for exp in experiments:
-        if exp.scan:
+        if exp.scan and (exp.scan.get_oscillation()[1] != 0.0):
             valid_image_ranges = exp.scan.get_valid_image_ranges(exp.identifier)
             if "valid_image_range" not in exp.scaling_model.configdict:
                 # only set if not currently set i.e. set initial
@@ -226,7 +226,7 @@ def create_scaling_model(params, experiments, reflections):
         if not expt.scaling_model or params.overwrite_existing_models:
             # need to make a new model
             if use_auto_model:
-                if not expt.scan:
+                if not expt.scan or (expt.scan.get_oscillation()[1] == 0.0):
                     model = KBScalingModel
                 else:  # set model as physical unless scan < 1.0 degree
                     osc_range = expt.scan.get_oscillation_range()

src/dials/array_family/flex_ext.py
@@ -937,7 +937,11 @@ class _:
 
         compute = CorrectionsMulti()
         for experiment in experiments:
-            if experiment.goniometer is not None:
+            if (
+                experiment.goniometer is not None
+                and experiment.scan is not None
+                and (experiment.scan.get_oscillation()[1] != 0.0)
+            ):
                 compute.append(
                     Corrections(
                         experiment.beam, experiment.goniometer, experiment.detector

src/dials/command_line/ssx_integrate.py
@@ -240,6 +240,7 @@ class InputToIntegrate:
     table: flex.reflection_table
     params: Any
     crystalno: int
+    imageset_index: int = 0
 
 
 @dataclass
@@ -248,6 +249,7 @@ class IntegrationResult:
     table: flex.reflection_table
     collector: Any
     crystalno: int
+    imageset_index: int = 0
 
 
 def wrap_integrate_one(input_to_integrate: InputToIntegrate):
@@ -258,7 +260,13 @@ def wrap_integrate_one(input_to_integrate: InputToIntegrate):
         input_to_integrate.integrator_class,
     )
 
-    result = IntegrationResult(expt, refls, collector, input_to_integrate.crystalno)
+    result = IntegrationResult(
+        expt,
+        refls,
+        collector,
+        input_to_integrate.crystalno,
+        input_to_integrate.imageset_index,
+    )
     if expt and refls:
         if not input_to_integrate.params.debug.output.shoeboxes:
             del result.table["shoebox"]
@@ -292,7 +300,28 @@ def process_batch(sub_tables, sub_expts, configuration, batch_offset=0):
 
     # create iterable
     input_iterable: List[InputToIntegrate] = []
+    from dxtbx.imageset import ImageSequence, ImageSet
+
+    original_isets = list(sub_expts.imagesets())
+    identifiers_to_scans = {}
+    n_iset = 0
+    if any(sub_expts.scans()):
+        identifiers_to_scans = {expt.identifier: expt.scan for expt in sub_expts}
     for i, (table, expt) in enumerate(zip(sub_tables, sub_expts)):
+        if expt.scan:  # backcompatibility for indexed.expt without scans
+            iset = expt.imageset
+            if isinstance(iset, ImageSequence):
+                # note, need to subtract imageset offset, as imageset may no longer start from same
+                # index as the imported scan
+                idx = (
+                    expt.scan.get_array_range()[0] - iset.get_scan().get_batch_offset()
+                )
+                n_iset = original_isets.index(iset)
+                subset = iset[idx : idx + 1]
+                expt.imageset = ImageSet(
+                    subset.data(), subset.indices()
+                )  # Needed for stills integration code
+                expt.scan = None  # Needed for some aspect of integration code, unclear what exactly.
         input_iterable.append(
             InputToIntegrate(
                 configuration["process"],
@@ -300,6 +329,7 @@ def process_batch(sub_tables, sub_expts, configuration, batch_offset=0):
                 table,
                 configuration["params"],
                 i + 1 + batch_offset,
+                imageset_index=n_iset,
             )
         )
     input_iterable = sorted(input_iterable, key=lambda i: i.table.size(), reverse=True)
@@ -321,9 +351,33 @@ def process_batch(sub_tables, sub_expts, configuration, batch_offset=0):
     integrated_reflections = flex.reflection_table()
     integrated_experiments = []
 
+    use_beam = None
+    use_gonio = None
+    use_detector = None
+    if len(sub_expts.beams()) == 1:
+        use_beam = sub_expts.beams()[0]
+    if len(sub_expts.goniometers()) == 1:
+        use_gonio = sub_expts.goniometers()[0]
+    if len(sub_expts.detectors()) == 1:
+        use_detector = sub_expts.detectors()[0]
+
     n_integrated = 0
-    for result in results:
+    for result in sorted(results, key=lambda result: result.crystalno):
         if result.table:
+            if identifiers_to_scans:
+                result.experiment.scan = identifiers_to_scans[
+                    result.experiment.identifier
+                ]
+                result.experiment.imageset = original_isets[result.imageset_index]
+                result.table["imageset_id"] = flex.int(
+                    result.table.size(), result.imageset_index
+                )
+                if use_beam:
+                    result.experiment.beam = use_beam
+                if use_gonio:
+                    result.experiment.goniometer = use_gonio
+                if use_detector:
+                    result.experiment.detector = use_detector
             ids_map = dict(result.table.experiment_identifiers())
             del result.table.experiment_identifiers()[list(ids_map.keys())[0]]
             result.table["id"] = flex.int(result.table.size(), n_integrated)

commit f743a4528b86fe866326623aad6dc3bf431b2a32

Fix image viewer crash for still experiments introduced in #2556

src/dials/util/image_viewer/spotfinder_frame.py
@@ -263,8 +263,8 @@ class SpotFrame(XrayFrame):
                 self.viewing_stills = False
                 break
         for experiment_list in self.experiments:
-            if any(
-                exp.scan and (exp.scan.get_oscillation()[1] != 0.0)
+            if not all(
+                exp.scan and (exp.scan.get_oscillation()[1] == 0.0)
                 for exp in experiment_list
             ):
                 self.viewing_still_scans = False