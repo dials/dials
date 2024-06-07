97d801f051dc404cc21ba967b23b73f24d726e6f
Nicholas Devenish <ndevenish@gmail.com>
2024-06-07 08:42:36 +0100

    Fix remaining flake8 references

--- .azure-pipelines/lint-validation.py
+++ .azure-pipelines/lint-validation.py
@@ -3,11 +3,11 @@ from __future__ import annotations
 import os
 import subprocess
 
-# Flake8 validation
+# Ruff validation
 known_bad = {}
 failures = 0
 try:
-    flake8 = subprocess.run(
+    process = subprocess.run(
         [
             "ruff",
             "--exit-zero",
@@ -19,14 +19,14 @@ try:
     )
 except (subprocess.CalledProcessError, subprocess.TimeoutExpired) as e:
     print(
-        "##vso[task.logissue type=error;]flake8 validation failed with",
+        "##vso[task.logissue type=error;]Ruff validation failed with",
         str(e.__class__.__name__),
     )
     print(e.stdout)
     print(e.stderr)
-    print("##vso[task.complete result=Failed;]flake8 validation failed")
+    print("##vso[task.complete result=Failed;]Ruff validation failed")
     exit()
-for line in flake8.stdout.split("\n"):
+for line in process.stdout.split("\n"):
     if ":" not in line:
         continue
     filename, lineno, column, error = line.split(":", maxsplit=3)
@@ -42,5 +42,5 @@ for line in flake8.stdout.split("\n"):
         )
 
 if failures:
-    print(f"##vso[task.logissue type=warning]Found {failures} flake8 violation(s)")
-    print(f"##vso[task.complete result=Failed;]Found {failures} flake8 violation(s)")
+    print(f"##vso[task.logissue type=warning]Found {failures} Ruff violation(s)")
+    print(f"##vso[task.complete result=Failed;]Found {failures} Ruff violation(s)")
