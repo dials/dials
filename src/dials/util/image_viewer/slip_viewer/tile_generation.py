18a1c3dd6d20ebcede7a3c0eb8ce3c287aa0fab5
Nicholas Devenish <ndevenish@gmail.com>
2024-10-17 19:19:04 +0100

    Revert "Fix beam.get_s0() errors in image viewer for polychromatic data. (#2765)"
    This was reported as causing image viewer resolution display to be incorrect.
    
    This reverts commit dd387a90af81b5e0bc78791b01b482aa0778568e.

--- src/dials/util/image_viewer/slip_viewer/tile_generation.py
+++ src/dials/util/image_viewer/slip_viewer/tile_generation.py
@@ -438,7 +438,7 @@ class _Tiles:
         beam = self.raw_image.get_beam()
         if detector is None or beam is None:
             return None
-        beam = beam.get_unit_s0()
+        beam = beam.get_s0()
 
         if readout is None:
             return None
