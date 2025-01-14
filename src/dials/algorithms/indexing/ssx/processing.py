dials/dials

commit c6681c74cceaae1b86b51f97297c54b1dd8cd664

``dials.ssx_index``: Add better error handling if scans data input.  (#2801)
Fixes #2797

src/dials/algorithms/indexing/ssx/processing.py
@@ -483,6 +483,11 @@ def index(
             )
             params.output.nuggets = None
 
+    if any(not s.is_still() for s in experiments.scans()):
+        raise DialsIndexError(
+            "Not all experiments are stills. For rotation data, use the dials.index program."
+        )
+
     reflection_tables, params, method_list = preprocess(experiments, observed, params)
 
     # Do the indexing and generate a summary of results

commit 6a7910b1cf1f35068fb6cc3873a9e3a6bd4c7007

Fix bug introduced in #2801 for scanless experiment objects (some image formats)

src/dials/algorithms/indexing/ssx/processing.py
@@ -484,7 +484,7 @@ def index(
             )
             params.output.nuggets = None
 
-    if any(not s.is_still() for s in experiments.scans()):
+    if any(s and not s.is_still() for s in experiments.scans()):
         raise DialsIndexError(
             "Not all experiments are stills. For rotation data, use the dials.index program."
         )