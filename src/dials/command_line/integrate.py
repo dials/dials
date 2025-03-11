df4e6bdd156062a3fd5f74e693c171a00706866a
David Waterman <dgwaterman@gmail.com>
2025-03-11 12:00:49 +0000

    Use Python dict instead of ordered_set here as the latter does not work for some reason

--- src/dials/command_line/integrate.py
+++ src/dials/command_line/integrate.py
@@ -26,8 +26,6 @@ import math
 import sys
 import warnings
 
-from ordered_set import OrderedSet
-
 from dxtbx.model.experiment_list import Experiment, ExperimentList
 from libtbx.phil import parse
 
@@ -512,11 +510,11 @@ def run_integration(params, experiments, reference=None):
         force_static=params.prediction.force_static,
         padding=params.prediction.padding,
     )
-    isets = OrderedSet(e.imageset for e in experiments)
+    isets = {e.imageset: i for i, e in enumerate(experiments)}
     predicted["imageset_id"] = flex.int(predicted.size(), 0)
     if len(isets) > 1:
         for e in experiments:
-            iset_id = isets.index(e.imageset)
+            iset_id = isets[e.imageset]
             for id_ in predicted.experiment_identifiers().keys():
                 identifier = predicted.experiment_identifiers()[id_]
                 if identifier == e.identifier:
