9d70921e7a0190e426535c7a6ec3f35119bf3c09
David Waterman <dagewa@users.noreply.github.com>
2025-03-12 09:22:33 +0000

    Update src/dials/command_line/export.py

--- src/dials/command_line/export.py
+++ src/dials/command_line/export.py
@@ -24,7 +24,7 @@ integrated.refl file. For exporting scaled data, the required input is a
 scaled.expt file and a scaled.refl file, in which case the intensity=scale flag will be
 set implicitly.
 
-NXS format exports the files as an NXmx file. The required input is a
+NXS format exports the files as an NXmx file. The required input is an
 integrated.expt file and an integrated.refl file.
 
 MMCIF format exports the files as an mmcif file. The required input is an
