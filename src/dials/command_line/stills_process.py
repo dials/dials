23fed5ef03fded8b7ab36eef7e7f13458a43efc2
dwmoreau <dwmoreau@lbl.gov>
2025-04-18 09:24:50 -0700

    Prevent pickling of Processor object with multiprocessing.

--- src/dials/command_line/stills_process.py
+++ src/dials/command_line/stills_process.py
@@ -679,7 +679,10 @@ class Script:
                     imageset.clear_cache()
                 if finalize:
                     processor.finalize()
-                return processor
+                if self.params.mp.method == 'multiprocessing':
+                    return None
+                else:
+                    return processor
 
             iterable = list(zip(tags, range(len(split_experiments))))
 
@@ -750,7 +753,10 @@ class Script:
                     processor.process_experiments(tag, experiments)
                 if finalize:
                     processor.finalize()
-                return processor
+                if self.params.mp.method == 'multiprocessing':
+                    return None
+                else:
+                    return processor
 
             iterable = list(zip(tags, all_paths))
 
