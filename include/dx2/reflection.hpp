f6c6828f4d0f33f79a7b2c7f9353d6778befe5be
Dimitri Vlachos <dimitri.j.vlachos@gmail.com>
2025-05-15 19:08:00 +0100

    Add ersatz_uuid4 function to generate UUID-like identifiers

--- include/dx2/reflection.hpp
+++ include/dx2/reflection.hpp
@@ -20,10 +20,14 @@
 #include "dx2/h5/h5utils.hpp"
 #include "dx2/h5/h5write.hpp"
 #include "dx2/logging.hpp"
+#include <array>
 #include <experimental/mdspan>
 #include <functional>
+#include <iomanip>
 #include <memory>
 #include <optional>
+#include <random>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -313,6 +317,51 @@ private:
 
   template <typename T>
   struct is_column_predicate<ColumnPredicate<T>> : std::true_type {};
+
+  /**
+   * @brief Generate a pseudo-random UUID-like identifier.
+   *
+   * This function replicates the behaviour of the Python function
+   * `ersatz_uuid4` from the dxtbx library. It generates a 128-bit
+   * random value and formats it as a UUID-style string using
+   * little-endian byte order, without enforcing RFC 4122 compliance.
+   *
+   * The output is a 36-character string in the format:
+   * `xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx`, where each `x` is a
+   * hexadecimal digit.
+   *
+   * @return A string representing the generated UUID-like identifier.
+   *
+   * @note This function does not set the version or variant bits as
+   *       specified in RFC 4122. It is intended for internal use where
+   *       uniqueness is sufficient, and compliance with UUID standards
+   *       is unnecessary.
+   */
+  inline std::string ersatz_uuid4() const {
+    // Generate 16 random bytes
+    std::array<unsigned char, 16> bytes;
+    std::random_device rd;
+    std::uniform_int_distribution<int> dist(0, 255);
+    for (auto &b : bytes) {
+      b = static_cast<unsigned char>(dist(rd));
+    }
+
+    // Convert bytes to a single 128-bit hex string (little endian)
+    std::ostringstream oss;
+    for (auto it = bytes.rbegin(); it != bytes.rend(); ++it) {
+      oss << std::hex << std::setw(2) << std::setfill('0')
+          << static_cast<int>(*it);
+    }
+    std::string hex = oss.str();
+
+    // Format as UUID: 8-4-4-4-12
+    std::ostringstream uuid;
+    uuid << hex.substr(0, 8) << "-" << hex.substr(8, 4) << "-"
+         << hex.substr(12, 4) << "-" << hex.substr(16, 4) << "-"
+         << hex.substr(20, 12);
+
+    return uuid.str();
+  }
 
 public:
 #pragma region Constructors
