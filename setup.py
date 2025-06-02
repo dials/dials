7b0f00909af5e8064d7f406fff86bfe16d793b2c
Nicholas Devenish <ndevenish@gmail.com>
2025-06-02 14:31:52 +0100

    Enable Python 3.13 and and make default in bootstrap (#2946)
    * Bootstrap: Allow choosing python 3.13
    * Remove flexparser pin: This was for a pint incompatibility, which is now solved.
    * Remove python upper pin from setup.py: This often causes more trouble than it saves.
    * Make Python 3.13 the default

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup_kwargs = {
         "homepage": "https://dials.github.io",
         "repository": "https://github.com/dials/dials",
     },
-    "python_requires": ">=3.11,<3.13",
+    "python_requires": ">=3.11",
     "packages": setuptools.find_packages(where="src"),
     "package_dir": {"": "src"},
     "package_data": {
