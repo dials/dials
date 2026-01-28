1983030670bbde29d66c2e52d28fa22c1b3d897c
David Waterman <dgwaterman@gmail.com>
2026-01-28 14:02:21 +0000

    Fix for line endings issues on Windows

--- tests/algorithms/symmetry/test_refstat.py
+++ tests/algorithms/symmetry/test_refstat.py
@@ -225,10 +225,11 @@ def test_refstat_symmetry_analysis_check_file(tmp_path):
         ),
         cwd=tmp_path,
         capture_output=True,
+        text=True,  # Convert bytes to strings and normalizes line endings
     )
     assert not result.check_returncode()
     assert result.stdout.endswith(
-        b"P21/n: 40% matches\nAll matches: P21/n\nTop acentric matches: \nTop centric matches: P21/n\n"
+        "P21/n: 40% matches\nAll matches: P21/n\nTop acentric matches: \nTop centric matches: P21/n\n"
     )
 
 
@@ -242,8 +243,9 @@ def test_refstat_symmetry_analysis_dials_input(dials_data, tmp_path):
         ),
         cwd=tmp_path,
         capture_output=True,
+        text=True,  # Convert bytes to strings and normalizes line endings
     )
     assert not result.check_returncode()
     assert result.stdout.endswith(
-        b"P3221: 100% matches\nAll matches: P31, P32, P3121, P3221\nTop acentric matches: P31, P32, P3121\nTop centric matches: \n"
+        "P3221: 100% matches\nAll matches: P31, P32, P3121, P3221\nTop acentric matches: P31, P32, P3121\nTop centric matches: \n"
     )
