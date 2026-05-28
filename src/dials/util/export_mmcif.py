dials/dials

commit 69ae1ec1778589f3151715f530c7a9de242f71cc

Use calculated detector x and y position in mtz and mmcif export. (#3131)
Fixes #1568
Fixes #2976

src/dials/util/export_mmcif.py
@@ -43,7 +43,7 @@ class MMCIFOutputFile:
         self._cif = iotbx.cif.model.cif()
         self.params = params
         self._v5_next_fmt = "%6i %2i %5i %5i %-2i %-2i %-2i"
-        self._v5_0_fmt = "%2i %6i %-2i %-2i %-2i %6i %5.3f %5.3f"
+        self._v5_0_fmt = "%2i %6i %-2i %-2i %-2i %5.3f %5.3f"
 
     def write(self, experiments, reflections):
         """
@@ -210,6 +210,7 @@ class MMCIFOutputFile:
         # _diffrn_detector.type = (full name of detector e.g. DECTRIS PILATUS3 2M)
         # One date is required, so if multiple just use the first date.
         cif_block["_diffrn_detector.diffrn_id"] = 1
+        cif_block["_diffrn_detector.id"] = 1
         if epochs:  # some still expts have scans, but some don't
             min_epoch = min(epochs)
             date_str = time.strftime("%Y-%m-%d", time.gmtime(min_epoch))
@@ -256,7 +257,6 @@ class MMCIFOutputFile:
             "_diffrn_refln.index_h",
             "_diffrn_refln.index_k",
             "_diffrn_refln.index_l",
-            "_diffrn_refln.pdbx_image_id",
             "_diffrn_refln.pdbx_detector_x",
             "_diffrn_refln.pdbx_detector_y",
         )
@@ -381,6 +381,75 @@ class MMCIFOutputFile:
             merged_block.update(merged_data)
             cif_block.update(merged_block)
 
+        # Write the necessary metadata to link the scans to the detector/dataset
+        cif_loop = iotbx.cif.model.loop(
+            header=(
+                "_diffrn_detector_element.detector_id",
+                "_diffrn_detector_element.id",
+            )
+        )
+        cif_loop.add_row((1, 1))
+        cif_block.add_loop(cif_loop)
+        detector_axis_cif_loop = iotbx.cif.model.loop(
+            header=(
+                "_diffrn_detector_axis.detector_id",
+                "_diffrn_detector_axis.axis_id",
+            )
+        )
+        diffrn_frame_cif_loop = iotbx.cif.model.loop(
+            header=(
+                "_diffrn_data_frame.detector_element_id",
+                "_diffrn_data_frame.id",
+            )
+        )
+
+        cif_loop = iotbx.cif.model.loop(
+            header=(
+                "_diffrn_scan_axis.scan_id",
+                "_diffrn_scan_axis.axis_id",
+                "_diffrn_scan_axis.angle_start",
+                "_diffrn_scan_axis.angle_increment",
+            )
+        )
+        scan_loop = iotbx.cif.model.loop(
+            header=[
+                "_diffrn_scan.id",
+                "_diffrn_scan.frame_id_start",
+                "_diffrn_scan.frame_id_end",
+                "_diffrn_scan.frames",
+            ]
+        )
+
+        for i, exp in enumerate(experiments):
+            scan = exp.scan
+            image_range = scan.get_image_range()
+            start, increment = scan.get_oscillation(deg=True)
+            scan_loop.add_row(
+                (
+                    i + 1,
+                    f"scan_{i + 1}_frame_start",
+                    f"scan_{i + 1}_frame_end",
+                    image_range[1] - image_range[0] + 1,
+                )
+            )
+            # for h, v in zip(header, vals):
+            #    cif_block[h] = v
+            cif_loop.add_row(
+                (
+                    i + 1,
+                    i + 1,
+                    start,
+                    increment,
+                )
+            )
+            diffrn_frame_cif_loop.add_row((1, f"scan_{i + 1}_frame_start"))
+            diffrn_frame_cif_loop.add_row((1, f"scan_{i + 1}_frame_end"))
+            detector_axis_cif_loop.add_row((1, i + 1))
+        cif_block.add_loop(diffrn_frame_cif_loop)
+        cif_block.add_loop(detector_axis_cif_loop)
+        cif_block.add_loop(scan_loop)
+        cif_block.add_loop(cif_loop)
+
         # Write the crystal information
         # if v5, that's all so return
         if self.params.mmcif.pdb_version == "v5":
@@ -388,17 +457,15 @@ class MMCIFOutputFile:
                 hkl.iround()
                 for hkl in reflections["miller_index"].as_vec3_double().parts()
             )
-            # Note, use observed position, so that we are within the
-            # allowed bounds (lower bound 0) for image_id
-            det_x, det_y, det_z = reflections["xyzobs.px.value"].parts()
-            image_id = flex.ceil(det_z).iround()
+            # Det_x, det_y are expected to be calculated positions
+            # according to the dictionary definition.
+            det_x, det_y, _ = reflections["xyzcal.px"].parts()
             loop_values = [
                 flex.int(len(reflections), 1),  # diffn id
                 flex.size_t_range(1, len(reflections) + 1),  # refln id
                 h,
                 k,
                 l,
-                image_id,
                 det_x,
                 det_y,
             ] + [reflections[name] for name in variables_present]

src/dials/util/batch_handling.py
@@ -94,7 +94,7 @@ class batch_manager:
 def assign_batches_to_reflections(reflections, batch_offsets):
     """Assign a 'batch' column to the reflection table"""
     for batch_offset, refl in zip(batch_offsets, reflections):
-        xdet, ydet, zdet = (flex.double(x) for x in refl["xyzobs.px.value"].parts())
+        _, _, zdet = (flex.double(x) for x in refl["xyzobs.px.value"].parts())
         # compute BATCH values - floor() to get (fortran) image captured within
         #                        +1     because FORTRAN counting; zdet+1=image_index
         #                        +off   because            image_index+o=batch

src/dials/util/export_mtz.py
@@ -361,9 +361,7 @@ def write_columns(mtz, reflection_table):
 
     nref = len(reflection_table["miller_index"])
     assert nref
-    xdet, ydet, _ = (
-        flex.double(x) for x in reflection_table["xyzobs.px.value"].parts()
-    )
+    xdet, ydet, _ = (flex.double(x) for x in reflection_table["xyzcal.px"].parts())
 
     type_table = {
         "H": "H",

commit 4ba46e93f8b5b8aac40d4d5623b1aebdb604cd14

Fix mmcif export crash on ssx data introduced in #3131

src/dials/util/export_mmcif.py
@@ -468,8 +468,12 @@ class MMCIFOutputFile:
 
         for i, exp in enumerate(experiments):
             scan = exp.scan
-            image_range = scan.get_image_range()
-            start, increment = scan.get_oscillation(deg=True)
+            if scan:
+                image_range = scan.get_image_range()
+                start, increment = scan.get_oscillation(deg=True)
+            else:
+                image_range = (1, 1)
+                start, increment = (0, 0)
             scan_loop.add_row(
                 (
                     i + 1,
@@ -566,8 +570,12 @@ class MMCIFOutputFile:
         for i, exp in enumerate(experiments):
             scan = exp.scan
             crystal_id = crystal_to_id[exp.crystal]
-            image_range = scan.get_image_range()
-            osc_range = scan.get_oscillation_range(deg=True)
+            if scan:
+                image_range = scan.get_image_range()
+                osc_range = scan.get_oscillation_range(deg=True)
+            else:
+                image_range = (1, 1)
+                osc_range = (0, 0)
             cif_loop.add_row(
                 (
                     i + 1,