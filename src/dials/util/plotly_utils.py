a782309024694d24601e3732813758cd355ce679
David Waterman <dgwaterman@gmail.com>
2026-06-12 10:16:42 +0100

    Increase default precision of round_for_json to 4 digits

--- src/dials/util/plotly_utils.py
+++ src/dials/util/plotly_utils.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import Sequence
 
 
-def round_for_json(seq: Sequence, ndigits: int = 3) -> list:
+def round_for_json(seq: Sequence, ndigits: int = 4) -> list:
     """
     Round floating-point numbers in a sequence to a specified number of digits
     for JSON serialization (https://github.com/dials/dials/issues/3190)
@@ -13,7 +13,7 @@ def round_for_json(seq: Sequence, ndigits: int = 3) -> list:
     seq : Sequence
         The sequence of numbers to round.
     ndigits : int, optional
-        The number of digits to round to (default is 3).
+        The number of digits to round to (default is 4).
 
     Returns
     -------
