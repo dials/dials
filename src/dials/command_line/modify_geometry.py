4f566012f59f33f16c3834266175edf88f424e35
James Beilsten-Edmands <30625594+jbeilstenedmands@users.noreply.github.com>
2026-07-24 19:33:58 +0200

    allow updating with a reference in dials.modify_geometry

--- src/dials/command_line/modify_geometry.py
+++ src/dials/command_line/modify_geometry.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
+import copy
 import warnings
 
 import libtbx.phil
 from dxtbx.model import ExperimentList
+from dxtbx.serialize import load
 
 import dials.util
 from dials.command_line.dials_import import ManualGeometryUpdater
@@ -15,6 +17,27 @@ help_message = """
 phil_scope = libtbx.phil.parse(
     """
 include scope dials.util.options.geometry_phil_scope
+input {
+   reference_geometry = None
+       .type = path
+        help = "Experimental geometry from this models.expt "
+              "will override the geometry from the input file"
+    use_beam_reference = True
+      .type = bool
+      .expert_level = 2
+      .help = "If True, the beam from reference_geometry will override "
+              "the beam from the input file."
+    use_gonio_reference = True
+      .type = bool
+      .expert_level = 2
+      .help = "If True, the goniometer from reference_geometry will override "
+              "the goniometer from the input file."
+    use_detector_reference = True
+      .type = bool
+      .expert_level = 2
+      .help = "If True, the detector from reference_geometry will override "
+              "the detector from the input file"
+}
 output {
   experiments = modified.expt
     .type = path
@@ -65,6 +88,33 @@ def run(args: list[str] = None, phil: libtbx.phil.scope = phil_scope) -> None:
         parser.print_help()
         exit(0)
 
+    if params.input.reference_geometry:
+        ref_expts = load.experiment_list(params.input.reference_geometry)
+        if params.input.use_detector_reference:
+            ref_det = ref_expts[0].detector
+            current_detectors = experiments.detectors()
+            # we want to retain the structure of the models i.e. shared or not.
+            new_detectors = [
+                copy.deepcopy(ref_det) for _ in range(len(current_detectors))
+            ]
+            for expt in experiments:
+                expt.detector = new_detectors[current_detectors.index(expt.detector)]
+        if params.input.use_beam_reference:
+            ref_beam = ref_expts[0].beam
+            current_beams = experiments.beams()
+            # we want to retain the structure of the models i.e. shared or not.
+            new_beams = [copy.deepcopy(ref_beam) for _ in range(len(current_beams))]
+            for expt in experiments:
+                expt.beam = new_beams[current_beams.index(expt.beam)]
+        if params.input.use_gonio_reference:
+            ref_gonio = ref_expts[0].goniometer
+            current_gonios = experiments.goniometers()
+            # we want to retain the structure of the models i.e. shared or not.
+            new_gonios = [copy.deepcopy(ref_gonio) for _ in range(len(current_gonios))]
+            for expt in experiments:
+                expt.goniometer = new_gonios[current_gonios.index(expt.goniometer)]
+
+    # update with any manual parameters set.
     new_experiments = update(experiments, params)
 
     if len(new_experiments):
