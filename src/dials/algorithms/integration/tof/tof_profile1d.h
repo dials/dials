a0a8624f96c91b3aa123b3100378343e7ff8b4aa
davidmcdonagh <davidnmcdonagh@gmail.com>
2026-07-31 13:31:36 +0100

    Add old files for deprecation warnings at compile time

---
+++ src/dials/algorithms/integration/tof/tof_profile1d.h
@@ -0,0 +1,10 @@
+#ifndef DIALS_ALGORITHMS_INTEGRATION_TOF_TOF_PROFILE1D_H
+#define DIALS_ALGORITHMS_INTEGRATION_TOF_TOF_PROFILE1D_H
+
+#warning \
+  "tof_profile1d.h is deprecated and will be removed in a future release; " \
+  "include tof_profile_1d_ibix.h instead."
+
+#include <dials/algorithms/integration/tof/tof_profile_1d_ibix.h>
+
+#endif /* DIALS_ALGORITHMS_INTEGRATION_TOF_TOF_PROFILE1D_H */
---
+++ src/dials/algorithms/integration/tof/tof_profile3d.h
@@ -0,0 +1,10 @@
+#ifndef DIALS_ALGORITHMS_INTEGRATION_TOF_TOF_PROFILE3D_H
+#define DIALS_ALGORITHMS_INTEGRATION_TOF_TOF_PROFILE3D_H
+
+#warning \
+  "tof_profile3d.h is deprecated and will be removed in a future release; " \
+  "include tof_profile_3d_gutmann.h instead."
+
+#include <dials/algorithms/integration/tof/tof_profile_3d_gutmann.h>
+
+#endif /* DIALS_ALGORITHMS_INTEGRATION_TOF_TOF_PROFILE3D_H */
