f9df992b94e70c761659176072672deea613f3ec
David McDonagh <60879630+toastisme@users.noreply.github.com>
2026-09-02 09:55:29 +0100

    tof_integration.h refactor (#3264)
    * Refactor of tof_integration.h

--- src/dials/algorithms/integration/tof/boost_python/tof_integration_ext.cc
+++ src/dials/algorithms/integration/tof/boost_python/tof_integration_ext.cc
@@ -11,6 +11,24 @@ namespace dials { namespace algorithms { namespace boost_python {
 
   using namespace boost::python;
 
+  std::shared_ptr<ProfileFitter> make_profile_fitter(
+    object profile_1d_ibix_params_obj,
+    object profile_3d_gutmann_params_obj) {
+    // Only one profile fitting method allowed
+    DIALS_ASSERT(!(!profile_1d_ibix_params_obj.is_none()
+                   && !profile_3d_gutmann_params_obj.is_none()));
+
+    if (!profile_1d_ibix_params_obj.is_none()) {
+      return std::make_shared<Profile1DIBIXFitter>(
+        extract<TOFProfile1DIBIXParams>(profile_1d_ibix_params_obj));
+    }
+    if (!profile_3d_gutmann_params_obj.is_none()) {
+      return std::make_shared<Profile3DGutmannFitter>(
+        extract<TOFProfile3DGutmannParams>(profile_3d_gutmann_params_obj));
+    }
+    return nullptr;
+  }
+
   void integrate_reflection_table_wrapper(dials::af::reflection_table& reflection_table,
                                           dxtbx::model::Experiment& experiment,
                                           dxtbx::ImageSequence& data,
@@ -20,61 +38,134 @@ namespace dials { namespace algorithms { namespace boost_python {
                                           int n_threads,
                                           object profile_1d_ibix_params_obj,
                                           object profile_3d_gutmann_params_obj) {
-    boost::optional<TOFProfile1DIBIXParams> profile_1d_ibix_params;
-    boost::optional<TOFProfile3DGutmannParams> profile_3d_gutmann_params;
-
-    if (!profile_1d_ibix_params_obj.is_none()) {
-      profile_1d_ibix_params =
-        extract<TOFProfile1DIBIXParams>(profile_1d_ibix_params_obj);
-    }
-
-    if (!profile_3d_gutmann_params_obj.is_none()) {
-      profile_3d_gutmann_params =
-        extract<TOFProfile3DGutmannParams>(profile_3d_gutmann_params_obj);
-    }
-
-    if (absorption_params_obj.is_none() && incident_params_obj.is_none()) {
-      integrate_reflection_table(reflection_table,
-                                 experiment,
-                                 data,
-                                 apply_lorentz,
-                                 n_threads,
-                                 profile_1d_ibix_params,
-                                 profile_3d_gutmann_params);
-
-      return;
-    }
+    boost::optional<dials_scaling::TOFIncidentSpectrumParams> incident_params;
+    boost::optional<dials_scaling::TOFAbsorptionParams> absorption_params;
 
     if (!incident_params_obj.is_none()) {
-      dials_scaling::TOFIncidentSpectrumParams incident_params =
+      incident_params =
         extract<dials_scaling::TOFIncidentSpectrumParams>(incident_params_obj);
-
-      if (!absorption_params_obj.is_none()) {
-        dials_scaling::TOFAbsorptionParams absorption_params =
-          extract<dials_scaling::TOFAbsorptionParams>(absorption_params_obj);
-
-        integrate_reflection_table(reflection_table,
-                                   experiment,
-                                   data,
-                                   incident_params,
-                                   absorption_params,
-                                   apply_lorentz,
-                                   n_threads,
-                                   profile_1d_ibix_params,
-                                   profile_3d_gutmann_params);
-      }
-
-      else {
-        integrate_reflection_table(reflection_table,
-                                   experiment,
-                                   data,
-                                   incident_params,
-                                   apply_lorentz,
-                                   n_threads,
-                                   profile_1d_ibix_params,
-                                   profile_3d_gutmann_params);
-      }
     }
+    if (!absorption_params_obj.is_none()) {
+      absorption_params =
+        extract<dials_scaling::TOFAbsorptionParams>(absorption_params_obj);
+    }
+
+    std::shared_ptr<ProfileFitter> profile_fitter =
+      make_profile_fitter(profile_1d_ibix_params_obj, profile_3d_gutmann_params_obj);
+
+    integrate_reflection_table(reflection_table,
+                               experiment,
+                               data,
+                               incident_params,
+                               absorption_params,
+                               apply_lorentz,
+                               n_threads,
+                               profile_fitter);
+  }
+
+  void extract_correction_params(
+    object incident_params_obj,
+    object absorption_params_obj,
+    boost::optional<dials_scaling::TOFIncidentSpectrumParams>& incident_params,
+    boost::optional<dials_scaling::TOFAbsorptionParams>& absorption_params) {
+    if (!incident_params_obj.is_none()) {
+      incident_params =
+        extract<dials_scaling::TOFIncidentSpectrumParams>(incident_params_obj);
+    }
+    if (!absorption_params_obj.is_none()) {
+      absorption_params =
+        extract<dials_scaling::TOFAbsorptionParams>(absorption_params_obj);
+    }
+  }
+
+  boost::python::tuple calculate_line_profile_for_reflection_wrapper(
+    dials::af::reflection_table& reflection,
+    dxtbx::model::Experiment& experiment,
+    dxtbx::ImageSequence& data,
+    object incident_params_obj,
+    object absorption_params_obj,
+    scitbx::af::shared<double> raw_projected_intensity_out,
+    scitbx::af::shared<double> projected_intensity_out,
+    scitbx::af::shared<double> projected_background_out,
+    scitbx::af::shared<double> tof_z_out,
+    const bool& apply_lorentz_correction) {
+    boost::optional<dials_scaling::TOFIncidentSpectrumParams> incident_params;
+    boost::optional<dials_scaling::TOFAbsorptionParams> absorption_params;
+    extract_correction_params(
+      incident_params_obj, absorption_params_obj, incident_params, absorption_params);
+
+    return calculate_line_profile_for_reflection(reflection,
+                                                 experiment,
+                                                 data,
+                                                 incident_params,
+                                                 absorption_params,
+                                                 raw_projected_intensity_out,
+                                                 projected_intensity_out,
+                                                 projected_background_out,
+                                                 tof_z_out,
+                                                 apply_lorentz_correction);
+  }
+
+  boost::python::tuple calculate_line_profile_for_reflection_1d_wrapper(
+    dials::af::reflection_table& reflection,
+    dxtbx::model::Experiment& experiment,
+    dxtbx::ImageSequence& data,
+    object incident_params_obj,
+    object absorption_params_obj,
+    scitbx::af::shared<double> raw_projected_intensity_out,
+    scitbx::af::shared<double> projected_intensity_out,
+    scitbx::af::shared<double> projected_background_out,
+    scitbx::af::shared<double> tof_z_out,
+    scitbx::af::shared<double> line_profile_out,
+    const bool& apply_lorentz_correction,
+    TOFProfile1DIBIXParams& profile_params_1d_ibix) {
+    boost::optional<dials_scaling::TOFIncidentSpectrumParams> incident_params;
+    boost::optional<dials_scaling::TOFAbsorptionParams> absorption_params;
+    extract_correction_params(
+      incident_params_obj, absorption_params_obj, incident_params, absorption_params);
+
+    return calculate_line_profile_for_reflection(reflection,
+                                                 experiment,
+                                                 data,
+                                                 incident_params,
+                                                 absorption_params,
+                                                 raw_projected_intensity_out,
+                                                 projected_intensity_out,
+                                                 projected_background_out,
+                                                 tof_z_out,
+                                                 line_profile_out,
+                                                 apply_lorentz_correction,
+                                                 profile_params_1d_ibix);
+  }
+
+  boost::python::tuple calculate_line_profile_for_reflection_3d_wrapper(
+    dials::af::reflection_table& reflection,
+    dxtbx::model::Experiment& experiment,
+    dxtbx::ImageSequence& data,
+    object incident_params_obj,
+    object absorption_params_obj,
+    scitbx::af::shared<double> raw_projected_intensity_out,
+    scitbx::af::shared<double> projected_intensity_out,
+    scitbx::af::shared<double> projected_background_out,
+    scitbx::af::shared<double> tof_z_out,
+    const bool& apply_lorentz_correction,
+    TOFProfile3DGutmannParams& profile_params_3d_gutmann) {
+    boost::optional<dials_scaling::TOFIncidentSpectrumParams> incident_params;
+    boost::optional<dials_scaling::TOFAbsorptionParams> absorption_params;
+    extract_correction_params(
+      incident_params_obj, absorption_params_obj, incident_params, absorption_params);
+
+    return calculate_line_profile_for_reflection_3d(reflection,
+                                                    experiment,
+                                                    data,
+                                                    incident_params,
+                                                    absorption_params,
+                                                    raw_projected_intensity_out,
+                                                    projected_intensity_out,
+                                                    projected_background_out,
+                                                    tof_z_out,
+                                                    apply_lorentz_correction,
+                                                    profile_params_3d_gutmann);
   }
 
   BOOST_PYTHON_MODULE(dials_algorithms_tof_integration_ext) {
@@ -135,95 +226,46 @@ namespace dials { namespace algorithms { namespace boost_python {
          arg("profile_1d_ibix_params") = object()));
 
     def("calculate_line_profile_for_reflection",
-        static_cast<boost::python::tuple (*)(dials::af::reflection_table&,
-                                             dxtbx::model::Experiment&,
-                                             dxtbx::ImageSequence&,
-                                             scitbx::af::shared<double>,
-                                             scitbx::af::shared<double>,
-                                             scitbx::af::shared<double>,
-                                             scitbx::af::shared<double>,
-                                             const bool&)>(
-          &calculate_line_profile_for_reflection));
+        &calculate_line_profile_for_reflection_wrapper,
+        (arg("reflection"),
+         arg("experiment"),
+         arg("data"),
+         arg("incident_params") = object(),
+         arg("absorption_params") = object(),
+         arg("raw_projected_intensity_out"),
+         arg("projected_intensity_out"),
+         arg("projected_background_out"),
+         arg("tof_z_out"),
+         arg("apply_lorentz_correction")));
+
+    def("calculate_line_profile_for_reflection",
+        &calculate_line_profile_for_reflection_1d_wrapper,
+        (arg("reflection"),
+         arg("experiment"),
+         arg("data"),
+         arg("incident_params"),
+         arg("absorption_params"),
+         arg("raw_projected_intensity_out"),
+         arg("projected_intensity_out"),
+         arg("projected_background_out"),
+         arg("tof_z_out"),
+         arg("line_profile_out"),
+         arg("apply_lorentz_correction"),
+         arg("profile_params_1d_ibix")));
 
     def("calculate_line_profile_for_reflection_3d",
-        static_cast<boost::python::tuple (*)(dials::af::reflection_table&,
-                                             dxtbx::model::Experiment&,
-                                             dxtbx::ImageSequence&,
-                                             scitbx::af::shared<vec3<double> >,
-                                             scitbx::af::shared<double>,
-                                             scitbx::af::shared<double>,
-                                             scitbx::af::shared<double>,
-                                             scitbx::af::shared<double>,
-                                             const bool&,
-                                             TOFProfile3DGutmannParams&)>(
-          &calculate_line_profile_for_reflection_3d));
-
-    def("calculate_line_profile_for_reflection",
-        static_cast<boost::python::tuple (*)(dials::af::reflection_table&,
-                                             dxtbx::model::Experiment&,
-                                             dxtbx::ImageSequence&,
-                                             scitbx::af::shared<double>,
-                                             scitbx::af::shared<double>,
-                                             scitbx::af::shared<double>,
-                                             scitbx::af::shared<double>,
-                                             scitbx::af::shared<double>,
-                                             const bool&,
-                                             TOFProfile1DIBIXParams&)>(
-          &calculate_line_profile_for_reflection));
-
-    def("calculate_line_profile_for_reflection",
-        static_cast<boost::python::tuple (*)(
-          dials::af::reflection_table&,
-          dxtbx::model::Experiment&,
-          dxtbx::ImageSequence&,
-          const dials_scaling::TOFIncidentSpectrumParams&,
-          scitbx::af::shared<double>,
-          scitbx::af::shared<double>,
-          scitbx::af::shared<double>,
-          scitbx::af::shared<double>,
-          const bool&)>(&calculate_line_profile_for_reflection));
-
-    def("calculate_line_profile_for_reflection",
-        static_cast<boost::python::tuple (*)(
-          dials::af::reflection_table&,
-          dxtbx::model::Experiment&,
-          dxtbx::ImageSequence&,
-          const dials_scaling::TOFIncidentSpectrumParams&,
-          scitbx::af::shared<double>,
-          scitbx::af::shared<double>,
-          scitbx::af::shared<double>,
-          scitbx::af::shared<double>,
-          scitbx::af::shared<double>,
-          const bool&,
-          TOFProfile1DIBIXParams&)>(&calculate_line_profile_for_reflection));
-
-    def("calculate_line_profile_for_reflection",
-        static_cast<boost::python::tuple (*)(
-          dials::af::reflection_table&,
-          dxtbx::model::Experiment&,
-          dxtbx::ImageSequence&,
-          const dials_scaling::TOFIncidentSpectrumParams&,
-          const dials_scaling::TOFAbsorptionParams&,
-          scitbx::af::shared<double>,
-          scitbx::af::shared<double>,
-          scitbx::af::shared<double>,
-          scitbx::af::shared<double>,
-          const bool&)>(&calculate_line_profile_for_reflection));
-
-    def("calculate_line_profile_for_reflection",
-        static_cast<boost::python::tuple (*)(
-          dials::af::reflection_table&,
-          dxtbx::model::Experiment&,
-          dxtbx::ImageSequence&,
-          const dials_scaling::TOFIncidentSpectrumParams&,
-          const dials_scaling::TOFAbsorptionParams&,
-          scitbx::af::shared<double>,
-          scitbx::af::shared<double>,
-          scitbx::af::shared<double>,
-          scitbx::af::shared<double>,
-          scitbx::af::shared<double>,
-          const bool&,
-          TOFProfile1DIBIXParams&)>(&calculate_line_profile_for_reflection));
+        &calculate_line_profile_for_reflection_3d_wrapper,
+        (arg("reflection"),
+         arg("experiment"),
+         arg("data"),
+         arg("incident_params"),
+         arg("absorption_params"),
+         arg("raw_projected_intensity_out"),
+         arg("projected_intensity_out"),
+         arg("projected_background_out"),
+         arg("tof_z_out"),
+         arg("apply_lorentz_correction"),
+         arg("profile_params_3d_gutmann")));
   }
 
 }}}  // namespace dials::algorithms::boost_python
